b7f62724deb33d5186afabfed6de569a371f7b75
Tristram Norman <tristram@simprints.com>
2016-10-12 18:25:36 +0100

    working on the walton!

--- id/src/main/java/com/simprints/id/tools/InternalConstants.java
+++ id/src/main/java/com/simprints/id/tools/InternalConstants.java
@@ -3,13 +3,17 @@ package com.simprints.id.tools;
 public class InternalConstants {
 
     public final static String ALERT_TYPE_EXTRA = "alertType";
-    public final static int RESULT_TRY_AGAIN = 100;
     public final static String COMMCARE_PACKAGE = "org.commcare.dalvik";
     public final static String COMMCARE_PERMISSION = "org.commcare.dalvik.provider.cases.read";
 
-    public final static int LOCATION_PERMISSION_REQUEST = 1;
-    public final static int RESOLUTION_REQUEST = 2;
-    public final static int GOOGLE_SERVICE_UPDATE_REQUEST = 3;
+    public final static int LOCATION_PERMISSION_REQUEST = 11;
+    public final static int RESOLUTION_REQUEST = 22;
+    public final static int GOOGLE_SERVICE_UPDATE_REQUEST = 33;
+
+    public final static int MAIN_ACTIVITY_REQUEST = 44;
+    public final static int ALERT_ACTIVITY_REQUEST = 55;
+
+    public final static int RESULT_TRY_AGAIN = 100;
 
     //Version numbers
     public final static int DATABASE_VERSION_NUMBER = 4;
