a13c66b368a095fa6d527110a8bc5391a2f08986
Tristram Norman <tristram@simprints.com>
2016-11-28 00:06:13 +0000

    add in Firebase to project. Move permission manager

--- id/src/main/java/com/simprints/id/tools/PermissionManager.java
+++ id/src/main/java/com/simprints/id/tools/PermissionManager.java
@@ -2,9 +2,10 @@ package com.simprints.id.tools;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.v4.app.ActivityCompat;
-
-import static com.simprints.id.activities.FrontActivity.hasPermissions;
 
 public class PermissionManager {
 
@@ -17,6 +18,17 @@ public class PermissionManager {
             return false;
         }
 
+        return true;
+    }
+
+    private static boolean hasPermissions(Context context, String... permissions) {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
         return true;
     }
 }
