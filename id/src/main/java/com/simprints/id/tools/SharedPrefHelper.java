e9d69c244dd561603e3d4ab0bf892ac1273c3fed
Tristram <tristram@simprints.com>
2017-01-24 13:04:25 +0000

    take the pref strings out or xml/Strings.

--- id/src/main/java/com/simprints/id/tools/SharedPrefHelper.java
+++ id/src/main/java/com/simprints/id/tools/SharedPrefHelper.java
@@ -1,19 +1,25 @@
 package com.simprints.id.tools;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import com.simprints.id.R;
-
 
 public class SharedPrefHelper {
-    private Context context;
     private SharedPreferences sharedPref;
 
+    private static final String preference_file_key = "b3f0cf9b-4f3f-4c5b-bf85-7b1f44eddd7a";
+    private static final String nudge_mode_bool = "NudgeModeBool";
+    private static final String consent_bool = "ConsentBool";
+    private static final String quality_threshold = "QualityThresholdInt";
+    private static final String nb_of_ids = "NbOfIdsInt";
+    private static final String language = "SelectedLanguage";
+    private static final String language_position = "SelectedLanguagePosition";
+    private static final String db_version_int = "DbVersionInt";
+    private static final String matcher_type = "MatcherType";
+    private static final String timeout_int = "TimeoutInt";
+
     public SharedPrefHelper(Context context) {
-        this.context = context;
-        sharedPref = context.getSharedPreferences(context.getString(R.string.preference_file_key),
+        sharedPref = context.getSharedPreferences(preference_file_key,
                 Context.MODE_PRIVATE);
     }
 
@@ -21,12 +27,12 @@ public class SharedPrefHelper {
      * Nudge mode. This determines if the UI should automatically slide forward
      */
     public Boolean getNudgeModeBool() {
-        return sharedPref.getBoolean(context.getString(R.string.pref_nudge_mode_bool), true);
+        return sharedPref.getBoolean(nudge_mode_bool, true);
     }
 
     public void setNudgeModeBool(Boolean nudgeModeBool) {
         SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putBoolean(context.getString(R.string.pref_nudge_mode_bool), nudgeModeBool);
+        editor.putBoolean(nudge_mode_bool, nudgeModeBool);
         editor.apply();
     }
 
@@ -34,12 +40,12 @@ public class SharedPrefHelper {
      * Consent. Has the CHW given consent to use Simprints ID
      */
     public Boolean getConsentBool() {
-        return sharedPref.getBoolean(context.getString(R.string.pref_consent_bool), true);
+        return sharedPref.getBoolean(consent_bool, true);
     }
 
     public void setConsentBool(Boolean consentBool) {
         SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putBoolean(context.getString(R.string.pref_consent_bool), consentBool);
+        editor.putBoolean(consent_bool, consentBool);
         editor.apply();
     }
 
@@ -47,12 +53,12 @@ public class SharedPrefHelper {
      * Quality threshold. This determines the UI feedback for the fingerprint quality
      */
     public int getQualityThresholdInt() {
-        return sharedPref.getInt(context.getString(R.string.pref_quality_theshold), 60);
+        return sharedPref.getInt(quality_threshold, 60);
     }
 
     public void setQualityThresholdInt(int qualityThreshold) {
         SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putInt(context.getString(R.string.pref_quality_theshold), qualityThreshold);
+        editor.putInt(quality_threshold, qualityThreshold);
         editor.apply();
     }
 
@@ -60,12 +66,12 @@ public class SharedPrefHelper {
      * The number of UIDs to be returned to the calling app
      */
     public int getReturnIdCountInt() {
-        return sharedPref.getInt(context.getString(R.string.pref_nb_of_ids), 10);
+        return sharedPref.getInt(nb_of_ids, 10);
     }
 
     public void setReturnIdCountInt(int returnIdCount) {
         SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putInt(context.getString(R.string.pref_nb_of_ids), returnIdCount);
+        editor.putInt(nb_of_ids, returnIdCount);
         editor.apply();
     }
 
@@ -73,12 +79,12 @@ public class SharedPrefHelper {
      * The selected language
      */
     public String getLanguageString() {
-        return sharedPref.getString(context.getString(R.string.pref_language), "");
+        return sharedPref.getString(language, "");
     }
 
     public void setLanguageString(String languageString) {
         SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putString(context.getString(R.string.pref_language), languageString);
+        editor.putString(language, languageString);
         editor.apply();
     }
 
@@ -86,12 +92,12 @@ public class SharedPrefHelper {
      * The active language position to be displayed in the list
      */
     public int getLanguagePositionInt() {
-        return sharedPref.getInt(context.getString(R.string.pref_language_position), 0);
+        return sharedPref.getInt(language_position, 0);
     }
 
     public void setLanguagePositionInt(int languagePositionInt) {
         SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putInt(context.getString(R.string.pref_language_position), languagePositionInt);
+        editor.putInt(language_position, languagePositionInt);
         editor.apply();
     }
 
@@ -99,12 +105,12 @@ public class SharedPrefHelper {
      * The current database version of LibData
      */
     public int getDbVersionInt() {
-        return sharedPref.getInt(context.getString(R.string.db_version_int), 0);
+        return sharedPref.getInt(db_version_int, 0);
     }
 
     public void setDbVersionInt(int dbVersionInt) {
         SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putInt(context.getString(R.string.db_version_int), dbVersionInt);
+        editor.putInt(db_version_int, dbVersionInt);
         editor.apply();
     }
 
@@ -112,13 +118,25 @@ public class SharedPrefHelper {
      * Matcher type
      */
     public int getMatcherTypeInt() {
-        return sharedPref.getInt(context.getString(R.string.pref_matcher_type), 0);
+        return sharedPref.getInt(matcher_type, 0);
     }
 
     public void setMatcherTypeInt(int matcherTypeInt) {
         SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putInt(context.getString(R.string.pref_matcher_type), matcherTypeInt);
+        editor.putInt(matcher_type, matcherTypeInt);
         editor.apply();
     }
 
+    /**
+     * Timeout seconds
+     */
+    public int getTimeoutInt() {
+        return sharedPref.getInt(timeout_int, 3);
+    }
+
+    public void setTimeoutInt(int timeoutInt) {
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(timeout_int, timeoutInt);
+        editor.apply();
+    }
 }
