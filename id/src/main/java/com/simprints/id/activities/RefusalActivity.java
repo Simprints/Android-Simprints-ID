0f7bcdb7cf5b56db9b6f3db5754d6d6da83d5fd8
Tristram Norman <tristram@simprints.com>
2017-02-08 23:24:26 +0000

    refusal form first draft

---
+++ id/src/main/java/com/simprints/id/activities/RefusalActivity.java
@@ -0,0 +1,15 @@
+package com.simprints.id.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.simprints.id.R;
+
+public class RefusalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refusal);
+    }
+}
