d3ed8331c9fd9f91e6a21d79c496994537a3c7bb
Ridwan <arfarouki@gmail.com>
2017-04-21 13:53:22 +0100

    added calls to saveVerification in libdata and returning of results of verification

--- id/src/main/java/com/simprints/id/activities/MatchingActivity.java
+++ id/src/main/java/com/simprints/id/activities/MatchingActivity.java
@@ -18,6 +18,7 @@ import com.simprints.id.tools.SharedPref;
 import com.simprints.libcommon.Person;
 import com.simprints.libdata.DATA_ERROR;
 import com.simprints.libdata.DataCallback;
+import com.simprints.libdata.models.enums.VERIFY_GUID_EXISTS_RESULT;
 import com.simprints.libmatcher.EVENT;
 import com.simprints.libmatcher.LibMatcher;
 import com.simprints.libmatcher.Progress;
@@ -26,6 +27,7 @@ import com.simprints.libsimprints.Constants;
 import com.simprints.libsimprints.Identification;
 import com.simprints.libsimprints.Tier;
 import com.simprints.libdata.tools.Constants.GROUP;
+import com.simprints.libsimprints.Verification;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -166,45 +168,80 @@ public class MatchingActivity extends AppCompatActivity implements MatcherEventL
     @Override
     public void onMatcherEvent(EVENT event) {
         switch (event) {
-            case MATCH_NOT_RUNNING:
+            case MATCH_NOT_RUNNING: {
                 Toast.makeText(MatchingActivity.this, event.details(), Toast.LENGTH_LONG).show();
                 break;
-            case MATCH_COMPLETED:
-                int nbOfResults = new SharedPref(getApplicationContext()).getReturnIdCountInt();
+            }
+            case MATCH_COMPLETED: {
+                Callout callout = appState.getCallout();
+                switch (callout) {
+                    case IDENTIFY: {
+                        int nbOfResults = new SharedPref(getApplicationContext()).getReturnIdCountInt();
 
-                ArrayList<Identification> topCandidates = new ArrayList<>();
+                        ArrayList<Identification> topCandidates = new ArrayList<>();
 
-                // Sort the indices of the person by decreasing score
-                final Integer[] idx = new Integer[candidates.size()];
-                for (int i = 0; i < candidates.size(); i++) {
-                    idx[i] = i;
-                }
+                        // Sort the indices of the person by decreasing score
+                        final Integer[] idx = new Integer[candidates.size()];
+                        for (int i = 0; i < candidates.size(); i++) {
+                            idx[i] = i;
+                        }
 
-                Arrays.sort(idx, new Comparator<Integer>() {
-                    @Override
-                    public int compare(final Integer i1, final Integer i2) {
-                        return Float.compare(scores.get(i2), scores.get(i1));
+                        Arrays.sort(idx, new Comparator<Integer>() {
+                            @Override
+                            public int compare(final Integer i1, final Integer i2) {
+                                return Float.compare(scores.get(i2), scores.get(i1));
+                            }
+                        });
+
+                        for (int i = 0; i < Math.min(nbOfResults, candidates.size()); i++) {
+                            Person candidate = candidates.get(idx[i]);
+
+                            topCandidates.add(new Identification(candidate.getGuid(),
+                                    scores.get(idx[i]).intValue(), computeTier(scores.get(idx[i]))));
+                        }
+
+                        if (appState.getData() != null) {
+                            appState.getData().saveIdentification(probe, topCandidates, appState.getSessionId());
+                        }
+
+                        // finish
+                        Intent resultData;
+                        resultData = new Intent(Constants.SIMPRINTS_IDENTIFY_INTENT);
+                        resultData.putExtra(Constants.SIMPRINTS_IDENTIFICATIONS, topCandidates);
+                        resultData.putExtra(Constants.SIMPRINTS_SESSION_ID, appState.getSessionId());
+                        setResult(RESULT_OK, resultData);
                     }
-                });
+                    case VERIFY: {
+                        Verification verification;
+                        VERIFY_GUID_EXISTS_RESULT guidExistsResult;
+                        int resultCode;
 
-                for (int i = 0; i < Math.min(nbOfResults, candidates.size()); i++) {
-                    Person candidate = candidates.get(idx[i]);
+                        if (candidates.size() > 0 && scores.size() > 0) {
+                            int score = scores.get(0).intValue();
+                            verification = new Verification(score, computeTier(score));
+                            guidExistsResult = VERIFY_GUID_EXISTS_RESULT.GUID_FOUND;
+                            resultCode = RESULT_OK;
+                        } else {
+                            verification = null;
+                            guidExistsResult = VERIFY_GUID_EXISTS_RESULT.GUID_NOT_FOUND_UNKNOWN;
+                            resultCode = Constants.SIMPRINTS_VERIFY_GUID_NOT_FOUND_ONLINE;
+                        }
 
-                    topCandidates.add(new Identification(candidate.getGuid(),
-                            scores.get(idx[i]).intValue(), computeTier(scores.get(idx[i]))));
+                        if (appState.getData() != null) {
+                            appState.getData().saveVerification(probe, verification, appState.getSessionId(), guidExistsResult);
+                        }
+
+                        // finish
+                        Intent resultData;
+                        resultData = new Intent(Constants.SIMPRINTS_VERIFY_INTENT);
+                        resultData.putExtra(Constants.SIMPRINTS_VERIFICATIONS, verification);
+                        resultData.putExtra(Constants.SIMPRINTS_SESSION_ID, appState.getSessionId());
+                        setResult(resultCode, resultData);
+                    }
                 }
-
-                if (appState.getData() != null) {
-                    appState.getData().saveIdentification(probe, topCandidates, appState.getSessionId());
-                }
-
-                // finish
-                Intent resultData = new Intent(Constants.SIMPRINTS_IDENTIFY_INTENT);
-                resultData.putExtra(Constants.SIMPRINTS_IDENTIFICATIONS, topCandidates);
-                resultData.putExtra(Constants.SIMPRINTS_SESSION_ID, appState.getSessionId());
-                setResult(RESULT_OK, resultData);
                 finish();
                 break;
+            }
         }
     }
 
