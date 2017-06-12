99beb607fa15f4f244fce9b4b5ad3248dd092c47
Ridwan <arfarouki@gmail.com>
2017-06-12 16:00:10 +0100

    Removed all hard-coded strings. Removed unused dependencies.

--- id/src/main/java/com/simprints/id/activities/TutorialActivity.java
+++ id/src/main/java/com/simprints/id/activities/TutorialActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.simprints.id.R;
-import com.simprints.id.tools.Analytics;
 import com.simprints.id.tools.Language;
 
 public class TutorialActivity extends AppCompatActivity {
