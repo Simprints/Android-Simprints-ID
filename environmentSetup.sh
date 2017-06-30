c1b34cb79fd28b153479914786b802e836f63541
Ridwan <arfarouki@gmail.com>
2017-06-30 15:37:19 +0100

    updating circle.yml

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -1,6 +1,6 @@
 #
 #           Circle CI & gradle.properties live in harmony
-# 
+#
 # Android convention is to store your API keys in a local, non-versioned
 # gradle.properties file. Circle CI doesn't allow users to upload pre-populated
 # gradle.properties files to store this secret information, but instaed allows
@@ -8,9 +8,9 @@
 #
 # This script creates a local gradle.properties file on current the Circle CI
 # instance. It then reads environment variable TEST_API_KEY_ENV_VAR which a user
-# has defined in their Circle CI project settings environment variables, and 
+# has defined in their Circle CI project settings environment variables, and
 # writes this value to the Circle CI instance's gradle.properties file.
-# 
+#
 # You must execute this script via your circle.yml as a pre-process dependency,
 # so your gradle build process has access to all variables.
 #
