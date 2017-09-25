25493fc17379c0f8270e30ebfac2c53e2b00d1cc
Ridwan <arfarouki@gmail.com>
2017-09-25 14:25:36 +0100

    Basic end-to-end tests automated and working

--- id/src/androidTest/java/com/simprints/id/happypath/HappyPathEnrolTest.java
+++ id/src/androidTest/java/com/simprints/id/happypath/HappyPathEnrolTest.java
@@ -1,16 +1,15 @@
-package com.simprints.id;
+package com.simprints.id.happypath;
 
 import android.content.Context;
 import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.Espresso;
-import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.text.format.DateUtils;
 import android.view.WindowManager;
 
+import com.simprints.id.R;
 import com.simprints.id.activities.LaunchActivity;
 import com.simprints.libsimprints.Constants;
 
@@ -20,19 +19,21 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.TimeUnit;
+
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.schibsted.spain.barista.BaristaSleepActions.sleep;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class IdentifyHappyPathTest {
+public class HappyPathEnrolTest {
     private static final String apiKey = "1b22a8e0-6e86-4d8b-92f7-905bd38ac1d5";
     private static final String userId = "1b22a8e0-6e86-4d8b-92f7-905bd38ac1d5";
     private static final String moduleId = "0";
-    private static IdlingResource idlingResource;
 
     @Rule
     public ActivityTestRule<LaunchActivity> launchActivityActivityTestRule = new ActivityTestRule<LaunchActivity>(LaunchActivity.class) {
@@ -40,7 +41,7 @@ public class IdentifyHappyPathTest {
         protected Intent getActivityIntent() {
             Context targetContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
             Intent result = new Intent(targetContext, LaunchActivity.class);
-            result.setAction(Constants.SIMPRINTS_IDENTIFY_INTENT);
+            result.setAction(Constants.SIMPRINTS_REGISTER_INTENT);
             result.putExtra(Constants.SIMPRINTS_API_KEY, apiKey);
             result.putExtra(Constants.SIMPRINTS_USER_ID, userId);
             result.putExtra(Constants.SIMPRINTS_MODULE_ID, moduleId);
@@ -51,7 +52,6 @@ public class IdentifyHappyPathTest {
 
     @Before
     public void setUp() {
-        idlingResource = new ElapsedTimeIdlingResource(DateUtils.SECOND_IN_MILLIS * 1);
         final LaunchActivity activity = launchActivityActivityTestRule.getActivity();
         Runnable wakeUpDevice = new Runnable() {
             public void run() {
@@ -62,22 +62,44 @@ public class IdentifyHappyPathTest {
         };
         activity.runOnUiThread(wakeUpDevice);
 
-        // ...and wait
-        Espresso.registerIdlingResources(idlingResource);
     }
 
 
     /**
-     * LaunchActivity behaves
+     * SetupActivity:
+     * Wait for setup activity to finish loading. Once it has, click through to the next.
+     *
+     * MainActivity:
+     * Click scan. Wait for a good print. Click scan again. Wait for another good print.
+     * Click through.
      */
     @Test
-    public void setupActivityBehaves() {
-        onView(withText(R.string.short_consent))
+    public void successfulEnrol() {
+
+        onView(ViewMatchers.withText(R.string.short_consent))
                 .check(matches(isDisplayed()));
+
+        sleep(12, TimeUnit.SECONDS);
+
+        onView(withText(R.string.confirm_consent))
+                .check(matches(isDisplayed()))
+                .perform(click());
+
+        sleep(2, TimeUnit.SECONDS);
+
+        onView(withText(R.string.scan))
+                .check(matches(isDisplayed()))
+                .perform(click());
+
+        sleep(8, TimeUnit.SECONDS);
+
+        onView(withText(R.string.scan))
+                .check(matches(isDisplayed()))
+                .perform(click());
+
+        sleep(8, TimeUnit.SECONDS);
     }
 
     @After
-    public void cleanUp() {
-        Espresso.unregisterIdlingResources(idlingResource);
-    }
+    public void cleanUp() {}
 }
