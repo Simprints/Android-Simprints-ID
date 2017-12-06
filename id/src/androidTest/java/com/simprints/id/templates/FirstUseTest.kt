ddbc13c2bfef608e81ea67663a91afd6e8681bbf
ridwan-simprints <ridwan@simprints.com>
2017-12-06 16:55:37 +0000

    Added Sync Test (WIP) + Refactoring
    - Refactored all espresso parts into TestSnippets.kt to conform with clean code principles
    - Refactored apikey, moduleId, userId, into a CalloutCredentials object
    - Improved logging capabilites including logging to external command line
    
    Former-commit-id: e2cfe21cb5a6f6fc1cab060dd57de076633de2b8

--- id/src/androidTest/java/com/simprints/id/templates/FirstUseTest.kt
+++ id/src/androidTest/java/com/simprints/id/templates/FirstUseTest.kt
@@ -1,16 +1,19 @@
-package com.simprints.id
+package com.simprints.id.templates
 
 import android.Manifest.permission
 import android.support.test.InstrumentationRegistry
-import android.util.Log
 import com.schibsted.spain.barista.permission.PermissionGranter
+import com.simprints.id.tools.CalloutCredentials
+import com.simprints.id.tools.RemoteAdminUtils
+import com.simprints.id.tools.StorageUtils
+import com.simprints.id.tools.log
 import com.simprints.remoteadminclient.ApiException
 import io.realm.RealmConfiguration
 import org.junit.After
 import org.junit.Before
 import java.util.*
 
-open class FirstUseTest {
+abstract class FirstUseTest {
 
     private val permissions = ArrayList(Arrays.asList(
             permission.ACCESS_NETWORK_STATE,
@@ -22,35 +25,30 @@ open class FirstUseTest {
             permission.VIBRATE
     ))
 
-    private var realmConfiguration: RealmConfiguration? = null
-    private var apiKey: String? = null
-
-    protected fun setRealmConfiguration(realmConfiguration: RealmConfiguration) {
-        this.realmConfiguration = realmConfiguration
-    }
-
-    protected fun setApiKey(apiKey: String) {
-        this.apiKey = apiKey
-    }
+    protected abstract var realmConfiguration: RealmConfiguration?
+    protected abstract val calloutCredentials: CalloutCredentials
 
     @Before
     @Throws(ApiException::class)
     open fun setUp() {
-        Log.d("EndToEndTests", "FirstUseTest.setUp(): cleaning app data")
+        log("FirstUseTest.setUp(): cleaning app data")
 
         // Clear any internal data
+        log("FirstUseTest.setUp(): cleaning internal data")
         StorageUtils.clearApplicationData(InstrumentationRegistry.getContext(), realmConfiguration!!)
 
         // Clear the project for the test's APIkey via remote admin
+        log("FirstUseTest.setUp(): cleaning remote data")
         val apiInstance = RemoteAdminUtils.configuredApiInstance
-        RemoteAdminUtils.clearProjectNode(apiInstance, apiKey!!)
+        RemoteAdminUtils.clearProjectNode(apiInstance, calloutCredentials.apiKey)
 
         // Allow all first-app permissions and dismiss the dialog box
+        log("FirstUseTest.setUp(): granting permissions")
         for (permission in permissions) PermissionGranter.allowPermissionsIfNeeded(permission)
     }
 
     @After
     open fun tearDown() {
-        Log.d("EndToEndTests", "FirstUseTest.tearDown(): nothing")
+        log("FirstUseTest.tearDown(): nothing")
     }
 }
