48e29b46f291d1ab6dbb7a4bfb309a7538ea8690
ethiery <etienne@simprints.com>
2017-12-14 16:10:53 +0000

    Refactor SyncClient
    Former-commit-id: 1dec8662d588edd5a1356004cf0e81e7d42dfc9b

--- id/src/main/java/com/simprints/id/services/sync/SyncClient.kt
+++ id/src/main/java/com/simprints/id/services/sync/SyncClient.kt
@@ -3,6 +3,7 @@ package com.simprints.id.services.sync
 import android.content.Context
 import com.simprints.id.exceptions.safe.TaskInProgressException
 import com.simprints.id.services.progress.client.ProgressClientImpl
+import com.simprints.id.services.progress.client.ProgressConnection
 import com.simprints.libcommon.Progress
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -17,45 +18,48 @@ import timber.log.Timber
 class SyncClient(context: Context)
     : ProgressClientImpl<SyncTaskParameters>(context, SyncService::class.java) {
 
-    private var progressReplayObservable: Observable<Progress>? = null
+    private var currentProgressReplayObservable: Observable<Progress>? = null
     private val disposables = mutableListOf<Disposable>()
 
     fun sync(syncParameters: SyncTaskParameters,
              onStarted: () -> Unit,
              onBusy: () -> Unit) {
         async(UI) {
-            val started = bg {
-                startService()
-                val connection = bind()
-                val started = try {
-                    connection.execute(syncParameters)
-                    connection.startForeground()
-                    setProgressReplayObservable(connection.progressReplayObservable)
-                    true
-                } catch (exception: TaskInProgressException) {
-                    false
-                }
-                connection.unbind()
-                started
-            }.await()
-            if (started) {
+            try {
+                bg { startSyncAndObserve(syncParameters) }.await()
                 onStarted()
-            } else {
+            } catch (exception: TaskInProgressException) {
                 onBusy()
             }
         }
     }
 
+    private fun startSyncAndObserve(syncParameters: SyncTaskParameters) {
+        startService()
+        connectAnd {
+            execute(syncParameters)
+            startForeground()
+            setProgressReplayObservable(progressReplayObservable)
+        }
+    }
+
+    private fun <T> connectAnd(op: ProgressConnection<SyncTaskParameters>.() -> T): T {
+        val connection = bind()
+        val result = connection.op()
+        connection.unbind()
+        return result
+    }
+
     private fun setProgressReplayObservable(observable: Observable<Progress>) {
         synchronized(this) {
-            progressReplayObservable = observable
+            currentProgressReplayObservable = observable
         }
     }
 
     fun startListening(observer: DisposableObserver<Progress>) {
         Timber.d("startListening()")
         synchronized(this) {
-            val observable = progressReplayObservable
+            val observable = currentProgressReplayObservable
             if (observable != null) {
                 disposables.add(observable
                         .observeOn(AndroidSchedulers.mainThread())
