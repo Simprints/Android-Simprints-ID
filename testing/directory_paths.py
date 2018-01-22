f81a41fbfaba03ef517505303aaef1b1c18d8fe6
Fabio Tuzza <fabio@viewranger.com>
2018-01-22 13:23:35 +0000

    Inject testing params from gradle (local properties) instead of python and run connectedAndroidTest
    Former-commit-id: a8c09055e52797549723af87022f530c6abdfcfb

--- testing/directory_paths.py
+++ testing/directory_paths.py
@@ -6,7 +6,7 @@ HERE = '' if platform.system() == 'Windows' else '.'
 # Relative paths should be relative to instrumented_test.py
 
 SIMPRINTS_ID_DIR_PATH = f'{HERE}'
-CERBERUS_DIR_PATH = f'{SIMPRINTS_ID_DIR_PATH}/Android-Cerberus'
+CERBERUS_DIR_PATH = f'/tmp/Android-Cerberus'
 LOG_DIR_BASE_NAME = f'{SIMPRINTS_ID_DIR_PATH}/testing/logs'
 
 # Some devices are known to use the same serial number.
@@ -14,10 +14,11 @@ LOG_DIR_BASE_NAME = f'{SIMPRINTS_ID_DIR_PATH}/testing/logs'
 # Use this custom version of adb that configures devices by their usb port rather than serial number.
 # This version of adb only works on Unix systems.
 ADB = 'adb' if platform.system() == 'Windows' else f'{SIMPRINTS_ID_DIR_PATH}/testing/adb'
-
+ADB = 'adb'
 GRADLEW = 'gradlew.bat' if platform.system() == 'Windows' else 'gradlew'
 SIMPRINTS_ID_MODULE_NAME = 'id'
 CERBERUS_APP_MODULE_NAME = 'cerberus-app'
 
 SIMPRINTS_ID_PACKAGE_NAME = 'com.simprints.id'
 CERBERUS_APP_PACKAGE_NAME = 'com.simprints.cerberusapp'
+CERBERUS_MAIN_ACTIVITY_NAME = f'{CERBERUS_APP_PACKAGE_NAME}/{CERBERUS_APP_PACKAGE_NAME}.activities.main.MainActivity'
