f82f67a437ed6d57b90eaa387121932e72ef90a6
Fabio Tuzza <fabio@viewranger.com>
2018-01-23 10:29:48 +0000

    Reverted last commit
    Former-commit-id: 71b833ee20c2d9539362d8828a159f63bc02af1a

--- testing/commands.py
+++ testing/commands.py
@@ -6,32 +6,32 @@ from testing.directory_paths import *
 
 
 def gradlew_command(dir_path: str, module_name: str, command: str):
-    return f'{dir_path}/{GRADLEW} -p {dir_path}/{module_name} {command} &'
+    return f'{dir_path}/{GRADLEW} -p {dir_path}/{module_name} {command}'
 
 
 def uninstall_apk_command(package_name: str, device: Device):
-    return f'{ADB} -s {device.device_id} uninstall {package_name} &'
+    return f'{ADB} -s {device.device_id} uninstall {package_name}'
 
 
 def uninstall_android_test_apk_command(package_name: str, device: Device):
-    return f'{ADB} -s {device.device_id} uninstall {package_name}.test &'
+    return f'{ADB} -s {device.device_id} uninstall {package_name}.test'
 
 
 def install_apk_command(dir_path: str, module_name: str, build_type: str, device: Device):
     return f'{ADB} -s {device.device_id} install -t -d -r ' \
-           f'{dir_path}/{module_name}/build/outputs/apk/{build_type}/{module_name}-{build_type}.apk &'
+           f'{dir_path}/{module_name}/build/outputs/apk/{build_type}/{module_name}-{build_type}.apk'
 
 def open_apk_command(package_name: str, device: Device):
-    return f'{ADB} -s {device.device_id} shell am start -n {package_name} &'
+    return f'{ADB} -s {device.device_id} shell am start -n {package_name}'
 
 def install_android_test_apk_command(dir_path: str, module_name: str, build_type: str, device: Device):
     return f'{ADB} -s {device.device_id} install -t -d -r ' \
            f'{dir_path}/' \
-           f'{module_name}/build/outputs/apk/androidTest/{build_type}/{module_name}-{build_type}-androidTest.apk &'
+           f'{module_name}/build/outputs/apk/androidTest/{build_type}/{module_name}-{build_type}-androidTest.apk'
 
 
 def query_devices():
-    return f'{ADB} devices -l &'
+    return f'{ADB} devices -l'
 
 
 # Specific implementations
@@ -42,7 +42,7 @@ def simprints_id_test_command(device: Device, test: str, scanner: Scanner = None
            (f'' if scanner is None else f'-e scanner_mac_address \"\'{scanner.mac_address}\'\" ') + \
            (f'' if wifi_network is None else f'-e wifi_network_ssid \"\'{wifi_network.ssid}\'\" ') + \
            (f'' if wifi_network is None else f'-e wifi_network_password \"\'{wifi_network.password}\'\" ') + \
-           f'-e class {test} com.simprints.id.test/android.support.test.runner.AndroidJUnitRunner &'
+           f'-e class {test} com.simprints.id.test/android.support.test.runner.AndroidJUnitRunner '
 
 
 def cerberus_app_gradlew_command(command: str):
@@ -57,7 +57,7 @@ def cerberus_app_install_apk_command(build_type: str, device: Device):
     return install_apk_command(CERBERUS_DIR_PATH, CERBERUS_APP_MODULE_NAME, build_type, device)
 
 def cerberus_app_open_apk_command(device: Device):
-    return open_apk_command(CERBERUS_MAIN_ACTIVITY_NAME, device)
+    return open_apk_command(CERBERUS_APP_PACKAGE_NAME, device)
 
 def simprints_id_gradlew_command(command: str):
     return gradlew_command(SIMPRINTS_ID_DIR_PATH, SIMPRINTS_ID_MODULE_NAME, command)
