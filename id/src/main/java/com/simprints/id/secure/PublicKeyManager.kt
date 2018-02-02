0defb9d4b29f421c4f5b689f4d355cbc60becd82
Ridwan Farouki <ridwan@simprints.com>
2018-02-02 14:52:49 +0000

    Implemented public key getting
    - Used retrofit to grab public key
    
    Former-commit-id: d18b85706db8800e401803c72788a0096f01f46d

--- id/src/main/java/com/simprints/id/secure/PublicKeyManager.kt
+++ id/src/main/java/com/simprints/id/secure/PublicKeyManager.kt
@@ -2,24 +2,15 @@ package com.simprints.id.secure
 
 import com.simprints.id.secure.cryptography.AsymmetricEncrypter
 import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class PublicKeyManager {
 
-    companion object {
-        private const val publicKeyGetUrl =
-            "https://project-manager-dot-simprints-dev.appspot.com/public-key?key=AIzaSyAORPo9YH-TBw0F1ch8BMP9IGkNElgon6s"
-    }
-
-    fun requestPublicKey(): Single<String> {
-        return Single.create<String> { emitter ->
-            Network().execute({
-                val publicKey = it
-                emitter.onSuccess(publicKey)
-            }, {
-                emitter.onError(it)
-            })
-        }
-    }
+    fun requestPublicKey(): Single<String> =
+        ApiService.publicKey("AIzaSyAORPo9YH-TBw0F1ch8BMP9IGkNElgon6s")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
 
     fun encryptProjectSecret(projectSecret: String, publicKey: String): Single<String> {
         return Single.create<String> { emitter ->
