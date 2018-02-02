fdfd57c797c8eae000045d0d3f15e34f2be802eb
Ridwan Farouki <ridwan@simprints.com>
2018-02-02 11:35:52 +0000

    Begin implementing functions
    - Some light refactoring too
    - Project secret now encrypted
    
    Former-commit-id: 1c2052b770735abe1d1cbc6e112b8b11c30643e0

--- id/src/main/java/com/simprints/id/secure/PublicKeyManager.kt
+++ id/src/main/java/com/simprints/id/secure/PublicKeyManager.kt
@@ -1,26 +1,30 @@
 package com.simprints.id.secure
+
+import com.simprints.id.secure.cryptography.AsymmetricEncrypter
 import io.reactivex.Single
 
 class PublicKeyManager {
 
     companion object {
+        private const val publicKeyGetUrl =
+            "https://project-manager-dot-simprints-dev.appspot.com/public-key?key=AIzaSyAORPo9YH-TBw0F1ch8BMP9IGkNElgon6s"
+    }
 
-        fun requestPublicKey(): Single<String> {
-            return Single.create<String> { emitter ->
-                Network().execute({
-                    val publicKey = it
-                    emitter.onSuccess(publicKey)
-                }, {
-                    emitter.onError(it)
-                })
-            }
+    fun requestPublicKey(): Single<String> {
+        return Single.create<String> { emitter ->
+            Network().execute({
+                val publicKey = it
+                emitter.onSuccess(publicKey)
+            }, {
+                emitter.onError(it)
+            })
         }
+    }
 
-        fun encryptProjectSecret(projectSecret: String, publicKey: String): Single<String> {
-            return Single.create<String> { emitter ->
-                val encryptProjectSecret = publicKey + projectSecret + "encrypt"
-                emitter.onSuccess(encryptProjectSecret)
-            }
+    fun encryptProjectSecret(projectSecret: String, publicKey: String): Single<String> {
+        return Single.create<String> { emitter ->
+            val encryptProjectSecret = AsymmetricEncrypter(publicKey).encrypt(projectSecret)
+            emitter.onSuccess(encryptProjectSecret)
         }
     }
 }
