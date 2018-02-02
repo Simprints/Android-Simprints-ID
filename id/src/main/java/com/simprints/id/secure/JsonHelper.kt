4695b81016296c5b24515f6a556da29a43d7f3db
Fabio Tuzza <fabio@viewranger.com>
2018-02-02 13:35:11 +0000

    Retrofit integrated
    Former-commit-id: 5241d0d71ad33418985ce96d4cceea134b2f2060

---
+++ id/src/main/java/com/simprints/id/secure/JsonHelper.kt
@@ -0,0 +1,16 @@
+package com.simprints.id.secure
+
+import com.google.gson.Gson
+
+class JsonHelper {
+
+    companion object {
+        val gson by lazy {
+            Gson()
+        }
+
+        fun toJson(nonceScope: NonceScope): String {
+            return gson.toJson(nonceScope)
+        }
+    }
+}
--- id/src/main/java/com/simprints/id/secure/Network.kt
+++ id/src/main/java/com/simprints/id/secure/Network.kt
@@ -1,11 +1,7 @@
 package com.simprints.id.secure
 
 class Network {
-    fun execute(success: (String) -> Unit, error: (Exception) -> Unit) {
-        if (Math.random() < 0.95) {
-            success("response")
-        } else {
-            error(Exception("Failed to request ApiKey"))
-        }
+    fun execute(success: (String) -> Unit, error: (e: Exception) -> Unit) {
+        success("response")
     }
 }
