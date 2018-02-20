42cc1210e3d5202788afd29142aab43f43fa50ca
Fabio Tuzza <fabio@viewranger.com>
2018-02-20 11:14:27 +0000

    Replaced zip and bifunction with rxkotlin Added suscribeOns in sub Managers
    Former-commit-id: 07acbcb1d4e5dcd88a5749ea284ffdbcc85410d7

--- id/src/main/java/com/simprints/id/secure/NonceManager.kt
+++ id/src/main/java/com/simprints/id/secure/NonceManager.kt
@@ -5,12 +5,13 @@ import com.google.gson.reflect.TypeToken
 import com.simprints.id.secure.models.Nonce
 import com.simprints.id.secure.models.NonceScope
 import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
 
 class NonceManager(val client: ApiServiceInterface) {
 
     fun requestNonce(nonceScope: NonceScope): Single<Nonce> {
         val headers = convertNonceScopeIntoMap(nonceScope)
-        return client.nonce(headers)
+        return client.nonce(headers).subscribeOn(Schedulers.io())
     }
 
     private fun convertNonceScopeIntoMap(nonceScope: NonceScope): Map<String, String> {
--- id/src/main/java/com/simprints/id/secure/PublicKeyManager.kt
+++ id/src/main/java/com/simprints/id/secure/PublicKeyManager.kt
@@ -2,9 +2,10 @@ package com.simprints.id.secure
 
 import com.simprints.id.secure.models.PublicKeyString
 import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
 
 class PublicKeyManager(val client: ApiServiceInterface) {
 
     fun requestPublicKey(): Single<PublicKeyString> =
-        client.publicKey()
+        client.publicKey().subscribeOn(Schedulers.io())
 }
