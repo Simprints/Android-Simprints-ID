060b2f487ccf23f557eddc876d229bf847bdf40e
Fabio Tuzza <fabio@viewranger.com>
2018-02-21 12:53:51 +0000

    Added sync button in the dashboard (no tested - Firebase integration missing)
    Former-commit-id: bfe3ab707e4a8eb37033210e8b41cfaee29927ac

--- id/src/main/java/com/simprints/id/activities/dashboard/DashboardContract.kt
+++ id/src/main/java/com/simprints/id/activities/dashboard/DashboardContract.kt
@@ -2,15 +2,20 @@ package com.simprints.id.activities.dashboard
 
 import com.simprints.id.activities.BasePresenter
 import com.simprints.id.activities.BaseView
+import com.simprints.id.model.ALERT_TYPE
 
 interface DashboardContract {
 
     interface View : BaseView<Presenter> {
-
+        fun getStringWithParams(stringRes: Int, currentValue: Int = 0, maxValue: Int = 0): String
+        fun setSyncItem(enabled: Boolean, string: String, icon: Int)
+        fun showToast(messageRes: Int)
+        fun launchAlertView(error: ALERT_TYPE)
     }
 
     interface Presenter : BasePresenter {
-
+        fun sync()
+        fun pause()
     }
 }
 
--- id/src/main/java/com/simprints/id/activities/dashboard/DashboardPresenter.kt
+++ id/src/main/java/com/simprints/id/activities/dashboard/DashboardPresenter.kt
@@ -1,11 +1,176 @@
 package com.simprints.id.activities.dashboard
 
-class DashboardPresenter(val view: DashboardContract.View) : DashboardContract.Presenter {
+import android.support.annotation.DrawableRes
+import android.support.annotation.StringRes
+import com.simprints.id.R
+import com.simprints.id.data.DataManager
+import com.simprints.id.exceptions.unsafe.InvalidSyncGroupError
+import com.simprints.id.exceptions.unsafe.UninitializedDataManagerError
+import com.simprints.id.model.ALERT_TYPE
+import com.simprints.id.services.sync.SyncClient
+import com.simprints.id.services.sync.SyncTaskParameters
+import com.simprints.libcommon.Progress
+import com.simprints.libdata.AuthListener
+import com.simprints.libdata.ConnectionListener
+import com.simprints.libdata.tools.Constants.GROUP
+import io.reactivex.observers.DisposableObserver
+import timber.log.Timber
+
+class DashboardPresenter(val view: DashboardContract.View,
+                         val syncClient: SyncClient,
+                         val dataManager: DataManager) : DashboardContract.Presenter {
+
+    private var started: Boolean = false
 
     init {
         view.setPresenter(this)
     }
 
     override fun start() {
+        if (!started) {
+            started = true
+            startListeners()
+        }
+    }
+
+    override fun pause() {
+        stopListeners()
+    }
+
+    override fun sync() {
+        val syncParameters = when (dataManager.syncGroup) {
+            GROUP.GLOBAL -> SyncTaskParameters.GlobalSyncTaskParameters(dataManager.getSignedInProjectIdOrEmpty())
+            GROUP.USER -> SyncTaskParameters.UserSyncTaskParameters(dataManager.getSignedInProjectIdOrEmpty(), dataManager.userId)
+            else -> {
+                handleUnexpectedError(InvalidSyncGroupError())
+                return
+            }
+        }
+
+        syncClient.sync(syncParameters, {
+            syncClient.startListening(newSyncObserver())
+        }, {
+            setErrorSyncItem()
+            view.showToast(R.string.wait_for_current_sync_to_finish)
+        })
+    }
+
+
+
+    private fun stopListeners() {
+        try {
+            syncClient.stopListening()
+            dataManager.unregisterRemoteAuthListener(authListener)
+            dataManager.unregisterRemoteConnectionListener(connectionListener)
+        } catch (error: UninitializedDataManagerError) {
+            handleUnexpectedError(error)
+        }
+    }
+
+    private fun startListeners() {
+        dataManager.registerRemoteAuthListener(authListener)
+        dataManager.registerRemoteConnectionListener(connectionListener)
+        updateConnectionState()
+        syncClient.startListening(newSyncObserver())
+    }
+
+    private fun updateConnectionState() {
+        if (dataManager.isRemoteConnected) {
+            connectionListener.onConnection()
+        } else {
+            connectionListener.onDisconnection()
+        }
+    }
+
+    private val authListener = object : AuthListener {
+        override fun onSignIn() {
+
+        }
+
+        override fun onSignOut() {
+            setOfflineSyncItem()
+        }
+
+    }
+
+    private val connectionListener = object : ConnectionListener {
+        override fun onConnection() {
+            setReadySyncItem()
+        }
+
+        override fun onDisconnection() {
+            setOfflineSyncItem()
+        }
+    }
+
+    private fun newSyncObserver(): DisposableObserver<Progress> {
+        return object : DisposableObserver<Progress>() {
+
+            override fun onNext(progress: Progress) {
+                Timber.d("onNext")
+                setProgressSyncItem(progress)
+            }
+
+            override fun onComplete() {
+                Timber.d("onComplete")
+                setCompleteSyncItem()
+                syncClient.stopListening()
+            }
+
+            override fun onError(throwable: Throwable) {
+                Timber.d("onError")
+                setErrorSyncItem()
+                logThrowable(throwable)
+                syncClient.stopListening()
+            }
+
+            private fun logThrowable(throwable: Throwable) {
+                if (throwable is Error) {
+                    dataManager.logError(throwable)
+                } else if (throwable is RuntimeException) {
+                    dataManager.logSafeException(throwable)
+                }
+            }
+        }
+    }
+
+    private fun handleUnexpectedError(error: Error) {
+        dataManager.logError(error)
+        view.launchAlertView(ALERT_TYPE.UNEXPECTED_ERROR)
+    }
+
+    private fun setProgressSyncItem(progress: Progress) {
+        if (isProgressZero(progress))
+            view.setSyncItem(false,
+                view.getStringWithParams(R.string.syncing_calculating),
+                R.drawable.ic_syncing)
+        else
+            view.setSyncItem(false,
+                view.getStringWithParams(R.string.syncing_with_progress, progress.currentValue, progress.maxValue),
+                R.drawable.ic_syncing)
+    }
+
+    private fun setCompleteSyncItem() {
+        setSyncItem(true, R.string.nav_sync_complete, R.drawable.ic_sync_success)
+    }
+
+    private fun setReadySyncItem() {
+        setSyncItem(true, R.string.nav_sync, R.drawable.ic_menu_sync_ready)
+    }
+
+    private fun setOfflineSyncItem() {
+        setSyncItem(false, R.string.not_signed_in, R.drawable.ic_menu_sync_off)
+    }
+
+    private fun setSyncItem(enabled: Boolean, @StringRes title: Int, @DrawableRes icon: Int) {
+        view.setSyncItem(enabled, view.getStringWithParams(title), icon)
+    }
+
+    private fun setErrorSyncItem() {
+        setSyncItem(true, R.string.nav_sync_failed, R.drawable.ic_sync_failed)
+    }
+
+    private fun isProgressZero(progress: Progress): Boolean {
+        return progress.currentValue == 0 && progress.maxValue == 0
     }
 }
