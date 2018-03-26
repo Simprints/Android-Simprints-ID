ca5e1366b3552ae17cc1c73de1f4f6d8ffc94dfd
Fabio Tuzza <fabio@viewranger.com>
2018-03-26 22:17:28 +0100

    Fix data models (added tests)
    Former-commit-id: 868dea2d3fa99cc93688118a35bc41da53617a24

---
+++ id/src/test/java/com/simprints/id/data/db/remote/models/fb_FingerprintTest.kt
@@ -0,0 +1,38 @@
+package com.simprints.id.data.db.remote.models
+
+import com.google.gson.JsonObject
+import com.simprints.id.BuildConfig
+import com.simprints.id.testUtils.roboletric.TestApplication
+import com.simprints.id.tools.json.JsonHelper
+import com.simprints.libcommon.Fingerprint
+import com.simprints.libsimprints.FingerIdentifier
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(constants = BuildConfig::class, application = TestApplication::class)
+
+class fb_FingerprintTest {
+    @Test
+    fun serialiseFbFingerprint_skipUnwantedFields() {
+        val fingerprint = fb_Fingerprint(Fingerprint.generateRandomFingerprint(FingerIdentifier.LEFT_THUMB))
+        val jsonString = JsonHelper.toJson(fingerprint)
+        val json = JsonHelper.gson.fromJson(jsonString, JsonObject::class.java)
+
+        print(jsonString)
+        assertTrue(json.has("quality"))
+        assertTrue(json.has("template"))
+        assertEquals(json.keySet().size, 2)
+    }
+
+    @Test
+    fun test() {
+
+        val right = FingerIdentifier.RIGHT_THUMB
+        assertEquals(right.toString(), right.name)
+    }
+}
