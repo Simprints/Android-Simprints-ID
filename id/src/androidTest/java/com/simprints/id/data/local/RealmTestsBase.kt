c85a80422dc3e3a868c4b622ac829657e5aae1f2
Tristram Norman <tristram@simprints.com>
2018-04-01 13:32:46 +0100

    create tests for realm manager.
    Former-commit-id: b2662d3315533d55694ca85c3923526ebf6f4908

---
+++ id/src/androidTest/java/com/simprints/id/data/local/RealmTestsBase.kt
@@ -0,0 +1,60 @@
+package com.simprints.id.data.local
+
+import android.support.test.InstrumentationRegistry
+import com.simprints.id.data.db.local.LocalDbKey
+import com.simprints.id.data.db.local.RealmConfig
+import com.simprints.id.data.db.local.models.rl_Person
+import com.simprints.id.tools.utils.PeopleGeneratorUtils
+import io.realm.Realm
+import io.realm.RealmConfiguration
+import java.io.File
+import java.util.*
+
+
+open class RealmTestsBase {
+
+    companion object {
+        private const val KEY_LENGTH = 64
+
+        const val legacyDatabaseName: String = "legacyDB"
+        const val newDatabaseName: String = "newDatabase"
+        val newDatabaseKey: ByteArray = Arrays.copyOf("newKey".toByteArray(), KEY_LENGTH)
+    }
+
+    protected val localDbKey = LocalDbKey(newDatabaseName, newDatabaseKey, legacyDatabaseName)
+    protected val config = RealmConfig.get(localDbKey.projectId, localDbKey.value)
+
+    protected val testContext = InstrumentationRegistry.getTargetContext()
+        ?: throw IllegalStateException()
+
+    init {
+        deleteRealmFiles(config)
+    }
+
+    protected fun getFakePerson(): rl_Person = PeopleGeneratorUtils.getRandomPerson()
+
+    protected fun saveFakePerson(realm: Realm, fakePerson: rl_Person) {
+        realm.executeTransaction {
+            it.insertOrUpdate(fakePerson)
+        }
+    }
+
+    protected fun deleteAll(realm: Realm) {
+        realm.executeTransaction {
+            it.deleteAll()
+        }
+    }
+
+    protected fun rl_Person.deepEquals(other: rl_Person): Boolean = when {
+        this.projectId != other.projectId -> false
+        this.userId != other.userId -> false
+        this.moduleId != other.moduleId -> false
+        else -> true
+    }
+
+    protected fun deleteRealmFiles(realmConfig: RealmConfiguration) {
+        Realm.deleteRealm(realmConfig)
+        File("${realmConfig.path}.lock").delete()
+    }
+
+}
