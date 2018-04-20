21bb2d3f623f5edd7f04af428f8d0b4aa2c17244
Ridwan Farouki <ridwan@simprints.com>
2018-04-20 18:45:57 +0100

    Tests now go through Auth succesfully and pass
    - Added google-service.json for test-fs server
    
    Former-commit-id: a48342ee4a3b7aaa3447a613edd8e5d21224fb6c

--- id/src/androidTest/java/com/simprints/id/secure/AuthTests_FirstUseHappyWifi.kt
+++ id/src/androidTest/java/com/simprints/id/secure/AuthTests_FirstUseHappyWifi.kt
@@ -6,15 +6,17 @@ import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.util.Base64
 import android.util.Base64.NO_WRAP
+import com.simprints.id.*
 import com.simprints.id.activities.checkLogin.openedByIntent.CheckLoginFromIntentActivity
 import com.simprints.id.data.db.local.models.LocalDbKey
 import com.simprints.id.data.db.local.realm.RealmConfig
 import com.simprints.id.templates.FirstUseLocal
 import com.simprints.id.templates.HappyWifi
-import com.simprints.id.launchAppFromIntentEnrol
+import com.simprints.id.tools.AppUtils.getApp
 import com.simprints.id.tools.CalloutCredentials
 import io.realm.Realm
 import io.realm.RealmConfiguration
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -34,7 +36,9 @@ class AuthTests_FirstUseHappyWifi: FirstUseLocal, HappyWifi {
     private val localDbKey = LocalDbKey(
         calloutCredentials.projectId,
         Base64.decode("Jk1P0NPgwjViIhnvrIZTN3eIpjWRrok5zBZUw1CiQGGWhTFgnANiS87J6asyTksjCHe4SHJo0dHeawAPz3JtgQ==", NO_WRAP),
-        calloutCredentials.legacyApiKey?: "")
+        calloutCredentials.legacyApiKey)
+
+    private val projectSecret = "orZje76yBgsjE2UWw/8jCtw/pBMgtURTfhO/4hZVP4vGnm1uji1OtwcRQkhn1MzQb5OoMjtu2xsbSIs40vSMEQ=="
 
     override var realmConfiguration: RealmConfiguration? = null
 
@@ -51,13 +55,25 @@ class AuthTests_FirstUseHappyWifi: FirstUseLocal, HappyWifi {
         super<FirstUseLocal>.setUp()
     }
 
+    @After()
+    fun tearDown()
+    {
+        getApp(activityTestRule).dataManager.remoteDbManager.signOutOfRemoteDb()
+    }
+
     @Test
     fun validLegacyCredentials_shouldSucceed() {
         launchAppFromIntentEnrol(calloutCredentials.toLegacy(), activityTestRule)
+        enterCredentialsDirectly(calloutCredentials, projectSecret)
+        pressSignIn()
+        awaitSignInAndEnsureSuccess(activityTestRule)
     }
 
     @Test
     fun validCredentials_shouldSucceed() {
         launchAppFromIntentEnrol(calloutCredentials.toLegacy(), activityTestRule)
+        enterCredentialsDirectly(calloutCredentials, projectSecret)
+        pressSignIn()
+        awaitSignInAndEnsureSuccess(activityTestRule)
     }
 }
