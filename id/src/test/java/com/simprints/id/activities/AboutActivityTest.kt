bf1f173456688ad22c333f31bab26be0cd6cb154
Fabio Tuzza <fabio@viewranger.com>
2018-04-24 16:11:27 +0100

    Fixed unit tests after merge for firestore migration
    Former-commit-id: 0d11831d635479397eaf7ff43111378caab0e2e7

--- id/src/test/java/com/simprints/id/activities/AboutActivityTest.kt
+++ id/src/test/java/com/simprints/id/activities/AboutActivityTest.kt
@@ -1,26 +1,33 @@
 package com.simprints.id.activities
 
+import com.nhaarman.mockito_kotlin.doReturn
 import com.simprints.id.Application
 import com.simprints.id.BuildConfig
 import com.simprints.id.data.analytics.AnalyticsManager
 import com.simprints.id.data.analytics.FirebaseAnalyticsManager
+import com.simprints.id.testUtils.anyNotNull
+import com.simprints.id.testUtils.base.RxJavaTest
 import com.simprints.id.testUtils.roboletric.TestApplication
 import com.simprints.id.testUtils.roboletric.createRoboAboutActivity
 import com.simprints.id.testUtils.roboletric.mockDbManager
 import com.simprints.id.testUtils.roboletric.mockLocalDbManager
+import com.simprints.id.testUtils.whenever
+import io.reactivex.Completable
+import io.reactivex.Single
 import kotlinx.android.synthetic.main.activity_about.*
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito
+import org.mockito.Mockito.spy
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
 @Config(constants = BuildConfig::class, application = TestApplication::class)
-class AboutActivityTest {
+class AboutActivityTest : RxJavaTest() {
 
     private lateinit var analyticsManagerMock: AnalyticsManager
     private lateinit var app: Application
@@ -29,6 +36,8 @@ class AboutActivityTest {
     fun setUp() {
         app = (RuntimeEnvironment.application as Application)
         mockLocalDbManager(app)
+        whenever(app.dbManager.getPeopleCount(anyNotNull(), anyNotNull(), anyNotNull(), anyNotNull())).thenReturn(Single.just(0))
+
         mockDbManager(app)
         mockAnalyticsManager()
     }
@@ -60,6 +69,10 @@ class AboutActivityTest {
 
     @Test
     fun recoverDbButton_disablesOncePressed() {
+        val dataManagerMock = spy(app.dataManager)
+        doReturn(Completable.create { }).`when`(dataManagerMock).recoverRealmDb(anyNotNull())
+        app.dataManager = dataManagerMock
+
         val controller = createRoboAboutActivity().start().resume().visible()
         val activity = controller.get()
 
--- id/src/test/java/com/simprints/id/testUtils/base/RxJavaTest.kt
+++ id/src/test/java/com/simprints/id/testUtils/base/RxJavaTest.kt
@@ -1,6 +1,7 @@
 package com.simprints.id.testUtils.base
 
 import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.plugins.RxJavaPlugins
 import io.reactivex.schedulers.Schedulers
 import org.junit.Before
 
@@ -12,5 +13,6 @@ open class RxJavaTest {
         // AndroidSchedulers.mainThread() requests Android APi, not available in unit tests
         // Setting a special main Scheduler https://medium.com/@peter.tackage/overriding-rxandroid-schedulers-in-rxjava-2-5561b3d14212
         RxAndroidPlugins.setInitMainThreadSchedulerHandler { _ -> Schedulers.trampoline() }
+        RxJavaPlugins.setIoSchedulerHandler { _ -> Schedulers.trampoline() }
     }
 }
