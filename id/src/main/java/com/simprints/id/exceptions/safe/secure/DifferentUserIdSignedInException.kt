4e3f8829aa9c58e13263519e5fbcc4de52811d90
Ridwan Farouki <ridwan@simprints.com>
2018-04-25 19:13:30 +0100

    Now verifying projectId and userId from intent matches stored versions
    - Fixed unit tests to reflect this
    - UserId and ProjectId changes from intent now require the user to sign out
    - WIP: Need to change the alert screen that shows when this occurs
    - WIP: Need to fix the androidTests for sign-in persistence
    
    Former-commit-id: af9676da6e1a7bc9d61ce75a56042714f817d841

---
+++ id/src/main/java/com/simprints/id/exceptions/safe/secure/DifferentUserIdSignedInException.kt
@@ -0,0 +1,6 @@
+package com.simprints.id.exceptions.safe.secure
+
+import com.simprints.id.exceptions.safe.SimprintsException
+
+
+class DifferentUserIdSignedInException(message: String = "DifferentUserIdSignedInException") : SimprintsException(message)
