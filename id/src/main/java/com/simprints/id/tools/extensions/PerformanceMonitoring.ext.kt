dd703765b156cf651cbd4ec7628f9a67ffe9346a
Tristram Norman <tristram@simprints.com>
2018-05-10 11:04:55 +0100

    Update performance monitoring
    Former-commit-id: 69b919fe77b47a32f83f70d8ba4500d48d227f1d

--- id/src/main/java/com/simprints/id/tools/extensions/PerformanceMonitoring.ext.kt
+++ id/src/main/java/com/simprints/id/tools/extensions/PerformanceMonitoring.ext.kt
@@ -2,43 +2,50 @@ package com.simprints.id.tools.extensions
 
 import com.google.android.gms.tasks.Task
 import com.google.firebase.perf.FirebasePerformance
+import com.google.firebase.perf.metrics.Trace
 import io.reactivex.Completable
 import io.reactivex.Observable
 import io.reactivex.Single
 
-private const val SUCCESS = "call_succeeded"
-private const val FAILED = "call_failed"
+private const val CALL_SUCCESS_NAME = "call_success"
+private const val FAILURE: Long = 0
+private const val SUCCESS: Long = 1
 
 fun Completable.trace(traceName: String): Completable {
     val trace = FirebasePerformance.getInstance().newTrace(traceName)
     return this.doOnSubscribe { trace.start() }
-        .doOnComplete { trace.incrementCounter(SUCCESS) }
-        .doOnError { trace.incrementCounter(FAILED) }
+        .doOnComplete { trace.setSuccess(true) }
+        .doOnError { trace.setSuccess(false) }
         .doFinally { trace.stop() }
 }
 
 fun <T> Single<T>.trace(traceName: String): Single<T> {
     val trace = FirebasePerformance.getInstance().newTrace(traceName)
     return this.doOnSubscribe { trace.start() }
-        .doOnSuccess { trace.incrementCounter(SUCCESS) }
-        .doOnError { trace.incrementCounter(FAILED) }
+        .doOnSuccess { trace.setSuccess(true) }
+        .doOnError { trace.setSuccess(false) }
         .doFinally { trace.stop() }
 }
 
 fun <T> Observable<T>.trace(traceName: String): Observable<T> {
     val trace = FirebasePerformance.getInstance().newTrace(traceName)
     return this.doOnSubscribe { trace.start() }
-        .doOnComplete { trace.incrementCounter(SUCCESS) }
-        .doOnError { trace.incrementCounter(FAILED) }
+        .doOnComplete { trace.setSuccess(true) }
+        .doOnError { trace.setSuccess(false) }
         .doFinally { trace.stop() }
 }
 
 fun <T> Task<T>.trace(traceName: String): Task<T> {
-    val trace = com.google.firebase.perf.FirebasePerformance.getInstance().newTrace(traceName)
+    val trace = FirebasePerformance.getInstance().newTrace(traceName)
     trace.start()
-    return this.addOnSuccessListener { trace.incrementCounter(com.simprints.id.tools.extensions.SUCCESS) }
-        .addOnFailureListener { trace.incrementCounter(com.simprints.id.tools.extensions.FAILED) }
-        .addOnCompleteListener { trace.stop() }
+    return this.addOnCompleteListener { trace.setSuccess(it.isSuccessful); trace.stop() }
 }
+
+private fun Trace.setSuccess(success: Boolean) =
+    if (success)
+        this.incrementCounter(CALL_SUCCESS_NAME, SUCCESS)
+    else
+        this.incrementCounter(CALL_SUCCESS_NAME, FAILURE)
+
 
 
