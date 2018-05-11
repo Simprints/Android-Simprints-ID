d125f6b9b22df8a8d91ce78ed9290ca4df5eb34e
Fabio Tuzza <fabio@simprints.com>
2018-05-12 00:56:58 +0100

    Fix building enum from shared prefs with integers instead of the name
    Former-commit-id: 345d648065a3039d87a14ae75115d305aa576b6f

--- id/src/main/java/com/simprints/id/tools/serializers/EnumSerializer.kt
+++ id/src/main/java/com/simprints/id/tools/serializers/EnumSerializer.kt
@@ -3,7 +3,7 @@ package com.simprints.id.tools.serializers
 import java.security.InvalidParameterException
 
 
-class EnumSerializer<T : Enum<*>>(enumClass: Class<T>): Serializer<T> {
+class EnumSerializer<T : Enum<*>>(private val enumClass: Class<T>): Serializer<T> {
 
     private val stringToEnumValue = enumClass.enumConstants
             .map { Pair(it.name, it) }
@@ -14,4 +14,8 @@ class EnumSerializer<T : Enum<*>>(enumClass: Class<T>): Serializer<T> {
 
     override fun deserialize(string: String): T =
             stringToEnumValue[string] ?: throw InvalidParameterException("Invalid serialized enum value")
+
+    fun deserialize(index: Int): T {
+        return enumClass.enumConstants[index]
+    }
 }
