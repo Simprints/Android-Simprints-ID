524e85fc1a5bcd6f0f7cdc68102259b7badc7d49
Fabio Tuzza <fabio@simprints.com>
2018-05-12 00:56:58 +0100

    Fix building enum from shared prefs with integers instead of the name
    Former-commit-id: efb8440edae066cb77e15ac1981e387ffb591577

--- id/src/main/java/com/simprints/id/tools/delegates/ComplexPreference.kt
+++ id/src/main/java/com/simprints/id/tools/delegates/ComplexPreference.kt
@@ -1,6 +1,8 @@
 package com.simprints.id.tools.delegates
 
 import com.simprints.id.data.prefs.improvedSharedPreferences.ImprovedSharedPreferences
+import com.simprints.id.exceptions.unsafe.MismatchedTypeError
+import com.simprints.id.tools.serializers.EnumSerializer
 import com.simprints.id.tools.serializers.Serializer
 import timber.log.Timber
 import kotlin.reflect.KProperty
@@ -13,7 +15,7 @@ import kotlin.reflect.KProperty
  *
  * @author etienne@simprints.com
  */
-class ComplexPreference<T:Any> (prefs: ImprovedSharedPreferences,
+class ComplexPreference<T:Any> (val prefs: ImprovedSharedPreferences,
                                 private val key: String,
                                 defValue: T,
                                 private val serializer: Serializer<T>) {
@@ -23,7 +25,34 @@ class ComplexPreference<T:Any> (prefs: ImprovedSharedPreferences,
 
     operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
         Timber.d("ComplexPreference.getValue $key")
-        return serializer.deserialize(serializedValue)
+
+        return try {
+            serializer.deserialize(serializedValue)
+        } catch (e: MismatchedTypeError){
+            ifEnumDeserializationThenWeTryReadEnumIndex() ?: throw e
+        }
+    }
+
+    /**
+     * if we deserialize an Enum and we get a MismatchedTypeError
+     * really likely that is because in the SharedPreference an integer (enum index) instead of String (enum name) is stored.
+     * It can happen when we change the type of a value stored in the SharedPref between versions
+     * without a migration process (as we do for Realm).
+     * https://fabric.io/simprints/android/apps/com.simprints.id/issues/5af29f5111e9fa0aa5f16cd8/sessions/latest?build=78388335
+     * https://github.com/Simprints/Android-Simprints-ID/commit/ae3c73e1d83739f0fc72a477f6f3f4576d223071#diff-b72b83e1b06c25b717b75c6919528a39
+     * So tentatively we try to build the enum from the integer saved in the sharedPref.
+     */
+    private fun ifEnumDeserializationThenWeTryReadEnumIndex(): T? {
+        try {
+            if(serializer is EnumSerializer) {
+                val serializedValue = prefs.getPrimitive(key, -1)
+                if (serializedValue > -1) {
+                    return serializer.deserialize(serializedValue)
+                }
+            }
+        } catch (t: Throwable){ t.printStackTrace() }
+
+        return null
     }
 
     operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
--- id/src/test/java/com/simprints/id/tools/delegates/ComplexPreferenceTest.kt
+++ id/src/test/java/com/simprints/id/tools/delegates/ComplexPreferenceTest.kt
@@ -1,8 +1,12 @@
 package com.simprints.id.tools.delegates
 
 import com.simprints.id.data.prefs.improvedSharedPreferences.ImprovedSharedPreferences
+import com.simprints.id.domain.Constants
+import com.simprints.id.exceptions.unsafe.MismatchedTypeError
+import com.simprints.id.testUtils.assertThrows
 import com.simprints.id.testUtils.mock
 import com.simprints.id.testUtils.whenever
+import com.simprints.id.tools.serializers.EnumSerializer
 import com.simprints.id.tools.serializers.Serializer
 import com.simprints.libsimprints.FingerIdentifier
 import org.junit.Assert.assertEquals
@@ -14,18 +18,27 @@ import org.mockito.Mockito.verify
 class ComplexPreferenceTest {
 
     companion object {
-        private val aKey = "aKey"
+        private const val aKey = "aKey"
+        private const val bKey = "bKey"
+        private const val cKey = "cKey"
         private val defaultFingerId = FingerIdentifier.LEFT_3RD_FINGER
-        private val serializedDefaultFingerId = "left_3rd_finger"
+        private const val serializedDefaultFingerId = "left_3rd_finger"
+
+        private val defaultGroupEnum = Constants.GROUP.USER
+        private val serializedDefaultGroupEnum = defaultGroupEnum.name
+
         private val storedFingerId = FingerIdentifier.LEFT_4TH_FINGER
-        private val storedSerializedFingerId = "left_4th_finger"
+        private const val storedSerializedFingerId = "left_4th_finger"
     }
 
     private val fingerIdSerializer = mockFingerIdentifierSerializer()
     private val improvedEditor = mockImprovedEditor()
     private val improvedPrefs = mockImprovedPreferences(improvedEditor)
-    private var fingerPreference by ComplexPreference(improvedPrefs, aKey, defaultFingerId,
-            fingerIdSerializer)
+    private var fingerPreference by ComplexPreference(improvedPrefs, aKey, defaultFingerId, fingerIdSerializer)
+
+    private var enumFromIndexInSharedPrefs by ComplexPreference(improvedPrefs, aKey, defaultGroupEnum, EnumSerializer(Constants.GROUP::class.java))
+    private var enumFromNameInSharedPrefs by ComplexPreference(improvedPrefs, bKey, defaultGroupEnum, EnumSerializer(Constants.GROUP::class.java))
+    private var enumFromWrontIndexInSharedPrefs by ComplexPreference(improvedPrefs, cKey, defaultGroupEnum, EnumSerializer(Constants.GROUP::class.java))
 
     private fun mockFingerIdentifierSerializer(): Serializer<FingerIdentifier> {
         val serializer = mock<Serializer<FingerIdentifier>>()
@@ -45,8 +58,34 @@ class ComplexPreferenceTest {
     private fun mockImprovedPreferences(editorToReturn: ImprovedSharedPreferences.Editor): ImprovedSharedPreferences {
         val prefs = mock<ImprovedSharedPreferences>()
         whenever(prefs.getPrimitive(aKey, serializedDefaultFingerId)).thenReturn(storedSerializedFingerId)
+
+        whenever(prefs.getPrimitive(aKey, -1)).thenReturn(2)
+        whenever(prefs.getPrimitive(aKey, serializedDefaultGroupEnum)).thenThrow(MismatchedTypeError("Expecting String, integer stored", Throwable("Expecting String, integer stored")))
+
+        whenever(prefs.getPrimitive(bKey, serializedDefaultGroupEnum)).thenReturn(Constants.GROUP.MODULE.name)
+
+        whenever(prefs.getPrimitive(cKey, -1)).thenReturn(5)
+        whenever(prefs.getPrimitive(cKey, serializedDefaultGroupEnum)).thenThrow(MismatchedTypeError("Expecting String, integer stored", Throwable("Expecting String, integer stored")))
         whenever(prefs.edit()).thenReturn(editorToReturn)
         return prefs
+    }
+
+    @Test
+    fun testDeserializeEnumWithIndexInSharedPrefs(){
+        //SharedPref: 2
+        assertEquals(enumFromIndexInSharedPrefs, Constants.GROUP.MODULE)
+    }
+
+    @Test
+    fun testDeserializingEnumWithWrongIndexInSharedPrefsThrowsAnException(){
+        //SharedPref: 5
+        assertThrows<MismatchedTypeError> { enumFromWrontIndexInSharedPrefs }
+    }
+
+    @Test
+    fun testDeserializeEnumWithNameInSharedPrefs(){
+        //SharedPref: "MODULE"
+        assertEquals(enumFromNameInSharedPrefs, Constants.GROUP.MODULE)
     }
 
     @Suppress("UNUSED_VARIABLE")
