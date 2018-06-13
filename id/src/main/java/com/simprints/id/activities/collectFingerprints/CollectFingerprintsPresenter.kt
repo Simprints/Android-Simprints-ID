4c7b1a944a2d27bd8fd51ce1d87a87e423c15f0e
Ridwan Farouki <ridwan@simprints.com>
2018-06-13 15:38:55 +0100

    Create MVP skeleton
    Former-commit-id: cfe3357d314abc887148595e53a1a96a9e4085b3

---
+++ id/src/main/java/com/simprints/id/activities/collectFingerprints/CollectFingerprintsPresenter.kt
@@ -0,0 +1,11 @@
+package com.simprints.id.activities.collectFingerprints
+
+import javax.inject.Inject
+
+
+class CollectFingerprintsPresenter : CollectFingerprintsContract.Presenter {
+
+    override fun start() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
