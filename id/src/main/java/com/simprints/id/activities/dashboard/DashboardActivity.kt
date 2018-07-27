8cb4acb015631643ef7e275f0756404ad807592f
Aashay Gaikwad <aashay@simprints.com>
2018-07-27 11:54:28 +0100

    WIP: Removing drawer from dashboard and collect fingerprints
    Former-commit-id: 4c275bb55ef299f736a15ba58658e526f2a604b4

--- id/src/main/java/com/simprints/id/activities/dashboard/DashboardActivity.kt
+++ id/src/main/java/com/simprints/id/activities/dashboard/DashboardActivity.kt
@@ -2,18 +2,13 @@ package com.simprints.id.activities.dashboard
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.NavigationView
-import android.support.v4.view.GravityCompat
-import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DefaultItemAnimator
-import android.view.MenuItem
-import android.widget.Toast
+import android.view.Menu
 import com.simprints.id.Application
 import com.simprints.id.R
 import com.simprints.id.activities.PrivacyActivity
 import com.simprints.id.activities.SettingsActivity
-import com.simprints.id.activities.about.AboutActivity
 import com.simprints.id.activities.dashboard.views.WrapContentLinearLayoutManager
 import com.simprints.id.activities.requestLogin.RequestLoginActivity
 import com.simprints.id.data.DataManager
@@ -22,12 +17,11 @@ import com.simprints.id.domain.ALERT_TYPE
 import com.simprints.id.tools.LanguageHelper
 import com.simprints.id.tools.extensions.launchAlert
 import kotlinx.android.synthetic.main.activity_dashboard.*
-import kotlinx.android.synthetic.main.app_bar_dashboard.*
 import kotlinx.android.synthetic.main.content_dashboard.*
 import org.jetbrains.anko.support.v4.onRefresh
 import javax.inject.Inject
 
-class DashboardActivity : AppCompatActivity(), DashboardContract.View, NavigationView.OnNavigationItemSelectedListener {
+class DashboardActivity : AppCompatActivity(), DashboardContract.View{
 
     @Inject lateinit var dataManager: DataManager
     @Inject lateinit var preferences: PreferencesManager
@@ -35,7 +29,6 @@ class DashboardActivity : AppCompatActivity(), DashboardContract.View, Navigatio
     companion object {
         private const val SETTINGS_ACTIVITY_REQUEST_CODE = 1
         private const val PRIVACY_ACTIVITY_REQUEST_CODE = 2
-        private const val ABOUT_ACTIVITY_REQUEST_CODE = 3
     }
 
     override lateinit var viewPresenter: DashboardContract.Presenter
@@ -46,21 +39,13 @@ class DashboardActivity : AppCompatActivity(), DashboardContract.View, Navigatio
         setContentView(R.layout.activity_dashboard)
         val component = (application as Application).component
         component.inject(this)
+        setSupportActionBar(dashboardToolbar)
         LanguageHelper.setLanguage(this, preferences.language)
 
         viewPresenter = DashboardPresenter(this, component)
+        setMenuItemClickListener()
 
-        initDrawer()
         initCards()
-    }
-
-    private fun initDrawer() {
-        setSupportActionBar(dashboardToolbar)
-        val toggle = ActionBarDrawerToggle(
-            this, dashboardDrawerLayout, dashboardToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        dashboardDrawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-        dashboardNavigationView.setNavigationItemSelectedListener(this)
     }
 
     private fun initCards() {
@@ -105,19 +90,36 @@ class DashboardActivity : AppCompatActivity(), DashboardContract.View, Navigatio
         viewPresenter.start()
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-
-        when (id) {
-            R.id.nav_help -> Toast.makeText(this, R.string.coming_soon, Toast.LENGTH_SHORT).show()
-            R.id.privacy -> startActivityForResult(Intent(this, PrivacyActivity::class.java), PRIVACY_ACTIVITY_REQUEST_CODE)
-            R.id.nav_about -> startActivityForResult(Intent(this, AboutActivity::class.java), ABOUT_ACTIVITY_REQUEST_CODE)
-            R.id.nav_settings -> startActivityForResult(Intent(this, SettingsActivity::class.java), SETTINGS_ACTIVITY_REQUEST_CODE)
-            R.id.logout -> logout()
-        }
-        dashboardDrawerLayout.closeDrawer(GravityCompat.START)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_menu, menu)
         return true
     }
+
+    private fun setMenuItemClickListener() {
+        dashboardToolbar.setOnMenuItemClickListener { menuItem ->
+
+            val id = menuItem.itemId
+            when(id) {
+                R.id.menuPrivacyNotice -> startActivityForResult(Intent(this, PrivacyActivity::class.java), PRIVACY_ACTIVITY_REQUEST_CODE)
+                R.id.menuSettings -> startActivityForResult(Intent(this, SettingsActivity::class.java), SETTINGS_ACTIVITY_REQUEST_CODE)
+                R.id.menuLogout -> logout()
+            }
+            true
+        }
+    }
+
+//    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+//        val id = item.itemId
+//
+//        when (id) {
+//            R.id.nav_help -> Toast.makeText(this, R.string.coming_soon, Toast.LENGTH_SHORT).show()
+//            R.id.privacy -> startActivityForResult(Intent(this, PrivacyActivity::class.java), PRIVACY_ACTIVITY_REQUEST_CODE)
+//            R.id.nav_about -> startActivityForResult(Intent(this, AboutActivity::class.java), ABOUT_ACTIVITY_REQUEST_CODE)
+//            R.id.nav_settings -> startActivityForResult(Intent(this, SettingsActivity::class.java), SETTINGS_ACTIVITY_REQUEST_CODE)
+//            R.id.logout -> logout()
+//        }
+//        return true
+//    }
 
     private fun logout() {
         viewPresenter.logout()
