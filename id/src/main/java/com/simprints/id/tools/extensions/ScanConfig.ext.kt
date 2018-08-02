87f38697a157eb25c9f29862041ef1191ce223c5
Ridwan Farouki <ridwan@simprints.com>
2018-08-02 16:01:19 +0100

    Implemented working remote config for all complex preferences
    - Includes remote config for which fingers to collect
    
    Former-commit-id: 3e74ab8670b99fe12c6d8d33ec0c50291e04acb3

--- id/src/main/java/com/simprints/id/tools/extensions/ScanConfig.ext.kt
+++ id/src/main/java/com/simprints/id/tools/extensions/ScanConfig.ext.kt
@@ -4,6 +4,4 @@ import com.simprints.libcommon.FingerConfig
 import com.simprints.libcommon.ScanConfig
 import com.simprints.libsimprints.FingerIdentifier
 
-fun ScanConfig.isFingerRequired(id: FingerIdentifier): Boolean = get(id) == FingerConfig.REQUIRED
-fun ScanConfig.isFingerOptional(id: FingerIdentifier): Boolean = get(id) == FingerConfig.OPTIONAL
 fun ScanConfig.isFingerNotCollectable(id: FingerIdentifier): Boolean = get(id) == FingerConfig.DO_NOT_COLLECT
