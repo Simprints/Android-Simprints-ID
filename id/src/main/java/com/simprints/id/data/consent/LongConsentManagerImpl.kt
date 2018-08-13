930800fc19cc26f7d09fedc7fe7b916c5c3d93e5
Tristram Norman <tristram@simprints.com>
2018-08-13 19:10:29 +0100

    finish first version of download + file fetching
    Former-commit-id: 5847126fa2e851048f53d72ca0faae9c9ffb7fcb

--- id/src/main/java/com/simprints/id/data/consent/LongConsentManagerImpl.kt
+++ id/src/main/java/com/simprints/id/data/consent/LongConsentManagerImpl.kt
@@ -1,38 +1,62 @@
 package com.simprints.id.data.consent
 
 import android.content.Context
+import android.net.Uri
 import com.google.firebase.storage.FirebaseStorage
-import com.simprints.id.data.prefs.PreferencesManager
+import com.simprints.id.data.loginInfo.LoginInfoManager
 import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
 import java.io.File
 
 
-class LongConsentManagerImpl(private val preferencesManager: PreferencesManager,
-                             private val context: Context) {
+class LongConsentManagerImpl(private val loginInfoManager: LoginInfoManager,
+                             context: Context,
+                             private val firebaseStorage: FirebaseStorage) : LongConsentManager {
 
     companion object {
         private const val FILE_PATH = "long-consents"
+        private const val FILE_TYPE = "pdf"
     }
 
-    // Create a storage reference from our app
-    val storage = FirebaseStorage.getInstance()
-    var projectRef = storage.getReference(preferencesManager.projectId)
+    private val filePath: File
 
-    fun downloadLongConsent(language: String):
-        Flowable<Pair<Long, String>> = Flowable.create<Pair<Long, String>>({ emitter ->
+    init {
+        filePath = File(context.filesDir.absolutePath +
+            File.separator +
+            FILE_PATH +
+            File.separator +
+            loginInfoManager.signedInProjectId)
 
-        val file = File(context.filesDir,
-            "$FILE_PATH/${preferencesManager.projectId}-$language.pdf")
+        if (!filePath.exists())
+            filePath.mkdirs()
+    }
 
-        projectRef.child(language).getFile(file).addOnSuccessListener {
-            emitter.onComplete()
-        }.addOnFailureListener {
-            emitter.onError(it)
-        }.addOnProgressListener {
-            emitter.onNext(Pair(it.bytesTransferred / it.totalByteCount, file.absolutePath))
-        }
+    override fun downloadLongConsent(language: String):
+        Flowable<Int> = Flowable.create<Int>({ emitter ->
+
+        if (language.isBlank())
+            emitter.onError(IllegalStateException("Invalid language choice: $language"))
+
+        val file = File(filePath, "$language.$FILE_TYPE")
+
+        firebaseStorage.getReference(FILE_PATH)
+            .child(loginInfoManager.signedInProjectId)
+            .child("$language.$FILE_TYPE")
+            .getFile(file)
+            .addOnSuccessListener {
+                emitter.onComplete()
+            }.addOnFailureListener {
+                emitter.onError(it)
+            }.addOnProgressListener {
+                emitter.onNext(((it.bytesTransferred.toDouble() / it.totalByteCount.toDouble()) * 100).toInt())
+            }
 
     }, BackpressureStrategy.BUFFER)
+
+    override fun checkIfLongConsentExists(language: String): Boolean = File(filePath, "$language.$FILE_TYPE").exists()
+
+    override fun getLongConsentUri(language: String): Uri {
+        return Uri.fromFile(File(filePath, "$language.$FILE_TYPE"))
+    }
 
 }
