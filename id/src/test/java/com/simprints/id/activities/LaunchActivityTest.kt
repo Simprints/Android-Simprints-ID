aaa5b2541f093c4561bb15245cb23cadc354ef34
Ridwan Farouki <ridwan@simprints.com>
2018-08-16 20:06:17 +0100

    Some more tests for malformed consent text with missing/extra key/values
    Former-commit-id: 4c291b7a55c83f6c22b1c481b4a326bb2458d093

--- id/src/test/java/com/simprints/id/activities/LaunchActivityTest.kt
+++ id/src/test/java/com/simprints/id/activities/LaunchActivityTest.kt
@@ -128,6 +128,51 @@ class LaunchActivityTest : RxJavaTest, DaggerForTests() {
         Assert.assertEquals(defaultParentalConsentText, parentConsentText)
     }
 
+    @Test
+    fun malformedConsentJson_showsDefaultConsent() {
+        mockSettingsPreferencesManager(generalConsentOptions = MALFORMED_CONSENT_OPTIONS)
+
+        val calloutAction = CalloutAction.IDENTIFY
+        preferencesManager.calloutAction = calloutAction
+
+        val controller = createRoboLaunchActivity().start().resume().visible()
+        val activity = controller.get()
+
+        val generalConsentText = activity.generalConsentTextView.text.toString()
+        val defaultGeneralConsentText = GeneralConsent().assembleText(activity, calloutAction, PROGRAM_NAME, ORGANIZATION_NAME)
+        Assert.assertEquals(defaultGeneralConsentText, generalConsentText)
+    }
+
+    @Test
+    fun extraUnrecognisedConsentOptions_stillShowsCorrectValues() {
+        mockSettingsPreferencesManager(generalConsentOptions = EXTRA_UNRECOGNISED_CONSENT_OPTIONS)
+
+        val calloutAction = CalloutAction.IDENTIFY
+        preferencesManager.calloutAction = calloutAction
+
+        val controller = createRoboLaunchActivity().start().resume().visible()
+        val activity = controller.get()
+
+        val generalConsentText = activity.generalConsentTextView.text.toString()
+        val targetConsentText = EXTRA_UNRECOGNISED_CONSENT_TARGET.assembleText(activity, calloutAction, PROGRAM_NAME, ORGANIZATION_NAME)
+        Assert.assertEquals(targetConsentText, generalConsentText)
+    }
+
+    @Test
+    fun partiallyMissingConsentOptions_stillShowsCorrectValues() {
+        mockSettingsPreferencesManager(generalConsentOptions = PARTIALLY_MISSING_CONSENT_OPTIONS)
+
+        val calloutAction = CalloutAction.IDENTIFY
+        preferencesManager.calloutAction = calloutAction
+
+        val controller = createRoboLaunchActivity().start().resume().visible()
+        val activity = controller.get()
+
+        val generalConsentText = activity.generalConsentTextView.text.toString()
+        val targetConsentText = PARTIALLY_MISSING_CONSENT_TARGET.assembleText(activity, calloutAction, PROGRAM_NAME, ORGANIZATION_NAME)
+        Assert.assertEquals(targetConsentText, generalConsentText)
+    }
+
     private fun mockSettingsPreferencesManager(parentalConsentExists: Boolean = false,
                                                generalConsentOptions: String = REMOTE_CONSENT_GENERAL_OPTIONS,
                                                parentalConsentOptions: String = REMOTE_CONSENT_PARENTAL_OPTIONS) {
@@ -147,5 +192,13 @@ class LaunchActivityTest : RxJavaTest, DaggerForTests() {
         private const val ORGANIZATION_NAME = "ORGANIZATION NAME"
         private const val REMOTE_CONSENT_GENERAL_OPTIONS = "{\"consent_enrol_only\":false,\"consent_enrol\":true,\"consent_id_verify\":true,\"consent_share_data_no\":true,\"consent_share_data_yes\":false,\"consent_collect_yes\":false,\"consent_privacy_rights\":true,\"consent_confirmation\":true}"
         private const val REMOTE_CONSENT_PARENTAL_OPTIONS = "{\"consent_parent_enrol_only\":false,\"consent_parent_enrol\":true,\"consent_parent_id_verify\":true,\"consent_parent_share_data_no\":true,\"consent_parent_share_data_yes\":false,\"consent_parent_collect_yes\":false,\"consent_parent_privacy_rights\":true,\"consent_parent_confirmation\":true}"
+
+        private const val MALFORMED_CONSENT_OPTIONS = "gibberish{\"000}\"\""
+
+        private const val EXTRA_UNRECOGNISED_CONSENT_OPTIONS = "{\"consent_enrol_only\":true,\"consent_enrol\":false,\"consent_id_verify\":true,\"consent_share_data_no\":true,\"consent_share_data_yes\":false,\"consent_collect_yes\":false,\"consent_privacy_rights\":true,\"consent_confirmation\":true,\"this_one_doesnt_exist\":true}"
+        private val EXTRA_UNRECOGNISED_CONSENT_TARGET = GeneralConsent(consentEnrolOnly = true, consentEnrol = false)
+
+        private const val PARTIALLY_MISSING_CONSENT_OPTIONS = "{\"consent_enrol_only\":true,\"consent_enrol\":false,\"consent_id_verify\":true,\"consent_share_data_no\":true,\"consent_share_data_yes\":false,\"consent_collect_yes\":false}"
+        private val PARTIALLY_MISSING_CONSENT_TARGET = GeneralConsent(consentEnrolOnly = true, consentEnrol = false)
     }
 }
