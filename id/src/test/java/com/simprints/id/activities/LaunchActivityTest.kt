1a871492ea82aac12b1b77fc5bdf2cf480b06ba6
Ridwan Farouki <ridwan@simprints.com>
2018-08-16 18:53:44 +0100

    Expanded launch activity tests to include parental consent
    Former-commit-id: ca7c3c680d1019a9e010d2b76c11a00ec8bfc785

--- id/src/test/java/com/simprints/id/activities/LaunchActivityTest.kt
+++ id/src/test/java/com/simprints/id/activities/LaunchActivityTest.kt
@@ -2,11 +2,15 @@ package com.simprints.id.activities
 
 import com.google.firebase.FirebaseApp
 import com.simprints.id.data.prefs.PreferencesManager
+import com.simprints.id.data.prefs.settings.SettingsPreferencesManager
 import com.simprints.id.di.AppModuleForTests
 import com.simprints.id.di.DaggerForTests
 import com.simprints.id.domain.consent.GeneralConsent
+import com.simprints.id.domain.consent.ParentalConsent
 import com.simprints.id.session.callout.CalloutAction
 import com.simprints.id.shared.DependencyRule.MockRule
+import com.simprints.id.shared.PreferencesModuleForAnyTests
+import com.simprints.id.shared.whenever
 import com.simprints.id.testUtils.base.RxJavaTest
 import com.simprints.id.testUtils.roboletric.TestApplication
 import com.simprints.id.testUtils.roboletric.createRoboLaunchActivity
@@ -26,6 +30,13 @@ import javax.inject.Inject
 class LaunchActivityTest : RxJavaTest, DaggerForTests() {
 
     @Inject lateinit var preferencesManager: PreferencesManager
+    @Inject lateinit var settingsPreferencesManager: SettingsPreferencesManager
+
+    override var preferencesModule by lazyVar {
+        PreferencesModuleForAnyTests(
+            settingsPreferencesManagerRule = MockRule
+        )
+    }
 
     override var module by lazyVar {
         AppModuleForTests(app,
@@ -44,7 +55,9 @@ class LaunchActivityTest : RxJavaTest, DaggerForTests() {
     }
 
     @Test
-    fun enrollmentCallout_showsCorrectGeneralConsentText() {
+    fun enrollmentCallout_showsCorrectGeneralConsentTextAndNoParentalByDefault() {
+        mockSettingsPreferencesManager(parentalConsentExists = false)
+
         val calloutAction = CalloutAction.REGISTER
         preferencesManager.calloutAction = calloutAction
 
@@ -52,12 +65,17 @@ class LaunchActivityTest : RxJavaTest, DaggerForTests() {
         val activity = controller.get()
 
         val generalConsentText = activity.generalConsentTextView.text.toString()
-        val defaultGeneralConsentText = GeneralConsent().assembleText(activity, calloutAction, preferencesManager.programName, preferencesManager.organizationName)
+        val defaultGeneralConsentText = GeneralConsent().assembleText(activity, calloutAction, PROGRAM_NAME, ORGANIZATION_NAME)
         Assert.assertEquals(defaultGeneralConsentText, generalConsentText)
+
+        val parentConsentText = activity.parentalConsentTextView.text.toString()
+        Assert.assertEquals("", parentConsentText)
     }
 
     @Test
-    fun identifyCallout_showsCorrectGeneralConsentText() {
+    fun identifyCallout_showsCorrectGeneralConsentTextAndNoParentalByDefault() {
+        mockSettingsPreferencesManager(parentalConsentExists = false)
+
         val calloutAction = CalloutAction.IDENTIFY
         preferencesManager.calloutAction = calloutAction
 
@@ -65,7 +83,69 @@ class LaunchActivityTest : RxJavaTest, DaggerForTests() {
         val activity = controller.get()
 
         val generalConsentText = activity.generalConsentTextView.text.toString()
-        val defaultGeneralConsentText = GeneralConsent().assembleText(activity, calloutAction, preferencesManager.programName, preferencesManager.organizationName)
+        val defaultGeneralConsentText = GeneralConsent().assembleText(activity, calloutAction, PROGRAM_NAME, ORGANIZATION_NAME)
         Assert.assertEquals(defaultGeneralConsentText, generalConsentText)
+
+        val parentConsentText = activity.parentalConsentTextView.text.toString()
+        Assert.assertEquals("", parentConsentText)
+    }
+
+    @Test
+    fun enrollmentCallout_showsBothConsentsCorrectlyWhenParentalConsentExists() {
+        mockSettingsPreferencesManager(parentalConsentExists = true)
+
+        val calloutAction = CalloutAction.REGISTER
+        preferencesManager.calloutAction = calloutAction
+
+        val controller = createRoboLaunchActivity().start().resume().visible()
+        val activity = controller.get()
+
+        val generalConsentText = activity.generalConsentTextView.text.toString()
+        val defaultGeneralConsentText = GeneralConsent().assembleText(activity, calloutAction, PROGRAM_NAME, ORGANIZATION_NAME)
+        Assert.assertEquals(defaultGeneralConsentText, generalConsentText)
+
+        val parentConsentText = activity.parentalConsentTextView.text.toString()
+        val defaultParentalConsentText = ParentalConsent().assembleText(activity, calloutAction, PROGRAM_NAME, ORGANIZATION_NAME)
+        Assert.assertEquals(defaultParentalConsentText, parentConsentText)
+    }
+
+    @Test
+    fun identifyCallout_showsBothConsentsCorrectlyWhenParentalConsentExists() {
+        mockSettingsPreferencesManager(parentalConsentExists = true)
+
+        val calloutAction = CalloutAction.IDENTIFY
+        preferencesManager.calloutAction = calloutAction
+
+        val controller = createRoboLaunchActivity().start().resume().visible()
+        val activity = controller.get()
+
+        val generalConsentText = activity.generalConsentTextView.text.toString()
+        val defaultGeneralConsentText = GeneralConsent().assembleText(activity, calloutAction, PROGRAM_NAME, ORGANIZATION_NAME)
+        Assert.assertEquals(defaultGeneralConsentText, generalConsentText)
+
+        val parentConsentText = activity.parentalConsentTextView.text.toString()
+        val defaultParentalConsentText = ParentalConsent().assembleText(activity, calloutAction, PROGRAM_NAME, ORGANIZATION_NAME)
+        Assert.assertEquals(defaultParentalConsentText, parentConsentText)
+    }
+
+    private fun mockSettingsPreferencesManager(parentalConsentExists: Boolean = false,
+                                               generalConsentOptions: String = REMOTE_CONSENT_GENERAL_OPTIONS,
+                                               parentalConsentOptions: String = REMOTE_CONSENT_PARENTAL_OPTIONS) {
+
+        whenever(settingsPreferencesManager.language).thenReturn(LANGUAGE)
+        whenever(settingsPreferencesManager.programName).thenReturn(PROGRAM_NAME)
+        whenever(settingsPreferencesManager.organizationName).thenReturn(ORGANIZATION_NAME)
+
+        whenever(settingsPreferencesManager.parentalConsentExists).thenReturn(parentalConsentExists)
+        whenever(settingsPreferencesManager.generalConsentOptionsJson).thenReturn(generalConsentOptions)
+        whenever(settingsPreferencesManager.parentalConsentOptionsJson).thenReturn(parentalConsentOptions)
+    }
+
+    companion object {
+        private const val LANGUAGE = "en"
+        private const val PROGRAM_NAME = "PROGRAM NAME"
+        private const val ORGANIZATION_NAME = "ORGANIZATION NAME"
+        private const val REMOTE_CONSENT_GENERAL_OPTIONS = "{\"consent_enrol_only\":false,\"consent_enrol\":true,\"consent_id_verify\":true,\"consent_share_data_no\":true,\"consent_share_data_yes\":false,\"consent_collect_yes\":false,\"consent_privacy_rights\":true,\"consent_confirmation\":true}"
+        private const val REMOTE_CONSENT_PARENTAL_OPTIONS = "{\"consent_parent_enrol_only\":false,\"consent_parent_enrol\":true,\"consent_parent_id_verify\":true,\"consent_parent_share_data_no\":true,\"consent_parent_share_data_yes\":false,\"consent_parent_collect_yes\":false,\"consent_parent_privacy_rights\":true,\"consent_parent_confirmation\":true}"
     }
 }
