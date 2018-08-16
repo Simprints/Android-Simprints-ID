585f599a73bbf4c19751d11550a375cfd1f01586
Tristram Norman <tristram@simprints.com>
2018-08-16 14:20:38 +0100

    implement long consent view
    Former-commit-id: bd1691fdabca3fbae03356aabe356670db851e0e

--- id/src/main/java/com/simprints/id/activities/longConsent/LongConsentActivity.kt
+++ id/src/main/java/com/simprints/id/activities/longConsent/LongConsentActivity.kt
@@ -4,22 +4,22 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.simprints.id.Application
 import com.simprints.id.R
-import com.simprints.id.data.consent.LongConsentManager
+import com.simprints.id.data.prefs.PreferencesManager
+import com.simprints.id.tools.LanguageHelper
 import kotlinx.android.synthetic.main.activity_long_consent.*
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.uiThread
 import javax.inject.Inject
 
 
 class LongConsentActivity : AppCompatActivity(), LongConsentContract.View {
 
     @Inject
-    lateinit var longConsentManager: LongConsentManager
+    lateinit var preferences: PreferencesManager
 
     override lateinit var viewPresenter: LongConsentContract.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LanguageHelper.setLanguage(this, preferences.language)
         setContentView(R.layout.activity_long_consent)
 
         val component = (application as Application).component
@@ -27,13 +27,11 @@ class LongConsentActivity : AppCompatActivity(), LongConsentContract.View {
 
         viewPresenter = LongConsentPresenter(this)
         viewPresenter.start()
-
-        doAsync {
-            val shitTonOfText = longConsentManager.getLongConsentText(longConsentManager.languages[0])
-            uiThread {
-                longConsent_TextView.text = shitTonOfText.subSequence(0, 15000)
-                longConsent_ScrollView.requestLayout()
-            }
-        }
     }
+
+    override fun setLongConsentText(text: String) {
+        longConsent_TextView.text = text
+        longConsent_ScrollView.requestLayout()
+    }
+
 }
--- id/src/main/java/com/simprints/id/activities/longConsent/LongConsentPresenter.kt
+++ id/src/main/java/com/simprints/id/activities/longConsent/LongConsentPresenter.kt
@@ -1,7 +1,32 @@
 package com.simprints.id.activities.longConsent
 
+import com.simprints.id.data.consent.LongConsentManager
+import com.simprints.id.data.prefs.PreferencesManager
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import javax.inject.Inject
+
 class LongConsentPresenter(val view: LongConsentContract.View) : LongConsentContract.Presenter {
 
+    @Inject
+    lateinit var longConsentManager: LongConsentManager
+    @Inject
+    lateinit var preferences: PreferencesManager
+
     override fun start() {
+
+        doAsync {
+
+            val selectedLanguage: String = preferences.language.let {
+                if (it.isBlank()) "en" else it
+            }
+
+            if (longConsentManager.checkIfLongConsentExists(selectedLanguage)) {
+                val longConsentText = longConsentManager.getLongConsentText(selectedLanguage)
+                uiThread { view.setLongConsentText(longConsentText) }
+            }
+
+        }
+
     }
 }
