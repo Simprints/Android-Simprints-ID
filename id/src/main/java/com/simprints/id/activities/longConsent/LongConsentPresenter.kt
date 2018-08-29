bf92113b7ab75a5b728e8d996e856edb938c3648
Ridwan Farouki <ridwan@simprints.com>
2018-08-29 12:51:35 +0100

    Added download functionality to long consent
    Former-commit-id: 8f15cc8f8f704e1a5984a924ca85ffb432530c98

--- id/src/main/java/com/simprints/id/activities/longConsent/LongConsentPresenter.kt
+++ id/src/main/java/com/simprints/id/activities/longConsent/LongConsentPresenter.kt
@@ -3,6 +3,7 @@ package com.simprints.id.activities.longConsent
 import com.simprints.id.data.consent.LongConsentManager
 import com.simprints.id.data.prefs.PreferencesManager
 import com.simprints.id.di.AppComponent
+import io.reactivex.rxkotlin.subscribeBy
 import javax.inject.Inject
 
 class LongConsentPresenter(val view: LongConsentContract.View,
@@ -20,10 +21,25 @@ class LongConsentPresenter(val view: LongConsentContract.View,
             val longConsentText = longConsentManager.getLongConsentText(preferences.language)
             view.setLongConsentText(longConsentText)
         } else {
-            view.setDefaultLongConsent()
+            view.setNoPrivacyNoticeFound()
         }
-
-        view.showProgressBar = false
     }
 
+    override fun downloadLongConsent() {
+        view.setDownloadInProgress(true)
+        longConsentManager.downloadLongConsentWithProgress(preferences.language)
+            .subscribeBy(
+                onNext = {
+                    view.setDownloadProgress(it)
+                },
+                onComplete = {
+                    view.setDownloadInProgress(false)
+                    view.setLongConsentText(longConsentManager.getLongConsentText(preferences.language))
+                },
+                onError = {
+                    view.setDownloadInProgress(false)
+                    view.showDownloadErrorToast()
+                }
+            )
+    }
 }
