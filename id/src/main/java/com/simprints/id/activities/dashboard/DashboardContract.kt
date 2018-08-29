01dc9eac3a35d8766365bb3f72251a5efe4bdc76
Ridwan Farouki <ridwan@simprints.com>
2018-08-29 13:14:28 +0100

    Removed download long consent from dashboard
    Former-commit-id: 0d58c075bfc7f25161b1ef218b10340c1ea38801

--- id/src/main/java/com/simprints/id/activities/dashboard/DashboardContract.kt
+++ id/src/main/java/com/simprints/id/activities/dashboard/DashboardContract.kt
@@ -13,9 +13,6 @@ interface DashboardContract {
         fun launchAlertView(error: ALERT_TYPE)
         fun notifyCardViewChanged(position: Int)
         fun getStringWithParams(stringRes: Int, currentValue: Int = 0, maxValue: Int = 0): String
-        fun updateNotification(language: String, progress: Int)
-        fun cancelNotification(language: String)
-        fun completeNotification(language: String)
         fun showConfirmationDialogForLogout()
     }
 
