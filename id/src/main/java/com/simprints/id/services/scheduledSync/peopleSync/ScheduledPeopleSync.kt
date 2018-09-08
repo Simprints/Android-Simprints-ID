002de16672bfd37ff7bc3f6e0cc530de20509db4
Fabio Tuzza <fabio@simprints.com>
2018-09-08 23:47:19 +0100

    Bug fixing: using SyncExecutor instad of SyncService for background sync
    Former-commit-id: f8ba566f8c9d054a95f47efec74dddcbb4312c6e

--- id/src/main/java/com/simprints/id/services/scheduledSync/peopleSync/ScheduledPeopleSync.kt
+++ id/src/main/java/com/simprints/id/services/scheduledSync/peopleSync/ScheduledPeopleSync.kt
@@ -2,28 +2,44 @@ package com.simprints.id.services.scheduledSync.peopleSync
 
 import androidx.work.Worker
 import com.simprints.id.Application
-import com.simprints.id.data.db.sync.SyncManager
+import com.simprints.id.data.db.DbManager
+import com.simprints.id.data.db.sync.SyncExecutor
 import com.simprints.id.data.loginInfo.LoginInfoManager
 import com.simprints.id.data.prefs.PreferencesManager
-import com.simprints.id.services.sync.SyncCategory
 import com.simprints.id.services.sync.SyncTaskParameters
+import com.simprints.id.tools.json.JsonHelper
+import io.reactivex.rxkotlin.subscribeBy
+import timber.log.Timber
+import java.util.concurrent.LinkedBlockingQueue
 import javax.inject.Inject
-
 
 class ScheduledPeopleSync : Worker() {
 
-    @Inject lateinit var syncManager: SyncManager
+    @Inject lateinit var dbManager: DbManager
     @Inject lateinit var preferencesManager: PreferencesManager
     @Inject lateinit var loginInfoManager: LoginInfoManager
 
     override fun doWork(): Result {
+        val result = LinkedBlockingQueue<Result>()
+
         if (applicationContext is Application) {
             (applicationContext as Application).component.inject(this)
 
-            syncManager.sync(SyncTaskParameters.build(preferencesManager.syncGroup, preferencesManager.moduleId, loginInfoManager), SyncCategory.SCHEDULED_BACKGROUND)
+            Timber.d("ScheduledPeopleSync - doWork")
 
-            return Result.SUCCESS
+            SyncExecutor(dbManager, JsonHelper.gson)
+                .sync(syncParams = SyncTaskParameters.build(preferencesManager.syncGroup, preferencesManager.moduleId, loginInfoManager))
+                .subscribeBy(onError = {
+                    Timber.d("ScheduledPeopleSync - onError")
+                    Timber.e(it)
+                    result.put(Result.FAILURE)
+                }, onComplete = {
+                    Timber.d("ScheduledPeopleSync - onComplete")
+                    result.put(Result.SUCCESS)
+                }, onNext = {
+                    Timber.d("ScheduledPeopleSync - onProgress: ${it.currentValue}/${it.maxValue}")
+                })
         }
-        return Result.FAILURE
+        return result.take()
     }
 }
