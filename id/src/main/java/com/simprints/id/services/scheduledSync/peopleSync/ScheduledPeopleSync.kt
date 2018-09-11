4d0cefe82f340871aaadbbb2d70e58ea442438bf
Fabio Tuzza <fabio@simprints.com>
2018-09-11 14:28:40 +0100

    Revert sheduledPeopleSync work to avoid using service
    Former-commit-id: 867714a12f114fe24cda354598a5ce4b5d1fac27

--- id/src/main/java/com/simprints/id/services/scheduledSync/peopleSync/ScheduledPeopleSync.kt
+++ id/src/main/java/com/simprints/id/services/scheduledSync/peopleSync/ScheduledPeopleSync.kt
@@ -2,44 +2,28 @@ package com.simprints.id.services.scheduledSync.peopleSync
 
 import androidx.work.Worker
 import com.simprints.id.Application
-import com.simprints.id.data.db.DbManager
-import com.simprints.id.data.db.sync.SyncExecutor
+import com.simprints.id.data.db.sync.SyncManager
 import com.simprints.id.data.loginInfo.LoginInfoManager
 import com.simprints.id.data.prefs.PreferencesManager
+import com.simprints.id.services.sync.SyncCategory
 import com.simprints.id.services.sync.SyncTaskParameters
-import com.simprints.id.tools.json.JsonHelper
-import io.reactivex.rxkotlin.subscribeBy
-import timber.log.Timber
-import java.util.concurrent.LinkedBlockingQueue
 import javax.inject.Inject
+
 
 class ScheduledPeopleSync : Worker() {
 
-    @Inject lateinit var dbManager: DbManager
+    @Inject lateinit var syncManager: SyncManager
     @Inject lateinit var preferencesManager: PreferencesManager
     @Inject lateinit var loginInfoManager: LoginInfoManager
 
     override fun doWork(): Result {
-        val result = LinkedBlockingQueue<Result>()
-
         if (applicationContext is Application) {
             (applicationContext as Application).component.inject(this)
 
-            Timber.d("ScheduledPeopleSync - doWork")
+            syncManager.sync(SyncTaskParameters.build(preferencesManager.syncGroup, preferencesManager.moduleId, loginInfoManager), SyncCategory.SCHEDULED_BACKGROUND)
 
-            SyncExecutor(dbManager, JsonHelper.gson)
-                .sync(syncParams = SyncTaskParameters.build(preferencesManager.syncGroup, preferencesManager.moduleId, loginInfoManager))
-                .subscribeBy(onError = {
-                    Timber.d("ScheduledPeopleSync - onError")
-                    Timber.e(it)
-                    result.put(Result.FAILURE)
-                }, onComplete = {
-                    Timber.d("ScheduledPeopleSync - onComplete")
-                    result.put(Result.SUCCESS)
-                }, onNext = {
-                    Timber.d("ScheduledPeopleSync - onProgress: ${it.currentValue}/${it.maxValue}")
-                })
+            return Result.SUCCESS
         }
-        return result.take()
+        return Result.FAILURE
     }
 }
