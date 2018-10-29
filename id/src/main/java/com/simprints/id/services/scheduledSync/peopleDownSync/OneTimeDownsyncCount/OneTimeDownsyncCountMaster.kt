bc10f55bd45a7ff1c16ff516b68f46e5821c3470
Aashay Gaikwad <aashay@simprints.com>
2018-10-29 11:51:41 +0000

    Worker for OneTimeDownSyncCount
    Former-commit-id: 1cc31d84ab43eae1ac2ee9027c406b44041195f1

---
+++ id/src/main/java/com/simprints/id/services/scheduledSync/peopleDownSync/OneTimeDownsyncCount/OneTimeDownsyncCountMaster.kt
@@ -0,0 +1,33 @@
+package com.simprints.id.services.scheduledSync.peopleDownSync.OneTimeDownsyncCount
+
+import androidx.work.*
+
+class OneTimeDownsyncCountMaster(
+    private val getWorkManager: () -> WorkManager = WorkManager::getInstance
+) {
+    fun schedule(projectId: String, userId: String) {
+        getWorkManager().beginUniqueWork(
+            getUniqueOneTimeDownSyncCountWorkName(projectId, userId),
+            ExistingWorkPolicy.REPLACE,
+            buildWorkRequest(projectId, userId)
+        )
+            .enqueue()
+    }
+
+    private fun buildWorkRequest(projectId: String, userId: String) =
+        OneTimeWorkRequestBuilder<OneTimeDownsyncCountWorker>()
+            .setConstraints(buildConstraints())
+            .build()
+
+    private fun buildConstraints() =
+        Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+    private fun getUniqueOneTimeDownSyncCountWorkName(projectId: String, userId: String) =
+        "$projectId-$userId-$ONE_TIME_DOWN_SYNC_WORK_NAME_SUFFIX"
+
+    companion object {
+        const val ONE_TIME_DOWN_SYNC_WORK_NAME_SUFFIX = "down-sync"
+    }
+}
---
+++ id/src/main/java/com/simprints/id/services/scheduledSync/peopleDownSync/OneTimeDownsyncCount/OneTimeDownsyncCountWorker.kt
@@ -0,0 +1,11 @@
+package com.simprints.id.services.scheduledSync.peopleDownSync.OneTimeDownsyncCount
+
+import androidx.work.Worker
+
+class OneTimeDownsyncCountWorker: Worker() {
+
+    override fun doWork(): Result {
+        //TODO: refer design spec
+        return Result.SUCCESS
+    }
+}
