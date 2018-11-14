2188e8b5c03e9e4e036139c55dac3fa754ab3eb0
Ridwan Farouki <ridwan@simprints.com>
2018-11-14 14:37:56 +0000

    Added documentation an examples to test templates
    Former-commit-id: 8ab29220a8be889f6ddffee9ec053e87d24988fd

--- id/src/androidTest/java/com/simprints/id/testTemplates/FirstUse.kt
+++ id/src/androidTest/java/com/simprints/id/testTemplates/FirstUse.kt
@@ -9,6 +9,53 @@ import com.simprints.id.testTools.remote.RemoteTestingManager
 import io.realm.Realm
 import io.realm.RealmConfiguration
 
+/**
+ * Interface for tests where an empty project should be created server side, as well as clearing
+ * local data. The newly created project will appear in [testProject].
+ *
+ * A sample test class should look like this:
+ *
+ * ```
+ * @RunWith(AndroidJUnit4::class)
+ * class AndroidTestClass : DaggerForAndroidTests(), FirstUse {
+ *
+ *     override lateinit var testProject: TestProject
+ *     override lateinit var peopleRealmConfiguration
+ *
+ *     @Inject lateinit var randomGeneratorMock: RandomGenerator
+ *     @Inject lateinit var remoteDbManager: RemoteDbManager
+ *
+ *     override var module by lazyVar {
+ *         AppModuleForAndroidTests(app,
+ *             randomGeneratorRule = MockRule)
+ *     }
+ *
+ *     @Before
+ *     override fun setUp() {
+ *         app = InstrumentationRegistry.getTargetContext().applicationContext as Application
+ *         super<DaggerForAndroidTests>.setUp()
+ *         testAppComponent.inject(this)
+ *
+ *         setupRandomGeneratorToGenerateKey(DEFAULT_REALM_KEY, randomGeneratorMock)
+ *
+ *         app.initDependencies()
+ *
+ *         super<FirstUse>.setUp()
+ *
+ *         signOut()
+ *     }
+ *
+ *     @After
+ *     override fun tearDown() {
+ *         super.tearDown()
+ *     }
+ *
+ *     private fun signOut() [
+ *         remoteDbManager.signOutOfRemoteDb()
+ *     }
+ * }
+ * ```
+ */
 interface FirstUse : FirstUseLocal {
 
     var testProject: TestProject
