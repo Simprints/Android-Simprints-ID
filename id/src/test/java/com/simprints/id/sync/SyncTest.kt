81b6c573bcd288f9ae7275c6201993640735256e
Ridwan Farouki <ridwan@simprints.com>
2018-11-29 14:07:16 +0000

    Fixed module ID sync tests
    Former-commit-id: 8814f2da3bfd6af7d9001158367b8515d153d5fe

--- id/src/test/java/com/simprints/id/sync/SyncTest.kt
+++ id/src/test/java/com/simprints/id/sync/SyncTest.kt
@@ -2,8 +2,9 @@ package com.simprints.id.sync
 
 import com.google.firebase.FirebaseApp
 import com.google.gson.stream.JsonReader
-import com.simprints.id.data.analytics.eventData.controllers.domain.SessionEventsManager
 import com.nhaarman.mockito_kotlin.anyOrNull
+import com.nhaarman.mockito_kotlin.eq
+import com.simprints.id.data.analytics.eventData.controllers.domain.SessionEventsManager
 import com.simprints.id.data.db.DbManager
 import com.simprints.id.data.db.DbManagerImpl
 import com.simprints.id.data.db.local.LocalDbManager
@@ -129,7 +130,7 @@ class SyncTest : RxJavaTest, DaggerForTests() {
     }
 
     @Test
-    fun downloadPatientsForModuleSync_shouldSuccess() {
+    fun downloadPatientsForModuleSync_oneModule_shouldSuccess() {
         PeopleRemoteInterface.baseUrl = this.mockServer.url("/").toString()
         val localDbMock = Mockito.mock(LocalDbManager::class.java)
 
@@ -166,6 +167,55 @@ class SyncTest : RxJavaTest, DaggerForTests() {
         assertQueryUrlParam(peopleRequestUrl, "moduleId", moduleIdTest)
         assertQueryUrlParam(peopleRequestUrl, "lastKnownPatientUpdatedAt", peopleToDownload.last().updatedAt!!.time, { it?.toLong() })
         assertQueryUrlParam(peopleRequestUrl, "lastKnownPatientId", peopleToDownload.last().patientId)
+    }
+
+    @Test
+    fun downloadPatientsForModuleSync_multipleModules_shouldSuccess() {
+        PeopleRemoteInterface.baseUrl = this.mockServer.url("/").toString()
+        val localDbMock = Mockito.mock(LocalDbManager::class.java)
+
+        //Params
+        val projectIdTest = "projectIdTest"
+        val moduleIds = setOf("module1", "module2", "module3")
+        val syncParams = SyncTaskParameters.ModuleIdSyncTaskParameters(projectIdTest, moduleIds)
+        val nPeopleToDownloadForModule = listOf(22000, 26000, 17000)
+        val nPatientsAlreadyInLocalDb = listOf(3000, 500, 1700)
+        val nPeopleToDownloadForProjectIdFromServerForModule = nPeopleToDownloadForModule.zip(nPatientsAlreadyInLocalDb) { a, b -> a + b }
+        val peopleToDownload = nPeopleToDownloadForModule.map { nPeopleToDownload ->
+            getRandomPeople(nPeopleToDownload).map { fb_Person(it) }.sortedBy { it.updatedAt }
+        }
+
+        val testObserver = makeFakeDownloadRequestsForMultipleModules(
+            peopleToDownload,
+            nPeopleToDownloadForProjectIdFromServerForModule,
+            localDbMock,
+            moduleIds.toList(),
+            nPatientsAlreadyInLocalDb,
+            syncParams
+        )
+        testObserver.awaitTerminalEvent()
+
+        testObserver
+            .assertNoErrors()
+            .assertComplete()
+
+        Assert.assertTrue(testObserver.values().containsAll(arrayListOf(
+            DownloadProgress(SyncExecutor.DOWN_BATCH_SIZE_FOR_UPDATING_UI, nPeopleToDownloadForModule.sum()),
+            DownloadProgress(nPeopleToDownloadForModule.sum(), nPeopleToDownloadForModule.sum()))))
+
+        moduleIds.forEach { moduleId ->
+            val peopleCountRequestUrl = mockServer.takeRequest().requestUrl
+            assertPathUrlParam(peopleCountRequestUrl, projectIdTest)
+            assertQueryUrlParam(peopleCountRequestUrl, "moduleId", moduleId)
+        }
+
+        moduleIds.forEachIndexed { index, moduleId ->
+            val peopleRequestUrl = mockServer.takeRequest().requestUrl
+            assertPathUrlParam(peopleRequestUrl, projectIdTest)
+            assertQueryUrlParam(peopleRequestUrl, "moduleId", moduleId)
+            assertQueryUrlParam(peopleRequestUrl, "lastKnownPatientUpdatedAt", peopleToDownload[index].last().updatedAt!!.time) { it?.toLong() }
+            assertQueryUrlParam(peopleRequestUrl, "lastKnownPatientId", peopleToDownload[index].last().patientId)
+        }
     }
 
     @Test
@@ -233,6 +283,49 @@ class SyncTest : RxJavaTest, DaggerForTests() {
 
         //Mock app RealmSyncInfo for syncParams
         whenever(localDbMock.getSyncInfoFor(anyNotNull(), anyOrNull())).thenReturn(Single.create { it.onSuccess(rl_SyncInfo(syncParams.toGroup(), rl_Person(peopleToDownload.last()), null)) })
+
+        // Mock when trying to save the syncInfo
+        whenever(localDbMock.updateSyncInfo(anyNotNull())).thenReturn(Completable.complete())
+
+        val sync = SyncExecutorMock(DbManagerImpl(localDbMock, remoteDbManagerSpy, secureDataManager, loginInfoManager, preferencesManager, sessionEventsManager, timeHelper, peopleUpSyncMaster), JsonHelper.gson)
+
+        return sync.downloadNewPatients({ false }, syncParams).test()
+    }
+
+    private fun makeFakeDownloadRequestsForMultipleModules(
+        peopleToDownloadPerModule: List<List<fb_Person>>,
+        nPatientsForModuleIdFromServer: List<Int>,
+        localDbMock: LocalDbManager,
+        moduleIds: List<String>,
+        patientsAlreadyInLocalDbForModule: List<Int>,
+        syncParams: SyncTaskParameters
+    ): TestObserver<Progress> {
+
+        // Build fake respose for GET patients for each module
+        val patientsToDownloadForEachModule = peopleToDownloadPerModule.map { ArrayList(it) }
+
+        //Mock GET patients-count
+        nPatientsForModuleIdFromServer.forEach {
+            mockServer.enqueue(mockResponseForPatientsCount(it))
+        }
+
+        //Mock GET patients
+        patientsToDownloadForEachModule.forEach {
+            mockServer.enqueue(mockResponseForDownloadPatients(it))
+        }
+
+        //Mock saving patient in Realm
+        mockLocalDbToSavePatientsFromStream(localDbMock)
+
+        //Mock app has already patients in localDb
+        moduleIds.forEachIndexed { index, moduleId ->
+            whenever(localDbMock.getPeopleCountFromLocal(anyNotNull(), anyNotNull(), eq(moduleId), anyNotNull())).thenReturn(Single.just(patientsAlreadyInLocalDbForModule[index]))
+        }
+
+        //Mock app RealmSyncInfo for syncParams
+        moduleIds.forEachIndexed { index, moduleId ->
+            whenever(localDbMock.getSyncInfoFor(anyNotNull(), eq(moduleId))).thenReturn(Single.create { it.onSuccess(rl_SyncInfo(syncParams.toGroup(), rl_Person(peopleToDownloadPerModule[index].last()), null)) })
+        }
 
         // Mock when trying to save the syncInfo
         whenever(localDbMock.updateSyncInfo(anyNotNull())).thenReturn(Completable.complete())
