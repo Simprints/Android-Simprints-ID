b9bb3eeeab8335163acab4e91a314d36f6de8ee9
Fabio Tuzza <fabiotuzza@MacBook-Pro-di-Fabio-4.local>
2018-11-30 11:04:18 +0000

    Added skeleton for workers
    Former-commit-id: b3fffdadfd4eed0c18674c6e580ed076b736d976

--- id/src/main/java/com/simprints/id/services/scheduledSync/peopleDownSync/newplan/workers/understudy/CountMergerWorker.kt
+++ id/src/main/java/com/simprints/id/services/scheduledSync/peopleDownSync/newplan/workers/understudy/CountMergerWorker.kt
@@ -1,21 +1,17 @@
 package com.simprints.id.services.scheduledSync.peopleDownSync.newplan.workers.understudy
 
-import android.util.Log
 import androidx.work.Worker
-import java.util.*
 
 /**
  * Tris - Worker to fetch counter for (p, u, m) using CountTask.
  * Invocated by CountWorker
  */
-class SubCountWorker : Worker() {
+class CountMergerWorker : Worker() {
 
     companion object {
-        const val SUBCOUNT_WORKER_TAG = "SUBCOUNT_WORKER_TAG"
+        const val COUNT_MERGER_WORKER_TAG = "COUNT_MERGER_WORKER_TAG"
     }
     override fun doWork(): Result {
-        Log.d("WM", "Running SubCountWorker with $inputData")
-        Thread.sleep(Random().nextInt(2) * 1000L)
-        return Result.SUCCESS
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
--- id/src/main/java/com/simprints/id/services/scheduledSync/peopleDownSync/newplan/workers/understudy/SubDownSyncWorker.kt
+++ id/src/main/java/com/simprints/id/services/scheduledSync/peopleDownSync/newplan/workers/understudy/SubDownSyncWorker.kt
@@ -1,9 +1,20 @@
 package com.simprints.id.services.scheduledSync.peopleDownSync.newplan.workers.understudy
 
+import android.util.Log
 import androidx.work.Worker
+import java.util.*
 
 /**
  * Tris - Worker to execute sync for (p, u, m) using DownCountTask.
  * Invocated by DownSyncWorker
  */
-class SubDownSyncWorker : Worker()
+class SubDownSyncWorker : Worker() {
+    companion object {
+        const val SUBDOWNSYNC_WORKER_TAG = "SUBDOWNSYNC_WORKER_TAG"
+    }
+    override fun doWork(): Result {
+        Log.d("WM", "Running SubDownSyncWorker with $inputData")
+        Thread.sleep(Random().nextInt(2) * 1000L)
+        return Result.SUCCESS
+    }
+}
