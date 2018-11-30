2dec7e18c0295782abe9495734a591d0b16afe82
Aashay Gaikwad <aashay@simprints.com>
2018-11-30 10:14:47 +0000

    Defined DownSync and UpSync db and dao
    Former-commit-id: 001c7536a02c45f904bf925cd6491ac4eb02a41d

---
+++ id/src/main/java/com/simprints/id/services/scheduledSync/peopleDownSync/newplan/room/NewSyncStatusDatabase.kt
@@ -0,0 +1,23 @@
+package com.simprints.id.services.scheduledSync.peopleDownSync.newplan.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [DownSyncStatus::class, UpSyncStatus::class], version = 1, exportSchema = false)
+abstract class NewSyncStatusDatabase : RoomDatabase() {
+
+    abstract val downSyncStatusModel: DownSyncDao
+
+    abstract val upSyncStatusModel: UpSyncDao
+
+    companion object {
+        private const val SYNC_STATUS_DB_NAME = "sync_status_db"
+
+        fun getDatabase(context: Context): NewSyncStatusDatabase = Room
+            .databaseBuilder(context.applicationContext, NewSyncStatusDatabase::class.java, SYNC_STATUS_DB_NAME)
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+}
--- id/src/main/java/com/simprints/id/services/scheduledSync/peopleDownSync/newplan/room/UpSyncStatus.kt
+++ id/src/main/java/com/simprints/id/services/scheduledSync/peopleDownSync/newplan/room/UpSyncStatus.kt
@@ -1,6 +1,18 @@
 package com.simprints.id.services.scheduledSync.peopleDownSync.newplan.room
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 /**
  * Aashay - UpSyncStatus: room entity
  *  1) LastSyncTime: Long?
  */
+@Entity(tableName = "UpSyncStatus")
+data class UpSyncStatus(
+    @PrimaryKey val upSyncStatusId: String = UP_SYNC_STATUS_CONST_ID,
+    val lastUpSyncTime: Long? = null
+) {
+    companion object {
+        const val UP_SYNC_STATUS_CONST_ID = "up_sync_status_const_id"
+    }
+}
