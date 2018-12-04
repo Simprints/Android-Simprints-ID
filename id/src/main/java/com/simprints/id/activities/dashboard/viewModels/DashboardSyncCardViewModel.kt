d2e0fb3e47f6aca013a4a931f6cc51c7cd1825ab
Unknown <fabio@simprints.com>
2018-12-04 14:03:28 +0000

    Commit WM work for dashboard cards
    Former-commit-id: 7d888e83648c2edc945ff4dfca23a7d34f2a6c9b

--- id/src/main/java/com/simprints/id/activities/dashboard/viewModels/DashboardSyncCardViewModel.kt
+++ id/src/main/java/com/simprints/id/activities/dashboard/viewModels/DashboardSyncCardViewModel.kt
@@ -1,56 +1,38 @@
 package com.simprints.id.activities.dashboard.viewModels
 
+import android.os.Looper
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
 import androidx.work.WorkInfo
 import androidx.work.WorkInfo.State.RUNNING
 import androidx.work.WorkManager
-import com.simprints.id.data.db.DbManager
-import com.simprints.id.data.db.local.LocalDbManager
 import com.simprints.id.data.db.local.room.DownSyncStatus
 import com.simprints.id.data.db.local.room.UpSyncStatus
-import com.simprints.id.data.db.remote.RemoteDbManager
-import com.simprints.id.data.loginInfo.LoginInfoManager
-import com.simprints.id.data.prefs.PreferencesManager
 import com.simprints.id.di.AppComponent
 import com.simprints.id.services.scheduledSync.peopleDownSync.SyncStatusDatabase
-import com.simprints.id.services.scheduledSync.peopleDownSync.controllers.DownSyncManager
 import com.simprints.id.services.scheduledSync.peopleDownSync.controllers.SyncScopesBuilder
 import com.simprints.id.services.scheduledSync.peopleDownSync.models.SyncScope
 import com.simprints.id.services.scheduledSync.peopleDownSync.workers.ConstantsWorkManager.Companion.SYNC_WORKER_TAG
-import io.reactivex.Completable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.rxkotlin.subscribeBy
-import io.reactivex.schedulers.Schedulers
-import java.text.DateFormat
-import java.util.*
 import javax.inject.Inject
-import kotlin.collections.ArrayList
 
 class DashboardSyncCardViewModel(override val type: DashboardCardType,
                                  override val position: Int,
-                                 private val lifecycleOwner: LifecycleOwner,
+                                 val lifecycleOwner: LifecycleOwner,
                                  val component: AppComponent,
-                                 defaultState: State = State()): CardViewModel(type, position) {
+                                 defaultState: State = State()) : CardViewModel(type, position) {
 
-    @Inject lateinit var preferencesManager: PreferencesManager
-    @Inject lateinit var loginInfoManager: LoginInfoManager
-    @Inject lateinit var dbManager: DbManager
-    @Inject lateinit var remoteDbManager: RemoteDbManager
-    @Inject lateinit var localDbManager: LocalDbManager
     @Inject lateinit var syncStatusDatabase: SyncStatusDatabase
-    @Inject lateinit var syncScopesBuilder: SyncScopesBuilder
-    @Inject lateinit var downSyncManager: DownSyncManager
+    @Inject
+    lateinit var syncScopesBuilder: SyncScopesBuilder
+    private val syncScope: SyncScope?
+        get() = syncScopesBuilder.buildSyncScope()
+
+    private val helper = DashboardSyncCardViewModelHelper(this)
 
     val stateLiveData: MutableLiveData<State> = MutableLiveData()
     var viewModelState: State = State()
-        set(value) {
-            field = value
-            stateLiveData.value = value
-        }
 
     init {
         viewModelState = defaultState
@@ -60,19 +42,8 @@ class DashboardSyncCardViewModel(override val type: DashboardCardType,
                      var peopleToUpload: Int = 0,
                      var peopleToDownload: Int = 0,
                      var peopleInDb: Int = 0,
-                     var isSyncRunning: Boolean = false,
+                     var isDownSyncRunning: Boolean? = null,
                      var lastSyncTime: String = "")
-
-
-    private val syncScope: SyncScope?
-        get() = syncScopesBuilder.buildSyncScope()
-
-    private val dateFormat: DateFormat by lazy {
-        DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.SHORT, Locale.getDefault())
-    }
-
-    private var latestDownSyncTime: Long? = null
-    private var latestUpSyncTime: Long? = null
 
     private var downSyncStatus: LiveData<List<DownSyncStatus>>
     private var upSyncStatus: LiveData<UpSyncStatus?>
@@ -85,133 +56,60 @@ class DashboardSyncCardViewModel(override val type: DashboardCardType,
         upSyncStatus = syncStatusDatabase.upSyncDao.getUpSyncStatus()
         downSyncWorkerStatus = syncScope?.let { WorkManager.getInstance().getWorkInfosByTagLiveData(SYNC_WORKER_TAG) }
 
-        initViewModel()
+        helper.initViewModel(component)
     }
 
-    private fun initViewModel() {
-        val isSyncRunning = downSyncManager.isDownSyncRunning()
-        if (isSyncRunning) {
-            registerObservers()
-            updateSyncInfo()
-        } else {
-            fetchDownSyncCounterAndThenRegisterObservers()
+    fun registerObserverForDownSyncWorkerState() {
+        val downSyncObserver = Observer<List<WorkInfo>> { subWorkersStatusInSyncChain ->
+            val isAnyOfWorkersInSyncChainRunning = subWorkersStatusInSyncChain.firstOrNull { it.state == RUNNING } != null
+            helper.onDownSyncRunningChangeState(isAnyOfWorkersInSyncChainRunning)
         }
+        downSyncWorkerStatus?.observe(lifecycleOwner, downSyncObserver)
     }
 
-    private fun fetchDownSyncCounterAndThenRegisterObservers() {
-        updateTotalPeopleToDownSyncCount()
-            .andThen(updateTotalLocalPeopleCount())
-            .andThen(updateLocalPeopleToUpSyncCount())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribeOn(Schedulers.io())
-            .subscribeBy(onComplete = {
-                registerObservers()
-            }, onError = {
-                registerObservers()
-                it.printStackTrace()
-            })
-    }
-
-    private fun registerObservers() {
-        observeAndUpdatePeopleToDownloadAndLatestDownSyncTimes()
-        observeAndUpdateLatestUpSyncTime()
-        observeDownSyncStatusAndUpdateButton()
-    }
-
-    private fun updateTotalPeopleToDownSyncCount(): Completable =
-        Completable.fromAction {
-            var peopleToDownload = 0
-            syncScope?.toSubSyncScopes()?.forEach { it ->
-                peopleToDownload += dbManager.calculateNPatientsToDownSync(it.projectId, it.userId, it.moduleId).blockingGet()
-            }
-            viewModelState = viewModelState.copy(peopleToDownload = peopleToDownload)
-        }
-
-    private fun updateTotalLocalPeopleCount(): Completable =
-        dbManager.getPeopleCountFromLocalForSyncGroup(preferencesManager.syncGroup)
-            .flatMapCompletable {
-                viewModelState = viewModelState.copy(peopleInDb = it)
-                Completable.complete()
-            }
-
-    private fun updateLocalPeopleToUpSyncCount(): Completable =
-        localDbManager.getPeopleCountFromLocal(toSync = true)
-            .flatMapCompletable {
-                viewModelState = viewModelState.copy(peopleToUpload = it)
-                Completable.complete()
-            }
-
-    private fun observeAndUpdatePeopleToDownloadAndLatestDownSyncTimes() {
+    fun registerObserverForDownSyncEvents() {
 
         val downSyncStatusObserver = Observer<List<DownSyncStatus>> { downSyncStatuses ->
-            if (downSyncStatuses.isNotEmpty()) {
-                var peopleToDownSync = 0
-                val latestDownSyncTimeForModules: ArrayList<Long> = ArrayList()
-                downSyncStatuses.forEach {
-                    peopleToDownSync += it.totalToDownload
-                    it.lastSyncTime?.let { lastSyncTime -> latestDownSyncTimeForModules.add(lastSyncTime) }
-                }
-                if (viewModelState.isSyncRunning) {
-                    viewModelState = viewModelState.copy(peopleToDownload = peopleToDownSync)
-                }
-                latestDownSyncTime = latestDownSyncTimeForModules.max()
-                val lastSyncTime = calculateLatestSyncTimeIfPossible(latestDownSyncTime, latestUpSyncTime)
-                viewModelState = viewModelState.copy(lastSyncTime = lastSyncTime)
-            }
+            helper.onDownSyncStatusChanged(downSyncStatuses)
         }
         downSyncStatus.observe(lifecycleOwner, downSyncStatusObserver)
     }
 
-    private fun observeAndUpdateLatestUpSyncTime() {
+    fun registerObserverForUpSyncEvents() {
 
-        val upSyncObserver = Observer<UpSyncStatus?> {
-            latestUpSyncTime = it?.lastUpSyncTime
-            val lastSyncTime = calculateLatestSyncTimeIfPossible(latestDownSyncTime, latestUpSyncTime)
-            viewModelState = viewModelState.copy(lastSyncTime = lastSyncTime)
-
+        val upSyncObserver = Observer<UpSyncStatus?> { upSyncStatus ->
+            helper.onUpSyncStatusChanged(upSyncStatus)
         }
         upSyncStatus.observe(lifecycleOwner, upSyncObserver)
     }
 
-    private fun calculateLatestSyncTimeIfPossible(lastDownSyncTime: Long?, lastUpSyncTime: Long?): String {
-        val lastDownSyncDate = lastDownSyncTime?.let { Date(it) }
-        val lastUpSyncDate = lastUpSyncTime?.let { Date(it) }
+    fun updateState(peopleToUpload: Int? = null,
+                    peopleToDownload: Int? = null,
+                    peopleInDb: Int? = null,
+                    isDownSyncRunning: Boolean? = null,
+                    lastSyncTime: String? = null,
+                    emitState: Boolean = false) {
 
-        if (lastDownSyncDate != null && lastUpSyncDate != null) {
-            return if (lastDownSyncDate.after(lastUpSyncDate)) {
-                lastDownSyncDate.toString()
-            } else {
-                lastUpSyncDate.toString()
-            }
+        viewModelState = viewModelState.let {
+            it.copy(
+                peopleToUpload = peopleToUpload ?: it.peopleToUpload,
+                peopleToDownload = peopleToDownload ?: it.peopleToDownload,
+                peopleInDb = peopleInDb ?: it.peopleInDb,
+                isDownSyncRunning = isDownSyncRunning ?: it.isDownSyncRunning,
+                lastSyncTime = lastSyncTime ?: it.lastSyncTime
+            )
         }
 
-        lastDownSyncDate?.let { return dateFormat.format(it) }
-        lastUpSyncDate?.let { return dateFormat.format(it) }
-
-        return ""
-    }
-
-    private fun updateSyncInfo() {
-        updateTotalLocalPeopleCount()
-            .andThen(updateLocalPeopleToUpSyncCount())
-            .subscribeBy(onComplete = {
-            }, onError = {
-                it.printStackTrace()
-            })
-    }
-
-    private fun observeDownSyncStatusAndUpdateButton() {
-
-        val downSyncObserver = Observer<List<WorkInfo>> { subWorkersStatusInSyncChain ->
-            val isAnyOfWorkersInSyncChainRunning = subWorkersStatusInSyncChain.firstOrNull { it.state == RUNNING } != null
-
-            if (viewModelState.isSyncRunning != isAnyOfWorkersInSyncChainRunning) {
-                viewModelState = viewModelState.copy(isSyncRunning = isAnyOfWorkersInSyncChainRunning)
-                if (!viewModelState.isSyncRunning) {
-                    updateSyncInfo()
-                }
-            }
+        if (emitState) {
+            emitState()
         }
-        downSyncWorkerStatus?.observe(lifecycleOwner, downSyncObserver)
+    }
+
+    private fun emitState() {
+        if (Looper.myLooper() == Looper.getMainLooper()) {
+            stateLiveData.value = viewModelState
+        } else {
+            stateLiveData.postValue(viewModelState)
+        }
     }
 }
