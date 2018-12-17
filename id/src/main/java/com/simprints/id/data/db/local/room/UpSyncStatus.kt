802685644934e7ceae05acc102d99c03c8477947
Aashay Gaikwad <aashay@simprints.com>
2018-12-17 10:33:51 +0000

    Removed comments
    Former-commit-id: 8ec1510815575bab66d61ec1527042ef6cf14cba

--- id/src/main/java/com/simprints/id/data/db/local/room/UpSyncStatus.kt
+++ id/src/main/java/com/simprints/id/data/db/local/room/UpSyncStatus.kt
@@ -3,10 +3,6 @@ package com.simprints.id.data.db.local.room
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-/**
- * Aashay - UpSyncStatus: room entity
- *  1) LastSyncTime: Long?
- */
 @Entity(tableName = "UpSyncStatus")
 data class UpSyncStatus(
     @PrimaryKey val upSyncStatusId: String = UP_SYNC_STATUS_CONST_ID,
