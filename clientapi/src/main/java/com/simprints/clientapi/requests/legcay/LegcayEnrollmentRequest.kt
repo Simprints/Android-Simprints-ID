5743a45b5138dc87ae70ddf20d7d356f93be41ab
Tristram Norman <tristram@simprints.com>
2018-12-23 16:02:59 +0000

    add legacy
    Former-commit-id: a66c0ccc538cef3f66c7e2ac4b8d5217e2e5afe2

--- clientapi/src/main/java/com/simprints/clientapi/requests/legcay/LegcayEnrollmentRequest.kt
+++ clientapi/src/main/java/com/simprints/clientapi/requests/legcay/LegcayEnrollmentRequest.kt
@@ -1,7 +1,45 @@
-package com.simprints.clientapi.requests
+package com.simprints.clientapi.requests.legcay
 
-data class LegcayEnrollmentRequest(val projectId: String,
+import android.os.Parcel
+import android.os.Parcelable
+import com.simprints.clientapi.requests.SimprintsIdRequest
+
+data class LegcayEnrollmentRequest(val apiKey: String,
                                    val moduleId: String,
                                    val userId: String,
                                    val callingPackage: String,
-                                   val metadata: String)
+                                   val metadata: String) : Parcelable, SimprintsIdRequest {
+
+    override val requestName: String = REQUEST_NAME
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "")
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(apiKey)
+        parcel.writeString(moduleId)
+        parcel.writeString(userId)
+        parcel.writeString(callingPackage)
+        parcel.writeString(metadata)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<LegcayEnrollmentRequest> {
+        private const val REQUEST_NAME = "legacyEnrollmentRequest"
+
+        override fun createFromParcel(parcel: Parcel): LegcayEnrollmentRequest {
+            return LegcayEnrollmentRequest(parcel)
+        }
+
+        override fun newArray(size: Int): Array<LegcayEnrollmentRequest?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
