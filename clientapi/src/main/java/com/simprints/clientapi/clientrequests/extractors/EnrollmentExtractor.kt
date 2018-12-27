cfb5153f5d07ec9017064ca4b3366e22f01ab244
Tristram Norman <tristram@simprints.com>
2018-12-27 12:11:27 +0000

    Add extractor for intent.
    Former-commit-id: 28d746a57be58d00b668d8017dae52e0d5692aab

---
+++ clientapi/src/main/java/com/simprints/clientapi/clientrequests/extractors/EnrollmentExtractor.kt
@@ -0,0 +1,6 @@
+package com.simprints.clientapi.clientrequests.extractors
+
+import android.content.Intent
+
+
+class EnrollmentExtractor(intent: Intent) : ClientRequestExtractor(intent)
---
+++ clientapi/src/main/java/com/simprints/clientapi/clientrequests/validators/ClientRequestValidator.kt
@@ -0,0 +1,32 @@
+package com.simprints.clientapi.clientrequests.validators
+
+import com.google.gson.Gson
+import com.simprints.clientapi.clientrequests.extractors.ClientRequestExtractor
+
+
+abstract class ClientRequestValidator(private val extractor: ClientRequestExtractor) {
+
+    abstract fun validateClientRequest()
+
+    fun hasValidProjectId(): Boolean = !extractor.getProjectId().isNullOrBlank()
+
+    fun hasValidModuleId(): Boolean = !extractor.getModuleId().isNullOrBlank()
+
+    fun hasValidUserId(): Boolean = !extractor.getUserId().isNullOrBlank()
+
+    // TODO: remove legacy
+    fun hasValidApiKey(): Boolean = !extractor.getLegacyApiKey().isNullOrBlank()
+
+    fun hasMetadata(): Boolean = !extractor.getMetatdata().isNullOrBlank()
+
+    // TODO: inject gson dependency
+    fun hasValidMetadata(): Boolean = try {
+        Gson().fromJson(extractor.getMetatdata(), Any::class.java)
+        true
+    } catch (ex: com.google.gson.JsonSyntaxException) {
+        false
+    }
+
+}
+
+
