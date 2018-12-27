10eee1c6e5fb34edba8288ca96b02820f3045468
Tristram Norman <tristram@simprints.com>
2018-12-27 13:17:41 +0000

    add api version and make client request abstract
    Former-commit-id: 2d14d9f5db4146a41f71f55542273c8c7419b040

---
+++ clientapi/src/main/java/com/simprints/clientapi/clientrequests/requests/ApiVersion.kt
@@ -0,0 +1,6 @@
+package com.simprints.clientapi.clientrequests.requests
+
+enum class ApiVersion {
+    V1,
+    V2
+}
--- clientapi/src/main/java/com/simprints/clientapi/clientrequests/validators/EnrollmentValidator.kt
+++ clientapi/src/main/java/com/simprints/clientapi/clientrequests/validators/EnrollmentValidator.kt
@@ -1,14 +1,15 @@
 package com.simprints.clientapi.clientrequests.validators
 
 import com.simprints.clientapi.clientrequests.extractors.EnrollmentExtractor
+import com.simprints.clientapi.clientrequests.requests.ClientEnrollmentRequest
 import com.simprints.clientapi.exceptions.InvalidMetadataException
 import com.simprints.clientapi.exceptions.InvalidModuleIdException
 import com.simprints.clientapi.exceptions.InvalidProjectIdException
 import com.simprints.clientapi.exceptions.InvalidUserIdException
 
-class EnrollmentValidator(extractor: EnrollmentExtractor) : ClientRequestValidator(extractor) {
+class EnrollmentValidator(private val extractor: EnrollmentExtractor) : ClientRequestValidator(extractor) {
 
-    override fun validateClientRequest() {
+    override fun validateClientRequest(): ClientEnrollmentRequest {
         if (!hasValidProjectId() || !hasValidApiKey())
             throw InvalidProjectIdException()
         else if (!hasValidUserId())
@@ -18,6 +19,14 @@ class EnrollmentValidator(extractor: EnrollmentExtractor) : ClientRequestValidat
         else if (hasMetadata())
             if (!hasValidMetadata())
                 throw InvalidMetadataException()
+
+        return ClientEnrollmentRequest(
+            projectId = extractor.getProjectId(),
+            apiKey = extractor.getLegacyApiKey(),
+            moduleId = extractor.getModuleId(),
+            userId = extractor.getUserId(),
+            metadata = extractor.getMetatdata()
+        )
     }
 
 }
