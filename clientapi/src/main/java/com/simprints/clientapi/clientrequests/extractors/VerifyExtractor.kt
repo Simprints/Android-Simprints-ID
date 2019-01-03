97188a1ac5cf15d20fd4b97787d3731ae374c6ea
Tristram Norman <tristram@simprints.com>
2019-01-03 10:39:19 +0000

    add verify extractor, validator + builder
    Former-commit-id: 3431d09aacffd1af9f0184b8978444a7b4b3ed73

---
+++ clientapi/src/main/java/com/simprints/clientapi/clientrequests/extractors/VerifyExtractor.kt
@@ -0,0 +1,11 @@
+package com.simprints.clientapi.clientrequests.extractors
+
+import android.content.Intent
+import com.simprints.libsimprints.Constants
+
+
+class VerifyExtractor(val intent: Intent) : ClientRequestExtractor(intent) {
+
+    fun getVerifyGuid(): String? = intent.getStringExtra(Constants.SIMPRINTS_VERIFY_GUID)
+
+}
---
+++ clientapi/src/main/java/com/simprints/clientapi/exceptions/InvalidVerifyIdException.kt
@@ -0,0 +1,4 @@
+package com.simprints.clientapi.exceptions
+
+
+class InvalidVerifyIdException(message: String = "") : Exception(message)
