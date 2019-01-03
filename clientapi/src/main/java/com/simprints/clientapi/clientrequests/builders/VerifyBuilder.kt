171c45b37129432d33f08ea6067a9608089bf657
Tristram Norman <tristram@simprints.com>
2019-01-03 10:39:19 +0000

    add verify extractor, validator + builder
    Former-commit-id: 30cac2f34903c026ae96a9bb8ab716b55ca29074

---
+++ clientapi/src/main/java/com/simprints/clientapi/clientrequests/builders/VerifyBuilder.kt
@@ -0,0 +1,29 @@
+package com.simprints.clientapi.clientrequests.builders
+
+import com.simprints.clientapi.clientrequests.extractors.VerifyExtractor
+import com.simprints.clientapi.clientrequests.requests.ClientRequest
+import com.simprints.clientapi.clientrequests.requests.ClientVerifyRequest
+import com.simprints.clientapi.clientrequests.requests.legacy.LegacyClientVerifyRequest
+import com.simprints.clientapi.clientrequests.validators.VerifyValidator
+
+
+class VerifyBuilder(private val extractor: VerifyExtractor, validator: VerifyValidator)
+    : ClientRequestBuilder(extractor, validator) {
+
+    override fun buildClientRequest(): ClientRequest = ClientVerifyRequest(
+        projectId = extractor.getProjectId()!!,
+        userId = extractor.getUserId()!!,
+        moduleId = extractor.getModuleId()!!,
+        metadata = extractor.getMetatdata(),
+        verifyGuid = extractor.getVerifyGuid()!!
+    )
+
+    override fun buildLegacyClientRequest(): ClientRequest = LegacyClientVerifyRequest(
+        apiKey = extractor.getLegacyApiKey()!!,
+        userId = extractor.getUserId()!!,
+        moduleId = extractor.getModuleId()!!,
+        metadata = extractor.getMetatdata(),
+        verifyGuid = extractor.getVerifyGuid()!!
+    )
+
+}
