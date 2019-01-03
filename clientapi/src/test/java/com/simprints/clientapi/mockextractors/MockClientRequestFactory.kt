8d70c62f6768c9925185359f8981b3c679d54726
Tristram Norman <tristram@simprints.com>
2019-01-03 12:40:59 +0000

    create verify tests
    Former-commit-id: 30725f1ad2bb748e27b709738ba0b6873b27efca

---
+++ clientapi/src/test/java/com/simprints/clientapi/mockextractors/MockClientRequestFactory.kt
@@ -0,0 +1,28 @@
+package com.simprints.clientapi.mockextractors
+
+import com.simprints.clientapi.clientrequests.extractors.ClientRequestExtractor
+import com.simprints.clientapi.clientrequests.validators.ClientRequestValidator
+import org.mockito.Mockito
+
+abstract class MockClientRequestFactory {
+
+    companion object {
+        const val MOCK_PROJECT_ID = "projectId"
+        const val MOCK_USER_ID = "userId"
+        const val MOCK_MODULE_ID = "moduleId"
+        const val MOCK_METADATA = ""
+        const val MOCK_VERIFY_GUID = "1d3a92c1-3410-40fb-9e88-4570c9abd150"
+    }
+
+    abstract fun getValidator(extractor: ClientRequestExtractor): ClientRequestValidator
+
+    abstract fun getValidMockExtractor(): ClientRequestExtractor
+
+    protected fun setMockDefaultExtractor(mockExtractor: ClientRequestExtractor) {
+        Mockito.`when`(mockExtractor.getProjectId()).thenReturn(MOCK_PROJECT_ID)
+        Mockito.`when`(mockExtractor.getUserId()).thenReturn(MOCK_USER_ID)
+        Mockito.`when`(mockExtractor.getModuleId()).thenReturn(MOCK_MODULE_ID)
+        Mockito.`when`(mockExtractor.getMetatdata()).thenReturn(MOCK_METADATA)
+    }
+
+}
