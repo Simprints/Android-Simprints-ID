cb7a307fc6cb9df4565db12fa1bf099b5d14ed96
Tristram Norman <tristram@simprints.com>
2019-01-05 17:05:58 +0000

    extend legacy api interfaces
    Former-commit-id: 0f44e10224cfea1e39c2fda0315071b95e6efef2

--- clientapi/src/main/java/com/simprints/clientapi/simprintsrequests/legacy/LegacySimprintsIdRequests.kt
+++ clientapi/src/main/java/com/simprints/clientapi/simprintsrequests/legacy/LegacySimprintsIdRequests.kt
@@ -6,12 +6,19 @@ import com.simprints.clientapi.simprintsrequests.SimprintsIdRequest
 
 interface LegacySimprintsIdRequest : SimprintsIdRequest {
 
-    override val apiVersion: ApiVersion
-        get() = ApiVersion.V1
-
-    override val projectId: String
-        get() = ""
-
+    override val apiVersion: ApiVersion get() = ApiVersion.V1
+    override val projectId: String get() = ""
     val legacyApiKey: String
 
 }
+
+interface LegacySimprintsActionRequest : LegacySimprintsIdRequest {
+
+    val userId: String
+    val moduleId: String
+    val metadata: String
+
+}
+
+interface SimprintsConfirmationRequest : LegacySimprintsIdRequest
+
