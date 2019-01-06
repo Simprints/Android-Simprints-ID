30f310a4a9de1c2ed8162c39dd0e849dd253a668
Tristram Norman <tristram@simprints.com>
2019-01-06 14:23:13 +0000

    add libsimprints flow
    Former-commit-id: bed0543b534535e5a9dff930164a307be838533a

--- clientapi/src/main/java/com/simprints/clientapi/activities/odk/OdkPresenter.kt
+++ clientapi/src/main/java/com/simprints/clientapi/activities/odk/OdkPresenter.kt
@@ -1,10 +1,6 @@
 package com.simprints.clientapi.activities.odk
 
-import com.simprints.clientapi.clientrequests.builders.*
-import com.simprints.clientapi.clientrequests.validators.ConfirmIdentifyValidator
-import com.simprints.clientapi.clientrequests.validators.EnrollValidator
-import com.simprints.clientapi.clientrequests.validators.IdentifyValidator
-import com.simprints.clientapi.clientrequests.validators.VerifyValidator
+import com.simprints.clientapi.activities.baserequest.RequestPresenter
 import com.simprints.clientapi.extensions.getConfidencesString
 import com.simprints.clientapi.extensions.getIdsString
 import com.simprints.clientapi.extensions.getTiersString
@@ -13,8 +9,8 @@ import com.simprints.libsimprints.Registration
 import com.simprints.libsimprints.Verification
 
 
-class OdkPresenter(private val view: OdkContract.View,
-                   private val action: String?) : OdkContract.Presenter {
+class OdkPresenter(val view: OdkContract.View,
+                   private val action: String?) : RequestPresenter(view), OdkContract.Presenter {
 
     companion object {
         private const val PACKAGE_NAME = "com.simprints.simodkadapter"
@@ -25,23 +21,23 @@ class OdkPresenter(private val view: OdkContract.View,
     }
 
     override fun start() = when (action) {
-        ACTION_REGISTER -> handleEnrollmentRequest()
-        ACTION_IDENTIFY -> handleIdentifyRequest()
-        ACTION_VERIFY -> handleVerifyRequest()
-        ACTION_CONFIRM_IDENTITY -> handleConfirmIdentifyRequest()
+        ACTION_REGISTER -> processEnrollRequest()
+        ACTION_IDENTIFY -> processIdentifyRequest()
+        ACTION_VERIFY -> processVerifyRequest()
+        ACTION_CONFIRM_IDENTITY -> processConfirmIdentifyRequest()
         else -> view.returnIntentActionErrorToClient()
     }
 
     override fun processRegistration(registration: Registration) =
         view.returnRegistration(registration.guid)
 
-    override fun processIdentification(identifications: ArrayList<Identification>, sessionId: String) =
-        view.returnIdentification(
-            identifications.getIdsString(),
-            identifications.getConfidencesString(),
-            identifications.getTiersString(),
-            sessionId
-        )
+    override fun processIdentification(identifications: ArrayList<Identification>,
+                                       sessionId: String) = view.returnIdentification(
+        identifications.getIdsString(),
+        identifications.getConfidencesString(),
+        identifications.getTiersString(),
+        sessionId
+    )
 
     override fun processVerification(verification: Verification) = view.returnVerification(
         verification.guid,
@@ -50,29 +46,5 @@ class OdkPresenter(private val view: OdkContract.View,
     )
 
     override fun processReturnError() = view.returnIntentActionErrorToClient()
-
-    private fun handleEnrollmentRequest() = validateAndSendRequest(
-        EnrollBuilder(view.enrollExtractor, EnrollValidator(view.enrollExtractor))
-    )
-
-    private fun handleVerifyRequest() = validateAndSendRequest(
-        VerifyBuilder(view.verifyExtractor, VerifyValidator(view.verifyExtractor))
-    )
-
-    private fun handleIdentifyRequest() = validateAndSendRequest(
-        IdentifyBuilder(view.identifyExtractor, IdentifyValidator(view.identifyExtractor))
-    )
-
-    private fun handleConfirmIdentifyRequest() = validateAndSendRequest(
-        ConfirmIdentifyBuilder(view.confirmIdentifyExtractor,
-            ConfirmIdentifyValidator(view.confirmIdentifyExtractor))
-    )
-
-    private fun validateAndSendRequest(builder: ClientRequestBuilder) = try {
-        val request = builder.build()
-        view.sendSimprintsRequest(request)
-    } catch (exception: Exception) {
-        view.handleClientRequestError(exception)
-    }
 
 }
