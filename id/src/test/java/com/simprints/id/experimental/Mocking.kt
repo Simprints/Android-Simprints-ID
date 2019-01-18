f1731d21a0e75f4fe8ce1ff0a65d68884d7ca85f
Ridwan Farouki <ridwan@simprints.com>
2019-01-18 15:54:17 +0000

    First working iteration of refactoring example
    Former-commit-id: f3195fa37f273d293352cff4fdd5f0eaf32059e2

---
+++ id/src/test/java/com/simprints/id/experimental/Mocking.kt
@@ -0,0 +1,42 @@
+package com.simprints.id.experimental
+
+import com.nhaarman.mockito_kotlin.atLeast
+import com.nhaarman.mockito_kotlin.atMost
+import com.nhaarman.mockito_kotlin.times
+import org.mockito.Mockito
+import org.mockito.stubbing.OngoingStubbing
+import org.mockito.verification.VerificationMode
+
+inline fun <reified T> mock(): T =
+    Mockito.mock(T::class.java)
+
+// Setup behaviour
+
+fun <T> whenever(methodCall: () -> T): OngoingStubbing<T> =
+    Mockito.`when`(methodCall())
+
+infix fun <T> OngoingStubbing<T>.thenReturn(value: T): OngoingStubbing<T> =
+    thenReturn(value)
+
+infix fun <T> OngoingStubbing<T>.thenThrow(e: Throwable): OngoingStubbing<T> =
+    thenThrow(e)
+
+// Assertion operations
+
+fun <T> verifyOnce(mock: T, methodCall: T.() -> Any?) =
+    verifyExactly(1, mock, methodCall)
+
+fun <T> verifyNever(mock: T, methodCall: T.() -> Any?) =
+    verifyExactly(0, mock, methodCall)
+
+fun <T> verifyExactly(times: Int, mock: T, methodCall: T.() -> Any?) =
+    verify(::times, times, mock, methodCall)
+
+fun <T> verifyAtLeast(times: Int, mock: T, methodCall: T.() -> Any?) =
+    verify(::atLeast, times, mock, methodCall)
+
+fun <T> verifyAtMost(times: Int, mock: T, methodCall: T.() -> Any?) =
+    verify(::atMost, times, mock, methodCall)
+
+private fun <T> verify(mode: (Int) -> VerificationMode, times: Int, mock: T, methodCall: T.() -> Any?) =
+    Mockito.verify(mock, mode(times)).methodCall()
---
+++ id/src/test/java/com/simprints/id/experimental/NewDaggerForTests.kt
@@ -0,0 +1,32 @@
+package com.simprints.id.experimental
+
+import com.simprints.id.di.AppComponentForTests
+import com.simprints.id.di.AppModuleForTests
+import com.simprints.id.di.DaggerAppComponentForTests
+import com.simprints.id.shared.PreferencesModuleForAnyTests
+import com.simprints.id.testUtils.roboletric.TestApplication
+import com.simprints.id.tools.delegates.lazyVar
+
+open class NewDaggerForTests {
+
+    lateinit var testAppComponent: AppComponentForTests
+    lateinit var app: TestApplication
+
+    open var module: AppModuleForTests by lazyVar {
+        AppModuleForTests(app)
+    }
+    open var preferencesModule by lazyVar {
+        PreferencesModuleForAnyTests()
+    }
+
+    fun initComponent() {
+
+        testAppComponent = DaggerAppComponentForTests
+            .builder()
+            .appModule(module)
+            .preferencesModule(preferencesModule)
+            .build()
+
+        app.component = testAppComponent
+    }
+}
