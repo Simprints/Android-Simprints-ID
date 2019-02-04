f04e9ce1e38791d363d1d99a44e9b4d89ef80d56
fabiotuzza <fabio@simprints.com>
2019-02-04 09:59:30 +0000

    Tidying SessionUploadFailureRetryException up a bit.
    Former-commit-id: 8e3400f1f71d7b18b7b5486d53e6c601aacdec85

--- id/src/main/java/com/simprints/id/exceptions/safe/session/SessionUploadFailureRetryException.kt
+++ id/src/main/java/com/simprints/id/exceptions/safe/session/SessionUploadFailureRetryException.kt
@@ -3,7 +3,4 @@ package com.simprints.id.exceptions.safe.session
 import com.simprints.id.exceptions.safe.SimprintsException
 
 
-open class SessionUploadFailureRetryException : SimprintsException {
-    constructor(): super("SessionUploadFailureRetryException")
-    constructor(cause: Throwable): super(cause)
-}
+open class SessionUploadFailureRetryException(cause: Throwable) : SimprintsException(cause)
