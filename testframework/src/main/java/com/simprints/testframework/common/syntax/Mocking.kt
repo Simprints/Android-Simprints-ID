9c60a0a4ed3e9819880be12f66b8dbec706f8bf9
Ridwan Farouki <ridwan@simprints.com>
2019-02-06 15:51:47 +0000

    Improved infixability of methods using subclass
    - Allows type to be inferred more strongly by kotlin compiler
    - Allows optional usage of infix without addition import
    
    Former-commit-id: c3f0a75f97f3c4ca56a858741f769ca7a38759da

--- testframework/src/main/java/com/simprints/testframework/common/syntax/Mocking.kt
+++ testframework/src/main/java/com/simprints/testframework/common/syntax/Mocking.kt
@@ -1,23 +1,43 @@
 package com.simprints.testframework.common.syntax
 
 import org.mockito.Mockito
+import org.mockito.stubbing.Answer
 import org.mockito.stubbing.OngoingStubbing
 
 inline fun <reified T> mock(): T =
     Mockito.mock(T::class.java)
 
-fun <T> whenever(methodCall: T): OngoingStubbing<T> =
-    Mockito.`when`(methodCall)
+fun <T> whenever(methodCall: T): InfixOngoingStubbing<T> =
+    InfixOngoingStubbing(Mockito.`when`(methodCall))
 
-fun <T> whenever(methodCall: () -> T): OngoingStubbing<T> =
-    Mockito.`when`(methodCall())
+fun <T> whenever(methodCall: () -> T): InfixOngoingStubbing<T> =
+    InfixOngoingStubbing(Mockito.`when`(methodCall()))
 
-infix fun <T> OngoingStubbing<T>.thenReturn(value: T): OngoingStubbing<T> =
-    thenReturn(value)
+/**
+ * This class lightly wraps [OngoingStubbing] to allow some methods to be infix
+ */
+class InfixOngoingStubbing<T>(private val ongoingStubbing: OngoingStubbing<T>)
+    : OngoingStubbing<T> by ongoingStubbing {
 
-infix fun <T> OngoingStubbing<T>.thenThrow(e: Throwable): OngoingStubbing<T> =
-    thenThrow(e)
+    override infix fun thenReturn(value: T) =
+        InfixOngoingStubbing(ongoingStubbing.thenReturn(value))
 
+    infix fun thenThrow(e: Throwable) =
+        InfixOngoingStubbing(ongoingStubbing.thenThrow(e))
+
+    override fun thenThrow(throwableType: Class<out Throwable>) =
+        InfixOngoingStubbing(ongoingStubbing.thenThrow(throwableType))
+
+    override fun then(answer: Answer<*>) =
+        InfixOngoingStubbing(ongoingStubbing.then(answer))
+
+    override fun thenAnswer(answer: Answer<*>?) =
+        InfixOngoingStubbing(ongoingStubbing.thenAnswer(answer))
+}
+
+/**
+ * A complement to [Mockito.any] that appropriately ensures non-null values
+ */
 fun <T> anyNotNull(): T {
     try {
         Mockito.any<T>()
@@ -28,4 +48,5 @@ fun <T> anyNotNull(): T {
     return uninitialized()
 }
 
+@Suppress("UNCHECKED_CAST")
 private fun <T> uninitialized(): T = null as T
