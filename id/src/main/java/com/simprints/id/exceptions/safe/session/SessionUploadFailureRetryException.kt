efe79433900d132bd978a219340e57d2ef801bb0
fabiotuzza <fabio@simprints.com>
2019-02-06 14:17:53 +0000

    Removed unnecessary open keywords
    Former-commit-id: 07aeb9fcf44062d7ff3748e3be1bd7a579d3db1f

--- id/src/main/java/com/simprints/id/exceptions/safe/session/SessionUploadFailureRetryException.kt
+++ id/src/main/java/com/simprints/id/exceptions/safe/session/SessionUploadFailureRetryException.kt
@@ -2,5 +2,4 @@ package com.simprints.id.exceptions.safe.session
 
 import com.simprints.id.exceptions.safe.SimprintsException
 
-
-open class SessionUploadFailureRetryException(cause: Throwable) : SimprintsException(cause)
+class SessionUploadFailureRetryException(cause: Throwable) : SimprintsException(cause)
