112bc9bae55ec82d41d5b17786029c01a3f8b23b
Aashay Gaikwad <aashay@simprints.com>
2019-02-19 12:40:28 +0000

    Exception changed to Error
    Former-commit-id: 082dc7fcde2f6dbc5c2080f7cbd8bbc5d5c2b70e

--- id/src/main/java/com/simprints/id/exceptions/SimprintsException.kt
+++ id/src/main/java/com/simprints/id/exceptions/SimprintsException.kt
@@ -1,7 +1,7 @@
 package com.simprints.id.exceptions
 
 
-open class SimprintsException : Exception {
+open class SimprintsException : Error {
     constructor(message: String, cause: Throwable): super(message, cause)
     constructor(message: String): super(message)
     constructor(cause: Throwable): super(cause)
