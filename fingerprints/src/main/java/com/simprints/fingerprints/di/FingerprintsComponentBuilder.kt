2cc98c425eef4d91894261f21ce9ba8591572b20
fabiotuzza <fabio@simprints.com>
2019-02-25 19:26:09 +0000

    Make MatchAct running in fingerprint module
    Former-commit-id: 651c8681e6de3970f52a34602503865b8f3d4075

--- fingerprints/src/main/java/com/simprints/fingerprints/di/FingerprintsComponentBuilder.kt
+++ fingerprints/src/main/java/com/simprints/fingerprints/di/FingerprintsComponentBuilder.kt
@@ -1,9 +1,21 @@
 package com.simprints.fingerprints.di
 
 import com.simprints.id.Application
+import com.simprints.id.data.analytics.eventData.controllers.domain.SessionEventsManager
+import com.simprints.id.data.db.DbManager
+import com.simprints.id.data.db.local.LocalDbManager
+import com.simprints.id.data.db.local.room.SyncStatusDatabase
+import com.simprints.id.data.db.remote.RemoteDbManager
+import com.simprints.id.data.db.remote.people.RemotePeopleManager
+import com.simprints.id.data.db.remote.project.RemoteProjectManager
+import com.simprints.id.data.loginInfo.LoginInfoManager
+import com.simprints.id.data.prefs.PreferencesManager
+import com.simprints.id.data.secure.SecureDataManager
 import com.simprints.id.di.AppModule
 import com.simprints.id.di.PreferencesModule
 import com.simprints.id.di.SerializerModule
+import com.simprints.id.services.scheduledSync.peopleUpsync.PeopleUpSyncMaster
+import com.simprints.id.tools.TimeHelper
 
 class FingerprintsComponentBuilder {
 
@@ -11,7 +23,11 @@ class FingerprintsComponentBuilder {
         @JvmStatic fun getComponent(app: Application): FingerprintsComponent =
             DaggerFingerprintsComponent
                 .builder()
-                .appModule(AppModule(app))
+                .appModule(object : AppModule(app){
+                    override fun provideDbManager(localDbManager: LocalDbManager, remoteDbManager: RemoteDbManager, secureDataManager: SecureDataManager, loginInfoManager: LoginInfoManager, preferencesManager: PreferencesManager, sessionEventsManager: SessionEventsManager, remotePeopleManager: RemotePeopleManager, remoteProjectManager: RemoteProjectManager, timeHelper: TimeHelper, peopleUpSyncMaster: PeopleUpSyncMaster, database: SyncStatusDatabase): DbManager {
+                        return super.provideDbManager(localDbManager, remoteDbManager, secureDataManager, loginInfoManager, preferencesManager, sessionEventsManager, remotePeopleManager, remoteProjectManager, timeHelper, peopleUpSyncMaster, database).also { it.initialiseDb() }
+                    }
+                })
                 .preferencesModule(PreferencesModule())
                 .serializerModule(SerializerModule())
                 .build()
