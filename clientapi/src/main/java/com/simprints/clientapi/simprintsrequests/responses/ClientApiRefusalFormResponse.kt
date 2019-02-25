9eaa80a76320f94bf4ab886b61545e17ab6195a1
Tristram Norman <tristram@simprints.com>
2019-02-25 22:55:59 +0000

    finish the response returns
    Former-commit-id: 367ba9995feef3a37cbf3739dbb9d52f0538e582

--- clientapi/src/main/java/com/simprints/clientapi/simprintsrequests/responses/ClientApiRefusalFormResponse.kt
+++ clientapi/src/main/java/com/simprints/clientapi/simprintsrequests/responses/ClientApiRefusalFormResponse.kt
@@ -2,14 +2,14 @@ package com.simprints.clientapi.simprintsrequests.responses
 
 import kotlinx.android.parcel.Parcelize
 
+
 @Parcelize
-data class EnrollResponse(val guid: String) : SimprintsIdResponse {
+data class ClientApiRefusalFormResponse(val reason: String, val extra: String) : SimprintsIdResponse {
 
     companion object {
-        const val BUNDLE_KEY = "enrollmentResponse"
+        const val BUNDLE_KEY = "refusalResponse"
     }
 
     override val bundleKey: String get() = BUNDLE_KEY
 
 }
-
