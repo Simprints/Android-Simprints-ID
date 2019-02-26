85040e44c86397ce7b317653de254e07840f887d
fabiotuzza <fabio@simprints.com>
2019-02-26 13:16:28 +0000

    Missing file from last commit
    Former-commit-id: 8c6ad1ee4b1f0f8f7519ee17af7c77f2486050f2

--- id/src/main/java/com/simprints/id/data/db/local/realm/models/rl_SyncInfo.kt
+++ id/src/main/java/com/simprints/id/data/db/local/realm/models/rl_SyncInfo.kt
@@ -1,6 +1,6 @@
 package com.simprints.id.data.db.local.realm.models
 
-import com.simprints.id.domain.Constants
+import com.simprints.id.domain.GROUP
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 import java.util.*
