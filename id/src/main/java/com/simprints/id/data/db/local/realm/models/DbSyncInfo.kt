5fa463565fc917d697deb577e9fa6443ea824626
fabiotuzza <fabio@simprints.com>
2019-02-27 22:32:43 +0000

    Renamed rl_XX to DbXX and fb_XX to ApiXX
    Former-commit-id: 60523b1e3a480553d49978833ba0257b661931a0

--- id/src/main/java/com/simprints/id/data/db/local/realm/models/DbSyncInfo.kt
+++ id/src/main/java/com/simprints/id/data/db/local/realm/models/DbSyncInfo.kt
@@ -5,7 +5,7 @@ import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 import java.util.*
 
-open class rl_SyncInfo : RealmObject {
+open class DbSyncInfo : RealmObject {
 
     @field:PrimaryKey
     var syncGroupId: Int = 0
