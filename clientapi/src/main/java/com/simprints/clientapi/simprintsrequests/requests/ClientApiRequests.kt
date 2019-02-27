8d90e35743025a6d6881a96fa442a50d9f6a616c
Tristram Norman <tristram@simprints.com>
2019-02-27 14:00:26 +0000

    add moduleinterfaces
    Former-commit-id: d5b7996aeecfb24d52c2d6dbe03b7a906977b413

--- clientapi/src/main/java/com/simprints/clientapi/simprintsrequests/requests/ClientApiRequests.kt
+++ clientapi/src/main/java/com/simprints/clientapi/simprintsrequests/requests/ClientApiRequests.kt
@@ -4,6 +4,7 @@ import android.os.Parcelable
 import com.simprints.clientapi.simprintsrequests.ApiVersion
 import kotlinx.android.parcel.IgnoredOnParcel
 
+
 interface ClientApiBaseRequest : Parcelable {
 
     @IgnoredOnParcel
---
+++ moduleinterfaces/src/main/java/com/simprints/moduleinterfaces/clientapi/responses/ClientResponseTier.kt
@@ -0,0 +1,10 @@
+package com.simprints.moduleinterfaces.clientapi.responses
+
+
+enum class ClientResponseTier {
+    TIER_1,
+    TIER_2,
+    TIER_3,
+    TIER_4,
+    TIER_5
+}
