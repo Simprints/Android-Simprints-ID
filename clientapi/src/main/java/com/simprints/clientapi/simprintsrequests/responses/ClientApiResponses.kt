d6da385c8c23a858d27b03b8f220c62d62e3be67
Tristram Norman <tristram@simprints.com>
2019-02-27 14:00:26 +0000

    add moduleinterfaces
    Former-commit-id: d285c90a6d77d847a74a2bc621db3e69a5f3be23

--- clientapi/src/main/java/com/simprints/clientapi/simprintsrequests/responses/ClientApiResponses.kt
+++ clientapi/src/main/java/com/simprints/clientapi/simprintsrequests/responses/ClientApiResponses.kt
@@ -2,15 +2,11 @@ package com.simprints.clientapi.simprintsrequests.responses
 
 import android.os.Parcelable
 import com.simprints.clientapi.simprintsrequests.ApiVersion
-import kotlinx.android.parcel.IgnoredOnParcel
 
 
 interface SimprintsIdResponse : Parcelable {
 
     val apiVersion: ApiVersion get() = ApiVersion.V2
-
-    @IgnoredOnParcel
-    val bundleKey: String
 
     companion object {
         const val BUNDLE_KEY: String = "SimprintsIdResponseBundleName"
