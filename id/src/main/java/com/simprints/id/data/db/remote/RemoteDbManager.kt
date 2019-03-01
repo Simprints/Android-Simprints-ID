b39478bb16bfb79cf73c115ca489963e1a7f1db2
fabiotuzza <fabio@simprints.com>
2019-03-01 01:25:03 +0000

    Removed Firestore for auth
    Former-commit-id: 71adfbf8dd7852e24a4ae12aa4473f63a2c122cb

--- id/src/main/java/com/simprints/id/data/db/remote/RemoteDbManager.kt
+++ id/src/main/java/com/simprints/id/data/db/remote/RemoteDbManager.kt
@@ -1,26 +1,17 @@
 package com.simprints.id.data.db.remote
 
-import com.google.firebase.FirebaseApp
 import com.simprints.id.exceptions.safe.secure.DifferentProjectIdSignedInException
-import com.simprints.id.secure.models.Tokens
 import io.reactivex.Completable
 import io.reactivex.Single
 import com.simprints.id.domain.fingerprint.Person as LibPerson
 
 interface RemoteDbManager {
 
-    // Lifecycle
-    fun initialiseRemoteDb()
-
-    fun signInToRemoteDb(tokens: Tokens): Completable
+    fun signInToRemoteDb(token: String): Completable
     fun signOutOfRemoteDb()
-
-    fun isRemoteDbInitialized(): Boolean
 
     /** @throws DifferentProjectIdSignedInException */
     fun isSignedIn(projectId: String, userId: String): Boolean
-
-    fun getFirebaseLegacyApp(): FirebaseApp
 
     fun getCurrentFirestoreToken(): Single<String>
 }
