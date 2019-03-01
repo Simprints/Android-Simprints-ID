3dbed82168e6a10a854088099b684f0a8143d66a
fabiotuzza <fabio@simprints.com>
2019-03-01 10:26:06 +0000

    Removed Firestore instance!!!!!
    Former-commit-id: 4d40812da84f75d7c592e9b4a4b013806dc6d75c

--- id/src/main/java/com/simprints/id/data/db/remote/FirebaseManagerImpl.kt
+++ id/src/main/java/com/simprints/id/data/db/remote/FirebaseManagerImpl.kt
@@ -1,11 +1,7 @@
 package com.simprints.id.data.db.remote
 
-import android.content.Context
-import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
-import com.simprints.id.Application
 import com.simprints.id.exceptions.unexpected.RemoteDbNotSignedInException
-import com.simprints.id.secure.cryptography.Hasher
 import com.simprints.id.tools.extensions.trace
 import io.reactivex.Completable
 import io.reactivex.Single
@@ -13,69 +9,33 @@ import org.jetbrains.anko.doAsync
 import timber.log.Timber
 import com.simprints.id.domain.fingerprint.Person as LibPerson
 
-open class FirebaseManagerImpl(
-    private val appContext: Context
-) : RemoteDbManager {
+open class FirebaseManagerImpl: RemoteDbManager {
 
-    // FirebaseApp
-    private val legacyFirebaseApp: FirebaseApp by lazy {
-        initialiseFirebaseApp()
+    private val firebaseAuth: FirebaseAuth by lazy {
+        FirebaseAuth.getInstance()
     }
 
-    private fun initialiseFirebaseApp(): FirebaseApp =
-        try {
-            Timber.d("Trying to initialise Firebase app")
-            FirebaseApp.initializeApp(appContext)
-        } catch (stateException: IllegalStateException) {
-            Timber.d("Firebase app: already initialized")
-            FirebaseApp.getInstance()
-        } ?: throw Throwable("test") //StopShp
-
-    private fun getFirebaseAuth(firebaseApp: FirebaseApp): FirebaseAuth =
-        FirebaseAuth.getInstance(firebaseApp)
-
-    override fun signInToRemoteDb(token: String): Completable = signInToDb(legacyFirebaseApp, token)
-
-    private fun signInToDb(firebaseApp: FirebaseApp, token: String): Completable =
+    override fun signInToRemoteDb(token: String): Completable =
         Completable.create {
-            getFirebaseAuth(firebaseApp).signInWithCustomToken(token).addOnCompleteListener { task ->
+            firebaseAuth.signInWithCustomToken(token).addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    Timber.d("Firebase Auth signInWithCustomToken for ${firebaseApp.name} successful")
+                    Timber.d("Firebase Auth signInWithCustomToken successfully")
                     it.onComplete()
                 } else {
-                    Timber.d("Firebase Auth signInWithCustomToken for ${firebaseApp.name} failed: ${task.exception}")
+                    Timber.d("Firebase Auth signInWithCustomToken failed: ${task.exception}")
                     it.onError(task.exception as Throwable)
                 }
             }
         }
 
     override fun signOutOfRemoteDb() {
-        getFirebaseAuth(legacyFirebaseApp).signOut()
+        firebaseAuth.signOut()
     }
 
-    override fun isSignedIn(projectId: String, userId: String): Boolean = isLegacySignedInUserAsExpected(projectId)
+    override fun isSignedIn(projectId: String, userId: String): Boolean = firebaseAuth.currentUser?.uid == projectId
 
-    private fun isLegacySignedInUserAsExpected(projectId: String): Boolean {
-        val firebaseUser = getFirebaseAuth(legacyFirebaseApp).currentUser ?: return false
-
-        // Note: LegacyApiKey and LegacyProjectId are the same thing.
-        // For legacy reason, the firebase user has the projectId(for new projects) or legacyApiKey
-        // (for old projects) as uid. Because the legacyApiKey soon will disappear, we try to map it immediately (CheckLogin)
-        // to a projectId and use it for any task. So we store <ProjectId, HashedLegacyApiKey> in the shared prefs.
-        // In this case, we need the legacyApiKey so we grab through the Application to avoid injecting
-        // it through all methods, so it will be easier to get rid of it.
-        val hashedLegacyApiKey = (appContext as Application).loginInfoManager.getHashedLegacyProjectIdForProjectIdOrEmpty(projectId)
-        return if (hashedLegacyApiKey.isNotEmpty()) {
-            Hasher().hash(firebaseUser.uid) == hashedLegacyApiKey
-        } else {
-            firebaseUser.uid == projectId
-        }
-    }
-
-    private fun getFirebaseLegacyApp(): FirebaseApp = legacyFirebaseApp
-
-    override fun getCurrentFirestoreToken(): Single<String> = Single.create {
-        getFirebaseLegacyApp().getToken(false).trace("getCurrentFirestoreToken")
+    override fun getCurrentToken(): Single<String> = Single.create {
+        firebaseAuth.getAccessToken(false).trace("getCurrentToken")
             .addOnSuccessListener { result ->
                 // Firebase callbacks return on main thread, so the emits
                 // will be in the same thread.
@@ -93,10 +53,5 @@ open class FirebaseManagerImpl(
 
     companion object {
         const val RETRY_ATTEMPTS_FOR_NETWORK_CALLS = 5L
-
-        fun getLegacyAppName(): String =
-            FirebaseApp.DEFAULT_APP_NAME
-
-        fun getFirestoreAppName(): String = "firestore"
     }
 }
