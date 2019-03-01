9b81e0dd0525851a3b84b7c5d2aa8fac74b681b5
fabiotuzza <fabio@simprints.com>
2019-03-01 18:21:30 +0000

    Remove applicationIdSuffix (Firebase)
    Former-commit-id: 7a7d7338a3a087ca719f69a921bc3bcab5fd94d4

--- clientapi/src/main/java/com/simprints/clientapi/extensions/AppRequests.ext.kt
+++ clientapi/src/main/java/com/simprints/clientapi/extensions/AppRequests.ext.kt
@@ -1,8 +1,6 @@
 package com.simprints.clientapi.extensions
 
 import android.content.Intent
-import com.simprints.clientapi.BuildConfig
-import com.simprints.libsimprints.Constants
 import com.simprints.libsimprints.Constants.SIMPRINTS_PACKAGE_NAME
 import com.simprints.moduleinterfaces.app.confirmations.IAppConfirmation
 import com.simprints.moduleinterfaces.app.requests.IAppRequest
@@ -10,12 +8,10 @@ import com.simprints.moduleinterfaces.app.requests.IAppRequest
 
 fun IAppRequest.toIntent(action: String): Intent = Intent(action).apply {
     putExtra(IAppRequest.BUNDLE_KEY, this@toIntent)
-    val packageName = if (BuildConfig.DEBUG) {"${Constants.SIMPRINTS_PACKAGE_NAME}.debug"} else { SIMPRINTS_PACKAGE_NAME }
-    setPackage(packageName)
+    setPackage(SIMPRINTS_PACKAGE_NAME)
 }
 
 fun IAppConfirmation.toIntent(action: String): Intent = Intent(action).apply {
     putExtra(IAppConfirmation.BUNDLE_KEY, this@toIntent)
-    val packageName = if (BuildConfig.DEBUG) {"${Constants.SIMPRINTS_PACKAGE_NAME}.debug"} else { SIMPRINTS_PACKAGE_NAME }
-    setPackage(packageName)
+    setPackage(SIMPRINTS_PACKAGE_NAME)
 }
