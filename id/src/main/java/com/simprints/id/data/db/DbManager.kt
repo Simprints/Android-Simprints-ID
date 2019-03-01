ae5f2e11d3594f48193ea3f88ee969000b0d72ba
Ridwan Farouki <ridwan@simprints.com>
2019-03-01 12:00:48 +0000

    Merge fixes, tweaks to DB call for loading people
    Former-commit-id: 0ebc6317d3e6c671f8cc41b669e42e67442a5674

--- id/src/main/java/com/simprints/id/data/db/DbManager.kt
+++ id/src/main/java/com/simprints/id/data/db/DbManager.kt
@@ -28,10 +28,9 @@ interface DbManager {
     // Data transfer
     fun savePerson(person: Person): Completable
 
-    fun loadPerson(destinationList: MutableList<Person>, projectId: String, guid: String, callback: DataCallback)
-    fun loadPerson(projectId: String, guid: String): Single<PersonFetchResult>
+    fun loadPerson(guid: String): Single<PersonFetchResult>
 
-    fun loadPeople(destinationList: MutableList<Person>, group: GROUP, moduleId: String, callback: DataCallback?)
+    fun loadPeople(group: GROUP): Single<List<Person>>
 
     fun loadProject(projectId: String): Single<Project>
 
