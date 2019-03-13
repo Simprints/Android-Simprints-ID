5055b5b4fa56c4fbca9f7d3e117ae4bb429078cd
Aashay Gaikwad <aashay@simprints.com>
2019-03-13 12:51:31 +0000

    Converting fragment and page adapter to kotlin
    Former-commit-id: 94f58e1c1dbe40eb74477305ac315db4a38be944

---
+++ id/src/main/java/com/simprints/id/activities/collectFingerprints/FingerPageAdapter.kt
@@ -0,0 +1,29 @@
+package com.simprints.id.activities.collectFingerprints
+
+import android.util.SparseArray
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.PagerAdapter
+import com.simprints.id.domain.Finger
+
+class FingerPageAdapter(fragmentManager: FragmentManager, private val activeFingers: List<Finger>) :
+    FragmentStatePagerAdapter(fragmentManager) {
+
+    private val fragmentSparseArray = SparseArray<FingerFragment>()
+
+    override fun getItem(pos: Int) = FingerFragment.newInstance(activeFingers[pos]).also {
+        fragmentSparseArray.append(pos, it)
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        super.destroyItem(container, position, `object`)
+        fragmentSparseArray.remove(position)
+    }
+
+    fun getFragment(pos: Int): FingerFragment = fragmentSparseArray.get(pos)
+
+    override fun getCount() = activeFingers.size
+
+    override fun getItemPosition(`object`: Any) = PagerAdapter.POSITION_NONE
+}
