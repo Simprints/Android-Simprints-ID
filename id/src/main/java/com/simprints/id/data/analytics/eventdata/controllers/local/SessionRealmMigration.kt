007788a47cd7862343f0142615409f0a8b31dc32
Aashay Gaikwad <aashay@simprints.com>
2019-04-03 15:58:08 +0100

    Migrating sessions
    Former-commit-id: 3ac8b02e6f3766b7495b7fa080fba8b0edf40ace

--- id/src/main/java/com/simprints/id/data/analytics/eventdata/controllers/local/SessionRealmMigration.kt
+++ id/src/main/java/com/simprints/id/data/analytics/eventdata/controllers/local/SessionRealmMigration.kt
@@ -3,6 +3,7 @@ package com.simprints.id.data.analytics.eventdata.controllers.local
 import com.simprints.id.data.analytics.eventdata.models.local.*
 import io.realm.DynamicRealm
 import io.realm.RealmMigration
+import io.realm.RealmSchema
 import io.realm.annotations.RealmModule
 
 internal class SessionRealmMigration : RealmMigration {
@@ -13,9 +14,28 @@ internal class SessionRealmMigration : RealmMigration {
 
     companion object {
         const val REALM_SCHEMA_VERSION: Long = 3
+        const val DB_INFO_TABLE = "DbDatabaseInfo"
+        const val DB_DEVICE_TABLE = "DbDevice"
+        const val DB_LOCATION_TABLE = "DbLocation"
+        const val DB_EVENT_TABLE = "DbEvent"
+        const val DB_SESSION_TABLE = "DbSession"
     }
 
-    override fun migrate(realm: DynamicRealm?, oldVersion: Long, newVersion: Long) {}
+    override fun migrate(realm: DynamicRealm, oldVersion: Long, newVersion: Long) {
+        for (i in oldVersion..newVersion) {
+            when (i.toInt()) {
+                3 -> migrateTo4(realm.schema)
+            }
+        }
+    }
+
+    private fun migrateTo4(schema: RealmSchema) {
+        schema.get("RlDatabaseInfo")?.className = DB_INFO_TABLE
+        schema.get("RlDevice")?.className = DB_DEVICE_TABLE
+        schema.get("RlLocation")?.className = DB_LOCATION_TABLE
+        schema.get("RlEvent")?.className = DB_EVENT_TABLE
+        schema.get("RlSession")?.className = DB_SESSION_TABLE
+    }
 
     override fun hashCode(): Int {
         return SessionRealmMigration.hashCode()
