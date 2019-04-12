093a71fa857c01c8b84e1d68110e9fc07bbbe122
fabiotuzza <fabio@simprints.com>
2019-04-12 16:11:11 +0100

    Added unit tests MultiModalitiesFlowBase
    Former-commit-id: 778bd492d0fa1ad6929285fe682753af9c1b79b1

---
+++ id/src/test/java/com/simprints/id/orchestrator/modality/flows/SingleModalityFlowBaseTest.kt
@@ -0,0 +1,81 @@
+package com.simprints.id.orchestrator.modality.flows
+
+import android.app.Activity
+import android.content.Intent
+import com.nhaarman.mockito_kotlin.any
+import com.simprints.id.domain.modality.ModalityResponse
+import com.simprints.id.orchestrator.modality.ModalityStepRequest
+import com.simprints.id.testtools.UnitTestConfig
+import com.simprints.testtools.common.syntax.*
+import org.junit.Before
+import org.junit.Test
+
+class SingleModalityFlowBaseTest {
+
+    companion object {
+        const val REQUEST_CODE_FOR_SINGLE_MODALITY_FLOW = 1
+    }
+
+    @Before
+    fun setUp() {
+        UnitTestConfig(this).rescheduleRxMainThread()
+    }
+
+    @Test
+    fun givenSingleModalityFlow_modalityStepRequestsGetsSubscribed_nextModalityStepRequestShouldBeEmitted() {
+        val singleModalityFlowBase = SingleModalityFlowBaseImpl()
+
+        val testObserver = singleModalityFlowBase.modalityStepRequests
+            .test()
+
+        testObserver.assertNoErrors()
+        testObserver.assertValueCount(1)
+    }
+
+    @Test
+    fun givenSingleModalityFlow_receivesASuccessfulResult_flowShouldEmitAResponse() {
+        with(spy<SingleModalityFlowBaseImpl>()) {
+            responsesEmitter = mock()
+            nextIntentEmitter = mock()
+
+            handleIntentResponse(REQUEST_CODE_FOR_SINGLE_MODALITY_FLOW, Activity.RESULT_OK, mock())
+
+            verifyOnce(this) { completeWithValidResponse(any()) }
+        }
+    }
+
+    @Test
+    fun givenSingleModalityFlow_receivesAnResultForDifferentModality_flowShouldIgnoreIt() {
+        with(spy<SingleModalityFlowBaseImpl>()) {
+            responsesEmitter = mock()
+            nextIntentEmitter = mock()
+
+            handleIntentResponse(REQUEST_CODE_FOR_SINGLE_MODALITY_FLOW + 1, Activity.RESULT_OK, mock())
+
+            verifyNever(this) { completeWithValidResponse(any()) }
+        }
+    }
+
+    @Test
+    fun givenSingleModalityFlow_receivesAMalformedResult_shouldThrowAnError() {
+        with(spy(SingleModalityFlowBaseImpl())) {
+            whenever(this) { intentRequestCode } thenReturn REQUEST_CODE_FOR_SINGLE_MODALITY_FLOW
+            whenever(this) { getModalityStepRequests() } thenReturn mock()
+            whenever(this) { extractModalityResponse(any(), any(), any()) } thenThrow RuntimeException("Malformed data")
+            responsesEmitter = mock()
+            nextIntentEmitter = mock()
+
+            handleIntentResponse(REQUEST_CODE_FOR_SINGLE_MODALITY_FLOW, Activity.RESULT_OK, Intent())
+
+            verifyNever(this) { completeWithValidResponse(any()) }
+            verifyOnce(this) { completeWithAnError(any()) }
+        }
+    }
+
+
+    class SingleModalityFlowBaseImpl : SingleModalityFlowBase() {
+        override val intentRequestCode: Int = REQUEST_CODE_FOR_SINGLE_MODALITY_FLOW
+        override fun getModalityStepRequests(): ModalityStepRequest = mock()
+        override fun extractModalityResponse(requestCode: Int, resultCode: Int, data: Intent?): ModalityResponse = mock()
+    }
+}
