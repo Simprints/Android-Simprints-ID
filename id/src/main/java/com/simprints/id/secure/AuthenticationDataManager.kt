7aad32a583584b201f87871755f8951f8736403c
Aashay Gaikwad <aashay@simprints.com>
2019-04-24 14:26:02 +0100

    Added authentication data
    Former-commit-id: ab330820e2bf6b090410ab29682788431a292ff8

--- id/src/main/java/com/simprints/id/secure/AuthenticationDataManager.kt
+++ id/src/main/java/com/simprints/id/secure/AuthenticationDataManager.kt
@@ -2,25 +2,26 @@ package com.simprints.id.secure
 
 import com.simprints.id.exceptions.safe.data.db.SimprintsInternalServerException
 import com.simprints.id.exceptions.safe.secure.AuthRequestInvalidCredentialsException
-import com.simprints.id.secure.models.Nonce
-import com.simprints.id.secure.models.NonceScope
+import com.simprints.id.secure.models.AuthenticationData
+import com.simprints.id.secure.models.remote.toDomainAuthData
 import com.simprints.id.tools.extensions.handleResponse
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import retrofit2.HttpException
 
-
-class NonceManager(val client: SecureApiInterface) {
-
-    fun requestNonce(nonceScope: NonceScope): Single<Nonce> {
-        return client.requestNonce(nonceScope.projectId, nonceScope.userId)
+class AuthenticationDataManager(val client: SecureApiInterface) {
+    fun requestAuthenticationData(projectId: String, userId: String): Single<AuthenticationData> {
+        return client.requestAuthenticationData(
+            projectId,
+            userId)
             .handleResponse(::handleResponseError)
+            .map { it.toDomainAuthData() }
             .subscribeOn(Schedulers.io())
     }
 
     private fun handleResponseError(e: HttpException): Nothing =
         when (e.code()) {
-            404 -> throw AuthRequestInvalidCredentialsException()
+            401, 404 -> throw AuthRequestInvalidCredentialsException()
             in 500..599 -> throw SimprintsInternalServerException()
             else -> throw e
         }
