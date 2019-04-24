256ec4d1830ee60e67cb9c3ae65cd7bb202fe1c5
Aashay Gaikwad <aashay@simprints.com>
2019-04-24 14:26:26 +0100

    Added ApiAuthenticationData
    Former-commit-id: 956645ee6c1adc87fc10f5dcd14383aa4e86a50e

--- id/src/main/java/com/simprints/id/secure/models/remote/ApiAuthenticationData.kt
+++ id/src/main/java/com/simprints/id/secure/models/remote/ApiAuthenticationData.kt
@@ -1,6 +1,12 @@
-package com.simprints.id.secure.models
+package com.simprints.id.secure.models.remote
 
 import androidx.annotation.Keep
+import com.simprints.id.secure.models.AuthenticationData
+import com.simprints.id.secure.models.Nonce
+import com.simprints.id.secure.models.PublicKeyString
 
 @Keep
 data class ApiAuthenticationData(val nonce: String, val publicKeyString: String)
+
+fun ApiAuthenticationData.toDomainAuthData() = AuthenticationData(Nonce(nonce), PublicKeyString(publicKeyString))
+
