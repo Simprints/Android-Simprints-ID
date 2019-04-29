111d65339152bdf60285c7785ed699de9335c754
fabiotuzza <fabio@simprints.com>
2019-04-29 17:40:59 +0100

    Fix GUID service test.
    Former-commit-id: a0f5d60726100d593e07a1402c9a39eb92aa8f4b

--- id/src/androidTest/java/com/simprints/id/testtools/di/AppComponentForAndroidTests.kt
+++ id/src/androidTest/java/com/simprints/id/testtools/di/AppComponentForAndroidTests.kt
@@ -1,7 +1,7 @@
 package com.simprints.id.testtools.di
 
-import com.simprints.id.activities.dashboard.DashboardActivityAndroidTest
 import com.simprints.id.Application
+import com.simprints.id.activities.dashboard.DashboardActivityAndroidTest
 import com.simprints.id.activities.login.LoginActivityAndroidTest
 import com.simprints.id.data.analytics.eventdata.controllers.local.RealmSessionEventsDbManagerImplTest
 import com.simprints.id.data.secure.SecureDataManagerTest
@@ -9,6 +9,7 @@ import com.simprints.id.di.AppComponent
 import com.simprints.id.di.AppModule
 import com.simprints.id.di.PreferencesModule
 import com.simprints.id.di.SerializerModule
+import com.simprints.id.services.GuidSelectionServiceAndroidTest
 import com.simprints.id.services.scheduledSync.sessionSync.SessionEventsSyncManagerImplAndroidTest
 import com.simprints.id.services.scheduledSync.sessionSync.SessionEventsUploaderTaskAndroidTest
 import dagger.BindsInstance
@@ -33,7 +34,7 @@ interface AppComponentForAndroidTests : AppComponent {
 
     fun inject(sessionEventsUploaderTaskAndroidTest: SessionEventsUploaderTaskAndroidTest)
     fun inject(loginActivityAndroidTest: LoginActivityAndroidTest)
-    //fun inject(guidSelectionServiceAndroidTest: GuidSelectionServiceAndroidTest)
+    fun inject(guidSelectionServiceAndroidTest: GuidSelectionServiceAndroidTest)
     fun inject(dashboardActivityAndroidTest: DashboardActivityAndroidTest)
     fun inject(secureDataManagerTest: SecureDataManagerTest)
     fun inject(localSessionEventsManagerImplTest: RealmSessionEventsDbManagerImplTest)
--- id/src/main/java/com/simprints/id/domain/moduleapi/app/AppToDomainRequest.kt
+++ id/src/main/java/com/simprints/id/domain/moduleapi/app/AppToDomainRequest.kt
@@ -1,10 +1,12 @@
 package com.simprints.id.domain.moduleapi.app
-import com.simprints.id.domain.moduleapi.app.requests.*
+import com.simprints.id.domain.moduleapi.app.requests.AppBaseRequest
+import com.simprints.id.domain.moduleapi.app.requests.AppEnrolRequest
+import com.simprints.id.domain.moduleapi.app.requests.AppIdentifyRequest
+import com.simprints.id.domain.moduleapi.app.requests.AppVerifyRequest
 import com.simprints.moduleapi.app.requests.IAppEnrollRequest
 import com.simprints.moduleapi.app.requests.IAppIdentifyRequest
 import com.simprints.moduleapi.app.requests.IAppRequest
 import com.simprints.moduleapi.app.requests.IAppVerifyRequest
-import com.simprints.moduleapi.app.requests.confirmations.IAppIdentifyConfirmation
 
 object AppRequestToDomainRequest {
 
@@ -13,7 +15,6 @@ object AppRequestToDomainRequest {
             is IAppEnrollRequest -> AppEnrolRequest(appRequest)
             is IAppVerifyRequest -> AppVerifyRequest(appRequest)
             is IAppIdentifyRequest -> AppIdentifyRequest(appRequest)
-            is IAppIdentifyConfirmation -> AppIdentityConfirmationRequest(appRequest)
             else -> throw IllegalArgumentException("Invalid app request") //StopShip
         }
 }
