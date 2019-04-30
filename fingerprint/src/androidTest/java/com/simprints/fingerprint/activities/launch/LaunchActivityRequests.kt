7f97d11cdceba6c4357c52834b6a3730ea422c5a
fabiotuzza <fabio@simprints.com>
2019-04-30 17:35:39 +0100

    Moved fingerprint:unitTest to Android tests - unable to run with gradlew
    Former-commit-id: 12e1eb38a912e2d046ffdaaf030ed3cba9db3bcb

--- fingerprint/src/androidTest/java/com/simprints/fingerprint/activities/launch/LaunchActivityRequests.kt
+++ fingerprint/src/androidTest/java/com/simprints/fingerprint/activities/launch/LaunchActivityRequests.kt
@@ -1,10 +1,7 @@
 package com.simprints.fingerprint.activities.launch
 
 import android.content.Intent
-import com.simprints.moduleapi.fingerprint.requests.IFingerIdentifier
-import com.simprints.moduleapi.fingerprint.requests.IFingerprintEnrolRequest
-import com.simprints.moduleapi.fingerprint.requests.IFingerprintRequest
-import com.simprints.moduleapi.fingerprint.requests.IFingerprintVerifyRequest
+import com.simprints.moduleapi.fingerprint.requests.*
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
@@ -38,6 +35,26 @@ internal data class FingerprintVerifyRequest(
     override val organizationName: String,
     override val verifyGuid: String
 ) : IFingerprintVerifyRequest {
+
+    fun toIntent() = Intent().also {
+        it.putExtra(IFingerprintRequest.BUNDLE_KEY, this)
+    }
+}
+
+@Parcelize
+internal data class FingerprintIdentifyRequest(
+    override val projectId: String,
+    override val userId: String,
+    override val moduleId: String,
+    override val metadata: String,
+    override val language: String,
+    override val fingerStatus: Map<IFingerIdentifier, Boolean>,
+    override val logoExists: Boolean,
+    override val programName: String,
+    override val organizationName: String,
+    override val matchGroup: IMatchGroup,
+    override val returnIdCount: Int
+) : IFingerprintIdentifyRequest {
 
     fun toIntent() = Intent().also {
         it.putExtra(IFingerprintRequest.BUNDLE_KEY, this)
