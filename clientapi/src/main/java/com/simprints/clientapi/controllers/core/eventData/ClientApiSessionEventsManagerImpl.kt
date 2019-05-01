5fe1abf81c1c9c174c2e1a1c468a2b93f2c6d469
Aashay Gaikwad <aashay@simprints.com>
2019-05-01 17:51:43 +0100

    Added Suspicious and Invalid intent events
    Former-commit-id: 94baede25e77690d090d7eca34203afc6170995f

---
+++ clientapi/src/main/java/com/simprints/clientapi/controllers/core/eventData/ClientApiSessionEventsManagerImpl.kt
@@ -0,0 +1,9 @@
+package com.simprints.clientapi.controllers.core.eventData
+
+import com.simprints.id.data.analytics.eventdata.controllers.domain.SessionEventsManager
+
+class ClientApiSessionEventsManagerImpl(private val sessionEventsManager: SessionEventsManager) :
+    ClientApiSessionEventsManager {
+
+    
+}
---
+++ clientapi/src/main/java/com/simprints/clientapi/controllers/core/eventData/model/Event.kt
@@ -0,0 +1,7 @@
+package com.simprints.clientapi.controllers.core.eventData.model
+
+import androidx.annotation.Keep
+import java.util.*
+
+@Keep
+abstract class Event(var type: EventType, val id: String = UUID.randomUUID().toString())
