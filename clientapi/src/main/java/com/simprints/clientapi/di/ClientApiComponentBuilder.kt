22bdc84956b2abcc1ef9dd23ccaccd3b22ed3bf8
Aashay Gaikwad <aashay@simprints.com>
2019-05-01 16:00:37 +0100

    WIP: Creating ClientApiComponent
    Former-commit-id: d46b53f89fb18d03469aecac4afdcfb2a9ca240c

---
+++ clientapi/src/main/java/com/simprints/clientapi/di/ClientApiComponentBuilder.kt
@@ -0,0 +1,29 @@
+package com.simprints.clientapi.di
+
+import com.simprints.id.Application
+import com.simprints.id.di.AppComponent
+
+class ClientApiComponentBuilder {
+    companion object {
+
+        private var component: ClientApiComponent? = null
+
+        //StopShip
+        @JvmStatic
+        fun getComponent(app: Application): ClientApiComponent =
+            component?.let {
+                it
+            } ?: buildComponent(app.component).also { component = it }
+
+        private fun buildComponent(appComponent: AppComponent): ClientApiComponent =
+            DaggerClientApiComponent
+                .builder()
+                .appComponent(appComponent)
+                .build()
+
+        fun setComponent(testAppComponent: ClientApiComponent) {
+            component = testAppComponent
+        }
+
+    }
+}
