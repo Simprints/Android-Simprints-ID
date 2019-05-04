08f53658555d21a95921eabdaa1b15986c4741ad
fabiotuzza <fabio@simprints.com>
2019-05-04 09:45:56 +0100

    Make RealmSessionEvents more reliable.
    Former-commit-id: 3fd74b08a296d0d3a307af3edf57e38ed9749261

--- id/src/androidTest/java/com/simprints/id/data/analytics/eventdata/controllers/local/RealmSessionEventsDbManagerImplTest.kt
+++ id/src/androidTest/java/com/simprints/id/data/analytics/eventdata/controllers/local/RealmSessionEventsDbManagerImplTest.kt
@@ -15,12 +15,15 @@ import com.simprints.id.data.analytics.eventdata.controllers.domain.SessionEvent
 import com.simprints.id.data.analytics.eventdata.models.domain.events.RefusalEvent
 import com.simprints.id.data.analytics.eventdata.models.domain.session.DatabaseInfo
 import com.simprints.id.data.analytics.eventdata.models.domain.session.Location
+import com.simprints.id.data.db.local.models.LocalDbKey
 import com.simprints.id.data.db.remote.RemoteDbManager
 import com.simprints.id.data.prefs.settings.SettingsPreferencesManager
+import com.simprints.id.data.secure.SecureDataManager
 import com.simprints.id.testtools.AndroidTestConfig
 import com.simprints.id.tools.RandomGenerator
 import com.simprints.id.tools.TimeHelper
 import com.simprints.testtools.common.di.DependencyRule
+import com.simprints.testtools.common.syntax.anyNotNull
 import com.simprints.testtools.common.syntax.mock
 import com.simprints.testtools.common.syntax.whenever
 import org.junit.Before
@@ -28,52 +31,31 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import javax.inject.Inject
+import kotlin.random.Random
 
 @RunWith(AndroidJUnit4::class)
 @SmallTest
 class RealmSessionEventsDbManagerImplTest { // TODO : Tests are failing because creating a project remotely is throwing a 404
 
-    private val app = ApplicationProvider.getApplicationContext<Application>()
+    private lateinit var realmSessionEventsManager: SessionEventsLocalDbManager
+    private val realmForDataEvent
+        get() = (realmSessionEventsManager as RealmSessionEventsDbManagerImpl).getRealmInstance().blockingGet()
+
+    private var localDbKey: LocalDbKey = LocalDbKey("database_name", Random.nextBytes(64))
 
     private val testProjectId1 = "test_project1"
     private val testProjectId2 = "test_project2"
     private val testProjectId3 = "test_project3"
 
-    @get:Rule val simprintsActionTestRule = ActivityTestRule(CheckLoginFromIntentActivity::class.java, false, false)
-
-    @Inject lateinit var realmSessionEventsManager: SessionEventsLocalDbManager
-    @Inject lateinit var sessionEventsManagerSpy: SessionEventsManager
-    @Inject lateinit var settingsPreferencesManagerSpy: SettingsPreferencesManager
-    @Inject lateinit var remoteDbManager: RemoteDbManager
     @Inject lateinit var timeHelper: TimeHelper
-    @Inject lateinit var randomGeneratorMock: RandomGenerator
-
-    private val preferencesModule by lazy {
-        TestPreferencesModule(settingsPreferencesManagerRule = DependencyRule.SpyRule)
-    }
-
-    private val module by lazy {
-        TestAppModule(
-            app,
-            randomGeneratorRule = DependencyRule.ReplaceRule { mock<RandomGenerator>().apply { setupRandomGeneratorToGenerateKey(this) } },
-            localDbManagerRule = DependencyRule.SpyRule,
-            remoteDbManagerRule = DependencyRule.SpyRule,
-            sessionEventsManagerRule = DependencyRule.SpyRule
-        )
-    }
-
-    private val realmForDataEvent
-        get() = (realmSessionEventsManager as RealmSessionEventsDbManagerImpl).getRealmInstance().blockingGet()
 
     @Before
     fun setUp() {
-        AndroidTestConfig(this, module, preferencesModule).fullSetup()
+        AndroidTestConfig(this).fullSetup()
 
-        signOut()
-
-        whenever(settingsPreferencesManagerSpy.fingerStatus).thenReturn(hashMapOf(
-            FingerIdentifier.LEFT_THUMB to true,
-            FingerIdentifier.LEFT_INDEX_FINGER to true))
+        val secureDataManager: SecureDataManager = mock()
+        whenever(secureDataManager) { getLocalDbKeyOrThrow(anyNotNull()) } thenReturn localDbKey
+        realmSessionEventsManager = RealmSessionEventsDbManagerImpl(ApplicationProvider.getApplicationContext(), secureDataManager)
     }
 
     @Test
@@ -85,12 +67,6 @@ class RealmSessionEventsDbManagerImplTest { // TODO : Tests are failing because 
         val sessionCloseProject2Id = createAndSaveCloseSession(testProjectId2)
         createAndSaveCloseSession(testProjectId3)
         createAndSaveCloseSession(testProjectId3)
-
-        sessionEventsManagerSpy.updateSession {
-            it.databaseInfo = DatabaseInfo(0)
-            it.location = Location(0.0, 0.0)
-            it.addEvent(RefusalEvent(200, 200, RefusalEvent.Answer.OTHER, "fake_event"))
-        }.blockingGet()
 
         verifyNumberOfSessionsInDb(5, realmForDataEvent)
 
@@ -121,8 +97,4 @@ class RealmSessionEventsDbManagerImplTest { // TODO : Tests are failing because 
 
     private fun createAndSaveOpenSession(projectId: String = testProjectId1): String =
         createAndSaveOpenFakeSession(timeHelper, realmSessionEventsManager, projectId)
-
-    private fun signOut() {
-        remoteDbManager.signOutOfRemoteDb()
-    }
 }
