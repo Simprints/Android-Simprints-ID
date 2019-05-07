83ee50dd676c66dcd2b0c4576a0ec764961d2309
Aashay Gaikwad <aashay@simprints.com>
2019-05-07 15:43:03 +0100

    added val
    Former-commit-id: 478f58f7e68bb895a8f62385473831de6b1d3f62

--- id/src/main/java/com/simprints/id/data/analytics/eventdata/models/remote/events/ApiIdentificationCallback.kt
+++ id/src/main/java/com/simprints/id/data/analytics/eventdata/models/remote/events/ApiIdentificationCallback.kt
@@ -3,4 +3,4 @@ package com.simprints.id.data.analytics.eventdata.models.remote.events
 import androidx.annotation.Keep
 
 @Keep
-class ApiIdentificationCallback(val sessionId: String, scores: List<ApiCallbackComparisonScore>): ApiCallback(ApiCallbackType.IDENTIFICATION)
+class ApiIdentificationCallback(val sessionId: String, val scores: List<ApiCallbackComparisonScore>): ApiCallback(ApiCallbackType.IDENTIFICATION)
