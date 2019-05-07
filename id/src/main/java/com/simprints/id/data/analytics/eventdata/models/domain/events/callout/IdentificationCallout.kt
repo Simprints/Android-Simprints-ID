2498befdf6b21635f016e2e160c41e8d528759d1
fabiotuzza <fabio@simprints.com>
2019-05-07 17:50:51 +0100

    Added Suspicious Event and InvalidEvent tests + fixed CalloutEvent tests
    Former-commit-id: 4c7e9d8994d4b22d83e314140a2ccc17b84408dd

--- id/src/main/java/com/simprints/id/data/analytics/eventdata/models/domain/events/callout/IdentificationCallout.kt
+++ id/src/main/java/com/simprints/id/data/analytics/eventdata/models/domain/events/callout/IdentificationCallout.kt
@@ -1,14 +1,13 @@
-package com.simprints.id.data.analytics.eventdata.models.domain.events
+package com.simprints.id.data.analytics.eventdata.models.domain.events.callout
 
 import androidx.annotation.Keep
-import com.simprints.id.data.analytics.eventdata.models.remote.events.ApiIdentificationCallout
+import com.simprints.id.data.analytics.eventdata.models.remote.events.callout.ApiIdentificationCallout
 
 @Keep
-class IdentificationCallout(val integration: String,
-                            val projectId: String,
+class IdentificationCallout(val projectId: String,
                             val userId: String,
                             val moduleId: String,
                             val metadata: String?): Callout(CalloutType.IDENTIFICATION)
 
 fun IdentificationCallout.toApiIdentificationCallout() =
-    ApiIdentificationCallout(integration, projectId, userId, moduleId, metadata)
+    ApiIdentificationCallout(projectId, userId, moduleId, metadata)
--- id/src/main/java/com/simprints/id/data/analytics/eventdata/models/domain/events/callout/VerificationCallout.kt
+++ id/src/main/java/com/simprints/id/data/analytics/eventdata/models/domain/events/callout/VerificationCallout.kt
@@ -1,15 +1,14 @@
-package com.simprints.id.data.analytics.eventdata.models.domain.events
+package com.simprints.id.data.analytics.eventdata.models.domain.events.callout
 
 import androidx.annotation.Keep
-import com.simprints.id.data.analytics.eventdata.models.remote.events.ApiVerificationCallout
+import com.simprints.id.data.analytics.eventdata.models.remote.events.callout.ApiVerificationCallout
 
 @Keep
-class VerificationCallout(val integration: String,
-                          val projectId: String,
+class VerificationCallout(val projectId: String,
                           val userId: String,
                           val moduleId: String,
                           val verifyGuid: String,
                           val metadata: String): Callout(CalloutType.VERIFICATION)
 
 fun VerificationCallout.toApiVerificationCallout() =
-    ApiVerificationCallout(integration, projectId, userId, moduleId, verifyGuid, metadata)
+    ApiVerificationCallout(projectId, userId, moduleId, verifyGuid, metadata)
