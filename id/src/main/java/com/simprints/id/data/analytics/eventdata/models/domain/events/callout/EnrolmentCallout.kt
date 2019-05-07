dba63cdb6e5f626429c9e54a59c47de18dd8edc1
fabiotuzza <fabio@simprints.com>
2019-05-07 17:50:51 +0100

    Added Suspicious Event and InvalidEvent tests + fixed CalloutEvent tests
    Former-commit-id: 43a3b51ccd0e41aeca6f1bffdac48e6e29dc9fed

--- id/src/main/java/com/simprints/id/data/analytics/eventdata/models/domain/events/callout/EnrolmentCallout.kt
+++ id/src/main/java/com/simprints/id/data/analytics/eventdata/models/domain/events/callout/EnrolmentCallout.kt
@@ -1,14 +1,13 @@
-package com.simprints.id.data.analytics.eventdata.models.domain.events
+package com.simprints.id.data.analytics.eventdata.models.domain.events.callout
 
 import androidx.annotation.Keep
-import com.simprints.id.data.analytics.eventdata.models.remote.events.ApiEnrolmentCallout
+import com.simprints.id.data.analytics.eventdata.models.remote.events.callout.ApiEnrolmentCallout
 
 @Keep
-class EnrolmentCallout(val integration: String,
-                       val projectId: String,
+class EnrolmentCallout(val projectId: String,
                        val userId: String,
                        val moduleId: String,
                        val metadata: String?): Callout(CalloutType.ENROLMENT)
 
 fun EnrolmentCallout.toApiEnrolmentCallout() =
-    ApiEnrolmentCallout(integration, projectId, userId, moduleId, metadata)
+    ApiEnrolmentCallout(projectId, userId, moduleId, metadata)
