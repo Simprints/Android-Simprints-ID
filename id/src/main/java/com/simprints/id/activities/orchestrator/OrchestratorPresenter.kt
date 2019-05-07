b2722082facb4cf402430da1a4f73be3c987ed78
Aashay Gaikwad <aashay@simprints.com>
2019-05-07 16:51:34 +0100

    Putting callbacks in sessions
    Former-commit-id: f373953a9f74a50a9bb9c60427cbcd13380c7483

--- id/src/main/java/com/simprints/id/activities/orchestrator/OrchestratorPresenter.kt
+++ id/src/main/java/com/simprints/id/activities/orchestrator/OrchestratorPresenter.kt
@@ -3,13 +3,15 @@ package com.simprints.id.activities.orchestrator
 import android.annotation.SuppressLint
 import android.content.Intent
 import com.simprints.id.data.analytics.eventdata.controllers.domain.SessionEventsManager
+import com.simprints.id.data.analytics.eventdata.models.domain.events.*
 import com.simprints.id.di.AppComponent
 import com.simprints.id.domain.moduleapi.app.DomainToAppResponse
 import com.simprints.id.domain.moduleapi.app.requests.AppRequest
-import com.simprints.id.domain.moduleapi.app.responses.AppResponse
+import com.simprints.id.domain.moduleapi.app.responses.*
 import com.simprints.id.orchestrator.OrchestratorManager
 import com.simprints.id.orchestrator.modality.ModalityStepRequest
 import com.simprints.id.services.scheduledSync.SyncSchedulerHelper
+import com.simprints.id.tools.TimeHelper
 import com.simprints.moduleapi.app.responses.IAppResponse
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -24,6 +26,7 @@ class OrchestratorPresenter(val view: OrchestratorContract.View,
     @Inject lateinit var orchestratorManager: OrchestratorManager
     @Inject lateinit var sessionEventsManager: SessionEventsManager
     @Inject lateinit var syncSchedulerHelper: SyncSchedulerHelper
+    @Inject lateinit var timeHelper: TimeHelper
 
     init {
         component.inject(this)
@@ -65,8 +68,10 @@ class OrchestratorPresenter(val view: OrchestratorContract.View,
                     handleErrorInTheModalitiesFlow(it)
                 })
 
-    private fun handleAppResponse(appResponse: AppResponse) =
+    private fun handleAppResponse(appResponse: AppResponse) {
+        addCallbackEventInSessions(appResponse)
         view.setResultAndFinish(appResponse)
+    }
 
     @SuppressLint("CheckResult")
     internal fun getSessionId(): Single<String> =
@@ -80,6 +85,44 @@ class OrchestratorPresenter(val view: OrchestratorContract.View,
         view.setCancelResultAndFinish()
         Timber.d(it.message)
     }
+
+    private fun addCallbackEventInSessions(appResponse: AppResponse) {
+        when(appResponse) {
+            is AppEnrolResponse -> sessionEventsManager
+                .addSessionEvent(buildEnrolmentCallbackEvent(appResponse))
+            is AppIdentifyResponse -> sessionEventsManager
+                .addSessionEvent(buildIdentificationCallbackEvent(appResponse))
+            is AppVerifyResponse -> sessionEventsManager
+                .addSessionEvent(buildVerificationCallbackEvent(appResponse))
+            is AppRefusalFormResponse -> sessionEventsManager
+                .addSessionEvent(buildRefusalCallbackEvent(appResponse))
+        }
+    }
+
+    private fun buildEnrolmentCallbackEvent(appResponse: AppEnrolResponse) =
+        CallbackEvent(timeHelper.now(), EnrolmentCallback(appResponse.guid))
+
+    private fun buildIdentificationCallbackEvent(appResponse: AppIdentifyResponse) =
+        CallbackEvent(timeHelper.now(), buildIdentificationCallback(appResponse))
+
+    private fun buildIdentificationCallback(appIdentifyResponse: AppIdentifyResponse) =
+        IdentificationCallback(appIdentifyResponse.sessionId,
+            appIdentifyResponse.identifications.map {
+                CallbackComparisonScore(it.guidFound, it.confidence, it.tier)
+            })
+
+    private fun buildVerificationCallbackEvent(appVerifyResponse: AppVerifyResponse) =
+        CallbackEvent(timeHelper.now(),
+            VerificationCallback(appVerifyResponse.matchingResult.let {
+                CallbackComparisonScore(it.guidFound, it.confidence, it.tier)
+            }))
+
+    private fun buildRefusalCallbackEvent(appRefusalResponse: AppRefusalFormResponse) =
+        CallbackEvent(timeHelper.now(), RefusalCallback(getAppRefusalResponseReasonOrEmpty(appRefusalResponse),
+            appRefusalResponse.answer.optionalText))
+
+    private fun getAppRefusalResponseReasonOrEmpty(appRefusalResponse: AppRefusalFormResponse) =
+        appRefusalResponse.answer.reason?.name ?: ""
 
     override fun handleResult(requestCode: Int, resultCode: Int, data: Intent?) {
         orchestratorManager.onModalStepRequestDone(requestCode, resultCode, data)
