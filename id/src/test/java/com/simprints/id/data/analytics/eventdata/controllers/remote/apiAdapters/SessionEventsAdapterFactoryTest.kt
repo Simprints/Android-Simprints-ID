b1e3f4a12e9e0ed0085295f2ddc9a0cf22d9c2ef
Aashay Gaikwad <aashay@simprints.com>
2019-05-08 11:01:20 +0100

    Tests for CallbackApiModel
    Former-commit-id: ce382f95cc628754fc7317fc0ce95c1d03336de0

--- id/src/test/java/com/simprints/id/data/analytics/eventdata/controllers/remote/apiAdapters/SessionEventsAdapterFactoryTest.kt
+++ id/src/test/java/com/simprints/id/data/analytics/eventdata/controllers/remote/apiAdapters/SessionEventsAdapterFactoryTest.kt
@@ -2,14 +2,16 @@ package com.simprints.id.data.analytics.eventdata.controllers.remote.apiAdapters
 
 import android.net.NetworkInfo
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.google.gson.Gson
 import com.simprints.core.tools.json.JsonHelper
 import com.simprints.id.FingerIdentifier
 import com.simprints.id.commontesttools.sessionEvents.*
 import com.simprints.id.data.analytics.eventdata.models.domain.events.*
 import com.simprints.id.data.analytics.eventdata.models.domain.events.OneToManyMatchEvent.MatchPool
 import com.simprints.id.data.analytics.eventdata.models.domain.events.OneToManyMatchEvent.MatchPoolType
-import com.simprints.id.data.analytics.eventdata.models.domain.events.callout.*
+import com.simprints.id.data.analytics.eventdata.models.domain.events.callout.ConfirmationCallout
+import com.simprints.id.data.analytics.eventdata.models.domain.events.callout.EnrolmentCallout
+import com.simprints.id.data.analytics.eventdata.models.domain.events.callout.IdentificationCallout
+import com.simprints.id.data.analytics.eventdata.models.domain.events.callout.VerificationCallout
 import com.simprints.id.data.analytics.eventdata.models.domain.session.DatabaseInfo
 import com.simprints.id.data.analytics.eventdata.models.domain.session.Device
 import com.simprints.id.data.analytics.eventdata.models.domain.session.Location
@@ -20,8 +22,6 @@ import com.simprints.id.data.analytics.eventdata.models.remote.session.ApiDevice
 import com.simprints.id.data.analytics.eventdata.models.remote.session.ApiLocation
 import com.simprints.id.data.analytics.eventdata.models.remote.session.ApiSessionEvents
 import com.simprints.id.domain.alert.Alert
-import com.simprints.id.domain.moduleapi.app.responses.AppVerifyResponse
-import com.simprints.id.domain.moduleapi.app.responses.entities.MatchResult
 import com.simprints.id.domain.moduleapi.app.responses.entities.Tier
 import com.simprints.id.testtools.TestApplication
 import com.simprints.id.tools.utils.SimNetworkUtils
@@ -80,15 +80,6 @@ class SessionEventsAdapterFactoryTest {
     }
 
     @Test
-    fun validate_callbackEventApiModel() {
-//        val event = VerifyResponseEvent(10,
-//            AppVerifyResponse(MatchResult("guid", 75, Tier.TIER_1)))
-//        val apiEvent = ApiCallbackEvent(event)
-//        val json = gsonWithAdapters.toJsonTree(apiEvent).asJsonObject
-//        validateCallbackEventApiModel(json)
-    }
-
-    @Test
     fun validate_calloutEventForVerificationApiModel() {
         val calloutVerification = VerificationCallout("projectId", "userId", "moduleId", "verifyGuid", "metadata")
         val calloutEvent = CalloutEvent("ODK_SHOULD_BE_ENUM", 10, calloutVerification)
@@ -127,6 +118,52 @@ class SessionEventsAdapterFactoryTest {
 
         validateCalloutEventApiModel(json)
     }
+
+    @Test
+    fun validate_callbackEventForEnrolmentApiModel() {
+        val callbackEnrolment = EnrolmentCallback("guid")
+        val callbackEvent = CallbackEvent(10, callbackEnrolment)
+        val apiEvent = ApiCallbackEvent(callbackEvent)
+        val json = gsonWithAdapters.toJsonTree(apiEvent).asJsonObject
+
+        validateCallbackEventApiModel(json)
+    }
+
+    @Test
+    fun validate_callbackEventForIdentificationApiModel() {
+        val callbackIdentification = IdentificationCallback("sessionId", getListOfCallbackComparisonScores())
+        val callbackEvent = CallbackEvent(10, callbackIdentification)
+        val apiEvent = ApiCallbackEvent(callbackEvent)
+        val json = gsonWithAdapters.toJsonTree(apiEvent).asJsonObject
+
+        validateCallbackEventApiModel(json)
+    }
+
+    @Test
+    fun validate_callbackEventForVerificationApiModel() {
+        val callbackVerification = VerificationCallback(CallbackComparisonScore("guid", 42, Tier.TIER_1))
+        val callbackEvent = CallbackEvent(10, callbackVerification)
+        val apiEvent = ApiCallbackEvent(callbackEvent)
+        val json = gsonWithAdapters.toJsonTree(apiEvent).asJsonObject
+
+        validateCallbackEventApiModel(json)
+    }
+
+    @Test
+    fun validate_callbackEventForRefusalApiModel() {
+        val callbackRefusal = RefusalCallback("reason", "extra")
+        val callbackEvent = CallbackEvent(10, callbackRefusal)
+        val apiEvent = ApiCallbackEvent(callbackEvent)
+        val json = gsonWithAdapters.toJsonTree(apiEvent).asJsonObject
+
+        validateCallbackEventApiModel(json)
+    }
+
+    private fun getListOfCallbackComparisonScores() = listOf(
+        CallbackComparisonScore("guid1", 42, Tier.TIER_1),
+        CallbackComparisonScore("guid2", 43, Tier.TIER_2),
+        CallbackComparisonScore("guid3", 44, Tier.TIER_3)
+    )
 
     @Test
     fun validate_candidateReadEventApiModel() {
