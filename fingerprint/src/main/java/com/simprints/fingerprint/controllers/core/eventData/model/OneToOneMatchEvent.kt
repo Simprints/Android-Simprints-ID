1b6852ef4de5b813baaea58b1a797cf83bdde393
fabiotuzza <fabio@simprints.com>
2019-05-20 11:29:30 +0100

    Bug fixing: missing transformation for Events from Fingerprint to Core.
    Former-commit-id: 812f84a7e88e2247349cfe8003271efcccaa4a9c

--- fingerprint/src/main/java/com/simprints/fingerprint/controllers/core/eventData/model/OneToOneMatchEvent.kt
+++ fingerprint/src/main/java/com/simprints/fingerprint/controllers/core/eventData/model/OneToOneMatchEvent.kt
@@ -1,9 +1,13 @@
 package com.simprints.fingerprint.controllers.core.eventData.model
 
 import androidx.annotation.Keep
+import com.simprints.id.data.analytics.eventdata.models.domain.events.OneToOneMatchEvent as CoreOneToOneMatchEvent
 
 @Keep
 class OneToOneMatchEvent(starTime: Long,
                          endTime: Long,
                          val candidateId: String,
                          val result: MatchEntry?) : Event(EventType.ONE_TO_ONE_MATCH, starTime, endTime)
+
+fun OneToOneMatchEvent.fromDomainToCore() =
+    CoreOneToOneMatchEvent(starTime, endTime, candidateId, result?.fromDomainToCore())
