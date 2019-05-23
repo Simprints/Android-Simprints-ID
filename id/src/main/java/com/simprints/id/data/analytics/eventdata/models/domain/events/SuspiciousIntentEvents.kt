380e247d1053610fd461d9ba7ee051adb2ab6532
fabiotuzza <fabio@simprints.com>
2019-05-23 16:20:33 +0100

    Fix Realm and R8 issue for production builds.
    Former-commit-id: 91cc8c74e454cf3bbd57377cc48119ce5bb96c16

--- id/src/main/java/com/simprints/id/data/analytics/eventdata/models/domain/events/SuspiciousIntentEvents.kt
+++ id/src/main/java/com/simprints/id/data/analytics/eventdata/models/domain/events/SuspiciousIntentEvents.kt
@@ -1,4 +1,7 @@
 package com.simprints.id.data.analytics.eventdata.models.domain.events
 
+import androidx.annotation.Keep
+
+@Keep
 class SuspiciousIntentEvent(starTime: Long,
                             val unexpectedExtras: Map<String, Any?>) : Event(EventType.SUSPICIOUS_INTENT, starTime)
