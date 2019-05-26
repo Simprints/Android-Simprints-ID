fdb2fc309e444fa38a510ca6b707be5ccd0adc91
Tristram Norman <tristram@simprints.com>
2019-05-26 14:30:30 +0100

    add koin module
    Former-commit-id: 67ebfb77432618943c7d68c47aebce79479e6845

--- clientapi/src/main/java/com/simprints/clientapi/di/KoinModules.kt
+++ clientapi/src/main/java/com/simprints/clientapi/di/KoinModules.kt
@@ -1,8 +1,35 @@
 package com.simprints.clientapi.di
 
+import com.simprints.clientapi.activities.errors.ErrorContract
+import com.simprints.clientapi.activities.errors.ErrorPresenter
+import com.simprints.clientapi.controllers.core.crashreport.ClientApiCrashReportManager
+import com.simprints.clientapi.controllers.core.crashreport.ClientApiCrashReportManagerImpl
+import com.simprints.clientapi.controllers.core.eventData.ClientApiSessionEventsManager
+import com.simprints.clientapi.controllers.core.eventData.ClientApiSessionEventsManagerImpl
+import com.simprints.clientapi.tools.ClientApiTimeHelper
+import com.simprints.clientapi.tools.ClientApiTimeHelperImpl
+import com.simprints.id.Application
+import com.simprints.id.data.analytics.crashreport.CoreCrashReportManager
+import com.simprints.id.data.analytics.eventdata.controllers.domain.SessionEventsManager
+import com.simprints.id.tools.TimeHelper
+import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
 
 val koinModule = module {
+
+    // Core Builders
+    // TODO: I don't know what casting to id application will do
+    factory { SessionEventsManager.build(androidApplication() as Application) }
+    factory { CoreCrashReportManager.build(androidApplication() as Application) }
+    factory { TimeHelper.build(androidApplication() as Application) }
+
+    // Domain
+    factory<ClientApiSessionEventsManager> { ClientApiSessionEventsManagerImpl(get()) }
+    factory<ClientApiCrashReportManager> { ClientApiCrashReportManagerImpl(get()) }
+    factory<ClientApiTimeHelper> { ClientApiTimeHelperImpl(get()) }
+
+    // Presenters
+    factory<ErrorContract.Presenter> { (view: ErrorContract.View) -> ErrorPresenter(view, get(), get()) }
 
 }
