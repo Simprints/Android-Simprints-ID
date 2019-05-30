d8ca6412284e5255e2fd8bd753d02e32ec450612
fabiotuzza <fabio@simprints.com>
2019-05-30 23:04:51 +0100

    Fixed legacy token issue
    Former-commit-id: 0945e70965b8c4445225d2e15854d6cfd1535af7

--- id/proguard-rules.pro
+++ id/proguard-rules.pro
@@ -85,3 +85,15 @@
 -keepclassmembernames class kotlinx.** {
     volatile <fields>;
 }
+
+
+# jwt
+-keep class com.fasterxml.jackson.databind.ObjectMapper {
+    public <methods>;
+    protected <methods>;
+}
+-keep class com.fasterxml.jackson.databind.ObjectWriter {
+    public ** writeValueAsString(**);
+}
+-keepnames class com.fasterxml.jackson.** { *; }
+-dontwarn com.fasterxml.jackson.databind.**
--- id/src/main/java/com/simprints/id/data/db/remote/FirebaseManagerImpl.kt
+++ id/src/main/java/com/simprints/id/data/db/remote/FirebaseManagerImpl.kt
@@ -1,16 +1,17 @@
 package com.simprints.id.data.db.remote
 
+import com.auth0.jwt.JWT
 import com.google.android.gms.tasks.Tasks
 import com.google.firebase.auth.FirebaseAuth
 import com.simprints.id.exceptions.unexpected.RemoteDbNotSignedInException
 import com.simprints.id.tools.extensions.trace
 import io.reactivex.Completable
 import io.reactivex.Single
-import org.jetbrains.anko.doAsync
+import io.reactivex.schedulers.Schedulers
 import timber.log.Timber
 import com.simprints.id.domain.Person as LibPerson
 
-open class FirebaseManagerImpl: RemoteDbManager {
+open class FirebaseManagerImpl : RemoteDbManager {
 
     private val firebaseAuth: FirebaseAuth by lazy {
         FirebaseAuth.getInstance()
@@ -26,17 +27,22 @@ open class FirebaseManagerImpl: RemoteDbManager {
         firebaseAuth.signOut()
     }
 
-    override fun isSignedIn(projectId: String, userId: String): Boolean = firebaseAuth.currentUser?.uid?.startsWith(projectId) ?: false
+    override fun isSignedIn(projectId: String, userId: String): Boolean =
+        (JWT.decode(getCurrentToken()
+            .subscribeOn(Schedulers.io())
+            .blockingGet())
+            .claims[projectIdClaim]?.asString() == projectId)
 
     override fun getCurrentToken(): Single<String> = Single.fromCallable {
-            val result = Tasks.await(firebaseAuth.getAccessToken(false))
-            result.token?.let {
-                it
-            } ?: throw RemoteDbNotSignedInException()
-        }
+        val result = Tasks.await(firebaseAuth.getAccessToken(false))
+        result.token?.let {
+            it
+        } ?: throw RemoteDbNotSignedInException()
+    }
 
 
     companion object {
+        private const val projectIdClaim = "projectId"
         const val RETRY_ATTEMPTS_FOR_NETWORK_CALLS = 5L
     }
 }
