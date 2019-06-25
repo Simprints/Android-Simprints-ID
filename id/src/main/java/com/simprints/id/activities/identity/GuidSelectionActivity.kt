e16a25b846d843baa75096c60e2cb40ba27a9bdf
Alan Camargo <alan.uk.br@gmail.com>
2019-06-25 12:56:08 +0100

    Remove comment and unnecessary extension function
    Former-commit-id: 35130f2ab615799d13d79e83ed966240620048d2

--- id/src/main/java/com/simprints/id/activities/identity/GuidSelectionActivity.kt
+++ id/src/main/java/com/simprints/id/activities/identity/GuidSelectionActivity.kt
@@ -1,25 +1,22 @@
 package com.simprints.id.activities.identity
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class GuidSelectionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //Toast.makeText(this, "Alan was here", Toast.LENGTH_SHORT).show()
         routeService(intent)
         finish()
     }
 
-    private fun Activity.routeService(intent: Intent) =
+    private fun routeService(intent: Intent) =
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-            this.startForegroundService(intent)
+            startForegroundService(intent)
         else
-            this.startService(intent)
+            startService(intent)
 
 }
