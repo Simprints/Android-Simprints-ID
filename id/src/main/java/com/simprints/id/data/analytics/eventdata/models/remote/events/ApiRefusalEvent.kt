22725543b9b1d1c7c524e429ec6ec1b3c100a396
Aashay Gaikwad <aashay@simprints.com>
2019-06-26 10:11:38 +0530

    Made refusal answer non-nullable and updated api models
    Former-commit-id: a7067ce4ca969a3f8c37785b6d975d5f27ee3c52

--- id/src/main/java/com/simprints/id/data/analytics/eventdata/models/remote/events/ApiRefusalEvent.kt
+++ id/src/main/java/com/simprints/id/data/analytics/eventdata/models/remote/events/ApiRefusalEvent.kt
@@ -11,14 +11,31 @@ class ApiRefusalEvent(val relativeStartTime: Long,
 
     @Keep
     enum class ApiAnswer {
-        BENEFICIARY_REFUSED,
+        REFUSED_RELIGION,
+        REFUSED_DATA_CONCERNS,
+        REFUSED_PERMISSION,
         SCANNER_NOT_WORKING,
+        REFUSED_YOUNG,
+        REFUSED_SICK,
+        REFUSED_PREGNANT,
         OTHER
     }
 
     constructor(refusalEvent: RefusalEvent) :
         this(refusalEvent.relativeStartTime ?: 0,
             refusalEvent.relativeEndTime ?: 0,
-            ApiAnswer.valueOf(refusalEvent.reason.toString()),
+            refusalEvent.reason.toApiRefusalEventAnswer(),
             refusalEvent.otherText)
 }
+
+fun RefusalEvent.Answer.toApiRefusalEventAnswer() =
+    when(this) {
+        RefusalEvent.Answer.REFUSED_RELIGION -> ApiRefusalEvent.ApiAnswer.REFUSED_RELIGION
+        RefusalEvent.Answer.REFUSED_DATA_CONCERNS -> ApiRefusalEvent.ApiAnswer.REFUSED_DATA_CONCERNS
+        RefusalEvent.Answer.REFUSED_PERMISSION -> ApiRefusalEvent.ApiAnswer.REFUSED_PERMISSION
+        RefusalEvent.Answer.SCANNER_NOT_WORKING -> ApiRefusalEvent.ApiAnswer.SCANNER_NOT_WORKING
+        RefusalEvent.Answer.REFUSED_YOUNG -> ApiRefusalEvent.ApiAnswer.REFUSED_YOUNG
+        RefusalEvent.Answer.REFUSED_SICK -> ApiRefusalEvent.ApiAnswer.REFUSED_SICK
+        RefusalEvent.Answer.REFUSED_PREGNANT -> ApiRefusalEvent.ApiAnswer.REFUSED_PREGNANT
+        RefusalEvent.Answer.OTHER -> ApiRefusalEvent.ApiAnswer.OTHER
+    }
