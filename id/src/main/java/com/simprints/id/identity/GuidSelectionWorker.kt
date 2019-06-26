e7953dc2d35ec87ec20615e5b0cdb47832a44cd2
Alan Camargo <alan.uk.br@gmail.com>
2019-06-26 16:23:02 +0100

    Implement GUID selection with WorkManager
    Former-commit-id: 3ec493b87eb4d204c815cbf9644e47c26d818632

---
+++ id/src/main/java/com/simprints/id/identity/GuidSelectionWorker.kt
@@ -0,0 +1,35 @@
+package com.simprints.id.identity
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.simprints.id.services.GuidSelectionManager
+import com.simprints.id.tools.extensions.parseAppConfirmation
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
+import javax.inject.Inject
+
+class GuidSelectionWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
+
+    @Inject lateinit var guidSelectionManager: GuidSelectionManager
+
+    override fun doWork(): Result {
+        handleIdentityConfirmationRequest()
+        return Result.success()
+    }
+
+    @SuppressLint("CheckResult")
+    private fun handleIdentityConfirmationRequest() {
+        val request = inputData.parseAppConfirmation()
+        guidSelectionManager.handleIdentityConfirmationRequest(request)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(onComplete = {
+                Timber.d("Added Guid Selection Event")
+            })
+    }
+
+}
