2e7318e0962e940ac960579b797d9bc62e0fd736
Ridwan Farouki <ridwan@simprints.com>
2019-06-26 17:09:32 +0100

    Inserted Fingerprint OrchestratorActivity between platform and Launch
    Former-commit-id: 36ca48e214724924c79f2cdb8e15d2ed27806a07

--- fingerprint/src/main/java/com/simprints/fingerprint/activities/orchestrator/OrchestratorActivity.kt
+++ fingerprint/src/main/java/com/simprints/fingerprint/activities/orchestrator/OrchestratorActivity.kt
@@ -1,9 +1,13 @@
 package com.simprints.fingerprint.activities.orchestrator
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import com.simprints.fingerprint.data.domain.moduleapi.fingerprint.FingerprintToDomainRequest
 import com.simprints.fingerprint.di.koinModule
+import com.simprints.fingerprint.exceptions.unexpected.InvalidRequestForFingerprintException
+import com.simprints.moduleapi.fingerprint.requests.IFingerprintRequest
 import org.koin.android.viewmodel.ext.android.viewModel
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.unloadKoinModules
@@ -16,10 +20,25 @@ class OrchestratorActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         loadKoinModules(koinModule)
 
+        val iFingerprintRequest: IFingerprintRequest = this.intent.extras?.getParcelable(IFingerprintRequest.BUNDLE_KEY)
+            ?: throw InvalidRequestForFingerprintException()
+        viewModel.fingerprintRequest = FingerprintToDomainRequest.fromFingerprintToDomainRequest(iFingerprintRequest)
+
         viewModel.finishedResult.observe(this, Observer {
             setResult(it.resultCode, it.resultData)
             finish()
         })
+
+        viewModel.nextActivity.observe(this, Observer {
+            startActivityForResult(it.toIntent(this), it.resultCode)
+        })
+
+        viewModel.start()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        viewModel.finishedResult.postValue(FinishedResult(resultCode, data))
     }
 
     override fun onDestroy() {
--- fingerprint/src/main/java/com/simprints/fingerprint/activities/orchestrator/OrchestratorViewModel.kt
+++ fingerprint/src/main/java/com/simprints/fingerprint/activities/orchestrator/OrchestratorViewModel.kt
@@ -1,12 +1,37 @@
 package com.simprints.fingerprint.activities.orchestrator
 
+import android.content.Context
 import android.content.Intent
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.simprints.fingerprint.activities.launch.LaunchActivity
+import com.simprints.fingerprint.data.domain.moduleapi.fingerprint.requests.FingerprintRequest
 
 class OrchestratorViewModel : ViewModel() {
 
+    lateinit var fingerprintRequest: FingerprintRequest
+
+    val nextActivity = MutableLiveData<ActivityRequest>()
     val finishedResult = MutableLiveData<FinishedResult>()
 
-    data class FinishedResult(val resultCode: Int, val resultData: Intent?)
+    fun start() {
+        nextActivity.postValue(ActivityRequest.Launch(fingerprintRequest))
+    }
 }
+
+sealed class ActivityRequest(
+    private val fingerprintRequest: FingerprintRequest,
+    private val targetClass: Class<*>,
+    val resultCode: Int
+) {
+
+    class Launch(fingerprintRequest: FingerprintRequest) :
+        ActivityRequest(fingerprintRequest, LaunchActivity::class.java, 0)
+
+    fun toIntent(packageContext: Context) =
+        Intent(packageContext, targetClass).apply {
+            putExtra(FingerprintRequest.BUNDLE_KEY, fingerprintRequest)
+        }
+}
+
+data class FinishedResult(val resultCode: Int, val resultData: Intent?)
