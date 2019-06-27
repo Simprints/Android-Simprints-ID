b3b1d955b42ab37520bf3b96449fffcffd661dc9
Alan Camargo <alan.uk.br@gmail.com>
2019-06-27 17:02:06 +0100

    Implement CommCareActivityRobot
    Former-commit-id: eeb5780882f8daefe996fe3f2af35cb64711dd59

---
+++ clientapi/src/androidTest/java/com/simprints/clientapi/activities/robots/BaseAssertions.kt
@@ -0,0 +1,21 @@
+package com.simprints.clientapi.activities.robots
+
+import androidx.annotation.StringRes
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewInteraction
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import com.simprints.clientapi.extensions.inToast
+
+open class BaseAssertions {
+
+    fun assertToastMessageIs(@StringRes messageId: Int) {
+        onToast(messageId).check(matches(isDisplayed()))
+    }
+
+    private fun onToast(@StringRes messageId: Int): ViewInteraction {
+        return onView(withText(messageId)).inToast()
+    }
+
+}
