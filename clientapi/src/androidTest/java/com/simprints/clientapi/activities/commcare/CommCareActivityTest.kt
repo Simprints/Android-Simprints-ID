f74df89170160096e362212aad1ec1ea10b9c79e
Alan Camargo <alan.uk.br@gmail.com>
2019-06-28 14:16:54 +0100

    Finish CommCareActivityTest
    Former-commit-id: f73fa68289ba45e8ec1b8110c65bc74662741942

--- clientapi/src/androidTest/java/com/simprints/clientapi/activities/commcare/CommCareActivityTest.kt
+++ clientapi/src/androidTest/java/com/simprints/clientapi/activities/commcare/CommCareActivityTest.kt
@@ -1,14 +1,18 @@
 package com.simprints.clientapi.activities.commcare
 
 import android.content.Intent
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import com.simprints.clientapi.activities.BaseActivityTest
 import com.simprints.clientapi.activities.robots.commCare
 import com.simprints.id.domain.moduleapi.app.requests.AppIdentityConfirmationRequest
+import com.simprints.libsimprints.Constants.*
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.mockito.Mock
 
 @SmallTest
+@RunWith(AndroidJUnit4::class)
 class CommCareActivityTest : BaseActivityTest<CommCareActivity>(CommCareActivity::class) {
 
     @Mock
@@ -25,7 +29,9 @@ class CommCareActivityTest : BaseActivityTest<CommCareActivity>(CommCareActivity
     override fun intent(): Intent {
         return super.intent()
             .setAction(CommCarePresenter.ACTION_CONFIRM_IDENTITY)
-            .putExtra()
+            .putExtra(SIMPRINTS_PROJECT_ID, request.projectId)
+            .putExtra(SIMPRINTS_SESSION_ID, request.sessionId)
+            .putExtra(SIMPRINTS_SELECTED_GUID, request.selectedGuid)
     }
 
 }
