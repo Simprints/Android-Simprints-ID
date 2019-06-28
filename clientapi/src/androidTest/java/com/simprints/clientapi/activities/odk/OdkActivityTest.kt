4b73667837af80663916e00c3d9514da2ac2464e
Alan Camargo <alan.uk.br@gmail.com>
2019-06-28 14:17:09 +0100

    Implement OdkActivityTest
    Former-commit-id: 0185a4b55b6b6ccf3691ccc9388b82622e6409ae

--- clientapi/src/androidTest/java/com/simprints/clientapi/activities/odk/OdkActivityTest.kt
+++ clientapi/src/androidTest/java/com/simprints/clientapi/activities/odk/OdkActivityTest.kt
@@ -1,14 +1,37 @@
 package com.simprints.clientapi.activities.odk
 
+import android.content.Intent
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
+import com.simprints.clientapi.activities.BaseActivityTest
+import com.simprints.clientapi.activities.robots.odk
+import com.simprints.id.domain.moduleapi.app.requests.AppIdentityConfirmationRequest
+import com.simprints.libsimprints.Constants.*
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
 
 @SmallTest
-class OdkActivityTest {
+@RunWith(AndroidJUnit4::class)
+class OdkActivityTest : BaseActivityTest<OdkActivity>(OdkActivity::class) {
+
+    @Mock
+    private lateinit var request: AppIdentityConfirmationRequest
 
     @Test
     fun withConfirmIdentityIntent_shouldDisplayCorrectToastMessage() {
+        odk {
+        } assert {
+            resultSentIsDisplayedOnToast()
+        }
+    }
 
+    override fun intent(): Intent {
+        return super.intent()
+            .setAction(OdkPresenter.ACTION_CONFIRM_IDENTITY)
+            .putExtra(SIMPRINTS_PROJECT_ID, request.projectId)
+            .putExtra(SIMPRINTS_SESSION_ID, request.sessionId)
+            .putExtra(SIMPRINTS_SELECTED_GUID, request.selectedGuid)
     }
 
 }
