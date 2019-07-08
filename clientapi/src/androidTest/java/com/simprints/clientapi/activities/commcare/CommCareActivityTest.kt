bfbc789f3856c5e62a721db54ad2275e034ac8e3
Alan Camargo <alan@simprints.com>
2019-07-08 10:42:51 +0100

    Use DI in CommCare and Odk tests
    Former-commit-id: 6c848dc35733dc4e232d5eb179c629cc5070f6ed

--- clientapi/src/androidTest/java/com/simprints/clientapi/activities/commcare/CommCareActivityTest.kt
+++ clientapi/src/androidTest/java/com/simprints/clientapi/activities/commcare/CommCareActivityTest.kt
@@ -1,23 +1,54 @@
 package com.simprints.clientapi.activities.commcare
 
+import android.content.Context
 import android.content.Intent
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
+import androidx.test.platform.app.InstrumentationRegistry
 import com.simprints.clientapi.activities.robots.commCare
+import com.simprints.clientapi.identity.CommCareGuidSelectionNotifier
+import com.simprints.clientapi.identity.OdkGuidSelectionNotifier
 import com.simprints.libsimprints.Constants.*
 import com.simprints.testtools.android.BaseActivityTest
+import com.simprints.testtools.common.syntax.verifyOnce
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.startKoin
+import org.koin.core.parameter.parametersOf
+import org.koin.dsl.module
+import org.koin.test.KoinTest
+import org.koin.test.inject
+import org.koin.test.mock.declareMock
 
 @SmallTest
 @RunWith(AndroidJUnit4::class)
-class CommCareActivityTest : BaseActivityTest<CommCareActivity>(CommCareActivity::class) {
+class CommCareActivityTest : BaseActivityTest<CommCareActivity>(CommCareActivity::class), KoinTest {
+
+    private val mod = module(override = true) {
+        factory { (context: Context) ->
+            CommCareGuidSelectionNotifier(context)
+        }
+    }
+
+    private val guidSelectionNotifier by inject<OdkGuidSelectionNotifier> {
+        parametersOf(InstrumentationRegistry.getInstrumentation().targetContext)
+    }
+
+    @Before
+    override fun setUp() {
+        super.setUp()
+        startKoin { listOf(mod) }
+        declareMock<CommCareGuidSelectionNotifier>()
+    }
 
     @Test
     fun withConfirmIdentityIntent_shouldDisplayCorrectToastMessage() {
         commCare {
         } assert {
-            dataSentIsDisplayedOnToast()
+            verifyOnce(guidSelectionNotifier) {
+                showMessage()
+            }
         }
     }
 
