248f53471f841181e9be78ba0fb8b4aa29cee9cb
Alan Camargo <alan.uk.br@gmail.com>
2019-07-15 14:47:41 +0100

    Add callback from ID to Client API
    Former-commit-id: 201c8be726548cdaafb867b544c04d7899eadd28

--- id/src/main/java/com/simprints/id/activities/guidselection/GuidSelectionActivity.kt
+++ id/src/main/java/com/simprints/id/activities/guidselection/GuidSelectionActivity.kt
@@ -1,5 +1,7 @@
 package com.simprints.id.activities.guidselection
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.work.Data
@@ -13,12 +15,18 @@ class GuidSelectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         scheduleGuidSelection()
-        finish()
+        sendOkResult()
     }
 
     private fun scheduleGuidSelection() {
         val guidSelectionWork = buildGuidSelectionWork()
         WorkManager.getInstance().enqueue(guidSelectionWork)
+    }
+
+    private fun sendOkResult() {
+        val data = Intent().putExtra(IDENTIFICATION_OUTCOME_KEY, true)
+        setResult(Activity.RESULT_OK, data)
+        finish()
     }
 
     private fun buildGuidSelectionWork() = OneTimeWorkRequestBuilder<GuidSelectionWorker>()
@@ -27,6 +35,10 @@ class GuidSelectionActivity : AppCompatActivity() {
 
     private fun prepareInputData() = intent.parseAppConfirmation().toMap().let {
         Data.Builder().putAll(it).build()
+    }
+
+    companion object {
+        private const val IDENTIFICATION_OUTCOME_KEY = "identification_outcome"
     }
 
 }
