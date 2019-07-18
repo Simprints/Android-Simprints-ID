47f9750703e26561266652b0dfcc918e656740a3
Alan Camargo <alan.uk.br@gmail.com>
2019-07-18 13:29:18 +0100

    Implement unit tests
    Former-commit-id: 5110e0b7dbc8a4c9aab6707b219629ba6b3f2383

--- id/src/main/java/com/simprints/id/data/analytics/eventdata/models/domain/events/ConfirmationEvent.kt
+++
@@ -1,9 +0,0 @@
-package com.simprints.id.data.analytics.eventdata.models.domain.events
-
-import androidx.annotation.Keep
-
-@Keep
-class ConfirmationEvent(
-    startTime: Long,
-    val identificationOutcome: Boolean
-) : Event(EventType.CALLBACK_CONFIRMATION, startTime)
