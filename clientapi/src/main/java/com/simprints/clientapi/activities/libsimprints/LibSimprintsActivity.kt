6106db8b19752d6d351ce4efd5399104756e5aac
Alan Camargo <alan.uk.br@gmail.com>
2019-07-24 13:02:46 +0100

    Add identificationOutcome to libsimprints
    Former-commit-id: 525aa17478e07f91310103a2e4fa31a6e15413c5

--- clientapi/src/main/java/com/simprints/clientapi/activities/libsimprints/LibSimprintsActivity.kt
+++ clientapi/src/main/java/com/simprints/clientapi/activities/libsimprints/LibSimprintsActivity.kt
@@ -52,10 +52,10 @@ class LibSimprintsActivity : RequestActivity(), LibSimprintsContract.View {
     }
 
     /**
-     * TODO: LibSimprints and the development documentation need to be updated including this return
+     * TODO: The development documentation needs to be updated including this return
      */
     override fun returnConfirmation(identificationOutcome: Boolean) = Intent().let {
-        it.putExtra(IDENTIFICATION_OUTCOME_KEY, identificationOutcome)
+        it.putExtra(Constants.SIMPRINTS_IDENTIFICATION_OUTCOME, identificationOutcome)
         sendOkResult(it)
     }
 
@@ -67,10 +67,6 @@ class LibSimprintsActivity : RequestActivity(), LibSimprintsContract.View {
     override fun onDestroy() {
         super.onDestroy()
         unloadClientApiKoinModules()
-    }
-
-    companion object {
-        private const val IDENTIFICATION_OUTCOME_KEY = "identificationOutcome"
     }
 
 }
