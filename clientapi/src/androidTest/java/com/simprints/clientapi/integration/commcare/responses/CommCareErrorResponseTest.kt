329142564839b413c7dcad8b328ec035bfd283fb
Aashay Gaikwad <aashay@simprints.com>
2019-07-24 11:12:32 +0100

    added test for LOGIN_NOT_COMPLETE in clientApi
    Former-commit-id: a2cc3b30d86d2f817b8302825902f76615b60506

--- clientapi/src/androidTest/java/com/simprints/clientapi/integration/commcare/responses/CommCareErrorResponseTest.kt
+++ clientapi/src/androidTest/java/com/simprints/clientapi/integration/commcare/responses/CommCareErrorResponseTest.kt
@@ -6,7 +6,6 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
 import com.simprints.clientapi.activities.commcare.CommCareActivity
 import com.simprints.clientapi.integration.AppErrorResponse
-import com.simprints.clientapi.integration.BaseClientApiTest
 import com.simprints.clientapi.integration.commcare.BaseCommCareClientApiTest
 import com.simprints.moduleapi.app.responses.IAppErrorReason
 import org.junit.Test
@@ -23,14 +22,25 @@ class CommCareErrorResponseTest : BaseCommCareClientApiTest() {
         val scenario =
             ActivityScenario.launch<CommCareActivity>(commCareBaseIntentRequest.apply { action = COMMCARE_ENROL_ACTION })
 
-        assertCommCareErrorResponse(scenario)
+        assertCommCareErrorResponse(scenario, SKIP_CHECK_VALUE_FOR_COMPLETED_FLOW)
     }
 
-    private fun assertCommCareErrorResponse(scenario: ActivityScenario<CommCareActivity>) {
+    @Test
+    fun appModuleSendsAnErrorAppResponseAsLoginNotComplete_shouldReturnACommCareErrorResponse() {
+        val appErrorResponse = AppErrorResponse(IAppErrorReason.LOGIN_NOT_COMPLETE)
+        mockAppModuleResponse(appErrorResponse, APP_ENROL_ACTION)
+
+        val scenario =
+            ActivityScenario.launch<CommCareActivity>(commCareBaseIntentRequest.apply { action = COMMCARE_ENROL_ACTION })
+
+        assertCommCareErrorResponse(scenario, SKIP_CHECK_VALUE_FOR_NOT_COMPLETED_FLOW)
+    }
+
+    private fun assertCommCareErrorResponse(scenario: ActivityScenario<CommCareActivity>, expectedSkipCheck: Boolean) {
         val result = scenario.result
         assertThat(result.resultCode).isEqualTo(Activity.RESULT_OK)
         result.resultData.extras?.getBundle(COMMCARE_BUNDLE_KEY)?.let {
-            assertThat(it.getString(SKIP_CHECK_KEY)).isEqualTo(BaseClientApiTest.SKIP_CHECK_VALUE_FOR_COMPLETED_FLOW.toString())
+            assertThat(it.getString(SKIP_CHECK_KEY)).isEqualTo(expectedSkipCheck.toString())
         } ?: throw Exception("No bundle found")
     }
 }
