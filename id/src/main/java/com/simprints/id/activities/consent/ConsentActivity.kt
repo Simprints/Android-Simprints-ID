3781a2a28e1c3f7f37c90a9234d794291bf44b72
Aashay Gaikwad <aashay@simprints.com>
2019-08-14 11:01:21 +0100

    Added consent from fingerprint
    Former-commit-id: 42af078bf7827790cf33557448c5af49b5998f76

---
+++ id/src/main/java/com/simprints/id/activities/consent/ConsentActivity.kt
@@ -0,0 +1,13 @@
+package com.simprints.id.activities.consent
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.simprints.id.R
+
+class ConsentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consent)
+    }
+}
