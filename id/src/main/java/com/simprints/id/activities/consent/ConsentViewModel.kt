302d17fdf90ff18a10d6b42c811fdd977bf7ee70
Aashay Gaikwad <aashay@simprints.com>
2019-08-16 11:09:01 +0100

    Added event on consent click
    Former-commit-id: 227318ada0cc1613e453d0bf37be5c772330eab1

--- id/src/main/java/com/simprints/id/activities/consent/ConsentViewModel.kt
+++ id/src/main/java/com/simprints/id/activities/consent/ConsentViewModel.kt
@@ -5,19 +5,32 @@ import androidx.lifecycle.ViewModel
 import com.google.gson.JsonSyntaxException
 import com.simprints.core.tools.json.JsonHelper
 import com.simprints.id.data.analytics.crashreport.CrashReportManager
+import com.simprints.id.data.analytics.eventdata.controllers.domain.SessionEventsManager
+import com.simprints.id.data.analytics.eventdata.models.domain.events.ConsentEvent
+import com.simprints.id.data.analytics.eventdata.models.domain.events.ConsentEvent.Result.ACCEPTED
+import com.simprints.id.data.analytics.eventdata.models.domain.events.ConsentEvent.Result.DECLINED
+import com.simprints.id.data.analytics.eventdata.models.domain.events.ConsentEvent.Type.INDIVIDUAL
+import com.simprints.id.data.analytics.eventdata.models.domain.events.ConsentEvent.Type.PARENTAL
 import com.simprints.id.data.consent.shortconsent.*
+import com.simprints.id.tools.TimeHelper
 
 class ConsentViewModel(private val consentDataManager: ConsentDataManager,
                        private val crashReportManager: CrashReportManager,
+                       private val sessionEventsManager: SessionEventsManager,
                        private val programName: String,
-                       private val organizationName: String) : ViewModel() {
+                       private val organizationName: String,
+                       private val timeHelper: TimeHelper) : ViewModel() {
 
     val generalConsentData = MutableLiveData<GeneralConsentData>()
     val parentalConsentData = MutableLiveData<ParentalConsentData>()
+    var isConsentTabGeneral = true
+    private var startConsentEventTime: Long = 0
+    var consentAcceptClick = MutableLiveData<Boolean>()
 
-    fun start() {
+    init {
         generalConsentData.postValue(getGeneralConsentData())
         parentalConsentData.postValue(getParentalConsentData())
+        startConsentEventTime = timeHelper.now()
     }
 
     private fun getGeneralConsentData() =
@@ -44,10 +57,25 @@ class ConsentViewModel(private val consentDataManager: ConsentDataManager,
     }
 
     fun handleConsentAcceptClick() {
-
+        addConsentEvent(ACCEPTED)
+        consentAcceptClick.postValue(true)
     }
 
     fun handleConsentDeclineClick() {
+        addConsentEvent(DECLINED)
+        consentAcceptClick.postValue(false)
+    }
 
+    private fun addConsentEvent(result: ConsentEvent.Result) {
+        sessionEventsManager.addEventInBackground(
+            ConsentEvent(
+                startConsentEventTime,
+                timeHelper.now(),
+                if (isConsentTabGeneral) {
+                    INDIVIDUAL
+                } else {
+                    PARENTAL
+                },
+                result))
     }
 }
