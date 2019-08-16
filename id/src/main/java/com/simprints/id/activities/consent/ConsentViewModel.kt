42980453f6ebb4cf6124da1488c83005e9f5e20a
Aashay Gaikwad <aashay@simprints.com>
2019-08-16 16:13:07 +0100

    Added text manager repo and removed view -> viewmodel interactions
    Former-commit-id: 10e4e43da2d5dc5faebccd010cfa3787eb0daf23

--- id/src/main/java/com/simprints/id/activities/consent/ConsentViewModel.kt
+++ id/src/main/java/com/simprints/id/activities/consent/ConsentViewModel.kt
@@ -1,81 +1,43 @@
 package com.simprints.id.activities.consent
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
-import com.google.gson.JsonSyntaxException
-import com.simprints.core.tools.json.JsonHelper
-import com.simprints.id.data.analytics.crashreport.CrashReportManager
 import com.simprints.id.data.analytics.eventdata.controllers.domain.SessionEventsManager
 import com.simprints.id.data.analytics.eventdata.models.domain.events.ConsentEvent
-import com.simprints.id.data.analytics.eventdata.models.domain.events.ConsentEvent.Result.ACCEPTED
-import com.simprints.id.data.analytics.eventdata.models.domain.events.ConsentEvent.Result.DECLINED
-import com.simprints.id.data.analytics.eventdata.models.domain.events.ConsentEvent.Type.INDIVIDUAL
-import com.simprints.id.data.analytics.eventdata.models.domain.events.ConsentEvent.Type.PARENTAL
-import com.simprints.id.data.consent.shortconsent.*
-import com.simprints.id.tools.TimeHelper
+import com.simprints.id.data.consent.shortconsent.ConsentTextManager
+import com.simprints.id.domain.moduleapi.app.requests.AppRequest
 
-class ConsentViewModel(private val consentDataManager: ConsentDataManager,
-                       private val crashReportManager: CrashReportManager,
+class ConsentViewModel(private val consentTextManager: ConsentTextManager,
                        private val sessionEventsManager: SessionEventsManager,
-                       private val programName: String,
-                       private val organizationName: String,
-                       private val timeHelper: TimeHelper) : ViewModel() {
+                       private val consentEvent: LiveData<ConsentEvent>) : ViewModel() {
 
-    val generalConsentData = MutableLiveData<GeneralConsentData>()
-    val parentalConsentData = MutableLiveData<ParentalConsentData>()
+    val appRequest = MutableLiveData<AppRequest>()
+
+    var generalConsentText: LiveData<String>
+
+    var parentalConsentText: LiveData<String>
+
+    val  parentalConsentExists = consentTextManager.parentalConsentExists()
+
     var isConsentTabGeneral = true
-    private var startConsentEventTime: Long = 0
-    var consentAcceptClick = MutableLiveData<Boolean>()
 
     init {
-        generalConsentData.postValue(getGeneralConsentData())
-        parentalConsentData.postValue(getParentalConsentData())
-        startConsentEventTime = timeHelper.now()
+        generalConsentText = Transformations.switchMap(appRequest) {
+            consentTextManager.getGeneralConsentText(it)
+        }
+
+        parentalConsentText = Transformations.switchMap(appRequest) {
+            consentTextManager.getParentalConsentText(it)
+        }
+
+        Transformations.map(consentEvent) {
+            addConsentEvent(it)
+        }
     }
 
-    private fun getGeneralConsentData() =
-        GeneralConsentData(getGeneralConsentOptions(),
-            programName, organizationName)
-
-    private fun getGeneralConsentOptions() = try {
-        JsonHelper.gson.fromJson(consentDataManager.generalConsentOptionsJson, GeneralConsentOptions::class.java)
-    } catch (e: JsonSyntaxException) {
-        crashReportManager.logExceptionOrSafeException(Exception("Malformed General Consent Text Error", e))
-        GeneralConsentOptions()
-    }
-
-    private fun getParentalConsentData() =
-        ParentalConsentData(consentDataManager.parentalConsentExists,
-            getParentalConsentOptions(),
-            programName, organizationName)
-
-    private fun getParentalConsentOptions() = try {
-        JsonHelper.gson.fromJson(consentDataManager.parentalConsentOptionsJson, ParentalConsentOptions::class.java)
-    } catch (e: JsonSyntaxException) {
-        crashReportManager.logExceptionOrSafeException(Exception("Malformed Parental Consent Text Error", e))
-        ParentalConsentOptions()
-    }
-
-    fun handleConsentAcceptClick() {
-        addConsentEvent(ACCEPTED)
-        consentAcceptClick.postValue(true)
-    }
-
-    fun handleConsentDeclineClick() {
-        addConsentEvent(DECLINED)
-        consentAcceptClick.postValue(false)
-    }
-
-    private fun addConsentEvent(result: ConsentEvent.Result) {
-        sessionEventsManager.addEventInBackground(
-            ConsentEvent(
-                startConsentEventTime,
-                timeHelper.now(),
-                if (isConsentTabGeneral) {
-                    INDIVIDUAL
-                } else {
-                    PARENTAL
-                },
-                result))
+    private fun addConsentEvent(consentEvent: ConsentEvent) {
+        sessionEventsManager.addEventInBackground(consentEvent)
     }
 }
--- id/src/main/java/com/simprints/id/activities/consent/ConsentViewModelFactory.kt
+++ id/src/main/java/com/simprints/id/activities/consent/ConsentViewModelFactory.kt
@@ -1,27 +1,21 @@
 package com.simprints.id.activities.consent
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.simprints.id.data.analytics.crashreport.CrashReportManager
 import com.simprints.id.data.analytics.eventdata.controllers.domain.SessionEventsManager
-import com.simprints.id.data.consent.shortconsent.ConsentDataManager
-import com.simprints.id.data.prefs.PreferencesManager
-import com.simprints.id.tools.TimeHelper
+import com.simprints.id.data.analytics.eventdata.models.domain.events.ConsentEvent
+import com.simprints.id.data.consent.shortconsent.ConsentTextManager
 
-class ConsentViewModelFactory(private val consentDataManager: ConsentDataManager,
-                              private val crashReportManager: CrashReportManager,
-                              private val preferencesManager: PreferencesManager,
-                              private val sessionEventsManager: SessionEventsManager,
-                              private val timeHelper: TimeHelper) : ViewModelProvider.Factory {
+class ConsentViewModelFactory(private val consentTextManager: ConsentTextManager,
+                              private val sessionEventsManager: SessionEventsManager) : ViewModelProvider.Factory {
+
+    lateinit var consentEvents: MutableLiveData<ConsentEvent>
+
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return if (modelClass.isAssignableFrom(ConsentViewModel::class.java)) {
-            ConsentViewModel(consentDataManager,
-                crashReportManager,
-                sessionEventsManager,
-                preferencesManager.programName,
-                preferencesManager.organizationName,
-                timeHelper) as T
+            ConsentViewModel(consentTextManager, sessionEventsManager, consentEvents) as T
         } else {
             throw IllegalArgumentException("ViewModel Not Found")
         }
