1a96a19664b5b7667c121a06159851f6902a0c82
Aashay Gaikwad <aashay@simprints.com>
2019-08-26 14:12:26 +0100

    Fixed translations and updated ViewModel
    Former-commit-id: 1a47a3b6308fd5bf05da34614c7b4213e97928dc

--- id/src/main/java/com/simprints/id/activities/consent/ConsentViewModelFactory.kt
+++ id/src/main/java/com/simprints/id/activities/consent/ConsentViewModelFactory.kt
@@ -1,21 +1,17 @@
 package com.simprints.id.activities.consent
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.simprints.id.data.analytics.eventdata.controllers.domain.SessionEventsManager
-import com.simprints.id.data.analytics.eventdata.models.domain.events.ConsentEvent
 import com.simprints.id.data.consent.shortconsent.ConsentTextManager
 
 class ConsentViewModelFactory(private val consentTextManager: ConsentTextManager,
                               private val sessionEventsManager: SessionEventsManager) : ViewModelProvider.Factory {
 
-    lateinit var consentEvents: MutableLiveData<ConsentEvent>
-
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return if (modelClass.isAssignableFrom(ConsentViewModel::class.java)) {
-            ConsentViewModel(consentTextManager, sessionEventsManager, consentEvents) as T
+            ConsentViewModel(consentTextManager, sessionEventsManager) as T
         } else {
             throw IllegalArgumentException("ViewModel Not Found")
         }
