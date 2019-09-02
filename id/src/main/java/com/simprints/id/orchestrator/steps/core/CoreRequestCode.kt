5e10c76a9633d549d0d380017f90b2f81f739ce5
Aashay Gaikwad <aashay@simprints.com>
2019-09-02 11:47:57 +0100

    Processing resilt in ModalityFlowBase
    Former-commit-id: ae58bb177bdb1927a6d26ca3d6e7b78936d68234

--- id/src/main/java/com/simprints/id/orchestrator/steps/core/CoreRequestCode.kt
+++ id/src/main/java/com/simprints/id/orchestrator/steps/core/CoreRequestCode.kt
@@ -4,5 +4,10 @@ private const val CORE_REQUEST_CODE = 300
 
 enum class CoreRequestCode(val value: Int) {
     CONSENT(CORE_REQUEST_CODE + 1),
-    VERIFICATION_CHECK(CORE_REQUEST_CODE + 2)
+    VERIFICATION_CHECK(CORE_REQUEST_CODE + 2);
+
+    companion object {
+        fun isCoreResult(requestCode: Int) =
+            CoreRequestCode.values().map { it.value }.contains(requestCode)
+    }
 }
