4bac5fa28a53299a59be9b7e97946ecc3dbd7b2c
Alan Camargo <alan@simprints.com>
2019-09-05 20:05:12 +0100

    Implementing custom JSON parsers
    Former-commit-id: 7dbfe168e0f929012b8ccb230af26790ea45dccc

--- id/src/main/java/com/simprints/id/domain/moduleapi/fingerprint/requests/FingerprintEnrolRequest.kt
+++ id/src/main/java/com/simprints/id/domain/moduleapi/fingerprint/requests/FingerprintEnrolRequest.kt
@@ -1,5 +1,6 @@
 package com.simprints.id.domain.moduleapi.fingerprint.requests
 
+import com.google.gson.stream.JsonReader
 import com.simprints.id.domain.moduleapi.fingerprint.requests.entities.FingerprintFingerIdentifier
 import kotlinx.android.parcel.Parcelize
 
@@ -12,4 +13,64 @@ data class FingerprintEnrolRequest(val projectId: String,
                                    val fingerStatus: Map<FingerprintFingerIdentifier, Boolean>,
                                    val logoExists: Boolean,
                                    val programName: String,
-                                   val organizationName: String): FingerprintRequest()
+                                   val organizationName: String): FingerprintRequest() {
+
+    companion object {
+        private const val FIELD_PROJECT_ID = "projectId"
+        private const val FIELD_USER_ID = "userId"
+        private const val FIELD_MODULE_ID = "moduleId"
+        private const val FIELD_METADATA = "metadata"
+        private const val FIELD_LANGUAGE = "language"
+        private const val FIELD_FINGER_STATUS = "fingerStatus"
+        private const val FIELD_LOGO_EXISTS = "logoExists"
+        private const val FIELD_PROGRAMME_NAME = "programName"
+        private const val FIELD_ORGANISATION_NAME = "organizationName"
+
+        fun tryParse(jsonReader: JsonReader): FingerprintEnrolRequest? {
+            var request: FingerprintEnrolRequest? = null
+
+
+            with(jsonReader) {
+                beginObject()
+
+                while (hasNext()) {
+                    val elementName = nextName()
+                    if (FingerprintEnrolRequest::class.java.fields.none { it.name == elementName }) {
+                        break
+                    } else {
+                        var projectId = ""
+                        var userId = ""
+                        var moduleId = ""
+                        var metadata = ""
+                        var language = ""
+                        var fingerStatus = mapOf<FingerprintFingerIdentifier, Boolean>()
+                        var logoExists = false
+                        var programmeName = ""
+                        var organisationName = ""
+
+                        when (elementName) {
+                            FIELD_PROJECT_ID -> projectId = nextString()
+                            FIELD_USER_ID -> userId = nextString()
+                            FIELD_MODULE_ID -> moduleId = nextString()
+                            FIELD_METADATA -> metadata = nextString()
+                            FIELD_LANGUAGE -> language = nextString()
+                            FIELD_FINGER_STATUS -> fingerStatus = TODO()
+                        }
+
+
+
+                        if (fieldsAreValid) {
+                            request = FingerprintEnrolRequest()
+                            break
+                        }
+                    }
+                }
+
+                endObject()
+            }
+
+            return request
+        }
+    }
+
+}
