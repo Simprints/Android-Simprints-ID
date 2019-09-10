71c6f8857ca7f896f709fea538cf00372badcb32
Aashay Gaikwad <aashay@simprints.com>
2019-09-10 10:02:41 +0530

    Refactoring: Removed AppRequest LiveData
    Former-commit-id: 08425e54f36f49020d47801996b117947a2fee47

--- id/src/main/java/com/simprints/id/activities/consent/ConsentViewModel.kt
+++ id/src/main/java/com/simprints/id/activities/consent/ConsentViewModel.kt
@@ -1,31 +1,19 @@
 package com.simprints.id.activities.consent
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.simprints.id.data.analytics.eventdata.controllers.domain.SessionEventsManager
 import com.simprints.id.data.analytics.eventdata.models.domain.events.ConsentEvent
 import com.simprints.id.data.consent.shortconsent.ConsentTextManager
 import com.simprints.id.domain.moduleapi.app.requests.AppRequest
 
-class ConsentViewModel(private val consentTextManager: ConsentTextManager,
+class ConsentViewModel(appRequest: AppRequest,
+                       consentTextManager: ConsentTextManager,
                        private val sessionEventsManager: SessionEventsManager) : ViewModel() {
 
-    val appRequest by lazy {  MutableLiveData<AppRequest>() }
-    var generalConsentText: LiveData<String>
-    var parentalConsentText: LiveData<String>
+    var generalConsentText: LiveData<String> = consentTextManager.getGeneralConsentText(appRequest)
+    var parentalConsentText: LiveData<String> = consentTextManager.getParentalConsentText(appRequest)
     val  parentalConsentExists = consentTextManager.parentalConsentExists()
-
-    init {
-        generalConsentText = Transformations.switchMap(appRequest) {
-            consentTextManager.getGeneralConsentText(it)
-        }
-
-        parentalConsentText = Transformations.switchMap(appRequest) {
-            consentTextManager.getParentalConsentText(it)
-        }
-    }
 
     fun addConsentEvent(consentEvent: ConsentEvent) {
         sessionEventsManager.addEventInBackground(consentEvent)
