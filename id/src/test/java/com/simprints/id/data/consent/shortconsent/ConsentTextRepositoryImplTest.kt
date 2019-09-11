0a80133d6661752d09251615edd8fb35128e361b
Aashay Gaikwad <aashay@simprints.com>
2019-09-11 09:54:50 +0530

    Setting language in consent repo and fixed tests
    Former-commit-id: dd59b181808dae3bebb4e7acd966aa920671608b

--- id/src/test/java/com/simprints/id/data/consent/shortconsent/ConsentTextRepositoryImplTest.kt
+++ id/src/test/java/com/simprints/id/data/consent/shortconsent/ConsentTextRepositoryImplTest.kt
@@ -16,7 +16,7 @@ import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
 @Config(application = TestApplication::class, shadows = [ShadowAndroidXMultiDex::class])
-class ConsentTextManagerImplTest {
+class ConsentTextRepositoryImplTest {
 
     val context = ApplicationProvider.getApplicationContext() as TestApplication
     companion object {
@@ -24,22 +24,20 @@ class ConsentTextManagerImplTest {
         private  const val PARENTAL_CONSENT_ENROL_TEXT = "I'd like to use your child's fingerprints to enrol them in program_name and identify them in the future. Simprints, a UK-based nonprofit, will have access to their fingerprint information and current location. If you accept, you may withdraw your permission at any time and ask for your child's data to be erased. May I use your child's fingerprints? Please say \"I accept\", \"I decline\", or \"I have questions.\""
         private const val PROGRAM_NAME = "program_name"
         private const val ORGANIZATION_NAME = "organization_name"
-        private const val PROJECT_ID = "project_id"
-        private const val USER_ID = "user_id"
-        private const val MODULE_ID = "module_id"
+        private const val LANGUAGE = "en"
     }
 
     @Test
     fun getGeneralConsentText_shouldReturnCorrectGeneralConsentText() {
 
-        val consentDataManagerMock = mock<ConsentLocalDataSource>().apply {
+        val consentDataSourceMock = mock<ConsentLocalDataSource>().apply {
             whenever(this) { generalConsentOptionsJson } thenReturn JsonHelper.toJson(GeneralConsentOptions())
         }
-        val consentTextManager = ConsentRepositoryImpl(context,
-            consentDataManagerMock, mock(), PROGRAM_NAME, ORGANIZATION_NAME)
+        val consentTextRepo = ConsentRepositoryImpl(context,
+            consentDataSourceMock, mock(), PROGRAM_NAME, ORGANIZATION_NAME, LANGUAGE)
 
         val generalConsentText =
-            consentTextManager.getGeneralConsentText(AskConsentRequest(ConsentType.ENROL)).value
+            consentTextRepo.getGeneralConsentText(AskConsentRequest(ConsentType.ENROL)).value
 
         assertThat(generalConsentText).isEqualTo(GENERAL_CONSENT_ENROL_TEXT)
     }
@@ -47,27 +45,27 @@ class ConsentTextManagerImplTest {
     @Test
     fun getParentalConsentText_shouldReturnCorrectParentalConsentText() {
 
-        val consentDataManagerMock = mock<ConsentLocalDataSource>().apply {
+        val consentDataSourceMock = mock<ConsentLocalDataSource>().apply {
             whenever(this) { parentalConsentOptionsJson } thenReturn JsonHelper.toJson(ParentalConsentOptions())
         }
-        val consentTextManager = ConsentRepositoryImpl(context,
-            consentDataManagerMock, mock(), PROGRAM_NAME, ORGANIZATION_NAME)
+        val consentTextRepo = ConsentRepositoryImpl(context,
+            consentDataSourceMock, mock(), PROGRAM_NAME, ORGANIZATION_NAME, LANGUAGE)
 
         val parentalConsentText =
-            consentTextManager.getParentalConsentText(AskConsentRequest(ConsentType.ENROL)).value
+            consentTextRepo.getParentalConsentText(AskConsentRequest(ConsentType.ENROL)).value
 
         assertThat(parentalConsentText).isEqualTo(PARENTAL_CONSENT_ENROL_TEXT)
     }
 
     @Test
     fun parentalConsentExists_shouldReturnValueFromConsentDataManager() {
-        val consentDataManagerMock = mock<ConsentLocalDataSource>().apply {
+        val consentDataSourceMock = mock<ConsentLocalDataSource>().apply {
             whenever(this) { parentalConsentExists } thenReturn true
         }
-        val consentTextManager = ConsentRepositoryImpl(context,
-            consentDataManagerMock, mock(), PROGRAM_NAME, ORGANIZATION_NAME)
+        val consentTextRepo = ConsentRepositoryImpl(context,
+            consentDataSourceMock, mock(), PROGRAM_NAME, ORGANIZATION_NAME, LANGUAGE)
 
-        val parentalConsentExists = consentTextManager.parentalConsentExists().value
+        val parentalConsentExists = consentTextRepo.parentalConsentExists().value
 
         assertThat(parentalConsentExists).isEqualTo(true)
     }
