b34bdcb8d5e8c643c921e6945b0b3fa4193b4ad8
Alan Camargo <alan@simprints.com>
2019-09-11 09:55:57 +0100

    Decouple encoding from hot cache
    Former-commit-id: 9a1821145a39f1088a853e9ed700ea1d9e8a65f6

--- id/src/main/java/com/simprints/id/orchestrator/cache/HotCacheImpl.kt
+++ id/src/main/java/com/simprints/id/orchestrator/cache/HotCacheImpl.kt
@@ -1,45 +1,26 @@
 package com.simprints.id.orchestrator.cache
 
 import android.content.SharedPreferences
-import com.simprints.id.data.secure.keystore.KeystoreManager
 import com.simprints.id.orchestrator.steps.Step
-import com.simprints.id.tools.ParcelableConverter
 
 class HotCacheImpl(private val preferences: SharedPreferences,
-                   private val keystoreManager: KeystoreManager) : HotCache {
+                   private val stepEncoder: StepEncoder) : HotCache {
 
     private val steps = preferences.getStringSet(KEY_STEPS, null)?.toMutableSet()
 
     override fun save(step: Step?) {
         step?.let {
-            encrypt(it).also { encryptedStep ->
-                steps?.add(encryptedStep)
+            stepEncoder.encode(it).also { encodedStep ->
+                steps?.add(encodedStep)
                 preferences.edit()?.putStringSet(KEY_STEPS, steps)?.apply()
             }
         }
     }
 
-    override fun load() = steps?.mapNotNull { decrypt(it) }
+    override fun load() = steps?.mapNotNull(stepEncoder::decode)
 
     override fun clear() {
         preferences.edit().putStringSet(KEY_STEPS, null).apply()
-    }
-
-    private fun encrypt(step: Step): String {
-        val converter = ParcelableConverter(step)
-        val string = String(converter.toBytes())
-        converter.recycle()
-        return keystoreManager.encryptString(string)
-    }
-
-    private fun decrypt(encryptedStep: String?): Step? {
-        return encryptedStep?.let {
-            val bytes = keystoreManager.decryptString(it).toByteArray()
-            val converter = ParcelableConverter(bytes)
-            val parcel = converter.getParcel()
-            converter.recycle()
-            Step.createFromParcel(parcel)
-        }
     }
 
     private companion object {
