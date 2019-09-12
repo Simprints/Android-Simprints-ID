4cf5b21c0bbb84f95bf29784ac0912b6cd647cbe
Alan Camargo <alan@simprints.com>
2019-09-12 16:08:26 +0100

    Fix step encoding
    Former-commit-id: 49bedf70a16d1930845c9aac8eb81bb2f70bbe3d

--- id/src/test/java/com/simprints/id/tools/ParcelableConverterTest.kt
+++ id/src/test/java/com/simprints/id/tools/ParcelableConverterTest.kt
@@ -3,9 +3,9 @@ package com.simprints.id.tools
 import android.os.Parcelable
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.simprints.id.domain.moduleapi.fingerprint.requests.FingerprintEnrolRequest
+import com.simprints.id.orchestrator.steps.Step
 import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.greaterThan
-import org.hamcrest.Matchers.notNullValue
+import org.hamcrest.Matchers.*
 import org.junit.After
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,9 +18,7 @@ class ParcelableConverterTest {
 
     @Test
     fun withParcelableInput_shouldConvertToByteArray() {
-        converter = ParcelableConverter(getParcelable())
-        val bytes = converter.toBytes()
-        converter.recycle()
+        val bytes = prepareByteArray()
 
         assertThat(bytes, notNullValue())
         assertThat(bytes.size, greaterThan(0))
@@ -31,9 +29,15 @@ class ParcelableConverterTest {
         val bytes = prepareByteArray()
         converter = ParcelableConverter(bytes)
         val parcel = converter.getParcel()
+        val step = Step.createFromParcel(parcel)
         converter.recycle()
 
         assertThat(parcel, notNullValue())
+        assertThat(step.requestCode, `is`(REQUEST_CODE))
+        assertThat(step.activityName, `is`(ACTIVITY_NAME))
+        assertThat(step.bundleKey, `is`(BUNDLE_KEY))
+        assertThat(step.request, `is`(request))
+        assertThat(step.status, `is`(Step.Status.ONGOING))
     }
 
     @After
@@ -42,7 +46,28 @@ class ParcelableConverterTest {
     }
 
     private fun getParcelable(): Parcelable {
-        return FingerprintEnrolRequest(
+        return Step(
+            REQUEST_CODE,
+            ACTIVITY_NAME,
+            BUNDLE_KEY,
+            request,
+            Step.Status.ONGOING
+        )
+    }
+
+    private fun prepareByteArray(): ByteArray {
+        converter = ParcelableConverter(getParcelable())
+        val bytes = converter.toBytes()
+        converter.recycle()
+        return bytes
+    }
+
+    companion object {
+        private const val REQUEST_CODE = 123
+        private const val ACTIVITY_NAME = "com.simprints.id.MyActivity"
+        private const val BUNDLE_KEY = "BUNDLE_KEY"
+
+        private val request: Step.Request = FingerprintEnrolRequest(
             "projectId",
             "userId",
             "moduleId",
@@ -53,13 +78,6 @@ class ParcelableConverterTest {
             "programmeName",
             "organisationName"
         )
-    }
-
-    private fun prepareByteArray(): ByteArray {
-        converter = ParcelableConverter(getParcelable())
-        val bytes = converter.toBytes()
-        converter.recycle()
-        return bytes
     }
 
 }
