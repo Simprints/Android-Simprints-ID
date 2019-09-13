9b7ced436c71cd10d9b4704da4e8e681de9bf2e3
Alan Camargo <alan@simprints.com>
2019-09-13 12:49:56 +0100

    Fix FaceCaptureResponseProcessor
    Former-commit-id: 3fac81da5161eb67436f9332014b6f5c587ec74c

--- id/src/main/java/com/simprints/id/orchestrator/cache/crypto/FaceCaptureResponseProcessor.kt
+++ id/src/main/java/com/simprints/id/orchestrator/cache/crypto/FaceCaptureResponseProcessor.kt
@@ -2,6 +2,7 @@ package com.simprints.id.orchestrator.cache.crypto
 
 import com.simprints.id.data.secure.keystore.KeystoreManager
 import com.simprints.id.domain.moduleapi.face.responses.FaceCaptureResponse
+import com.simprints.id.domain.moduleapi.face.responses.entities.FaceCaptureResult
 import com.simprints.id.orchestrator.steps.Step
 
 class FaceCaptureResponseProcessor(
@@ -11,17 +12,18 @@ class FaceCaptureResponseProcessor(
     override fun process(response: Step.Result?, operation: Operation): Step.Result {
         require(response is FaceCaptureResponse)
 
-        val capturingResult = response.capturingResult.map {
-            it.apply {
-                result?.template?.let { template ->
+        val capturingResult = arrayListOf<FaceCaptureResult>()
+        response.capturingResult.forEach {
+            it.also { item ->
+                item.result?.template?.let { template ->
                     val tmpTemplateString = String(template)
                     val processedTemplate = when (operation) {
                         Operation.ENCODE -> keystoreManager.encryptString(tmpTemplateString)
                         Operation.DECODE -> keystoreManager.decryptString(tmpTemplateString)
                     }.toByteArray()
 
-                    val faceSample = result.copy(template = processedTemplate)
-                    copy(result = faceSample)
+                    val faceSample = item.result.copy(template = processedTemplate)
+                    capturingResult.add(item.copy(result = faceSample))
                 }
             }
         }
