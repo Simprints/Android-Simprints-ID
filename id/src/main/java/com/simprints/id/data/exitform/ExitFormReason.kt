2a8bc690b691fa09c26b294e1a96199f38d83383
Aashay Gaikwad <aashay@simprints.com>
2019-09-16 21:48:25 +0530

    Changing function name and location for conversion to moduleApi
    Former-commit-id: 1e5502316315c281e99ff2e3df65ba0878e00168

--- id/src/main/java/com/simprints/id/data/exitform/ExitFormReason.kt
+++ id/src/main/java/com/simprints/id/data/exitform/ExitFormReason.kt
@@ -1,7 +1,6 @@
 package com.simprints.id.data.exitform
 
 import com.simprints.id.data.analytics.eventdata.models.domain.events.RefusalEvent
-import com.simprints.id.domain.moduleapi.app.responses.entities.RefusalFormReason
 
 enum class ExitFormReason {
     REFUSED_RELIGION,
@@ -22,15 +21,4 @@ fun ExitFormReason.toRefusalEventAnswer(): RefusalEvent.Answer =
         ExitFormReason.REFUSED_NOT_PRESENT -> RefusalEvent.Answer.REFUSED_NOT_PRESENT
         ExitFormReason.REFUSED_YOUNG -> RefusalEvent.Answer.REFUSED_YOUNG
         ExitFormReason.OTHER -> RefusalEvent.Answer.OTHER
-    }
-
-fun ExitFormReason.toAppRefusalFormReason(): RefusalFormReason =
-    when (this) {
-        ExitFormReason.REFUSED_RELIGION -> RefusalFormReason.REFUSED_RELIGION
-        ExitFormReason.REFUSED_DATA_CONCERNS -> RefusalFormReason.REFUSED_DATA_CONCERNS
-        ExitFormReason.REFUSED_PERMISSION -> RefusalFormReason.REFUSED_PERMISSION
-        ExitFormReason.SCANNER_NOT_WORKING -> RefusalFormReason.SCANNER_NOT_WORKING
-        ExitFormReason.REFUSED_NOT_PRESENT -> RefusalFormReason.REFUSED_NOT_PRESENT
-        ExitFormReason.REFUSED_YOUNG -> RefusalFormReason.REFUSED_YOUNG
-        ExitFormReason.OTHER -> RefusalFormReason.OTHER
     }
