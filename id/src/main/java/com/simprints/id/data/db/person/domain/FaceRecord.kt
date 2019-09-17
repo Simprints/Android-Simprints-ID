284ded495119b6e01f95561aeaf8a96a1cdb6c86
fabiotuzza <fabio@simprints.com>
2019-09-17 12:10:15 +0100

    Added comments for unused classes
    Former-commit-id: bdce63e6f0d5340e305eeaf37bd5be563afdcbea

--- id/src/main/java/com/simprints/id/data/db/person/domain/FaceRecord.kt
+++ id/src/main/java/com/simprints/id/data/db/person/domain/FaceRecord.kt
@@ -4,6 +4,7 @@ import android.os.Parcelable
 import com.simprints.core.images.SecuredImageRef
 import kotlinx.android.parcel.Parcelize
 
+// FaceRecord = FaceSample + patient Id (it will be used for matchings)
 @Parcelize
 class FaceRecord(val personId: String,
                  override val template: ByteArray,
