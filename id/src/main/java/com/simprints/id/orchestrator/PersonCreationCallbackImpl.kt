92efe555e6ebcd6a2ae24e5c766b6244b91b7952
Alan Camargo <alan@simprints.com>
2019-09-19 14:42:49 +0100

    Register event on person creation
    Former-commit-id: cb2cad0447f55a9bf0d387540af65d4fad716560

--- id/src/main/java/com/simprints/id/orchestrator/PersonCreationCallbackImpl.kt
+++ id/src/main/java/com/simprints/id/orchestrator/PersonCreationCallbackImpl.kt
@@ -1,21 +1,41 @@
 package com.simprints.id.orchestrator
 
+import com.simprints.id.data.analytics.eventdata.controllers.domain.SessionEventsManager
+import com.simprints.id.data.analytics.eventdata.models.domain.events.EnrolmentEvent
 import com.simprints.id.data.db.person.PersonRepository
 import com.simprints.id.data.db.person.domain.Person
 import com.simprints.id.orchestrator.responsebuilders.AppResponseBuilderForEnrol
+import com.simprints.id.tools.TimeHelper
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class PersonCreationCallbackImpl(
-    private val repository: PersonRepository
+    private val repository: PersonRepository,
+    private val sessionEventsManager: SessionEventsManager,
+    private val timeHelper: TimeHelper
 ) : AppResponseBuilderForEnrol.PersonCreationCallback {
 
     override fun onPersonCreated(person: Person?) {
-        CoroutineScope(Dispatchers.Default).launch {
-            if (person != null)
-                repository.saveAndUpload(person)
+        person?.let {
+            saveAndUpload(it)
+            registerEvent(it)
         }
+    }
+
+    private fun saveAndUpload(person: Person) {
+        CoroutineScope(Dispatchers.Default).launch {
+            repository.saveAndUpload(person)
+        }
+    }
+
+    private fun registerEvent(person: Person) {
+        sessionEventsManager
+            .updateSession {
+                it.addEvent(
+                    EnrolmentEvent(timeHelper.now(), person.patientId)
+                )
+            }
     }
 
 }
