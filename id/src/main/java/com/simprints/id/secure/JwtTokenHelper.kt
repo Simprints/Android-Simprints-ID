f28571d102ae4e0c8f21f87d181fa8e0eb8b2034
fabiotuzza <fabio@simprints.com>
2019-09-19 20:30:36 +0100

    Added unit test for jwtTokenHelper
    Former-commit-id: ff84fa6f74854d47ab47d9ea593aa4274d8cfcb2

--- id/src/main/java/com/simprints/id/secure/JwtTokenHelper.kt
+++ id/src/main/java/com/simprints/id/secure/JwtTokenHelper.kt
@@ -7,15 +7,21 @@ class JwtTokenHelper {
 
     companion object {
         fun extractTokenPayloadAsJson(jwtToken: String): JSONObject? {
-            //the JWT (JSON WEB TOKEN) is just a 3 base64 encoded parts concatenated by a . character
-            val jwtParts = extractingJWTParts(jwtToken)
+            return try {
+                //the JWT (JSON WEB TOKEN) is just a 3 base64 encoded parts concatenated by a . character
+                val jwtParts = extractingJWTParts(jwtToken)
 
-            return if (jwtParts.size == 3) {
-                //we're only really interested in the body/payload
-                val decodedPayload = String(Base64.decode(jwtParts[1], Base64.DEFAULT))
 
-                JSONObject(decodedPayload)
-            } else {
+                if (jwtParts.size == 3) {
+                    //we're only really interested in the body/payload
+                    val decodedPayload = String(Base64.decode(jwtParts[1], Base64.DEFAULT))
+
+                    JSONObject(decodedPayload)
+                } else {
+                    throw Throwable("Impossible to parse jwt")
+                }
+            } catch (t: Throwable) {
+                t.printStackTrace()
                 null
             }
         }
