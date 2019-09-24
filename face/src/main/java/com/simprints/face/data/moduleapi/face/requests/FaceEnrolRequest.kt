834203d28bd14ef9e363fc66b1b372963e00a3b8
Tsuharesu <tsuharesu01@gmail.com>
2019-09-24 14:30:11 +0100

    Remove unnecessary entities from face
    Former-commit-id: 974f02bc5389043f7846df6c277fc2d45bd9d9e0

--- face/src/main/java/com/simprints/face/data/moduleapi/face/requests/FaceEnrolRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.simprints.face.data.moduleapi.face.requests
-
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class FaceEnrolRequest(val projectId: String,
-                            val userId: String,
-                            val moduleId: String) : FaceRequest
