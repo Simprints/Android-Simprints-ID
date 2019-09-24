297e9593aaa62eb4221d1b59c74387c8f27920aa
Tsuharesu <tsuharesu01@gmail.com>
2019-09-24 14:30:11 +0100

    Remove unnecessary entities from face
    Former-commit-id: 39a25668457a32f731579c421d1475a8900e9fc0

--- face/src/main/java/com/simprints/face/data/moduleapi/face/responses/entities/FaceMatchingResult.kt
+++
@@ -1,9 +0,0 @@
-package com.simprints.face.data.moduleapi.face.responses.entities
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class FaceMatchingResult(val guidFound: String,
-                              val confidence: Int,
-                              val tier: FaceTier) : Parcelable
