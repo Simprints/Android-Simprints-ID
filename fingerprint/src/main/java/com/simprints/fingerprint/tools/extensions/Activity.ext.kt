06fe3e829481cfc6a0aacdfe272f85a704b11252
Ridwan Farouki <ridwan@simprints.com>
2019-09-26 17:19:31 +0100

    Fixed Toast messages not getting string through resources
    Former-commit-id: fb4b69209cca61073e9e6a00ce607dbbb5a7fc17

--- fingerprint/src/main/java/com/simprints/fingerprint/tools/extensions/Activity.ext.kt
+++ fingerprint/src/main/java/com/simprints/fingerprint/tools/extensions/Activity.ext.kt
@@ -27,7 +27,7 @@ fun Activity.runOnUiThreadIfStillRunning(then: () -> Unit, otherwise: () -> Unit
     }
 }
 
-fun Activity.showToast(stringRes: Int) =
+fun Activity.showToast(string: String) =
     runOnUiThread {
-        Toast.makeText(this, stringRes, Toast.LENGTH_LONG).show()
+        Toast.makeText(this, string, Toast.LENGTH_LONG).show()
     }
