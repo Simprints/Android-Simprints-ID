d3a385b54e8c361cde75d08b336e810a463272bd
Ridwan Farouki <ridwan@simprints.com>
2019-10-01 16:32:40 +0100

    Introduce ScannerWrapper interface
    Former-commit-id: 3c8bb06f72dd1c4d7fc05ac3d55da4938b784005

---
+++ fingerprint/src/main/java/com/simprints/fingerprint/controllers/scanner/wrapper/domain/CaptureFingerprintResponse.kt
@@ -0,0 +1,6 @@
+package com.simprints.fingerprint.controllers.scanner.wrapper.domain
+
+class CaptureFingerprintResponse(
+    val template: ByteArray?,
+    val qualityScore: Int?
+)
---
+++ fingerprint/src/main/java/com/simprints/fingerprint/controllers/scanner/wrapper/domain/ScannerVersionInformation.kt
@@ -0,0 +1,5 @@
+package com.simprints.fingerprint.controllers.scanner.wrapper.domain
+
+class ScannerVersionInformation(val veroVersion: Int,
+                                val firmwareVersion: Int,
+                                val ucVersion: Int)
