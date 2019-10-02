dea15b7e439bd199cd818ee63f3cacc489b92207
Aashay Gaikwad <aashay@simprints.com>
2019-10-02 10:55:44 +0530

    Added placeholder in strings and modified data generators for modalities
    Former-commit-id: 88dc27fcad19885a7eb1685fadbc953e443638e6

--- id/src/main/java/com/simprints/id/data/consent/shortconsent/GeneralConsentDataGenerator.kt
+++ id/src/main/java/com/simprints/id/data/consent/shortconsent/GeneralConsentDataGenerator.kt
@@ -2,12 +2,14 @@ package com.simprints.id.data.consent.shortconsent
 
 import android.content.Context
 import com.simprints.id.R
+import com.simprints.id.domain.modality.Modality
 import com.simprints.id.domain.moduleapi.core.requests.AskConsentRequest
 import com.simprints.id.domain.moduleapi.core.requests.ConsentType
 
 data class GeneralConsentDataGenerator(val generalConsentOptions: GeneralConsentOptions,
                                        val programName: String,
-                                       val organizationName: String) {
+                                       val organizationName: String,
+                                       val modality: Modality) {
 
     fun assembleText(context: Context, askConsentRequest: AskConsentRequest) = StringBuilder().apply {
         filterAppRequestForConsent(askConsentRequest, context)
@@ -22,19 +24,51 @@ data class GeneralConsentDataGenerator(val generalConsentOptions: GeneralConsent
     }
 
     private fun StringBuilder.appendTextForConsentEnrol(context: Context) {
-        if (generalConsentOptions.consentEnrolOnly) append(context.getString(R.string.consent_enrol_only).format(programName))
-        if (generalConsentOptions.consentEnrol) append(context.getString(R.string.consent_enrol).format(programName))
+        if (generalConsentOptions.consentEnrolOnly) {
+            append(context.getString(R.string.consent_enrol_only)
+                .format(getModalitySpecificUseCaseText(context), programName))
+        }
+        if (generalConsentOptions.consentEnrol) {
+            append(context.getString(R.string.consent_enrol)
+                .format(getModalitySpecificUseCaseText(context), programName))
+        }
     }
 
     private fun StringBuilder.appendTextForConsentVerifyOrIdentify(context: Context) {
-        if (generalConsentOptions.consentIdVerify) append(context.getString(R.string.consent_id_verify).format(programName))
+        if (generalConsentOptions.consentIdVerify) {
+            append(context.getString(R.string.consent_id_verify)
+                .format(getModalitySpecificUseCaseText(context), programName))
+        }
     }
 
     private fun StringBuilder.filterForDataSharingOptions(context: Context) {
-        if (generalConsentOptions.consentShareDataNo) append(context.getString(R.string.consent_share_data_no))
-        if (generalConsentOptions.consentShareDataYes) append(context.getString(R.string.consent_share_data_yes).format(organizationName))
-        if (generalConsentOptions.consentCollectYes) append(context.getString(R.string.consent_collect_yes))
-        if (generalConsentOptions.consentPrivacyRights) append(context.getString(R.string.consent_privacy_rights))
-        if (generalConsentOptions.consentConfirmation) append(context.getString(R.string.consent_confirmation))
+        if (generalConsentOptions.consentShareDataNo) {
+            append(context.getString(R.string.consent_share_data_no)
+                .format(getModalitySpecificAccessText(context)))
+        }
+        if (generalConsentOptions.consentShareDataYes) {
+            append(context.getString(R.string.consent_share_data_yes)
+                .format(organizationName, getModalitySpecificAccessText(context)))
+        }
+        if (generalConsentOptions.consentCollectYes) {
+            append(context.getString(R.string.consent_collect_yes))
+        }
+        if (generalConsentOptions.consentPrivacyRights) {
+            append(context.getString(R.string.consent_privacy_rights))
+        }
+        if (generalConsentOptions.consentConfirmation) {
+            append(context.getString(R.string.consent_confirmation)
+                .format(getModalitySpecificUseCaseText(context)))
+        }
+    }
+
+    private fun getModalitySpecificUseCaseText(context: Context) = when (modality) {
+        Modality.FACE -> context.getString(R.string.biometric_general_face)
+        Modality.FINGER -> context.getString(R.string.biometrics_general_fingerprint)
+    }
+
+    private fun getModalitySpecificAccessText(context: Context) = when (modality) {
+        Modality.FACE -> context.getString(R.string.biometrics_general_access_face)
+        Modality.FINGER -> context.getString(R.string.biometrics_general_access_fingerprint)
     }
 }
--- id/src/main/java/com/simprints/id/data/consent/shortconsent/ParentalConsentDataGenerator.kt
+++ id/src/main/java/com/simprints/id/data/consent/shortconsent/ParentalConsentDataGenerator.kt
@@ -2,13 +2,15 @@ package com.simprints.id.data.consent.shortconsent
 
 import android.content.Context
 import com.simprints.id.R
+import com.simprints.id.domain.modality.Modality
 import com.simprints.id.domain.moduleapi.core.requests.AskConsentRequest
 import com.simprints.id.domain.moduleapi.core.requests.ConsentType
 
 data class ParentalConsentDataGenerator(val parentalConsentExists: Boolean,
                                         val parentalConsentOptions: ParentalConsentOptions,
                                         val programName: String,
-                                        val organizationName: String) {
+                                        val organizationName: String,
+                                        val modality: Modality) {
 
     fun assembleText(context: Context, askConsentRequest: AskConsentRequest) = StringBuilder().apply {
         filterAppRequestForParentalConsent(askConsentRequest, context)
@@ -23,19 +25,50 @@ data class ParentalConsentDataGenerator(val parentalConsentExists: Boolean,
     }
 
     private fun StringBuilder.appendTextForParentalEnrol(context: Context) {
-        if (parentalConsentOptions.consentParentEnrolOnly) append(context.getString(R.string.consent_parental_enrol_only).format(programName))
-        if (parentalConsentOptions.consentParentEnrol) append(context.getString(R.string.consent_parental_enrol).format(programName))
+        if (parentalConsentOptions.consentParentEnrolOnly) {
+            append(context.getString(R.string.consent_parental_enrol_only)
+                .format(getModalitySpecificUseCaseText(context), programName))
+        }
+        if (parentalConsentOptions.consentParentEnrol) {
+            append(context.getString(R.string.consent_parental_enrol)
+                .format(getModalitySpecificUseCaseText(context), programName))
+        }
     }
 
     private fun StringBuilder.appendTextForIdentifyOrVerify(context: Context) {
-        if (parentalConsentOptions.consentParentIdVerify) append(context.getString(R.string.consent_parental_id_verify).format(programName))
+        if (parentalConsentOptions.consentParentIdVerify) {
+            append(context.getString(R.string.consent_parental_id_verify)
+                .format(getModalitySpecificUseCaseText(context), programName))
+        }
     }
 
     private fun StringBuilder.extractDataSharingOptions(context: Context) {
-        if (parentalConsentOptions.consentParentShareDataNo) append(context.getString(R.string.consent_parental_share_data_no))
-        if (parentalConsentOptions.consentParentShareDataYes) append(context.getString(R.string.consent_parental_share_data_yes).format(organizationName))
-        if (parentalConsentOptions.consentCollectYes) append(context.getString(R.string.consent_collect_yes))
-        if (parentalConsentOptions.consentParentPrivacyRights) append(context.getString(R.string.consent_parental_privacy_rights))
-        if (parentalConsentOptions.consentParentConfirmation) append(context.getString(R.string.consent_parental_confirmation))
+        if (parentalConsentOptions.consentParentShareDataNo) {
+            append(context.getString(R.string.consent_parental_share_data_no))
+        }
+        if (parentalConsentOptions.consentParentShareDataYes) {
+            append(context.getString(R.string.consent_parental_share_data_yes)
+                .format(organizationName, getModalitySpecificAccessText(context)))
+        }
+        if (parentalConsentOptions.consentCollectYes) {
+            append(context.getString(R.string.consent_collect_yes))
+        }
+        if (parentalConsentOptions.consentParentPrivacyRights) {
+            append(context.getString(R.string.consent_parental_privacy_rights))
+        }
+        if (parentalConsentOptions.consentParentConfirmation) {
+            append(context.getString(R.string.consent_parental_confirmation)
+                .format(getModalitySpecificUseCaseText(context)))
+        }
+    }
+
+    private fun getModalitySpecificUseCaseText(context: Context) = when (modality) {
+        Modality.FACE -> context.getString(R.string.biometrics_parental_face)
+        Modality.FINGER -> context.getString(R.string.biometrics_parental_fingerprint)
+    }
+
+    private fun getModalitySpecificAccessText(context: Context) = when (modality) {
+        Modality.FACE -> context.getString(R.string.biometrics_general_access_face)
+        Modality.FINGER -> context.getString(R.string.biometrics_general_access_fingerprint)
     }
 }
