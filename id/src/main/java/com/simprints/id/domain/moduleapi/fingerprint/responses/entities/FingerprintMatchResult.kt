4ac3be034798b729d0f3de9282973ea2913ec83d
fabiotuzza <fabio@simprints.com>
2019-10-07 08:39:53 +0200

    WIP matching request/response
    Former-commit-id: 3b49944ff255a4e27baa15aa4c81d3730299ed23

--- id/src/main/java/com/simprints/id/domain/moduleapi/fingerprint/responses/entities/FingerprintMatchResult.kt
+++ id/src/main/java/com/simprints/id/domain/moduleapi/fingerprint/responses/entities/FingerprintMatchResult.kt
@@ -1,18 +1,8 @@
 package com.simprints.id.domain.moduleapi.fingerprint.responses.entities
 
 import android.os.Parcelable
-import com.simprints.id.domain.moduleapi.app.responses.entities.MatchResult
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class FingerprintMatchingResult(val personId: String,
-                                     val confidenceScore: Int) : Parcelable
-
-fun FingerprintMatchingResult.toAppMatchResult() =
-    MatchResult(this.guidFound, this.confidence, tier.toAppTier())
-
-
-interface IFingerprintMatchResult {
-    val personId: String
-    val confidenceScore: Float
-}
+data class FingerprintMatchResult(val personId: String,
+                                  val confidenceScore: Int) : Parcelable
