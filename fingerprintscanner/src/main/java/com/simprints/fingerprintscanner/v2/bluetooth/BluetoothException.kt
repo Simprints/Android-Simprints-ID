caee5358287b733d5ca612e924c63724c2e6f818
Ridwan Farouki <ridwan@simprints.com>
2019-10-14 17:47:56 +0100

    Removed redundant files
    Former-commit-id: bf2e54d0e8fe68ab51a96e2ec8e6218444bf6946

--- fingerprintscanner/src/main/java/com/simprints/fingerprintscanner/v2/bluetooth/BluetoothException.kt
+++
@@ -1,9 +0,0 @@
-package com.simprints.fingerprintscanner.v2.bluetooth
-
-import java.io.IOException
-
-sealed class BluetoothException(message: String) : RuntimeException(message)
-class BluetoothNotSupportedException : BluetoothException("Bluetooth not supported on this platform")
-class BluetoothNotEnabledException : BluetoothException("Bluetooth not currently enabled")
-class BluetoothDeviceNotPairedException(macAddress: String) : BluetoothException("Bluetooth device not currently paired with MAC Address $macAddress")
-class BluetoothConnectionException(cause: IOException) : BluetoothException("Bluetooth connection failed. Reason: ${cause.message}")
