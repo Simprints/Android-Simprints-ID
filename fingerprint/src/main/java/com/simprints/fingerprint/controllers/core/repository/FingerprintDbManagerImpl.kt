59730ad0aa9d37b5efc457e1d8123e2c1fc50271
fabiotuzza <fabio@simprints.com>
2019-10-15 11:10:56 +0100

    Fixing compiling issues
    Former-commit-id: 1fd993dc6724fdd87192ba75918b9ef6aad9063d

--- fingerprint/src/main/java/com/simprints/fingerprint/controllers/core/repository/FingerprintDbManagerImpl.kt
+++ fingerprint/src/main/java/com/simprints/fingerprint/controllers/core/repository/FingerprintDbManagerImpl.kt
@@ -1,35 +1,18 @@
 package com.simprints.fingerprint.controllers.core.repository
 
-import com.simprints.core.tools.extentions.completableWithSuspend
 import com.simprints.core.tools.extentions.singleWithSuspend
-import com.simprints.fingerprint.controllers.core.repository.models.PersonFetchResult
-import com.simprints.fingerprint.data.domain.person.Person
+import com.simprints.id.data.db.person.domain.FingerprintRecord
 import com.simprints.id.data.db.person.local.FingerprintRecordLocalDataSource
-import io.reactivex.Completable
 import io.reactivex.Single
+import kotlinx.coroutines.flow.toList
+import java.io.Serializable
 
 class FingerprintDbManagerImpl(private val coreFingerprintRecordLocalDataSource: FingerprintRecordLocalDataSource) : FingerprintDbManager {
 
-    //STOPSHIP: To remove - use loadPeople(query: Serializable)
-    override fun loadPerson(projectId: String, verifyGuid: String): Single<PersonFetchResult> =
-        singleWithSuspend {
-            //val corePersonFetch = runBlocking { personRepository.loadFromRemoteIfNeeded(projectId = projectId, patientId = verifyGuid) }
-            //PersonFetchResult.fromCoreToDomain(corePersonFetch)
-        }
+    //StopShip: Transform return to fingerprintRecord domain classes
 
-    //STOPSHIP: To be removed after enrolment
-    override fun savePerson(person: Person): Completable =
-        completableWithSuspend {
-            person.toSync = true
-            //personRepository.saveAndUpload(person.fromDomainToCore())
-        }
-
-    //STOPSHIP: change to loadPeople(query: Serializable). Query is received in the Matching Request
-    override fun loadPeople(projectId: String,
-                            userId: String?,
-                            moduleId: String?): Single<List<Person>> =
+    override fun loadPeople(query: Serializable): Single<List<FingerprintRecord>> =
         singleWithSuspend {
-            //val peopleList = personRepository.load(PersonLocalDataSource.Query(projectId = projectId, userId = userId, moduleId = moduleId))
-            //peopleList.toList().map { Person.fromCoreToDomain(it) }
+            coreFingerprintRecordLocalDataSource.loadFingerprintRecords(query).toList()
         }
 }
