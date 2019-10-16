92bb3565a97ca1d068978967145e3e7567cbdf71
Ridwan Farouki <ridwan@simprints.com>
2019-10-16 17:36:12 +0100

    Factored tools a little bit
    Former-commit-id: 330ffc5d3231901a0309cd4900f30f2589fc9016

--- fingerprintscannermock/src/main/java/com/simprints/fingerprintscannermock/simulated/v1/SimulatedScannerV1.kt
+++ fingerprintscannermock/src/main/java/com/simprints/fingerprintscannermock/simulated/v1/SimulatedScannerV1.kt
@@ -1,10 +1,9 @@
 package com.simprints.fingerprintscannermock.simulated.v1
 
 import com.simprints.fingerprintscanner.v1.Message
-import com.simprints.fingerprintscannermock.simulated.common.ScannerState
 import com.simprints.fingerprintscannermock.simulated.SimulatedScannerManager
+import com.simprints.fingerprintscannermock.simulated.common.ScannerState
 import com.simprints.fingerprintscannermock.simulated.common.SimulatedScanner
-import com.simprints.fingerprintscannermock.simulated.tools.bytesToMessageV1
 import java.io.OutputStream
 
 class SimulatedScannerV1(simulatedScannerManager: SimulatedScannerManager,
@@ -14,7 +13,7 @@ class SimulatedScannerV1(simulatedScannerManager: SimulatedScannerManager,
     private val responseHelper = SimulatedResponseHelperV1(simulatedScannerManager, this)
 
     override fun handleAppToScannerEvent(bytes: ByteArray, returnStream: OutputStream) {
-        val message: Message = bytesToMessageV1(bytes)
+        val message: Message = bytes.toMessageV1()
         scannerState.updateStateAccordingToOutgoingMessage(message)
         val response: ByteArray = responseHelper.createMockResponse(message)
         writeResponseToStream(response, returnStream)
---
+++ fingerprintscannermock/src/main/java/com/simprints/fingerprintscannermock/simulated/v2/SimulatedScannerV2.kt
@@ -0,0 +1,17 @@
+package com.simprints.fingerprintscannermock.simulated.v2
+
+import com.simprints.fingerprintscannermock.simulated.SimulatedScannerManager
+import com.simprints.fingerprintscannermock.simulated.common.ScannerState
+import com.simprints.fingerprintscannermock.simulated.common.SimulatedScanner
+import java.io.OutputStream
+
+class SimulatedScannerV2(simulatedScannerManager: SimulatedScannerManager,
+                         scannerState: ScannerState = ScannerState())
+    : SimulatedScanner(scannerState) {
+
+    override fun handleAppToScannerEvent(bytes: ByteArray, returnStream: OutputStream) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun respondWithMessage()
+}
