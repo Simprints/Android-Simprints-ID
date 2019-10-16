7f8fd3a8ac147d71bc9697bac7089a91389ae8ba
Alan Camargo <alan@simprints.com>
2019-10-16 09:53:27 +0100

    Implement conversion for IFingerprintCaptureResult
    Former-commit-id: f6974a4c8f1fcd762a8b91f0ba9651d93404125b

--- id/src/main/java/com/simprints/id/domain/moduleapi/fingerprint/ModuleApiToDomainFingerprintResponse.kt
+++ id/src/main/java/com/simprints/id/domain/moduleapi/fingerprint/ModuleApiToDomainFingerprintResponse.kt
@@ -4,6 +4,7 @@ import com.simprints.id.domain.moduleapi.fingerprint.responses.*
 import com.simprints.id.domain.moduleapi.fingerprint.responses.entities.FingerprintMatchingResult
 import com.simprints.id.domain.moduleapi.fingerprint.responses.entities.FingerprintRefusalFormReason
 import com.simprints.id.domain.moduleapi.fingerprint.responses.entities.FingerprintTier
+import com.simprints.id.domain.moduleapi.fingerprint.responses.entities.fromModuleApiToDomain
 import com.simprints.moduleapi.fingerprint.responses.*
 
 object ModuleApiToDomainFingerprintResponse {
@@ -18,7 +19,9 @@ object ModuleApiToDomainFingerprintResponse {
         }
 
     private fun fromModuleApiToFingerprintCaptureResponse(fingerprintResponse: IFingerprintCaptureResponse): FingerprintCaptureResponse {
-        return FingerprintCaptureResponse(captureResult = fingerprintResponse.captureResult)
+        return FingerprintCaptureResponse(
+            captureResult = fingerprintResponse.captureResult.map { it.fromModuleApiToDomain() }
+        )
     }
 
     private fun fromModuleApiToFingerprintVerifyResponse(fingerprintResponse: IFingerprintVerifyResponse): FingerprintVerifyResponse {
@@ -40,7 +43,7 @@ object ModuleApiToDomainFingerprintResponse {
         FingerprintMatchingResult(matchingResult.guid, matchingResult.confidence, fromFingerprintToDomainTier(matchingResult.tier))
 
     private fun fromModuleApiToFingerprintRefusalResponse(fingerprintResponse: IFingerprintExitFormResponse): FingerprintResponse {
-        val reason = when(fingerprintResponse.reason) {
+        val reason = when (fingerprintResponse.reason) {
             IFingerprintExitReason.REFUSED_RELIGION -> FingerprintRefusalFormReason.REFUSED_RELIGION
             IFingerprintExitReason.REFUSED_DATA_CONCERNS -> FingerprintRefusalFormReason.REFUSED_DATA_CONCERNS
             IFingerprintExitReason.REFUSED_PERMISSION -> FingerprintRefusalFormReason.REFUSED_PERMISSION
@@ -54,7 +57,7 @@ object ModuleApiToDomainFingerprintResponse {
     }
 
     private fun fromFingerprintToDomainError(error: IFingerprintErrorReason): FingerprintErrorReason =
-        when(error) {
+        when (error) {
             IFingerprintErrorReason.UNEXPECTED_ERROR -> FingerprintErrorReason.UNEXPECTED_ERROR
             IFingerprintErrorReason.BLUETOOTH_NOT_SUPPORTED -> FingerprintErrorReason.BLUETOOTH_NOT_SUPPORTED
             IFingerprintErrorReason.GUID_NOT_FOUND_ONLINE -> FingerprintErrorReason.GUID_NOT_FOUND_ONLINE
