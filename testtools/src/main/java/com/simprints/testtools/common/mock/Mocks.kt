bdc2ffa6097b92235b1d6cea039098f698f0ea32
Alan Camargo <alan@simprints.com>
2019-10-18 15:02:33 +0100

    Add comment to mock template generating function
    Former-commit-id: 371e830cbcbddb41bab44ebfead51f8234edc6e6

--- testtools/src/main/java/com/simprints/testtools/common/mock/Mocks.kt
+++ testtools/src/main/java/com/simprints/testtools/common/mock/Mocks.kt
@@ -2,6 +2,9 @@ package com.simprints.testtools.common.mock
 
 import java.util.*
 
+/**
+ * Generates a mock fingerprint or face template: a byte array larger than 256B
+ */
 fun mockTemplate(): ByteArray {
     return ("askdfjhsalkdjfhlaksdjfhaklsdhfjjaksdfgkashdgfkjashbdfkjasbfhkajsfsgfs" +
         "faskdjhfkajsfhalskfhaslkhf${UUID.randomUUID()}asdjfhgaskjfgjkasfgkjasfgas" +
