6ca3ba45f52a33cdc30847605b84d256b6804eec
Aashay Gaikwad <aashay@simprints.com>
2019-10-21 12:37:19 +0530

    Renaming
    Former-commit-id: 9a64756d148d3fb3273d8a6d4a7939f2082884ed

--- id/src/main/java/com/simprints/id/domain/moduleapi/face/responses/entities/FaceMatchResult.kt
+++ id/src/main/java/com/simprints/id/domain/moduleapi/face/responses/entities/FaceMatchResult.kt
@@ -1,13 +1,13 @@
 package com.simprints.id.domain.moduleapi.face.responses.entities
 
 import android.os.Parcelable
-import com.simprints.moduleapi.face.responses.entities.IFaceMatchingResult
+import com.simprints.moduleapi.face.responses.IFaceMatchResult
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class FaceMatchingResult(val guidFound: String,
-                              val confidence: Int,
-                              val tier: FaceTier) : Parcelable
+data class FaceMatchResult(val guidFound: String,
+                           val confidence: Int,
+                           val tier: FaceTier) : Parcelable
 
-fun IFaceMatchingResult.fromModuleApiToDomain() =
-    FaceMatchingResult(guid, confidence, tier.fromDomainApiToDomain())
+fun IFaceMatchResult.fromModuleApiToDomain() =
+    FaceMatchResult(guid, confidence, tier.fromDomainApiToDomain())
