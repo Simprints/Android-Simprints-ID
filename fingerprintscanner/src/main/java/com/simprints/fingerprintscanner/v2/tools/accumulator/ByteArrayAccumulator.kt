b5f11163e40a932a2c2d3988f933dafb54b5dc55
Ridwan Farouki <ridwan@simprints.com>
2019-10-21 15:02:01 +0100

    Added documentation to Accumulator, improved extensions
    Former-commit-id: b96295f55a35f43696a40bc98d33f836bc34214a

--- fingerprintscanner/src/main/java/com/simprints/fingerprintscanner/v2/tools/accumulator/ByteArrayAccumulator.kt
+++ fingerprintscanner/src/main/java/com/simprints/fingerprintscanner/v2/tools/accumulator/ByteArrayAccumulator.kt
@@ -1,5 +1,13 @@
 package com.simprints.fingerprintscanner.v2.tools.accumulator
 
+/**
+ * An [Accumulator] that uses a [ByteArray] as the intermediate collection.
+ *
+ * See [Accumulator] for details on behaviour and usage.
+ *
+ * @param fragmentAsByteArray how to reduce a [Fragment] to a ByteArray
+ * @param buildElement how to subsequently build an [Element] from a ByteArray
+ */
 abstract class ByteArrayAccumulator<in Fragment, Element>(
     inline val fragmentAsByteArray: (Fragment) -> ByteArray,
     canComputeElementLength: (ByteArray) -> Boolean,
