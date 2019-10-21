b7aac847bb09c269f0881006e73ee6b25f35ec53
Ridwan Farouki <ridwan@simprints.com>
2019-10-21 15:02:01 +0100

    Added documentation to Accumulator, improved extensions
    Former-commit-id: 33acb275d890df72d9f100425b14e49ede2a1858

--- fingerprintscanner/src/main/java/com/simprints/fingerprintscanner/v2/tools/accumulator/Accumulate.kt
+++ fingerprintscanner/src/main/java/com/simprints/fingerprintscanner/v2/tools/accumulator/Accumulate.kt
@@ -2,20 +2,28 @@ package com.simprints.fingerprintscanner.v2.tools.accumulator
 
 import io.reactivex.Flowable
 
+/**
+ * Transforms a stream of [Fragment]s into [Element]s.
+ *
+ * See [Accumulator] for details on behaviour and usage.
+ *
+ * @param accumulator the entity that scans the stream for [Fragment]s, stores them in a [FragmentCollection], and produces [Element]s.
+ * The same instance of the Accumulator is used for the lifetime of the stream.
+ */
 fun <Fragment, FragmentCollection, Element> Flowable<out Fragment>.accumulateAndTakeElements(
     accumulator: Accumulator<Fragment, FragmentCollection, Element>
 ): Flowable<Element> = this
     .accumulate(accumulator)
     .takeElements()
 
-fun <Fragment, FragmentCollection, Element> Flowable<out Fragment>.accumulate(
+private fun <Fragment, FragmentCollection, Element> Flowable<out Fragment>.accumulate(
     accumulator: Accumulator<Fragment, FragmentCollection, Element>
 ): Flowable<Accumulator<Fragment, FragmentCollection, Element>> =
     scan(accumulator) { acc: Accumulator<Fragment, FragmentCollection, Element>, newFragment: Fragment ->
         acc.apply { updateWithNewFragment(newFragment) }
     }
 
-fun <Fragment, FragmentCollection, Element> Flowable<out Accumulator<Fragment, FragmentCollection, Element>>.takeElements(): Flowable<Element> =
+private fun <Fragment, FragmentCollection, Element> Flowable<out Accumulator<Fragment, FragmentCollection, Element>>.takeElements(): Flowable<Element> =
     concatMap {
         it.takeElements()
     }
