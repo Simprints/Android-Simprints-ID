16d8c983cdd201f98958b2151854b90f1eb3cc4b
Alan Camargo <alan@simprints.com>
2019-10-22 11:07:30 +0100

    Remove yaw and pitch from ApiFace
    Former-commit-id: 808143417d375a6b4923ca525792c5c29c721ba2

--- id/src/main/java/com/simprints/id/data/db/person/remote/models/ApiFace.kt
+++ id/src/main/java/com/simprints/id/data/db/person/remote/models/ApiFace.kt
@@ -3,6 +3,4 @@ package com.simprints.id.data.db.person.remote.models
 import androidx.annotation.Keep
 
 @Keep
-data class ApiFace(val template: String = "",
-                   val yaw: String = "",
-                   val pitch: String = "")
+data class ApiFace(val template: String = "")
