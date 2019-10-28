817667578f59ce759350d198f64213cb3fdd4677
Alan Camargo <alan@simprints.com>
2019-10-28 13:36:22 +0000

    Create structure for ModuleSelectionFragment
    Former-commit-id: 59c21a8a4f7356169c3ca4e2602953e08e8b5ced

---
+++ id/src/main/java/com/simprints/id/activities/settings/ModuleSelectionActivity.kt
@@ -0,0 +1,37 @@
+package com.simprints.id.activities.settings
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.simprints.id.R
+import com.simprints.id.activities.settings.fragments.moduleSelection.ModuleSelectionFragment
+import kotlinx.android.synthetic.main.settings_toolbar.*
+
+class ModuleSelectionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_toolbar)
+        configureToolbar()
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.prefContent, ModuleSelectionFragment())
+            .commit()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun configureToolbar() {
+        setSupportActionBar(settingsToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+}
