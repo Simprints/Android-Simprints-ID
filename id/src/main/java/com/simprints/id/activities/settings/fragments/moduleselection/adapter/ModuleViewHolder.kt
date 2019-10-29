675b88a4af0c823df814acfc9510394e0c354a66
Alan Camargo <alan@simprints.com>
2019-10-29 09:11:22 +0000

    Add checkboxes
    Former-commit-id: de3bc88d7be40164f5514c91fa581c0e2ea586ef

--- id/src/main/java/com/simprints/id/activities/settings/fragments/moduleselection/adapter/ModuleViewHolder.kt
+++ id/src/main/java/com/simprints/id/activities/settings/fragments/moduleselection/adapter/ModuleViewHolder.kt
@@ -1,17 +1,25 @@
 package com.simprints.id.activities.settings.fragments.moduleselection.adapter
 
 import android.view.View
-import android.widget.TextView
+import android.widget.CheckBox
 import androidx.recyclerview.widget.RecyclerView
 import com.simprints.id.R
 import com.simprints.id.moduleselection.model.Module
+import org.jetbrains.anko.sdk27.coroutines.onCheckedChange
 
 class ModuleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    private val txtModuleName: TextView = itemView.findViewById(R.id.txtModuleName)
+    private val checkBox: CheckBox = itemView.findViewById(R.id.checkBox)
 
     fun bindTo(module: Module) {
-        txtModuleName.text = module.name
+        with(checkBox) {
+            text = module.name
+            isChecked = module.isSelected
+
+            onCheckedChange { _, isChecked ->
+                module.isSelected = isChecked
+            }
+        }
     }
 
 }
