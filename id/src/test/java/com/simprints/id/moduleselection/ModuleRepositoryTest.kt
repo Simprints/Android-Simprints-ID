4a87cda41e8ecdd3e26fe28ce050461b915cb452
Alan Camargo <alan@simprints.com>
2019-10-30 11:28:11 +0000

    ModuleRepositoryTest ready
    Former-commit-id: f98c5bfeddc2f85261b56306d2b749f52714aa38

--- id/src/test/java/com/simprints/id/moduleselection/ModuleRepositoryTest.kt
+++ id/src/test/java/com/simprints/id/moduleselection/ModuleRepositoryTest.kt
@@ -4,9 +4,14 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.nhaarman.mockitokotlin2.verify
 import com.simprints.id.commontesttools.di.TestAppModule
+import com.simprints.id.commontesttools.di.TestPreferencesModule
 import com.simprints.id.moduleselection.model.Module
 import com.simprints.id.testtools.TestApplication
 import com.simprints.id.testtools.UnitTestConfig
+import com.simprints.testtools.common.di.DependencyRule
+import com.simprints.testtools.common.syntax.whenever
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -17,14 +22,16 @@ import org.robolectric.annotation.Config
 class ModuleRepositoryTest {
 
     private val app = ApplicationProvider.getApplicationContext<TestApplication>()
-    private val appModule = TestAppModule(app)
+    private val appModule = TestAppModule(app, crashReportManagerRule = DependencyRule.MockRule)
+    private val preferencesModule = TestPreferencesModule(settingsPreferencesManagerRule = DependencyRule.MockRule)
 
     private lateinit var repository: ModuleRepository
 
     @Before
     fun setUp() {
-        UnitTestConfig(this, appModule).fullSetup()
+        UnitTestConfig(this, appModule, preferencesModule).fullSetup()
         repository = ModuleRepository(app.component)
+        configureMock()
     }
 
     @Test
@@ -51,6 +58,57 @@ class ModuleRepositoryTest {
         repository.setSelectedModules(selectedModules)
 
         verify(repository.callback).tooManyModulesSelected()
+    }
+
+    @Test
+    fun whenSelectingAnAcceptableNumberOfModules_shouldLogOnCrashReport() {
+        val selectedModules = listOf(
+            Module("1", true),
+            Module("2", true),
+            Module("3", true),
+            Module("4", true),
+            Module("5", true)
+        )
+
+        repository.setSelectedModules(selectedModules)
+
+        verify(repository.crashReportManager)
+            .setModuleIdsCrashlyticsKey(repository.preferencesManager.selectedModules)
+    }
+
+    @Test
+    fun shouldReturnAvailableModules() {
+        val expected = listOf(
+            Module("a", false),
+            Module("b", false),
+            Module("c", false),
+            Module("d", false)
+        )
+
+        val actual = repository.getAvailableModules()
+
+        assertThat(actual, `is`(expected))
+    }
+
+    @Test
+    fun shouldReturnSelectedModules() {
+        val expected = listOf(
+            Module("b", true),
+            Module("c", true)
+        )
+
+        val actual = repository.getSelectedModules()
+
+        assertThat(actual, `is`(expected))
+    }
+
+    private fun configureMock() {
+        whenever {
+            repository.preferencesManager.moduleIdOptions
+        } thenReturn setOf("a", "b", "c", "d")
+        whenever {
+            repository.preferencesManager.selectedModules
+        } thenReturn setOf("b", "c")
     }
 
 }
