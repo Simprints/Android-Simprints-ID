936b284b26549cc6a1ebce73825b8adf0a1f96f6
Alan Camargo <alan@simprints.com>
2019-10-30 13:09:41 +0000

    Implement ModuleSelectionActivityAndroidTest
    Former-commit-id: 70379596cfaf988f3160827c45b5cda59ecdada4

--- id/src/androidTest/java/com/simprints/id/testtools/di/AppComponentForAndroidTests.kt
+++ id/src/androidTest/java/com/simprints/id/testtools/di/AppComponentForAndroidTests.kt
@@ -3,6 +3,7 @@ package com.simprints.id.testtools.di
 import com.simprints.id.Application
 import com.simprints.id.activities.dashboard.DashboardActivityAndroidTest
 import com.simprints.id.activities.login.LoginActivityAndroidTest
+import com.simprints.id.activities.settings.ModuleSelectionActivityAndroidTest
 import com.simprints.id.data.analytics.eventdata.controllers.local.RealmSessionEventsDbManagerImplTest
 import com.simprints.id.data.secure.SecureDataManagerTest
 import com.simprints.id.di.AppComponent
@@ -37,4 +38,5 @@ interface AppComponentForAndroidTests : AppComponent {
     fun inject(secureDataManagerTest: SecureDataManagerTest)
     fun inject(localSessionEventsManagerImplTest: RealmSessionEventsDbManagerImplTest)
     fun inject(sessionEventsSyncManagerImplTest: SessionEventsSyncManagerImplAndroidTest)
+    fun inject(moduleSelectionActivityAndroidTest: ModuleSelectionActivityAndroidTest)
 }
