84af5accaefc38e44bd08507d1754352a35d2b29
Alan Camargo <alan@simprints.com>
2019-10-31 21:39:33 +0000

    Add line break for better readability
    Former-commit-id: 4bd64fa95901f2b18b67163ac8fa61cb75b24fff

--- id/src/main/java/com/simprints/id/tools/utils/QueryFilter.kt
+++ id/src/main/java/com/simprints/id/tools/utils/QueryFilter.kt
@@ -1,6 +1,7 @@
 package com.simprints.id.tools.utils
 
 interface QueryFilter<T> {
+
     fun filter(items: List<T>, query: String?, callback: SearchResultCallback? = null): List<T>
 
     interface SearchResultCallback {
