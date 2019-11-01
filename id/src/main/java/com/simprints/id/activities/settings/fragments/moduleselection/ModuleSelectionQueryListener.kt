b3e538d01290484ccfed489fa6567f6c89140dda
Alan Camargo <alan@simprints.com>
2019-11-01 13:16:06 +0000

    Remove callback from query listener
    Former-commit-id: 1af12d48e847b8b54fb5abe4580b3cb5aaedcc6f

--- id/src/main/java/com/simprints/id/activities/settings/fragments/moduleselection/ModuleSelectionQueryListener.kt
+++ id/src/main/java/com/simprints/id/activities/settings/fragments/moduleselection/ModuleSelectionQueryListener.kt
@@ -1,35 +1,23 @@
 package com.simprints.id.activities.settings.fragments.moduleselection
 
 import androidx.appcompat.widget.SearchView
-import com.simprints.id.activities.settings.fragments.moduleselection.adapter.ModuleAdapter
+import androidx.lifecycle.MutableLiveData
 import com.simprints.id.moduleselection.model.Module
 import com.simprints.id.tools.utils.QueryFilter
 
 class ModuleSelectionQueryListener(
-    private val adapter: ModuleAdapter,
-    private val modules: List<Module>,
-    private val searchResultCallback: SearchResultCallback
+    private val modules: List<Module>
 ) : SearchView.OnQueryTextListener {
+
+    val searchResults = MutableLiveData<List<Module>>()
 
     private val queryFilter: QueryFilter<Module> = ModuleQueryFilter()
 
     override fun onQueryTextSubmit(query: String?) = false
 
     override fun onQueryTextChange(newText: String?): Boolean {
-        val filteredList = queryFilter.getFilteredList(modules, newText)
-
-        if (filteredList.isEmpty())
-            searchResultCallback.onNothingFound()
-        else
-            searchResultCallback.onResultsFound()
-
-        adapter.submitList(filteredList)
+        searchResults.value = queryFilter.getFilteredList(modules, newText)
         return false
-    }
-
-    interface SearchResultCallback {
-        fun onNothingFound()
-        fun onResultsFound()
     }
 
 }
