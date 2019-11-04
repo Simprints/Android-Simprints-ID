10f3e5e50d12552bf0b22ca54ee844da2227b787
Alan Camargo <alan@simprints.com>
2019-11-04 20:45:18 +0000

    Update UI tests
    Former-commit-id: 3b47a01d1398006d79a6b8d728e3066399fcadab

--- id/src/androidTest/java/com/simprints/id/activities/settings/ModuleSelectionActivityAndroidTest.kt
+++ id/src/androidTest/java/com/simprints/id/activities/settings/ModuleSelectionActivityAndroidTest.kt
@@ -3,6 +3,7 @@ package com.simprints.id.activities.settings
 import android.content.Intent
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.rule.ActivityTestRule
+import br.com.concretesolutions.kappuccino.actions.ClickActions.click
 import br.com.concretesolutions.kappuccino.assertions.VisibilityAssertions.displayed
 import br.com.concretesolutions.kappuccino.assertions.VisibilityAssertions.notDisplayed
 import br.com.concretesolutions.kappuccino.custom.recyclerView.RecyclerViewInteractions.recyclerView
@@ -37,7 +38,7 @@ class ModuleSelectionActivityAndroidTest {
     }
 
     @Test
-    fun shouldLoadModules() {
+    fun shouldLoadOnlyUnselectedModules() {
         recyclerView(R.id.rvModules) {
             sizeIs(4)
         }
@@ -57,43 +58,41 @@ class ModuleSelectionActivityAndroidTest {
     }
 
     @Test
-    fun whenNoModulesAreSelected_noModulesSelectedTextShouldBeVisible() {
+    fun whenDeselectingAllModules_noModulesSelectedTextShouldBeVisible() {
+        click {
+            id(ID_CHIP_CLOSE_BUTTON)
+        }
+
         displayed {
             id(R.id.txtNoModulesSelected)
         }
     }
 
     @Test
-    fun whenDeselectingAllModules_noModulesSelectedTextShouldBeVisible() {
+    fun whenSelectingAModule_shouldBeRemovedFromList() {
         recyclerView(R.id.rvModules) {
             atPosition(0) {
                 click()
             }
-        }
 
-        notDisplayed {
-            id(R.id.txtNoModulesSelected)
-        }
-
-        recyclerView(R.id.rvModules) {
-            atPosition(0) {
-                click()
-            }
-        }
-
-        displayed {
-            id(R.id.txtNoModulesSelected)
+            sizeIs(3)
         }
     }
 
     private fun configureMock() {
         val preferencesManager = app.component.getPreferencesManager()
+
         whenever {
             preferencesManager.moduleIdOptions
-        } thenReturn setOf("a", "b", "c", "d")
+        } thenReturn setOf("a", "b", "c", "d", "e")
+
         whenever {
             preferencesManager.selectedModules
-        } thenReturn emptySet()
+        } thenReturn setOf("b")
+    }
+
+    private companion object {
+        const val ID_CHIP_CLOSE_BUTTON = 1
     }
 
 }
