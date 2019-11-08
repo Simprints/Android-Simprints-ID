316c32bb081eab963c086ae64da87c9ae1cc95b8
Alan Camargo <alan@simprints.com>
2019-11-08 12:41:08 +0000

    Fuzzy search draft
    Former-commit-id: 36acd4633d88261184296f96c325d2b98269b2ae

--- id/src/main/java/com/simprints/id/activities/settings/fragments/moduleselection/ModuleQueryFilter.kt
+++ id/src/main/java/com/simprints/id/activities/settings/fragments/moduleselection/ModuleQueryFilter.kt
@@ -2,15 +2,21 @@ package com.simprints.id.activities.settings.fragments.moduleselection
 
 import com.simprints.id.moduleselection.model.Module
 import com.simprints.id.tools.utils.QueryFilter
+import me.xdrop.fuzzywuzzy.FuzzySearch
 
 class ModuleQueryFilter : QueryFilter<Module> {
 
     override fun getFilteredList(items: List<Module>, query: String?): List<Module> {
-        return query?.let { searchTerm ->
-            items.filter { module ->
-                module.name.contains(searchTerm, ignoreCase = true) && !module.isSelected
-            }
-        } ?: emptyList()
+        val limit = 5 // TODO: check if it's really necessary
+        val searchResults = FuzzySearch.extractTop(query, items.map { it.name }, limit).apply {
+            sortByDescending { it.score }
+        }
+
+        val moduleNames = searchResults.map { it.string }
+
+        return items.filter { module ->
+            moduleNames.contains(module.name)
+        }
     }
 
 }
