d1e0bbf4dc703efcd1ddffe74f0d51214b106f36
Alan Camargo <alan@simprints.com>
2019-11-08 15:38:27 +0000

    Fuzzy search ready
    Former-commit-id: 0ad2bc6ee936aa4acc51b3bb8f9bc25e6e070cfb

--- id/src/main/java/com/simprints/id/activities/settings/fragments/moduleselection/ModuleQueryFilter.kt
+++ id/src/main/java/com/simprints/id/activities/settings/fragments/moduleselection/ModuleQueryFilter.kt
@@ -7,13 +7,16 @@ import me.xdrop.fuzzywuzzy.FuzzySearch
 class ModuleQueryFilter : QueryFilter<Module> {
 
     override fun getFilteredList(items: List<Module>, query: String?): List<Module> {
+        if (query.isNullOrEmpty() || query.isBlank() || items.isEmpty())
+            return items
+
         val moduleNames = FuzzySearch.extractAll(query, items.map { it.name })
             .filter { it.score > 50 }
             .sortedByDescending { it.score }
             .map { it.string }
 
         return items.filter { module ->
-            moduleNames.contains(module.name) && !module.isSelected
+            moduleNames.contains(module.name)
         }
     }
 
