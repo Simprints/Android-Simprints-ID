aa6522a85bfe7d8e724fb665aec11d4042c12d67
Alan Camargo <alan@simprints.com>
2019-11-12 11:24:59 +0000

    Adding repository to DI
    Former-commit-id: 76c147d2627e66276133901a718e65a30a121835

--- id/src/main/java/com/simprints/id/activities/settings/fragments/moduleselection/ModuleViewModel.kt
+++ id/src/main/java/com/simprints/id/activities/settings/fragments/moduleselection/ModuleViewModel.kt
@@ -5,11 +5,11 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import com.simprints.id.moduleselection.ModuleRepository
 import com.simprints.id.moduleselection.model.Module
-import com.simprints.id.Application as SimprintsApplication
 
-class ModuleViewModel(application: Application) : AndroidViewModel(application) {
-
-    private val repository = ModuleRepository((application as SimprintsApplication).component)
+class ModuleViewModel(
+    application: Application,
+    private val repository: ModuleRepository
+) : AndroidViewModel(application) {
 
     fun getModules(): LiveData<List<Module>> = repository.getModules()
 
--- id/src/main/java/com/simprints/id/moduleselection/ModuleRepository.kt
+++ id/src/main/java/com/simprints/id/moduleselection/ModuleRepository.kt
@@ -1,65 +1,10 @@
 package com.simprints.id.moduleselection
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import com.simprints.id.data.analytics.crashreport.CrashReportManager
-import com.simprints.id.data.analytics.crashreport.CrashReportTag
-import com.simprints.id.data.analytics.crashreport.CrashReportTrigger
-import com.simprints.id.data.prefs.PreferencesManager
-import com.simprints.id.di.AppComponent
 import com.simprints.id.moduleselection.model.Module
-import javax.inject.Inject
 
-class ModuleRepository(component: AppComponent) {
-
-    @Inject
-    lateinit var preferencesManager: PreferencesManager
-    @Inject
-    lateinit var crashReportManager: CrashReportManager
-
-    private val modules = MutableLiveData<List<Module>>()
-
-    init {
-        component.inject(this)
-    }
-
-    fun getModules(): LiveData<List<Module>> = modules.apply {
-        value = buildModulesList()
-    }
-
-    fun updateModules(modules: List<Module>) {
-        this.modules.value = modules
-        setSelectedModules(modules.filter { it.isSelected })
-    }
-
-    fun getMaxSelectedModules(): Int = MAX_SELECTED_MODULES
-
-    private fun buildModulesList() = preferencesManager.moduleIdOptions.map {
-        Module(it, isModuleSelected(it))
-    }
-
-    private fun isModuleSelected(moduleName: String): Boolean {
-        return preferencesManager.selectedModules.contains(moduleName)
-    }
-
-    private fun setSelectedModules(selectedModules: List<Module>) {
-        preferencesManager.selectedModules = selectedModules.map { it.name }.toSet()
-        logMessageForCrashReport("Modules set to ${preferencesManager.selectedModules}")
-        setCrashlyticsKeyForModules()
-    }
-
-    private fun setCrashlyticsKeyForModules() {
-        crashReportManager.setModuleIdsCrashlyticsKey(preferencesManager.selectedModules)
-    }
-
-    private fun logMessageForCrashReport(message: String) {
-        crashReportManager.logMessageForCrashReport(
-            CrashReportTag.SETTINGS, CrashReportTrigger.UI, message = message
-        )
-    }
-
-    private companion object {
-        const val MAX_SELECTED_MODULES = 6
-    }
-
+interface ModuleRepository {
+    fun getModules(): LiveData<List<Module>>
+    fun updateModules(modules: List<Module>)
+    fun getMaxSelectedModules(): Int
 }
