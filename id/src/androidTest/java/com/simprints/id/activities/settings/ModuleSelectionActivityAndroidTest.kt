7ba88bc910906b9b0167e3115351d8cd2205b61a
Alan Camargo <alan@simprints.com>
2019-11-12 14:58:34 +0000

    Use ActivityScenario
    Former-commit-id: 6187794745999cfd4ca56a32f31fdecfba0b21d4

--- id/src/androidTest/java/com/simprints/id/activities/settings/ModuleSelectionActivityAndroidTest.kt
+++ id/src/androidTest/java/com/simprints/id/activities/settings/ModuleSelectionActivityAndroidTest.kt
@@ -1,8 +1,7 @@
 package com.simprints.id.activities.settings
 
-import android.content.Intent
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.rule.ActivityTestRule
 import br.com.concretesolutions.kappuccino.assertions.VisibilityAssertions.displayed
 import br.com.concretesolutions.kappuccino.assertions.VisibilityAssertions.notDisplayed
 import br.com.concretesolutions.kappuccino.custom.recyclerView.RecyclerViewInteractions.recyclerView
@@ -14,16 +13,9 @@ import com.simprints.id.testtools.AndroidTestConfig
 import com.simprints.testtools.common.di.DependencyRule
 import com.simprints.testtools.common.syntax.whenever
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 
 class ModuleSelectionActivityAndroidTest {
-
-    @Rule
-    @JvmField
-    val rule = ActivityTestRule<ModuleSelectionActivity>(
-        ModuleSelectionActivity::class.java, true, false
-    )
 
     private val app = ApplicationProvider.getApplicationContext<Application>()
     private val preferencesModule = TestPreferencesModule(
@@ -120,7 +112,7 @@ class ModuleSelectionActivityAndroidTest {
             preferencesManager.selectedModules
         } thenReturn setOf("b")
 
-        rule.launchActivity(Intent())
+        ActivityScenario.launch(ModuleSelectionActivity::class.java)
     }
 
     private fun launchWithoutModulesSelected() {
@@ -132,7 +124,7 @@ class ModuleSelectionActivityAndroidTest {
             preferencesManager.selectedModules
         } thenReturn emptySet()
 
-        rule.launchActivity(Intent())
+        ActivityScenario.launch(ModuleSelectionActivity::class.java)
     }
 
 }
