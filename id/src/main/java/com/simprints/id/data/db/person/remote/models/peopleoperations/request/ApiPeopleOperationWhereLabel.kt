114792e39b6e39aecb39bb0fa54ffdda3c49b899
Aashay Gaikwad <aashay@simprints.com>
2019-11-13 17:19:38 +0530

    Created Api models
    Former-commit-id: 02e606f79d56d50c482ddd0bb581edfcff9cd6b6

---
+++ id/src/main/java/com/simprints/id/data/db/person/remote/models/peopleoperations/request/ApiPeopleOperationWhereLabel.kt
@@ -0,0 +1,9 @@
+package com.simprints.id.data.db.person.remote.models.peopleoperations.request
+
+import androidx.annotation.Keep
+
+@Keep
+data class ApiPeopleOperationWhereLabel(
+    val key: String,
+    val value: String
+)
