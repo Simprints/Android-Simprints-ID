836fede3f2401169ad302d6fb1410035b8e186a8
Alan Camargo <alan@simprints.com>
2019-11-13 09:31:38 +0000

    Abstract fuzzy search into extension function
    Former-commit-id: b02f033f2b9986a230fde657cf322e3d29c21d32

--- id/src/main/java/com/simprints/id/activities/settings/fragments/moduleselection/tools/ModuleQueryFilter.kt
+++ id/src/main/java/com/simprints/id/activities/settings/fragments/moduleselection/tools/ModuleQueryFilter.kt
@@ -1,30 +1,20 @@
 package com.simprints.id.activities.settings.fragments.moduleselection.tools
 
 import com.simprints.id.moduleselection.model.Module
+import com.simprints.id.tools.extensions.fuzzySearch
 import com.simprints.id.tools.utils.QueryFilter
-import me.xdrop.fuzzywuzzy.FuzzySearch
 
 class ModuleQueryFilter : QueryFilter<Module> {
 
     override fun getFilteredList(items: List<Module>, query: String?): List<Module> {
         return if (isRelevantQuery(query) && items.isNotEmpty())
-            items.extractResults(query)
+            items.fuzzySearch(query, { it.name })
         else
             items
     }
 
     private fun isRelevantQuery(query: String?): Boolean {
         return query != null && query.isNotEmpty() && query.isNotBlank()
-    }
-
-    private fun List<Module>.extractResults(query: String?): List<Module> {
-        return FuzzySearch.extractAll(
-            query, this, { it.name }, MATCHING_SCORE_THRESHOLD
-        ).sortedByDescending { it.score }.map { it.referent }
-    }
-
-    companion object {
-        private const val MATCHING_SCORE_THRESHOLD = 50
     }
 
 }
