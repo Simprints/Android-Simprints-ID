43efeca11b33904a3a84b3ddd71e0a0e9df3136f
Alan Camargo <alan@simprints.com>
2019-11-13 09:18:46 +0000

    Simplify getFilteredList
    Former-commit-id: b0969d6f5ec36d3a358fe15910a4501dbad0e3d0

--- id/src/main/java/com/simprints/id/activities/settings/fragments/moduleselection/tools/ModuleQueryFilter.kt
+++ id/src/main/java/com/simprints/id/activities/settings/fragments/moduleselection/tools/ModuleQueryFilter.kt
@@ -7,17 +7,20 @@ import me.xdrop.fuzzywuzzy.FuzzySearch
 class ModuleQueryFilter : QueryFilter<Module> {
 
     override fun getFilteredList(items: List<Module>, query: String?): List<Module> {
-        return if (isRelevantQuery(query) && items.isNotEmpty()) {
-            FuzzySearch.extractAll(
-                query, items, { it.name }, MATCHING_SCORE_THRESHOLD
-            ).sortedByDescending { it.score }.map { it.referent }
-        } else {
+        return if (isRelevantQuery(query) && items.isNotEmpty())
+            items.extractResults(query)
+        else
             items
-        }
     }
 
     private fun isRelevantQuery(query: String?): Boolean {
         return query != null && query.isNotEmpty() && query.isNotBlank()
+    }
+
+    private fun List<Module>.extractResults(query: String?): List<Module> {
+        return FuzzySearch.extractAll(
+            query, this, { it.name }, MATCHING_SCORE_THRESHOLD
+        ).sortedByDescending { it.score }.map { it.referent }
     }
 
     companion object {
