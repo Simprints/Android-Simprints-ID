119b8c34ca126bf30afea9ad407bc6826c5a3ca3
fabiotuzza <fabio@simprints.com>
2019-11-15 13:30:21 +0000

    Fixed android test
    Former-commit-id: 6aaefca57a5286d59ff761019a9cdabf15f80095

--- fingerprint/src/androidTest/java/com/simprints/fingerprint/integration/FingerprintRequest.kt
+++ fingerprint/src/androidTest/java/com/simprints/fingerprint/integration/FingerprintRequest.kt
@@ -5,7 +5,7 @@ import androidx.test.core.app.ApplicationProvider
 import com.simprints.fingerprint.controllers.core.eventData.model.fromDomainToCore
 import com.simprints.fingerprint.data.domain.fingerprint.Fingerprint
 import com.simprints.id.Application
-import com.simprints.id.data.db.person.domain.fromDomainToModuleApi
+import com.simprints.id.domain.moduleapi.fingerprint.responses.entities.fromDomainToModuleApi
 import com.simprints.id.orchestrator.steps.fingerprint.FingerprintStepProcessorImpl
 import com.simprints.moduleapi.fingerprint.IFingerIdentifier
 import com.simprints.moduleapi.fingerprint.IFingerprintSample
