b73b40608324af9fc9742bda99318cace414d5d6
fabiotuzza <fabio@simprints.com>
2019-12-13 14:03:16 +0000

    unit tests for count and downsync workers
    Former-commit-id: 717f2b19d6dc44224cd86caf6726a182be257f07

--- id/src/test/java/com/simprints/id/services/scheduledSync/peopleDownSync/controllers/DownSyncManagerImplTest.kt
+++ id/src/test/java/com/simprints/id/services/scheduledSync/peopleDownSync/controllers/DownSyncManagerImplTest.kt
@@ -1,16 +1,13 @@
-package com.simprints.id.services.scheduledSync.peopleDownSync.worker
+package com.simprints.id.services.scheduledSync.peopleDownSync.controllers
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.work.NetworkType
 import androidx.work.WorkInfo
-import com.simprints.id.services.scheduledSync.peopleDownSync.controllers.DownSyncManager
-import com.simprints.id.services.scheduledSync.peopleDownSync.controllers.DownSyncManagerImpl
 import com.simprints.id.services.scheduledSync.peopleDownSync.workers.master.DownSyncMasterWorker
 import com.simprints.id.testtools.TestApplication
 import com.simprints.id.testtools.UnitTestConfig
 import com.simprints.testtools.unit.robolectric.ShadowAndroidXMultiDex
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertTrue
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -20,7 +17,7 @@ import javax.inject.Inject
 
 @RunWith(AndroidJUnit4::class)
 @Config(application = TestApplication::class, shadows = [ShadowAndroidXMultiDex::class])
-class DownSyncManagerTest {
+class DownSyncManagerImplTest {
 
     @Inject lateinit var downSyncManager: DownSyncManager
 
@@ -34,17 +31,17 @@ class DownSyncManagerTest {
     @Test
     fun createOneTimeWorkRequest_shouldCreateWithConstraints() {
         val workSpec = (downSyncManager as DownSyncManagerImpl).buildOneTimeRequest().workSpec
-        assertEquals(NetworkType.CONNECTED, workSpec.constraints.requiredNetworkType)
-        assertEquals(DownSyncMasterWorker::class.qualifiedName, workSpec.workerClassName)
-        assertEquals(WorkInfo.State.ENQUEUED, workSpec.state)
+        Assert.assertEquals(NetworkType.CONNECTED, workSpec.constraints.requiredNetworkType)
+        Assert.assertEquals(DownSyncMasterWorker::class.qualifiedName, workSpec.workerClassName)
+        Assert.assertEquals(WorkInfo.State.ENQUEUED, workSpec.state)
     }
 
     @Test
     fun createPeriodicWorkRequest_shouldCreatePeriodicWithConstraints() {
         val workSpec = (downSyncManager as DownSyncManagerImpl).buildPeriodicRequest().workSpec
-        assertEquals(NetworkType.CONNECTED, workSpec.constraints.requiredNetworkType)
-        assertEquals(DownSyncMasterWorker::class.qualifiedName, workSpec.workerClassName)
-        assertTrue(workSpec.isPeriodic)
-        assertEquals(WorkInfo.State.ENQUEUED, workSpec.state)
+        Assert.assertEquals(NetworkType.CONNECTED, workSpec.constraints.requiredNetworkType)
+        Assert.assertEquals(DownSyncMasterWorker::class.qualifiedName, workSpec.workerClassName)
+        Assert.assertTrue(workSpec.isPeriodic)
+        Assert.assertEquals(WorkInfo.State.ENQUEUED, workSpec.state)
     }
 }
---
+++ id/src/test/java/com/simprints/id/services/scheduledSync/peopleDownSync/workers/count/CountWorkerTest.kt
@@ -0,0 +1,84 @@
+package com.simprints.id.services.scheduledSync.peopleDownSync.worker
+
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import androidx.work.testing.TestListenableWorkerBuilder
+import androidx.work.workDataOf
+import com.simprints.core.tools.json.JsonHelper
+import com.simprints.id.commontesttools.DefaultTestConstants.DEFAULT_PROJECT_ID
+import com.simprints.id.data.db.down_sync_info.domain.PeopleCount
+import com.simprints.id.data.db.down_sync_info.domain.ProjectSyncScope
+import com.simprints.id.domain.modality.Modes
+import com.simprints.id.services.scheduledSync.peopleDownSync.workers.count.CountWorker
+import com.simprints.id.testtools.TestApplication
+import com.simprints.id.testtools.UnitTestConfig
+import com.simprints.testtools.unit.robolectric.ShadowAndroidXMultiDex
+import io.mockk.coEvery
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.MockitoAnnotations
+import org.robolectric.annotation.Config
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+@Config(application = TestApplication::class, shadows = [ShadowAndroidXMultiDex::class])
+class CountWorkerTest {
+
+    private val app = ApplicationProvider.getApplicationContext() as TestApplication
+    private lateinit var countWorker: CountWorker
+
+    @Before
+    fun setUp() {
+        UnitTestConfig(this).fullSetup()
+        MockitoAnnotations.initMocks(this)
+        countWorker = TestListenableWorkerBuilder<CountWorker>(app).build()
+        app.component = mockk(relaxed = true)
+        countWorker.downSyncScopeRepository = mockk(relaxed = true)
+        countWorker.crashReportManager = mockk(relaxed = true)
+        countWorker.personRepository = mockk(relaxed = true)
+        countWorker.resultSetter = mockk(relaxed = true)
+    }
+
+    @Test
+    fun countWorker_shouldExtractTheDownSyncScopeFromTheRepo() = runBlockingTest {
+        countWorker.doWork()
+
+        verify { countWorker.downSyncScopeRepository.getDownSyncScope() }
+    }
+
+    @Test
+    fun countWorker_shouldExecuteTheTaskSuccessfully() = runBlockingTest {
+        val counts = listOf(PeopleCount(1, 1, 1))
+        coEvery { countWorker.personRepository.countToDownSync(any()) } returns counts
+        coEvery { countWorker.downSyncScopeRepository.getDownSyncScope() } returns ProjectSyncScope(DEFAULT_PROJECT_ID, listOf(Modes.FINGERPRINT))
+
+        countWorker.doWork()
+
+        val expectedSuccessfulOutput = workDataOf(CountWorker.COUNT_PROGRESS to JsonHelper.gson.toJson(counts))
+        verify { countWorker.resultSetter.success(expectedSuccessfulOutput) }
+    }
+
+    @Test
+    fun countWorker_anNetworkErrorOccurs_shouldRetry() = runBlockingTest {
+        coEvery { countWorker.personRepository.countToDownSync(any()) } throws Throwable("IO Error")
+        coEvery { countWorker.downSyncScopeRepository.getDownSyncScope() } returns ProjectSyncScope(DEFAULT_PROJECT_ID, listOf(Modes.FINGERPRINT))
+
+        countWorker.doWork()
+
+        verify { countWorker.resultSetter.retry() }
+    }
+
+    @Test
+    fun countWorker_anUnexpectedErrorOccurs_shouldFail() = runBlockingTest {
+        coEvery { countWorker.downSyncScopeRepository.getDownSyncScope() } throws Throwable("Impossible to extract downSyncScope")
+
+        countWorker.doWork()
+
+        verify { countWorker.resultSetter.failure() }
+    }
+}
