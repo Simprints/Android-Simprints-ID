b2ff2c50ecbac72a100cc770f31c6ec3f5c2387f
Ridwan Farouki <ridwan@simprints.com>
2019-12-15 23:56:49 +0000

    Fixed response bytes
    - fingerprintscanner+mock 2019..4.0-dev6
    
    Former-commit-id: fb75f169ffbd7f62303ff17e4f40595ee1894e9b

--- fingerprintscanner/src/main/java/com/simprints/fingerprintscanner/v2/domain/message/un20/responses/GetTemplateResponse.kt
+++ fingerprintscanner/src/main/java/com/simprints/fingerprintscanner/v2/domain/message/un20/responses/GetTemplateResponse.kt
@@ -4,11 +4,12 @@ import com.simprints.fingerprintscanner.v2.domain.message.un20.Un20Response
 import com.simprints.fingerprintscanner.v2.domain.message.un20.models.TemplateData
 import com.simprints.fingerprintscanner.v2.domain.message.un20.models.TemplateType
 import com.simprints.fingerprintscanner.v2.domain.message.un20.models.Un20MessageType
+import com.simprints.fingerprintscanner.v2.tools.primitives.byteArrayOf
 import com.simprints.fingerprintscanner.v2.tools.primitives.unsignedToInt
 
 class GetTemplateResponse(val templateData: TemplateData) : Un20Response(Un20MessageType.GetTemplate(templateData.templateType.byte)) {
 
-    override fun getDataBytes(): ByteArray = templateData.template
+    override fun getDataBytes(): ByteArray = byteArrayOf(templateData.quality.toByte(), templateData.template)
 
     companion object {
         fun fromBytes(minorResponseByte: Byte, data: ByteArray) =
