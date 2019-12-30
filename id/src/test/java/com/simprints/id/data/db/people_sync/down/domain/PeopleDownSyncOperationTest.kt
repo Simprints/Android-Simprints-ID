3b0265f2fdf2d5927941119f0c792b5cc7a084b8
fabiotuzza <fabio@simprints.com>
2019-12-30 12:52:14 +0100

    More unit tests for down sync people + better naming
    Former-commit-id: 3ae103b9e93f7801c290b16a30aed8c1f2b95cb8

--- id/src/test/java/com/simprints/id/data/db/people_sync/down/domain/PeopleDownSyncOperationTest.kt
+++ id/src/test/java/com/simprints/id/data/db/people_sync/down/domain/PeopleDownSyncOperationTest.kt
@@ -1,5 +1,72 @@
 package com.simprints.id.data.db.people_sync.down.domain
 
-import org.junit.Assert.*
+import com.google.common.truth.Truth.assertThat
+import com.simprints.id.commontesttools.DefaultTestConstants.DEFAULT_MODES
+import com.simprints.id.commontesttools.DefaultTestConstants.DEFAULT_MODULE_ID
+import com.simprints.id.commontesttools.DefaultTestConstants.DEFAULT_PROJECT_ID
+import com.simprints.id.commontesttools.DefaultTestConstants.DEFAULT_USER_ID
+import com.simprints.id.data.db.people_sync.down.domain.PeopleDownSyncOperation.Companion.buildModuleSyncOperation
+import com.simprints.id.data.db.people_sync.down.domain.PeopleDownSyncOperation.Companion.buildProjectSyncOperation
+import com.simprints.id.data.db.people_sync.down.domain.PeopleDownSyncOperation.Companion.buildUserSyncOperation
+import com.simprints.id.data.db.people_sync.down.domain.PeopleDownSyncOperationResult.DownSyncState.COMPLETE
+import com.simprints.id.data.db.people_sync.down.local.DbPeopleDownSyncOperationKey
+import org.junit.Test
 
-class PeopleDownSyncOperationTest
+class PeopleDownSyncOperationTest {
+
+    companion object {
+        const val LAST_PATIENT_ID = "lastPatientId"
+        const val LAST_PATIENT_UPDATED_AT = 1L
+        const val LAST_SYNC_TIME = 2L
+
+    }
+
+    val result = PeopleDownSyncOperationResult(COMPLETE, LAST_PATIENT_ID, LAST_PATIENT_UPDATED_AT, LAST_SYNC_TIME)
+
+    @Test
+    fun testBuildProjectSyncOperation() {
+        val op = buildProjectSyncOperation(DEFAULT_PROJECT_ID, DEFAULT_MODES, null)
+        assertThat(op.projectId).isEqualTo(DEFAULT_PROJECT_ID)
+        assertThat(op.userId).isNull()
+        assertThat(op.moduleId).isNull()
+        assertThat(op.modes).isEqualTo(DEFAULT_MODES)
+        assertThat(op.lastResult).isNull()
+    }
+
+    @Test
+    fun testBuildUserSyncOperation() {
+        val op = buildUserSyncOperation(DEFAULT_PROJECT_ID, DEFAULT_USER_ID, DEFAULT_MODES, null)
+        assertThat(op.projectId).isEqualTo(DEFAULT_PROJECT_ID)
+        assertThat(op.userId).isEqualTo(DEFAULT_USER_ID)
+        assertThat(op.moduleId).isNull()
+        assertThat(op.modes).isEqualTo(DEFAULT_MODES)
+        assertThat(op.lastResult).isNull()
+    }
+
+    @Test
+    fun testBuildModuleSyncOperation() {
+        val op = buildModuleSyncOperation(DEFAULT_PROJECT_ID, DEFAULT_MODULE_ID, DEFAULT_MODES, result)
+        assertThat(op.projectId).isEqualTo(DEFAULT_PROJECT_ID)
+        assertThat(op.userId).isNull()
+        assertThat(op.moduleId).isEqualTo(DEFAULT_MODULE_ID)
+        assertThat(op.modes).isEqualTo(DEFAULT_MODES)
+        assertThat(op.lastResult).isEqualTo(result)
+    }
+
+    @Test
+    fun testOpFromDomainToDb() {
+        val op = buildProjectSyncOperation(DEFAULT_PROJECT_ID, DEFAULT_MODES, result)
+
+        with(op.fromDomainToDb()) {
+            assertThat(id.key).isEqualTo(DbPeopleDownSyncOperationKey(DEFAULT_PROJECT_ID, DEFAULT_MODES, null).key)
+            assertThat(projectId).isEqualTo(DEFAULT_PROJECT_ID)
+            assertThat(userId).isNull()
+            assertThat(moduleId).isNull()
+            assertThat(modes).isEqualTo(DEFAULT_MODES)
+            assertThat(lastState).isEqualTo(COMPLETE)
+            assertThat(lastPatientId).isEqualTo(LAST_PATIENT_ID)
+            assertThat(lastPatientUpdatedAt).isEqualTo(LAST_PATIENT_UPDATED_AT)
+            assertThat(lastSyncTime).isEqualTo(LAST_SYNC_TIME)
+        }
+    }
+}
