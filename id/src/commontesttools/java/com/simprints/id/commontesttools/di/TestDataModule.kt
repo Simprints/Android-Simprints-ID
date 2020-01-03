335d6edd3651db4631f06c473832f12fb7c73413
Alan Camargo <alan@simprints.com>
2020-01-03 22:08:11 +0000

    Write tests for worker
    Former-commit-id: 9b68591ba0637a91ff980a223fe3a1f3b654c383

--- id/src/commontesttools/java/com/simprints/id/commontesttools/di/TestDataModule.kt
+++ id/src/commontesttools/java/com/simprints/id/commontesttools/di/TestDataModule.kt
@@ -2,6 +2,9 @@ package com.simprints.id.commontesttools.di
 
 import android.content.Context
 import com.simprints.id.data.db.common.RemoteDbManager
+import com.simprints.id.data.db.image.local.ImageLocalDataSource
+import com.simprints.id.data.db.image.remote.ImageRemoteDataSource
+import com.simprints.id.data.db.image.repository.ImageRepository
 import com.simprints.id.data.db.person.PersonRepository
 import com.simprints.id.data.db.person.local.PersonLocalDataSource
 import com.simprints.id.data.db.person.remote.PersonRemoteDataSource
@@ -15,39 +18,99 @@ import com.simprints.id.di.DataModule
 import com.simprints.id.services.scheduledSync.peopleUpsync.PeopleUpSyncMaster
 import com.simprints.testtools.common.di.DependencyRule
 
-class TestDataModule(private val syncInfoLocalDataSourceRule: DependencyRule = DependencyRule.RealRule,
-                     private val projectLocalDataSourceRule: DependencyRule = DependencyRule.RealRule,
-                     private val projectRemoteDataSourceRule: DependencyRule = DependencyRule.RealRule,
-                     private val projectRepositoryRule: DependencyRule = DependencyRule.RealRule,
-                     private val personLocalDataSource: DependencyRule = DependencyRule.RealRule,
-                     private val personRepositoryRule: DependencyRule = DependencyRule.RealRule) : DataModule() {
+class TestDataModule(
+    private val syncInfoLocalDataSourceRule: DependencyRule = DependencyRule.RealRule,
+    private val projectLocalDataSourceRule: DependencyRule = DependencyRule.RealRule,
+    private val projectRemoteDataSourceRule: DependencyRule = DependencyRule.RealRule,
+    private val projectRepositoryRule: DependencyRule = DependencyRule.RealRule,
+    private val personLocalDataSourceRule: DependencyRule = DependencyRule.RealRule,
+    private val personRepositoryRule: DependencyRule = DependencyRule.RealRule,
+    private val imageLocalDataSourceRule: DependencyRule = DependencyRule.RealRule,
+    private val imageRemoteDataSourceRule: DependencyRule = DependencyRule.RealRule,
+    private val imageRepositoryRule: DependencyRule = DependencyRule.RealRule
+) : DataModule() {
 
-    override fun provideSyncInfoLocalDataSource(ctx: Context,
-                                                secureDataManager: SecureDataManager,
-                                                loginInfoManager: LoginInfoManager): SyncInfoLocalDataSource =
-        syncInfoLocalDataSourceRule.resolveDependency { super.provideSyncInfoLocalDataSource(ctx, secureDataManager, loginInfoManager) }
+    override fun provideSyncInfoLocalDataSource(
+        ctx: Context,
+        secureDataManager: SecureDataManager,
+        loginInfoManager: LoginInfoManager
+    ): SyncInfoLocalDataSource {
+        return syncInfoLocalDataSourceRule.resolveDependency {
+            super.provideSyncInfoLocalDataSource(ctx, secureDataManager, loginInfoManager)
+        }
+    }
 
-    override fun provideProjectLocalDataSource(ctx: Context,
-                                               secureDataManager: SecureDataManager,
-                                               loginInfoManager: LoginInfoManager): ProjectLocalDataSource =
-        projectLocalDataSourceRule.resolveDependency { super.provideProjectLocalDataSource(ctx, secureDataManager, loginInfoManager) }
+    override fun provideProjectLocalDataSource(
+        ctx: Context,
+        secureDataManager: SecureDataManager,
+        loginInfoManager: LoginInfoManager
+    ): ProjectLocalDataSource {
+        return projectLocalDataSourceRule.resolveDependency {
+            super.provideProjectLocalDataSource(ctx, secureDataManager, loginInfoManager)
+        }
+    }
 
-    override fun provideProjectRemoteDataSource(remoteDbManager: RemoteDbManager): ProjectRemoteDataSource =
-        projectRemoteDataSourceRule.resolveDependency { super.provideProjectRemoteDataSource(remoteDbManager) }
+    override fun provideProjectRemoteDataSource(
+        remoteDbManager: RemoteDbManager
+    ): ProjectRemoteDataSource {
+        return projectRemoteDataSourceRule.resolveDependency {
+            super.provideProjectRemoteDataSource(remoteDbManager)
+        }
+    }
+
+    override fun provideProjectRepository(
+        projectLocalDataSource: ProjectLocalDataSource,
+        projectRemoteDataSource: ProjectRemoteDataSource
+    ): ProjectRepository {
+        return projectRepositoryRule.resolveDependency {
+            super.provideProjectRepository(projectLocalDataSource, projectRemoteDataSource)
+        }
+    }
+
+    override fun providePersonRepository(
+        personLocalDataSource: PersonLocalDataSource,
+        personRemoteDataSource: PersonRemoteDataSource,
+        peopleUpSyncMaster: PeopleUpSyncMaster
+    ): PersonRepository {
+        return personRepositoryRule.resolveDependency {
+            super.providePersonRepository(
+                personLocalDataSource,
+                personRemoteDataSource,
+                peopleUpSyncMaster
+            )
+        }
+    }
 
 
-    override fun provideProjectRepository(projectLocalDataSource: ProjectLocalDataSource,
-                                          projectRemoteDataSource: ProjectRemoteDataSource): ProjectRepository =
-        projectRepositoryRule.resolveDependency { super.provideProjectRepository(projectLocalDataSource, projectRemoteDataSource) }
+    override fun providePersonLocalDataSource(
+        ctx: Context,
+        secureDataManager: SecureDataManager,
+        loginInfoManager: LoginInfoManager
+    ): PersonLocalDataSource {
+        return personLocalDataSourceRule.resolveDependency {
+            super.providePersonLocalDataSource(ctx, secureDataManager, loginInfoManager)
+        }
+    }
 
-    override fun providePersonRepository(personLocalDataSource: PersonLocalDataSource,
-                                         personRemoteDataSource: PersonRemoteDataSource,
-                                         peopleUpSyncMaster: PeopleUpSyncMaster): PersonRepository =
-        personRepositoryRule.resolveDependency { super.providePersonRepository(personLocalDataSource, personRemoteDataSource, peopleUpSyncMaster) }
+    override fun provideImageLocalDataSource(context: Context): ImageLocalDataSource {
+        return imageLocalDataSourceRule.resolveDependency {
+            super.provideImageLocalDataSource(context)
+        }
+    }
 
+    override fun provideImageRemoteDataSource(): ImageRemoteDataSource {
+        return imageRemoteDataSourceRule.resolveDependency {
+            super.provideImageRemoteDataSource()
+        }
+    }
 
-    override fun providePersonLocalDataSource(ctx: Context,
-                                              secureDataManager: SecureDataManager,
-                                              loginInfoManager: LoginInfoManager): PersonLocalDataSource =
-        personLocalDataSource.resolveDependency { super.providePersonLocalDataSource(ctx, secureDataManager, loginInfoManager) }
+    override fun provideImageRepository(
+        localDataSource: ImageLocalDataSource,
+        remoteDataSource: ImageRemoteDataSource
+    ): ImageRepository {
+        return imageRepositoryRule.resolveDependency {
+            super.provideImageRepository(localDataSource, remoteDataSource)
+        }
+    }
+
 }
---
+++ id/src/test/java/com/simprints/id/services/scheduledSync/imageUpSync/ImageUpSyncWorkerTest.kt
@@ -0,0 +1,109 @@
+package com.simprints.id.services.scheduledSync.imageUpSync
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.work.ListenableWorker
+import androidx.work.WorkerParameters
+import com.google.common.truth.Truth.assertThat
+import com.simprints.core.images.SecuredImageRef
+import com.simprints.id.commontesttools.di.TestAppModule
+import com.simprints.id.commontesttools.di.TestDataModule
+import com.simprints.id.data.db.image.remote.UploadResult
+import com.simprints.id.data.db.image.repository.ImageRepository
+import com.simprints.id.testtools.TestApplication
+import com.simprints.id.testtools.UnitTestConfig
+import com.simprints.testtools.common.di.DependencyRule
+import com.simprints.testtools.common.syntax.wheneverOnSuspend
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.robolectric.annotation.Config
+import javax.inject.Inject
+
+@RunWith(AndroidJUnit4::class)
+@Config(application = TestApplication::class)
+class ImageUpSyncWorkerTest {
+
+    private val app = ApplicationProvider.getApplicationContext() as TestApplication
+
+    @Inject
+    lateinit var context: Context
+
+    @Inject
+    lateinit var repository: ImageRepository
+
+    @Mock
+    lateinit var workerParams: WorkerParameters
+
+    private lateinit var imageUpSyncWorker: ImageUpSyncWorker
+
+    private val appModule by lazy {
+        TestAppModule(app)
+    }
+
+    private val dataModule by lazy {
+        TestDataModule(
+            imageLocalDataSourceRule = DependencyRule.MockRule,
+            imageRepositoryRule = DependencyRule.MockRule
+        )
+    }
+
+    @Before
+    fun setUp() {
+        UnitTestConfig(this, appModule = appModule, dataModule = dataModule).fullSetup()
+        MockitoAnnotations.initMocks(this)
+        imageUpSyncWorker = ImageUpSyncWorker(context, workerParams)
+    }
+
+    @Test
+    fun whenAllUploadsAreSuccessful_shouldReturnSuccess() {
+        mockUploadResults()
+        val workResult = imageUpSyncWorker.doWork()
+
+        assertThat(workResult).isEqualTo(ListenableWorker.Result.success())
+    }
+
+    @Test
+    fun whenAnyUploadFails_shouldReturnRetry() {
+        mockUploadResults(addFailedUpload = true)
+        val workResult = imageUpSyncWorker.doWork()
+
+        assertThat(workResult).isEqualTo(ListenableWorker.Result.retry())
+    }
+
+    @Test
+    fun withNoImagesToUpload_shouldReturnSuccess() {
+        wheneverOnSuspend(repository) {
+            uploadImages()
+        } thenOnBlockingReturn emptyList()
+
+        val workResult = imageUpSyncWorker.doWork()
+
+        assertThat(workResult).isEqualTo(ListenableWorker.Result.success())
+    }
+
+    private fun mockUploadResults(addFailedUpload: Boolean = false) {
+        val uploadResults = mutableListOf(
+            UploadResult(SecuredImageRef(PATH), UploadResult.Status.SUCCESSFUL),
+            UploadResult(SecuredImageRef(PATH), UploadResult.Status.SUCCESSFUL),
+            UploadResult(SecuredImageRef(PATH), UploadResult.Status.SUCCESSFUL),
+            UploadResult(SecuredImageRef(PATH), UploadResult.Status.SUCCESSFUL),
+            UploadResult(SecuredImageRef(PATH), UploadResult.Status.SUCCESSFUL)
+        ).apply {
+            if (addFailedUpload)
+                add(UploadResult(SecuredImageRef(PATH), UploadResult.Status.FAILED))
+        }
+
+        wheneverOnSuspend(repository) {
+            uploadImages()
+        } thenOnBlockingReturn uploadResults
+    }
+
+    companion object {
+        private const val PATH = "mock/path"
+    }
+
+}
