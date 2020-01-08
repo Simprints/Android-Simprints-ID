0c5e2ef9a4d999c67d4fd419c1a18ddc29f1b6c7
Fabio Tuzza <fabio@simprints.com>
2020-01-08 16:41:42 +0000

    Removing old dashboard code.
    Former-commit-id: 54ec8dc83962df736fd3d9d760271153d3b2f73c

--- id/src/main/java/com/simprints/id/activities/dashboard/DashboardActivity.kt
+++ id/src/main/java/com/simprints/id/activities/dashboard/DashboardActivity.kt
@@ -4,28 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DefaultItemAnimator
-import androidx.recyclerview.widget.RecyclerView
 import com.simprints.id.Application
 import com.simprints.id.R
 import com.simprints.id.activities.alert.AlertActivityHelper
-import com.simprints.id.activities.dashboard.views.WrapContentLinearLayoutManager
 import com.simprints.id.activities.debug.DebugActivity
 import com.simprints.id.activities.longConsent.PrivacyNoticeActivity
 import com.simprints.id.activities.requestLogin.RequestLoginActivity
 import com.simprints.id.activities.settings.SettingsActivity
-import com.simprints.id.data.loginInfo.LoginInfoManager
-import com.simprints.id.data.prefs.PreferencesManager
 import com.simprints.id.tools.AndroidResourcesHelper
-import com.simprints.id.tools.extensions.showToast
 import kotlinx.android.synthetic.main.activity_dashboard.*
-import kotlinx.android.synthetic.main.content_dashboard.*
 import javax.inject.Inject
 
-class DashboardActivity : AppCompatActivity(), DashboardContract.View {
+class DashboardActivity : AppCompatActivity() {
 
-    @Inject lateinit var preferences: PreferencesManager
-    @Inject lateinit var loginInfoManager: LoginInfoManager
 
     @Inject
     lateinit var androidResourcesHelper: AndroidResourcesHelper
@@ -36,9 +27,6 @@ class DashboardActivity : AppCompatActivity(), DashboardContract.View {
 
     }
 
-    override lateinit var viewPresenter: DashboardContract.Presenter
-    private lateinit var cardsViewAdapter: DashboardCardAdapter
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
@@ -48,58 +36,12 @@ class DashboardActivity : AppCompatActivity(), DashboardContract.View {
 
         setupActionBar()
 
-        viewPresenter = DashboardPresenter(this, component)
         setMenuItemClickListener()
-
-        initCards()
     }
 
     private fun setupActionBar() {
         dashboardToolbar.title = androidResourcesHelper.getString(R.string.dashboard_label)
         setSupportActionBar(dashboardToolbar)
-    }
-
-    private fun initCards() {
-        initRecyclerCardViews(viewPresenter)
-        initSwipeRefreshLayout(viewPresenter)
-    }
-
-    private fun initRecyclerCardViews(viewPresenter: DashboardContract.Presenter) {
-        cardsViewAdapter = DashboardCardAdapter(viewPresenter.cardsViewModelsList)
-        (dashboardCardsView as RecyclerView).also {
-            it.setHasFixedSize(false)
-            it.itemAnimator = DefaultItemAnimator()
-            it.layoutManager = WrapContentLinearLayoutManager(this)
-            it.adapter = cardsViewAdapter
-        }
-    }
-
-    private fun initSwipeRefreshLayout(viewPresenter: DashboardContract.Presenter) {
-        swipeRefreshLayout.setOnRefreshListener {
-            viewPresenter.userDidWantToRefreshCardsIfPossible()
-        }
-    }
-
-    override fun notifyCardViewChanged(position: Int) {
-        runOnUiThread {
-            cardsViewAdapter.notifyItemChanged(position)
-        }
-    }
-
-    override fun updateCardViews() {
-        runOnUiThread {
-            dashboardCardsView.recycledViewPool.clear()
-            cardsViewAdapter.notifyDataSetChanged()
-        }
-    }
-
-    override fun stopRequestIfRequired() {
-        swipeRefreshLayout.isRefreshing = false
-    }
-
-    override fun onResume() {
-        super.onResume()
-        viewPresenter.start()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -131,24 +73,12 @@ class DashboardActivity : AppCompatActivity(), DashboardContract.View {
         }
 
         if (resultCode == LOGOUT_RESULT_CODE && requestCode == SETTINGS_ACTIVITY_REQUEST_CODE) {
-            viewPresenter.logout()
+            startCheckLoginActivityAndFinish()
         }
     }
 
-    override fun startCheckLoginActivityAndFinish(){
+    private fun startCheckLoginActivityAndFinish() {
         startActivity(Intent(this, RequestLoginActivity::class.java))
         finish()
-    }
-
-    override fun showToastForUserOffline() {
-        showToast(androidResourcesHelper, R.string.login_no_network)
-    }
-
-    override fun showToastForRecordsUpToDate() {
-        showToast(androidResourcesHelper, R.string.records_up_to_date)
-    }
-
-    override fun showToastForNoModulesSelected() {
-        showToast(androidResourcesHelper, R.string.settings_no_modules_toast)
     }
 }
