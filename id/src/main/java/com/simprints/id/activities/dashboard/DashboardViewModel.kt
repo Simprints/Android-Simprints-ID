95ce7f1e18410ac368e023937928b4931b061197
Fabio Tuzza <fabio@simprints.com>
2020-01-10 11:38:51 +0000

    WIP created Dashboard viewmodel
    Former-commit-id: f85a8e1145698665eae9746fe916dbd27350848e

---
+++ id/src/main/java/com/simprints/id/activities/dashboard/DashboardViewModel.kt
@@ -0,0 +1,7 @@
+package com.simprints.id.activities.dashboard
+
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+}
---
+++ id/src/main/java/com/simprints/id/activities/dashboard/DashboardViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.simprints.id.activities.dashboard
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.simprints.id.activities.consent.ConsentViewModel
+
+class DashboardViewModelFactory() : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return if (modelClass.isAssignableFrom(ConsentViewModel::class.java)) {
+            DashboardViewModel() as T
+        } else {
+            throw IllegalArgumentException("ViewModel Not Found")
+        }
+    }
+}
