2e212af9d1fe6bf34d43103d795b5b493142ffa4
Fabio Tuzza <fabio@simprints.com>
2020-01-13 15:30:55 +0000

    Updated UI for sync card
    Former-commit-id: 4e1b5da3f0bbc27f420f6519cb66a5e6f1ff1d93

--- id/src/main/java/com/simprints/id/activities/dashboard/DashboardViewModelFactory.kt
+++ id/src/main/java/com/simprints/id/activities/dashboard/DashboardViewModelFactory.kt
@@ -8,7 +8,7 @@ class DashboardViewModelFactory() : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return if (modelClass.isAssignableFrom(ConsentViewModel::class.java)) {
+        return if (modelClass.isAssignableFrom(DashboardViewModel::class.java)) {
             DashboardViewModel() as T
         } else {
             throw IllegalArgumentException("ViewModel Not Found")
