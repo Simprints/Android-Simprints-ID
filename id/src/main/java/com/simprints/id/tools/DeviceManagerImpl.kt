6ab445183280526c02969b4719d61ee51fa4165d
Alan Camargo <alan@simprints.com>
2020-01-16 08:29:53 +0000

    Simplify root detection
    Former-commit-id: f1dd203a306114ed06e351a0e0789745863396ec

--- id/src/main/java/com/simprints/id/tools/DeviceManagerImpl.kt
+++ id/src/main/java/com/simprints/id/tools/DeviceManagerImpl.kt
@@ -5,8 +5,6 @@ import com.scottyab.rootbeer.RootBeer
 
 class DeviceManagerImpl(private val context: Context) : DeviceManager {
 
-    override fun isDeviceRooted(): Boolean = RootBeer(context).run {
-        isRooted || isRootedWithoutBusyBoxCheck
-    }
+    override fun isDeviceRooted(): Boolean = RootBeer(context).isRootedWithoutBusyBoxCheck
 
 }
