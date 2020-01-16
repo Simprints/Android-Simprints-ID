8c72ad1499c8ee1efc86c9e1813463749d620797
Alan Camargo <alan@simprints.com>
2020-01-16 09:59:42 +0000

    Add tests
    Former-commit-id: d4dc1932dda87c9cc672fcb02da96f2b8669bf27

--- id/src/main/java/com/simprints/id/exceptions/safe/secure/RootedDeviceException.kt
+++
@@ -1,5 +0,0 @@
-package com.simprints.id.exceptions.safe.secure
-
-import com.simprints.id.exceptions.safe.SafeException
-
-class RootedDeviceException(message: String = "RootedDeviceException") : SafeException(message)
