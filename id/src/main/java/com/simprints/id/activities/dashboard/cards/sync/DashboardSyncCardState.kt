ca8c27c5047dbff798d717c26ce7a15d9c5177d0
Fabio <fabio@simprints.com>
2020-01-16 15:20:38 +0000

    WIP binding logic to sync card ui
    Former-commit-id: 6d5799d6eec38e4f3fbf76fc62ebf2ff34271dea

--- id/src/main/java/com/simprints/id/activities/dashboard/cards/sync/DashboardSyncCardState.kt
+++ id/src/main/java/com/simprints/id/activities/dashboard/cards/sync/DashboardSyncCardState.kt
@@ -6,14 +6,14 @@ sealed class DashboardSyncCardState(open val lastSyncTime: Date) {
 
     data class SyncDefault(override val lastSyncTime: Date) : DashboardSyncCardState(lastSyncTime)
 
-    data class SyncFailed(override val lastSyncTime: Date) : DashboardSyncCardState(lastSyncTime)
-    data class SyncTryAgain(override val lastSyncTime: Date) : DashboardSyncCardState(lastSyncTime)
-    data class SyncNoModules(override val lastSyncTime: Date) : DashboardSyncCardState(lastSyncTime)
-    data class SyncOffline(override val lastSyncTime: Date) : DashboardSyncCardState(lastSyncTime)
-
     data class SyncProgress(override val lastSyncTime: Date, val progress: Int, val total: Int) : DashboardSyncCardState(lastSyncTime)
     data class SyncConnecting(override val lastSyncTime: Date, val progress: Int, val total: Int) : DashboardSyncCardState(lastSyncTime)
 
+    data class SyncFailed(override val lastSyncTime: Date) : DashboardSyncCardState(lastSyncTime)
+    data class SyncTryAgain(override val lastSyncTime: Date) : DashboardSyncCardState(lastSyncTime)
     data class SyncComplete(override val lastSyncTime: Date) : DashboardSyncCardState(lastSyncTime)
+
+    data class SyncNoModules(override val lastSyncTime: Date) : DashboardSyncCardState(lastSyncTime)
+    data class SyncOffline(override val lastSyncTime: Date) : DashboardSyncCardState(lastSyncTime)
 }
 
