4a88e705b299e9da214f13b5c45fdfbd2b51bb06
Ridwan Farouki <ridwan@simprints.com>
2020-01-17 07:51:36 +0000

    Added coroutine context
    Former-commit-id: 87c37f4b441a7632647e756d8e09e58034322d7f

--- fingerprint/src/main/java/com/simprints/fingerprint/controllers/core/image/FingerprintImageManagerImpl.kt
+++ fingerprint/src/main/java/com/simprints/fingerprint/controllers/core/image/FingerprintImageManagerImpl.kt
@@ -1,9 +1,12 @@
 package com.simprints.fingerprint.controllers.core.image
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
 class FingerprintImageManagerImpl : FingerprintImageManager {
 
-    override suspend fun save(imageBytes: ByteArray): FingerprintImageRef {
+    override suspend fun save(imageBytes: ByteArray): FingerprintImageRef = withContext(Dispatchers.IO) {
         // TODO : To delegate to core image manager
-        return FingerprintImageRef("image uri")
+        FingerprintImageRef("image uri")
     }
 }
