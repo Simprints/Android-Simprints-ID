054656d3999567a94a1ad15205d1287d0b0a87af
Alan Camargo <alan@simprints.com>
2020-01-21 10:48:37 +0000

    Implement card data binding
    Former-commit-id: 0d0363ece2e25d313af02812c79e9b75424a62ce

--- id/src/main/java/com/simprints/id/activities/dashboard/cards/project/DashboardProjectDetailsCardDisplayerImpl.kt
+++ id/src/main/java/com/simprints/id/activities/dashboard/cards/project/DashboardProjectDetailsCardDisplayerImpl.kt
@@ -3,6 +3,7 @@ package com.simprints.id.activities.dashboard.cards.project
 import android.content.Context
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.TextView
 import com.simprints.id.R
 import com.simprints.id.tools.AndroidResourcesHelper
 import org.jetbrains.anko.layoutInflater
@@ -20,12 +21,37 @@ class DashboardProjectDetailsCardDisplayerImpl(
         card = inflateCard(root)
     }
 
+    // TODO STOPSHIP use real values
     override fun displayProjectDetails() {
-
+        with(card) {
+            setTitle("World domination project")
+            setCurrentUser("Pinky and The Brain")
+            setScannerUsed("SP1234")
+        }
     }
 
     private fun inflateCard(root: LinearLayout) = context.layoutInflater.inflate(
         R.layout.activity_dashboard_card_project_details, root, false
     )
+
+    private fun View.setTitle(title: String) {
+        findViewById<TextView>(R.id.dashboard_project_details_card_title).text = title
+    }
+
+    private fun View.setCurrentUser(currentUser: String) {
+        findViewById<TextView>(
+            R.id.dashboard_project_details_card_current_user
+        ).text = androidResourcesHelper.getString(
+            R.string.dashboard_card_current_user, arrayOf(currentUser)
+        )
+    }
+
+    private fun View.setScannerUsed(scannerUsed: String) {
+        findViewById<TextView>(
+            R.id.dashboard_project_details_card_scanner_used
+        ).text = androidResourcesHelper.getString(
+            R.string.dashboard_card_scanner_used, arrayOf(scannerUsed)
+        )
+    }
 
 }
