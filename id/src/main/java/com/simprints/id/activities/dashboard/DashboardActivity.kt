a4f4d5f616577b31e06ddd974dde21a5c113c096
Alan Camargo <alan@simprints.com>
2020-01-21 11:55:00 +0000

    Display project details card
    Former-commit-id: 6f3ade9a2cb87d2184617036e915b1caff4f506e

--- id/src/main/java/com/simprints/id/activities/dashboard/DashboardActivity.kt
+++ id/src/main/java/com/simprints/id/activities/dashboard/DashboardActivity.kt
@@ -4,23 +4,38 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.simprints.id.Application
 import com.simprints.id.R
 import com.simprints.id.activities.alert.AlertActivityHelper
+import com.simprints.id.activities.dashboard.cards.project.DashboardProjectDetailsCardDisplayer
 import com.simprints.id.activities.debug.DebugActivity
 import com.simprints.id.activities.longConsent.PrivacyNoticeActivity
 import com.simprints.id.activities.requestLogin.RequestLoginActivity
 import com.simprints.id.activities.settings.SettingsActivity
+import com.simprints.id.data.db.project.ProjectRepository
+import com.simprints.id.data.loginInfo.LoginInfoManager
+import com.simprints.id.data.prefs.PreferencesManager
 import com.simprints.id.tools.AndroidResourcesHelper
 import kotlinx.android.synthetic.main.activity_dashboard.*
+import kotlinx.android.synthetic.main.activity_dashboard_card_project_details.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class DashboardActivity : AppCompatActivity() {
 
     @Inject lateinit var androidResourcesHelper: AndroidResourcesHelper
+    @Inject lateinit var projectRepository: ProjectRepository
+    @Inject lateinit var loginInfoManager: LoginInfoManager
+    @Inject lateinit var preferencesManager: PreferencesManager
+    @Inject lateinit var projectDetailsCardDisplayer: DashboardProjectDetailsCardDisplayer
+
     lateinit var viewModel: DashboardViewModel
-    private val viewModelFactory = DashboardViewModelFactory()
+
+    private lateinit var viewModelFactory: DashboardViewModelFactory
 
     companion object {
         private const val SETTINGS_ACTIVITY_REQUEST_CODE = 1
@@ -34,11 +49,38 @@ class DashboardActivity : AppCompatActivity() {
         component.inject(this)
         title = androidResourcesHelper.getString(R.string.dashboard_label)
 
-        viewModel = ViewModelProvider(this, viewModelFactory).get(DashboardViewModel::class.java)
-
+        setupCards()
+        setupViewModel()
         setupActionBar()
-
         setMenuItemClickListener()
+
+        observeForProjectDetails()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val potentialAlertScreenResponse = AlertActivityHelper.extractPotentialAlertScreenResponse(data)
+        if (potentialAlertScreenResponse != null) {
+            finish()
+        }
+
+        if (resultCode == LOGOUT_RESULT_CODE && requestCode == SETTINGS_ACTIVITY_REQUEST_CODE) {
+            startCheckLoginActivityAndFinish()
+        }
+    }
+
+    private fun setupCards() {
+        projectDetailsCardDisplayer.initRoot(dashboard_project_details_card)
+    }
+
+    private fun setupViewModel() {
+        viewModelFactory = DashboardViewModelFactory(
+            projectRepository, loginInfoManager, preferencesManager
+        )
+
+        viewModel = ViewModelProvider(this, viewModelFactory).get(
+            DashboardViewModel::class.java
+        )
     }
 
     private fun setupActionBar() {
@@ -67,15 +109,14 @@ class DashboardActivity : AppCompatActivity() {
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        val potentialAlertScreenResponse = AlertActivityHelper.extractPotentialAlertScreenResponse(data)
-        if (potentialAlertScreenResponse != null) {
-            finish()
-        }
-
-        if (resultCode == LOGOUT_RESULT_CODE && requestCode == SETTINGS_ACTIVITY_REQUEST_CODE) {
-            startCheckLoginActivityAndFinish()
+    private fun observeForProjectDetails() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val projectDetails = viewModel.getProjectDetails()
+            CoroutineScope(Dispatchers.Main).launch {
+                projectDetails.observe(this@DashboardActivity, Observer {
+                    projectDetailsCardDisplayer.displayProjectDetails(it)
+                })
+            }
         }
     }
 
---
+++ id/src/main/java/com/simprints/id/activities/dashboard/cards/project/model/DashboardProjectWrapper.kt
@@ -0,0 +1,3 @@
+package com.simprints.id.activities.dashboard.cards.project.model
+
+data class DashboardProjectWrapper(val title: String, val lastUser: String, val lastScanner: String)
