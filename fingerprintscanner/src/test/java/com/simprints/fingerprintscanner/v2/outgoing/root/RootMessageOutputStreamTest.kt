6ba7c51c1004c2fa8a6f049d211b3af8320b2761
Ridwan Farouki <ridwan@simprints.com>
2020-01-21 18:11:33 +0000

    Wrote outgoing tests for RootMode
    Former-commit-id: ecb5539df6f47432c3fd90ff5156d602f684b451

---
+++ fingerprintscanner/src/test/java/com/simprints/fingerprintscanner/v2/outgoing/root/RootMessageOutputStreamTest.kt
@@ -0,0 +1,42 @@
+package com.simprints.fingerprintscanner.v2.outgoing.root
+
+import com.google.common.truth.Truth.assertThat
+import com.nhaarman.mockitokotlin2.eq
+import com.simprints.fingerprintscanner.v2.domain.root.commands.EnterMainModeCommand
+import com.simprints.fingerprintscanner.v2.tools.reactive.toFlowable
+import com.simprints.testtools.common.syntax.mock
+import com.simprints.testtools.common.syntax.whenever
+import com.simprints.testtools.unit.reactive.testSubscribe
+import org.junit.Test
+import java.io.PipedInputStream
+import java.io.PipedOutputStream
+
+class RootMessageOutputStreamTest {
+
+    private val rootMessageSerializerMock: RootMessageSerializer = mock()
+
+    @Test
+    fun messageOutputStream_sendMessage_serializesAndDispatchesMessageCorrectly() {
+        val message = EnterMainModeCommand()
+        val packets = listOf(byteArrayOf(0x10, 0x20, 0x30), byteArrayOf(0x40, 0x50))
+
+        whenever(rootMessageSerializerMock) { serialize(eq(message)) } thenReturn packets
+
+        val messageOutputStream = RootMessageOutputStream(rootMessageSerializerMock)
+
+        val outputStream = PipedOutputStream()
+        val inputStream = PipedInputStream()
+        inputStream.connect(outputStream)
+
+        messageOutputStream.connect(outputStream)
+
+        val testSubscriber = inputStream.toFlowable().testSubscribe()
+
+        messageOutputStream.sendMessage(message).test().await()
+        outputStream.close()
+
+        assertThat(testSubscriber.values().reduce { acc, bytes -> acc + bytes })
+            .isEqualTo(packets.reduce { acc, bytes -> acc + bytes })
+        assertThat(testSubscriber.valueCount()).isEqualTo(packets.size)
+    }
+}
