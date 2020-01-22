ff4557e98623e159b110d3e272f8b0bc739e36ec
Fabio <fabio@simprints.com>
2020-01-22 14:06:12 +0000

    More unit tests for SyncDownloaderWorker
    Former-commit-id: 3a24e7eae7b949adb1a722f2a232ca335b5cf3fe

--- id/src/test/java/com/simprints/id/services/scheduledSync/people/down/workers/PeopleDownSyncDownloaderWorkerTest.kt
+++ id/src/test/java/com/simprints/id/services/scheduledSync/people/down/workers/PeopleDownSyncDownloaderWorkerTest.kt
@@ -3,8 +3,10 @@ package com.simprints.id.services.scheduledSync.people.down.workers
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.work.Data
+import androidx.work.WorkInfo
 import androidx.work.testing.TestListenableWorkerBuilder
 import androidx.work.workDataOf
+import com.google.common.truth.Truth.assertThat
 import com.simprints.core.tools.json.JsonHelper
 import com.simprints.id.commontesttools.DefaultTestConstants.DEFAULT_PROJECT_ID
 import com.simprints.id.data.db.people_sync.down.domain.PeopleDownSyncOperation
@@ -13,18 +15,18 @@ import com.simprints.id.exceptions.safe.sync.SyncCloudIntegrationException
 import com.simprints.id.services.scheduledSync.people.down.workers.PeopleDownSyncDownloaderWorker.Companion.INPUT_DOWN_SYNC_OPS
 import com.simprints.id.services.scheduledSync.people.down.workers.PeopleDownSyncDownloaderWorker.Companion.OUTPUT_DOWN_SYNC
 import com.simprints.id.services.scheduledSync.people.down.workers.PeopleDownSyncDownloaderWorker.Companion.OUTPUT_FAILED_BECAUSE_CLOUD_INTEGRATION
+import com.simprints.id.services.scheduledSync.people.down.workers.PeopleDownSyncDownloaderWorker.Companion.PROGRESS_DOWN_SYNC
+import com.simprints.id.services.scheduledSync.people.master.internal.PeopleSyncCache
 import com.simprints.id.testtools.TestApplication
 import com.simprints.id.testtools.UnitTestConfig
 import com.simprints.testtools.unit.robolectric.ShadowAndroidXMultiDex
-import io.mockk.coEvery
-import io.mockk.coVerify
-import io.mockk.mockk
-import io.mockk.verify
+import io.mockk.*
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
+import java.util.*
 
 @RunWith(AndroidJUnit4::class)
 @Config(application = TestApplication::class, shadows = [ShadowAndroidXMultiDex::class])
@@ -52,7 +54,7 @@ class PeopleDownSyncDownloaderWorkerTest {
     }
 
     @Test
-    fun downSyncWorker_shouldParseInputDataCorrectly() = runBlockingTest {
+    fun worker_shouldParseInputDataCorrectly() = runBlockingTest {
         with(peopleDownSyncDownloaderWorker) {
             doWork()
             coEvery { peopleDownSyncDownloaderTask.execute(projectSyncOp, any(), any()) }
@@ -60,7 +62,7 @@ class PeopleDownSyncDownloaderWorkerTest {
     }
 
     @Test
-    fun downSyncWorker_shouldExecuteTheTask() = runBlockingTest {
+    fun worker_shouldExecuteTheTask() = runBlockingTest {
         with(peopleDownSyncDownloaderWorker) {
             coEvery { peopleDownSyncDownloaderTask.execute(any(), any(), any()) } returns 0
 
@@ -73,7 +75,7 @@ class PeopleDownSyncDownloaderWorkerTest {
 
 
     @Test
-    fun downSyncWorker_failForCloudIntegration_shouldFail() = runBlockingTest {
+    fun worker_failForCloudIntegration_shouldFail() = runBlockingTest {
         with(peopleDownSyncDownloaderWorker) {
             coEvery { peopleDownSyncDownloaderTask.execute(any(), any(), any()) } throws SyncCloudIntegrationException("Cloud integration", Throwable())
 
@@ -84,7 +86,7 @@ class PeopleDownSyncDownloaderWorkerTest {
     }
 
     @Test
-    fun downSyncWorker_failForNetworkIssue_shouldRetry() = runBlockingTest {
+    fun worker_failForNetworkIssue_shouldRetry() = runBlockingTest {
         with(peopleDownSyncDownloaderWorker) {
             coEvery { peopleDownSyncDownloaderTask.execute(any(), any(), any()) } throws Throwable("Network Exception")
 
@@ -92,6 +94,47 @@ class PeopleDownSyncDownloaderWorkerTest {
 
             verify { resultSetter.retry() }
         }
+    }
+
+    @Test
+    fun worker_inputDataIsWrong_shouldFail() = runBlockingTest {
+        peopleDownSyncDownloaderWorker = createWorker(workDataOf(INPUT_DOWN_SYNC_OPS to ""))
+        with(peopleDownSyncDownloaderWorker) {
+
+            doWork()
+
+            verify { resultSetter.failure(any()) }
+        }
+    }
+
+    @Test
+    fun worker_progressCountInProgressData_shouldExtractTheProgressCountCorrectly() = runBlockingTest {
+        val progress = 2
+        val syncCacheMock = mockk<PeopleSyncCache>()
+        every { syncCacheMock.readProgress(any()) } returns 1
+
+        val workInfo = WorkInfo(UUID.randomUUID(), WorkInfo.State.RUNNING, workDataOf(), listOf(), workDataOf(PROGRESS_DOWN_SYNC to progress), 2)
+        assertThat(workInfo.extractDownSyncProgress(syncCacheMock)).isEqualTo(progress)
+    }
+
+    @Test
+    fun worker_SyncDown_shouldExtractTheFinalProgressCountCorrectly() = runBlockingTest {
+        val progress = 2
+        val syncCacheMock = mockk<PeopleSyncCache>()
+        every { syncCacheMock.readProgress(any()) } returns 1
+
+        val workInfo = WorkInfo(UUID.randomUUID(), WorkInfo.State.SUCCEEDED, workDataOf(OUTPUT_DOWN_SYNC to progress), listOf(), workDataOf(), 2)
+        assertThat(workInfo.extractDownSyncProgress(syncCacheMock)).isEqualTo(progress)
+    }
+
+    @Test
+    fun workerResumed_progressCountInCache_shouldExtractTheProgressCountCorrectly() = runBlockingTest {
+        val progress = 2
+        val syncCacheMock = mockk<PeopleSyncCache>()
+        every { syncCacheMock.readProgress(any()) } returns progress
+
+        val workInfo = WorkInfo(UUID.randomUUID(), WorkInfo.State.RUNNING, workDataOf(), listOf(), workDataOf(), 2)
+        assertThat(workInfo.extractDownSyncProgress(syncCacheMock)).isEqualTo(progress)
     }
 
     private fun createWorker(inputData: Data? = null) =
