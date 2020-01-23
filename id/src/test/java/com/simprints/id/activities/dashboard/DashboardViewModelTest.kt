66352d407753a0522f568e6415dd4ac4c8ad0c13
Fabio <fabio@simprints.com>
2020-01-23 17:29:27 +0000

    Fixed unit tests
    Former-commit-id: 1c30504b4e27f15fc9c86112108c2cc9a5f3b38e

--- id/src/test/java/com/simprints/id/activities/dashboard/DashboardViewModelTest.kt
+++ id/src/test/java/com/simprints/id/activities/dashboard/DashboardViewModelTest.kt
@@ -3,6 +3,7 @@ package com.simprints.id.activities.dashboard
 import androidx.lifecycle.MutableLiveData
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
+import com.simprints.id.activities.dashboard.DashboardViewModel.Companion.TIME_BETWEEN_TWO_SYNCS
 import com.simprints.id.activities.dashboard.cards.sync.DashboardSyncCardState.*
 import com.simprints.id.commontesttools.DefaultTestConstants.DEFAULT_MODULE_ID
 import com.simprints.id.commontesttools.DefaultTestConstants.moduleSyncScope
@@ -16,11 +17,13 @@ import com.simprints.id.services.scheduledSync.people.master.models.PeopleSyncSt
 import com.simprints.id.services.scheduledSync.people.master.models.PeopleSyncWorkerState.*
 import com.simprints.id.services.scheduledSync.people.master.models.PeopleSyncWorkerType.DOWN_COUNTER
 import com.simprints.id.services.scheduledSync.people.master.models.PeopleSyncWorkerType.UP_COUNTER
+import com.simprints.id.tools.TimeHelper
 import com.simprints.id.tools.device.DeviceManager
 import com.simprints.testtools.common.livedata.testObserver
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
+import io.mockk.verify
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -35,6 +38,8 @@ class DashboardViewModelTest {
     @MockK lateinit var preferencesManager: PreferencesManager
     @MockK lateinit var syncScopeRepository: PeopleDownSyncScopeRepository
     @MockK lateinit var cacheSync: PeopleSyncCache
+    @MockK lateinit var timeHelper: TimeHelper
+
     private lateinit var isConnectedUpdates: MutableLiveData<Boolean>
     private lateinit var syncStateLiveData: MutableLiveData<PeopleSyncState>
     private val syncId = UUID.randomUUID().toString()
@@ -123,6 +128,7 @@ class DashboardViewModelTest {
 
     @Test
     fun syncWorkersFailedBecauseCloudIntegration_syncStateShouldBeTryAgain() {
+        dashboardViewModel.hasSyncEverRun = true
         syncStateLiveData.value = PeopleSyncState(syncId, 0, null, listOf(SyncWorkerInfo(DOWN_COUNTER, Failed(false))), listOf(SyncWorkerInfo(UP_COUNTER, Succeeded)))
 
         val tester = dashboardViewModel.syncCardState.testObserver()
@@ -131,7 +137,66 @@ class DashboardViewModelTest {
     }
 
     @Test
+    fun noHistoryAboutSync_syncStateShouldBeDefault() {
+        syncStateLiveData.value = PeopleSyncState(syncId, 0, null, listOf(SyncWorkerInfo(DOWN_COUNTER, Failed(false))), listOf(SyncWorkerInfo(UP_COUNTER, Succeeded)))
+
+        val tester = dashboardViewModel.syncCardState.testObserver()
+
+        assertThat(tester.observedValues).isEmpty()
+        verify { peopleSyncManager.sync() }
+    }
+
+    @Test
+    fun syncFailedAndNeverRunInTheDashboard_shouldNotEmitStateAndLaunchTheSync() {
+        syncStateLiveData.value = PeopleSyncState(syncId, 0, null, listOf(), listOf())
+
+        val tester = dashboardViewModel.syncCardState.testObserver()
+
+        assertThat(tester.observedValues.last()).isEqualTo(SyncDefault(lastSyncTime))
+    }
+
+    @Test
+    fun syncCompletedButLongTimeAgo_shouldNotEmitAStateAndLaunchTheSync() {
+        every { timeHelper.msBetweenNowAndTime(any()) } returns TIME_BETWEEN_TWO_SYNCS + 1
+        syncStateLiveData.value = PeopleSyncState(syncId, 0, null, listOf(SyncWorkerInfo(DOWN_COUNTER, Succeeded)), listOf(SyncWorkerInfo(UP_COUNTER, Succeeded)))
+
+        val tester = dashboardViewModel.syncCardState.testObserver()
+
+        assertThat(tester.observedValues).isEmpty()
+        verify { peopleSyncManager.sync() }
+    }
+
+    @Test
+    fun syncFinishedButLongTimeAgo_syncIfRequired_shouldLaunchTheSync() {
+        every { timeHelper.msBetweenNowAndTime(any()) } returns TIME_BETWEEN_TWO_SYNCS + 1
+        syncStateLiveData.value = PeopleSyncState(syncId, 0, null, listOf(SyncWorkerInfo(DOWN_COUNTER, Succeeded)), listOf(SyncWorkerInfo(UP_COUNTER, Succeeded)))
+
+        dashboardViewModel.syncIfRequired()
+
+        verify(exactly = 0) { peopleSyncManager.sync() }
+    }
+
+
+    @Test
+    fun syncFinishedButOnlyRecently_syncIfRequired_shouldNotLaunchTheSync() {
+        every { timeHelper.msBetweenNowAndTime(any()) } returns TIME_BETWEEN_TWO_SYNCS - 1
+        syncStateLiveData.value = PeopleSyncState(syncId, 0, null, listOf(SyncWorkerInfo(DOWN_COUNTER, Succeeded)), listOf(SyncWorkerInfo(UP_COUNTER, Succeeded)))
+
+        dashboardViewModel.syncIfRequired()
+
+        verify(exactly = 0) { peopleSyncManager.sync() }
+    }
+
+    @Test
+    fun noHistoryAboutSync_syncIfRequired_shouldNotLaunchTheSync() {
+        dashboardViewModel.syncIfRequired()
+
+        verify(exactly = 0) { peopleSyncManager.sync() }
+    }
+
+    @Test
     fun syncWorkersFailedDueToTimeout_syncStateShouldBeFailed() {
+        dashboardViewModel.hasSyncEverRun = true
         syncStateLiveData.value = PeopleSyncState(syncId, 0, null, listOf(SyncWorkerInfo(DOWN_COUNTER, Failed(true))), listOf(SyncWorkerInfo(UP_COUNTER, Succeeded)))
 
         val tester = dashboardViewModel.syncCardState.testObserver()
@@ -152,6 +217,6 @@ class DashboardViewModelTest {
 
 
     private fun createViewModel() =
-        DashboardViewModel(peopleSyncManager, deviceManager, preferencesManager, syncScopeRepository, cacheSync)
+        DashboardViewModel(peopleSyncManager, deviceManager, preferencesManager, syncScopeRepository, cacheSync, timeHelper)
 
 }
