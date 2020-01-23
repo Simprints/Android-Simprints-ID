4ed5fa655140ff5067a6d751d3138e9885ca60d0
Alan Camargo <alan@simprints.com>
2020-01-23 16:54:44 +0000

    Document ImageRemoteDataSource and UploadResult
    Former-commit-id: 72f0cab8ed24983cbba8703f38a4554140f1d4f0

--- id/src/main/java/com/simprints/id/data/db/image/remote/ImageRemoteDataSource.kt
+++ id/src/main/java/com/simprints/id/data/db/image/remote/ImageRemoteDataSource.kt
@@ -2,8 +2,18 @@ package com.simprints.id.data.db.image.remote
 
 import com.simprints.core.images.SecuredImageRef
 
+/**
+ * Interface for remote image file operations
+ */
 interface ImageRemoteDataSource {
 
+    /**
+     * Uploads an image
+     *
+     * @param image the image to be uploaded
+     * @return the result of the operation.
+     * @see [UploadResult]
+     */
     suspend fun uploadImage(image: SecuredImageRef): UploadResult
 
 }
