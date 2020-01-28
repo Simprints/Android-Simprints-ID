21a2785c9ea720d1037a55c1dc412a1de2bdfe2b
Alan Camargo <alan@simprints.com>
2020-01-28 12:17:00 +0000

    Fix context switch in repository
    Former-commit-id: 8c9fc3ee3277a20d2b60383a3380cb5d8a18823e

--- id/src/main/java/com/simprints/id/data/db/project/ProjectRepositoryImpl.kt
+++ id/src/main/java/com/simprints/id/data/db/project/ProjectRepositoryImpl.kt
@@ -4,28 +4,24 @@ import com.google.firebase.perf.FirebasePerformance
 import com.simprints.id.data.db.project.domain.Project
 import com.simprints.id.data.db.project.local.ProjectLocalDataSource
 import com.simprints.id.data.db.project.remote.ProjectRemoteDataSource
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class ProjectRepositoryImpl(private val projectLocalDataSource: ProjectLocalDataSource,
-                            private val projectRemoteDataSource: ProjectRemoteDataSource,
-                            private val performanceTracker: FirebasePerformance = FirebasePerformance.getInstance(),
-                            private val dispatcher: CoroutineDispatcher = Dispatchers.IO) :
-    ProjectRepository,
+class ProjectRepositoryImpl(
+    private val projectLocalDataSource: ProjectLocalDataSource,
+    private val projectRemoteDataSource: ProjectRemoteDataSource,
+    private val performanceTracker: FirebasePerformance = FirebasePerformance.getInstance()
+) : ProjectRepository,
     ProjectLocalDataSource by projectLocalDataSource,
     ProjectRemoteDataSource by projectRemoteDataSource {
 
     override suspend fun loadFromRemoteAndRefreshCache(projectId: String): Project? {
         val trace = performanceTracker.newTrace("refreshProjectInfoWithServer").apply { start() }
         val projectInLocal = projectLocalDataSource.load(projectId)
-        return projectInLocal?.also {
-            GlobalScope.launch(dispatcher) {
-                fetchAndUpdateCache(projectId)
-                trace.stop()
-            }
-        } ?: fetchAndUpdateCache(projectId).also {
+        return projectInLocal?.apply {
+            fetchAndUpdateCache(projectId)
+            trace.stop()
+        } ?: fetchAndUpdateCache(projectId).apply {
             trace.stop()
         }
     }
@@ -35,8 +31,10 @@ class ProjectRepositoryImpl(private val projectLocalDataSource: ProjectLocalData
     }
 
     private suspend fun fetchAndUpdateCache(projectId: String): Project? = try {
-        projectRemoteDataSource.loadProjectFromRemote(projectId).blockingGet().also {
-            projectLocalDataSource.save(it)
+        withContext(Dispatchers.IO) {
+            projectRemoteDataSource.loadProjectFromRemote(projectId).blockingGet().also {
+                projectLocalDataSource.save(it)
+            }
         }
     } catch (t: Throwable) {
         t.printStackTrace()
