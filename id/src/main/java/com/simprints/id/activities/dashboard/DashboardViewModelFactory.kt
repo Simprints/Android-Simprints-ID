517daf9a4bd6800554364f72e42f64d99f15c57c
Alan Camargo <alan@simprints.com>
2020-01-28 11:17:15 +0000

    Inject view model factory
    Former-commit-id: c4e66b7278debb647277df242748c68b63424744

--- id/src/main/java/com/simprints/id/activities/dashboard/DashboardViewModelFactory.kt
+++ id/src/main/java/com/simprints/id/activities/dashboard/DashboardViewModelFactory.kt
@@ -2,26 +2,20 @@ package com.simprints.id.activities.dashboard
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.simprints.id.data.db.project.ProjectRepository
-import com.simprints.id.data.loginInfo.LoginInfoManager
-import com.simprints.id.data.prefs.PreferencesManager
+import com.simprints.id.activities.dashboard.cards.project.repository.DashboardProjectDetailsRepository
 import com.simprints.id.services.scheduledSync.people.master.PeopleSyncManager
 
 class DashboardViewModelFactory(
-    private val projectRepository: ProjectRepository,
-    private val loginInfoManager: LoginInfoManager,
-    private val preferencesManager: PreferencesManager,
-    private val peopleSyncManager: PeopleSyncManager
+    private val peopleSyncManager: PeopleSyncManager,
+    private val projectDetailsRepository: DashboardProjectDetailsRepository
 ) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return if (modelClass.isAssignableFrom(DashboardViewModel::class.java)) {
             DashboardViewModel(
-                projectRepository,
-                loginInfoManager,
-                preferencesManager,
-                peopleSyncManager
+                peopleSyncManager,
+                projectDetailsRepository
             ) as T
         } else {
             throw IllegalArgumentException("ViewModel Not Found")
