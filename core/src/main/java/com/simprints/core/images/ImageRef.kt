106ec3a486fbd290e9f687ae9f92c4493c27a565
Alan Camargo <alan@simprints.com>
2020-01-30 13:57:20 +0000

    Remove unused function
    Former-commit-id: 7cba5b66fc9271ae3b3c791b5434cb00ea36a9c5

--- core/src/main/java/com/simprints/core/images/ImageRef.kt
+++ core/src/main/java/com/simprints/core/images/ImageRef.kt
@@ -2,12 +2,5 @@ package com.simprints.core.images
 
 import android.os.Parcelable
 import com.simprints.moduleapi.common.IPath
-import java.io.File
 
-abstract class ImageRef(open val path: IPath) : Parcelable {
-
-    fun getFileName(): String {
-        return File(path.compose()).name
-    }
-
-}
+abstract class ImageRef(open val path: IPath) : Parcelable
