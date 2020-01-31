2cc095d3c018d9a717964da865de04d05c238757
Alan Camargo <alan@simprints.com>
2020-01-31 11:29:08 +0000

    Building displayer
    Former-commit-id: 94c890806a7980be2a45e0108040588105b04a4f

---
+++ id/src/main/java/com/simprints/id/activities/dashboard/cards/activity/repository/DashboardDailyActivityRepository.kt
@@ -0,0 +1,11 @@
+package com.simprints.id.activities.dashboard.cards.activity.repository
+
+import com.simprints.id.activities.dashboard.cards.activity.model.DailyActivityState
+
+class DashboardDailyActivityRepository {
+    
+    suspend fun getDailyActivity(): DailyActivityState {
+        TODO("not implemented")
+    }
+
+}
