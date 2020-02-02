20a706af53a59381156dbfb5aa24ca56e281fe95
Ridwan Farouki <ridwan@simprints.com>
2020-02-02 16:31:22 +0000

    FIxed UN20 OTA bug 2020.1.0-dev19
    Former-commit-id: fc520694a1818cb965f04d19dd0530be2141aff8

--- fingerprintscanner/src/main/java/com/simprints/fingerprintscanner/v2/domain/main/message/un20/commands/WriteOtaChunkCommand.kt
+++ fingerprintscanner/src/main/java/com/simprints/fingerprintscanner/v2/domain/main/message/un20/commands/WriteOtaChunkCommand.kt
@@ -3,7 +3,7 @@ package com.simprints.fingerprintscanner.v2.domain.main.message.un20.commands
 import com.simprints.fingerprintscanner.v2.domain.main.message.un20.Un20Command
 import com.simprints.fingerprintscanner.v2.domain.main.message.un20.models.Un20MessageType
 
-class WriteOtaChunkCommand(val firmwareChunk: ByteArray) : Un20Command(Un20MessageType.StartOta) {
+class WriteOtaChunkCommand(val firmwareChunk: ByteArray) : Un20Command(Un20MessageType.WriteOtaChunk) {
 
     override fun getDataBytes(): ByteArray = firmwareChunk
 
