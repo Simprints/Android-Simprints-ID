eb6cc2fb8f65ffee87049e71f5b08191f278179d
Ridwan Farouki <ridwan@simprints.com>
2020-02-02 16:31:22 +0000

    FIxed UN20 OTA bug 2020.1.0-dev19
    Former-commit-id: a651b8cb774cb7792668e704c0078d3636a72781

--- fingerprintscanner/src/main/java/com/simprints/fingerprintscanner/v2/domain/main/message/un20/commands/VerifyOtaCommand.kt
+++ fingerprintscanner/src/main/java/com/simprints/fingerprintscanner/v2/domain/main/message/un20/commands/VerifyOtaCommand.kt
@@ -4,7 +4,7 @@ import com.simprints.fingerprintscanner.v2.domain.main.message.un20.Un20Command
 import com.simprints.fingerprintscanner.v2.domain.main.message.un20.Un20MessageProtocol
 import com.simprints.fingerprintscanner.v2.domain.main.message.un20.models.Un20MessageType
 
-class VerifyOtaCommand(val crcCheck: Int) : Un20Command(Un20MessageType.StartOta) {
+class VerifyOtaCommand(val crcCheck: Int) : Un20Command(Un20MessageType.VerifyOta) {
 
     override fun getDataBytes(): ByteArray = with(Un20MessageProtocol) { crcCheck.toByteArray() }
 
