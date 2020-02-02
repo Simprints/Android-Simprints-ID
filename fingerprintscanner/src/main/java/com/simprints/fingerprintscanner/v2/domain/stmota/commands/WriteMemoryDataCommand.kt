e846535721a6151eb051929e158eff968036857a
Ridwan Farouki <ridwan@simprints.com>
2020-02-02 13:15:46 +0000

    Fixed missing StmOta message: 2020.1.0-dev17
    Former-commit-id: 5fcb71e9eea493e8afd1fbf134f001d670adfba8

--- fingerprintscanner/src/main/java/com/simprints/fingerprintscanner/v2/domain/stmota/commands/WriteMemoryDataCommand.kt
+++ fingerprintscanner/src/main/java/com/simprints/fingerprintscanner/v2/domain/stmota/commands/WriteMemoryDataCommand.kt
@@ -1,8 +1,9 @@
 package com.simprints.fingerprintscanner.v2.domain.stmota.commands
 
 import com.simprints.fingerprintscanner.v2.domain.stmota.StmOtaCommand
+import com.simprints.fingerprintscanner.v2.tools.primitives.byteArrayOf
 
 class WriteMemoryDataCommand(val data: ByteArray) : StmOtaCommand() {
 
-    override fun getDataBytes(): ByteArray = com.simprints.fingerprintscanner.v2.tools.primitives.byteArrayOf(data.size.toByte(), data)
+    override fun getDataBytes(): ByteArray = byteArrayOf(data.size.toByte(), data)
 }
