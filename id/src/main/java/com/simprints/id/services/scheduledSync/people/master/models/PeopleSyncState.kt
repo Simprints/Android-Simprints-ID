d2e0f6390e85fc06ecdf9cfeb538c44c3cd7cce7
Fabio <fabio@simprints.com>
2020-02-18 08:22:19 +0000

    Removed unnecessary json operation for logs
    Former-commit-id: e408524ca17e8d7e17367d19c234537b034443ab

--- id/src/main/java/com/simprints/id/services/scheduledSync/people/master/models/PeopleSyncState.kt
+++ id/src/main/java/com/simprints/id/services/scheduledSync/people/master/models/PeopleSyncState.kt
@@ -3,7 +3,7 @@ package com.simprints.id.services.scheduledSync.people.master.models
 import androidx.annotation.Keep
 
 @Keep
-class PeopleSyncState(val syncId: String,
+data class PeopleSyncState(val syncId: String,
                       val progress: Int,
                       val total: Int?,
                       val upSyncWorkersInfo: List<SyncWorkerInfo>,
