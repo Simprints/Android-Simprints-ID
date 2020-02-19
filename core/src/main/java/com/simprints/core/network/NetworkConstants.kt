9ffa6cb5a8f9f8987c96f3a1598b4f57955430c0
Fabio <fabio@simprints.com>
2020-02-19 15:31:19 +0000

    Better cloud codes to retry network calls
    Former-commit-id: f3cc077cc1b8fcd7316cf9f4c82740be072aaeab

--- core/src/main/java/com/simprints/core/network/NetworkConstants.kt
+++ core/src/main/java/com/simprints/core/network/NetworkConstants.kt
@@ -6,7 +6,7 @@ class NetworkConstants {
     companion object {
         private const val apiVersion = "v2"
         const val BASE_URL = "https://${BuildConfig.END_POINT}.simprints-apis.com/androidapi/$apiVersion/"
-        val httpCodesForIntegrationIssues = (400..404) + (500..599)
+        val httpCodesForIntegrationIssues = (400..404) + (500..599) - (502..503)
         const val RETRY_ATTEMPTS_FOR_NETWORK_CALLS = 5
     }
 }
