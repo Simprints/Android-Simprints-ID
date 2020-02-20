cfe59aed844cc0826a63e61e640fe984a7057c38
Ridwan Farouki <ridwan@simprints.com>
2020-02-20 14:04:26 +0000

    Add some more acceptable DPIs
    Former-commit-id: 92ab9650230852bc1abd9b0a2cf8879d07070cac

--- fingerprint/src/main/java/com/simprints/fingerprint/data/domain/fingerprint/CaptureFingerprintStrategy.kt
+++ fingerprint/src/main/java/com/simprints/fingerprint/data/domain/fingerprint/CaptureFingerprintStrategy.kt
@@ -1,5 +1,8 @@
 package com.simprints.fingerprint.data.domain.fingerprint
 
 enum class CaptureFingerprintStrategy {
+    SECUGEN_ISO_500_DPI,
+    SECUGEN_ISO_1000_DPI,
+    SECUGEN_ISO_1300_DPI,
     SECUGEN_ISO_1700_DPI
 }
