e5afe1768ab82dc7589474a37b8c6efbdc9e790e
Alan Camargo <alan@simprints.com>
2020-02-25 10:45:54 +0000

    Move GUID validation function to core
    Former-commit-id: 81995da22a87520f6d9811e67a2c19c7b87c8afd

--- core/src/main/java/com/simprints/core/tools/utils/StringUtils.kt
+++ core/src/main/java/com/simprints/core/tools/utils/StringUtils.kt
@@ -2,6 +2,15 @@ package com.simprints.core.tools.utils
 
 import java.util.*
 
+private const val REGEX_GUID =
+    "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[34][0-9a-fA-F]{3}-[89ab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}"
+
 fun randomUUID(): String {
     return UUID.randomUUID().toString()
 }
+
+/**
+ * For a detailed description of what defines a UUID,
+ * @see [java.util.UUID]
+ */
+fun String.isValidGuid(): Boolean = this.matches(REGEX_GUID.toRegex())
