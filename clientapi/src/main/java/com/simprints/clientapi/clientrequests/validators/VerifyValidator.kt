76e0b985f35c80a4299a4b69018ca39de6f8a33d
Alan Camargo <alan@simprints.com>
2020-02-25 10:45:54 +0000

    Move GUID validation function to core
    Former-commit-id: 113f4f1895133ae893fca8e377cdc619e872e758

--- clientapi/src/main/java/com/simprints/clientapi/clientrequests/validators/VerifyValidator.kt
+++ clientapi/src/main/java/com/simprints/clientapi/clientrequests/validators/VerifyValidator.kt
@@ -2,6 +2,7 @@ package com.simprints.clientapi.clientrequests.validators
 
 import com.simprints.clientapi.clientrequests.extractors.VerifyExtractor
 import com.simprints.clientapi.exceptions.InvalidVerifyIdException
+import com.simprints.core.tools.utils.isValidGuid
 
 class VerifyValidator(val extractor: VerifyExtractor) : ClientRequestValidator(extractor) {
 
@@ -13,20 +14,6 @@ class VerifyValidator(val extractor: VerifyExtractor) : ClientRequestValidator(e
     private fun validateVerifyGuid(verifyGuid: String?) {
         if (verifyGuid.isNullOrBlank() || !verifyGuid.isValidGuid())
             throw InvalidVerifyIdException("Invalid verify ID")
-    }
-
-    /**
-     * "A UUID is made of up of hex digits  (4 chars each) along with 4 “-”
-     * symbols which make its length equal to 36 characters"
-     *
-     * @see [https://www.baeldung.com/java-uuid]
-     * @see [java.util.UUID]
-     */
-    private fun String.isValidGuid(): Boolean = this.matches(REGEX_GUID.toRegex())
-
-    private companion object {
-        const val REGEX_GUID =
-            "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[34][0-9a-fA-F]{3}-[89ab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}"
     }
 
 }
