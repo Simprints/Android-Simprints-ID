9e435861552e1d3046ee667b25a176232da5fa36
Fabio <fabio@simprints.com>
2020-03-03 11:46:19 +0000

    Avoid exposing SessionLocalDataSource - using Repo instead
    Former-commit-id: 3532981f81199b44c314e7d74ebaef7c29d87e3a

--- id/src/main/java/com/simprints/id/exceptions/unexpected/UnexpectedException.kt
+++ id/src/main/java/com/simprints/id/exceptions/unexpected/UnexpectedException.kt
@@ -1,8 +1,6 @@
 package com.simprints.id.exceptions.unexpected
 
-import com.simprints.id.exceptions.SimprintsException
-
-open class UnexpectedException : SimprintsException {
+open class UnexpectedException : RuntimeException {
     constructor(message: String, cause: Throwable): super(message, cause)
     constructor(message: String): super(message)
     constructor(cause: Throwable): super(cause)
