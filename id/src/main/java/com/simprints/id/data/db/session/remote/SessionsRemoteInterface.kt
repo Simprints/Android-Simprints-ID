c4c4e896a3f2f9a19f745cf7e4b4846dd438e9e2
Fabio <fabio@simprints.com>
2020-03-10 16:46:11 +0000

    Fixing network request to upload sessions
    Former-commit-id: c1322da1e599c1a4b3097b0bcb75ea9372f34d46

--- id/src/main/java/com/simprints/id/data/db/session/remote/SessionsRemoteInterface.kt
+++ id/src/main/java/com/simprints/id/data/db/session/remote/SessionsRemoteInterface.kt
@@ -2,15 +2,13 @@ package com.simprints.id.data.db.session.remote
 
 import com.simprints.core.network.SimRemoteInterface
 import com.simprints.id.data.db.session.remote.session.ApiSessionEvents
-import io.reactivex.Single
-import retrofit2.adapter.rxjava2.Result
 import retrofit2.http.Body
 import retrofit2.http.POST
 import retrofit2.http.Path
 
-interface SessionsRemoteInterface: SimRemoteInterface {
+interface SessionsRemoteInterface : SimRemoteInterface {
 
     @POST("projects/{projectId}/sessions")
-    fun uploadSessions(@Path("projectId") projectId: String,
-                       @Body sessionsJson: HashMap<String, Array<ApiSessionEvents>>): Single<Result<Void?>>
+    suspend fun uploadSessions(@Path("projectId") projectId: String,
+                               @Body sessionsJson: HashMap<String, Array<ApiSessionEvents>>)
 }
