1a6cb684a4d42d8b96a23ba6b412d6b57e09f248
Alan Camargo <alan@simprints.com>
2020-03-10 18:56:14 +0000

    Use coroutines in QR code scanning
    Former-commit-id: 1f9c135bc47378fbc055fe5e253238f97e971ea0

--- id/src/main/java/com/simprints/id/activities/qrcapture/tools/QrCodeDetectorImpl.kt
+++ id/src/main/java/com/simprints/id/activities/qrcapture/tools/QrCodeDetectorImpl.kt
@@ -6,31 +6,25 @@ import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata
+import com.simprints.id.activities.qrcapture.model.RawImage
 import com.simprints.id.data.analytics.crashreport.CrashReportManager
 import com.simprints.id.tools.extensions.awaitTask
 
 class QrCodeDetectorImpl(private val crashReportManager: CrashReportManager) : QrCodeDetector {
 
     override suspend fun detectInImage(rawImage: RawImage): String? {
-
         val firebaseRotation = degreesToFirebaseRotation(rawImage.rotationDegrees)
         val firebaseImage = FirebaseVisionImage.fromMediaImage(rawImage.image, firebaseRotation)
         val firebaseDetector = buildFirebaseDetector()
 
         return try {
-                val qrCodes = firebaseDetector.detectInImage(firebaseImage).awaitTask()
-                if (!qrCodes.isNullOrEmpty()) {
-                    val qrCode = qrCodes.first { !it.rawValue.isNullOrEmpty() }
-                    qrCode.rawValue
-                } else {
-                    null
-                }
-            } catch (t: Throwable) {
-                crashReportManager.logExceptionOrSafeException(t)
-                null
-            }
+            val qrCodes = firebaseDetector.detectInImage(firebaseImage).awaitTask()
+            return qrCodes?.firstOrNull { !it.rawValue.isNullOrEmpty() }?.rawValue
+        } catch (t: Throwable) {
+            crashReportManager.logExceptionOrSafeException(t)
+            null
         }
-
+    }
 
     private fun degreesToFirebaseRotation(degrees: Int): Int = when (degrees) {
         0 -> FirebaseVisionImageMetadata.ROTATION_0
---
+++ id/src/main/java/com/simprints/id/activities/qrcapture/tools/QrPreviewBuilder.kt
@@ -0,0 +1,15 @@
+package com.simprints.id.activities.qrcapture.tools
+
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.Preview
+import androidx.camera.core.PreviewConfig
+
+class QrPreviewBuilder {
+    fun buildPreview(): Preview {
+        val previewConfig = PreviewConfig.Builder()
+            .setTargetAspectRatio(AspectRatio.RATIO_16_9)
+            .build()
+
+        return Preview(previewConfig)
+    }
+}
