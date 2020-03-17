49ba8969883be3529ff064c13302c786df383b0b
Aashay Gaikwad <aashay@simprints.com>
2020-03-17 11:03:58 +0530

    Refactoring auth managers
    Former-commit-id: 2d8ab048250514c751923b9f8dd12ecad510859d

--- id/src/main/java/com/simprints/id/secure/AuthManager.kt
+++ id/src/main/java/com/simprints/id/secure/AuthManager.kt
@@ -4,9 +4,11 @@ import com.simprints.id.exceptions.safe.data.db.SimprintsInternalServerException
 import com.simprints.id.exceptions.safe.secure.AuthRequestInvalidCredentialsException
 import com.simprints.id.secure.models.AuthRequest
 import com.simprints.id.secure.models.Token
+import com.simprints.id.secure.models.remote.ApiToken
 import com.simprints.id.secure.models.remote.toDomainToken
 import com.simprints.id.tools.utils.retrySimNetworkCalls
 import retrofit2.HttpException
+import retrofit2.Response
 
 class AuthManager(val client: SecureApiInterface) {
 
@@ -16,14 +18,17 @@ class AuthManager(val client: SecureApiInterface) {
                 authRequest.userId,
                 authRequest.authRequestBody)
         }, "requestAuthToken")
-        return response.body()!!.toDomainToken()
+
+        response.body()?.let {
+            return it.toDomainToken()
+        } ?: handleResponseError(response)
     }
 
-    private fun handleResponseError(e: HttpException): Nothing =
-        when (e.code()) {
+    private fun handleResponseError(response: Response<ApiToken>): Nothing =
+        when (response.code()) {
             401, 404 -> throw AuthRequestInvalidCredentialsException()
             in 500..599 -> throw SimprintsInternalServerException()
-            else -> throw e
+            else -> throw HttpException(response)
         }
 
     private suspend fun <T> makeNetworkRequest(block: suspend (client: SecureApiInterface) -> T, traceName: String): T =
