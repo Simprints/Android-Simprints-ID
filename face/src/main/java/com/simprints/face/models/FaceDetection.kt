da291e64ebdde96809a12fa27e13881a7389ac09
Tsuharesu <tsuharesu01@gmail.com>
2020-03-27 17:16:18 +0000

    Transform detection into sample
    Former-commit-id: f8d6210775fc47f5fae0f1f091203d823c3bf312

--- face/src/main/java/com/simprints/face/models/FaceDetection.kt
+++ face/src/main/java/com/simprints/face/models/FaceDetection.kt
@@ -1,12 +1,16 @@
 package com.simprints.face.models
 
+import com.simprints.face.data.moduleapi.face.responses.entities.FaceSample
+import com.simprints.face.data.moduleapi.face.responses.entities.SecuredImageRef
 import com.simprints.face.detection.Face
 import com.simprints.uicomponents.models.PreviewFrame
+import java.util.*
 
 data class FaceDetection(
     val frame: PreviewFrame,
     val face: Face?,
     val status: Status,
+    val securedImageRef: SecuredImageRef? = null,
     val detectionTime: Long = System.currentTimeMillis()
 ) {
     enum class Status {
@@ -19,4 +23,6 @@ data class FaceDetection(
         TOOFAR
     }
 
+    fun toFaceSample(): FaceSample =
+        FaceSample(UUID.randomUUID().toString(), face?.template ?: ByteArray(0), securedImageRef)
 }
