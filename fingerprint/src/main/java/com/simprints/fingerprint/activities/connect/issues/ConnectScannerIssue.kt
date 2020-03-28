4f719d74487d5842f41b8c3b9ab9ff309e53ce2d
Ridwan Farouki <ridwan@simprints.com>
2020-03-28 19:38:55 +0000

    Remove res ids from domain class
    Former-commit-id: cd2f92504900109e1fbba107dcdf5627600ec8e1

--- fingerprint/src/main/java/com/simprints/fingerprint/activities/connect/issues/ConnectScannerIssue.kt
+++ fingerprint/src/main/java/com/simprints/fingerprint/activities/connect/issues/ConnectScannerIssue.kt
@@ -1,12 +1,9 @@
 package com.simprints.fingerprint.activities.connect.issues
 
-import androidx.annotation.IdRes
-import com.simprints.fingerprint.R
-
-enum class ConnectScannerIssue(@IdRes val navActionId: Int) {
-    BLUETOOTH_OFF(R.id.action_connectScannerMainFragment_to_bluetoothOffFragment),
-    NFC_OFF(R.id.action_connectScannerMainFragment_to_nfcOffFragment),
-    NFC_PAIR(R.id.action_connectScannerMainFragment_to_nfcPairFragment),
-    SERIAL_ENTRY_PAIR(R.id.action_connectScannerMainFragment_to_serialEntryFragment),
-    TURN_ON_SCANNER(R.id.action_connectScannerMainFragment_to_turnOnScannerFragment)
+enum class ConnectScannerIssue {
+    BLUETOOTH_OFF,
+    NFC_OFF,
+    NFC_PAIR,
+    SERIAL_ENTRY_PAIR,
+    TURN_ON_SCANNER
 }
