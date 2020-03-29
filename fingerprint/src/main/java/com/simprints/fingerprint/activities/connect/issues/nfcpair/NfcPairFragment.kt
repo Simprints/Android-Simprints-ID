e0062612ad0e5d8bfd3f84bd0d35c9b5393336e5
Ridwan Farouki <ridwan@simprints.com>
2020-03-29 13:59:03 +0100

    Prepare UI for NfcPairActivity
    Former-commit-id: 98bc01061ee28899f877ffa538169e40d4fa76f5

--- fingerprint/src/main/java/com/simprints/fingerprint/activities/connect/issues/nfcpair/NfcPairFragment.kt
+++ fingerprint/src/main/java/com/simprints/fingerprint/activities/connect/issues/nfcpair/NfcPairFragment.kt
@@ -1,14 +1,28 @@
 package com.simprints.fingerprint.activities.connect.issues.nfcpair
 
+import android.graphics.Paint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.simprints.fingerprint.R
+import kotlinx.android.synthetic.main.fragment_nfc_pair.*
 
 class NfcPairFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
         inflater.inflate(R.layout.fragment_nfc_pair, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        couldNotPairTextView.paintFlags = couldNotPairTextView.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+        couldNotPairTextView.setOnClickListener { goToSerialEntryPair() }
+    }
+
+    private fun goToSerialEntryPair() {
+        findNavController().navigate(R.id.action_nfcPairFragment_to_serialEntryPairFragment)
+    }
 }
--- fingerprint/src/main/java/com/simprints/fingerprint/activities/connect/issues/serialentrypair/SerialEntryPairFragment.kt
+++ fingerprint/src/main/java/com/simprints/fingerprint/activities/connect/issues/serialentrypair/SerialEntryPairFragment.kt
@@ -7,8 +7,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.simprints.fingerprint.R
 
-class SerialEntryFragment : Fragment() {
+class SerialEntryPairFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.fragment_serial_entry, container, false)
+        inflater.inflate(R.layout.fragment_serial_entry_pair, container, false)
 }
