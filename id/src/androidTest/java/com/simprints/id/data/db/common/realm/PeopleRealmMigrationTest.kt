552c0fe059fa5be92bbca327c601b198c7467803
Alan Camargo <alan@simprints.com>
2020-03-31 11:11:15 +0100

    Add comment to migration test
    Former-commit-id: e31aeb7a00b0e234c6dd85f9e66c481b47aace53

--- id/src/androidTest/java/com/simprints/id/data/db/common/realm/PeopleRealmMigrationTest.kt
+++ id/src/androidTest/java/com/simprints/id/data/db/common/realm/PeopleRealmMigrationTest.kt
@@ -12,6 +12,11 @@ import org.junit.Test
 import java.io.File
 import java.io.FileOutputStream
 
+/* These 2 test cases are currently passing because we are using a specific db version
+ * and then migrating to the next one.
+ * FIXME: do not add new migration test cases as it will cause other tests to fail due to
+ *  all Realm objects being always at the latest version
+ */
 class PeopleRealmMigrationTest {
 
     @Test
@@ -22,11 +27,6 @@ class PeopleRealmMigrationTest {
     @Test
     fun migrateFromV7ToV8() {
         testMigration(DB_V7_FILENAME, DB_V7_KEY, targetVersion = 8)
-    }
-
-    @Test
-    fun migrateFromV8ToV9() {
-        testMigration(DB_V8_FILENAME, DB_V8_KEY, targetVersion = 9)
     }
 
     private fun testMigration(dbFileName: String, key: String, targetVersion: Long) {
@@ -73,11 +73,9 @@ class PeopleRealmMigrationTest {
 
         const val DB_V6_FILENAME = "db_v6.realm"
         const val DB_V7_FILENAME = "db_v7.realm"
-        const val DB_V8_FILENAME = "db_v8.realm"
 
         const val DB_V6_KEY = "KEWsuk0UdOawjyBtjSTMAv6D8o126b6zB+uDR8okr7UVpIe1+btEsZ/KtFRkkVlLp9SgsdC5P8VF\nfLvNHeiE0g==\n"
         const val DB_V7_KEY = "j1kkhzjFKEHlBnHO3AkijxaT03Fii6avZAcig0PfQbVlK8kRnHSNKrzRwdaLzpxzFaQIYoJ0naA5\n4yGAywAutg==\n"
-        const val DB_V8_KEY = "QIQsfr9S+1oMrhgZTl2aVKPjGCo/rZGqoHxuYWbyTFBzJ7uYjzA0pimGTEblPEDHCQpnokg0lf9b\nQE1mqSIWrA==\n"
     }
 
 }
