e10316104e5814ac7df4e518719ded43b44890a5
Tsuharesu <tsuharesu01@gmail.com>
2020-04-01 17:35:05 +0100

    Clean up FrameProcessor with method extraction and better names
    Former-commit-id: 0f0da66e5902d7e2002305728fd87410ba15a818

--- face/src/main/java/com/simprints/face/capture/livefeedback/tools/FrameProcessor.kt
+++ face/src/main/java/com/simprints/face/capture/livefeedback/tools/FrameProcessor.kt
@@ -4,8 +4,8 @@ import android.graphics.ImageFormat
 import android.graphics.RectF
 import androidx.core.graphics.toRect
 import com.otaliastudios.cameraview.frame.Frame
-import com.simprints.uicomponents.models.PreviewFrame
 import com.simprints.uicomponents.imageTools.LibYuvJni
+import com.simprints.uicomponents.models.PreviewFrame
 import com.simprints.uicomponents.models.Size
 import kotlin.math.min
 
@@ -23,45 +23,61 @@ class FrameProcessor(private val libYuvJni: LibYuvJni) {
         val screenWidth = screenSize.width
         val screenHeight = screenSize.height
 
-        val (rotatedCameraWidth, rotatedCameraHeight) =
-            if (cameraFrame.rotation in arrayOf(90, 270)) {
-                Pair(cameraFrame.size.height, cameraFrame.size.width)
-            } else {
-                Pair(cameraFrame.size.width, cameraFrame.size.height)
-            }
+        val (rotatedCameraWidth, rotatedCameraHeight) = getCameraRotatedPair(cameraFrame)
 
-        val newRectSize =
-            if (screenWidth == rotatedCameraWidth || screenHeight == rotatedCameraHeight) {
-                val cameraArea = rotatedCameraHeight * rotatedCameraWidth
-                val screenArea = screenHeight * screenWidth
-                // This means center crop is tight
-                if (cameraArea > screenArea) {
-                    boxOnTheScreen.width()
-                } else {
-                    // This means the camera is zooming in to center crop
-                    sizeFromMinRatio(
-                        rotatedCameraWidth,
-                        screenWidth,
-                        rotatedCameraHeight,
-                        screenHeight,
-                        boxOnTheScreen.width()
-                    )
-                }
-            } else {
-                // This means the camera is zooming out to center crop
-                sizeFromMinRatio(
-                    rotatedCameraWidth,
-                    screenWidth,
-                    rotatedCameraHeight,
-                    screenHeight,
-                    boxOnTheScreen.width()
-                )
-            }
+        val newRectSize = getRectSizeBasedOnCameraCropping(
+            screenWidth,
+            screenHeight,
+            rotatedCameraWidth,
+            rotatedCameraHeight,
+            boxOnTheScreen
+        )
 
         val newBoundingBox =
             CameraTargetOverlay.rectForPlane(rotatedCameraWidth, rotatedCameraHeight, newRectSize)
 
-        val rotatedTargetBoundingBox = when (360 - cameraFrame.rotation) {
+        val rotatedTargetBoundingBox =
+            getRotatedBoundingBox(cameraFrame, newBoundingBox, cameraWidth, cameraHeight)
+
+        require(cameraFrame.format == ImageFormat.NV21)
+        val (croppedFrameSize, croppedFrameBytes) = getCroppedFrameAndBytes(
+            cameraWidth,
+            cameraHeight,
+            cameraFrame,
+            rotatedTargetBoundingBox
+        )
+
+        return PreviewFrame(
+            croppedFrameSize.width,
+            croppedFrameSize.height,
+            cameraFrame.format,
+            facingFront,
+            croppedFrameBytes
+        ).also {
+            cameraFrame.release()
+        }
+    }
+
+    private fun getCroppedFrameAndBytes(
+        cameraWidth: Int,
+        cameraHeight: Int,
+        cameraFrame: Frame,
+        rotatedTargetBoundingBox: RectF
+    ): Pair<Size, ByteArray> {
+        return libYuvJni.cropRotateYuvNV21(
+            Size(cameraWidth, cameraHeight), cameraFrame.data,
+            rect = rotatedTargetBoundingBox.toRect(),
+            rotation = cameraFrame.rotation
+        )
+    }
+
+    private fun getRotatedBoundingBox(
+        cameraFrame: Frame,
+        newBoundingBox: RectF,
+        cameraWidth: Int,
+        cameraHeight: Int
+    ): RectF {
+        return when (360 - cameraFrame.rotation) {
             0, 360 -> newBoundingBox
             90 -> RectF(
                 cameraWidth - newBoundingBox.bottom,
@@ -83,24 +99,51 @@ class FrameProcessor(private val libYuvJni: LibYuvJni) {
             )
             else -> throw IllegalArgumentException("Unsupported rotation angle: ${cameraFrame.rotation}")
         }
+    }
 
-        require(cameraFrame.format == ImageFormat.NV21)
-        val (croppedFrameSize, croppedFrameBytes) = libYuvJni.cropRotateYuvNV21(
-            Size(cameraWidth, cameraHeight), cameraFrame.data,
-            rect = rotatedTargetBoundingBox.toRect(),
-            rotation = cameraFrame.rotation
-        )
-
-        return PreviewFrame(
-            croppedFrameSize.width,
-            croppedFrameSize.height,
-            cameraFrame.format,
-            facingFront,
-            croppedFrameBytes
-        ).also {
-            cameraFrame.release()
+    private fun getRectSizeBasedOnCameraCropping(
+        screenWidth: Int,
+        screenHeight: Int,
+        cameraWidth: Int,
+        cameraHeight: Int,
+        boxOnTheScreen: RectF
+    ): Float {
+        return if (screenWidth == cameraWidth || screenHeight == cameraHeight) {
+            val cameraArea = cameraHeight * cameraWidth
+            val screenArea = screenHeight * screenWidth
+            // This means center crop is tight
+            if (cameraArea > screenArea) {
+                boxOnTheScreen.width()
+            } else {
+                // This means the camera is zooming in to center crop
+                sizeFromMinRatio(
+                    cameraWidth,
+                    screenWidth,
+                    cameraHeight,
+                    screenHeight,
+                    boxOnTheScreen.width()
+                )
+            }
+        } else {
+            // This means the camera is zooming out to center crop
+            sizeFromMinRatio(
+                cameraWidth,
+                screenWidth,
+                cameraHeight,
+                screenHeight,
+                boxOnTheScreen.width()
+            )
         }
     }
+
+    private fun getCameraRotatedPair(cameraFrame: Frame): Pair<Int, Int> =
+        if (cameraRotatedPortrait(cameraFrame)) {
+            Pair(cameraFrame.size.height, cameraFrame.size.width)
+        } else {
+            Pair(cameraFrame.size.width, cameraFrame.size.height)
+        }
+
+    private fun cameraRotatedPortrait(cameraFrame: Frame) = cameraFrame.rotation in arrayOf(90, 270)
 
     private fun sizeFromMinRatio(
         cameraWidth: Int,
