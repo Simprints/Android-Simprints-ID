9c91d1e173c728099eef528a60a29086c1e5e78c
Tsuharesu <tsuharesu01@gmail.com>
2020-04-09 19:11:35 +0100

    Add FacePreferenceManager to ViewModels and create default values
    Former-commit-id: c6de015eb06b6b8bf074ee3c0f1723d280fee4b7

--- face/src/main/java/com/simprints/face/controllers/core/preferencesManager/FacePreferencesManager.kt
+++ face/src/main/java/com/simprints/face/controllers/core/preferencesManager/FacePreferencesManager.kt
@@ -1,3 +1,14 @@
 package com.simprints.face.controllers.core.preferencesManager
 
-interface FacePreferencesManager
+interface FacePreferencesManager {
+    /**
+     * Maximum number of retries possible after first try
+     * E.g. 2 retries will make for 3 chances (0, 1, 2)
+     */
+    var maxRetries: Int
+
+    /**
+     * What is the minimum quality for a picture to be deemed as a good image
+     */
+    var qualityThreshold: Float
+}
--- face/src/main/java/com/simprints/face/controllers/core/preferencesManager/FacePreferencesManagerImpl.kt
+++ face/src/main/java/com/simprints/face/controllers/core/preferencesManager/FacePreferencesManagerImpl.kt
@@ -2,4 +2,9 @@ package com.simprints.face.controllers.core.preferencesManager
 
 import com.simprints.id.data.prefs.PreferencesManager
 
-class FacePreferencesManagerImpl(private val prefs: PreferencesManager) : FacePreferencesManager
+// TODO: get correct value from PreferencesManager
+class FacePreferencesManagerImpl(private val prefs: PreferencesManager) : FacePreferencesManager {
+    override var maxRetries: Int = 2
+
+    override var qualityThreshold: Float = -1f
+}
