36e346b773af282688c609b51cc4a1a561e8f1a8
Aashay Gaikwad <aashay@simprints.com>
2020-04-09 13:09:04 +0530

    WIP: Added EventRemoteDataSource to repository
    Former-commit-id: 1f6ca652ec59564cb361c7a09254a3ff6dc7ab2a

--- id/src/main/java/com/simprints/id/data/db/person/PersonRepository.kt
+++ id/src/main/java/com/simprints/id/data/db/person/PersonRepository.kt
@@ -1,7 +1,7 @@
 package com.simprints.id.data.db.person
 
 import com.simprints.id.data.db.PersonFetchResult
-import com.simprints.id.data.db.common.models.PeopleCount
+import com.simprints.id.data.db.common.models.EventCount
 import com.simprints.id.data.db.people_sync.down.domain.PeopleDownSyncScope
 import com.simprints.id.data.db.person.domain.Person
 import com.simprints.id.data.db.person.local.FingerprintIdentityLocalDataSource
@@ -10,7 +10,7 @@ import com.simprints.id.data.db.person.remote.PersonRemoteDataSource
 
 interface PersonRepository : PersonLocalDataSource, PersonRemoteDataSource, FingerprintIdentityLocalDataSource {
 
-    suspend fun countToDownSync(peopleDownSyncScope: PeopleDownSyncScope): List<PeopleCount>
+    suspend fun countToDownSync(peopleDownSyncScope: PeopleDownSyncScope): List<EventCount>
 
     suspend fun saveAndUpload(person: Person)
     suspend fun loadFromRemoteIfNeeded(projectId: String, patientId: String): PersonFetchResult
