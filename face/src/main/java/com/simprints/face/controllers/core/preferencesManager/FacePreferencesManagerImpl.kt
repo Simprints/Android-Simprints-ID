a0ea9784608ae67e2fea96d06fe0bde9098d443b
Tsuharesu <tsuharesu01@gmail.com>
2020-04-10 14:17:08 +0100

    Add default values to preferences
    Former-commit-id: c97917c39666daf38ae32f9df2dacee694d946fc

--- face/src/main/java/com/simprints/face/controllers/core/preferencesManager/FacePreferencesManagerImpl.kt
+++ face/src/main/java/com/simprints/face/controllers/core/preferencesManager/FacePreferencesManagerImpl.kt
@@ -2,9 +2,16 @@ package com.simprints.face.controllers.core.preferencesManager
 
 import com.simprints.id.data.prefs.PreferencesManager
 
-// TODO: get correct value from PreferencesManager
 class FacePreferencesManagerImpl(private val prefs: PreferencesManager) : FacePreferencesManager {
-    override var maxRetries: Int = 2
+    override var maxRetries: Int = prefs.faceMaxRetries
+        set(value) {
+            field = value
+            prefs.faceMaxRetries = field
+        }
 
-    override var qualityThreshold: Float = -1f
+    override var qualityThreshold: Float = prefs.faceQualityThreshold
+        set(value) {
+            field = value
+            prefs.faceQualityThreshold = field
+        }
 }
