76fb7a8629a48ba687724b34bc7e41cd68a2ca45
Aashay Gaikwad <aashay@simprints.com>
2020-04-16 18:23:33 +0530

    Added tests
    Former-commit-id: dcf1ff4f77484a2265a0ced2dcbab81618cddd7d

--- id/src/main/java/com/simprints/id/data/db/person/remote/EventRemoteDataSourceImpl.kt
+++ id/src/main/java/com/simprints/id/data/db/person/remote/EventRemoteDataSourceImpl.kt
@@ -41,7 +41,7 @@ class EventRemoteDataSourceImpl(private val remoteDbManager: RemoteDbManager,
         }, "RecordDownload")
     }
 
-    override suspend fun write(projectId: String, events: ApiEvents) {
+    override suspend fun post(projectId: String, events: ApiEvents) {
         makeNetworkRequest({
             it.postEnrolmentRecordEvents(projectId, events)
         }, "RecordWrite")
--- id/src/test/java/com/simprints/id/data/db/person/remote/EventRemoteDataSourceImplTest.kt
+++ id/src/test/java/com/simprints/id/data/db/person/remote/EventRemoteDataSourceImplTest.kt
@@ -2,6 +2,7 @@ package com.simprints.id.data.db.person.remote
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
+import com.google.gson.reflect.TypeToken
 import com.simprints.core.network.BaseUrlProvider
 import com.simprints.core.network.SimApiClientFactory
 import com.simprints.id.commontesttools.EnrolmentRecordsGeneratorUtils.getRandomEnrolmentEvents
@@ -9,10 +10,10 @@ import com.simprints.id.data.db.common.models.EventCount
 import com.simprints.id.data.db.common.models.EventType
 import com.simprints.id.data.db.people_sync.down.domain.EventQuery
 import com.simprints.id.data.db.person.domain.personevents.EnrolmentRecordOperationType
-import com.simprints.id.data.db.person.remote.models.personevents.ApiEvents
-import com.simprints.id.data.db.person.remote.models.personevents.fromDomainToApi
+import com.simprints.id.data.db.person.remote.models.personevents.*
 import com.simprints.id.domain.modality.Modes
 import com.simprints.id.testtools.UnitTestConfig
+import com.simprints.id.tools.json.SimJsonHelper
 import com.simprints.testtools.unit.mockserver.mockSuccessfulResponse
 import io.mockk.coEvery
 import io.mockk.every
@@ -35,10 +36,12 @@ class EventRemoteDataSourceImplTest {
         private const val SUBJECT_ID = "subject_id"
         private const val LAST_EVENT_ID = "last_event_id"
         private val MODULES = listOf("module1", "module2")
+        private const val EVENTS_JSON = "[{\"id\":\"e9257686-663f-4943-943e-09f9fdd9252b\",\"labels\":{\"moduleId\":[\"module1\"],\"projectId\":[\"projectId\"],\"attendantId\":[\"user1\"]},\"payload\":{\"type\":\"EnrolmentRecordCreation\",\"subjectId\":\"2d6b446d-e7a6-4665-a2ad-23c4c95f297a\",\"projectId\":\"projectId\",\"attendantId\":\"user1\",\"moduleId\":\"module1\",\"biometricReferences\":[{\"type\":\"FingerprintReference\",\"metadata\":{\"vero\":\"VERO_2\"},\"templates\":[{\"template\":\"Rk1SACAyMAAAAADSAAABLAGQAMUAxQEAABBfHoCoAE5iAECVAHFnAEBBAIx7AEBLANd7AECyANrRAECRANvUAEBgAOTUAECDAO/HAIBfAPVVAIBRAPZ4AECbAPvHAEC7AP5RAEBuAQFQAEB7AQJKAEBhAQRiAIBEAQeqAEA+ARPQAECMARZHAEB3ARxRAECrASpRAEBmAStiAECTATJUAEBiAUVsAEByAUloAECFAUldAIA0AUpoAECUAUxfAECRAWNzAEB7AWp1AECKAYN9AAAA\",\"finger\":\"LEFT_THUMB\",\"quality\":82}]},{\"type\":\"FaceReference\",\"metadata\":{\"SDK\":\"ML_KIT\"},\"templates\":[{\"template\":\"Rk1SACAyMAAAAADSAAABLAGQAMUAxQEAABBfHoCoAE5iAECVAHFnAEBBAIx7AEBLANd7AECyANrRAECRANvUAEBgAOTUAECDAO/HAIBfAPVVAIBRAPZ4AECbAPvHAEC7AP5RAEBuAQFQAEB7AQJKAEBhAQRiAIBEAQeqAEA+ARPQAECMARZHAEB3ARxRAECrASpRAEBmAStiAECTATJUAEBiAUVsAEByAUloAECFAUldAIA0AUpoAECUAUxfAECRAWNzAEB7AWp1AECKAYN9AAAA\"}]}]}},{\"id\":\"e9257686-663f-4943-943e-09f9fdd925sd\",\"labels\":{\"moduleId\":[\"module1\"],\"projectId\":[\"projectId\"],\"attendantId\":[\"user1\"]},\"payload\":{\"type\":\"EnrolmentRecordDeletion\",\"subjectId\":\"2d6b446d-e7a6-4665-a2ad-23c4c95f297a\",\"projectId\":\"projectId\",\"attendantId\":\"user1\",\"moduleId\":\"module1\",\"biometricReferences\":[{\"type\":\"FingerprintReference\",\"metadata\":{\"vero\":\"VERO_2\"},\"templates\":[{\"template\":\"Rk1SACAyMAAAAADSAAABLAGQAMUAxQEAABBfHoCoAE5iAECVAHFnAEBBAIx7AEBLANd7AECyANrRAECRANvUAEBgAOTUAECDAO/HAIBfAPVVAIBRAPZ4AECbAPvHAEC7AP5RAEBuAQFQAEB7AQJKAEBhAQRiAIBEAQeqAEA+ARPQAECMARZHAEB3ARxRAECrASpRAEBmAStiAECTATJUAEBiAUVsAEByAUloAECFAUldAIA0AUpoAECUAUxfAECRAWNzAEB7AWp1AECKAYN9AAAA\",\"finger\":\"LEFT_THUMB\",\"quality\":82}]},{\"type\":\"FaceReference\",\"metadata\":{\"SDK\":\"ML_KIT\"},\"templates\":[{\"template\":\"Rk1SACAyMAAAAADSAAABLAGQAMUAxQEAABBfHoCoAE5iAECVAHFnAEBBAIx7AEBLANd7AECyANrRAECRANvUAEBgAOTUAECDAO/HAIBfAPVVAIBRAPZ4AECbAPvHAEC7AP5RAEBuAQFQAEB7AQJKAEBhAQRiAIBEAQeqAEA+ARPQAECMARZHAEB3ARxRAECrASpRAEBmAStiAECTATJUAEBiAUVsAEByAUloAECFAUldAIA0AUpoAECUAUxfAECRAWNzAEB7AWp1AECKAYN9AAAA\"}]}]}},{\"id\":\"e9257686-663f-4943-943e-09f9fdd925dsa\",\"labels\":{\"moduleId\":[\"module1\"],\"projectId\":[\"projectId\"],\"attendantId\":[\"user1\"]},\"payload\":{\"type\":\"EnrolmentRecordMove\",\"subjectId\":\"2d6b446d-e7a6-4665-a2ad-23c4c95f297a\",\"projectId\":\"projectId\",\"attendantId\":\"user1\",\"moduleId\":\"module1\",\"biometricReferences\":[{\"type\":\"FingerprintReference\",\"metadata\":{\"vero\":\"VERO_2\"},\"templates\":[{\"template\":\"Rk1SACAyMAAAAADSAAABLAGQAMUAxQEAABBfHoCoAE5iAECVAHFnAEBBAIx7AEBLANd7AECyANrRAECRANvUAEBgAOTUAECDAO/HAIBfAPVVAIBRAPZ4AECbAPvHAEC7AP5RAEBuAQFQAEB7AQJKAEBhAQRiAIBEAQeqAEA+ARPQAECMARZHAEB3ARxRAECrASpRAEBmAStiAECTATJUAEBiAUVsAEByAUloAECFAUldAIA0AUpoAECUAUxfAECRAWNzAEB7AWp1AECKAYN9AAAA\",\"finger\":\"LEFT_THUMB\",\"quality\":82}]},{\"type\":\"FaceReference\",\"metadata\":{\"SDK\":\"ML_KIT\"},\"templates\":[{\"template\":\"Rk1SACAyMAAAAADSAAABLAGQAMUAxQEAABBfHoCoAE5iAECVAHFnAEBBAIx7AEBLANd7AECyANrRAECRANvUAEBgAOTUAECDAO/HAIBfAPVVAIBRAPZ4AECbAPvHAEC7AP5RAEBuAQFQAEB7AQJKAEBhAQRiAIBEAQeqAEA+ARPQAECMARZHAEB3ARxRAECrASpRAEBmAStiAECTATJUAEBiAUVsAEByAUloAECFAUldAIA0AUpoAECUAUxfAECRAWNzAEB7AWp1AECKAYN9AAAA\"}]}]}}]"
     }
 
     private val mockServer = MockWebServer()
     private val mockBaseUrlProvider: BaseUrlProvider = mockk()
+    private val gson = SimJsonHelper.gson
 
     private val eventRemoteDataSourceSpy = spyk(EventRemoteDataSourceImpl(mockk(), mockk()))
     private lateinit var enrolmentEventRecordRemoteInterface: EnrolmentEventRecordRemoteInterface
@@ -54,15 +57,19 @@ class EventRemoteDataSourceImplTest {
     @Test
     fun successfulResponse_onWrite() {
         runBlocking {
+            val expectedRequestUrlFormat = "/projects/projectId/events"
             enrolmentEventRecordRemoteInterface = SimApiClientFactory(
-                mockBaseUrlProvider, "deviceId"
+                mockBaseUrlProvider, "deviceId", gson
             ).build<EnrolmentEventRecordRemoteInterface>().api
             coEvery { eventRemoteDataSourceSpy.getPeopleApiClient() } returns enrolmentEventRecordRemoteInterface
             mockServer.enqueue(mockSuccessfulResponse())
 
-            eventRemoteDataSourceSpy.write("projectId", buildEnrolmentRecordEvents())
+            eventRemoteDataSourceSpy.post("projectId", buildEnrolmentRecordEvents())
 
-            assertThat(mockServer.requestCount).isEqualTo(1)
+            with(mockServer) {
+                assertThat(requestCount).isEqualTo(1)
+                assertThat(takeRequest().requestUrl.toString()).contains(expectedRequestUrlFormat)
+            }
         }
     }
 
@@ -76,7 +83,7 @@ class EventRemoteDataSourceImplTest {
             )
             val expectedRequestUrlFormat = "projects/project_id/events/count?l_moduleId=module1&l_moduleId=module2&l_attendantId=user_id&l_subjectId=subject_id&l_mode=FINGERPRINT&l_mode=FACE&lastEventId=last_event_id&type=EnrolmentRecordMove&type=EnrolmentRecordDeletion&type=EnrolmentRecordCreation"
             enrolmentEventRecordRemoteInterface = SimApiClientFactory(
-                mockBaseUrlProvider, "deviceId"
+                mockBaseUrlProvider, "deviceId", gson
             ).build<EnrolmentEventRecordRemoteInterface>().api
 
             coEvery { eventRemoteDataSourceSpy.getPeopleApiClient() } returns enrolmentEventRecordRemoteInterface
@@ -93,13 +100,35 @@ class EventRemoteDataSourceImplTest {
         }
     }
 
-    /*TODO: We will be creating a separate helper for the downsync of the enrolment records.
-       Will be adding tests for that in the EnrolmentRecordRepository work*/
+    @Test
+    fun successfulResponse_get_shouldFormCorrectUrlAndInstances() {
+        runBlocking {
+            val expectedUrlFormat = "/projects/project_id/events?l_moduleId=module1&l_moduleId=module2&l_attendantId=user_id&l_subjectId=subject_id&l_mode=FINGERPRINT&l_mode=FACE&lastEventId=last_event_id&type=EnrolmentRecordMove&type=EnrolmentRecordDeletion&type=EnrolmentRecordCreation"
+            enrolmentEventRecordRemoteInterface = SimApiClientFactory(
+                mockBaseUrlProvider, "deviceId", gson
+            ).build<EnrolmentEventRecordRemoteInterface>().api
+            coEvery { eventRemoteDataSourceSpy.getPeopleApiClient() } returns enrolmentEventRecordRemoteInterface
+            mockServer.enqueue(buildSuccessfulResponseForGetEvents())
+
+            val responseString = eventRemoteDataSourceSpy.get(buildEventQuery()).string()
+
+            assertThat(mockServer.requestCount).isEqualTo(1)
+            assertThat(mockServer.takeRequest().requestUrl.toString()).contains(expectedUrlFormat)
+            val apiEvents = parseApiEventsFromResponse(responseString)
+            with (apiEvents) {
+                assertThat(size).isEqualTo(3)
+                assertThat(get(0).payload).isInstanceOf(ApiEnrolmentRecordCreation::class.java)
+                assertThat(get(1).payload).isInstanceOf(ApiEnrolmentRecordDeletion::class.java)
+                assertThat(get(2).payload).isInstanceOf(ApiEnrolmentRecordMove::class.java)
+            }
+        }
+
+    }
 
     private fun buildEnrolmentRecordEvents() = ApiEvents(buildApiEventsList())
 
     private fun buildApiEventsList() =
-        getRandomEnrolmentEvents(5, "projectId", "userId", "moduleId").map {
+        getRandomEnrolmentEvents(5, PROJECT_ID, USER_ID, "module_id").map {
             it.fromDomainToApi()
         }
 
@@ -118,5 +147,15 @@ class EventRemoteDataSourceImplTest {
     private fun buildSuccessfulResponseForCount() = MockResponse().apply {
         setResponseCode(200)
         setBody("[{\"type\":\"EnrolmentRecordCreation\",\"count\":42},{\"type\":\"EnrolmentRecordDeletion\",\"count\":42},{\"type\":\"EnrolmentRecordMove\",\"count\":42}]")
+    }
+
+    private fun buildSuccessfulResponseForGetEvents() = MockResponse().apply {
+        setResponseCode(200)
+        setBody(EVENTS_JSON)
+    }
+
+    private fun parseApiEventsFromResponse(responseString: String): List<ApiEvent> {
+        val listType = object : TypeToken<ArrayList<ApiEvent?>?>() {}.type
+        return SimJsonHelper.gson.fromJson<List<ApiEvent>>(responseString, listType)
     }
 }
