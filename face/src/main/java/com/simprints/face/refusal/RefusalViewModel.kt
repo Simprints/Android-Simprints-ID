c47703edf3329380e5ccb4e1a41004990286d721
Tsuharesu <tsuharesu01@gmail.com>
2020-04-23 17:38:29 +0100

    Create exit form with correct layout
    Former-commit-id: 39025aaf4c8f5488b429d097d6ec3ab7a21d9dca

--- face/src/main/java/com/simprints/face/refusal/RefusalViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.simprints.face.refusal
-
-import androidx.lifecycle.ViewModel
-import com.simprints.face.capture.FaceCaptureViewModel
-
-class RefusalViewModel(private val mainVM: FaceCaptureViewModel) : ViewModel() {
-
-}
