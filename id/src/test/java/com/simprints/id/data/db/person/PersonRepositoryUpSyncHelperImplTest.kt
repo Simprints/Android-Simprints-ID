c94a2e1f5bc45acbdffae922c1981a941b8b7264
Aashay Gaikwad <aashay@simprints.com>
2020-04-25 12:29:34 +0530

    Tests for upsync helper
    Former-commit-id: 053058b4d50aa2baffffcab0d356aab697f138e2

--- id/src/test/java/com/simprints/id/data/db/person/PersonRepositoryUpSyncHelperImplTest.kt
+++ id/src/test/java/com/simprints/id/data/db/person/PersonRepositoryUpSyncHelperImplTest.kt
@@ -1,6 +1,5 @@
 package com.simprints.id.data.db.person
 
-import com.google.common.truth.Truth
 import com.google.common.truth.Truth.assertThat
 import com.simprints.id.data.db.people_sync.up.PeopleUpSyncScopeRepository
 import com.simprints.id.data.db.person.domain.Person
@@ -9,11 +8,13 @@ import com.simprints.id.data.db.person.local.PersonLocalDataSource
 import com.simprints.id.data.db.person.remote.EventRemoteDataSource
 import com.simprints.id.data.loginInfo.LoginInfoManager
 import com.simprints.id.domain.modality.Modality
-import com.simprints.id.services.scheduledSync.people.master.internal.PeopleSyncCache
+import com.simprints.id.exceptions.safe.sync.SyncCloudIntegrationException
 import com.simprints.id.testtools.UnitTestConfig
+import com.simprints.testtools.common.channel.testChannel
 import com.simprints.testtools.common.syntax.assertThrows
 import io.mockk.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
@@ -21,18 +22,17 @@ import org.junit.Before
 import org.junit.Test
 import java.util.*
 
+@ExperimentalCoroutinesApi
 class PersonRepositoryUpSyncHelperImplTest {
     private val loginInfoManager: LoginInfoManager = mockk(relaxed = true)
     private val personLocalDataSource: PersonLocalDataSource = mockk(relaxed = true)
-    private val eventRemoteDataSource: EventRemoteDataSource = mockk(relaxed = true)
+    private val eventRemoteDataSource: EventRemoteDataSource = mockk()
     private val peopleUpSyncScopeRepository: PeopleUpSyncScopeRepository = mockk(relaxed = true)
-    private val peopleSyncCache: PeopleSyncCache = mockk(relaxed = true)
     private val modalities = listOf(Modality.FACE, Modality.FINGER)
 
-    val personRepositoryUpSyncHelper = spyk(PersonRepositoryUpSyncHelperImpl(loginInfoManager,
+    private val personRepositoryUpSyncHelper = spyk(PersonRepositoryUpSyncHelperImpl(loginInfoManager,
             personLocalDataSource, eventRemoteDataSource, peopleUpSyncScopeRepository, modalities))
 
-    private val uniqueWorkerId = "uniqueWorkerId"
     private val projectIdToSync = "projectIdToSync"
     private val userIdToSync = "userIdToSync"
     private val batchSize = 2
@@ -42,7 +42,6 @@ class PersonRepositoryUpSyncHelperImplTest {
     )
     private val notYetSyncedPerson2 = notYetSyncedPerson1.copy(patientId = "patientId2")
     private val notYetSyncedPerson3 = notYetSyncedPerson1.copy(patientId = "patientId3")
-    private val events = personRepositoryUpSyncHelper.createEvents(listOf(notYetSyncedPerson1, notYetSyncedPerson2))
 
     private val syncedPerson1 = notYetSyncedPerson1.copy(toSync = false)
     private val syncedPerson2 = notYetSyncedPerson2.copy(toSync = false)
@@ -52,6 +51,8 @@ class PersonRepositoryUpSyncHelperImplTest {
     fun setUp() {
         UnitTestConfig(this)
             .coroutinesMainThread()
+        setupBatchSize()
+        mockHelperToGenerateSameUuidForEvents()
     }
 
     @Test
@@ -68,40 +69,111 @@ class PersonRepositoryUpSyncHelperImplTest {
         }
     }
 
-//    @Test
-//    fun singleBatchNoConcurrentWrite() =
-//        testSuccessfulUpSync(
-//            localQueryResults = arrayOf(
-//                listOf(notYetSyncedPerson1, notYetSyncedPerson2)
-//            ),
-//            expectedUploadBatches = personRepositoryUpSyncHelper.createEvents(
-//                listOf(notYetSyncedPerson1, notYetSyncedPerson2)
-//            ),
-//            expectedLocalUpdates = arrayOf(
-//                listOf(syncedPerson1, syncedPerson2)
-//            )
-//        )
+    @Test
+    fun simprintsInternalServerException_shouldWrapInTransientSyncFailureException() {
+        runBlocking {
+            mockSignedInUser(projectIdToSync, userIdToSync)
+            mockSuccessfulLocalPeopleQueries(listOf(notYetSyncedPerson1))
+            coEvery { eventRemoteDataSource.post(projectIdToSync, any()) } throws SyncCloudIntegrationException("", Throwable())
 
+            assertThrows<SyncCloudIntegrationException> {
+                withContext(Dispatchers.IO) {
+                    personRepositoryUpSyncHelper.executeUploadWithProgress(this)
+                }
+            }
+        }
+    }
+
+    @Test
+    fun singleBatchNoConcurrentWrite() {
+        val peopleBatches = arrayOf(listOf(notYetSyncedPerson1, notYetSyncedPerson2))
+        val events = createEventsFromPeople(
+            arrayOf(
+                listOf(notYetSyncedPerson1, notYetSyncedPerson2)
+            )
+        )
+
+        testSuccessfulUpSync(
+            localQueryResults = peopleBatches,
+            expectedUploadBatches = events,
+            expectedLocalUpdates = arrayOf(listOf(syncedPerson1, syncedPerson2))
+        )
+    }
+
+    @Test
+    fun multipleBatchesNoConcurrentWrite() {
+        val peopleBatches = arrayOf(listOf(notYetSyncedPerson1, notYetSyncedPerson2, notYetSyncedPerson3))
+        val events = createEventsFromPeople(
+            arrayOf(
+                listOf(notYetSyncedPerson1, notYetSyncedPerson2),
+                listOf(notYetSyncedPerson3)
+            )
+        )
+
+        testSuccessfulUpSync(
+            localQueryResults = peopleBatches,
+            expectedUploadBatches = events,
+            expectedLocalUpdates = arrayOf(
+                listOf(syncedPerson1, syncedPerson2),
+                listOf(syncedPerson3)
+            )
+        )
+    }
+
+    @Test
+    fun singleBatchConcurrentWrite() {
+        val peopleBatches = arrayOf(
+            listOf(notYetSyncedPerson1, notYetSyncedPerson2),
+            listOf(notYetSyncedPerson3)
+        )
+        val events = createEventsFromPeople(
+            arrayOf(
+                listOf(notYetSyncedPerson1, notYetSyncedPerson2),
+                listOf(notYetSyncedPerson3)
+            )
+        )
+
+        testSuccessfulUpSync(
+            localQueryResults = peopleBatches,
+            expectedUploadBatches = events,
+            expectedLocalUpdates = arrayOf(
+                listOf(syncedPerson1, syncedPerson2),
+                listOf(syncedPerson3)
+            )
+        )
+    }
+
+    private fun setupBatchSize() {
+        every { personRepositoryUpSyncHelper.batchSize } returns batchSize
+    }
 
     private fun testSuccessfulUpSync(
         localQueryResults: Array<List<Person>>,
-        expectedUploadBatches: Events,
+        expectedUploadBatches: Array<Events>,
         expectedLocalUpdates: Array<List<Person>>
     ) {
-        mockSignedInUser(projectIdToSync, userIdToSync)
-        mockSuccessfulLocalPeopleQueries(*localQueryResults)
-        mockSuccessfulLocalPeopleUpdates(*expectedLocalUpdates)
-        every { personRepositoryUpSyncHelper.createEvents(any()) } returns events
-        mockSyncStatusModel()
-
         runBlocking {
-            personRepositoryUpSyncHelper.executeUploadWithProgress(this)
+
+            mockSignedInUser(projectIdToSync, userIdToSync)
+            mockSuccessfulLocalPeopleQueries(*localQueryResults)
+            mockSuccessfulLocalPeopleUpdates()
+            mockEventRemoteDataSource()
+            mockSyncStatusModel()
+
+            withContext(Dispatchers.IO) {
+                personRepositoryUpSyncHelper.executeUploadWithProgress(this).testChannel()
+                verifyLocalPeopleQueries()
+                verifyPeopleUploads(expectedUploadBatches)
+                verifyLocalPeopleUpdates(*expectedLocalUpdates)
+            }
         }
 
-        verifyLocalPeopleQueries(*localQueryResults)
-        verifyPeopleUploads(expectedUploadBatches)
-        verifyLocalPeopleUpdates(*expectedLocalUpdates)
     }
+
+    private fun createEventsFromPeople(people: Array<List<Person>>) =
+        people.map {
+            personRepositoryUpSyncHelper.createEvents(it)
+        }.toTypedArray()
 
     private fun mockSignedInUser(projectId: String, userId: String) {
         every { loginInfoManager.getSignedInProjectIdOrEmpty() } returns projectId
@@ -114,26 +186,34 @@ class PersonRepositoryUpSyncHelperImplTest {
         }
     }
 
-    private fun mockSuccessfulLocalPeopleUpdates(vararg updates: List<Person>) {
-        updates.forEach { update ->
-            coEvery { personLocalDataSource.insertOrUpdate(any()) } returns Unit
-        }
+    private fun mockSuccessfulLocalPeopleUpdates() {
+        coEvery { personLocalDataSource.insertOrUpdate(any()) } returns Unit
+    }
+
+    private fun mockHelperToGenerateSameUuidForEvents() {
+        every { personRepositoryUpSyncHelper.getRandomUuid() } returns "random_uuid"
+    }
+
+    private fun mockEventRemoteDataSource() {
+        coEvery{ eventRemoteDataSource.post(projectIdToSync, any()) } returns Unit
     }
 
     private fun mockSyncStatusModel() {
         coEvery { peopleUpSyncScopeRepository.insertOrUpdate(any()) } returns Unit
     }
 
-    private fun verifyLocalPeopleQueries(vararg queryResults: List<Person>) {
+    private fun verifyLocalPeopleQueries() {
         coVerify(exactly = 1) {
             personLocalDataSource.load(withArg {
-                Truth.assertThat(it.toSync).isEqualTo(true)
+                assertThat(it.toSync).isEqualTo(true)
             })
         }
     }
 
-    private fun verifyPeopleUploads(events: Events) {
-        coVerify(exactly = 1) { eventRemoteDataSource.post(projectIdToSync, events) }
+    private fun verifyPeopleUploads(events: Array<Events>) {
+        events.forEach {
+            coVerify(exactly = 1) { eventRemoteDataSource.post(projectIdToSync, it) }
+        }
     }
 
     private fun verifyLocalPeopleUpdates(vararg updates: List<Person>) {
