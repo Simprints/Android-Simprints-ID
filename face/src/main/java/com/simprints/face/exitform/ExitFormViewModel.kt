b4fe28c298c093b1b5e531dedc4cb2599506faaf
Tsuharesu <tsuharesu01@gmail.com>
2020-04-27 12:16:45 +0100

    Implement listeners and save reason changes
    Former-commit-id: 7bfd649b20ef7aa6e6f4bfaccada61791ee4ec11

--- face/src/main/java/com/simprints/face/exitform/ExitFormViewModel.kt
+++ face/src/main/java/com/simprints/face/exitform/ExitFormViewModel.kt
@@ -1,8 +1,65 @@
 package com.simprints.face.exitform
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.simprints.core.livedata.LiveDataEvent
+import com.simprints.core.livedata.send
 import com.simprints.face.capture.FaceCaptureViewModel
+import com.simprints.face.data.domain.exitform.RefusalFormReason
+import com.simprints.face.data.domain.exitform.RefusalFormReason.*
 
 class ExitFormViewModel(private val mainVM: FaceCaptureViewModel) : ViewModel() {
+    private var reason: RefusalFormReason = OTHER
 
+    val requestReasonEvent: MutableLiveData<LiveDataEvent> = MutableLiveData()
+
+    fun handleReligiousConcernsRadioClick() {
+        reason = REFUSED_RELIGION
+        logRadioOptionForCrashReport("Religious concerns")
+    }
+
+    fun handleDataConcernsRadioClick() {
+        reason = REFUSED_DATA_CONCERNS
+        logRadioOptionForCrashReport("Data concerns")
+    }
+
+    fun handleTooYoungRadioClick() {
+        reason = REFUSED_YOUNG
+        logRadioOptionForCrashReport("Too young")
+    }
+
+    fun handlePersonNotPresentRadioClick() {
+        reason = REFUSED_NOT_PRESENT
+        logRadioOptionForCrashReport("Person not present")
+    }
+
+    fun handleDoesNotHavePermissionRadioClick() {
+        reason = REFUSED_PERMISSION
+        logRadioOptionForCrashReport("Does not have permission")
+    }
+
+    fun handleAppNotWorkingRadioClick() {
+        reason = SCANNER_NOT_WORKING
+        requestReasonEvent.send()
+        logRadioOptionForCrashReport("App not working")
+    }
+
+    fun handleOtherRadioOptionClick() {
+        reason = OTHER
+        requestReasonEvent.send()
+        logRadioOptionForCrashReport("Other")
+    }
+
+    fun submitExitForm(exitFormText: String) {
+        // TODO: create the correct answer and send back
+    }
+
+    private fun logRadioOptionForCrashReport(option: String) {
+        logMessageForCrashReport("Radio option $option clicked")
+    }
+
+    private fun logMessageForCrashReport(message: String) {
+        // TODO: log on the crash report manager
+//        crashReportManager.logMessageForCrashReport(CrashReportTag.REFUSAL, CrashReportTrigger.UI, message = message)
+    }
 }
