3c7f92a851224440c04390400ea39a3a1a400d70
Aashay Gaikwad <aashay@simprints.com>
2020-04-29 10:03:20 +0530

    CR Changes
    Former-commit-id: 9acc58eaa4aa72cd634ec5fd69512d53e3ddc56b

--- id/src/main/java/com/simprints/id/data/db/person/remote/models/personcounts/ApiEventCount.kt
+++ id/src/main/java/com/simprints/id/data/db/person/remote/models/personcounts/ApiEventCount.kt
@@ -1,22 +1,15 @@
 package com.simprints.id.data.db.person.remote.models.personcounts
 
 import androidx.annotation.Keep
-import com.google.gson.annotations.SerializedName
 import com.simprints.id.data.db.common.models.EventCount
-import com.simprints.id.data.db.common.models.EventType
+import com.simprints.id.data.db.person.domain.personevents.EventPayloadType
+import com.simprints.id.data.db.person.remote.models.personevents.ApiEventPayloadType
 
 @Keep
-class ApiEventCount(val type: ApiEventCountType, val count: Int)
-
-@Keep
-enum class ApiEventCountType {
-    @SerializedName("EnrolmentRecordCreation") ENROLMENT_RECORD_CREATION,
-    @SerializedName("EnrolmentRecordDeletion") ENROLMENT_RECORD_DELETION,
-    @SerializedName("EnrolmentRecordMove") ENROLMENT_RECORD_MOVE
-}
+class ApiEventCount(val type: ApiEventPayloadType, val count: Int)
 
 fun ApiEventCount.fromApiToDomain() = when(type) {
-    ApiEventCountType.ENROLMENT_RECORD_CREATION -> EventCount(EventType.ENROLMENT_RECORD_CREATION, count)
-    ApiEventCountType.ENROLMENT_RECORD_DELETION -> EventCount(EventType.ENROLMENT_RECORD_DELETION, count)
-    ApiEventCountType.ENROLMENT_RECORD_MOVE -> EventCount(EventType.ENROLMENT_RECORD_MOVE, count)
+    ApiEventPayloadType.ENROLMENT_RECORD_CREATION -> EventCount(EventPayloadType.ENROLMENT_RECORD_CREATION, count)
+    ApiEventPayloadType.ENROLMENT_RECORD_DELETION -> EventCount(EventPayloadType.ENROLMENT_RECORD_DELETION, count)
+    ApiEventPayloadType.ENROLMENT_RECORD_MOVE -> EventCount(EventPayloadType.ENROLMENT_RECORD_MOVE, count)
 }
