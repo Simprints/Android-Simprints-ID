9cc15f4369ab6e11552bf5accab95591b81b9edb
Ridwan Farouki <ridwan@simprints.com>
2020-05-07 13:27:51 +0100

    Factored new connect fragments
    Former-commit-id: e8d3174e1e8aa2ac41bef12b8606e7060e83d7fb

--- fingerprint/src/main/java/com/simprints/fingerprint/activities/connect/main/ConnectScannerMainFragment.kt
+++ fingerprint/src/main/java/com/simprints/fingerprint/activities/connect/main/ConnectScannerMainFragment.kt
@@ -1,4 +1,4 @@
-package com.simprints.fingerprint.activities.connect
+package com.simprints.fingerprint.activities.connect.main
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,11 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.simprints.fingerprint.R
 import com.simprints.fingerprint.activities.base.FingerprintFragment
+import com.simprints.fingerprint.activities.connect.ConnectScannerViewModel
 import com.simprints.fingerprint.activities.connect.request.ConnectScannerTaskRequest
-import com.simprints.fingerprint.exceptions.unexpected.request.InvalidRequestForConnectScannerActivityException
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 
 class ConnectScannerMainFragment : FingerprintFragment() {
+
+    private val connectScannerViewModel: ConnectScannerViewModel by sharedViewModel()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_connect_scanner_main, container, false)
@@ -19,11 +21,7 @@ class ConnectScannerMainFragment : FingerprintFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val connectScannerRequest: ConnectScannerTaskRequest =
-            arguments?.getParcelable(ConnectScannerTaskRequest.BUNDLE_KEY) as ConnectScannerTaskRequest?
-                ?: throw InvalidRequestForConnectScannerActivityException()
-
-        val targetFragment = when (connectScannerRequest.connectMode) {
+        val targetFragment = when (connectScannerViewModel.connectMode) {
             ConnectScannerTaskRequest.ConnectMode.INITIAL_CONNECT -> InitialConnectFragment()
             ConnectScannerTaskRequest.ConnectMode.RECONNECT -> ReconnectFragment()
         }
---
+++ fingerprint/src/main/java/com/simprints/fingerprint/activities/connect/main/ReconnectFragment.kt
@@ -0,0 +1,15 @@
+package com.simprints.fingerprint.activities.connect.main
+
+import com.simprints.fingerprint.R
+import kotlinx.android.synthetic.main.fragment_reconnect.*
+
+class ReconnectFragment : ConnectFragment(R.layout.fragment_reconnect) {
+
+    override fun initUiComponents() {
+        reconnect_message_text_view.text = androidResourcesHelper.getString(R.string.reconnecting_message)
+    }
+
+    override fun observeScannerEvents() {
+        connectScannerViewModel.showScannerErrorDialogWithScannerId.fragmentObserveEventWith { showDialogForScannerErrorConfirmation(it) }
+    }
+}
