27810e32777f8d53e60cf0189440b552f0a5d793
Ridwan Farouki <ridwan@simprints.com>
2020-05-11 23:47:56 +0100

    clean-up
    Former-commit-id: 45c071d883985ee31a5303cb7a32d9c681effb6a

--- fingerprint/src/main/java/com/simprints/fingerprint/activities/collect/CollectFingerprintsActivity.kt
+++ fingerprint/src/main/java/com/simprints/fingerprint/activities/collect/CollectFingerprintsActivity.kt
@@ -4,7 +4,6 @@ import android.app.AlertDialog
 import android.content.Intent
 import android.graphics.Paint
 import android.os.Bundle
-import android.view.View
 import android.view.WindowManager
 import androidx.lifecycle.Lifecycle
 import com.simprints.fingerprint.R
@@ -137,7 +136,7 @@ class CollectFingerprintsActivity : FingerprintActivity() {
     }
 
     private fun CollectFingerprintsState.updateViewPagerManager() {
-        fingerViewPagerManager.setCurrentPageAndFingerIds(fingerStates, currentFingerIndex)
+        fingerViewPagerManager.setCurrentPageAndFingerStates(fingerStates, currentFingerIndex)
     }
 
     private fun CollectFingerprintsState.updateScanButton() {
