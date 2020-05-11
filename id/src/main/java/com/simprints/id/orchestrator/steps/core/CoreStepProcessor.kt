afc84abb7346261ae19918a1aa834e369866eac6
Aashay Gaikwad <aashay@simprints.com>
2020-05-11 15:10:42 +0530

    Added setup step in core step processor
    Former-commit-id: ea7519af9313de7a4cb29b0809635f42e40241a6

--- id/src/main/java/com/simprints/id/orchestrator/steps/core/CoreStepProcessor.kt
+++ id/src/main/java/com/simprints/id/orchestrator/steps/core/CoreStepProcessor.kt
@@ -6,6 +6,8 @@ import com.simprints.id.orchestrator.steps.Step
 
 interface CoreStepProcessor {
 
+    fun buildStepSetup(): Step
+
     fun buildStepConsent(consentType: ConsentType): Step
 
     fun buildStepVerify(projectId: String, verifyGuid: String): Step
