c0dffd6c791e34953030dd3c23f4ba02d38cb2a4
Aashay Gaikwad <aashay@simprints.com>
2020-05-13 11:05:06 +0530

    Added SetupResponse
    Former-commit-id: 364ed37b41f34dbbedcc731cee469a771135196b

--- id/src/main/java/com/simprints/id/activities/setup/SetupActivity.kt
+++ id/src/main/java/com/simprints/id/activities/setup/SetupActivity.kt
@@ -1,6 +1,9 @@
 package com.simprints.id.activities.setup
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.location.LocationRequest
@@ -8,6 +11,8 @@ import com.simprints.id.Application
 import com.simprints.id.data.analytics.crashreport.CrashReportManager
 import com.simprints.id.data.db.session.SessionRepository
 import com.simprints.id.data.db.session.domain.models.session.Location
+import com.simprints.id.domain.moduleapi.core.response.CoreResponse
+import com.simprints.id.domain.moduleapi.core.response.SetupResponse
 import com.simprints.id.exceptions.safe.FailedToRetrieveUserLocation
 import com.simprints.id.tools.LocationManager
 import com.simprints.id.tools.extensions.requestPermissionsIfRequired
@@ -30,7 +35,18 @@ class SetupActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         askLocationPermissionIfRequired()
-        finish()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    storeUserLocationIntoCurrentSession()
+                }
+                setResultAndFinish()
+            }
+        }
     }
 
     private fun injectDependencies() {
@@ -39,12 +55,8 @@ class SetupActivity: AppCompatActivity() {
     }
 
     private fun askLocationPermissionIfRequired() {
-        val requestingPermissions = requestPermissionsIfRequired(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+        requestPermissionsIfRequired(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
             LOCATION_PERMISSION_REQUEST_CODE)
-
-        if (!requestingPermissions) {
-            storeUserLocationIntoCurrentSession()
-        }
     }
 
     private fun storeUserLocationIntoCurrentSession() {
@@ -65,6 +77,15 @@ class SetupActivity: AppCompatActivity() {
                 crashReportManager.logExceptionOrSafeException(FailedToRetrieveUserLocation(t))
             }
         }
+    }
+
+    private fun setResultAndFinish() {
+        setResult(Activity.RESULT_OK, buildIntentForResponse())
+        finish()
+    }
+
+    private fun buildIntentForResponse() = Intent().apply {
+        putExtra(CoreResponse.CORE_STEP_BUNDLE, SetupResponse())
     }
 
     companion object {
