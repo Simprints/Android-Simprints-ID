d4830d86253c5a4df9fe4ab5a42208d879ada042
Tristram Norman <tristram@simprints.com>
2020-05-16 14:46:54 +0100

    purposely try to break quality barrier
    Former-commit-id: 8a1a8200fe00e43628b8860acc7f53e430500f18

--- clientapi/src/main/java/com/simprints/clientapi/activities/BasePresenter.kt
+++ clientapi/src/main/java/com/simprints/clientapi/activities/BasePresenter.kt
@@ -3,6 +3,37 @@ package com.simprints.clientapi.activities
 
 interface BasePresenter {
 
-    suspend fun start()
+    suspend fun start() {
+        //            renderDebugInfo(it.face)
+        //            renderDebugInfo(it.face)
+        //            renderDebugInfo(it.face)
+        //            renderDebugInfo(it.face)
+        //            renderDebugInfo(it.face)
+        //            renderDebugInfo(it.face)
+        //            renderDebugInfo(it.face)
+        //            renderDebugInfo(it.face)//            renderDebugInfo(it.face)
+        //            renderDebugInfo(it.face)
+        //            renderDebugInfo(it.face)
+        //            renderDebugInfo(it.face)
+        //            renderDebugInfo(it.face)
+        //            renderDebugInfo(it.face)
+
+        if (true) {
+            if (true) {
+                return
+            }
+        }
+        if (true) {
+            if (true) {
+                return
+            }
+        }
+        if (true) {
+            if (true) {
+                return
+            }
+        }
+
+    }
 
 }
