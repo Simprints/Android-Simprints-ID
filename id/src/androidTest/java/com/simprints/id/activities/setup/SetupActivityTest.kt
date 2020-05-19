6d6786433920f655628fecee58ef6ee59e741a07
Aashay Gaikwad <aashay@simprints.com>
2020-05-19 13:01:17 +0530

    Mocking location response
    Former-commit-id: fffd901df66dd36e09f73bf5b01a7693141bc705

--- id/src/androidTest/java/com/simprints/id/activities/setup/SetupActivityTest.kt
+++ id/src/androidTest/java/com/simprints/id/activities/setup/SetupActivityTest.kt
@@ -1,9 +1,9 @@
 package com.simprints.id.activities.setup
 
 import android.content.Intent
+import android.location.Location
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.intent.Intents
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
 import com.simprints.id.Application
@@ -13,11 +13,14 @@ import com.simprints.id.domain.moduleapi.core.requests.SetupPermission
 import com.simprints.id.domain.moduleapi.core.requests.SetupRequest
 import com.simprints.id.domain.moduleapi.core.response.CoreResponse
 import com.simprints.id.testtools.AndroidTestConfig
+import com.simprints.id.tools.LocationManager
 import com.simprints.testtools.common.di.DependencyRule
 import io.mockk.MockKAnnotations
+import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.impl.annotations.RelaxedMockK
-import org.junit.After
+import io.mockk.mockk
+import kotlinx.coroutines.flow.flowOf
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -26,7 +29,16 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class SetupActivityTest {
 
+    companion object {
+        private const val PROVIDER = "flp"
+        private const val LAT = 37.377166
+        private const val LNG = -122.086966
+        private const val ACCURACY = 3.0f
+    }
+
     @RelaxedMockK lateinit var mockSessionRepository: SessionRepository
+    @RelaxedMockK lateinit var mockLocationManager: LocationManager
+
     private val app = ApplicationProvider.getApplicationContext<Application>()
 
     @get:Rule val permissionRule: GrantPermissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION)
@@ -35,6 +47,9 @@ class SetupActivityTest {
         TestAppModule(app,
             sessionEventsManagerRule = DependencyRule.ReplaceRule {
                 mockSessionRepository
+            },
+            locationManagerRule = DependencyRule.ReplaceRule {
+                mockLocationManager
             }
         )
     }
@@ -42,6 +57,7 @@ class SetupActivityTest {
     @Before
     fun setUp() {
         MockKAnnotations.init(this, relaxed = true)
+        coEvery { mockLocationManager.requestLocation(any()) } returns flowOf(listOf(buildFakeLocation()))
 
         app.component = AndroidTestConfig(this, appModule = appModule)
             .componentBuilder()
@@ -60,5 +76,11 @@ class SetupActivityTest {
         ActivityScenario.launch<SetupActivity>(intent)
 
         coVerify(exactly = 1) { mockSessionRepository.updateCurrentSession(any())}
+    }
+
+    private fun buildFakeLocation() = Location(PROVIDER).apply {
+        longitude = LNG
+        latitude = LAT
+        accuracy = ACCURACY
     }
 }
