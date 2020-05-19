8a8edb09bf6ce6f6f8f95b5e2b24f500da5d87f9
Fabio <fabio@simprints.com>
2020-05-19 16:51:36 +0100

    Using EnrolLastBiometrics Request in the Activity
    Former-commit-id: 0d51f187a4c7f6700c95be3e8c09ec20373b8a4c

--- id/src/main/java/com/simprints/id/activities/enrollast/EnrolLastBiometricsActivity.kt
+++ id/src/main/java/com/simprints/id/activities/enrollast/EnrolLastBiometricsActivity.kt
@@ -8,26 +8,20 @@ import androidx.lifecycle.lifecycleScope
 import com.simprints.id.Application
 import com.simprints.id.R
 import com.simprints.id.activities.alert.AlertActivityHelper
-import com.simprints.id.data.db.session.SessionRepository
-import com.simprints.id.data.db.session.domain.models.events.callout.IdentificationCalloutEvent
 import com.simprints.id.domain.alert.AlertType
-import com.simprints.id.domain.moduleapi.app.requests.AppRequest
 import com.simprints.id.domain.moduleapi.face.responses.FaceCaptureResponse
 import com.simprints.id.domain.moduleapi.fingerprint.responses.FingerprintCaptureResponse
+import com.simprints.id.exceptions.unexpected.InvalidAppRequest
 import com.simprints.id.orchestrator.EnrolmentHelper
-import com.simprints.id.orchestrator.cache.HotCache
 import com.simprints.id.orchestrator.steps.Step
-import com.simprints.id.orchestrator.steps.core.response.CoreEnrolLastBiometricsResponse
+import com.simprints.id.orchestrator.steps.core.requests.EnrolLastBiometricsRequest
+import com.simprints.id.orchestrator.steps.core.response.EnrolLastBiometricsResponse
 import com.simprints.id.orchestrator.steps.core.response.CoreResponse.Companion.CORE_STEP_BUNDLE
 import com.simprints.id.tools.TimeHelper
 import timber.log.Timber
-import java.util.*
 import javax.inject.Inject
 
 class EnrolLastBiometricsActivity : AppCompatActivity() {
-
-//    @Inject
-//    lateinit var hotCache: HotCache
 
     @Inject
     lateinit var enrolmentHelper: EnrolmentHelper
@@ -35,35 +29,37 @@ class EnrolLastBiometricsActivity : AppCompatActivity() {
     @Inject
     lateinit var timeHelper: TimeHelper
 
+    private lateinit var enrolLastBiometricsRequest: EnrolLastBiometricsRequest
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
         injectDependencies()
         Timber.d("EnrolLastBiometrics started")
 
+        enrolLastBiometricsRequest = intent.extras?.getParcelable(CORE_STEP_BUNDLE)
+            ?: throw InvalidAppRequest()
+
         lifecycleScope.launchWhenCreated {
-            try {
-//                val steps = hotCache.load()
-//                val request = hotCache.appRequest as AppRequest.AppEnrolLastBiometricsRequest
-//                val person = enrolmentHelper.buildPerson(
-//                    request.projectId,
-//                    request.userId,
-//                    request.moduleId,
-//                    getCaptureResponse<FingerprintCaptureResponse>(steps),
-//                    getCaptureResponse<FaceCaptureResponse>(steps),
-//                    timeHelper)
-//
-//                enrolmentHelper.enrol(person)
-//                sendOkResult(person.patientId)
-            } catch (t: Throwable) {
-                Timber.d(t)
-                AlertActivityHelper.launchAlert(this@EnrolLastBiometricsActivity, AlertType.ENROLMENT_LAST_BIOMETRIC_FAILED)
+            with(enrolLastBiometricsRequest) {
+                try {
+                    val person = enrolmentHelper.buildPerson(
+                        projectId,
+                        userId,
+                        moduleId,
+                        fingerprintCaptureResponse,
+                        faceCaptureResponse,
+                        timeHelper)
+
+                    enrolmentHelper.enrol(person)
+                    sendOkResult(person.patientId)
+                } catch (t: Throwable) {
+                    Timber.d(t)
+                    AlertActivityHelper.launchAlert(this@EnrolLastBiometricsActivity, AlertType.ENROLMENT_LAST_BIOMETRIC_FAILED)
+                }
             }
         }
     }
-
-    private inline fun <reified T> getCaptureResponse(steps: List<Step>) =
-        steps.firstOrNull { it.getResult() is T }?.getResult() as T?
 
 
     private fun injectDependencies() {
@@ -72,7 +68,7 @@ class EnrolLastBiometricsActivity : AppCompatActivity() {
     }
 
     private fun sendOkResult(newSubjectId: String) {
-        val response = CoreEnrolLastBiometricsResponse(newSubjectId)
+        val response = EnrolLastBiometricsResponse(newSubjectId)
         setResult(Activity.RESULT_OK, Intent().apply {
             putExtra(CORE_STEP_BUNDLE, response)
         })
