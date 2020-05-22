6045771594db3243d78922411671136a485287b6
Tsuharesu <tsuharesu01@gmail.com>
2020-05-22 19:00:38 +0100

    Run code in a flow
    Former-commit-id: a202182eaddbd82aaba8516fdfef49f168f655aa [formerly d888136ec939ba828518c494e2f964c27191b7af]
    Former-commit-id: 81816d1c288b67201ed51bd1dbb90649ab24f1b1

--- face/src/main/java/com/simprints/face/match/FaceMatchViewModel.kt
+++ face/src/main/java/com/simprints/face/match/FaceMatchViewModel.kt
@@ -12,8 +12,16 @@ import com.simprints.face.data.db.person.FaceIdentity
 import com.simprints.face.data.db.person.FaceSample
 import com.simprints.face.data.moduleapi.face.requests.FaceMatchRequest
 import com.simprints.face.data.moduleapi.face.responses.entities.FaceMatchResult
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
 import java.io.Serializable
+import kotlin.system.measureTimeMillis
 
 class FaceMatchViewModel(
     private val masterFlowManager: MasterFlowManager,
@@ -44,27 +52,30 @@ class FaceMatchViewModel(
             }
         }
 
-        val candidates = loadCandidates()
-        val results = matchCandidates(candidates)
-        sendSortedResults(results)
+        val time = measureTimeMillis {
+            val candidates = loadCandidates()
+            val results = matchCandidates(candidates)
+            sendSortedResults(results)
+        }
+        println("Time to complete $time ms")
     }
 
-    private suspend fun loadCandidates(): List<FaceIdentity> {
+    private suspend fun loadCandidates(): Flow<FaceIdentity> {
         matchState.value = MatchState.LOADING_CANDIDATES
         return faceDbManager.loadPeople(queryForCandidates)
     }
 
-    private suspend fun matchCandidates(candidates: List<FaceIdentity>): List<FaceMatchResult> {
+    private suspend fun matchCandidates(candidates: Flow<FaceIdentity>): Flow<FaceMatchResult> {
         matchState.value = MatchState.MATCHING
         return getMatchResultsForCandidates(candidates)
     }
 
-    private fun sendSortedResults(results: List<FaceMatchResult>) {
+    private suspend fun sendSortedResults(results: Flow<FaceMatchResult>) {
         matchState.value = MatchState.FINISHED
-        sortedResults.send(results.sortedByDescending { it.confidence })
+        sortedResults.send(results.toList().sortedByDescending { it.confidence })
     }
 
-    private suspend fun getMatchResultsForCandidates(candidates: List<FaceIdentity>) =
+    private suspend fun getMatchResultsForCandidates(candidates: Flow<FaceIdentity>) =
         candidates.map { candidate ->
             FaceMatchResult(
                 candidate.faceId,
