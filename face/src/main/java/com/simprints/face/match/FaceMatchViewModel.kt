2c25e5ace8e8830ae61848d3bdb09bf0e053033f
Tsuharesu <tsuharesu01@gmail.com>
2020-05-25 18:44:24 +0100

    Create view for matching
    Former-commit-id: 0fd277c26f3d45400b7cc86127dacf2e4eec6414 [formerly 8b87ad1dbd285393614ba5b02a21b9247bb6e884]
    Former-commit-id: 8a19e6f5109077cda9e053276aaa6a2fdc16e420

--- face/src/main/java/com/simprints/face/match/FaceMatchViewModel.kt
+++ face/src/main/java/com/simprints/face/match/FaceMatchViewModel.kt
@@ -16,6 +16,7 @@ import com.simprints.face.data.db.person.FaceSample
 import com.simprints.face.data.moduleapi.face.requests.FaceMatchRequest
 import com.simprints.face.data.moduleapi.face.responses.FaceMatchResponse
 import com.simprints.face.data.moduleapi.face.responses.entities.FaceMatchResult
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
@@ -30,17 +31,23 @@ class FaceMatchViewModel(
     private lateinit var probeFaceSamples: List<FaceSample>
     private lateinit var queryForCandidates: Serializable
 
-    val matchState: MutableLiveData<MatchState> = MutableLiveData()
+    val matchState: MutableLiveData<MatchState> = MutableLiveData(MatchState.NotStarted)
     val faceMatchResponse: MutableLiveData<LiveDataEventWithContent<FaceMatchResponse>> =
         MutableLiveData()
     private val returnCount = 10
+    private val veryGoodMatchThreshold = 50.0
+    private val goodMatchThreshold = 35.0
+    private val fairMatchThreshold = 20.0
+    private val matchingEndWaitTimeInMillis = 1000L
+
+    private var candidatesMatched = 0
 
     fun setupMatch(faceRequest: FaceMatchRequest) = viewModelScope.launch {
         probeFaceSamples = faceRequest.probeFaceSamples
         queryForCandidates = faceRequest.queryForCandidates
 
         if (masterFlowManager.getCurrentAction() == Action.ENROL) {
-            matchState.value = MatchState.ERROR
+            matchState.value = MatchState.Error
             return@launch
         }
 
@@ -51,22 +58,12 @@ class FaceMatchViewModel(
     }
 
     private suspend fun loadCandidates(): Flow<FaceIdentity> {
-        matchState.value = MatchState.LOADING_CANDIDATES
+        matchState.value = MatchState.LoadingCandidates
         return faceDbManager.loadPeople(queryForCandidates)
     }
 
     private suspend fun matchCandidates(candidates: Flow<FaceIdentity>): Flow<FaceMatchResult> {
-        matchState.value = MatchState.MATCHING
         return getConcurrentMatchResultsForCandidates(candidates)
-    }
-
-    private suspend fun getNSortedResult(results: Flow<FaceMatchResult>): List<FaceMatchResult> =
-        results.toList().sortedByDescending { it.confidence }.take(returnCount)
-
-    private fun sendFaceMatchResponse(results: List<FaceMatchResult>) {
-        matchState.value = MatchState.FINISHED
-        val response = FaceMatchResponse(results)
-        faceMatchResponse.send(response)
     }
 
     /**
@@ -74,13 +71,48 @@ class FaceMatchViewModel(
      */
     private suspend fun getConcurrentMatchResultsForCandidates(candidates: Flow<FaceIdentity>) =
         candidates.concurrentMap(dispatcherProvider.default()) { candidate ->
+            matchState.postValue(MatchState.Matching(++candidatesMatched))
             FaceMatchResult(
                 candidate.faceId,
                 faceMatcher.getHighestComparisonScoreForCandidate(probeFaceSamples, candidate)
             )
         }
 
-    enum class MatchState {
-        NOT_STARTED, LOADING_CANDIDATES, MATCHING, FINISHED, ERROR
+    private suspend fun getNSortedResult(results: Flow<FaceMatchResult>): List<FaceMatchResult> =
+        results.toList().sortedByDescending { it.confidence }.take(returnCount)
+
+    private suspend fun sendFaceMatchResponse(results: List<FaceMatchResult>) {
+        val veryGoodMatches = results.count { (_, score) -> veryGoodMatchThreshold <= score }
+        val goodMatches =
+            results.count { (_, score) -> goodMatchThreshold <= score && score < veryGoodMatchThreshold }
+        val fairMatches =
+            results.count { (_, score) -> fairMatchThreshold <= score && score < goodMatchThreshold }
+
+        matchState.value =
+            MatchState.Finished(
+                returnCount,
+                veryGoodMatches,
+                goodMatches,
+                fairMatches
+            )
+        val response = FaceMatchResponse(results)
+
+        // Wait a bit so the user can see the results
+        delay(matchingEndWaitTimeInMillis)
+        faceMatchResponse.send(response)
+    }
+
+    sealed class MatchState {
+        object NotStarted : MatchState()
+        object Error : MatchState()
+        object LoadingCandidates : MatchState()
+        data class Matching(val candidatesMatched: Int) : MatchState()
+        data class Finished(
+            val returnSize: Int,
+            val veryGoodMatches: Int,
+            val goodMatches: Int,
+            val fairMatches: Int
+        ) :
+            MatchState()
     }
 }
