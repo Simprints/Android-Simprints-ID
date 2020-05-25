7fcfa396925e0a486e6ff50035f9e8506e408957
Tsuharesu <tsuharesu01@gmail.com>
2020-05-25 15:48:50 +0100

    Return correct response to simprintsID from face orchestrator
    Former-commit-id: 4e5b4f11d699da4ff665f5cc7ba9b0a581020b4e [formerly 77628b537bbe86401c42ad38219f0c342bda52ed]
    Former-commit-id: a7c0de3c1cf2dce502f059011603eb050a2c732d

--- face/src/main/java/com/simprints/face/match/FaceMatchActivity.kt
+++ face/src/main/java/com/simprints/face/match/FaceMatchActivity.kt
@@ -1,13 +1,16 @@
 package com.simprints.face.match
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.simprints.core.livedata.LiveDataEventWithContentObserver
 import com.simprints.face.R
 import com.simprints.face.data.moduleapi.face.requests.FaceMatchRequest
 import com.simprints.face.exceptions.InvalidFaceRequestException
 import com.simprints.moduleapi.face.requests.IFaceRequest
+import com.simprints.moduleapi.face.responses.IFaceResponse
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class FaceMatchActivity : AppCompatActivity() {
@@ -20,7 +23,16 @@ class FaceMatchActivity : AppCompatActivity() {
         val faceRequest: FaceMatchRequest = this.intent.extras?.getParcelable(IFaceRequest.BUNDLE_KEY)
             ?: throw InvalidFaceRequestException("No IFaceRequest found for FaceMatchActivity")
 
+        observeViewModel()
         vm.setupMatch(faceRequest)
+    }
+
+    private fun observeViewModel() {
+        vm.sortedResults.observe(this, LiveDataEventWithContentObserver {
+            val intent = Intent().apply { putExtra(IFaceResponse.BUNDLE_KEY, it) }
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        })
     }
 
     override fun onBackPressed() {}
