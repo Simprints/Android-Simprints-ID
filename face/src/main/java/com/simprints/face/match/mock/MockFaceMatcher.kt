af72b79a220162b38dddea26904a55aedd2f5702
Tsuharesu <tsuharesu01@gmail.com>
2020-05-25 22:25:24 +0100

    Add a bit of randomness to mock detector and matcher
    Former-commit-id: afd1da8523e955a91434157a9303badd1c0799ac [formerly a410589bedfdb2845e7c2bd49305b000e4305f7a]
    Former-commit-id: 8b7caeaddc00c2b08eea6a39d356e28353660c3f

--- face/src/main/java/com/simprints/face/match/mock/MockFaceMatcher.kt
+++ face/src/main/java/com/simprints/face/match/mock/MockFaceMatcher.kt
@@ -1,12 +1,13 @@
 package com.simprints.face.match.mock
 
 import com.simprints.face.match.FaceMatcher
+import kotlin.random.Random
 
 class MockFaceMatcher : FaceMatcher() {
     /**
      * Returns a comparison score from 0.0 - 100.0
      */
     override suspend fun getComparisonScore(probe: ByteArray, matchAgainst: ByteArray): Float {
-        return 100f
+        return (Random.nextFloat() * 100)
     }
 }
