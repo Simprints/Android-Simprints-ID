8a363bb8bcd5bb96c4ceb0a0130230bbe0cb8745
Tsuharesu <tsuharesu01@gmail.com>
2020-05-25 18:44:24 +0100

    Create view for matching
    Former-commit-id: 0fd277c26f3d45400b7cc86127dacf2e4eec6414 [formerly 8b87ad1dbd285393614ba5b02a21b9247bb6e884]
    Former-commit-id: 8a19e6f5109077cda9e053276aaa6a2fdc16e420

--- face/src/main/java/com/simprints/face/match/FaceMatchActivity.kt
+++ face/src/main/java/com/simprints/face/match/FaceMatchActivity.kt
@@ -1,31 +1,62 @@
 package com.simprints.face.match
 
+import android.animation.ObjectAnimator
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import androidx.lifecycle.Observer
 import com.simprints.core.livedata.LiveDataEventWithContentObserver
 import com.simprints.face.R
+import com.simprints.face.controllers.core.androidResources.FaceAndroidResourcesHelper
 import com.simprints.face.data.moduleapi.face.requests.FaceMatchRequest
 import com.simprints.face.exceptions.InvalidFaceRequestException
+import com.simprints.face.match.FaceMatchViewModel.MatchState.*
 import com.simprints.moduleapi.face.requests.IFaceRequest
 import com.simprints.moduleapi.face.responses.IFaceResponse
+import kotlinx.android.synthetic.main.activity_face_match.*
+import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
 
-class FaceMatchActivity : AppCompatActivity() {
+@Suppress("WHEN_ENUM_CAN_BE_NULL_IN_JAVA")
+class FaceMatchActivity : AppCompatActivity(R.layout.activity_face_match) {
     private val vm: FaceMatchViewModel by viewModel()
+    private val androidResourcesHelper: FaceAndroidResourcesHelper by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_face_match)
+        setTextInLayout()
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
-        val faceRequest: FaceMatchRequest = this.intent.extras?.getParcelable(IFaceRequest.BUNDLE_KEY)
-            ?: throw InvalidFaceRequestException("No IFaceRequest found for FaceMatchActivity")
+        val faceRequest: FaceMatchRequest =
+            this.intent.extras?.getParcelable(IFaceRequest.BUNDLE_KEY)
+                ?: throw InvalidFaceRequestException("No IFaceRequest found for FaceMatchActivity")
 
         observeViewModel()
         vm.setupMatch(faceRequest)
     }
+
+    private fun setTextInLayout() {
+        face_match_please_wait.text =
+            androidResourcesHelper.getString(R.string.face_match_please_wait)
+    }
+
+    private fun setIdentificationProgress(progress: Int) =
+        runOnUiThread {
+            ObjectAnimator
+                .ofInt(
+                    face_match_progress,
+                    "progress",
+                    face_match_progress.progress,
+                    progress
+                )
+                .setDuration((progress * 10).toLong())
+                .start()
+        }
 
     private fun observeViewModel() {
         vm.faceMatchResponse.observe(this, LiveDataEventWithContentObserver {
@@ -33,6 +64,83 @@ class FaceMatchActivity : AppCompatActivity() {
             setResult(Activity.RESULT_OK, intent)
             finish()
         })
+        vm.matchState.observe(this, Observer { matchState ->
+            when (matchState) {
+                NotStarted -> renderNotStarted()
+                Error -> TODO()
+                LoadingCandidates -> renderLoadingCandidates()
+                is Matching -> renderMatching(matchState)
+                is Finished -> renderFinished(matchState)
+            }
+        })
+    }
+
+    private fun renderNotStarted() {
+        face_match_tv_matchingProgressStatus1.isGone = true
+        face_match_tv_matchingProgressStatus2.isGone = true
+        face_match_progress.isGone = true
+        face_match_tv_matchingResultStatus1.isGone = true
+        face_match_tv_matchingResultStatus2.isGone = true
+        face_match_tv_matchingResultStatus3.isGone = true
+    }
+
+    private fun renderLoadingCandidates() {
+        face_match_tv_matchingProgressStatus1.isVisible = true
+        face_match_tv_matchingProgressStatus1.text =
+            androidResourcesHelper.getString(R.string.face_match_loading_candidates)
+
+        face_match_progress.isVisible = true
+        setIdentificationProgress(25)
+    }
+
+    private fun renderMatching(matchState: Matching) {
+        face_match_tv_matchingProgressStatus1.text =
+            androidResourcesHelper.getString(R.string.face_match_matching_candidates)
+
+
+        face_match_tv_matchingProgressStatus2.isVisible = true
+        face_match_tv_matchingProgressStatus2.text = androidResourcesHelper.getStringPlural(
+            R.string.face_match_matched_candidates_quantity_key,
+            matchState.candidatesMatched,
+            arrayOf(matchState.candidatesMatched)
+        )
+
+        setIdentificationProgress(50)
+    }
+
+    private fun renderFinished(matchState: Finished) {
+        face_match_tv_matchingProgressStatus2.text = androidResourcesHelper.getStringPlural(
+            R.string.face_match_returned_results_quantity_key,
+            matchState.returnSize,
+            arrayOf(matchState.returnSize)
+        )
+
+        if (matchState.veryGoodMatches > 0) {
+            face_match_tv_matchingResultStatus1.isVisible = true
+            face_match_tv_matchingResultStatus1.text = androidResourcesHelper.getStringPlural(
+                R.string.face_match_tier1or2_matches_quantity_key,
+                matchState.veryGoodMatches,
+                arrayOf(matchState.veryGoodMatches)
+            )
+        }
+        if (matchState.goodMatches > 0) {
+            face_match_tv_matchingResultStatus2.isVisible = true
+            face_match_tv_matchingResultStatus2.text = androidResourcesHelper.getStringPlural(
+                R.string.face_match_tier3_matches_quantity_key,
+                matchState.goodMatches,
+                arrayOf(matchState.goodMatches)
+            )
+        }
+        if (matchState.veryGoodMatches < 1 && matchState.goodMatches < 1 || matchState.fairMatches > 1) {
+            face_match_tv_matchingResultStatus3.isVisible = true
+            face_match_tv_matchingResultStatus3.text = androidResourcesHelper.getStringPlural(
+                R.string.face_match_tier4_matches_quantity_key,
+                matchState.fairMatches,
+                arrayOf(matchState.fairMatches)
+            )
+        }
+
+        setIdentificationProgress(100)
     }
 
     override fun onBackPressed() {}
