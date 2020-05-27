58088fb8936d7e50b5f30af29fe34ab5ea0a9930
Tsuharesu <tsuharesu01@gmail.com>
2020-05-27 13:16:21 +0100

    Add RankOneFaceMatcher (but don't use it)
    Former-commit-id: b1029326db4fe5a68ace1685035e1aed10088380 [formerly 17e54cd27fb0800be91e2f7b52e60a748ac41956]
    Former-commit-id: 2cc6a05f1a7c4f7c0ffde608b0331f3764756c73

--- face/src/main/java/com/simprints/face/match/FaceMatcher.kt
+++ face/src/main/java/com/simprints/face/match/FaceMatcher.kt
@@ -4,6 +4,9 @@ import com.simprints.face.data.db.person.FaceIdentity
 import com.simprints.face.data.db.person.FaceSample
 
 abstract class FaceMatcher {
+    /**
+     * Returns a comparison score from 0.0 - 100.0
+     */
     abstract suspend fun getComparisonScore(probe: ByteArray, matchAgainst: ByteArray): Float
 
     suspend fun getHighestComparisonScoreForCandidate(probes: List<FaceSample>, candidate: FaceIdentity): Float {
--- face/src/main/java/com/simprints/face/match/mock/MockFaceMatcher.kt
+++ face/src/main/java/com/simprints/face/match/mock/MockFaceMatcher.kt
@@ -4,9 +4,6 @@ import com.simprints.face.match.FaceMatcher
 import kotlin.random.Random
 
 class MockFaceMatcher : FaceMatcher() {
-    /**
-     * Returns a comparison score from 0.0 - 100.0
-     */
     override suspend fun getComparisonScore(probe: ByteArray, matchAgainst: ByteArray): Float {
         return (Random.nextFloat() * 100)
     }
