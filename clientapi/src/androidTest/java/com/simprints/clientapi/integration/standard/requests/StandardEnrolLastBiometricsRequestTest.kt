daad6b61a079ab7c2d3da4511d30e5453e1629a4
Fabio <fabio@simprints.com>
2020-05-28 00:24:51 +0100

    Fixed client api android test
    Former-commit-id: 95d22410f2d7ccdb35ceeebe06da58e7e0f24800 [formerly 5f57139e52aff3fa9f7fe06ba4834dff4cac3c78]
    Former-commit-id: beeb5f1a1c57bd65795db5399ef71b716d2710e5

--- clientapi/src/androidTest/java/com/simprints/clientapi/integration/standard/requests/StandardEnrolLastBiometricsRequestTest.kt
+++ clientapi/src/androidTest/java/com/simprints/clientapi/integration/standard/requests/StandardEnrolLastBiometricsRequestTest.kt
@@ -9,25 +9,39 @@ import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtras
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.simprints.clientapi.activities.libsimprints.LibSimprintsActivity
+import com.simprints.clientapi.controllers.core.eventData.ClientApiSessionEventsManager
 import com.simprints.clientapi.integration.AppEnrolLastBiometricsRequest
 import com.simprints.clientapi.integration.key
 import com.simprints.clientapi.integration.standard.BaseStandardClientApiTest
 import com.simprints.clientapi.integration.value
 import com.simprints.moduleapi.app.requests.IAppRequest
+import io.mockk.coEvery
+import io.mockk.mockk
 import org.hamcrest.CoreMatchers.not
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.test.mock.declareModule
 
 
 @RunWith(AndroidJUnit4::class)
 class StandardEnrolLastBiometricsRequestTest : BaseStandardClientApiTest() {
+
+    private lateinit var clientApiSessionEventsManager: ClientApiSessionEventsManager
 
     @Before
     override fun setUp() {
         super.setUp()
         val intentResultOk = ActivityResult(Activity.RESULT_OK, null)
         intending(hasAction(APP_ENROL_LAST_BIOMETRICS_ACTION)).respondWith(intentResultOk)
+
+        clientApiSessionEventsManager = mockk(relaxed = true)
+        coEvery { clientApiSessionEventsManager.isCurrentSessionAnIdentification() } returns true
+        coEvery { clientApiSessionEventsManager.getCurrentSessionId() } returns sessionIdField.value()
+
+        declareModule {
+            factory { clientApiSessionEventsManager }
+        }
     }
 
     @Test
@@ -50,13 +64,19 @@ class StandardEnrolLastBiometricsRequestTest : BaseStandardClientApiTest() {
 
     @Test
     fun callingAppSendsASuspiciousEnrolRequest_shouldLaunchAnAppEnrolRequest() {
-        ActivityScenario.launch<LibSimprintsActivity>(makeIntentRequestSuspicious(standardBaseFlowIntentRequest).apply { action = STANDARD_ENROL_LAST_BIOMETRICS_ACTION })
+        ActivityScenario.launch<LibSimprintsActivity>(makeIntentRequestSuspicious(standardBaseFlowIntentRequest).apply {
+            action = STANDARD_ENROL_LAST_BIOMETRICS_ACTION
+            putExtra(sessionIdField.key(), sessionIdField.value())
+        })
         intended(hasAction(APP_ENROL_LAST_BIOMETRICS_ACTION))
     }
 
     @Test
     fun callingAppSendsAnInvalidEnrolRequest_shouldNotLaunchAnAppEnrolRequest() {
-        ActivityScenario.launch<LibSimprintsActivity>(makeIntentRequestInvalid(standardBaseFlowIntentRequest).apply { action = STANDARD_ENROL_LAST_BIOMETRICS_ACTION })
+        ActivityScenario.launch<LibSimprintsActivity>(makeIntentRequestInvalid(standardBaseFlowIntentRequest).apply {
+            action = STANDARD_ENROL_LAST_BIOMETRICS_ACTION
+            putExtra(sessionIdField.key(), sessionIdField.value())
+        })
         intended(not(hasAction(APP_ENROL_LAST_BIOMETRICS_ACTION)), times(2))
     }
 }
