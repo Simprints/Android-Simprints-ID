3c5bb5c6a39075c301b5aaeec95914d2819a2735
Ridwan Farouki <ridwan@simprints.com>
2020-05-31 22:55:09 +0100

    Fix ConnectScannerVM tests
    Former-commit-id: f3f8727b517a8cd18eedc52606af9147bbe41d3a [formerly f15f5ea2ba3ee755da8780e0e2c0438130b44a78]
    Former-commit-id: 4da225a417e766900ebed07dcbeff4bdea17c33c

--- fingerprint/src/test/java/com/simprints/fingerprint/activities/connect/ConnectScannerViewModelTest.kt
+++ fingerprint/src/test/java/com/simprints/fingerprint/activities/connect/ConnectScannerViewModelTest.kt
@@ -12,8 +12,10 @@ import com.simprints.fingerprint.controllers.core.preferencesManager.Fingerprint
 import com.simprints.fingerprint.controllers.core.timehelper.FingerprintTimeHelper
 import com.simprints.fingerprint.controllers.fingerprint.NfcManager
 import com.simprints.fingerprint.scanner.domain.ScannerGeneration
-import com.simprints.fingerprint.scanner.exceptions.safe.ScannerDisconnectedException
+import com.simprints.fingerprint.scanner.domain.ScannerGeneration.*
+import com.simprints.fingerprint.scanner.domain.versions.*
 import com.simprints.fingerprint.scanner.exceptions.safe.BluetoothNotSupportedException
+import com.simprints.fingerprint.scanner.exceptions.safe.ScannerDisconnectedException
 import com.simprints.fingerprint.scanner.factory.ScannerFactory
 import com.simprints.fingerprint.scanner.wrapper.ScannerWrapper
 import com.simprints.fingerprint.testtools.FullUnitTestConfigRule
@@ -66,19 +68,23 @@ class ConnectScannerViewModelTest : KoinTest {
         viewModel = get()
     }
 
-    private fun mockScannerWrapper(connectFailException: Throwable? = null) = mockk<ScannerWrapper> {
+    private fun mockScannerWrapper(scannerGeneration: ScannerGeneration, connectFailException: Throwable? = null) = mockk<ScannerWrapper> {
         every { disconnect() } returns Completable.complete()
         every { connect() } returns if (connectFailException == null) Completable.complete() else Completable.error(connectFailException)
         every { setup() } returns Completable.complete()
         every { sensorWakeUp() } returns Completable.complete()
         every { setUiIdle() } returns Completable.complete()
-        every { versionInformation() } returns mockk(relaxed = true)
+        every { versionInformation() } returns when (scannerGeneration) {
+            VERO_1 -> VERO_1_VERSION
+            VERO_2 -> VERO_2_VERSION
+        }
+        every { batteryInformation() } returns mockk(relaxed = true)
     }
 
     @Test
     fun start_bluetoothOff_sendsBluetoothOffIssueEvent() {
         setupBluetooth(isEnabled = false)
-        every { scannerFactory.create(any()) } returns mockScannerWrapper()
+        every { scannerFactory.create(any()) } returns mockScannerWrapper(VERO_2)
 
         val connectScannerIssueObserver = viewModel.connectScannerIssue.testObserver()
 
@@ -90,7 +96,7 @@ class ConnectScannerViewModelTest : KoinTest {
     @Test
     fun start_bluetoothNotSupported_sendsBluetoothNotSupportedAlert() {
         setupBluetooth(numberOfPairedScanners = 1)
-        every { scannerFactory.create(any()) } returns mockScannerWrapper(connectFailException = BluetoothNotSupportedException())
+        every { scannerFactory.create(any()) } returns mockScannerWrapper(VERO_2, connectFailException = BluetoothNotSupportedException())
 
         val launchAlertObserver = viewModel.launchAlert.testObserver()
 
@@ -100,9 +106,9 @@ class ConnectScannerViewModelTest : KoinTest {
     }
 
     @Test
-    fun start_scannerConnectSucceeds_sendsScannerConnectedEventAndProgressValuesAndLogsPropertiesAndSessionEvent() {
+    fun startVero1_scannerConnectSucceeds_sendsScannerConnectedEventAndProgressValuesAndLogsPropertiesAndSessionEvent() {
         setupBluetooth(numberOfPairedScanners = 1)
-        every { scannerFactory.create(any()) } returns mockScannerWrapper()
+        every { scannerFactory.create(any()) } returns mockScannerWrapper(VERO_1)
 
         val scannerConnectedObserver = viewModel.scannerConnected.testObserver()
         val scannerProgressObserver = viewModel.progress.testObserver()
@@ -112,15 +118,36 @@ class ConnectScannerViewModelTest : KoinTest {
         scannerConnectedObserver.assertEventReceivedWithContent(true)
         assertThat(scannerProgressObserver.observedValues.size).isEqualTo(ConnectScannerViewModel.NUMBER_OF_STEPS + 2) // 2 at the start
         verify { fingerprintAnalyticsManager.logScannerProperties(any(), any()) }
-        verify { sessionEventsManager.addEventInBackground(any()) }
-        verify { sessionEventsManager.updateHardwareVersionInScannerConnectivityEvent(any()) }
+        verify { preferencesManager.lastScannerUsed = any() }
+        verify { preferencesManager.lastScannerVersion = any() }
+        verify(exactly = 1) { sessionEventsManager.addEventInBackground(any()) }
+        verify(exactly = 1) { sessionEventsManager.updateHardwareVersionInScannerConnectivityEvent(any()) }
+    }
+
+    @Test
+    fun startVero2_scannerConnectSucceeds_sendsScannerConnectedEventAndProgressValuesAndLogsPropertiesAndSessionEvent() {
+        setupBluetooth(numberOfPairedScanners = 1)
+        every { scannerFactory.create(any()) } returns mockScannerWrapper(VERO_2)
+
+        val scannerConnectedObserver = viewModel.scannerConnected.testObserver()
+        val scannerProgressObserver = viewModel.progress.testObserver()
+
+        viewModel.start(ConnectScannerTaskRequest.ConnectMode.INITIAL_CONNECT)
+
+        scannerConnectedObserver.assertEventReceivedWithContent(true)
+        assertThat(scannerProgressObserver.observedValues.size).isEqualTo(ConnectScannerViewModel.NUMBER_OF_STEPS + 2) // 2 at the start
+        verify { fingerprintAnalyticsManager.logScannerProperties(any(), any()) }
+        verify { preferencesManager.lastScannerUsed = any() }
+        verify { preferencesManager.lastScannerVersion = any() }
+        verify(exactly = 2) { sessionEventsManager.addEventInBackground(any()) }    // The ScannerConnectionEvent + Vero2InfoSnapshotEvent
+        verify(exactly = 0) { sessionEventsManager.updateHardwareVersionInScannerConnectivityEvent(any()) }
     }
 
     @Test
     fun start_noScannersPairedWithVero2WithNfcAvailableAndOn_sendsNfcPairIssueEvent() {
         setupBluetooth(numberOfPairedScanners = 0)
         setupNfc(doesDeviceHaveNfcCapability = true, isEnabled = true)
-        every { preferencesManager.scannerGenerations } returns listOf(ScannerGeneration.VERO_2)
+        every { preferencesManager.scannerGenerations } returns listOf(VERO_2)
 
         val connectScannerIssueObserver = viewModel.connectScannerIssue.testObserver()
 
@@ -133,7 +160,7 @@ class ConnectScannerViewModelTest : KoinTest {
     fun start_noScannersPairedWithVero2WithNfcAvailableAndOff_sendsNfcOffIssueEvent() {
         setupBluetooth(numberOfPairedScanners = 0)
         setupNfc(doesDeviceHaveNfcCapability = true, isEnabled = false)
-        every { preferencesManager.scannerGenerations } returns listOf(ScannerGeneration.VERO_2)
+        every { preferencesManager.scannerGenerations } returns listOf(VERO_2)
 
         val connectScannerIssueObserver = viewModel.connectScannerIssue.testObserver()
 
@@ -146,7 +173,7 @@ class ConnectScannerViewModelTest : KoinTest {
     fun start_noScannersPairedWithVero2WithNfcNotAvailable_sendsSerialEntryIssueEvent() {
         setupBluetooth(numberOfPairedScanners = 0)
         setupNfc(doesDeviceHaveNfcCapability = false)
-        every { preferencesManager.scannerGenerations } returns listOf(ScannerGeneration.VERO_2)
+        every { preferencesManager.scannerGenerations } returns listOf(VERO_2)
 
         val connectScannerIssueObserver = viewModel.connectScannerIssue.testObserver()
 
@@ -159,7 +186,7 @@ class ConnectScannerViewModelTest : KoinTest {
     fun start_noScannersPairedWithVero1WithNfcAvailableAndOn_sendsSerialEntryIssueEvent() {
         setupBluetooth(numberOfPairedScanners = 0)
         setupNfc(doesDeviceHaveNfcCapability = true, isEnabled = true)
-        every { preferencesManager.scannerGenerations } returns listOf(ScannerGeneration.VERO_1)
+        every { preferencesManager.scannerGenerations } returns listOf(VERO_1)
 
         val connectScannerIssueObserver = viewModel.connectScannerIssue.testObserver()
 
@@ -172,7 +199,7 @@ class ConnectScannerViewModelTest : KoinTest {
     fun start_noScannersPairedWithMixedVeroGenerationsWithNfcAvailableAndOn_sendsSerialEntryIssueEvent() {
         setupBluetooth(numberOfPairedScanners = 0)
         setupNfc(doesDeviceHaveNfcCapability = true, isEnabled = true)
-        every { preferencesManager.scannerGenerations } returns listOf(ScannerGeneration.VERO_1, ScannerGeneration.VERO_2)
+        every { preferencesManager.scannerGenerations } returns listOf(VERO_1, VERO_2)
 
         val connectScannerIssueObserver = viewModel.connectScannerIssue.testObserver()
 
@@ -185,7 +212,7 @@ class ConnectScannerViewModelTest : KoinTest {
     fun start_multipleScannersPairedWithVero2WithNfcAvailableAndOn_sendsNfcPairIssueEvent() {
         setupBluetooth(numberOfPairedScanners = 2)
         setupNfc(doesDeviceHaveNfcCapability = true, isEnabled = true)
-        every { preferencesManager.scannerGenerations } returns listOf(ScannerGeneration.VERO_2)
+        every { preferencesManager.scannerGenerations } returns listOf(VERO_2)
 
         val connectScannerIssueObserver = viewModel.connectScannerIssue.testObserver()
 
@@ -197,7 +224,7 @@ class ConnectScannerViewModelTest : KoinTest {
     @Test
     fun start_scannerConnectFailsWithDisconnectedException_sendsScannerConnectedFailedEvent() {
         setupBluetooth(numberOfPairedScanners = 1)
-        every { scannerFactory.create(any()) } returns mockScannerWrapper(ScannerDisconnectedException())
+        every { scannerFactory.create(any()) } returns mockScannerWrapper(VERO_2, ScannerDisconnectedException())
 
         val scannerConnectedObserver = viewModel.scannerConnected.testObserver()
 
@@ -210,7 +237,7 @@ class ConnectScannerViewModelTest : KoinTest {
     fun start_scannerConnectFailsWithUnexpectedException_sendsAlertEventAndLogsCrashlytics() {
         val error = Error("Oops")
         setupBluetooth(numberOfPairedScanners = 1)
-        every { scannerFactory.create(any()) } returns mockScannerWrapper(error)
+        every { scannerFactory.create(any()) } returns mockScannerWrapper(VERO_2, error)
 
         val scannerConnectedObserver = viewModel.scannerConnected.testObserver()
         val launchAlertObserver = viewModel.launchAlert.testObserver()
@@ -234,7 +261,7 @@ class ConnectScannerViewModelTest : KoinTest {
     @Test
     fun handleScannerDisconnectedNoClick_vero2WithNfcAvailableAndOn_sendsNfcPairIssueEvent() {
         setupNfc(doesDeviceHaveNfcCapability = true, isEnabled = true)
-        every { preferencesManager.scannerGenerations } returns listOf(ScannerGeneration.VERO_2)
+        every { preferencesManager.scannerGenerations } returns listOf(VERO_2)
 
         val connectScannerIssueObserver = viewModel.connectScannerIssue.testObserver()
 
@@ -246,7 +273,7 @@ class ConnectScannerViewModelTest : KoinTest {
     @Test
     fun handleIncorrectScanner_vero2WithNfcAvailableAndOn_sendsNfcPairIssueEvent() {
         setupNfc(doesDeviceHaveNfcCapability = true, isEnabled = true)
-        every { preferencesManager.scannerGenerations } returns listOf(ScannerGeneration.VERO_2)
+        every { preferencesManager.scannerGenerations } returns listOf(VERO_2)
 
         val connectScannerIssueObserver = viewModel.connectScannerIssue.testObserver()
 
@@ -276,5 +303,23 @@ class ConnectScannerViewModelTest : KoinTest {
         if (isEnabled != null) {
             every { nfcManager.isNfcEnabled() } returns isEnabled
         }
+    }
+
+    companion object {
+        val VERO_1_VERSION = ScannerVersion(
+            VERO_1,
+            ScannerFirmwareVersions(
+                cypress = ChipFirmwareVersion.UNKNOWN,
+                stm = ChipFirmwareVersion(6, 0),
+                un20 = ChipFirmwareVersion(1, 0)
+            ),
+            ScannerApiVersions.UNKNOWN
+        )
+
+        val VERO_2_VERSION = ScannerVersion(
+            VERO_2,
+            ScannerFirmwareVersions(ChipFirmwareVersion(1, 2), ChipFirmwareVersion(3, 4), ChipFirmwareVersion(5, 6)),
+            ScannerApiVersions(ChipApiVersion(7, 8), ChipApiVersion(9, 10), ChipApiVersion(11, 12))
+        )
     }
 }
