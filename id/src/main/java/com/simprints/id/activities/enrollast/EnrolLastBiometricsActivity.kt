f2e9d04b753093ca6c37c5aecdf57fb4e5a1f141
Fabio <fabio@simprints.com>
2020-06-01 10:35:57 +0100

    Better string names + Toast message
    Former-commit-id: 97b0367d0585bcd814d2323eb8d241af90afd13e [formerly 048fe2fb450f09867b193312404a1cecb6a81f5d]
    Former-commit-id: 44510d57c412c556ff6ca413bf1ee5e66294a10a

--- id/src/main/java/com/simprints/id/activities/enrollast/EnrolLastBiometricsActivity.kt
+++ id/src/main/java/com/simprints/id/activities/enrollast/EnrolLastBiometricsActivity.kt
@@ -3,6 +3,7 @@ package com.simprints.id.activities.enrollast
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -17,6 +18,7 @@ import com.simprints.id.orchestrator.steps.core.requests.EnrolLastBiometricsRequ
 import com.simprints.id.orchestrator.steps.core.response.CoreResponse
 import com.simprints.id.orchestrator.steps.core.response.CoreResponse.Companion.CORE_STEP_BUNDLE
 import com.simprints.id.orchestrator.steps.core.response.EnrolLastBiometricsResponse
+import com.simprints.id.tools.AndroidResourcesHelper
 import com.simprints.id.tools.TimeHelper
 import timber.log.Timber
 import javax.inject.Inject
@@ -31,6 +33,9 @@ class EnrolLastBiometricsActivity : AppCompatActivity() {
 
     @Inject
     lateinit var viewModelFactory: EnrolLastBiometricsViewModelFactory
+
+    @Inject
+    lateinit var androidResourcesHelper: AndroidResourcesHelper
 
     private lateinit var enrolLastBiometricsRequest: EnrolLastBiometricsRequest
 
@@ -60,6 +65,7 @@ class EnrolLastBiometricsActivity : AppCompatActivity() {
     private fun observeViewState() {
         vm.getViewStateLiveData().observe(this, Observer {
             if (it is ViewState.Success) {
+                Toast.makeText(this, androidResourcesHelper.getString(R.string.enrol_last_biometrics_success), Toast.LENGTH_LONG).show()
                 sendOkResult(it.newGuid)
             } else {
                 AlertActivityHelper.launchAlert(this@EnrolLastBiometricsActivity, AlertType.ENROLMENT_LAST_BIOMETRICS_FAILED)
