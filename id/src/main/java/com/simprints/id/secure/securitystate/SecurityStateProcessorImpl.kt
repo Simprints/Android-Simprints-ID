38d88b0630f20d45ba1a62bef2426ee5978a9087
Alan Camargo <alan@simprints.com>
2020-06-04 11:25:18 +0100

    Create SecurityStateProcessor skeleton
    Former-commit-id: 55b3a7efd247f3de320fde0f961db2cfeb576981 [formerly ad4193a16deadd1891fd2d63475b15f646d9369b]
    Former-commit-id: 5497b0aa7379b66fdb10f83cb021dcbdccb0aac1

---
+++ id/src/main/java/com/simprints/id/secure/securitystate/SecurityStateProcessorImpl.kt
@@ -0,0 +1,11 @@
+package com.simprints.id.secure.securitystate
+
+import com.simprints.id.secure.models.SecurityState
+
+class SecurityStateProcessorImpl : SecurityStateProcessor {
+
+    override suspend fun processSecurityState(securityState: SecurityState) {
+        // TODO
+    }
+
+}
