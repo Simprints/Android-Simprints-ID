96f808be3702805bbf3d53059219b63109436e78
Alan Camargo <alan@simprints.com>
2020-06-05 14:10:54 +0100

    Implement scheduler
    Former-commit-id: c97e499c2402219be37a7cdcfe3254cc24e84d63 [formerly ceec736773e9f62db6f952d44fd60d8c9e0007b3]
    Former-commit-id: 00880d1c24f9b039027ae1f98142ba7a7b602000

---
+++ id/src/main/java/com/simprints/id/services/securitystate/SecurityStateSchedulerImpl.kt
@@ -0,0 +1,42 @@
+package com.simprints.id.services.securitystate
+
+import android.content.Context
+import androidx.work.*
+import java.util.concurrent.TimeUnit
+
+class SecurityStateSchedulerImpl(context: Context) : SecurityStateScheduler {
+
+    private val workManager = WorkManager.getInstance(context)
+
+    override fun scheduleSecurityStateCheck() {
+        workManager.enqueueUniquePeriodicWork(
+            WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            buildWork()
+        )
+    }
+
+    override fun cancelSecurityStateCheck() {
+        workManager.cancelUniqueWork(WORK_NAME)
+    }
+
+    private fun buildWork(): PeriodicWorkRequest {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        return PeriodicWorkRequestBuilder<SecurityStateWorker>(REPEAT_INTERVAL, TimeUnit.MINUTES)
+            .setConstraints(constraints)
+            .setBackoffCriteria(
+                BackoffPolicy.EXPONENTIAL,
+                PeriodicWorkRequest.MIN_BACKOFF_MILLIS,
+                TimeUnit.MILLISECONDS
+            ).build()
+    }
+
+    private companion object {
+        const val WORK_NAME = "security-status-check-work"
+        const val REPEAT_INTERVAL = 30L
+    }
+
+}
