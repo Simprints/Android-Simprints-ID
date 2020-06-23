63c36ed1d1235e72f7662296b3d7ad50a82eac02
Fabio <fabio@simprints.com>
2020-06-24 00:59:54 +0100

    Changed domain classes to use Event Payload
    Former-commit-id: d0f509f4deaa88d973589485ccf7548bb8035975 [formerly 9e9b3ab700a9e15cd1a7b61c4bcd0c7538b5fd4c]
    Former-commit-id: c02a91b3597a1ed4787c26a6345563155067d0ba

--- id/src/main/java/com/simprints/id/data/db/session/domain/models/events/Events.kt
+++ id/src/main/java/com/simprints/id/data/db/session/domain/models/events/Events.kt
@@ -1,8 +1,6 @@
-package com.simprints.id.data.db.subject.domain.subjectevents
+package com.simprints.id.data.db.session.domain.models.events
 
 import androidx.annotation.Keep
-import com.simprints.id.data.db.session.domain.models.events.EventPayload
-import com.simprints.id.data.db.session.domain.models.events.fromApiToDomainOrNullIfNoBiometricReferences
 import com.simprints.id.data.db.subject.remote.models.subjectevents.ApiEvent
 import com.simprints.id.domain.modality.Modes
 
@@ -10,25 +8,47 @@ import com.simprints.id.domain.modality.Modes
 data class Events(val events: List<Event>)
 
 @Keep
-data class Event(val id: String,
-                 val projectId: List<String>,
-                 val subjectId: List<String>,
-                 val attendantId: List<String>,
-                 val moduleId: List<String>,
-                 val mode: List<Modes>,
-                 val payload: EventPayload) {
+open class Event(val id: String,
+                 val labels: List<EventLabel>,
+                 val payload: EventPayload)
+
+sealed class EventLabel(val key: String) {
+    class ProjectId(labels: String) : EventLabel("projectId")
+    class SubjectId(labels: String) : EventLabel("subjectId")
+    class AttendantId(labels: String) : EventLabel("attendantId")
+    class ModuleId(labels: List<String>) : EventLabel("moduleId")
+    class Mode(labels: List<Modes>) : EventLabel("mode")
+    class SessionId(labels: String) : EventLabel("sessionId")
+
+    companion object {
+        fun fromApiToDomain(labels: Map<String, List<String>>) =
+            labels.mapNotNull {
+                when (it.key) {
+                    ApiEvent.PROJECT_ID_LABEL -> {
+                        ProjectId(it.value.first())
+                    }
+                    ApiEvent.SUBJECT_ID_LABEL -> {
+                        SubjectId(it.value.first())
+                    }
+                    ApiEvent.ATTENDANT_ID_LABEL -> {
+                        AttendantId(it.value.first())
+                    }
+                    ApiEvent.MODULE_ID_LABEL -> {
+                        ModuleId(it.value)
+                    }
+                    ApiEvent.MODE_LABEL -> {
+                        Mode(it.value.map { mode -> Modes.valueOf(mode) })
+                    }
+                    else -> {
+                        null
+                    }
+                }
+            }
+    }
 }
 
 fun ApiEvent.fromApiToDomainOrNullIfNoBiometricReferences() = try {
-    payload.fromApiToDomainOrNullIfNoBiometricReferences()?.let {
-        Event(id,
-            labels.getValue(ApiEvent.PROJECT_ID_LABEL),
-            labels.getValue(ApiEvent.SUBJECT_ID_LABEL),
-            labels.getValue(ApiEvent.ATTENDANT_ID_LABEL),
-            labels.getValue(ApiEvent.MODULE_ID_LABEL),
-            labels.getValue(ApiEvent.MODE_LABEL).map { Modes.valueOf(it) },
-            it)
-    }
+    Event(id, EventLabel.fromApiToDomain(labels), payload.fromApiToDomainOrNullIfNoBiometricReferences())
 } catch (t: Throwable) {
     throw IllegalStateException("Did not get all the labels from cloud")
 }
