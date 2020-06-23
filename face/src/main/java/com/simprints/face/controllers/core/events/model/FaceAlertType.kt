debb2a2de7e050fa45fd472d5e2bd8f94f7cf95e
Tsuharesu <tsuharesu01@gmail.com>
2020-06-23 15:08:19 +0100

    Send alert events
    Former-commit-id: 30b4f4cb2b29ff1501dfb6f4c4a2eb8d684eef62 [formerly e35687321711a1b07159601520ad48a207271d49]
    Former-commit-id: 4020dcc4dd436c8b241df779857ab32f06fb1e20

--- face/src/main/java/com/simprints/face/controllers/core/events/model/FaceAlertType.kt
+++ face/src/main/java/com/simprints/face/controllers/core/events/model/FaceAlertType.kt
@@ -1,5 +1,6 @@
 package com.simprints.face.controllers.core.events.model
 
+import com.simprints.face.error.ErrorType
 import com.simprints.id.data.db.session.domain.models.events.AlertScreenEvent.AlertScreenEventType
 import com.simprints.id.data.db.session.domain.models.events.AlertScreenEvent.AlertScreenEventType.UNEXPECTED_ERROR
 
@@ -11,5 +12,12 @@ enum class FaceAlertType {
     fun fromDomainToCore(): AlertScreenEventType = when (this) {
         FACE_INVALID_LICENSE -> UNEXPECTED_ERROR
         FACE_MISSING_LICENSE -> UNEXPECTED_ERROR
+    }
+
+    companion object {
+        fun fromErrorType(errorType: ErrorType): FaceAlertType = when (errorType) {
+            ErrorType.LicenseMissing -> FACE_MISSING_LICENSE
+            ErrorType.LicenseInvalid -> FACE_INVALID_LICENSE
+        }
     }
 }
