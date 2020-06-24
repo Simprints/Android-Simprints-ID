e1dc447a5775dd81fead79025c61bc17021e5b4c
Tsuharesu <tsuharesu01@gmail.com>
2020-06-24 21:57:58 +0100

    Improve the matcher tests to check events
    Former-commit-id: d217499f9f2684c00ff6984513654d7fa05541eb [formerly 21639b546be56b5cf23490ebfa40539da4343a3b]
    Former-commit-id: 51a681c41cdfbb94fdf382b900142a6238eb1442

--- face/src/main/java/com/simprints/face/controllers/core/events/model/MatchEntry.kt
+++ face/src/main/java/com/simprints/face/controllers/core/events/model/MatchEntry.kt
@@ -4,7 +4,7 @@ import androidx.annotation.Keep
 import com.simprints.id.data.db.session.domain.models.events.MatchEntry as MatchEntryCore
 
 @Keep
-class MatchEntry(val candidateId: String, val score: Float) {
+data class MatchEntry(val candidateId: String, val score: Float) {
     fun fromDomainToCore() = MatchEntryCore(candidateId, score)
 }
 
