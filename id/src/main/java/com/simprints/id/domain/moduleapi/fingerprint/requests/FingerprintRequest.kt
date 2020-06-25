19cbb912b23a84db9b35dfed2ffab253c107f144
Alan Camargo <alan@simprints.com>
2020-06-25 12:07:51 +0100

    Create fingerprint request
    Former-commit-id: a9d4277be4c1fa6e9d192e84f8845aab7c947258 [formerly 18cfeaab1d38118ed56b745d20367c03aafaa291]
    Former-commit-id: 8265a79794963caa04d84b18a87e10ca6ea2f15e

--- id/src/main/java/com/simprints/id/domain/moduleapi/fingerprint/requests/FingerprintRequest.kt
+++ id/src/main/java/com/simprints/id/domain/moduleapi/fingerprint/requests/FingerprintRequest.kt
@@ -2,18 +2,18 @@ package com.simprints.id.domain.moduleapi.fingerprint.requests
 
 import com.simprints.id.orchestrator.steps.Step.Request
 
-interface FingerprintRequest: Request {
+interface FingerprintRequest : Request {
     val type: FingerprintRequestType
 }
 
 enum class FingerprintRequestType {
     CAPTURE,
-    MATCH
+    MATCH,
+    CONFIGURATION
 }
 
-
-fun FingerprintRequest.fromDomainToModuleApi() =
-    when (this.type) {
-        FingerprintRequestType.CAPTURE -> (this as FingerprintCaptureRequest).fromDomainToModuleApi()
-        FingerprintRequestType.MATCH -> (this as FingerprintMatchRequest).fromDomainToModuleApi()
-    }
+fun FingerprintRequest.fromDomainToModuleApi() = when (this.type) {
+    FingerprintRequestType.CAPTURE -> (this as FingerprintCaptureRequest).fromDomainToModuleApi()
+    FingerprintRequestType.MATCH -> (this as FingerprintMatchRequest).fromDomainToModuleApi()
+    FingerprintRequestType.CONFIGURATION -> (this as FingerprintConfigurationRequest).fromDomainToModuleApi()
+}
