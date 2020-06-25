c8323cb370a558e6f852d779aab6d1fac194f64c
Alan Camargo <alan@simprints.com>
2020-06-25 12:13:53 +0100

    Create face request
    Former-commit-id: 5edec0b3f7ceac77455c98edfd5df38f69d6e76c [formerly 6c634f6039a3cc97ce3449199d7759d4b2eff9dd]
    Former-commit-id: ba20e6c8570489c15abd79f4390e884d6015aaaa

--- id/src/main/java/com/simprints/id/domain/moduleapi/face/FaceRequestFactoryImpl.kt
+++ id/src/main/java/com/simprints/id/domain/moduleapi/face/FaceRequestFactoryImpl.kt
@@ -1,16 +1,22 @@
 package com.simprints.id.domain.moduleapi.face
 
 import com.simprints.id.domain.moduleapi.face.requests.FaceCaptureRequest
+import com.simprints.id.domain.moduleapi.face.requests.FaceConfigurationRequest
 import com.simprints.id.domain.moduleapi.face.requests.FaceMatchRequest
 import com.simprints.id.domain.moduleapi.face.responses.entities.FaceCaptureSample
 import java.io.Serializable
 
 class FaceRequestFactoryImpl: FaceRequestFactory {
 
-    override fun buildCaptureRequest(nFaceSamplesToCapture: Int) = FaceCaptureRequest(nFaceSamplesToCapture = nFaceSamplesToCapture)
+    override fun buildCaptureRequest(
+        nFaceSamplesToCapture: Int
+    ) = FaceCaptureRequest(nFaceSamplesToCapture = nFaceSamplesToCapture)
 
     override fun buildFaceMatchRequest(
         probeFaceSamples: List<FaceCaptureSample>,
         queryForCandidates: Serializable
     ): FaceMatchRequest = FaceMatchRequest(probeFaceSamples, queryForCandidates)
+
+    override fun buildFaceConfigurationRequest() = FaceConfigurationRequest()
+
 }
