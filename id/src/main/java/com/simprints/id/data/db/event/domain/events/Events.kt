01cef2f8d4c8092fa20cfe918318fa93a9a2af1f
Fabio <fabio@simprints.com>
2020-06-29 17:33:35 +0100

    Added ApiEvent
    Former-commit-id: 1edf62e8bfce5b61bd283732a4f478e9199847fa [formerly 4d6af370efc064a40bdf666a8a7cd4218c7d72d0]
    Former-commit-id: 2e133e6ad25165fd943a3120f97551b87dea54cf

--- id/src/main/java/com/simprints/id/data/db/event/domain/events/Events.kt
+++ id/src/main/java/com/simprints/id/data/db/event/domain/events/Events.kt
@@ -12,37 +12,12 @@ open class Event(val id: String,
                  val labels: List<EventLabel>,
                  val payload: EventPayload)
 
-sealed class EventLabel(val key: String) {
-    class ProjectId(labels: String) : EventLabel("projectId")
-    class SubjectId(labels: String) : EventLabel("subjectId")
-    class AttendantId(labels: String) : EventLabel("attendantId")
-    class ModuleId(labels: List<String>) : EventLabel("moduleId")
-    class Mode(labels: List<Modes>) : EventLabel("mode")
-    class SessionId(labels: String) : EventLabel("sessionId")
+sealed class EventLabel(val key: String, val labels: List<String>) {
 
-    companion object {
-        fun fromApiToDomain(labels: Map<String, List<String>>) =
-            labels.mapNotNull {
-                when (it.key) {
-                    ApiEventNew.PROJECT_ID_LABEL -> {
-                        ProjectId(it.value.first())
-                    }
-                    ApiEventNew.SUBJECT_ID_LABEL -> {
-                        SubjectId(it.value.first())
-                    }
-                    ApiEventNew.ATTENDANT_ID_LABEL -> {
-                        AttendantId(it.value.first())
-                    }
-                    ApiEventNew.MODULE_ID_LABEL -> {
-                        ModuleId(it.value)
-                    }
-                    ApiEventNew.MODE_LABEL -> {
-                        Mode(it.value.map { mode -> Modes.valueOf(mode) })
-                    }
-                    else -> {
-                        null
-                    }
-                }
-            }
-    }
+    class ProjectId(label: String) : EventLabel("projectId", listOf(label))
+    class SubjectId(label: String) : EventLabel("subjectId", listOf(label))
+    class AttendantId(label: String) : EventLabel("attendantId", listOf(label))
+    class ModuleId(labels: List<String>) : EventLabel("moduleId", labels)
+    class Mode(labels: List<Modes>) : EventLabel("mode", labels.map { it.name })
+    class SessionId(label: String) : EventLabel("sessionId", listOf(label))
 }
--- id/src/main/java/com/simprints/id/data/db/event/remote/events/ApiAlertScreenEvent.kt
+++ id/src/main/java/com/simprints/id/data/db/event/remote/events/ApiAlertScreenEvent.kt
@@ -7,84 +7,92 @@ import com.simprints.id.data.db.event.domain.events.AlertScreenEvent.AlertScreen
 import com.simprints.id.data.db.event.remote.events.ApiAlertScreenEvent.ApiAlertScreenEvent.Companion.fromDomainToApi
 
 @Keep
-class ApiAlertScreenEvent(val relativeStartTime: Long,
-                          val alertType: ApiAlertScreenEvent) : ApiEvent(ApiEventType.ALERT_SCREEN) {
+class ApiAlertScreenEvent(alertScreenEventDomain: AlertScreenEvent)
+    : ApiEvent(alertScreenEventDomain.id,
+    alertScreenEventDomain.labels.fromDomainToApi(),
+    alertScreenEventDomain.payload.fromDomainToApi()) {
 
-    constructor(alertScreenEventDomain: AlertScreenEvent) :
-        this((alertScreenEventDomain.payload as AlertScreenPayload).startTime, fromDomainToApi(alertScreenEventDomain.payload.alertType))
 
     @Keep
-    enum class ApiAlertScreenEvent {
-        DIFFERENT_PROJECT_ID,
-        DIFFERENT_USER_ID,
-        GUID_NOT_FOUND_ONLINE,
-        GUID_NOT_FOUND_OFFLINE,
-        BLUETOOTH_NOT_SUPPORTED,
-        LOW_BATTERY,
+    class ApiAlertScreenPayload(val relativeStartTime: Long,
+                                val alertType: ApiAlertScreenEvent) : ApiEvent(ApiEventType.ALERT_SCREEN) {
 
-        @Deprecated("Fingerprint module doesn't triggers it anymore")
-        UNKNOWN_BLUETOOTH_ISSUE,
-        UNEXPECTED_ERROR,
-        DISCONNECTED,
+        constructor(alertScreenEventDomain: AlertScreenEvent) :
+            this((alertScreenEventDomain.payload as AlertScreenPayload).startTime, fromDomainToApi(alertScreenEventDomain.payload.alertType))
 
-        @Deprecated("Fingerprint module doesn't triggers it anymore")
-        MULTIPLE_PAIRED_SCANNERS,
+        @Keep
+        enum class ApiAlertScreenEvent {
+            DIFFERENT_PROJECT_ID,
+            DIFFERENT_USER_ID,
+            GUID_NOT_FOUND_ONLINE,
+            GUID_NOT_FOUND_OFFLINE,
+            BLUETOOTH_NOT_SUPPORTED,
+            LOW_BATTERY,
 
-        @Deprecated("Fingerprint module doesn't triggers it anymore")
-        NOT_PAIRED,
-        BLUETOOTH_NOT_ENABLED,
-        NFC_NOT_ENABLED,
-        NFC_PAIR,
-        SERIAL_ENTRY_PAIR,
-        OTA,
-        OTA_RECOVERY,
-        OTA_FAILED,
+            @Deprecated("Fingerprint module doesn't triggers it anymore")
+            UNKNOWN_BLUETOOTH_ISSUE,
+            UNEXPECTED_ERROR,
+            DISCONNECTED,
 
-        @Deprecated("That can never been triggered, so to be removed soon")
-        INVALID_INTENT_ACTION,
-        ENROLMENT_LAST_BIOMETRICS_FAILED,
-        INVALID_STATE_FOR_INTENT_ACTION,
-        INVALID_METADATA,
-        INVALID_MODULE_ID,
-        INVALID_PROJECT_ID,
-        INVALID_SELECTED_ID,
-        INVALID_SESSION_ID,
-        INVALID_USER_ID,
-        INVALID_VERIFY_ID,
-        SAFETYNET_ERROR;
+            @Deprecated("Fingerprint module doesn't triggers it anymore")
+            MULTIPLE_PAIRED_SCANNERS,
 
-        companion object {
-            fun fromDomainToApi(type: AlertScreenEventType): ApiAlertScreenEvent =
-                when (type) {
-                    AlertScreenEventType.DIFFERENT_PROJECT_ID -> DIFFERENT_PROJECT_ID
-                    AlertScreenEventType.DIFFERENT_USER_ID -> DIFFERENT_USER_ID
-                    AlertScreenEventType.GUID_NOT_FOUND_ONLINE -> GUID_NOT_FOUND_ONLINE
-                    AlertScreenEventType.GUID_NOT_FOUND_OFFLINE -> GUID_NOT_FOUND_OFFLINE
-                    AlertScreenEventType.BLUETOOTH_NOT_SUPPORTED -> BLUETOOTH_NOT_SUPPORTED
-                    AlertScreenEventType.LOW_BATTERY -> LOW_BATTERY
-                    AlertScreenEventType.UNEXPECTED_ERROR -> UNEXPECTED_ERROR
-                    AlertScreenEventType.DISCONNECTED -> DISCONNECTED
-                    AlertScreenEventType.MULTIPLE_PAIRED_SCANNERS -> MULTIPLE_PAIRED_SCANNERS
-                    AlertScreenEventType.NOT_PAIRED -> NOT_PAIRED
-                    AlertScreenEventType.BLUETOOTH_NOT_ENABLED -> BLUETOOTH_NOT_ENABLED
-                    AlertScreenEventType.NFC_NOT_ENABLED -> NFC_NOT_ENABLED
-                    AlertScreenEventType.NFC_PAIR -> NFC_PAIR
-                    AlertScreenEventType.SERIAL_ENTRY_PAIR -> SERIAL_ENTRY_PAIR
-                    AlertScreenEventType.OTA -> OTA
-                    AlertScreenEventType.OTA_RECOVERY -> OTA_RECOVERY
-                    AlertScreenEventType.OTA_FAILED -> OTA_FAILED
-                    AlertScreenEventType.INVALID_INTENT_ACTION -> INVALID_INTENT_ACTION
-                    AlertScreenEventType.INVALID_METADATA -> INVALID_METADATA
-                    AlertScreenEventType.INVALID_MODULE_ID -> INVALID_MODULE_ID
-                    AlertScreenEventType.INVALID_PROJECT_ID -> INVALID_PROJECT_ID
-                    AlertScreenEventType.INVALID_SELECTED_ID -> INVALID_SELECTED_ID
-                    AlertScreenEventType.INVALID_SESSION_ID -> INVALID_SESSION_ID
-                    AlertScreenEventType.INVALID_USER_ID -> INVALID_USER_ID
-                    AlertScreenEventType.INVALID_VERIFY_ID -> INVALID_VERIFY_ID
-                    AlertScreenEventType.SAFETYNET_ERROR -> SAFETYNET_ERROR
-                    AlertScreenEventType.ENROLMENT_LAST_BIOMETRICS_FAILED -> ENROLMENT_LAST_BIOMETRICS_FAILED
-                    AlertScreenEventType.INVALID_STATE_FOR_INTENT_ACTION -> INVALID_STATE_FOR_INTENT_ACTION
-                }
+            @Deprecated("Fingerprint module doesn't triggers it anymore")
+            NOT_PAIRED,
+            BLUETOOTH_NOT_ENABLED,
+            NFC_NOT_ENABLED,
+            NFC_PAIR,
+            SERIAL_ENTRY_PAIR,
+            OTA,
+            OTA_RECOVERY,
+            OTA_FAILED,
+
+            @Deprecated("That can never been triggered, so to be removed soon")
+            INVALID_INTENT_ACTION,
+            ENROLMENT_LAST_BIOMETRICS_FAILED,
+            INVALID_STATE_FOR_INTENT_ACTION,
+            INVALID_METADATA,
+            INVALID_MODULE_ID,
+            INVALID_PROJECT_ID,
+            INVALID_SELECTED_ID,
+            INVALID_SESSION_ID,
+            INVALID_USER_ID,
+            INVALID_VERIFY_ID,
+            SAFETYNET_ERROR;
+
+            companion object {
+                fun fromDomainToApi(type: AlertScreenEventType): ApiAlertScreenEvent =
+                    when (type) {
+                        AlertScreenEventType.DIFFERENT_PROJECT_ID -> DIFFERENT_PROJECT_ID
+                        AlertScreenEventType.DIFFERENT_USER_ID -> DIFFERENT_USER_ID
+                        AlertScreenEventType.GUID_NOT_FOUND_ONLINE -> GUID_NOT_FOUND_ONLINE
+                        AlertScreenEventType.GUID_NOT_FOUND_OFFLINE -> GUID_NOT_FOUND_OFFLINE
+                        AlertScreenEventType.BLUETOOTH_NOT_SUPPORTED -> BLUETOOTH_NOT_SUPPORTED
+                        AlertScreenEventType.LOW_BATTERY -> LOW_BATTERY
+                        AlertScreenEventType.UNEXPECTED_ERROR -> UNEXPECTED_ERROR
+                        AlertScreenEventType.DISCONNECTED -> DISCONNECTED
+                        AlertScreenEventType.MULTIPLE_PAIRED_SCANNERS -> MULTIPLE_PAIRED_SCANNERS
+                        AlertScreenEventType.NOT_PAIRED -> NOT_PAIRED
+                        AlertScreenEventType.BLUETOOTH_NOT_ENABLED -> BLUETOOTH_NOT_ENABLED
+                        AlertScreenEventType.NFC_NOT_ENABLED -> NFC_NOT_ENABLED
+                        AlertScreenEventType.NFC_PAIR -> NFC_PAIR
+                        AlertScreenEventType.SERIAL_ENTRY_PAIR -> SERIAL_ENTRY_PAIR
+                        AlertScreenEventType.OTA -> OTA
+                        AlertScreenEventType.OTA_RECOVERY -> OTA_RECOVERY
+                        AlertScreenEventType.OTA_FAILED -> OTA_FAILED
+                        AlertScreenEventType.INVALID_INTENT_ACTION -> INVALID_INTENT_ACTION
+                        AlertScreenEventType.INVALID_METADATA -> INVALID_METADATA
+                        AlertScreenEventType.INVALID_MODULE_ID -> INVALID_MODULE_ID
+                        AlertScreenEventType.INVALID_PROJECT_ID -> INVALID_PROJECT_ID
+                        AlertScreenEventType.INVALID_SELECTED_ID -> INVALID_SELECTED_ID
+                        AlertScreenEventType.INVALID_SESSION_ID -> INVALID_SESSION_ID
+                        AlertScreenEventType.INVALID_USER_ID -> INVALID_USER_ID
+                        AlertScreenEventType.INVALID_VERIFY_ID -> INVALID_VERIFY_ID
+                        AlertScreenEventType.SAFETYNET_ERROR -> SAFETYNET_ERROR
+                        AlertScreenEventType.ENROLMENT_LAST_BIOMETRICS_FAILED -> ENROLMENT_LAST_BIOMETRICS_FAILED
+                        AlertScreenEventType.INVALID_STATE_FOR_INTENT_ACTION -> INVALID_STATE_FOR_INTENT_ACTION
+                    }
+            }
         }
     }
 }
