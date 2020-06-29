34ba864076fe52f60f6dc23d6288529bdc466e85
Tsuharesu <tsuharesu01@gmail.com>
2020-06-29 16:42:24 +0100

    Map events to platform correctly
    Former-commit-id: 1d63046c2c78fc074bbe73a60ec297c895726055 [formerly bc1a465832ed94aad30dd3d409ca370728de15d4]
    Former-commit-id: e40c5fe38fa603a310e5cb85d61ea41a260ae600

--- face/src/main/java/com/simprints/face/controllers/core/events/model/FaceFallbackCaptureEvent.kt
+++ face/src/main/java/com/simprints/face/controllers/core/events/model/FaceFallbackCaptureEvent.kt
@@ -1,6 +1,10 @@
 package com.simprints.face.controllers.core.events.model
 
+import com.simprints.id.data.db.session.domain.models.events.FaceFallbackCaptureEvent as CoreFaceFallbackCaptureEvent
+
 class FaceFallbackCaptureEvent(
     startTime: Long,
     endTime: Long
-) : Event(EventType.FACE_FALLBACK_CAPTURE, startTime, endTime)
+) : Event(EventType.FACE_FALLBACK_CAPTURE, startTime, endTime) {
+    fun fromDomainToCore(): CoreFaceFallbackCaptureEvent = CoreFaceFallbackCaptureEvent(startTime, endTime)
+}
--- face/src/main/java/com/simprints/face/controllers/core/events/model/Matcher.kt
+++ face/src/main/java/com/simprints/face/controllers/core/events/model/Matcher.kt
@@ -1,3 +1,14 @@
 package com.simprints.face.controllers.core.events.model
 
-enum class Matcher { RANK_ONE, UNKNOWN }
+import com.simprints.face.exceptions.FaceUnexpectedException
+import com.simprints.id.data.db.session.domain.models.events.Matcher as CoreMatcher
+
+enum class Matcher {
+    RANK_ONE, UNKNOWN;
+
+    fun fromDomainToCore(): CoreMatcher =
+        when (this) {
+            RANK_ONE -> CoreMatcher.RANK_ONE
+            UNKNOWN -> throw FaceUnexpectedException("Unknown matcher")
+        }
+}
