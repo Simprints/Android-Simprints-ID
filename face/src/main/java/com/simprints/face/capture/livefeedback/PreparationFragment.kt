b021a90dd11ee1bdd8212b6160489fbd33ae62b3
Aashay Gaikwad <aashay@simprints.com>
2020-06-29 12:55:43 +0530

    Removed AndroidResourcesHelper for face
    Former-commit-id: 2c6fcb3ece63a91e5b2dbd806b75da19eb8e7106 [formerly dd0f1c45345a54e5f9d01f4718153b9a39a0c34c]
    Former-commit-id: 76302d6c146da0f87fcb2b5d9746f9e6ab1a0812

--- face/src/main/java/com/simprints/face/capture/livefeedback/PreparationFragment.kt
+++ face/src/main/java/com/simprints/face/capture/livefeedback/PreparationFragment.kt
@@ -5,12 +5,9 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.simprints.face.R
-import com.simprints.face.controllers.core.androidResources.FaceAndroidResourcesHelper
 import kotlinx.android.synthetic.main.fragment_preparation.*
-import org.koin.android.ext.android.inject
 
 class PreparationFragment : Fragment(R.layout.fragment_preparation) {
-    private val androidResourcesHelper: FaceAndroidResourcesHelper by inject()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         setTextInLayout()
@@ -21,12 +18,9 @@ class PreparationFragment : Fragment(R.layout.fragment_preparation) {
     }
 
     private fun setTextInLayout() {
-        with(androidResourcesHelper) {
-            detection_onboarding_light_txt.text = getString(R.string.onboarding_light)
-            detection_onboarding_fill_txt.text = getString(R.string.onboarding_fill)
-            detection_onboarding_straight_txt.text = getString(R.string.onboarding_straight)
-            detection_onboarding_txt_continue.text = getString(R.string.onboarding_continue)
-        }
+        detection_onboarding_light_txt.text = getString(R.string.onboarding_light)
+        detection_onboarding_fill_txt.text = getString(R.string.onboarding_fill)
+        detection_onboarding_straight_txt.text = getString(R.string.onboarding_straight)
+        detection_onboarding_txt_continue.text = getString(R.string.onboarding_continue)
     }
-
 }
