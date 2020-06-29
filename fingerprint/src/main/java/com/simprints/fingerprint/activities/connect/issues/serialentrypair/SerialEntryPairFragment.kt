6202911a17126efc209700c644b035a9fb2a3dfa
Aashay Gaikwad <aashay@simprints.com>
2020-06-29 12:15:46 +0530

    Removed AndroidResourcesHelper for finger
    Former-commit-id: ed7b2bccd103d96a1b4e119f8c72bf97b3fd2d92 [formerly d9a248d92ebfa802050ed5b97360cfbf5c9294a5]
    Former-commit-id: 68700fa6f77748972340ad3dd46c4badd610ef30

--- fingerprint/src/main/java/com/simprints/fingerprint/activities/connect/issues/serialentrypair/SerialEntryPairFragment.kt
+++ fingerprint/src/main/java/com/simprints/fingerprint/activities/connect/issues/serialentrypair/SerialEntryPairFragment.kt
@@ -10,11 +10,11 @@ import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo.IME_ACTION_DONE
 import android.view.inputmethod.InputMethodManager
 import androidx.navigation.fragment.findNavController
+import com.simprints.core.tools.utils.getParamsString
 import com.simprints.fingerprint.R
 import com.simprints.fingerprint.activities.base.FingerprintFragment
 import com.simprints.fingerprint.activities.connect.ConnectScannerViewModel
 import com.simprints.fingerprint.activities.connect.issues.ConnectScannerIssue
-import com.simprints.fingerprint.controllers.core.androidResources.FingerprintAndroidResourcesHelper
 import com.simprints.fingerprint.controllers.core.eventData.FingerprintSessionEventsManager
 import com.simprints.fingerprint.controllers.core.eventData.model.AlertScreenEventWithScannerIssue
 import com.simprints.fingerprint.controllers.core.timehelper.FingerprintTimeHelper
@@ -34,7 +34,6 @@ class SerialEntryPairFragment : FingerprintFragment() {
 
     private val scannerPairingManager: ScannerPairingManager by inject()
     private val serialNumberConverter: SerialNumberConverter by inject()
-    private val resourceHelper: FingerprintAndroidResourcesHelper by inject()
     private val timeHelper: FingerprintTimeHelper by inject()
     private val sessionManager: FingerprintSessionEventsManager by inject()
 
@@ -69,12 +68,10 @@ class SerialEntryPairFragment : FingerprintFragment() {
     }
 
     private fun setTextInLayout() {
-        with(resourceHelper) {
-            serialEntryOkButton.text = getString(R.string.serial_entry_ok)
-            serialEntryPairInstructionsTextView.text = getString(R.string.enter_scanner_number)
-            serialEntryPairTitleTextView.text = getString(R.string.serial_entry_pair_title)
-            serialEntryPairInstructionsDetailTextView.text = getString(R.string.enter_scanner_number_detail)
-        }
+        serialEntryOkButton.text = getString(R.string.serial_entry_ok)
+        serialEntryPairInstructionsTextView.text = getString(R.string.enter_scanner_number)
+        serialEntryPairTitleTextView.text = getString(R.string.serial_entry_pair_title)
+        serialEntryPairInstructionsDetailTextView.text = getString(R.string.enter_scanner_number_detail)
     }
 
     private fun setupDoneButtonForEditText() {
@@ -116,7 +113,7 @@ class SerialEntryPairFragment : FingerprintFragment() {
             val serialNumber = scannerPairingManager.interpretEnteredTextAsSerialNumber(serialEntryEditText.text.toString())
             viewModel.startPairing(serialNumber)
         } catch (e: NumberFormatException) {
-            context?.showToast(resourceHelper.getString(R.string.serial_entry_pair_toast_invalid))
+            context?.showToast(getString(R.string.serial_entry_pair_toast_invalid))
         }
     }
 
@@ -134,7 +131,7 @@ class SerialEntryPairFragment : FingerprintFragment() {
             serialEntryPairProgressBar.visibility = View.INVISIBLE
             serialEntryOkButton.visibility = View.VISIBLE
             serialEntryPairInstructionsDetailTextView.visibility = View.INVISIBLE
-            serialEntryPairInstructionsTextView.text = resourceHelper.getString(R.string.serial_entry_pair_failed,
+            serialEntryPairInstructionsTextView.text = requireActivity().getParamsString(R.string.serial_entry_pair_failed,
                 arrayOf(serialNumberConverter.convertMacAddressToSerialNumber(macAddressEvent.peekContent())))
         }
     }
