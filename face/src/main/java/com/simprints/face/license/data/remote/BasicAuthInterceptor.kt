7a15095ecbb351dda6b8db986d7ea2275030e9c7
Tsuharesu <tsuharesu01@gmail.com>
2020-07-13 22:57:43 +0100

    Add license server retrofit
    Former-commit-id: 75d7463ad20bfe9b0303faf231ff1b2611bda3d6 [formerly c0efcc9baee40b6f4b57f3793bd1a4967f01d94f]
    Former-commit-id: 9144429c789cc23307d74f669262e6fbb803498a

---
+++ face/src/main/java/com/simprints/face/license/data/remote/BasicAuthInterceptor.kt
@@ -0,0 +1,21 @@
+package com.simprints.face.license.data.remote
+
+import okhttp3.Credentials
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.io.IOException
+
+/**
+ * Using an interceptor instead of Authenticator so it sends the credentials preemptively
+ */
+class BasicAuthInterceptor(user: String, password: String) : Interceptor {
+    private val credentials: String = Credentials.basic(user, password)
+
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val authenticatedRequest = request.newBuilder()
+            .header("Authorization", credentials).build()
+        return chain.proceed(authenticatedRequest)
+    }
+}
