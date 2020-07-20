f17ac5b4749d1b458c2422a0cd58e614d18bc803
Fabio <fabio@simprints.com>
2020-07-20 14:59:32 +0100

    handling db <> domain <> db with klaxon
    Former-commit-id: 75ae27ccd96171fd3609056055298c7c3224fb23 [formerly 7f1a7f092b61d8fa8f83325338e96d82afb4954a]
    Former-commit-id: fbf8882faf6433f34a4b03b62fd0359be3dab47e

---
+++ id/src/main/java/com/simprints/id/data/db/event/domain/models/Event.kt
@@ -0,0 +1,22 @@
+package com.simprints.id.data.db.event.domain.models
+
+import androidx.annotation.Keep
+import com.beust.klaxon.TypeFor
+import com.simprints.id.data.db.event.domain.models.EventLabel.SessionIdLabel
+import com.simprints.id.data.db.event.local.EventAdapter
+
+@Keep
+@TypeFor(field = "type", adapter = EventAdapter::class)
+abstract class Event(open val id: String,
+            open val labels: MutableList<EventLabel>,
+            open val payload: EventPayload,
+            open val type: EventType) {
+
+    companion object {
+        const val DEFAULT_EVENT_VERSION = 0
+    }
+}
+
+
+fun Event.getSessionLabelIfExists(): SessionIdLabel? =
+    labels.firstOrNull { it is SessionIdLabel } as SessionIdLabel?
