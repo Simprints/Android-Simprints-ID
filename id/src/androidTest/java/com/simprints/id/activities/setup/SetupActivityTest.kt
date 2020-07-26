d0629be32ce1f25a4f7abb274d9c493a52196230
Fabio <fabio@simprints.com>
2020-07-27 00:05:23 +0100

    Making cAT compiling
    Former-commit-id: b2d197abdffef0b689e35a2643ac09de5dc44443 [formerly dc69cec1b80358cb54f9e64c5f5b6bb99d1ec844]
    Former-commit-id: 220b2843992f7ece5b1693ae08fb1387c2f7d897

--- id/src/androidTest/java/com/simprints/id/activities/setup/SetupActivityTest.kt
+++ id/src/androidTest/java/com/simprints/id/activities/setup/SetupActivityTest.kt
@@ -9,8 +9,9 @@ import androidx.test.rule.GrantPermissionRule
 import com.simprints.id.Application
 import com.simprints.id.commontesttools.di.TestAppModule
 import com.simprints.id.data.db.event.EventRepository
-import com.simprints.id.domain.moduleapi.core.requests.SetupPermission
-import com.simprints.id.domain.moduleapi.core.requests.SetupRequest
+import com.simprints.id.domain.modality.Modality
+import com.simprints.id.orchestrator.steps.core.requests.SetupPermission
+import com.simprints.id.orchestrator.steps.core.requests.SetupRequest
 import com.simprints.id.orchestrator.steps.core.response.CoreResponse
 import com.simprints.id.testtools.AndroidTestConfig
 import com.simprints.id.tools.LocationManager
