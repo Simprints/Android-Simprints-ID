79a2f4048c2f0269aa88c6fb463456947de69f41
Fabio <fabio@simprints.com>
2020-07-28 20:49:55 +0100

    Removed unnecessary ApiEvent structure and left only ApiPayload
    Former-commit-id: 2a50277d2a4c2ffbb38ab3e086dd7e8b59088afa [formerly cc82aef8c6516028c54f043f314c43a041cd3c84]
    Former-commit-id: 232cf8467c443a31580fa9d814618d844d65434e

--- id/src/test/java/com/simprints/id/data/db/event/remote/events/ApiEventLabelsTest.kt
+++ id/src/test/java/com/simprints/id/data/db/event/remote/events/ApiEventLabelsTest.kt
@@ -1,5 +1,29 @@
 package com.simprints.id.data.db.event.remote.events
 
-import org.junit.Assert.*
+import com.google.common.truth.Truth
+import com.simprints.id.commontesttools.DefaultTestConstants.DEFAULT_MODULE_ID
+import com.simprints.id.commontesttools.DefaultTestConstants.DEFAULT_PROJECT_ID
+import com.simprints.id.commontesttools.DefaultTestConstants.DEFAULT_USER_ID
+import com.simprints.id.domain.modality.Modes.FACE
+import com.simprints.id.domain.modality.Modes.FINGERPRINT
+import com.simprints.id.orchestrator.SOME_GUID1
+import com.simprints.id.orchestrator.SOME_GUID2
+import org.junit.Test
 
-class ApiEventLabelsTest
+class ApiEventLabelsTest {
+
+    @Test
+    fun create_ApiEventLabels() {
+        val apiEventLabels = ApiEventLabels().apply {
+            this["projectId"] = listOf(DEFAULT_PROJECT_ID)
+            this["subjectId"] = listOf(SOME_GUID1)
+            this["attendantId"] = listOf(DEFAULT_USER_ID)
+            this["sessionId"] = listOf(SOME_GUID1)
+            this["moduleId"] = listOf(DEFAULT_MODULE_ID)
+            this["mode"] = listOf(FINGERPRINT, FACE).map { it.name }
+            this["deviceId"] = listOf(SOME_GUID2)
+        }
+
+        Truth.assertThat(apiEventLabels).isEqualTo(apiEventLabels.fromApiToDomain().fromDomainToApi())
+    }
+}
