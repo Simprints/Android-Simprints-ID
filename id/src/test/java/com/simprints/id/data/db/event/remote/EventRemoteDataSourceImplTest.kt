b4559c62e5b469bdcdfb62fc125e8e69a6583c46
Fabio <fabio@simprints.com>
2020-08-04 09:48:08 +0100

    Added unit tests for EventRemoteDataSource
    Former-commit-id: 94d3c2eaa2e98ffd88a6e8cb5f1d1ed28fbb7da1 [formerly 6dd2c8315c7f300387a24c95f00f0a8cf184f180]
    Former-commit-id: fbe79947a3c69fa4d084db3e42cc6c3f905d2c0e

--- id/src/test/java/com/simprints/id/data/db/event/remote/EventRemoteDataSourceImplTest.kt
+++ id/src/test/java/com/simprints/id/data/db/event/remote/EventRemoteDataSourceImplTest.kt
@@ -1,39 +1,161 @@
 package com.simprints.id.data.db.event.remote
 
-//@RunWith(AndroidJUnit4::class)
-//@Config(application = TestApplication::class, shadows = [ShadowAndroidXMultiDex::class])
-//class EventRemoteDataSourceImplTest {
-//
-//    private val timeHelper: TimeHelper = TimeHelperImpl()
-//
-//    @MockK
-//    lateinit var simApiClientFactory: SimApiClientFactory
-//    @MockK
-//    lateinit var simApiClient: SimApiClient<EventRemoteInterface>
-//    lateinit var eventRemoteDataSource: EventRemoteDataSource
-//
-//    @Before
-//    @ExperimentalCoroutinesApi
-//    fun setUp() {
-//        MockKAnnotations.init(this, relaxed = true)
-//        UnitTestConfig(this).setupFirebase()
-//
-//        coEvery { simApiClientFactory.buildClient(EventRemoteInterface::class) } returns simApiClient
-//        eventRemoteDataSource = EventRemoteDataSourceImpl(simApiClientFactory)
-//    }
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
+import com.simprints.id.commontesttools.DefaultTestConstants.DEFAULT_MODULE_ID
+import com.simprints.id.commontesttools.DefaultTestConstants.DEFAULT_MODULE_ID_2
+import com.simprints.id.commontesttools.DefaultTestConstants.DEFAULT_PROJECT_ID
+import com.simprints.id.commontesttools.DefaultTestConstants.DEFAULT_USER_ID
+import com.simprints.id.commontesttools.DefaultTestConstants.GUID1
+import com.simprints.id.commontesttools.DefaultTestConstants.GUID2
+import com.simprints.id.commontesttools.events.createSessionCaptureEvent
+import com.simprints.id.data.db.common.models.EventCount
+import com.simprints.id.data.db.event.domain.models.EventType
+import com.simprints.id.data.db.event.remote.models.ApiEventCount
+import com.simprints.id.data.db.event.remote.models.ApiEventPayloadType.EnrolmentRecordCreation
+import com.simprints.id.data.db.event.remote.models.fromDomainToApi
+import com.simprints.id.network.SimApiClient
+import com.simprints.id.network.SimApiClientFactory
+import com.simprints.id.testtools.UnitTestConfig
+import io.kotlintest.shouldThrow
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
 
-//    @Test
-//    fun successfulResponseOnUpload() {
-//        runBlocking {
-//
-//            val sessions = listOf(
-//                createFakeClosedSession(timeHelper),
-//                createFakeClosedSession(timeHelper)
-//            )
-//
-//            eventRemoteDataSource.uploadSessions("projectId", sessions)
-//
-//            coVerify(exactly = 1) { simApiClient.executeCall(any(), any()) }
-//        }
-//    }
-//}
+typealias CountInvocation<T, V> = suspend (T) -> V
+
+@RunWith(AndroidJUnit4::class)
+class EventRemoteDataSourceImplTest {
+
+    @MockK lateinit var simApiClientFactory: SimApiClientFactory
+    @MockK lateinit var simApiClient: SimApiClient<EventRemoteInterface>
+    @MockK lateinit var eventRemoteInterface: EventRemoteInterface
+
+    private lateinit var eventRemoteDataSource: EventRemoteDataSource
+    private val query = ApiEventQuery(
+        projectId = DEFAULT_PROJECT_ID,
+        userId = DEFAULT_USER_ID,
+        moduleIds = listOf(DEFAULT_MODULE_ID, DEFAULT_MODULE_ID_2),
+        subjectId = GUID1,
+        lastEventId = GUID2,
+        modes = listOf(ApiModes.FACE, ApiModes.FINGERPRINT),
+        types = listOf(EnrolmentRecordCreation)
+    )
+
+    @Before
+    @ExperimentalCoroutinesApi
+    fun setUp() {
+        MockKAnnotations.init(this, relaxed = true)
+        UnitTestConfig(this).setupFirebase()
+
+        coEvery { simApiClient.executeCall<Int>(any(), any()) } coAnswers {
+            val args = this.args
+            (args[1] as CountInvocation<EventRemoteInterface, Int>).invoke(eventRemoteInterface)
+        }
+
+        coEvery { simApiClientFactory.buildClient(EventRemoteInterface::class) } returns simApiClient
+        eventRemoteDataSource = EventRemoteDataSourceImpl(simApiClientFactory)
+    }
+
+    @Test
+    fun count_shouldMakeANetworkRequest() {
+        runBlocking {
+            coEvery { eventRemoteInterface.countEvents(any(), any(), any(), any(), any(), any(), any()) } returns listOf(ApiEventCount(EnrolmentRecordCreation, 1))
+
+            val count = eventRemoteDataSource.count(query)
+
+            Truth.assertThat(count).isEqualTo(listOf(EventCount(EventType.ENROLMENT_RECORD_CREATION, 1)))
+            coVerify(exactly = 1) {
+                eventRemoteInterface.countEvents(
+                    DEFAULT_PROJECT_ID,
+                    listOf(DEFAULT_MODULE_ID, DEFAULT_MODULE_ID_2),
+                    DEFAULT_USER_ID, GUID1,
+                    listOf(ApiModes.FACE, ApiModes.FINGERPRINT),
+                    GUID2,
+                    listOf(EnrolmentRecordCreation.toString())
+                )
+            }
+        }
+    }
+
+    @Test
+    fun errorForCountRequestFails_shouldThrowAnException() {
+        runBlocking {
+            coEvery { eventRemoteInterface.countEvents(any(), any(), any(), any(), any(), any(), any()) } throws Throwable("Request issue")
+
+            shouldThrow<Throwable> {
+                eventRemoteDataSource.count(query)
+            }
+        }
+    }
+
+    @Test
+    fun getStreaming_shouldMakeANetworkRequest() {
+        runBlocking {
+            coEvery { eventRemoteInterface.downloadEvents(any(), any(), any(), any(), any(), any(), any()) } returns mockk()
+
+            eventRemoteDataSource.getStreaming(query)
+
+            coVerify(exactly = 1) {
+                eventRemoteInterface.downloadEvents(
+                    DEFAULT_PROJECT_ID,
+                    listOf(DEFAULT_MODULE_ID, DEFAULT_MODULE_ID_2),
+                    DEFAULT_USER_ID, GUID1,
+                    listOf(ApiModes.FACE, ApiModes.FINGERPRINT),
+                    GUID2,
+                    listOf(EnrolmentRecordCreation.toString())
+                )
+            }
+        }
+    }
+
+    @Test
+    fun getStreamingFails_shouldThrowAnException() {
+        runBlocking {
+            coEvery { eventRemoteInterface.downloadEvents(any(), any(), any(), any(), any(), any(), any()) } throws Throwable("Request issue")
+
+            shouldThrow<Throwable> {
+                eventRemoteDataSource.getStreaming(query)
+            }
+        }
+    }
+
+    @Test
+    fun postEvent_shouldUploadEvents() {
+        runBlocking {
+            coEvery { eventRemoteInterface.uploadEvents(any(), any()) } returns mockk()
+
+            val events = listOf(createSessionCaptureEvent())
+            eventRemoteDataSource.post(DEFAULT_PROJECT_ID, events)
+
+            coVerify(exactly = 1) {
+                eventRemoteInterface.uploadEvents(
+                    DEFAULT_PROJECT_ID,
+                    match {
+                        assertThat(it.events).containsExactlyElementsIn(events.map { it.fromDomainToApi() })
+                        true
+                    }
+                )
+            }
+        }
+    }
+
+    @Test
+    fun postEventFails_shouldThrowAnException() {
+        runBlocking {
+            coEvery { eventRemoteInterface.uploadEvents(any(), any()) } throws Throwable("Request issue")
+
+            shouldThrow<Throwable> {
+                eventRemoteDataSource.post(DEFAULT_PROJECT_ID, listOf(createSessionCaptureEvent()))
+            }
+        }
+    }
+}
