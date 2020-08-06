89dcb14377907e14fb5ff81f3357b3a6d944097b
Fabio <fabio@simprints.com>
2020-08-06 12:05:53 +0100

    Bug fixing on upsyncworker
    Former-commit-id: 165a0e51f58250507511e57a493ce0aade5920af [formerly ae6b706ee19ba3608a361a7003c0a9977227eb17]
    Former-commit-id: 1a15c9153bf4d9fc97beb60ebcc735029b6eebb0

--- id/src/main/java/com/simprints/id/data/db/events_sync/up/domain/EventUpSyncScope.kt
+++ id/src/main/java/com/simprints/id/data/db/events_sync/up/domain/EventUpSyncScope.kt
@@ -1,10 +1,24 @@
 package com.simprints.id.data.db.events_sync.up.domain
 
-abstract class EventUpSyncScope(var operation: EventUpSyncOperation) {
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.simprints.id.data.db.events_sync.up.domain.EventUpSyncScope.Companion.PROJECT_SUBJECT_SYNC_KEY
+import com.simprints.id.data.db.events_sync.up.domain.EventUpSyncScope.EventUpSyncScopeType.PROJECT_SUBJECT_SYNC
+import com.simprints.id.data.db.events_sync.up.domain.EventUpSyncScope.SubjectProjectScope
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes(
+    JsonSubTypes.Type(value = SubjectProjectScope::class, name = PROJECT_SUBJECT_SYNC_KEY)
+)
+abstract class EventUpSyncScope(val type: EventUpSyncScopeType,
+                                var operation: EventUpSyncOperation) {
 
     abstract val id: String
 
-    data class SubjectProjectScope(val projectId: String) : EventUpSyncScope(EventUpSyncOperation(buildId(projectId), LocalEventQuery(projectId = projectId))) {
+    data class SubjectProjectScope(val projectId: String) :
+        EventUpSyncScope(PROJECT_SUBJECT_SYNC, EventUpSyncOperation(buildId(projectId), LocalEventQuery(projectId = projectId))) {
+
         override val id: String
             get() = buildId(projectId)
 
@@ -13,7 +27,14 @@ abstract class EventUpSyncScope(var operation: EventUpSyncOperation) {
         }
     }
 
+    enum class EventUpSyncScopeType {
+        @JsonProperty(PROJECT_SUBJECT_SYNC_KEY) PROJECT_SUBJECT_SYNC
+    }
+
     companion object {
+        const val PROJECT_SUBJECT_SYNC_KEY = "PROJECT_SUBJECT_SYNC"
+
         private const val separator = "||"
     }
+
 }
