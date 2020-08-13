9245075cb5c5341ecb6bb1c327a6fc693f3327e0
tristram <tristram@simprints.com>
2020-08-13 12:12:19 +0100

    create kronos ntp time helper. create time directory in tools.
    Former-commit-id: e500dd44581ffa883eb839aed83c66791afcce78 [formerly d5df01c24b2a87f562338c0041b388d9e62fc97a]
    Former-commit-id: f2d8fd74989a350524817f8d7a2750366f7970a4

---
+++ id/src/main/java/com/simprints/id/tools/time/KronosTimeHelperImpl.kt
@@ -0,0 +1,37 @@
+package com.simprints.id.tools.time
+
+import com.simprints.id.tools.TimeHelper
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+class KronosTimeHelperImpl : TimeHelper{
+
+    override fun now(): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun nowMinus(duration: Long, unit: TimeUnit): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun msBetweenNowAndTime(time: Long): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun readableBetweenNowAndTime(date: Date): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCurrentDateAsString(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun todayInMillis(): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun tomorrowInMillis(): Long {
+        TODO("Not yet implemented")
+    }
+
+}
