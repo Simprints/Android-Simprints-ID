39d284ac8524443040bc35466713c67a80e15dc5
tristram <tristram@simprints.com>
2020-08-19 15:03:26 +0100

    fill in implementation
    Former-commit-id: 3256727bc5fd1fe4259db61d097479e79ca666cf [formerly c4372179994c412fa718d1bf57c5699a458e5a64]
    Former-commit-id: 579dd694e244293c1df1f7ffa0ebd9038801dc7f

--- id/src/main/java/com/simprints/id/tools/time/KronosTimeHelperImpl.kt
+++ id/src/main/java/com/simprints/id/tools/time/KronosTimeHelperImpl.kt
@@ -1,37 +1,49 @@
 package com.simprints.id.tools.time
 
+import android.text.format.DateUtils.*
+import com.lyft.kronos.KronosClock
 import com.simprints.id.tools.TimeHelper
+import java.text.DateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
 
-class KronosTimeHelperImpl : TimeHelper{
+class KronosTimeHelperImpl(private val clock: KronosClock) : TimeHelper {
 
-    override fun now(): Long {
-        TODO("Not yet implemented")
+    init {
+        clock.syncInBackground()
     }
 
-    override fun nowMinus(duration: Long, unit: TimeUnit): Long {
-        TODO("Not yet implemented")
-    }
+    override fun now(): Long = clock.getCurrentTimeMs()
 
-    override fun msBetweenNowAndTime(time: Long): Long {
-        TODO("Not yet implemented")
-    }
+    override fun nowMinus(duration: Long, unit: TimeUnit): Long = now() - unit.toMillis(duration)
 
-    override fun readableBetweenNowAndTime(date: Date): String {
-        TODO("Not yet implemented")
-    }
+    override fun msBetweenNowAndTime(time: Long): Long = now() - time
+
+    override fun readableBetweenNowAndTime(date: Date): String =
+        getRelativeTimeSpanString(date.time, Date().time, MINUTE_IN_MILLIS, FORMAT_SHOW_DATE).toString()
 
     override fun getCurrentDateAsString(): String {
-        TODO("Not yet implemented")
+        val dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.getDefault())
+        return dateFormat.format(Date())
     }
 
-    override fun todayInMillis(): Long {
-        TODO("Not yet implemented")
+    override fun todayInMillis(): Long = Calendar.getInstance().run {
+        set(Calendar.HOUR_OF_DAY, 0)
+        set(Calendar.MINUTE, 0)
+        set(Calendar.SECOND, 0)
+        set(Calendar.MILLISECOND, 0)
+
+        timeInMillis
     }
 
-    override fun tomorrowInMillis(): Long {
-        TODO("Not yet implemented")
+    override fun tomorrowInMillis(): Long = Calendar.getInstance().run {
+        add(Calendar.DATE, 1)
+        set(Calendar.HOUR_OF_DAY, 0)
+        set(Calendar.MINUTE, 0)
+        set(Calendar.SECOND, 0)
+        set(Calendar.MILLISECOND, 0)
+
+        timeInMillis
     }
 
 }
