fe11a43a7cbea381f016bb39888384fe396b4d42
Ridwan Farouki <ridwan@simprints.com>
2020-08-24 11:02:59 +0100

    Fixed tests
    Former-commit-id: 5f45c0d3a0e3515e82d703f21bb38033fb8656b8 [formerly 730ef1e826a391f01aa48bd0ef6232b9b108b368]
    Former-commit-id: 644f1bbc77913de360e65cb8894dceb0a1e90d23

--- fingerprint/src/androidTest/java/com/simprints/fingerprint/activities/collect/CollectFingerprintsActivityTest.kt
+++ fingerprint/src/androidTest/java/com/simprints/fingerprint/activities/collect/CollectFingerprintsActivityTest.kt
@@ -21,8 +21,9 @@ import com.simprints.fingerprint.activities.alert.AlertActivity
 import com.simprints.fingerprint.activities.alert.FingerprintAlert
 import com.simprints.fingerprint.activities.collect.request.CollectFingerprintsTaskRequest
 import com.simprints.fingerprint.activities.collect.result.CollectFingerprintsTaskResult
-import com.simprints.fingerprint.activities.collect.state.CollectFingerprintsState
 import com.simprints.fingerprint.activities.collect.state.CaptureState
+import com.simprints.fingerprint.activities.collect.state.CollectFingerprintsState
+import com.simprints.fingerprint.activities.collect.state.FingerState
 import com.simprints.fingerprint.activities.collect.state.ScanResult
 import com.simprints.fingerprint.activities.collect.tryagainsplash.SplashScreenActivity
 import com.simprints.fingerprint.activities.connect.ConnectScannerActivity
@@ -81,7 +82,7 @@ class CollectFingerprintsActivityTest : KoinTest {
 
     private fun startingState(fingers: List<FingerIdentifier>): CollectFingerprintsState =
         CollectFingerprintsState(
-            fingerStates = fingers.map { CaptureState.NotCollected },
+            fingerStates = fingers.map { FingerState(it, listOf(CaptureState.NotCollected)) },
             currentFingerIndex = 0,
             isAskingRescan = false,
             isShowingConfirmDialog = false,
@@ -169,8 +170,8 @@ class CollectFingerprintsActivityTest : KoinTest {
 
         val initialState = startingState(FOUR_FINGERS_IDS).apply {
             fingerStates = fingerStates.toMutableList().also {
-                it[0] = CaptureState.Collected(FOUR_FINGERS_IDS[0], ScanResult(GOOD_QUALITY, TEMPLATE, null, 60))
-                it[1] = CaptureState.Collected(FOUR_FINGERS_IDS[1], ScanResult(BAD_QUALITY, TEMPLATE, null, 60))
+                it[0] = FingerState(FOUR_FINGERS_IDS[0], listOf(CaptureState.Collected(ScanResult(GOOD_QUALITY, TEMPLATE, null, 60))))
+                it[1] = FingerState(FOUR_FINGERS_IDS[1], listOf(CaptureState.Collected(ScanResult(BAD_QUALITY, TEMPLATE, null, 60))))
             }
         }
 
@@ -295,7 +296,11 @@ class CollectFingerprintsActivityTest : KoinTest {
     private fun CollectFingerprintsState.updateCurrentFingerState(block: CaptureState.() -> CaptureState) =
         apply {
             fingerStates = fingerStates.toMutableList().also { fingerStates ->
-                fingerStates[currentFingerIndex] = currentFingerState().block()
+                fingerStates[currentFingerIndex] = fingerStates[currentFingerIndex]
+                    .copy(captures = fingerStates[currentFingerIndex].captures.toMutableList()
+                        .apply {
+                            this[fingerStates[currentFingerIndex].currentCaptureIndex] = fingerStates[currentFingerIndex].currentCapture().block()
+                        })
             }
         }
 
