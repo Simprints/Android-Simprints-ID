f4769b6c7e888f71f0e454fc7039c7db115d28e5
Aashay Gaikwad <aashay@simprints.com>
2020-09-16 17:29:20 +0100

    Showing images to upload in the UI
    Former-commit-id: f72570a654b8cf176e77fac6c6d25375863f76f3 [formerly f604a561cfe9bd82f27d8f8a5142e79127560255]
    Former-commit-id: 229fce316470fc17d1ac811a2606b1f5ca181e3b

--- id/src/main/java/com/simprints/id/activities/settings/syncinformation/SyncInformationViewModelFactory.kt
+++ id/src/main/java/com/simprints/id/activities/settings/syncinformation/SyncInformationViewModelFactory.kt
@@ -5,19 +5,21 @@ import androidx.lifecycle.ViewModelProvider
 import com.simprints.id.data.db.subjects_sync.down.SubjectsDownSyncScopeRepository
 import com.simprints.id.data.db.subject.SubjectRepository
 import com.simprints.id.data.db.subject.local.SubjectLocalDataSource
+import com.simprints.id.data.images.repository.ImageRepository
 import com.simprints.id.data.prefs.PreferencesManager
 
 class SyncInformationViewModelFactory(private val personRepository: SubjectRepository,
                                       private val subjectLocalDataSource: SubjectLocalDataSource,
                                       private val preferencesManager: PreferencesManager,
                                       private val projectId: String,
-                                      private val downSyncScopeRepository: SubjectsDownSyncScopeRepository) : ViewModelProvider.Factory {
+                                      private val downSyncScopeRepository: SubjectsDownSyncScopeRepository,
+                                      private val imageRepository: ImageRepository) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return if (modelClass.isAssignableFrom(SyncInformationViewModel::class.java)) {
             SyncInformationViewModel(personRepository, subjectLocalDataSource, preferencesManager,
-                projectId, downSyncScopeRepository) as T
+                projectId, downSyncScopeRepository, imageRepository) as T
         } else {
             throw IllegalArgumentException("ViewModel Not Found")
         }
