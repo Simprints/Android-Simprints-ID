5eafc7acfb3e6afd6878727e27a6f064218928db
Samad <samad@simprints.com>
2020-09-16 14:06:46 +0100

    Implementing protocol v 1.1
    Former-commit-id: 14bc4888aff2a757e1398d2a279f0764cb11d31f [formerly a9c90eb82fe48054fb9c2949e8a744431f051491]
    Former-commit-id: e3d2dc4981330e5f5706f06c910bc3b8c6e069de

---
+++ fingerprintscanner/src/main/java/com/simprints/fingerprintscanner/v2/domain/main/message/un20/responses/GetImageQualityPreviewResponse.kt
@@ -0,0 +1,20 @@
+package com.simprints.fingerprintscanner.v2.domain.main.message.un20.responses
+
+import com.simprints.fingerprintscanner.v2.domain.main.message.un20.Un20Response
+import com.simprints.fingerprintscanner.v2.domain.main.message.un20.models.Un20MessageType
+import com.simprints.fingerprintscanner.v2.tools.primitives.unsignedToInt
+
+class GetImageQualityPreviewResponse(val imageQualityScore: Int?) : Un20Response(Un20MessageType.GetImageQualityPreview) {
+
+    override fun getDataBytes(): ByteArray = imageQualityScore?.let { byteArrayOf(it.toByte()) }
+        ?: byteArrayOf()
+
+    companion object {
+        fun fromBytes(data: ByteArray) =
+            GetImageQualityPreviewResponse(
+                if (data.isNotEmpty())
+                    data[0].unsignedToInt()
+                else null
+            )
+    }
+}
