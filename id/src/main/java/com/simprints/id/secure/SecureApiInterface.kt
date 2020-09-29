9b373c1244d95f7ea675f8d768301d3bcc14db7d
Aashay Gaikwad <aashay@simprints.com>
2020-09-29 14:10:35 +0100

    Removed ApiKey from SecureApiInterface
    Former-commit-id: 9e4f59abe966f0e8f4e5ebabacecd58e7cf6699f [formerly 55683514b7e432a9a05b2f4c305f6d605a3dc634]
    Former-commit-id: b644d00bfe3084b41ce50324c21f12b283ff105f

--- id/src/main/java/com/simprints/id/secure/SecureApiInterface.kt
+++ id/src/main/java/com/simprints/id/secure/SecureApiInterface.kt
@@ -1,6 +1,5 @@
 package com.simprints.id.secure
 
-import com.simprints.id.BuildConfig
 import com.simprints.id.network.SimRemoteInterface
 import com.simprints.id.secure.models.AuthRequestBody
 import com.simprints.id.secure.models.SecurityState
@@ -11,24 +10,18 @@ import retrofit2.http.*
 
 interface SecureApiInterface : SimRemoteInterface {
 
-    companion object {
-        const val API_KEY: String = BuildConfig.ANDROID_AUTH_API_KEY
-    }
-
     @GET("projects/{projectId}/users/{userId}/authentication-data")
     suspend fun requestAuthenticationData(
         @Path("projectId") projectId: String,
         @Path("userId") userId: String,
-        @Query("deviceId") deviceId: String,
-        @Query("key") key: String = API_KEY
+        @Query("deviceId") deviceId: String
     ): Response<ApiAuthenticationData>
 
     @POST("projects/{projectId}/users/{userId}/authenticate")
     suspend fun requestCustomTokens(
         @Path("projectId") projectId: String,
         @Path("userId") userId: String,
-        @Body credentials: AuthRequestBody,
-        @Query("key") key: String = API_KEY
+        @Body credentials: AuthRequestBody
     ): Response<ApiToken>
 
     @GET("projects/{projectId}/devices/{deviceId}")
