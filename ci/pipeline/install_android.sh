9f50f1f49b671c4591bcf5a7b41358ff8d2ade8a
Fabio <fabio@simprints.com>
2020-12-03 16:27:23 +0000

    Better folders structure
    Former-commit-id: 01ec064d2b036994d3b97231f2e05a9d1e835ba2 [formerly 0452d046d1e43791024bcc5a00691cc8aac1d5c4]
    Former-commit-id: 0a392f03894fc4bf2137cc3172cdecfde7005c6b

--- ci/pipeline/install_android.sh
+++ ci/pipeline/install_android.sh
@@ -10,20 +10,23 @@ ANDROID_SDK_TOOLS=6858069
 apt-get install -y unzip make expect # NDK stuff
 
 # Get SDK tools (link from https://developer.android.com/studio/index.html#downloads)
-if [ ! -d "./cmdline-tools" ] 
+if [ ! -d "./android" ] 
 then
+	mkdir android
+	cd android
 	echo "Android cache doesn't exist"
 	wget -nv https://dl.google.com/android/repository/commandlinetools-linux-${ANDROID_SDK_TOOLS}_latest.zip
 	unzip -q -d cmdline-tools commandlinetools-linux-${ANDROID_SDK_TOOLS}_latest.zip
 	mv cmdline-tools/cmdline-tools cmdline-tools/tools
+	mkdir sdks
 else 
 	echo "Android cache does exist"
 fi
 
 echo "Installing ndk${ANDROID_NDK}"
 
-echo y | sdkmanager "platform-tools" "platforms;android-${ANDROID_COMPILE_SDK}"
-echo y | sdkmanager "build-tools;${ANDROID_BUILD_TOOLS}"
-echo y | sdkmanager ndk-bundle
+echo y | sdkmanager "platform-tools" "platforms;android-${ANDROID_COMPILE_SDK}" --sdk_root="~/android/sdks"
+echo y | sdkmanager "build-tools;${ANDROID_BUILD_TOOLS}" --sdk_root="~/android/sdks"
+echo y | sdkmanager ndk-bundle --sdk_root="~/android/sdks"
 
 yes | sdkmanager --licenses
