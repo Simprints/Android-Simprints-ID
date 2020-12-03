fbc51fadc9cb17512be03baaca6ec71255d4ae8e
Fabio <fabio@simprints.com>
2020-12-03 11:02:45 +0000

    fix sdk root
    Former-commit-id: 99e1b251484257c1791e467bdc010e472ba7397e [formerly cc0d16c95edeaea5f1676b0ce0150e9af9cfe2f7]
    Former-commit-id: 2a8126ce108c5f7d10e70633d635a6b1453c3a05

--- ci/pipeline/install_android.sh
+++ ci/pipeline/install_android.sh
@@ -10,21 +10,18 @@ ANDROID_SDK_TOOLS=6858069
 apt-get install -y unzip make expect # NDK stuff
 
 # Get SDK tools (link from https://developer.android.com/studio/index.html#downloads)
-wget -q https://dl.google.com/android/repository/commandlinetools-linux-${ANDROID_SDK_TOOLS}_latest.zip
-unzip -q -d android-sdk-linux commandlinetools-linux-${ANDROID_SDK_TOOLS}_latest.zip
+wget -nv https://dl.google.com/android/repository/commandlinetools-linux-${ANDROID_SDK_TOOLS}_latest.zip
+unzip -q -d cmdline-tools commandlinetools-linux-${ANDROID_SDK_TOOLS}_latest.zip
+mv cmdline-tools/cmdline-tools cmdline-tools/tools
 
-export ANDROID_HOME=$PWD/android-sdk-linux
-export ANDROID_SDK_ROOT=$PWD/android-sdk-linux
-export PATH=$PATH:$ANDROID_SDK_ROOT/cmdline-tools/bin
+export ANDROID_HOME=$PWD/cmdline-tools
+export ANDROID_SDK_ROOT=$PWD/cmdline-tools
+export PATH=$PATH:$ANDROID_SDK_ROOT/cmdline-tools/tools/bin
 
 source ~/.bashrc
 
-ls $ANDROID_SDK_ROOT
-ls $PATH
-ls $ANDROID_SDK_ROOT/cmdline-tools/bin
-
-echo y | sdkmanager "platform-tools" "platforms;android-${ANDROID_COMPILE_SDK}" >/dev/null
-echo y | sdkmanager "build-tools;${ANDROID_BUILD_TOOLS}" >/dev/null
+echo y | sdkmanager --sdk_root=$ANDROID_SDK_ROOT "platform-tools" "platforms;android-${ANDROID_COMPILE_SDK}" >/dev/null
+echo y | sdkmanager -sdk_root=$ANDROID_SDK_ROOT "build-tools;${ANDROID_BUILD_TOOLS}" >/dev/null
 yes | sdkmanager --licenses
 
 echo $ANDROID_SDK_ROOT
