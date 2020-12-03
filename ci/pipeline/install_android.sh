1d34d22bcbc07e2609f58674b1be17ccfdd6cc22
Fabio <fabio@simprints.com>
2020-12-03 21:50:28 +0000

    Removed unnecessary code
    Former-commit-id: 8285c54d8140dd8359e7f7c74e2293aaec240686 [formerly 89bcb6602e0f3fa758a852e06f6b169bec8dce7e]
    Former-commit-id: a260654b75abad51ad00b562f17a6418ed153377

--- ci/pipeline/install_android.sh
+++ ci/pipeline/install_android.sh
@@ -6,24 +6,15 @@ ANDROID_SDK_TOOLS=6858069
 
 apt-get install -y unzip make expect # NDK stuff
 
-if [ ! -d "./android" ] || [ -z "$(ls -A ./android)" ];
-then
-	echo "Android cache doesn't exist"
-	rm -r android
-	mkdir android
-	cd android
-	wget -nv https://dl.google.com/android/repository/commandlinetools-linux-${ANDROID_SDK_TOOLS}_latest.zip
-	unzip -q -d cmdline-tools commandlinetools-linux-${ANDROID_SDK_TOOLS}_latest.zip
-	mv cmdline-tools/cmdline-tools cmdline-tools/tools
-	mkdir sdks
+mkdir android
+cd android
+wget -nv https://dl.google.com/android/repository/commandlinetools-linux-${ANDROID_SDK_TOOLS}_latest.zip
+unzip -q -d cmdline-tools commandlinetools-linux-${ANDROID_SDK_TOOLS}_latest.zip
+mv cmdline-tools/cmdline-tools cmdline-tools/tools
+mkdir sdks
 
-	echo y | sdkmanager "platform-tools" "platforms;android-${ANDROID_COMPILE_SDK}" --sdk_root="./sdks"
-	echo y | sdkmanager "build-tools;${ANDROID_BUILD_TOOLS}" --sdk_root="./sdks"
-	echo y | sdkmanager ndk-bundle --sdk_root="./sdks"
+echo y | sdkmanager "platform-tools" "platforms;android-${ANDROID_COMPILE_SDK}" --sdk_root="./sdks"
+echo y | sdkmanager "build-tools;${ANDROID_BUILD_TOOLS}" --sdk_root="./sdks"
+echo y | sdkmanager ndk-bundle --sdk_root="./sdks"
 
-	yes | sdkmanager --licenses
-
-	echo "Android sdks installed!"
-else 
-	echo "Android cache does exist"
-fi
+yes | sdkmanager --licenses
