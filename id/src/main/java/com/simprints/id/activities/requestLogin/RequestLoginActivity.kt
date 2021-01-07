7cb94905060d8c141d0eee9397af70d3b7f6b523
Fbada006 <ferdinandkamata@yahoo.com>
2021-01-07 08:51:52 +0300

    add selectable textView with the device id
    Former-commit-id: 8dca4d0284cd8cd44cf4d42246178b716001951b [formerly 57fe72908756dc328a94614a53ec0e1c439be71c]
    Former-commit-id: bba3c97679016eed7720a238da1c5965c3d88125

--- id/src/main/java/com/simprints/id/activities/requestLogin/RequestLoginActivity.kt
+++ id/src/main/java/com/simprints/id/activities/requestLogin/RequestLoginActivity.kt
@@ -5,8 +5,12 @@ import com.simprints.core.tools.activity.BaseSplitActivity
 import com.simprints.id.Application
 import com.simprints.id.R
 import com.simprints.id.data.prefs.PreferencesManager
+import com.simprints.id.tools.extensions.deviceId
 import com.simprints.id.tools.extensions.packageVersionName
-import kotlinx.android.synthetic.main.activity_front.*
+import kotlinx.android.synthetic.main.activity_front.libSimprintsVersionTextView
+import kotlinx.android.synthetic.main.activity_front.requestLogin
+import kotlinx.android.synthetic.main.activity_front.simprintsIdVersionTextView
+import kotlinx.android.synthetic.main.activity_front.tv_device_id
 import javax.inject.Inject
 
 open class RequestLoginActivity : BaseSplitActivity() {
@@ -31,6 +35,7 @@ open class RequestLoginActivity : BaseSplitActivity() {
         libSimprintsVersionTextView.text = getString(R.string.libsimprints_label)
         simprintsIdVersionTextView.text = getString(R.string.simprints_label)
         requestLogin.text = getString(R.string.requestLogin_message)
+        tv_device_id.text = getString(R.string.device_id, app.deviceId)
     }
 
     private fun initSimprintsIdVersionTextView(simprintsIdVersion: String) {
