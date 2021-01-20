c7215f07bc6f33d8b80a32cd39a8e2a4e3734e37
Fabio <fabio@simprints.com>
2021-01-20 16:44:42 +0000

    Added default format for fingerprint and face template format
    Former-commit-id: 505539364b5f2ca86792bb99c9596a615c3b244a [formerly 6c91111d8fdadde2d9590169cafcb96f81e9cebd]
    Former-commit-id: 8fd5faf7e6518ed7a195f8cfed28191b9999d3f8

--- id/src/main/java/com/simprints/id/data/db/event/remote/models/subject/biometricref/fingerprint/ApiFingerprintReference.kt
+++ id/src/main/java/com/simprints/id/data/db/event/remote/models/subject/biometricref/fingerprint/ApiFingerprintReference.kt
@@ -4,13 +4,14 @@ import androidx.annotation.Keep
 import com.simprints.id.data.db.event.remote.models.subject.biometricref.ApiBiometricReference
 import com.simprints.id.data.db.event.remote.models.subject.biometricref.ApiBiometricReferenceType
 import com.simprints.id.data.db.event.remote.models.subject.biometricref.ApiBiometricReferenceType.FingerprintReference
+import com.simprints.id.data.db.event.remote.models.subject.biometricref.fingerprint.ApiFingerprintTemplateFormat.ISO_19794_2
 import java.util.*
 
 @Keep
 data class ApiFingerprintReference(
     override val id: String = UUID.randomUUID().toString(),
     val templates: List<ApiFingerprintTemplate>,
-    val format: ApiFingerprintTemplateFormat,
+    val format: ApiFingerprintTemplateFormat = ISO_19794_2,
     val metadata: HashMap<String, String>? = null) : ApiBiometricReference {
     override val type: ApiBiometricReferenceType = FingerprintReference
 }
