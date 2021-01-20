5f9a9f71434e32f3e03412906741bd365db8ad8f
Fabio <fabio@simprints.com>
2021-01-20 08:55:24 +0000

    Fixing unit tests
    Former-commit-id: 8c489eddb9b6066a5ccaba396ef2a2f0a5bb70c8 [formerly 79ef422c816e45a0a9da7ac0c657c0bdfc89c14a]
    Former-commit-id: 72dbe837e302065c3d8fd52b09067b185071a63e

--- id/src/test/java/com/simprints/id/data/db/event/domain/models/EnrolmentEventTest.kt
+++ id/src/test/java/com/simprints/id/data/db/event/domain/models/EnrolmentEventTest.kt
@@ -1,12 +1,14 @@
 package com.simprints.id.data.db.event.domain.models
 
 import com.google.common.truth.Truth.assertThat
+import com.simprints.id.commontesttools.DefaultTestConstants.DEFAULT_MODULE_ID
+import com.simprints.id.commontesttools.DefaultTestConstants.DEFAULT_PROJECT_ID
+import com.simprints.id.commontesttools.DefaultTestConstants.DEFAULT_USER_ID
 import com.simprints.id.commontesttools.DefaultTestConstants.GUID1
 import com.simprints.id.commontesttools.DefaultTestConstants.GUID2
 import com.simprints.id.commontesttools.events.CREATED_AT
 import com.simprints.id.commontesttools.events.DEFAULT_ENDED_AT
 import com.simprints.id.data.db.event.domain.models.EnrolmentEvent.Companion.EVENT_VERSION
-import com.simprints.id.data.db.event.domain.models.EnrolmentEvent.EnrolmentPayload
 import com.simprints.id.data.db.event.domain.models.EventType.ENROLMENT
 import org.junit.Test
 
@@ -15,16 +17,19 @@ class EnrolmentEventTest {
     @Test
     fun create_EnrolmentEvent() {
         val labels = EventLabels(sessionId = GUID1)
-        val event = EnrolmentEvent(CREATED_AT, GUID2, labels)
+        val event = EnrolmentEvent(CREATED_AT, GUID1, DEFAULT_PROJECT_ID, DEFAULT_MODULE_ID, DEFAULT_USER_ID, GUID2, labels)
         assertThat(event.id).isNotNull()
         assertThat(event.labels).isEqualTo(labels)
         assertThat(event.type).isEqualTo(ENROLMENT)
-        with(event.payload as EnrolmentPayload) {
+        with(event.payload) {
             assertThat(createdAt).isEqualTo(CREATED_AT)
             assertThat(endedAt).isEqualTo(DEFAULT_ENDED_AT)
             assertThat(eventVersion).isEqualTo(EVENT_VERSION)
             assertThat(type).isEqualTo(ENROLMENT)
-            assertThat(personId).isEqualTo(GUID2)
+            assertThat(projectId).isEqualTo(DEFAULT_PROJECT_ID)
+            assertThat(moduleId).isEqualTo(DEFAULT_MODULE_ID)
+            assertThat(attendantId).isEqualTo(DEFAULT_USER_ID)
+            assertThat(personCreationEventId).isEqualTo(GUID2)
         }
     }
 }
--- id/src/test/java/com/simprints/id/orchestrator/EnrolmentHelperImplTest.kt
+++ id/src/test/java/com/simprints/id/orchestrator/EnrolmentHelperImplTest.kt
@@ -3,21 +3,20 @@ package com.simprints.id.orchestrator
 import com.simprints.id.commontesttools.DefaultTestConstants.DEFAULT_PROJECT_ID
 import com.simprints.id.commontesttools.DefaultTestConstants.defaultSubject
 import com.simprints.id.commontesttools.events.CREATED_AT
+import com.simprints.id.commontesttools.events.createPersonCreationEvent
+import com.simprints.id.commontesttools.events.createSessionCaptureEvent
 import com.simprints.id.data.db.event.EventRepository
 import com.simprints.id.data.db.event.domain.models.EnrolmentEvent
-import com.simprints.id.data.db.event.domain.models.subject.EnrolmentRecordCreationEvent
 import com.simprints.id.data.db.events_sync.up.domain.LocalEventQuery
 import com.simprints.id.data.db.subject.SubjectRepository
 import com.simprints.id.data.db.subject.domain.SubjectAction
 import com.simprints.id.data.loginInfo.LoginInfoManager
-import com.simprints.id.data.prefs.PreferencesManager
-import com.simprints.id.domain.modality.Modality.FINGER
-import com.simprints.id.domain.modality.Modes.FINGERPRINT
 import com.simprints.id.tools.mockUUID
 import com.simprints.id.tools.time.TimeHelper
 import com.simprints.testtools.common.coroutines.TestCoroutineRule
 import io.mockk.*
 import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
@@ -31,20 +30,21 @@ class EnrolmentHelperImplTest {
 
     @MockK lateinit var subjectRepository: SubjectRepository
     @MockK lateinit var eventRepository: EventRepository
-    @MockK lateinit var preferencesManager: PreferencesManager
     @MockK lateinit var loginInfoManager: LoginInfoManager
     @MockK lateinit var timeHelper: TimeHelper
 
     private lateinit var enrolmentHelper: EnrolmentHelper
+    private val personCreationEvent = createPersonCreationEvent()
 
     @Before
     fun setUp() {
         MockKAnnotations.init(this, relaxed = true)
 
-        enrolmentHelper = EnrolmentHelperImpl(subjectRepository, eventRepository, preferencesManager, loginInfoManager, timeHelper)
+        enrolmentHelper = EnrolmentHelperImpl(subjectRepository, eventRepository, loginInfoManager, timeHelper)
         every { timeHelper.now() } returns CREATED_AT
-        coEvery { preferencesManager.modalities } returns listOf(FINGER)
-        every { loginInfoManager.getSignedInProjectIdOrEmpty()  } returns DEFAULT_PROJECT_ID
+        every { loginInfoManager.getSignedInProjectIdOrEmpty() } returns DEFAULT_PROJECT_ID
+        coEvery { eventRepository.getCurrentCaptureSessionEvent() } returns createSessionCaptureEvent()
+        coEvery { eventRepository.loadEvents(any()) } returns flowOf(personCreationEvent)
 
         mockUUID()
     }
@@ -55,21 +55,10 @@ class EnrolmentHelperImplTest {
 
             enrolmentHelper.enrol(defaultSubject)
 
-            val expectedEnrolmentEvent = EnrolmentEvent(CREATED_AT, defaultSubject.subjectId)
-            val expectedEnrolmentRecordCreationEvent = EnrolmentRecordCreationEvent(
-                CREATED_AT,
-                defaultSubject.subjectId,
-                defaultSubject.projectId,
-                defaultSubject.moduleId,
-                defaultSubject.attendantId,
-                listOf(FINGERPRINT),
-                EnrolmentRecordCreationEvent.buildBiometricReferences(defaultSubject.fingerprintSamples, defaultSubject.faceSamples))
-
+            val expectedEnrolmentEvent = EnrolmentEvent(CREATED_AT, defaultSubject.subjectId, defaultSubject.projectId, defaultSubject.moduleId, defaultSubject.attendantId, personCreationEvent.id)
 
             coVerify {
                 eventRepository.addEventToCurrentSession(expectedEnrolmentEvent)
-
-                eventRepository.addEvent(expectedEnrolmentRecordCreationEvent)
             }
         }
     }
