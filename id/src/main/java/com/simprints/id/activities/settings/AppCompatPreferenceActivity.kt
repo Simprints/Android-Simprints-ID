fa8c15f68c8d4e307924476e640a27e4971f632d
Fbada006 <ferdinandkamata@yahoo.com>
2021-01-29 17:25:49 +0300

    disable unnecessary code in AppCompatPreferenceActivity.kt
    Former-commit-id: e9fa938faac32642d89ef6440f42c6d9d92d9c52 [formerly d22e0586709a1527f1522b55dc15d62b03d458c0]
    Former-commit-id: 86a3bf7d4fa9397c65bc8e6640bbac53bf65dc34

--- id/src/main/java/com/simprints/id/activities/settings/AppCompatPreferenceActivity.kt
+++ id/src/main/java/com/simprints/id/activities/settings/AppCompatPreferenceActivity.kt
@@ -1,81 +1,78 @@
 package com.simprints.id.activities.settings
 
 import android.content.Context
-import android.os.Bundle
-import android.view.MenuInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.simprints.core.tools.utils.LanguageHelper
 
 /**
- * A [android.preference.PreferenceActivity] which implements and proxies the necessary calls
+ * An [AppCompatActivity] which implements and proxies the necessary calls
  * to be used with AppCompat.
  */
 abstract class AppCompatPreferenceActivity : AppCompatActivity() {
 
+    // This method is overridden in the SettingsActivity. Why?
     override fun attachBaseContext(newBase: Context) {
         val languageCtx = LanguageHelper.getLanguageConfigurationContext(newBase)
         super.attachBaseContext(languageCtx)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        delegate.installViewFactory()
-        delegate.onCreate(savedInstanceState)
-    }
+// Are these delegate calls really necessary considering that the base Activity class already has a  delegate initialized lazily?
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        delegate.installViewFactory()
+//        delegate.onCreate(savedInstanceState)
+//    }
 
-    override fun onPostCreate(savedInstanceState: Bundle?) {
-        super.onPostCreate(savedInstanceState)
-        delegate.onPostCreate(savedInstanceState)
-    }
+//    override fun onPostCreate(savedInstanceState: Bundle?) {
+//        super.onPostCreate(savedInstanceState)
+//        delegate.onPostCreate(savedInstanceState)
+//    }
 
     override fun setSupportActionBar(toolbar: Toolbar?) {
         delegate.setSupportActionBar(toolbar)
         delegate.supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    override fun getMenuInflater(): MenuInflater {
-        return delegate.menuInflater
-    }
+//    override fun getMenuInflater(): MenuInflater {
+//        return delegate.menuInflater
+//    }
 
-    override fun setContentView(@LayoutRes layoutResID: Int) {
-        delegate.setContentView(layoutResID)
-    }
+//    override fun setContentView(@LayoutRes layoutResID: Int) {
+//        delegate.setContentView(layoutResID)
+//    }
 
-    override fun setContentView(view: View) {
-        delegate.setContentView(view)
-    }
+//    override fun setContentView(view: View) {
+//        delegate.setContentView(view)
+//    }
 
-    override fun setContentView(view: View, params: ViewGroup.LayoutParams) {
-        delegate.setContentView(view, params)
-    }
+//    override fun setContentView(view: View, params: ViewGroup.LayoutParams) {
+//        delegate.setContentView(view, params)
+//    }
 
-    override fun addContentView(view: View, params: ViewGroup.LayoutParams) {
-        delegate.addContentView(view, params)
-    }
+//    override fun addContentView(view: View, params: ViewGroup.LayoutParams) {
+//        delegate.addContentView(view, params)
+//    }
 
-    override fun onPostResume() {
-        super.onPostResume()
-        delegate.onPostResume()
-    }
+//    override fun onPostResume() {
+//        super.onPostResume()
+//        delegate.onPostResume()
+//    }
 
-    override fun onTitleChanged(title: CharSequence, color: Int) {
-        super.onTitleChanged(title, color)
-        delegate.setTitle(title)
-    }
+//    override fun onTitleChanged(title: CharSequence, color: Int) {
+//        super.onTitleChanged(title, color)
+//        delegate.setTitle(title)
+//    }
 
-    override fun onStop() {
-        super.onStop()
-        delegate.onStop()
-    }
+//    override fun onStop() {
+//        super.onStop()
+//        delegate.onStop()
+//    }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        delegate.onDestroy()
-    }
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        delegate.onDestroy()
+//    }
 
 //    private val delegate: AppCompatDelegate by lazy {
 //        AppCompatDelegate.create(this, null)
