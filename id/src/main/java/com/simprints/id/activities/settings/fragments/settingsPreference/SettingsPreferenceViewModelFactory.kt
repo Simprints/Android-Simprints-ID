d11099697665275395333ccc4ae511dd03577497
Fbada006 <ferdinandkamata@yahoo.com>
2021-02-11 10:37:49 +0300

    add vm and factory for SettingsPreferenceFragment.kt
    Former-commit-id: 6495851a796d6772cb0d9cea50f1f09920323dd7 [formerly 2cdd382770a8ad14634f5a2abe6a6ace74295eb3]
    Former-commit-id: 4aed78f5278f9f2a2e746e1744c24d5f7b72d533

---
+++ id/src/main/java/com/simprints/id/activities/settings/fragments/settingsPreference/SettingsPreferenceViewModelFactory.kt
@@ -0,0 +1,10 @@
+package com.simprints.id.activities.settings.fragments.settingsPreference
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class SettingsPreferenceViewModelFactory : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        TODO("Not yet implemented")
+    }
+}
