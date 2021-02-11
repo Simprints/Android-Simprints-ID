a0f5faad6129eb1865219d129cd78b3d20111021
Tsuharesu <tsuharesu01@gmail.com>
2021-02-11 15:34:22 +0000

    feat: make guid selection a sync event
    This makes sure the event is always saved before returning to the calling app.
    Previous implementation of worker could be deferred and the event not returned as needed.
    
    
    Former-commit-id: 4920359ad5b2710a566dc19e49417656ede0fc92 [formerly 6e8dcbf32b34614d237bcd658a94468aae484efd]
    Former-commit-id: 6b78a446138ac65e7031a7957611dfe6f01c55c7

--- id/src/main/java/com/simprints/id/activities/guidselection/GuidSelectionActivity.kt
+++ id/src/main/java/com/simprints/id/activities/guidselection/GuidSelectionActivity.kt
@@ -1,29 +1,43 @@
 package com.simprints.id.activities.guidselection
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import androidx.work.Data
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
 import com.simprints.core.tools.activity.BaseSplitActivity
 import com.simprints.id.Application
 import com.simprints.id.R
+import com.simprints.id.data.analytics.crashreport.CrashReportManager
+import com.simprints.id.data.analytics.crashreport.CrashReportTag
+import com.simprints.id.data.analytics.crashreport.CrashReportTrigger
 import com.simprints.id.data.db.event.EventRepository
 import com.simprints.id.exceptions.unexpected.InvalidAppRequest
-import com.simprints.id.guidselection.GuidSelectionWorker
 import com.simprints.id.orchestrator.steps.core.requests.GuidSelectionRequest
 import com.simprints.id.orchestrator.steps.core.response.CoreResponse.Companion.CORE_STEP_BUNDLE
 import com.simprints.id.orchestrator.steps.core.response.GuidSelectionResponse
+import com.simprints.id.services.guidselection.GuidSelectionManager
 import com.simprints.id.tools.time.TimeHelper
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
 
 class GuidSelectionActivity : BaseSplitActivity() {
 
-    @Inject lateinit var eventRepository: EventRepository
-    @Inject lateinit var timeHelper: TimeHelper
-    private lateinit var guildSelectionRequest: GuidSelectionRequest
+    @Inject
+    lateinit var eventRepository: EventRepository
+
+    @Inject
+    lateinit var timeHelper: TimeHelper
+
+    @Inject
+    lateinit var guidSelectionManager: GuidSelectionManager
+
+    @Inject
+    lateinit var crashReportManager: CrashReportManager
+
+    private lateinit var guidSelectionRequest: GuidSelectionRequest
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,10 +45,14 @@ class GuidSelectionActivity : BaseSplitActivity() {
         injectDependencies()
         Timber.d("GuidSelectionActivity started")
 
-        guildSelectionRequest = intent.extras?.getParcelable(CORE_STEP_BUNDLE) ?: throw InvalidAppRequest()
+        guidSelectionRequest = intent.extras?.getParcelable(CORE_STEP_BUNDLE) ?: throw InvalidAppRequest()
 
-        scheduleGuidSelection()
-        sendOkResult()
+        CoroutineScope(Dispatchers.Main).launch {
+            handleGuidSelectionRequest()
+        }.invokeOnCompletion {
+            // It doesn't matter if it was an error, we always return a good result as before
+            sendOkResult()
+        }
     }
 
     private fun injectDependencies() {
@@ -42,10 +60,21 @@ class GuidSelectionActivity : BaseSplitActivity() {
         component.inject(this)
     }
 
-    private fun scheduleGuidSelection() {
-        val guidSelectionWork = buildGuidSelectionWork()
-        WorkManager.getInstance(this).enqueue(guidSelectionWork)
+    @SuppressLint("CheckResult")
+    private suspend fun handleGuidSelectionRequest() {
+        try {
+            guidSelectionManager.handleConfirmIdentityRequest(guidSelectionRequest)
+            Timber.d("Added Guid Selection Event")
+            crashReportManager.logMessageForCrashReport(
+                CrashReportTag.SESSION,
+                CrashReportTrigger.UI, message = "Added Guid Selection Event"
+            )
+        } catch (t: Throwable) {
+            Timber.e(t)
+            crashReportManager.logException(t)
+        }
     }
+
 
     private fun sendOkResult() {
         val response = GuidSelectionResponse(identificationOutcome = true)
@@ -57,9 +86,4 @@ class GuidSelectionActivity : BaseSplitActivity() {
         finish()
     }
 
-    private fun buildGuidSelectionWork() = OneTimeWorkRequestBuilder<GuidSelectionWorker>()
-        .setInputData(prepareInputData())
-        .build()
-
-    private fun prepareInputData() = Data.Builder().putAll(guildSelectionRequest.toMap()).build()
 }
