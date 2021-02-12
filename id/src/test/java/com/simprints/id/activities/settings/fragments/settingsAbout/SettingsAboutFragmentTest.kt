1346b715ea013bd29ebb5662f70d2dea6bb0f49a
Fbada006 <ferdinandkamata@yahoo.com>
2021-02-12 12:29:00 +0300

    migrate test for logout pref
    Former-commit-id: 979ffe0be309e73d96676101b57f9db31b31fa7c [formerly 80b89df7c1b46c60c4d12ec7f45b7895c2822b48]
    Former-commit-id: 27763990be149944a5c7c9650ec22cca542f3635

--- id/src/test/java/com/simprints/id/activities/settings/fragments/settingsAbout/SettingsAboutFragmentTest.kt
+++ id/src/test/java/com/simprints/id/activities/settings/fragments/settingsAbout/SettingsAboutFragmentTest.kt
@@ -1,6 +1,7 @@
 package com.simprints.id.activities.settings.fragments.settingsAbout
 
 import android.content.DialogInterface.BUTTON_POSITIVE
+import androidx.preference.Preference
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.simprints.id.activities.settings.SettingsAboutActivity
@@ -10,8 +11,12 @@ import com.simprints.id.testtools.UnitTestConfig
 import com.simprints.testtools.unit.robolectric.ShadowAndroidXMultiDex
 import io.mockk.coEvery
 import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
 import io.mockk.spyk
+import io.mockk.verify
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -23,9 +28,18 @@ import org.robolectric.annotation.Config
 @Config(application = TestApplication::class, shadows = [ShadowAndroidXMultiDex::class])
 class SettingsAboutFragmentTest {
 
+    companion object {
+        const val PREFERENCE_KEY_FOR_SYNC_AND_SEARCH = "select_sync_and_search"
+        const val PREFERENCE_KEY_FOR_APP_VERSION = "app_version"
+        const val PREFERENCE_KEY_FOR_SCANNER_VERSION = "scanner_version"
+        const val PREFERENCE_KEY_FOR_LOGOUT = "finishSettings"
+        const val PREFERENCE_KEY_FOR_DEVICE_ID = "device_id"
+    }
+
     private val app = ApplicationProvider.getApplicationContext() as TestApplication
 
     private lateinit var settingsAboutActivity: SettingsAboutActivity
+    private lateinit var fragment: SettingsAboutFragment
 
     private val module by lazy {
         TestAppModule(app)
@@ -40,12 +54,13 @@ class SettingsAboutFragmentTest {
             .start()
             .resume()
             .get()
+
+        val fragmentManager = settingsAboutActivity.supportFragmentManager
+        fragment = fragmentManager.findFragmentById(com.simprints.id.R.id.prefContent) as SettingsAboutFragment
     }
 
     @Test
     fun logoutDialogShown_userClicksOk_presenterShouldPerformLogout() = runBlocking {
-        val fragmentManager = settingsAboutActivity.supportFragmentManager
-        val fragment = fragmentManager.findFragmentById(com.simprints.id.R.id.prefContent) as SettingsAboutFragment
         fragment.settingsAboutViewModel = spyk(fragment.settingsAboutViewModel)
         coEvery { fragment.settingsAboutViewModel.logout() } coAnswers { }
         val dialog = fragment.buildConfirmationDialogForLogout()
@@ -54,5 +69,24 @@ class SettingsAboutFragmentTest {
         dialog.getButton(BUTTON_POSITIVE).performClick()
 
         coVerify { fragment.settingsAboutViewModel.logout() }
+    }
+
+    @Test
+    fun logoutPreference_userClicksOnIt_viewShouldShowDialog() {
+        val mockPreference: Preference = mockk(relaxed = true)
+        every { fragment.getLogoutPreference() } returns mockPreference
+        every { fragment.getKeyForLogoutPreference() } returns PREFERENCE_KEY_FOR_LOGOUT
+        every { mockPreference.key } returns PREFERENCE_KEY_FOR_LOGOUT
+        var actionForLogoutPreference: Preference.OnPreferenceClickListener? = null
+        every { mockPreference.setOnPreferenceClickListener(any()) } answers {
+            actionForLogoutPreference = this.args.first() as Preference.OnPreferenceClickListener
+        }
+
+        fragment.loadValueAndBindChangeListener(mockPreference)
+
+        actionForLogoutPreference?.let {
+            it.onPreferenceClick(mockPreference)
+            verify(exactly = 1) { fragment.showConfirmationDialogForLogout() }
+        } ?: Assert.fail("Action for logout preference not set.")
     }
 }
