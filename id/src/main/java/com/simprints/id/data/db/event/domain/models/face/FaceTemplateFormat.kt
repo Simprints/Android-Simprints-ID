5ee622e783cce774dfa1e59e99ac0c27f0d3d8ec
Tsuharesu <tsuharesu01@gmail.com>
2021-03-03 17:07:16 +0000

    refactor: change name of template format enum
    No need to create multiple, let's reuse the Enum if possible in the same module
    
    
    Former-commit-id: b22fcdf017a9bb2f7e1898fa2faac840397545e1 [formerly 4f98956ae31deb8b07665bbc4bf3591e792aee1e]
    Former-commit-id: b6681183e9920d3a65d943b082aebd4963cd9b28

---
+++ id/src/main/java/com/simprints/id/data/db/event/domain/models/face/FaceTemplateFormat.kt
@@ -0,0 +1,8 @@
+package com.simprints.id.data.db.event.domain.models.face
+
+import androidx.annotation.Keep
+
+@Keep
+enum class FaceTemplateFormat {
+    RANK_ONE_1_23
+}
--- id/src/main/java/com/simprints/id/data/db/event/remote/models/subject/biometricref/fingerprint/ApiFingerprintTemplateFormat.kt
+++
@@ -1,9 +0,0 @@
-package com.simprints.id.data.db.event.remote.models.subject.biometricref.fingerprint
-
-import androidx.annotation.Keep
-
-@Keep
-enum class ApiFingerprintTemplateFormat {
-    ISO_19794_2,
-    NEC
-}
