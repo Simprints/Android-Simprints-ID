38134272155b4a21db1bb2d7917a22be0d15a592
Tsuharesu <tsuharesu01@gmail.com>
2021-03-05 18:19:37 +0000

    feat: add template format to module api and add to some sample classes
    Former-commit-id: 54a0999e3721aae6cf0f270c04d6613193a5fe7f [formerly 0a033d366278b2f1417e258dfac48af0550d9f96]
    Former-commit-id: 1af8094286bf239e42eb2263768bea417583008d

--- id/src/main/java/com/simprints/id/domain/moduleapi/face/responses/entities/FaceCaptureSample.kt
+++ id/src/main/java/com/simprints/id/domain/moduleapi/face/responses/entities/FaceCaptureSample.kt
@@ -1,29 +1,34 @@
 package com.simprints.id.domain.moduleapi.face.responses.entities
 
 import android.os.Parcelable
+import com.simprints.id.data.db.event.domain.models.face.FaceTemplateFormat
+import com.simprints.id.data.db.event.domain.models.face.fromModuleApiToDomain
 import com.simprints.id.data.images.model.SecuredImageRef
 import com.simprints.id.domain.moduleapi.images.fromDomainToModuleApi
 import com.simprints.id.domain.moduleapi.images.fromModuleApiToDomain
 import com.simprints.moduleapi.common.ISecuredImageRef
 import com.simprints.moduleapi.face.responses.entities.IFaceSample
+import com.simprints.moduleapi.face.responses.entities.IFaceTemplateFormat
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-class FaceCaptureSample (
+class FaceCaptureSample(
     val faceId: String,
     val template: ByteArray,
-    val imageRef: SecuredImageRef?
-): Parcelable
+    val imageRef: SecuredImageRef?,
+    val format: FaceTemplateFormat
+) : Parcelable
 
 fun FaceCaptureSample.fromDomainToModuleApi(): IFaceSample =
-    IFaceSampleImpl(faceId, template, imageRef?.fromDomainToModuleApi())
+    IFaceSampleImpl(faceId, template, imageRef?.fromDomainToModuleApi(), format.fromDomainToModuleApi())
 
 fun IFaceSample.fromModuleApiToDomain() =
-    FaceCaptureSample(faceId, template, imageRef?.fromModuleApiToDomain())
+    FaceCaptureSample(faceId, template, imageRef?.fromModuleApiToDomain(), format.fromModuleApiToDomain())
 
 @Parcelize
 private class IFaceSampleImpl(
     override val faceId: String,
     override val template: ByteArray,
-    override val imageRef: ISecuredImageRef?
-): IFaceSample
+    override val imageRef: ISecuredImageRef?,
+    override val format: IFaceTemplateFormat
+) : IFaceSample
