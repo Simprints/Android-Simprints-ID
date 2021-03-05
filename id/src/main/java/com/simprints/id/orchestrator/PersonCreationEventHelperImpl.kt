2a432a212c972705ea50574123722aef6fec08a1
Tsuharesu <tsuharesu01@gmail.com>
2021-03-05 18:19:37 +0000

    feat: add template format to module api and add to some sample classes
    Former-commit-id: 54a0999e3721aae6cf0f270c04d6613193a5fe7f [formerly 0a033d366278b2f1417e258dfac48af0550d9f96]
    Former-commit-id: 1af8094286bf239e42eb2263768bea417583008d

--- id/src/main/java/com/simprints/id/orchestrator/PersonCreationEventHelperImpl.kt
+++ id/src/main/java/com/simprints/id/orchestrator/PersonCreationEventHelperImpl.kt
@@ -1,10 +1,10 @@
 package com.simprints.id.orchestrator
 
 import com.simprints.id.data.db.event.EventRepository
-import com.simprints.id.data.db.event.domain.models.fingerprint.FingerprintCaptureEvent
-import com.simprints.id.data.db.event.domain.models.fingerprint.FingerprintCaptureEvent.FingerprintCapturePayload.Result.SKIPPED
 import com.simprints.id.data.db.event.domain.models.PersonCreationEvent
 import com.simprints.id.data.db.event.domain.models.face.FaceCaptureEvent
+import com.simprints.id.data.db.event.domain.models.fingerprint.FingerprintCaptureEvent
+import com.simprints.id.data.db.event.domain.models.fingerprint.FingerprintCaptureEvent.FingerprintCapturePayload.Result.SKIPPED
 import com.simprints.id.data.db.subject.domain.FaceSample
 import com.simprints.id.data.db.subject.domain.FingerprintSample
 import com.simprints.id.data.db.subject.domain.uniqueId
@@ -16,13 +16,16 @@ import com.simprints.id.tools.utils.EncodingUtils
 import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.toList
 
-class PersonCreationEventHelperImpl(val eventRepository: EventRepository,
-                                    val timeHelper: TimeHelper,
-                                    private val encodingUtils: EncodingUtils) : PersonCreationEventHelper {
+class PersonCreationEventHelperImpl(
+    val eventRepository: EventRepository,
+    val timeHelper: TimeHelper,
+    private val encodingUtils: EncodingUtils
+) : PersonCreationEventHelper {
 
     override suspend fun addPersonCreationEventIfNeeded(steps: List<Result>) {
         val currentSession = eventRepository.getCurrentCaptureSessionEvent()
-        val personCreationEventInSession = eventRepository.loadEvents(currentSession.id).filterIsInstance<PersonCreationEvent>().toList()
+        val personCreationEventInSession =
+            eventRepository.loadEvents(currentSession.id).filterIsInstance<PersonCreationEvent>().toList()
         // If a personCreationEvent is already in the current session,
         // we don' want to add it again (the capture steps would still be the same)
         if (personCreationEventInSession.isEmpty()) {
@@ -39,7 +42,7 @@ class PersonCreationEventHelperImpl(val eventRepository: EventRepository,
             it.captureResult.mapNotNull { captureResult ->
                 val fingerId = captureResult.identifier
                 captureResult.sample?.let { sample ->
-                    FingerprintSample(fingerId, sample.template, sample.templateQualityScore)
+                    FingerprintSample(fingerId, sample.template, sample.templateQualityScore, sample.format)
                 }
             }
         }.flatten()
@@ -48,19 +51,32 @@ class PersonCreationEventHelperImpl(val eventRepository: EventRepository,
     private fun extractFaceSamples(responses: List<FaceCaptureResponse>) =
         responses.map {
             it.capturingResult.mapNotNull { captureResult ->
-                captureResult.result?.let {
-                    FaceSample(it.template)
+                captureResult.result?.let { sample ->
+                    FaceSample(sample.template, sample.format)
                 }
             }
         }.flatten()
 
-    private suspend fun addPersonCreationEvent(fingerprintSamples: List<FingerprintSample>,
-                                               faceSamples: List<FaceSample>) {
+    private suspend fun addPersonCreationEvent(
+        fingerprintSamples: List<FingerprintSample>,
+        faceSamples: List<FaceSample>
+    ) {
         val currentCaptureSessionEvent = eventRepository.getCurrentCaptureSessionEvent()
-        val fingerprintCaptureEvents = eventRepository.loadEvents(currentCaptureSessionEvent.id).filterIsInstance<FingerprintCaptureEvent>().toList()
-        val faceCaptureEvents = eventRepository.loadEvents(currentCaptureSessionEvent.id).filterIsInstance<FaceCaptureEvent>().toList()
+        val fingerprintCaptureEvents =
+            eventRepository.loadEvents(currentCaptureSessionEvent.id).filterIsInstance<FingerprintCaptureEvent>()
+                .toList()
+        val faceCaptureEvents =
+            eventRepository.loadEvents(currentCaptureSessionEvent.id).filterIsInstance<FaceCaptureEvent>().toList()
 
-        eventRepository.addEventToCurrentSession(build(timeHelper, faceCaptureEvents, fingerprintCaptureEvents, faceSamples, fingerprintSamples))
+        eventRepository.addEventToCurrentSession(
+            build(
+                timeHelper,
+                faceCaptureEvents,
+                fingerprintCaptureEvents,
+                faceSamples,
+                fingerprintSamples
+            )
+        )
     }
 
     fun build(
---
+++ moduleapi/src/main/java/com/simprints/moduleapi/fingerprint/IFingerprintTemplateFormat.kt
@@ -0,0 +1,6 @@
+package com.simprints.moduleapi.fingerprint
+
+enum class IFingerprintTemplateFormat {
+    ISO_19794_2,
+    NEC
+}
