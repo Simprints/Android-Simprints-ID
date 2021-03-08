aa38578d2861e364a3f7448c97d53b761f2421ab
Tsuharesu <tsuharesu01@gmail.com>
2021-03-08 20:24:24 +0000

    feat: migrate realm to new schema version
    Former-commit-id: f8d4e0ed6fd166a07d427257bc9cf3af7a81c427 [formerly 65560973c00d60c0ad77be1d8d2c46272fef770d]
    Former-commit-id: a3a51bee465d307f6abc08fdc3e79a3371ccc378

---
+++ id/src/main/java/com/simprints/id/data/db/subject/migration/oldschemas/SubjectsSchemaV11.kt
@@ -0,0 +1,8 @@
+package com.simprints.id.data.db.subject.migration.oldschemas
+
+/**
+ * Should add field format to new DbFaceSample and DbFingerprintSample
+ */
+object SubjectsSchemaV11 {
+    const val FIELD_FORMAT: String = "format"
+}
