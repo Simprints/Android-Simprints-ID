1c2b7bee3c830f6419cc4ce18ad135dca8bea78f
Ebi Igweze <ebi.igweze@gmail.com>
2021-03-11 23:09:06 +0100

    adds unit tests for displaying sync-card in DashboardActivity
    Former-commit-id: e2fc5e631fbc3fd321964e782b2d037ade4d226b [formerly d6896f6350e7d16980ca2a0c0f382c82429be5c8]
    Former-commit-id: 8ddd3163e89b416040ae31c3083e56a6fb46ca7b

--- id/src/androidTest/java/com/simprints/id/activities/dashboard/DashboardActivityAndroidTest.kt
+++ id/src/androidTest/java/com/simprints/id/activities/dashboard/DashboardActivityAndroidTest.kt
@@ -1,28 +1,26 @@
 package com.simprints.id.activities.dashboard
 
-import androidx.core.view.isVisible
+import androidx.lifecycle.MutableLiveData
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
-import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import br.com.concretesolutions.kappuccino.assertions.VisibilityAssertions.displayed
 import com.simprints.id.Application
 import com.simprints.id.R
+import com.simprints.id.activities.dashboard.cards.sync.DashboardSyncCardState
 import com.simprints.id.commontesttools.di.TestPreferencesModule
 import com.simprints.id.commontesttools.di.TestViewModelModule
 import com.simprints.id.data.prefs.settings.SettingsPreferencesManager
 import com.simprints.id.domain.SyncDestinationSetting
 import com.simprints.id.testtools.AndroidTestConfig
-import com.simprints.testtools.android.getCurrentActivity
+import com.simprints.testtools.android.waitOnUi
 import com.simprints.testtools.common.di.DependencyRule
-import io.mockk.MockKAnnotations
-import io.mockk.every
+import io.mockk.*
 import io.mockk.impl.annotations.MockK
-import io.mockk.mockk
 import org.hamcrest.CoreMatchers.not
 import org.junit.Before
 import org.junit.Test
@@ -34,6 +32,9 @@ class DashboardActivityAndroidTest {
 
     @MockK
     lateinit var mockViewModelFactory: DashboardViewModelFactory
+
+    @MockK
+    lateinit var mockViewModel: DashboardViewModel
 
     private val app = ApplicationProvider.getApplicationContext<Application>()
 
@@ -48,18 +49,18 @@ class DashboardActivityAndroidTest {
         MockKAnnotations.init(this, relaxed = true)
         every {
             mockViewModelFactory.create<DashboardViewModel>(any())
-        } returns mockk(relaxed = true)
+        } returns mockViewModel
 
         app.component = AndroidTestConfig(this, preferencesModule = preferencesModule)
             .componentBuilder()
             .viewModelModule(buildViewModelModule())
             .build()
-
-        ActivityScenario.launch(DashboardActivity::class.java)
     }
 
     @Test
     fun withConsentRequiredDisabled_shouldNotShowPrivacyNoticeMenuItem() {
+        ActivityScenario.launch(DashboardActivity::class.java)
+
         every { mockPreferencesManager.consentRequired } returns false
 
         openActionBarOverflowOrOptionsMenu(app.applicationContext)
@@ -69,6 +70,8 @@ class DashboardActivityAndroidTest {
 
     @Test
     fun withConsentRequiredEnabled_shouldShowPrivacyNoticeMenuItem() {
+        ActivityScenario.launch(DashboardActivity::class.java)
+
         every { mockPreferencesManager.consentRequired } returns true
 
         openActionBarOverflowOrOptionsMenu(app.applicationContext)
@@ -78,6 +81,43 @@ class DashboardActivityAndroidTest {
         }
     }
 
+    @Test
+    fun withOnlyCommCareAsSyncLocation_shouldNotTriggerSyncAndNotShowSyncCard() {
+
+        every { mockPreferencesManager.syncDestinationSettings } returns
+            listOf(SyncDestinationSetting.COMMCARE)
+
+        ActivityScenario.launch(DashboardActivity::class.java)
+
+        waitOnUi(1000)
+
+        coVerify(exactly = 0) {  mockViewModel.syncIfRequired() }
+
+        onView(withId(R.id.dashboard_sync_card)).check(matches(not(isDisplayed())))
+    }
+
+    @Test
+    fun withSimprintsAsSyncLocation_shouldTriggerSyncAndShowSyncCard() {
+        every { mockPreferencesManager.syncDestinationSettings } returns
+            listOf(SyncDestinationSetting.COMMCARE, SyncDestinationSetting.SIMPRINTS)
+
+        val mockSyncStateLiveData = MutableLiveData<DashboardSyncCardState>()
+
+        every { mockViewModel.syncCardStateLiveData } returns mockSyncStateLiveData
+
+        coEvery { mockViewModel.syncIfRequired() } answers  {
+            val connectingState = DashboardSyncCardState.SyncConnecting(null, 0, null)
+            mockSyncStateLiveData.postValue(connectingState)
+        }
+
+        ActivityScenario.launch(DashboardActivity::class.java)
+
+        waitOnUi(1000)
+
+        coVerify(exactly = 1) {  mockViewModel.syncIfRequired() }
+
+        onView(withId(R.id.dashboard_sync_card)).check(matches(isDisplayed()))
+    }
 
     private fun buildViewModelModule() = TestViewModelModule(
         dashboardViewModelFactoryRule = DependencyRule.ReplaceRule {
