bcccecbb102c2126a579f75d0a950a430bb22a41
Tristram Norman <tristram@simprints.com>
2021-03-18 19:18:34 +0000

    remove flavors use build config
    Former-commit-id: 3738a981ea81cf6ac14191deb8e055254edbffcc [formerly 99d3164e47437091688b660747a463bb5c044e95]
    Former-commit-id: e3837c2523c4674b86be56b56e2f51d27e7f918d

--- id/src/main/java/com/simprints/id/tools/logging/TimberFileLoggingConfigHelper.kt
+++ id/src/main/java/com/simprints/id/tools/logging/TimberFileLoggingConfigHelper.kt
@@ -2,8 +2,7 @@ package com.simprints.id.tools.logging
 
 import timber.log.Timber
 
-class TimberFileLoggingConfigHelper : LoggingConfigHelper() {
-
+class TimberFileLoggingConfigHelper: LoggingConfigHelper() {
     override fun setUpLogging() {
         Timber.plant(FileLoggingTree())
         Timber.d("File logging set.")
