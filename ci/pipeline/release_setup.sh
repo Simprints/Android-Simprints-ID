784418554fa07fc1823bd8f140498b515a067708
Tristram Norman <tristram@simprints.com>
2021-03-19 14:40:12 +0000

    move signing props to gradle
    Former-commit-id: 7c93784105291e68cc7355446cba2e4b1599738c [formerly 4d16cb48516dd4b27988c207c8f4642eff11f7e8]
    Former-commit-id: bee90475c9fda65f919145c6390a27c204826cbb

--- ci/pipeline/release_setup.sh
+++ ci/pipeline/release_setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-echo "storeFile=android-signing-keystore.jks"
-echo "storePassword=${SIGNING_KEYSTORE_PASSWORD}" >>buildSrc/keystore.properties
-echo "keyAlias=${SIGNING_KEY_ALIAS}" >>buildSrc/keystore.properties
-echo "keyPassword=${SIGNING_KEY_PASSWORD}" >>buildSrc/keystore.properties
-
 echo "$SIGNING_JKS_FILE" | base64 -di >android-signing-keystore.jks
 
-printf "ext { \n VERSION_CODE=%d as Integer \n VERSION_SUFFIX=%s as String \n DEBUGGABLE=false }" "${BITBUCKET_BUILD_NUMBER}" "\"${BITBUCKET_DEPLOYMENT_ENVIRONMENT}\"" >>buildSrc/build_properties.gradle
+printf "ext { \n
+store_file = \"$\rootDir/android-signing-keystore.jks\" \n
+store_password = %s \n
+key_alias=%s \n
+key_password=%s } " "\"${SIGNING_KEYSTORE_PASSWORD}\"" "\"${SIGNING_KEY_ALIAS}\"" "\"${SIGNING_KEY_PASSWORD}\"" >>buildSrc/keystore.properties
+
+printf "ext { \n
+VERSION_CODE=%d as Integer \n
+VERSION_SUFFIX=%s as String \n
+DEBUGGABLE=false }" "${BITBUCKET_BUILD_NUMBER}" "\"${BITBUCKET_DEPLOYMENT_ENVIRONMENT}\"" >>buildSrc/build_properties.gradle
