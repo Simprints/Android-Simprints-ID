32b4720789f1144df3ab4769cc892670461c263c
TristramN <tristram@simprints.com>
2021-03-19 14:43:27 +0000

    fix escape and file name
    Former-commit-id: a2c17ddeceec27ce649105b9bafa3d729759bb22 [formerly 770caf1b54118a7688e17c3732ff0efa3af4cf34]
    Former-commit-id: 28b6366c0b692f2591f78665d66b08f65cf1a2b0

--- ci/pipeline/release_setup.sh
+++ ci/pipeline/release_setup.sh
@@ -3,10 +3,10 @@
 echo "$SIGNING_JKS_FILE" | base64 -di >android-signing-keystore.jks
 
 printf "ext { \n
-store_file = \"$\rootDir/android-signing-keystore.jks\" \n
+store_file = \"\$rootDir/android-signing-keystore.jks\" \n
 store_password = %s \n
 key_alias=%s \n
-key_password=%s } " "\"${SIGNING_KEYSTORE_PASSWORD}\"" "\"${SIGNING_KEY_ALIAS}\"" "\"${SIGNING_KEY_PASSWORD}\"" >>buildSrc/keystore.properties
+key_password=%s } " "\"${SIGNING_KEYSTORE_PASSWORD}\"" "\"${SIGNING_KEY_ALIAS}\"" "\"${SIGNING_KEY_PASSWORD}\"" >>buildSrc/signing_properties.gradle
 
 printf "ext { \n
 VERSION_CODE=%d as Integer \n
