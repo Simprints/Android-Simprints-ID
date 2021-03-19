8be830e4c81a6d571f8adc6cbc0831ccf2d6986b
TristramN <tristram@simprints.com>
2021-03-19 02:01:59 +0000

    fix formatting
    Former-commit-id: d29f2297713aad57cbfebdfcb35a094656b9ec3d [formerly c597936a867c7652645d6798035c83cb8340e9b4]
    Former-commit-id: 7494ddc9d410113a7bd149d8390edb466c68bfde

--- ci/pipeline/release_setup.sh
+++ ci/pipeline/release_setup.sh
@@ -7,4 +7,4 @@ echo "keyPassword=${SIGNING_KEY_PASSWORD}" >>buildSrc/keystore.properties
 
 echo "$SIGNING_JKS_FILE" | base64 -di >android-signing-keystore.jks
 
-printf "ext { \n VERSION_CODE=%d as Integer \n VERSION_SUFFIX=%s as String \n DEBUGGABLE=false }" "${BITBUCKET_BUILD_NUMBER}" "${BITBUCKET_DEPLOYMENT_ENVIRONMENT}" >>buildSrc/build_properties.gradle
+printf "ext { \n VERSION_CODE=%d as Integer \n VERSION_SUFFIX=%s as String \n DEBUGGABLE=false }" "${BITBUCKET_BUILD_NUMBER}" "\"${BITBUCKET_DEPLOYMENT_ENVIRONMENT}\"" >>buildSrc/build_properties.gradle
