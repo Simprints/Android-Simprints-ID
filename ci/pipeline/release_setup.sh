d1d4a1532ff615d99b55eb081a0135ca090dca5d
Tristram Norman <tristram@simprints.com>
2021-03-19 15:00:17 +0000

    move the safetynet key to the new format
    Former-commit-id: b957d61607482055749929e7692bedf5815da65e [formerly f1ec4564a252f47ead086abd46987220c6083999]
    Former-commit-id: acbdc4599bc6ea8c9bcf730a376385022658cac8

--- ci/pipeline/release_setup.sh
+++ ci/pipeline/release_setup.sh
@@ -6,9 +6,12 @@ printf "ext { \n
 store_file = \"\$rootDir/android-signing-keystore.jks\" \n
 store_password = %s \n
 key_alias=%s \n
-key_password=%s } " "\"${SIGNING_KEYSTORE_PASSWORD}\"" "\"${SIGNING_KEY_ALIAS}\"" "\"${SIGNING_KEY_PASSWORD}\"" >>buildSrc/signing_properties.gradle
+key_password=%s
+}" "\"${SIGNING_KEYSTORE_PASSWORD}\"" "\"${SIGNING_KEY_ALIAS}\"" "\"${SIGNING_KEY_PASSWORD}\"" >>buildSrc/signing_properties.gradle
 
 printf "ext { \n
 VERSION_CODE=%d as Integer \n
 VERSION_SUFFIX=%s as String \n
-DEBUGGABLE=false }" "${BITBUCKET_BUILD_NUMBER}" "\"${BITBUCKET_DEPLOYMENT_ENVIRONMENT}\"" >>buildSrc/build_properties.gradle
+DEBUGGABLE=false \n
+SAFETYNET_KEY=%s
+}" "${BITBUCKET_BUILD_NUMBER}" "\"${BITBUCKET_DEPLOYMENT_ENVIRONMENT}\"" "\"${SAFETYNET_KEY}\"" >>buildSrc/build_properties.gradle
