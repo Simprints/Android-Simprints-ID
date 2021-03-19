a05ad7b692acd84a3e84897b257829a2cbe149aa
Tristram Norman <tristram@simprints.com>
2021-03-19 15:43:52 +0000

    we want the staging key local so we can test pre_builds
    Former-commit-id: dfc08709a3d36b898b1b86bb10cc995f85e059ea [formerly 01c9fcb56f17e30ecc003711e5af7b76bb004a85]
    Former-commit-id: 157cad27f0a67ce78d587cc396aca7a07f7acac8

--- ci/pipeline/release_setup.sh
+++ ci/pipeline/release_setup.sh
@@ -13,5 +13,5 @@ printf "ext { \n
 VERSION_CODE=%d as Integer \n
 VERSION_SUFFIX=%s as String \n
 DEBUGGABLE=false \n
-SAFETYNET_KEY=%s
-}" "${BITBUCKET_BUILD_NUMBER}" "\"${BITBUCKET_DEPLOYMENT_ENVIRONMENT}\"" "\"${SAFETYNET_KEY}\"" >>buildSrc/build_properties.gradle
+RELEASE_SAFETYNET_KEY=%s
+}" "${BITBUCKET_BUILD_NUMBER}" "\"${BITBUCKET_DEPLOYMENT_ENVIRONMENT}\"" "\"${RELEASE_SAFETYNET_KEY}\"" >>buildSrc/build_properties.gradle
