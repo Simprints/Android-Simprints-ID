05201363de9624ac6c9d649bbef3c152b7926125
Ebi Igweze <ebi.igweze@gmail.com>
2021-04-09 00:38:32 +0100

    updates parcelize plugin for fingerprint requests & response data classes in id module
    Former-commit-id: 8717c2ab45e5e40dde8255871fb3f2a5abdfec4c [formerly a253d95cb948de6da3ba7c1a2215fc1815105074]
    Former-commit-id: b725f707fd6c10f0d0c6415079a36d129b09a1fe

--- id/src/main/java/com/simprints/id/domain/moduleapi/fingerprint/responses/FingerprintMatchResponse.kt
+++ id/src/main/java/com/simprints/id/domain/moduleapi/fingerprint/responses/FingerprintMatchResponse.kt
@@ -3,8 +3,8 @@ package com.simprints.id.domain.moduleapi.fingerprint.responses
 import com.simprints.id.domain.moduleapi.fingerprint.responses.entities.FingerprintMatchResult
 import com.simprints.id.domain.moduleapi.fingerprint.responses.entities.fromModuleApiToDomain
 import com.simprints.moduleapi.fingerprint.responses.IFingerprintMatchResponse
-import kotlinx.android.parcel.IgnoredOnParcel
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class FingerprintMatchResponse(
