36f559c1b80b8f73a2622d66e9541bab26ec9c55
Tsuharesu <tsuharesu01@gmail.com>
2021-04-08 16:29:01 +0100

    fix: move androidTest to correct folder
    Former-commit-id: 96badb01246dfd05cfb27fe0b84b4a6e3cd458f9 [formerly 5d30fcf2fbf792b09637ec779e3dadec2a698141]
    Former-commit-id: 1fd7c8deec67ea90a3be6edc9293b945eceab2a4

--- id/src/androidTest/java/com/simprints/id/data/license/local/LicenseLocalDataSourceImplTest.kt
+++ id/src/androidTest/java/com/simprints/id/data/license/local/LicenseLocalDataSourceImplTest.kt
@@ -1,21 +1,17 @@
-package com.simprints.face.license.data.local
+package com.simprints.id.data.license.local
 
 import android.app.Application
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import com.google.common.truth.Truth.assertThat
-import com.simprints.id.data.license.local.LicenseLocalDataSourceImpl
 import io.mockk.every
 import io.mockk.spyk
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
 import java.io.File
 import java.util.*
 
 @SmallTest
-@RunWith(AndroidJUnit4::class)
 class LicenseLocalDataSourceImplTest {
     private val app = ApplicationProvider.getApplicationContext<Application>()
     private val filesFolder = "${app.filesDir}/${LicenseLocalDataSource.LICENSES_FOLDER}"
