7720f94777b8bd8157fa879612f0e0b17eace800
Ebi Igweze <ebi.igweze@gmail.com>
2021-04-09 00:17:25 +0100

    updates db data classes in id module
    Former-commit-id: d74e0ce59dd37d1256c12765878b54d03c386aae [formerly 2edde4fc8152c7b200e16687b882e8e805d01d6e]
    Former-commit-id: 6adf31191dc7b05ff5a2759940b88b452ebb4d63

--- id/src/main/java/com/simprints/id/data/db/subject/domain/FingerprintIdentity.kt
+++ id/src/main/java/com/simprints/id/data/db/subject/domain/FingerprintIdentity.kt
@@ -1,7 +1,7 @@
 package com.simprints.id.data.db.subject.domain
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 class FingerprintIdentity(val patientId: String,
