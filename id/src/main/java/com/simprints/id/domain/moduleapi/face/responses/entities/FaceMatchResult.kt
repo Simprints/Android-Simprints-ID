d936d21cafa860f2eb00e3aa4479afb4cac7be10
Ebi Igweze <ebi.igweze@gmail.com>
2021-04-09 00:27:14 +0100

    updates parcelize plugin for face requests & response data classes in id module
    Former-commit-id: 6d9bcc67f6519fcbe4e9d497b2bd41321b57b656 [formerly 43e6fd2c66e1fc98079df10f75889e969f558ae6]
    Former-commit-id: 047c62fc98e0ca13215065f1776f082a99c17f9f

--- id/src/main/java/com/simprints/id/domain/moduleapi/face/responses/entities/FaceMatchResult.kt
+++ id/src/main/java/com/simprints/id/domain/moduleapi/face/responses/entities/FaceMatchResult.kt
@@ -2,7 +2,7 @@ package com.simprints.id.domain.moduleapi.face.responses.entities
 
 import android.os.Parcelable
 import com.simprints.moduleapi.face.responses.IFaceMatchResult
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class FaceMatchResult(val guidFound: String,
