f16f6bcfe6aec334ea9972a262cc3115d52ccc09
Tsuharesu <tsuharesu01@gmail.com>
2021-04-08 16:13:17 +0100

    refactor: extract getting license to a method
    Former-commit-id: 48c15d7ff7296b0ed0b467eb01971d58fabfbdd0 [formerly fc5df5e6309cfd7c5487b00eac0cd31bfbe12d7a]
    Former-commit-id: 751695fb31faf26e1dcdc257576db46992f2aa63

--- id/src/main/java/com/simprints/id/data/license/remote/ApiLicense.kt
+++ id/src/main/java/com/simprints/id/data/license/remote/ApiLicense.kt
@@ -1,9 +1,29 @@
 package com.simprints.id.data.license.remote
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.simprints.id.data.license.repository.LicenseVendor
 
-data class ApiLicense(@JsonProperty("RANK_ONE_FACE") val rankOneLicense: RankOneLicense?)
+/**
+ * ApiLicense only populates some fields, based on which vendor was asked when retrieving the license.
+ */
+data class ApiLicense(@JsonProperty("RANK_ONE_FACE") val rankOneLicense: RankOneLicense?) {
+
+    /**
+     * This method gets the correct license data based on which vendor is passed to it.
+     * If the license doesn't contain data for that vendor, returns an empty string.
+     */
+    fun getLicenseBasedOnVendor(licenseVendor: LicenseVendor): String =
+        when (licenseVendor) {
+            LicenseVendor.RANK_ONE_FACE -> rankOneLicense?.data ?: ""
+        }
+}
 
 data class RankOneLicense(val vendor: String, val expiration: String, val data: String)
 
+/**
+ * BFSID returns an error in the following format:
+ * ```
+ * { "error": "001" }
+ * ```
+ */
 data class ApiLicenseError(val error: String)
