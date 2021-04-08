637f5e1dc9af3a71d192481a7e7c5ddd4d983811
Ebi Igweze <ebi.igweze@gmail.com>
2021-04-08 23:52:32 +0100

    updates parcelize plugin for activity request & response data classes in id module
    Former-commit-id: 5b4385d5346490a064793af233e30a878bddd906 [formerly 289f423d123caae97dbc9f5f34c0347e8b7b9d5d]
    Former-commit-id: 8143b5d9f60e761fa0b1209d4d9e57efe1c59d2d

--- id/src/main/java/com/simprints/id/activities/alert/request/AlertActRequest.kt
+++ id/src/main/java/com/simprints/id/activities/alert/request/AlertActRequest.kt
@@ -2,7 +2,7 @@ package com.simprints.id.activities.alert.request
 
 import android.os.Parcelable
 import com.simprints.id.domain.alert.AlertType
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 class AlertActRequest(val alertType: AlertType): Parcelable {
