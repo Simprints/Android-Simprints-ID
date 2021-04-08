afe7a19caf4cc8feeb5e3673edd72925eab3988e
Ebi Igweze <ebi.igweze@gmail.com>
2021-04-09 00:17:25 +0100

    updates db data classes in id module
    Former-commit-id: d74e0ce59dd37d1256c12765878b54d03c386aae [formerly 2edde4fc8152c7b200e16687b882e8e805d01d6e]
    Former-commit-id: 6adf31191dc7b05ff5a2759940b88b452ebb4d63

--- id/src/main/java/com/simprints/id/data/db/subject/domain/FaceIdentity.kt
+++ id/src/main/java/com/simprints/id/data/db/subject/domain/FaceIdentity.kt
@@ -1,8 +1,7 @@
 package com.simprints.id.data.db.subject.domain
 
 import android.os.Parcelable
-import com.simprints.id.data.db.subject.domain.FaceSample
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 class FaceIdentity(val personId: String, val faces: List<FaceSample>) : Parcelable
