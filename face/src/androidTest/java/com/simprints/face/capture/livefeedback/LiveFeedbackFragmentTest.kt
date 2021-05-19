cec65094856e81e74ba2da698358e9e325524192
Fbada006 <ferdinandkamata@yahoo.com>
2021-05-19 14:08:01 +0300

    add test for livefeedback
    Former-commit-id: 5ef09e106a6fe055e0a7ac979684ff6758ba208b [formerly 0a64f133a03cf325d922ac5ad81b70a142804b2a]
    Former-commit-id: a1db219c209e0ee58436080b979571461c462066

---
+++ face/src/androidTest/java/com/simprints/face/capture/livefeedback/LiveFeedbackFragmentTest.kt
@@ -0,0 +1,64 @@
+package com.simprints.face.capture.livefeedback
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.NavHostController
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.simprints.face.R
+import com.simprints.face.capture.FaceCaptureViewModel
+import io.mockk.mockk
+import org.hamcrest.CoreMatchers.allOf
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.core.context.loadKoinModules
+import org.koin.dsl.module
+import org.koin.test.KoinTest
+
+@RunWith(AndroidJUnit4::class)
+class LiveFeedbackFragmentTest : KoinTest {
+
+    private val faceCaptureViewModel: FaceCaptureViewModel = mockk(relaxed = true)
+    private val liveFeedBackVm: LiveFeedbackFragmentViewModel = mockk(relaxed = true)
+
+    @Before
+    fun setUp() {
+        loadKoinModules(
+            module(override = true) {
+                viewModel { faceCaptureViewModel }
+                viewModel { liveFeedBackVm }
+            })
+    }
+
+    @Test
+    fun renameThisTest() {
+        val navController: NavHostController = TestNavHostController(
+            ApplicationProvider.getApplicationContext()
+        )
+        val liveFeedBackScenario =
+            launchFragmentInContainer<LiveFeedbackFragment>()
+
+        liveFeedBackScenario.onFragment { liveFeedbackFragment ->
+            navController.setGraph(R.navigation.capture_graph)
+            Navigation.setViewNavController(liveFeedbackFragment.requireView(), navController)
+        }
+
+        onView(
+            allOf(
+                withId(R.id.capture_feedback_txt_title),
+                withText(R.string.capture_title_previewing)
+            )
+        )
+            .check(matches(isDisplayed()))
+
+    }
+}
+
