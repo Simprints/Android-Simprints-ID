1882f789914ad973914bcdeeeb540e240787d822
Fbada006 <ferdinandkamata@yahoo.com>
2021-06-29 23:11:37 +0300

    grant permissions in test
    Former-commit-id: 7a206af3f4e2756555db007e4bba2461f69d3ae8 [formerly fe0d7db8f0a707a1b2620e10e7cf212ea57c0472]
    Former-commit-id: d47b18aa26fe7c1771a93cb3b5d8fb35c56ca1e2

--- face/src/androidTest/java/com/simprints/face/capture/livefeedback/LiveFeedbackFragmentTest.kt
+++ face/src/androidTest/java/com/simprints/face/capture/livefeedback/LiveFeedbackFragmentTest.kt
@@ -11,11 +11,13 @@ import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.GrantPermissionRule
 import com.simprints.face.R
 import com.simprints.face.capture.FaceCaptureViewModel
 import io.mockk.mockk
 import org.hamcrest.CoreMatchers.allOf
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.android.viewmodel.dsl.viewModel
@@ -26,6 +28,12 @@ import org.koin.test.KoinTest
 @RunWith(AndroidJUnit4::class)
 class LiveFeedbackFragmentTest : KoinTest {
 
+    @Rule
+    @JvmField
+    val grantPermissions: GrantPermissionRule = GrantPermissionRule.grant(
+        android.Manifest.permission.ACCESS_FINE_LOCATION,
+        android.Manifest.permission.ACCESS_COARSE_LOCATION
+    )
     private val faceCaptureViewModel: FaceCaptureViewModel = mockk(relaxed = true)
     private val liveFeedBackVm: LiveFeedbackFragmentViewModel = mockk(relaxed = true)
 
@@ -44,7 +52,7 @@ class LiveFeedbackFragmentTest : KoinTest {
             ApplicationProvider.getApplicationContext()
         )
         val liveFeedBackScenario =
-            launchFragmentInContainer<LiveFeedbackFragment>()
+            launchFragmentInContainer<LiveFeedbackFragment>(themeResId = com.simprints.id.R.style.AppTheme)
 
         liveFeedBackScenario.onFragment { liveFeedbackFragment ->
             navController.setGraph(R.navigation.capture_graph)
