2f5ee0eed3d9ec538c115cf35d6a4dce41d669d9
Fbada006 <ferdinandkamata@yahoo.com>
2021-07-07 10:29:38 +0300

    extract subject method as ext function
    Former-commit-id: d5ce962bfb496848603d19c03f00435d83aec8f5 [formerly 11867f172f473daf263a102c78a589014d7eca70]
    Former-commit-id: 5bece5737f0f44a1ae8c2b39cb27e88ad83c6575

--- id/src/main/java/com/simprints/id/data/db/subject/domain/Subject.kt
+++ id/src/main/java/com/simprints/id/data/db/subject/domain/Subject.kt
@@ -3,6 +3,10 @@ package com.simprints.id.data.db.subject.domain
 import android.os.Parcelable
 import com.simprints.core.domain.face.FaceSample
 import com.simprints.core.domain.fingerprint.FingerprintSample
+import com.simprints.core.domain.modality.Modality
+import com.simprints.core.domain.modality.toMode
+import com.simprints.core.tools.utils.EncodingUtils
+import com.simprints.eventsystem.event.domain.models.subject.EnrolmentRecordCreationEvent
 import kotlinx.parcelize.Parcelize
 import java.util.*
 
@@ -18,4 +22,25 @@ data class Subject(
     var faceSamples: List<FaceSample> = emptyList(),
 
     @Deprecated("See SubjectToEventDbMigrationManagerImpl doc")
-    val toSync: Boolean = false) : Parcelable
+    val toSync: Boolean = false
+) : Parcelable
+
+fun Subject.fromSubjectToEnrolmentCreationEvent(
+    now: Long,
+    modalities: List<Modality>,
+    encoder: EncodingUtils
+): EnrolmentRecordCreationEvent {
+    return EnrolmentRecordCreationEvent(
+        now,
+        subjectId,
+        projectId,
+        moduleId,
+        attendantId,
+        modalities.map { it.toMode() },
+        EnrolmentRecordCreationEvent.buildBiometricReferences(
+            fingerprintSamples,
+            faceSamples,
+            encoder
+        )
+    )
+}
