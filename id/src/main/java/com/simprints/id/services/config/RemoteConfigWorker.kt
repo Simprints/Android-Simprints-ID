791469ad51bf0ba54b64a8955948b441737210e4
Tristram <tristram@simprints.com>
2021-07-09 16:24:06 +0100

    add remote config worker and di
    Former-commit-id: 7680291da1658a234f950a9a4e708c9f83905215 [formerly db8e25f66438fcb9ee01ed21a8eb70f5a06cb725]
    Former-commit-id: f54a92e7fa169a64686232fdb04aeb7b21e6f7cb

---
+++ id/src/main/java/com/simprints/id/services/config/RemoteConfigWorker.kt
@@ -0,0 +1,61 @@
+package com.simprints.id.services.config
+
+import android.content.Context
+import androidx.work.WorkerParameters
+import com.fasterxml.jackson.databind.JsonNode
+import com.simprints.core.login.LoginInfoManager
+import com.simprints.core.tools.coroutines.DispatcherProvider
+import com.simprints.id.data.db.project.ProjectRepository
+import com.simprints.id.data.prefs.RemoteConfigWrapper
+import com.simprints.id.services.sync.events.common.SimCoroutineWorker
+import com.simprints.logging.Simber
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class RemoteConfigWorker(context: Context, params: WorkerParameters) : SimCoroutineWorker(context, params) {
+    override val tag: String = RemoteConfigWorker::class.java.simpleName
+
+    @Inject
+    lateinit var dispatcherProvider: DispatcherProvider
+
+    @Inject
+    lateinit var loginInfoManager: LoginInfoManager
+
+    @Inject
+    lateinit var projectRepository: ProjectRepository
+
+    @Inject
+    lateinit var remoteConfigWrapper: RemoteConfigWrapper
+
+    override suspend fun doWork(): Result {
+        getComponent<RemoteConfigWorker> { it.inject(this@RemoteConfigWorker) }
+
+        return withContext(dispatcherProvider.io()) {
+            try {
+                crashlyticsLog("Starting")
+                val projectId = loginInfoManager.getSignedInProjectIdOrEmpty()
+
+                // if the user is not signed in, we shouldn't try again
+                if (projectId.isEmpty()) {
+                    return@withContext success()
+                }
+
+                val settingsJson = fetchProjectRemoteConfigSettings(projectId)
+                storeProjectRemoteConfigSettings(settingsJson)
+
+                success(message = "Successfully updated configs")
+            } catch (t: Throwable) {
+                Simber.e(t)
+                retry(message = "Failed to update configs")
+            }
+        }
+    }
+
+    private suspend fun fetchProjectRemoteConfigSettings(projectId: String): JsonNode =
+        projectRepository.loadProjectRemoteConfigSettingsJsonString(projectId)
+
+    private fun storeProjectRemoteConfigSettings(settingsJson: JsonNode) {
+        val jsonString = settingsJson.toString()
+        remoteConfigWrapper.projectSettingsJsonString = jsonString
+    }
+}
