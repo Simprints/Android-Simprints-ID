7aea3aae59b263ad67de45d5c8e1b81959d570f1
Ebi Igweze <ebi.igweze@gmail.com>
2021-09-09 01:05:47 +0100

    updates espresso test in id module to inject test coroutine dispatchers
    Former-commit-id: b60310b069863641f84a03d76ef322fc6ad752b3 [formerly 11139e58a803ccb23fa21b1d20e499472201ee97]
    Former-commit-id: a12d83a7a389b7ea716fd26c062eb5d1da2a4366

--- id/src/androidTest/java/com/simprints/id/activities/login/LoginActivityAndroidTest.kt
+++ id/src/androidTest/java/com/simprints/id/activities/login/LoginActivityAndroidTest.kt
@@ -2,6 +2,8 @@ package com.simprints.id.activities.login
 
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.intent.Intents
+import com.simprints.core.tools.coroutines.DefaultDispatcherProvider
+import com.simprints.core.tools.coroutines.DispatcherProvider
 import com.simprints.eventsystem.event.domain.models.AuthenticationEvent.AuthenticationPayload
 import com.simprints.eventsystem.event.domain.models.AuthenticationEvent.AuthenticationPayload.Result.AUTHENTICATED
 import com.simprints.eventsystem.event.domain.models.AuthenticationEvent.AuthenticationPayload.Result.BAD_CREDENTIALS
@@ -18,12 +20,15 @@ import com.simprints.id.commontesttools.di.TestSecurityModule
 import com.simprints.id.commontesttools.di.TestViewModelModule
 import com.simprints.id.secure.AuthenticationHelper
 import com.simprints.id.testtools.AndroidTestConfig
+import com.simprints.testtools.common.coroutines.TestCoroutineRule
 import com.simprints.testtools.common.di.DependencyRule
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.CoroutineDispatcher
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import javax.inject.Inject
 
@@ -35,6 +40,10 @@ class LoginActivityAndroidTest {
     @MockK
     lateinit var mockLoginActivityHelper: LoginActivityHelper
 
+//    @get:Rule
+//    val testCoroutineRule = TestCoroutineRule()
+
+    private val testDispatcherProvider = DefaultDispatcherProvider()
     private val app = ApplicationProvider.getApplicationContext<Application>()
 
     private val appModule by lazy {
@@ -53,7 +62,7 @@ class LoginActivityAndroidTest {
     private val viewModelModule by lazy {
         TestViewModelModule(
             loginViewModelFactoryRule = DependencyRule.ReplaceRule {
-                LoginViewModelFactory(mockAuthenticationHelper)
+                LoginViewModelFactory(mockAuthenticationHelper, testDispatcherProvider)
             }
         )
     }
