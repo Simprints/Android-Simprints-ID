dcceee0d8f57b8a4132540052bb5db418afbfa04
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2021-10-21 13:41:48 +0300

    add tests for module selection functionality
    Former-commit-id: 7c8ea4f9c4ef5a5d23609ed85a4b66503377f6d4 [formerly 6945201087124f6c629d00dbd0c136d56cbcc887]
    Former-commit-id: 4b853c8b614f415944e1d68508dd738453ef42e2

--- id/src/test/java/com/simprints/id/activities/settings/fragments/moduleselection/ModuleSelectionFragmentTest.kt
+++ id/src/test/java/com/simprints/id/activities/settings/fragments/moduleselection/ModuleSelectionFragmentTest.kt
@@ -1,11 +1,19 @@
 package com.simprints.id.activities.settings.fragments.moduleselection
 
-import androidx.fragment.app.testing.launchFragment
+import androidx.core.content.res.ResourcesCompat
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.google.common.truth.Truth
 import com.simprints.id.R
 import com.simprints.id.activities.BaseActivityTest
+import com.simprints.id.activities.settings.ModuleSelectionActivity
 import com.simprints.id.testtools.TestApplication
+import com.simprints.testtools.unit.robolectric.ShadowAndroidXMultiDex
+import com.simprints.testtools.unit.robolectric.createAndStartActivity
+import io.mockk.every
+import io.mockk.mockkStatic
+import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
 import org.junit.Test
@@ -14,7 +22,11 @@ import org.robolectric.annotation.Config
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
-@Config(application = TestApplication::class)
+@Config(
+    instrumentedPackages = ["androidx.loader.content"],
+    application = TestApplication::class,
+    shadows = [ShadowAndroidXMultiDex::class]
+)
 class ModuleSelectionFragmentTest : BaseActivityTest() {
 
     @Before
@@ -23,8 +35,29 @@ class ModuleSelectionFragmentTest : BaseActivityTest() {
     }
 
     @Test
-    fun test() {
-        launchFragment<ModuleSelectionFragment>(themeResId = R.style.AppTheme)
-        Truth.assertThat("as").isEqualTo("as")
+    fun onLaunchModuleSelectionFragmentConfirmSearchViewIsVisible() {
+        createAndStartActivity<ModuleSelectionActivity>()
+        verifyConfirmationUIVisibility(R.id.searchView, ViewMatchers.Visibility.VISIBLE)
+        verifyConfirmationUIVisibility(androidx.appcompat.R.id.search_src_text, ViewMatchers.Visibility.VISIBLE)
+    }
+
+    @Test
+    fun onlaunchFragmentAssertGetFontIsCalled() {
+        mockkStatic(ResourcesCompat::class)
+        createAndStartActivity<ModuleSelectionActivity>()
+        verify { ResourcesCompat.getFont(any(), any()) }
+    }
+
+    @Test(expected = Test.None::class)
+    fun onlaunchFragmentThrowGetFontExceptionContinuesExecution() {
+        mockkStatic(ResourcesCompat::class)
+        createAndStartActivity<ModuleSelectionActivity>()
+        every { ResourcesCompat.getFont(any(), any()) } throws Exception()
+    }
+
+    private fun verifyConfirmationUIVisibility(id: Int, expectedVisibility: ViewMatchers.Visibility) {
+        Espresso.onView(ViewMatchers.withId(id)).check(
+            ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(expectedVisibility))
+        )
     }
 }
