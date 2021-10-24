5627943ef4f685180a2886bf74bf428beeceb997
Melad Gaied <melad@simprints.com>
2021-10-24 18:42:19 +0000

    Merged in bugfix/CORE-792-crashlytics-ScannerWrapperV1-errorHandler (pull request #387)
    CORE-792
    
    * Fix "exception could not be delivered to the consumer" issue
    
    
    Approved-by: Tristram Norman
    Approved-by: Ferdinand Kamata
    Approved-by: Ebi Igweze
    Former-commit-id: 309630db65ee26fadec8bf66b6def021ba783810 [formerly 6e92dc9699350ffd9ad9e0ba6df15a8ba163be83]
    Former-commit-id: 9f5d23f8f87860e2cbcb70dfe01042a70abc6663

---
+++ fingerprint/src/test/java/com/simprints/fingerprint/scanner/wrapper/ScannerWrapperV1Test.kt
@@ -0,0 +1,93 @@
+package com.simprints.fingerprint.scanner.wrapper
+
+
+import com.simprints.fingerprint.activities.collect.CollectFingerprintsViewModel
+import com.simprints.fingerprint.data.domain.fingerprint.CaptureFingerprintStrategy
+import com.simprints.fingerprint.scanner.exceptions.safe.NoFingerDetectedException
+import com.simprints.fingerprint.scanner.exceptions.safe.ScannerDisconnectedException
+import com.simprints.fingerprint.scanner.exceptions.safe.ScannerOperationInterruptedException
+import com.simprints.fingerprint.scanner.exceptions.unexpected.UnexpectedScannerException
+import com.simprints.fingerprintscanner.v1.SCANNER_ERROR
+import com.simprints.fingerprintscanner.v1.Scanner
+import com.simprints.fingerprintscanner.v1.ScannerCallback
+import io.mockk.*
+import io.mockk.impl.annotations.MockK
+import org.junit.Before
+import org.junit.Test
+
+class ScannerWrapperV1Test {
+
+    private lateinit var scannerWrapper: ScannerWrapperV1
+
+    @MockK
+    lateinit var scanner: Scanner
+    private var captureCallback = slot<ScannerCallback>()
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this, relaxed = true)
+        scannerWrapper = ScannerWrapperV1(scanner)
+    }
+
+    @Test
+    fun `test captureFingerprint success`() {
+        // Given
+        every { scanner.startContinuousCapture(any(), any(), capture(captureCallback)) } answers {
+            captureCallback.captured.onSuccess()
+        }
+        // When
+        val testObserver = getTestObserver()
+        // Then
+        testObserver.assertNoErrors()
+        testObserver.assertComplete()
+
+    }
+    @Test
+    fun `test captureFingerprint ScannerOperationInterruptedException`() {
+        // Given
+        mockScannerError(SCANNER_ERROR.INTERRUPTED)
+        // When
+        val testObserver = getTestObserver()
+        // Then
+        testObserver.assertError(ScannerOperationInterruptedException::class.java)
+    }
+    @Test
+    fun `test captureFingerprint NoFingerDetectedException`() {
+        // Given
+        mockScannerError(SCANNER_ERROR.UN20_SDK_ERROR)
+        // When
+        val testObserver = getTestObserver()
+        // Then
+        testObserver.assertError(NoFingerDetectedException::class.java)
+    }
+    @Test
+    fun `test captureFingerprint ScannerDisconnectedException`() {
+        // Given
+        mockScannerError(SCANNER_ERROR.INVALID_STATE)
+        // When
+        val testObserver = getTestObserver()
+        // Then
+        testObserver.assertError(ScannerDisconnectedException::class.java)
+    }
+    @Test
+    fun `test captureFingerprint UnexpectedScannerException`() {
+        // Given
+        mockScannerError(SCANNER_ERROR.BLUETOOTH_DISABLED)
+        // When
+        val testObserver = getTestObserver()
+        // Then
+        testObserver.assertError(UnexpectedScannerException::class.java)
+    }
+
+    private fun mockScannerError(scannerError: SCANNER_ERROR) {
+        every { scanner.startContinuousCapture(any(), any(), capture(captureCallback)) } answers {
+            captureCallback.captured.onFailure(scannerError)
+        }
+    }
+
+    private fun getTestObserver() = scannerWrapper.captureFingerprint(
+        CaptureFingerprintStrategy.SECUGEN_ISO_1000_DPI,
+        CollectFingerprintsViewModel.scanningTimeoutMs.toInt(),
+        60
+    ).test()
+}
