c67cecdfe52da5186e95a57df2afeaf40184dc0b
Ebi Igweze <ebi.igweze@gmail.com>
2021-12-16 10:53:59 +0100

    prevents logging of duplicate guidSelectEvent on crashlytics
    Former-commit-id: c4f54f076ab3196479524c8dc265a20155ccfaa6 [formerly 1262d9349a38d2db093a6ac05d0ec5c13d6e7aa5]
    Former-commit-id: 3ab1d21efee4024e425c840e075abe0f39c0d25f

--- eventsystem/src/main/java/com/simprints/eventsystem/exceptions/validator/GuidSelectEventValidatorException.kt
+++ eventsystem/src/main/java/com/simprints/eventsystem/exceptions/validator/GuidSelectEventValidatorException.kt
@@ -3,3 +3,4 @@ package com.simprints.eventsystem.exceptions.validator
 import com.simprints.eventsystem.exceptions.SessionDataSourceException
 
 class GuidSelectEventValidatorException(message: String): SessionDataSourceException(message)
+class DuplicateGuidSelectEventValidatorException(message: String): SessionDataSourceException(message)
