f6e2a1b217570776f965cfd13666c7fc6fc09ec6
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-02-07 16:03:00 +0300

    add check for maintenance mode exception in event sync
    Former-commit-id: 5b9588adbb3ec42a6e17254d71e6dc2f78bd5b77 [formerly f4fc7eabaaf8eacc6aaa5554f96e80ce2f96fc69]
    Former-commit-id: adf9bddf54dc860d2b9214f5bcbb513af8549122

--- id/src/main/java/com/simprints/id/services/sync/events/master/internal/WorkInfo.ext.kt
+++ id/src/main/java/com/simprints/id/services/sync/events/master/internal/WorkInfo.ext.kt
@@ -3,6 +3,10 @@ package com.simprints.id.services.sync.events.master.internal
 import androidx.work.WorkInfo
 
 const val OUTPUT_FAILED_BECAUSE_CLOUD_INTEGRATION = "OUTPUT_FAILED_BECAUSE_CLOUD_INTEGRATION"
+const val OUTPUT_FAILED_BECAUSE_BACKEND_MAINTENANCE = "OUTPUT_FAILED_BECAUSE_BACKEND_MAINTENANCE"
 
 fun WorkInfo.didFailBecauseCloudIntegration(): Boolean =
     this.outputData.getBoolean(OUTPUT_FAILED_BECAUSE_CLOUD_INTEGRATION, false)
+
+fun WorkInfo.didFailBecauseBackendMaintenance(): Boolean =
+    this.outputData.getBoolean(OUTPUT_FAILED_BECAUSE_BACKEND_MAINTENANCE, false)
