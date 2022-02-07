696fcdb6895e649d77cf6f1c016b85af32dd6b80
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-02-07 10:55:09 +0300

    add check for maintenance mode exception in long consent download
    Former-commit-id: 31aebbcb8f79de8fceac6ff0f40e20ac15bcf9c5 [formerly e038b50697e22591bb1c42906469c1886d5e257c]
    Former-commit-id: 2dda8c63600a7ebed2159705022189aeddff74c9

--- id/src/main/java/com/simprints/id/data/consent/longconsent/LongConsentFetchResult.kt
+++ id/src/main/java/com/simprints/id/data/consent/longconsent/LongConsentFetchResult.kt
@@ -2,11 +2,16 @@ package com.simprints.id.data.consent.longconsent
 
 sealed class LongConsentFetchResult(open val language: String) {
 
-    data class Succeed(override val language: String,
-                       val consent: String) : LongConsentFetchResult(language)
+    data class Succeed(
+        override val language: String,
+        val consent: String
+    ) : LongConsentFetchResult(language)
 
-    data class Failed(override val language: String,
-                      val error: Throwable) : LongConsentFetchResult(language)
+    data class Failed(
+        override val language: String,
+        val error: Throwable,
+        val isMaintenanceMode: Boolean = false
+    ) : LongConsentFetchResult(language)
 
     data class InProgress(override val language: String) : LongConsentFetchResult(language)
 }
