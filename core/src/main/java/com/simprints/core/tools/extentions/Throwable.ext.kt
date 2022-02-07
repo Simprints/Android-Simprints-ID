db810821041b59d228830641485f454a2f680262
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-02-07 10:55:48 +0300

    add check for maintenance mode exception in login
    Former-commit-id: 4cda647fad5df1b4338bd2401a5cfb4f4f368777 [formerly 2dfd9ce1aedb9c5745bf4d4f19ef95321eaf9f8f]
    Former-commit-id: a77705cd531dea41cb8101e45f6c0369d474ed91

--- core/src/main/java/com/simprints/core/tools/extentions/Throwable.ext.kt
+++ core/src/main/java/com/simprints/core/tools/extentions/Throwable.ext.kt
@@ -1,7 +1,21 @@
 package com.simprints.core.tools.extentions
 
 import com.simprints.core.network.NetworkConstants
+import org.json.JSONObject
 import retrofit2.HttpException
 
 fun Throwable.isCloudRecoverableIssue() =
     this is HttpException && NetworkConstants.httpCodesForRecoverableCloudIssues.contains(this.code())
+
+fun Throwable.isBackendMaitenanceException(): Boolean {
+    if (this is HttpException) {
+        val responseJson = response()?.errorBody().toString()
+        val jsonObect = JSONObject(responseJson.substring(responseJson.indexOf("{"), responseJson.lastIndexOf("}") + 1))
+        if (response()?.code() == 503 && jsonObect.has("error")) {
+            if (jsonObect.getString("error") == "002") {
+                return true
+            }
+        }
+    }
+    return false
+}
--- id/src/main/java/com/simprints/id/secure/AuthenticationHelperImpl.kt
+++ id/src/main/java/com/simprints/id/secure/AuthenticationHelperImpl.kt
@@ -7,8 +7,15 @@ import com.simprints.core.tools.time.TimeHelper
 import com.simprints.eventsystem.event.EventRepository
 import com.simprints.eventsystem.event.domain.models.AuthenticationEvent
 import com.simprints.eventsystem.event.domain.models.AuthenticationEvent.AuthenticationPayload.Result
-import com.simprints.eventsystem.event.domain.models.AuthenticationEvent.AuthenticationPayload.Result.*
+import com.simprints.eventsystem.event.domain.models.AuthenticationEvent.AuthenticationPayload.Result.BACKEND_MAINTENANCE
+import com.simprints.eventsystem.event.domain.models.AuthenticationEvent.AuthenticationPayload.Result.BAD_CREDENTIALS
+import com.simprints.eventsystem.event.domain.models.AuthenticationEvent.AuthenticationPayload.Result.OFFLINE
+import com.simprints.eventsystem.event.domain.models.AuthenticationEvent.AuthenticationPayload.Result.SAFETYNET_INVALID_CLAIM
+import com.simprints.eventsystem.event.domain.models.AuthenticationEvent.AuthenticationPayload.Result.SAFETYNET_UNAVAILABLE
+import com.simprints.eventsystem.event.domain.models.AuthenticationEvent.AuthenticationPayload.Result.TECHNICAL_FAILURE
+import com.simprints.eventsystem.event.domain.models.AuthenticationEvent.AuthenticationPayload.Result.UNKNOWN
 import com.simprints.eventsystem.event.domain.models.AuthenticationEvent.AuthenticationPayload.UserInfo
+import com.simprints.id.exceptions.safe.BackendMaintenanceException
 import com.simprints.id.exceptions.safe.SimprintsInternalServerException
 import com.simprints.id.exceptions.safe.secure.AuthRequestInvalidCredentialsException
 import com.simprints.id.exceptions.safe.secure.SafetyNetException
@@ -60,6 +67,7 @@ class AuthenticationHelperImpl(
             is IOException -> OFFLINE
             is AuthRequestInvalidCredentialsException -> BAD_CREDENTIALS
             is SimprintsInternalServerException -> TECHNICAL_FAILURE
+            is BackendMaintenanceException -> BACKEND_MAINTENANCE
             is SafetyNetException -> getSafetyNetExceptionReason(t.reason)
             else -> UNKNOWN
         }
