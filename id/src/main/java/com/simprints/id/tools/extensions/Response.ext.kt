6e26e4f5c70fe62fcce5286921a6a30856305b1b
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-02-10 13:35:09 +0300

    refactor auth functionality to get outage time
    Former-commit-id: 4959866a82f4e9d0ed4bf617be88ddf935810c4c [formerly a4d1785f9c27481a3a232db59d038bea3f2f55ea]
    Former-commit-id: 17abaf95c2e5931bd43b19be80dfa8ebbdd568fd

--- id/src/main/java/com/simprints/id/tools/extensions/Response.ext.kt
+++ id/src/main/java/com/simprints/id/tools/extensions/Response.ext.kt
@@ -1,19 +1,27 @@
 package com.simprints.id.tools.extensions
 
-import com.simprints.id.exceptions.safe.BackendMaintenanceException
-import com.simprints.id.exceptions.safe.SimprintsInternalServerException
 import org.json.JSONObject
+import retrofit2.HttpException
 import retrofit2.Response
 
-fun <T> Response<T>.checkForMaintenanceAndThrow(): Nothing {
+fun <T> Response<T>.isBackendMaitenanceException(): Boolean {
     if (this.code() == 503) {
         val responseJson = this.errorBody().toString()
         val jsonObect = JSONObject(responseJson.substring(responseJson.indexOf("{"), responseJson.lastIndexOf("}") + 1))
         if (jsonObect.has("error")) {
             if (jsonObect.getString("error") == "002") {
-                throw BackendMaintenanceException()
+                return true
             }
         }
     }
-    throw SimprintsInternalServerException()
+    return false
+}
+
+fun <T> Response<T>.getEstimatedOutage(): Long? {
+    return if (this.isBackendMaitenanceException()) {
+        val headers = this.headers()
+        headers.get("Retry-After")?.toLong()
+    } else {
+        null
+    }
 }
