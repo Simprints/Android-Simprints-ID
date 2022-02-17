0349f2c5be40218aa84dc23874cefab07a719ef3
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-02-17 16:43:43 +0300

    filter white space from reposen
    Former-commit-id: d8efda6f603320a22de87ae19638c8c08a3c80af [formerly 6dab56c0078e93d5d3e18b91deafb18dd0111782]
    Former-commit-id: 65b1bfb946467e793d34fa64736e251d509ba71a

--- id/src/main/java/com/simprints/id/tools/extensions/Response.ext.kt
+++ id/src/main/java/com/simprints/id/tools/extensions/Response.ext.kt
@@ -5,7 +5,7 @@ import retrofit2.Response
 
 fun <T> Response<T>.isBackendMaitenanceException(): Boolean {
     if (this.code() == 503) {
-        val jsonReseponse = this.errorBody()?.string()
+        val jsonReseponse = this.errorBody()?.string()?.filterNot { it.isWhitespace() }
         return jsonReseponse != null && jsonReseponse.contains(NetworkConstants.BACKEND_MAINTENANCE_ERROR_STRING)
     }
     return false
