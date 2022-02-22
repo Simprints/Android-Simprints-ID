fb56dc4da0f15a5bf41717e8a6442ddc0fb5614d
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-02-22 16:51:04 +0300

    add more test cases for time utils
    Former-commit-id: 16356a30f498cb9ae55f56c16b7ca61ab54fa0a5 [formerly afd662f2e3c1ca70289461065f1ebfac32fe5297]
    Former-commit-id: d900d78014198796f569d98c2baa1cc637159de7

--- id/src/test/java/com/simprints/id/tools/utils/TimeUtilKtTest.kt
+++ id/src/test/java/com/simprints/id/tools/utils/TimeUtilKtTest.kt
@@ -1,6 +1,7 @@
 package com.simprints.id.tools.utils
 
 import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 
 class TimeUtilKtTest {
@@ -9,13 +10,27 @@ class TimeUtilKtTest {
     fun `given a time in seconds with minutes and seconds format it properly`() {
         val formattedTime = getFormattedEstimatedOutage(180L)
 
-        Truth.assertThat(formattedTime).isEqualTo("03 minutes, 00 seconds")
+        assertThat(formattedTime).isEqualTo("03 minutes, 00 seconds")
+    }
+
+    @Test
+    fun `given a time in seconds with seconds format it properly`() {
+        val formattedTime = getFormattedEstimatedOutage(18L)
+
+        assertThat(formattedTime).isEqualTo("18 seconds")
+    }
+
+    @Test
+    fun `given 0 time in seconds format it properly`() {
+        val formattedTime = getFormattedEstimatedOutage(0L)
+
+       assertThat(formattedTime).isEqualTo("00 seconds")
     }
 
     @Test
     fun `given a time in seconds with hours minutes and seconds format it properly`() {
         val formattedTime = getFormattedEstimatedOutage(4000L)
 
-        Truth.assertThat(formattedTime).isEqualTo("01 hours, 06 minutes, 40 seconds")
+        assertThat(formattedTime).isEqualTo("01 hours, 06 minutes, 40 seconds")
     }
 }
