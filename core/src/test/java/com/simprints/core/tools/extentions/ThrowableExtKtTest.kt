cea11050b85870c4a27b95eb87487464121e29b9
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-02-23 13:36:28 +0300

    add tests for Throwable.ext.kt
    Former-commit-id: bb92abf62b5d70142a5402570682921e91520caf [formerly 028f7f2b62ef1098c2290c9485d2e4db1154d14c]
    Former-commit-id: 7a26145538af78923eb9d1817f4df4faf99855d7

--- core/src/test/java/com/simprints/core/tools/extentions/ThrowableExtKtTest.kt
+++ core/src/test/java/com/simprints/core/tools/extentions/ThrowableExtKtTest.kt
@@ -1,6 +1,9 @@
 package com.simprints.core.tools.extentions
 
 import com.google.common.truth.Truth.assertThat
+import io.mockk.every
+import io.mockk.mockk
+import okhttp3.Headers
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.ResponseBody.Companion.toResponseBody
 import org.junit.Test
@@ -9,6 +12,10 @@ import retrofit2.Response
 
 class ThrowableExtKtTest {
 
+    private val errorResponse =
+        "{\"some\":\"thing\"}"
+    private val backendMaintenanceErroresponse = "{\"error\":\"002\"}"
+
     @Test
     fun gettingNoBackendMaintenanceErrorReturnsFalse() {
         assertThat(Throwable().isBackendMaintenanceException()).isFalse()
@@ -16,12 +23,57 @@ class ThrowableExtKtTest {
 
     @Test
     fun gettingBackendMaintenanceErrorReturnsTrue() {
-        val errorResponse =
-            "{\"error\":\"002\"}"
-        val errorResponseBody = errorResponse.toResponseBody("application/json".toMediaTypeOrNull())
-        val mockResponse = Response.error<Any>(503, errorResponseBody)
-        val throwable = HttpException(mockResponse)
+        val throwable = createHttpException(503, backendMaintenanceErroresponse)
 
         assertThat(throwable.isBackendMaintenanceException()).isTrue()
+    }
+
+    @Test
+    fun gettingNoBackendErrorReturnsFalse() {
+        val throwable = createHttpException(500, errorResponse)
+
+        assertThat(throwable.isBackendMaintenanceException()).isFalse()
+    }
+
+    @Test
+    fun gettingNoHeadersReturnsNull() {
+        assertThat(Throwable().getEstimatedOutage()).isNull()
+    }
+
+    @Test
+    fun gettingBackendMaintenanceErrorWithNoHeaderReturns0() {
+        val throwable = createHttpException(503, backendMaintenanceErroresponse)
+        assertThat(throwable.getEstimatedOutage()).isEqualTo(0L)
+    }
+
+    @Test
+    fun gettingBackendErrorWithHeaderReturnsValidRetry() {
+        val exception: HttpException = mockk()
+
+        every {
+            exception.response()
+        } returns mockk()
+
+        every {
+            exception.response()?.code()
+        } returns 503
+
+        every {
+            exception.response()?.errorBody()?.string()
+        } returns backendMaintenanceErroresponse
+
+        every {
+            exception.response()?.headers()
+        } returns Headers.Builder()
+            .add("Retry-After", "600")
+            .build()
+
+        assertThat(exception.getEstimatedOutage()).isEqualTo(600L)
+    }
+
+    private fun createHttpException(code: Int, errorResponse: String): HttpException {
+        val errorResponseBody = errorResponse.toResponseBody("application/json".toMediaTypeOrNull())
+        val mockResponse = Response.error<Any>(code, errorResponseBody)
+        return HttpException(mockResponse)
     }
 }
