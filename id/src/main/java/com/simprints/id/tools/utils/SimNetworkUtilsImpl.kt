e2ae6d0c0ee29ed588d7947815a4673141651423
melad <melad@simprints.com>
2022-03-03 17:36:44 +0200

    Remove all usage of network info
    Approved-by: Tristram Norman
    Approved-by: Ferdinand Kamata
    Approved-by: Ebi Igweze
    
    Former-commit-id: 33698bea1f65e03d1b48058bf6e0c2b7cadf1f0f [formerly 9cf39e9187a3a2c2c6d6d2a9fee95640c6d67b7a]
    Former-commit-id: 269709edc76207e795f4ea46ef245d5f3f1e22e0

--- id/src/main/java/com/simprints/id/tools/utils/SimNetworkUtilsImpl.kt
+++ id/src/main/java/com/simprints/id/tools/utils/SimNetworkUtilsImpl.kt
@@ -4,22 +4,36 @@ import android.content.Context
 import android.net.ConnectivityManager
 import com.simprints.core.tools.utils.SimNetworkUtils
 import com.simprints.core.tools.utils.SimNetworkUtils.Connection
+import com.simprints.core.tools.utils.SimNetworkUtils.ConnectionType
+import android.net.NetworkCapabilities
+
 
 open class SimNetworkUtilsImpl(val ctx: Context) : SimNetworkUtils {
 
-    private val connectivityManager = ctx.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    private val connectivityManager =
+        ctx.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    private val activeNetwork = connectivityManager.activeNetwork
+    private val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
 
+    /**
+     * This property should contain two states for mobile and wifi connections
+     */
     override var connectionsStates: List<Connection> =
         arrayListOf<Connection>().apply {
-            try {
-                connectivityManager.allNetworkInfo.map {
-                    if (it.typeName != null && it.detailedState != null) {
-                        add(Connection(it.typeName, it.detailedState))
-                    }
-                }
-
-            } catch (e: Exception) {
-            }
+            add(createMobileDataConnection())
+            add(createWifiConnection())
         }
 
+    private fun createWifiConnection() =
+        Connection(ConnectionType.WIFI, networkStatus(NetworkCapabilities.TRANSPORT_WIFI))
+
+    private fun createMobileDataConnection() =
+        Connection(ConnectionType.MOBILE, networkStatus(NetworkCapabilities.TRANSPORT_CELLULAR))
+
+    private fun networkStatus(transportType: Int): SimNetworkUtils.ConnectionState =
+        if (networkCapabilities?.hasTransport(transportType) == true) {
+            SimNetworkUtils.ConnectionState.CONNECTED
+        } else {
+            SimNetworkUtils.ConnectionState.DISCONNECTED
+        }
 }
