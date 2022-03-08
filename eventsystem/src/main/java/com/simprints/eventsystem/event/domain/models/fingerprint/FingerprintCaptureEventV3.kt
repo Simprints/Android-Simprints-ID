3d4af65d4d7bc4dab45f3de9fe8e2e1412e0522e
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-03-08 11:56:59 +0300

    add fingerprint capture v3 and its payload
    Former-commit-id: 8e197c8c679c2e811ddabad522199de28ace9575 [formerly ed79c53637006d60c98e9e2659cbb5db55cb53ea]
    Former-commit-id: 76a4fc4c67854a76705db19fc4ea92ef05ec5b40

---
+++ eventsystem/src/main/java/com/simprints/eventsystem/event/domain/models/fingerprint/FingerprintCaptureEventV3.kt
@@ -0,0 +1,71 @@
+package com.simprints.eventsystem.event.domain.models.fingerprint
+
+import androidx.annotation.Keep
+import com.simprints.eventsystem.event.domain.models.Event
+import com.simprints.eventsystem.event.domain.models.EventLabels
+import com.simprints.eventsystem.event.domain.models.EventPayload
+import com.simprints.eventsystem.event.domain.models.EventType
+import com.simprints.eventsystem.event.domain.models.EventType.FINGERPRINT_CAPTURE_V3
+import com.simprints.moduleapi.fingerprint.IFingerIdentifier
+import java.util.UUID
+
+/*The naming here (3 instead of 2) is to keep in line with the backend, which is at V3 of this. See EVENT_VERSION in the companion
+* object, which also needed incrementing*/
+@Keep
+data class FingerprintCaptureEventV3(
+    override val id: String = UUID.randomUUID().toString(),
+    override var labels: EventLabels,
+    override val payload: FingerprintCapturePayloadV3,
+    override val type: EventType
+) : Event() {
+
+    constructor(
+        createdAt: Long,
+        endTime: Long,
+        finger: IFingerIdentifier,
+        qualityThreshold: Int,
+        result: FingerprintCapturePayloadV3.Result,
+        fingerprint: FingerprintCapturePayloadV3.Fingerprint?,
+        id: String = UUID.randomUUID().toString(),
+        labels: EventLabels = EventLabels()
+    ) : this(
+        id,
+        labels,
+        FingerprintCapturePayloadV3(createdAt, EVENT_VERSION, endTime, finger, qualityThreshold, result, fingerprint, id),
+        FINGERPRINT_CAPTURE_V3
+    )
+
+    @Keep
+    data class FingerprintCapturePayloadV3(
+        override val createdAt: Long,
+        override val eventVersion: Int,
+        override var endedAt: Long,
+        val finger: IFingerIdentifier,
+        val qualityThreshold: Int,
+        val result: Result,
+        val fingerprint: Fingerprint?,
+        val id: String,
+        override val type: EventType = FINGERPRINT_CAPTURE_V3
+    ) : EventPayload() {
+
+        @Keep
+        data class Fingerprint(
+            val finger: IFingerIdentifier,
+            val quality: Int,
+            val format: FingerprintTemplateFormat = FingerprintTemplateFormat.ISO_19794_2
+        )
+
+        @Keep
+        enum class Result {
+            GOOD_SCAN,
+            BAD_QUALITY,
+            NO_FINGER_DETECTED,
+            SKIPPED,
+            FAILURE_TO_ACQUIRE;
+        }
+    }
+
+    companion object {
+        const val EVENT_VERSION = 3
+    }
+}
