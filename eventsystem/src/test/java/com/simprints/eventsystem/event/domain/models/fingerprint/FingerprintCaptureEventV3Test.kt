6af87953faa10a965cae897027a1c1fd53cc7d8a
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-03-08 12:12:14 +0300

    add tests in FingerprintCaptureEventV3Test.kt
    Former-commit-id: 6df6e4a9575febac3a336d55aa160327a2d038f2 [formerly 39b2ef7e3df43687006d6be7141b3d8cddff382f]
    Former-commit-id: 5866c303e1ef4322977dbfce96b8add431ed6e43

---
+++ eventsystem/src/test/java/com/simprints/eventsystem/event/domain/models/fingerprint/FingerprintCaptureEventV3Test.kt
@@ -0,0 +1,36 @@
+package com.simprints.eventsystem.event.domain.models.fingerprint
+
+import com.google.common.truth.Truth.assertThat
+import com.simprints.eventsystem.event.domain.models.EventLabels
+import com.simprints.eventsystem.event.domain.models.EventType
+import com.simprints.eventsystem.sampledata.FingerprintCaptureEventV3Sample
+import com.simprints.eventsystem.sampledata.SampleDefaults
+import com.simprints.moduleapi.fingerprint.IFingerIdentifier
+import org.junit.Test
+
+class FingerprintCaptureEventV3Test {
+
+    @Test
+    fun create_FingerprintCaptureEvent() {
+        val labels = EventLabels(sessionId = SampleDefaults.GUID1)
+        val fingerprint = FingerprintCaptureEventV3.FingerprintCapturePayloadV3.Fingerprint(
+            IFingerIdentifier.LEFT_THUMB,
+            8,
+            FingerprintTemplateFormat.ISO_19794_2
+        )
+        val event = FingerprintCaptureEventV3Sample.getEvent(labels)
+
+        assertThat(event.id).isNotNull()
+        assertThat(event.labels).isEqualTo(labels)
+        assertThat(event.type).isEqualTo(EventType.FINGERPRINT_CAPTURE_V3)
+        with(event.payload) {
+            assertThat(createdAt).isEqualTo(SampleDefaults.CREATED_AT)
+            assertThat(endedAt).isEqualTo(SampleDefaults.ENDED_AT)
+            assertThat(eventVersion).isEqualTo(FingerprintCaptureEventV3.EVENT_VERSION)
+            assertThat(type).isEqualTo(EventType.FINGERPRINT_CAPTURE_V3)
+            assertThat(finger).isEqualTo(IFingerIdentifier.LEFT_THUMB)
+            assertThat(qualityThreshold).isEqualTo(10)
+            assertThat(fingerprint).isEqualTo(fingerprint)
+        }
+    }
+}
