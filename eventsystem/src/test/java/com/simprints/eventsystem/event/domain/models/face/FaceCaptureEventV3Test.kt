13f5e6753cbc9e706f8561871c30742f5bd5adc5
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-03-09 12:03:05 +0300

    add tests for FaceCaptureEventV3
    Former-commit-id: 825a359b5683b58f828e967177965442162d060f [formerly b131bc26e818adbe7cd2454cc903a6ccbc771b6d]
    Former-commit-id: c9cf32fb08c0c637bb76a94b6a8cb0009534f2cb

---
+++ eventsystem/src/test/java/com/simprints/eventsystem/event/domain/models/face/FaceCaptureEventV3Test.kt
@@ -0,0 +1,37 @@
+package com.simprints.eventsystem.event.domain.models.face
+
+import androidx.annotation.Keep
+import com.google.common.truth.Truth.assertThat
+import com.simprints.eventsystem.event.domain.models.EventLabels
+import com.simprints.eventsystem.event.domain.models.EventType
+import com.simprints.eventsystem.sampledata.SampleDefaults
+import org.junit.Test
+
+@Keep
+class FaceCaptureEventV3Test {
+    @Test
+    fun create_FaceCaptureEvent() {
+        val labels = EventLabels(sessionId = SampleDefaults.GUID1)
+        val faceArg = FaceCaptureEventV3.FaceCapturePayloadV3.Face(0F, 1F, 2F, FaceTemplateFormat.RANK_ONE_1_23)
+        val event = FaceCaptureEventV3(
+            SampleDefaults.CREATED_AT,
+            SampleDefaults.ENDED_AT, 0, 1F,
+            FaceCaptureEventV3.FaceCapturePayloadV3.Result.VALID, true, faceArg, labels
+        )
+        assertThat(event.id).isNotNull()
+        assertThat(event.labels).isEqualTo(labels)
+        assertThat(event.type).isEqualTo(EventType.FACE_CAPTURE)
+        with(event.payload) {
+            assertThat(createdAt).isEqualTo(SampleDefaults.CREATED_AT)
+            assertThat(endedAt).isEqualTo(SampleDefaults.ENDED_AT)
+            assertThat(eventVersion).isEqualTo(FaceCaptureEventV3.EVENT_VERSION)
+            assertThat(type).isEqualTo(EventType.FACE_CAPTURE_V3)
+            assertThat(attemptNb).isEqualTo(0)
+            assertThat(qualityThreshold).isEqualTo(1F)
+            assertThat(result).isEqualTo(FaceCaptureEventV3.FaceCapturePayloadV3.Result.VALID)
+            assertThat(isFallback).isEqualTo(true)
+            assertThat(face).isEqualTo(faceArg)
+        }
+    }
+}
+
