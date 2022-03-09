86e902f90871e2ddfd0222063fc8ec4053361413
melad <melad@simprints.com>
2022-03-09 15:26:17 +0200

    keep SimNetworkUtils and EventCount
    Former-commit-id: b3fb9130c46a8a2825471a80656be587abc496b5 [formerly 34cf84bb0d03b43554503af785b549adb274c9c7]
    Former-commit-id: 0bd56d46a389700cd31ca405090d6a28ff637790

--- eventsystem/src/main/java/com/simprints/eventsystem/event/domain/EventCount.kt
+++ eventsystem/src/main/java/com/simprints/eventsystem/event/domain/EventCount.kt
@@ -1,6 +1,7 @@
 package com.simprints.eventsystem.event.domain
 
+import androidx.annotation.Keep
 import com.simprints.eventsystem.event.domain.models.EventType
 
-
+@Keep
 data class EventCount(val type: EventType, val count: Int)
