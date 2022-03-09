9395ec79db1f65cac3f8fa488e8424e801c86e40
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-03-09 12:05:41 +0300

    modify EnrolmentEventValidator.kt to use V3 of FaceCaptureEvent
    Former-commit-id: 0934ef4c954b36aaf919c007275172c58753ef50 [formerly db7e930141d603cb5984916d89810d4766d792b8]
    Former-commit-id: 19b5fd38a112e3578b76e085753ca17ffa38dd0b

--- eventsystem/src/main/java/com/simprints/eventsystem/event/domain/validators/EnrolmentEventValidator.kt
+++ eventsystem/src/main/java/com/simprints/eventsystem/event/domain/validators/EnrolmentEventValidator.kt
@@ -3,8 +3,8 @@ package com.simprints.eventsystem.event.domain.validators
 import com.simprints.eventsystem.event.domain.models.EnrolmentEventV2
 import com.simprints.eventsystem.event.domain.models.Event
 import com.simprints.eventsystem.event.domain.models.PersonCreationEvent
-import com.simprints.eventsystem.event.domain.models.face.FaceCaptureEvent
-import com.simprints.eventsystem.event.domain.models.fingerprint.FingerprintCaptureEvent
+import com.simprints.eventsystem.event.domain.models.face.FaceCaptureEventV3
+import com.simprints.eventsystem.event.domain.models.fingerprint.FingerprintCaptureEventV3
 import com.simprints.eventsystem.exceptions.validator.EnrolmentEventValidatorException
 
 class EnrolmentEventValidator : EventValidator {
@@ -15,8 +15,8 @@ class EnrolmentEventValidator : EventValidator {
      */
     override fun validate(currentEvents: List<Event>, eventToAdd: Event) {
         if (eventToAdd is EnrolmentEventV2) {
-            val hasFingerprint = currentEvents.any { it is FingerprintCaptureEvent }
-            val hasFace = currentEvents.any { it is FaceCaptureEvent }
+            val hasFingerprint = currentEvents.any { it is FingerprintCaptureEventV3 }
+            val hasFace = currentEvents.any { it is FaceCaptureEventV3 }
             val hasPersonCreation = currentEvents.any { it is PersonCreationEvent }
 
             if (!hasFingerprint && !hasFace)
