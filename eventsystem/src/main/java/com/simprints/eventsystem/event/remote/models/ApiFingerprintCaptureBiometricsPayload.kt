7f78add4a058e07edc6d60f2d425330c18a1e714
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-03-10 16:51:49 +0300

    add api paylodads for face and finger capture biometric events
    Former-commit-id: 10b82a1717e2634d928cb33ccaaaeb6e62e21757 [formerly 2c8d74bc96a5c3d8cb98232bbe6b16ececef7639]
    Former-commit-id: 7bc6ff4319b0bf8a57373efa90cc284c3bf1d379

---
+++ eventsystem/src/main/java/com/simprints/eventsystem/event/remote/models/ApiFingerprintCaptureBiometricsPayload.kt
@@ -0,0 +1,56 @@
+package com.simprints.eventsystem.event.remote.models
+
+import androidx.annotation.Keep
+import com.simprints.eventsystem.event.domain.models.fingerprint.FingerprintCaptureBiometricsEvent
+import com.simprints.eventsystem.event.domain.models.fingerprint.FingerprintTemplateFormat
+import com.simprints.moduleapi.fingerprint.IFingerIdentifier
+
+@Keep
+data class ApiFingerprintCaptureBiometricsPayload(
+    override val version: Int,
+    override val startTime: Long,
+    val result: ApiResult,
+    val fingerprint: Fingerprint?,
+    val id: String,
+) : ApiEventPayload(ApiEventPayloadType.FingerprintCaptureBiometrics, version, startTime) {
+
+    @Keep
+    data class Fingerprint(
+        val finger: IFingerIdentifier,
+        val template: String,
+        val format: FingerprintTemplateFormat
+    ) {
+        constructor(finger: FingerprintCaptureBiometricsEvent.FingerprintCaptureBiometricsPayload.Fingerprint) : this(
+            finger.finger,
+            finger.template,
+            finger.format
+        )
+    }
+
+    constructor(domainPayload: FingerprintCaptureBiometricsEvent.FingerprintCaptureBiometricsPayload) : this(
+        domainPayload.eventVersion,
+        domainPayload.createdAt,
+        domainPayload.result.fromDomainToApi(),
+        domainPayload.fingerprint?.let { Fingerprint(it) },
+        domainPayload.id
+    )
+
+    @Keep
+    enum class ApiResult {
+        GOOD_SCAN,
+        BAD_QUALITY,
+        NO_FINGER_DETECTED,
+        SKIPPED,
+        FAILURE_TO_ACQUIRE
+    }
+}
+
+fun FingerprintCaptureBiometricsEvent.FingerprintCaptureBiometricsPayload.Result.fromDomainToApi() =
+    when (this) {
+        FingerprintCaptureBiometricsEvent.FingerprintCaptureBiometricsPayload.Result.GOOD_SCAN -> ApiFingerprintCaptureBiometricsPayload.ApiResult.GOOD_SCAN
+        FingerprintCaptureBiometricsEvent.FingerprintCaptureBiometricsPayload.Result.BAD_QUALITY -> ApiFingerprintCaptureBiometricsPayload.ApiResult.BAD_QUALITY
+        FingerprintCaptureBiometricsEvent.FingerprintCaptureBiometricsPayload.Result.NO_FINGER_DETECTED -> ApiFingerprintCaptureBiometricsPayload.ApiResult.NO_FINGER_DETECTED
+        FingerprintCaptureBiometricsEvent.FingerprintCaptureBiometricsPayload.Result.SKIPPED -> ApiFingerprintCaptureBiometricsPayload.ApiResult.SKIPPED
+        FingerprintCaptureBiometricsEvent.FingerprintCaptureBiometricsPayload.Result.FAILURE_TO_ACQUIRE -> ApiFingerprintCaptureBiometricsPayload.ApiResult.FAILURE_TO_ACQUIRE
+    }
+
---
+++ eventsystem/src/main/java/com/simprints/eventsystem/event/remote/models/face/ApiFaceCaptureBiometricsPayload.kt
@@ -0,0 +1,58 @@
+package com.simprints.eventsystem.event.remote.models.face
+
+import androidx.annotation.Keep
+import com.simprints.eventsystem.event.domain.models.face.FaceCaptureBiometricsEvent
+import com.simprints.eventsystem.event.domain.models.face.FaceTemplateFormat
+import com.simprints.eventsystem.event.remote.models.ApiEventPayload
+import com.simprints.eventsystem.event.remote.models.ApiEventPayloadType
+
+@Keep
+class ApiFaceCaptureBiometricsPayload(
+    override val version: Int,
+    override val startTime: Long,
+    val id: String,
+    val qualityThreshold: Float,
+    val result: ApiResult,
+    val face: Face?
+) : ApiEventPayload(ApiEventPayloadType.FaceCaptureBiometrics, version, startTime) {
+
+    @Keep
+    data class Face(
+        val template: String,
+        val format: FaceTemplateFormat
+    ) {
+        constructor(face: FaceCaptureBiometricsEvent.FaceCaptureBiometricsPayload.Face) : this(
+            face.template,
+            face.format
+        )
+    }
+
+    constructor(domainPayload: FaceCaptureBiometricsEvent.FaceCaptureBiometricsPayload) : this(
+        domainPayload.eventVersion,
+        domainPayload.createdAt,
+        domainPayload.id,
+        domainPayload.qualityThreshold,
+        domainPayload.result.fromDomainToApi(),
+        domainPayload.face?.let { Face(it) }
+    )
+
+    @Keep
+    enum class ApiResult {
+        VALID,
+        INVALID,
+        OFF_YAW,
+        OFF_ROLL,
+        TOO_CLOSE,
+        TOO_FAR
+    }
+}
+
+fun FaceCaptureBiometricsEvent.FaceCaptureBiometricsPayload.Result.fromDomainToApi() =
+    when (this) {
+        FaceCaptureBiometricsEvent.FaceCaptureBiometricsPayload.Result.VALID -> ApiFaceCaptureBiometricsPayload.ApiResult.VALID
+        FaceCaptureBiometricsEvent.FaceCaptureBiometricsPayload.Result.INVALID -> ApiFaceCaptureBiometricsPayload.ApiResult.INVALID
+        FaceCaptureBiometricsEvent.FaceCaptureBiometricsPayload.Result.OFF_YAW -> ApiFaceCaptureBiometricsPayload.ApiResult.OFF_YAW
+        FaceCaptureBiometricsEvent.FaceCaptureBiometricsPayload.Result.OFF_ROLL -> ApiFaceCaptureBiometricsPayload.ApiResult.OFF_ROLL
+        FaceCaptureBiometricsEvent.FaceCaptureBiometricsPayload.Result.TOO_CLOSE -> ApiFaceCaptureBiometricsPayload.ApiResult.TOO_CLOSE
+        FaceCaptureBiometricsEvent.FaceCaptureBiometricsPayload.Result.TOO_FAR -> ApiFaceCaptureBiometricsPayload.ApiResult.TOO_FAR
+    }
