f7e79bd2497d509f14c0f3b8268f4332eff1ff39
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-03-10 16:47:44 +0300

    finalise face and finger capture biometric events
    Former-commit-id: 2259cda309b499be3effae1a520b94323a431116 [formerly 1b9b9e6db5cdd1cae01b2bdeb048d6727d1da7c8]
    Former-commit-id: 3507d27f06529c183304ca38d87cb42d8e3f9820

---
+++ eventsystem/src/main/java/com/simprints/eventsystem/event/domain/models/face/FaceCaptureBiometricsEvent.kt
@@ -0,0 +1,63 @@
+package com.simprints.eventsystem.event.domain.models.face
+
+import androidx.annotation.Keep
+import com.simprints.eventsystem.event.domain.models.Event
+import com.simprints.eventsystem.event.domain.models.EventLabels
+import com.simprints.eventsystem.event.domain.models.EventPayload
+import com.simprints.eventsystem.event.domain.models.EventType
+import java.util.UUID
+
+@Keep
+data class FaceCaptureBiometricsEvent(
+    override val id: String = UUID.randomUUID().toString(),
+    override var labels: EventLabels,
+    override val payload: FaceCaptureBiometricsPayload,
+    override val type: EventType
+) : Event() {
+
+    constructor(
+        startTime: Long,
+        qualityThreshold: Float,
+        result: FaceCaptureBiometricsPayload.Result,
+        face: FaceCaptureBiometricsPayload.Face?,
+        labels: EventLabels = EventLabels(),
+        id: String = UUID.randomUUID().toString(),
+    ) : this(
+        id,
+        labels,
+        FaceCaptureBiometricsPayload(id, startTime, EVENT_VERSION, qualityThreshold, result, face),
+        EventType.FACE_CAPTURE_BIOMETRICS
+    )
+
+    @Keep
+    data class FaceCaptureBiometricsPayload(
+        val id: String,
+        override val createdAt: Long,
+        override val eventVersion: Int,
+        val qualityThreshold: Float,
+        val result: Result,
+        val face: Face?,
+        override var endedAt: Long = 0,
+        override val type: EventType = EventType.FACE_CAPTURE_BIOMETRICS
+    ) : EventPayload() {
+
+        @Keep
+        data class Face(
+            val template: String,
+            val format: FaceTemplateFormat = FaceTemplateFormat.RANK_ONE_1_23
+        )
+
+        enum class Result {
+            VALID,
+            INVALID,
+            OFF_YAW,
+            OFF_ROLL,
+            TOO_CLOSE,
+            TOO_FAR
+        }
+    }
+
+    companion object {
+        const val EVENT_VERSION = 0
+    }
+}
