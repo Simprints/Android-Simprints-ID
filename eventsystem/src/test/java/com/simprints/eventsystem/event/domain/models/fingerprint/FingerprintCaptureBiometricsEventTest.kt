9ea44398a17d1ae8c406bb60d124bbf8f8f38fe2
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-03-11 11:07:45 +0300

    add tests for fingerprint capture biometrics event and add more assertions to face biometric event test
    Former-commit-id: 8bef97962da4a8c876d704be71fa5cefee9ed0b5 [formerly d6685f1497f12a68365ab8fdaf2735c90de71617]
    Former-commit-id: f985827cf0a5bb0b1d8be76a6eda86f35ef441f9

---
+++ eventsystem/src/test/java/com/simprints/eventsystem/event/domain/models/fingerprint/FingerprintCaptureBiometricsEventTest.kt
@@ -0,0 +1,37 @@
+package com.simprints.eventsystem.event.domain.models.fingerprint
+
+import com.google.common.truth.Truth.assertThat
+import com.simprints.eventsystem.event.domain.models.EventLabels
+import com.simprints.eventsystem.event.domain.models.EventType
+import com.simprints.eventsystem.sampledata.SampleDefaults
+import com.simprints.moduleapi.fingerprint.IFingerIdentifier
+import org.junit.Test
+
+class FingerprintCaptureBiometricsEventTest {
+
+    @Test
+    fun create_FaceCaptureBiometricsEvent() {
+        val labels = EventLabels(sessionId = SampleDefaults.GUID1)
+        val fingerArg =
+            FingerprintCaptureBiometricsEvent.FingerprintCaptureBiometricsPayload.Fingerprint(IFingerIdentifier.LEFT_3RD_FINGER, "template")
+        val event = FingerprintCaptureBiometricsEvent(
+            createdAt = SampleDefaults.CREATED_AT,
+            result = FingerprintCaptureBiometricsEvent.FingerprintCaptureBiometricsPayload.Result.GOOD_SCAN,
+            fingerprint = fingerArg,
+            id = "someId",
+            labels = labels
+        )
+
+        assertThat(event.id).isNotNull()
+        assertThat(event.labels).isEqualTo(labels)
+        assertThat(event.type).isEqualTo(EventType.FINGERPRINT_CAPTURE_BIOMETRICS)
+
+        with(event.payload) {
+            assertThat(id).isEqualTo("someId")
+            assertThat(type).isEqualTo(EventType.FINGERPRINT_CAPTURE_BIOMETRICS)
+            assertThat(result).isEqualTo(FingerprintCaptureBiometricsEvent.FingerprintCaptureBiometricsPayload.Result.GOOD_SCAN)
+            assertThat(fingerprint).isEqualTo(fingerArg)
+            assertThat(eventVersion).isEqualTo(0)
+        }
+    }
+}
