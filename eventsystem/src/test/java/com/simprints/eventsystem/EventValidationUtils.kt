f4de648c3a154a87cf2ab3081639241836a0dfe8
Ebi Igweze <ebi.igweze@gmail.com>
2022-03-15 11:18:56 +0100

    updates event system tests to include tests for updated event payload
    Former-commit-id: e9e456744f8ada59709a2b469f8fc5d9a90ca3d3 [formerly 15f09cb599d01d88596b738ab6c73cad123cf325]
    Former-commit-id: a46cb3b9d2c0d1f26789129eb4e7708955e3cee8

--- eventsystem/src/test/java/com/simprints/eventsystem/EventValidationUtils.kt
+++ eventsystem/src/test/java/com/simprints/eventsystem/EventValidationUtils.kt
@@ -579,18 +579,15 @@ fun validateScannerConnectionEventApiModel(json: JSONObject) {
 fun validateVero2InfoSnapshotEventApiModel(json: JSONObject) {
     validateCommonParams(json, "Vero2InfoSnapshot")
     with(json.getJSONObject("payload")) {
-        assertThat(getInt("version")).isEqualTo(1)
+        assertThat(getInt("version")).isEqualTo(2)
         assertThat(getLong("startTime"))
 
         with(getJSONObject("scannerVersion")) {
-            assertThat(getString("master")).isNotEmpty()
+            assertThat(getString("hardwareVersion")).isNotEmpty()
             assertThat(getString("cypressApp")).isNotEmpty()
-            assertThat(getString("cypressApi")).isNotEmpty()
             assertThat(getString("stmApp")).isNotEmpty()
-            assertThat(getString("stmApi")).isNotEmpty()
             assertThat(getString("un20App")).isNotEmpty()
-            assertThat(getString("un20Api")).isNotEmpty()
-            assertThat(length()).isEqualTo(7)
+            assertThat(length()).isEqualTo(4)
         }
 
         with(getJSONObject("battery")) {
