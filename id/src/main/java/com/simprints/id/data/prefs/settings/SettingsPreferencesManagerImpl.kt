adef5d04244dbdbc87d4210682608763f34c6937
melad <melad@simprints.com>
2022-03-19 19:14:07 +0200

    replace  /firmware/versions api with remote config values
    Former-commit-id: 7ada2db403bbac9bd40a368c4182221dcc270eca [formerly 7206e107eacb182efc9b90508299f369bee20dcd]
    Former-commit-id: 4d1031f4ddcd14db75ce9355c79c525810fc4d47

--- id/src/main/java/com/simprints/id/data/prefs/settings/SettingsPreferencesManagerImpl.kt
+++ id/src/main/java/com/simprints/id/data/prefs/settings/SettingsPreferencesManagerImpl.kt
@@ -518,7 +518,8 @@ open class SettingsPreferencesManagerImpl(
         )
 
         const val VERO2_FIRMWARE_VERSIONS_KEY = "Vero2FirmwareVersions"
-        const val VERO2_FIRMWARE_VERSIONS_DEFAULT = ""
+        const val VERO2_FIRMWARE_VERSIONS_DEFAULT =
+            "{\"E-1\":{\"cypress\":\"1.E-1.1\",\"stm\":\"1.E-1.1\",\"un20\":\"1.E-1.0\"}}"
 
     }
 
