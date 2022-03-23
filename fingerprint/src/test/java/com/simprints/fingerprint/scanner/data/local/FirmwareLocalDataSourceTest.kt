0f835043cb15655c47a40d09f15f1e1d0be154fa
melad <melad@simprints.com>
2022-03-23 12:00:56 +0200

    Add check for local available files before starting an OTA
    Former-commit-id: be7dda9ccc643f4cbda22a544613a5bb43d8b61e [formerly 2b1233fcffe94963e3291e9190e7410a80fa1b38]
    Former-commit-id: f6a74cce5f7736b0a538474ea6e8e4a014e8898f

---
+++ fingerprint/src/test/java/com/simprints/fingerprint/scanner/data/local/FirmwareLocalDataSourceTest.kt
@@ -0,0 +1,50 @@
+package com.simprints.fingerprint.scanner.data.local
+
+import android.content.Context
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.MockK
+import org.junit.Before
+import org.junit.Test
+
+class FirmwareLocalDataSourceTest {
+
+//    @MockK
+//    lateinit var context: Context
+//    lateinit var firmwareLocalDataSource:FirmwareLocalDataSource
+//    @Before
+//    fun setUp() {
+//        MockKAnnotations.init(this)
+//        firmwareLocalDataSource = FirmwareLocalDataSource(context)
+//    }
+//
+//    @Test
+//    fun getAvailableScannerFirmwareVersions() {
+//        //Given
+//        //When
+//        firmwareLocalDataSource.getAvailableScannerFirmwareVersions()
+//    }
+//
+//    @Test
+//    fun loadCypressFirmwareBytes() {
+//    }
+//
+//    @Test
+//    fun loadStmFirmwareBytes() {
+//    }
+//
+//    @Test
+//    fun loadUn20FirmwareBytes() {
+//    }
+//
+//    @Test
+//    fun saveCypressFirmwareBytes() {
+//    }
+//
+//    @Test
+//    fun saveStmFirmwareBytes() {
+//    }
+//
+//    @Test
+//    fun saveUn20FirmwareBytes() {
+//    }
+}
