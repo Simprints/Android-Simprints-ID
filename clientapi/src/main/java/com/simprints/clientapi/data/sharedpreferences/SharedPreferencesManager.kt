6cf9b0df8fe55d13f00033461488f12671a2b2f0
Fbada006 <ferdinandkamata@yahoo.com>
2022-04-08 10:30:02 +0300

    add cosync settings to to SharedPreferencesManager.kt
    Former-commit-id: edfe64eaabdb7e51e47769305188b8a451916cb6 [formerly ad1d66b3f1b47d03eeb93da501e6ab34498d021f]
    Former-commit-id: e270685d0d95f4a293b438193739c664dcb1cfaf

--- clientapi/src/main/java/com/simprints/clientapi/data/sharedpreferences/SharedPreferencesManager.kt
+++ clientapi/src/main/java/com/simprints/clientapi/data/sharedpreferences/SharedPreferencesManager.kt
@@ -1,11 +1,14 @@
 package com.simprints.clientapi.data.sharedpreferences
 
 import com.simprints.core.domain.modality.Modality
+import com.simprints.id.domain.CosyncSetting
 import com.simprints.id.domain.SyncDestinationSetting
 
 
 interface SharedPreferencesManager {
     val syncDestinationSettings: List<SyncDestinationSetting>
+
+    val cosyncSyncSettings: List<CosyncSetting>
 
     val modalities: List<Modality>
 
