a2a93438a7420e03aec212fb1f6dbcdda694982d
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-04-13 18:20:09 +0300

    remove unneeded files
    Former-commit-id: 08f4622184c38a99c77a1bb654db492bf5db8439 [formerly 4d004f606b6fa730f412227690135a029333d672]
    Former-commit-id: 9b767dfb112713fc0eba7df88e986b1f4e616dd7

--- id/src/main/java/com/simprints/id/activities/settings/syncinformation/fetchRecords/OutageData.kt
+++
@@ -1,6 +0,0 @@
-package com.simprints.id.activities.settings.syncinformation.fetchRecords
-
-data class OutageData(
-    val isFailureBackendMaintenance: Boolean,
-    val estimatedOutage: Long? = null
-)
