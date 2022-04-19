be83c8d12e890f85cebed3ab9e2dfd019dbf6a61
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-04-19 12:46:15 +0300

    use jsonhelper to check for exception and update test variables
    Former-commit-id: ce39677687eea40225145bf37b9d705adc72a5b8 [formerly c5c8471ccde8890c8d0bcdb55e1b5a133cf71c6f]
    Former-commit-id: 1274d5e24b90fa5555ef20a64570661ce8b96702

--- core/src/main/java/com/simprints/core/tools/extentions/Throwable.ext.kt
+++ core/src/main/java/com/simprints/core/tools/extentions/Throwable.ext.kt
@@ -1,6 +1,8 @@
 package com.simprints.core.tools.extentions
 
+import androidx.annotation.Keep
 import com.simprints.core.network.NetworkConstants
+import com.simprints.core.tools.json.JsonHelper
 import retrofit2.HttpException
 
 fun Throwable.isCloudRecoverableIssue() =
@@ -8,22 +10,26 @@ fun Throwable.isCloudRecoverableIssue() =
 
 fun Throwable.isBackendMaintenanceException(): Boolean {
     if (this is HttpException && response()?.code() == 503) {
-        val jsonResponse = response()?.errorBody()?.string()?.filterNot { it.isWhitespace() }
-        return jsonResponse != null && jsonResponse.contains(NetworkConstants.BACKEND_MAINTENANCE_ERROR_STRING)
+        return try {
+            val error =
+                response()?.errorBody()?.string()?.let { JsonHelper.fromJson<ApiError>(it).error }
+            error != null && error == NetworkConstants.BACKEND_MAINTENANCE_ERROR_STRING
+        } catch (e: Throwable) {
+            false
+        }
     }
     return false
 }
 
 fun Throwable.getEstimatedOutage(): Long? {
-    return if (this.isBackendMaintenanceException()) {
-        try {
-            val exception = this as HttpException
-            val headers = exception.response()?.headers()
-            headers?.get(NetworkConstants.HEADER_RETRY_AFTER)?.toLong() ?: 0L
-        } catch (e: NumberFormatException) {
-            null
-        }
-    } else {
+    return try {
+        val exception = this as HttpException
+        val headers = exception.response()?.headers()
+        headers?.get(NetworkConstants.HEADER_RETRY_AFTER)?.toLong()
+    } catch (e: Throwable) {
         null
     }
 }
+
+@Keep
+data class ApiError(val error: String)
