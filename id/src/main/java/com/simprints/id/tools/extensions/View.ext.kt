c2f2df634f12b43913fa59a77f10fc58e150035b
melad <melad@simprints.com>
2022-05-02 13:12:43 +0200

    add comment for the ext fun purpose
    Former-commit-id: c4c88fe491ca0b523c420e2d3d744393ca617009 [formerly db73a14f9159b270bd5f0cd8be28006e7a027759]
    Former-commit-id: 7d9a2e03eabb7d6cd242e19a518edb747f8d32bd

--- id/src/main/java/com/simprints/id/tools/extensions/View.ext.kt
+++ id/src/main/java/com/simprints/id/tools/extensions/View.ext.kt
@@ -6,4 +6,5 @@ var android.widget.TextView.textColor: Int
     get() = throw IllegalAccessException("no getter for text color")
     set(v) = setTextColor(v)
 
+// This ext fun is created to avoid changing in all files that was using a anko which is a deprecated lib
 fun View.onLayoutChange(l: View.OnLayoutChangeListener) = addOnLayoutChangeListener(l)
