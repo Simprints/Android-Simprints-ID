dac149faa884081b3f5d8a536b6f0295326ab033
melad <melad@simprints.com>
2022-05-19 16:07:45 +0200

    Remove commented code
    Former-commit-id: 1664643d27796f28d0316e5ffe5ed2c8da9ebef4 [formerly 88a4bc5d5e253821f930980ee7c5daf931a50b57]
    Former-commit-id: c8009779795ee1e59c7c2462bc3c90741bf8b698

--- id/src/main/java/com/simprints/id/data/db/subject/local/SubjectLocalDataSourceImpl.kt
+++ id/src/main/java/com/simprints/id/data/db/subject/local/SubjectLocalDataSourceImpl.kt
@@ -58,13 +58,6 @@ class SubjectLocalDataSourceImpl(
         loginInfoManager.getSignedInProjectIdOrEmpty().let {
             return if (it.isNotEmpty()) {
                  createAndSaveRealmConfig(secureDataManager.getLocalDbKeyOrThrow(it))
-
-                // switching keys with roomDB temporarily to reproduce realm issues
-                // uncomment the lines below comment the lines above
-//                val keyName = "dbevents"
-//                val key = secureDataManager.getLocalDbKeyOrThrow(keyName)
-//                val newKey = key.copy(projectId = it)
-//                createAndSaveRealmConfig(newKey)
             } else {
                 throw RealmUninitialisedException("No signed in project id found")
             }
@@ -72,17 +65,6 @@ class SubjectLocalDataSourceImpl(
 
     private fun createAndSaveRealmConfig(localDbKey: LocalDbKey): RealmConfiguration =
         SubjectsRealmConfig.get(localDbKey.projectId, localDbKey.value, localDbKey.projectId)
-
-
-    override suspend fun insertOrUpdate(subjects: List<Subject>) {
-        withContext(dispatcher.main()) {
-            Realm.getInstance(config).use { realm ->
-                realm.transactAwait {
-                    it.insertOrUpdate(subjects.map(Subject::fromDomainToDb))
-                }
-            }
-        }
-    }
 
     override suspend fun load(query: SubjectQuery?): Flow<Subject> =
         withContext(dispatcher.main()) {
