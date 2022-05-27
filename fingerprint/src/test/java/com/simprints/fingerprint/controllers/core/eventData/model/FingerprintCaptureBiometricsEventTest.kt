3f6c8c3aac6f37086800518216b91c2000825291
Fbada006 <ferdinandkamata@yahoo.com>
2022-05-27 13:19:36 +0300

    add more tests in FingerprintCaptureBiometricsEventTest.kt
    Former-commit-id: 47f36df291507617ae4b0ddc238b754dc435b53d [formerly 7b91e7c9df541fa403dc30c643db07db2de3e555]
    Former-commit-id: 994ed6dfac78521e58cbb2b79bf188399a7107c3

---
+++ fingerprint/src/test/java/com/simprints/fingerprint/controllers/core/eventData/model/FingerprintCaptureBiometricsEventTest.kt
@@ -0,0 +1,35 @@
+package com.simprints.fingerprint.controllers.core.eventData.model
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import com.simprints.eventsystem.event.domain.models.fingerprint.FingerprintCaptureBiometricsEvent as FingerprintCaptureBiometricsEventCore
+
+class FingerprintCaptureBiometricsEventTest {
+
+    @Test
+    fun `from domain to core maps correctly`() {
+        val domainEvent = FingerprintCaptureBiometricsEvent(
+            createdAt = 0,
+            endedAt = 0,
+            result = FingerprintCaptureBiometricsEvent.Result.GOOD_SCAN,
+            fingerprint = null
+        )
+
+        val coreEvent = domainEvent.fromDomainToCore()
+
+        assertThat(domainEvent.fingerprint).isEqualTo(coreEvent.payload.fingerprint)
+        assertThat(domainEvent.startTime).isEqualTo(coreEvent.payload.createdAt)
+        assertThat(domainEvent.endTime).isEqualTo(coreEvent.payload.endedAt)
+        assertThat(coreEvent.payload.result).isEqualTo(FingerprintCaptureBiometricsEventCore.FingerprintCaptureBiometricsPayload.Result.GOOD_SCAN)
+    }
+
+    @Test
+    fun `domain result maps to core properly`() {
+        // This test counts on the fact that the enum values have been arranged the same so keep this in mind if ever edited
+        FingerprintCaptureBiometricsEvent.Result.values().zip(
+            FingerprintCaptureBiometricsEventCore.FingerprintCaptureBiometricsPayload.Result.values()
+        ).forEach {
+            assertThat(it.first.fromDomainToCore()).isEqualTo(it.second)
+        }
+    }
+}
