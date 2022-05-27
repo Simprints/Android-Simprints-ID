8d5e2ec7b921ce219fc7eb21fed875d1691076dc
Fbada006 <ferdinandkamata@yahoo.com>
2022-05-27 17:21:54 +0300

    add biometrics tests
    Former-commit-id: 3377050a2fa82c822faf6f5babd74aacb3dd22fa [formerly 85a86b82a5fe4f6cd80e2d9a748284de974da07e]
    Former-commit-id: adadecb6d2525d7ba8ee56ede30c820ff3115630

---
+++ face/src/test/java/com/simprints/face/controllers/core/events/model/FaceCaptureBiometricsEventTest.kt
@@ -0,0 +1,78 @@
+package com.simprints.face.controllers.core.events.model
+
+import com.google.common.truth.Truth.assertThat
+import com.simprints.face.controllers.core.events.model.FaceCaptureBiometricsEvent.Result.Companion.fromFaceDetectionStatus
+import com.simprints.face.models.FaceDetection
+import org.junit.Test
+import com.simprints.eventsystem.event.domain.models.face.FaceCaptureBiometricsEvent as CoreFaceCaptureBiometricsEventFace
+
+class FaceCaptureBiometricsEventTest {
+
+    @Test
+    fun `fromDomainToCore maps correctly`() {
+        val domain = FaceCaptureBiometricsEvent(
+            startTime = 0,
+            endTime = 0,
+            result = FaceCaptureBiometricsEvent.Result.VALID,
+            qualityThreshold = 0.0f,
+            eventFace = null
+        )
+
+        val core = domain.fromDomainToCore()
+
+        with(core) {
+            assertThat(payload.face).isEqualTo(domain.eventFace)
+            assertThat(payload.result).isEqualTo(CoreFaceCaptureBiometricsEventFace.FaceCaptureBiometricsPayload.Result.VALID)
+            assertThat(payload.createdAt).isEqualTo(domain.startTime)
+            assertThat(payload.qualityThreshold).isEqualTo(domain.qualityThreshold)
+        }
+    }
+
+    @Test
+    fun `result fromDomainToCore maps correctly`() {
+        listOf(
+            FaceCaptureBiometricsEvent.Result.VALID,
+            FaceCaptureBiometricsEvent.Result.INVALID,
+            FaceCaptureBiometricsEvent.Result.OFF_YAW,
+            FaceCaptureBiometricsEvent.Result.OFF_ROLL,
+            FaceCaptureBiometricsEvent.Result.TOO_CLOSE,
+            FaceCaptureBiometricsEvent.Result.TOO_FAR,
+        ).zip(
+            listOf(
+                CoreFaceCaptureBiometricsEventFace.FaceCaptureBiometricsPayload.Result.VALID,
+                CoreFaceCaptureBiometricsEventFace.FaceCaptureBiometricsPayload.Result.INVALID,
+                CoreFaceCaptureBiometricsEventFace.FaceCaptureBiometricsPayload.Result.OFF_YAW,
+                CoreFaceCaptureBiometricsEventFace.FaceCaptureBiometricsPayload.Result.OFF_ROLL,
+                CoreFaceCaptureBiometricsEventFace.FaceCaptureBiometricsPayload.Result.TOO_CLOSE,
+                CoreFaceCaptureBiometricsEventFace.FaceCaptureBiometricsPayload.Result.TOO_FAR
+            )
+        ).forEach {
+            assertThat(it.first.fromDomainToCore()).isEqualTo(it.second)
+        }
+    }
+
+    @Test
+    fun `fromFaceDetectionStatus maps correctly`() {
+        listOf(
+            FaceDetection.Status.VALID,
+            FaceDetection.Status.VALID_CAPTURING,
+            FaceDetection.Status.NOFACE,
+            FaceDetection.Status.OFFYAW,
+            FaceDetection.Status.OFFROLL,
+            FaceDetection.Status.TOOCLOSE,
+            FaceDetection.Status.TOOFAR,
+        ).zip(
+            listOf(
+                FaceCaptureBiometricsEvent.Result.VALID,
+                FaceCaptureBiometricsEvent.Result.VALID,
+                FaceCaptureBiometricsEvent.Result.INVALID,
+                FaceCaptureBiometricsEvent.Result.OFF_YAW,
+                FaceCaptureBiometricsEvent.Result.OFF_ROLL,
+                FaceCaptureBiometricsEvent.Result.TOO_CLOSE,
+                FaceCaptureBiometricsEvent.Result.TOO_FAR
+            )
+        ).forEach {
+            assertThat(fromFaceDetectionStatus(it.first)).isEqualTo(it.second)
+        }
+    }
+}
