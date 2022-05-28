f6c0845888bd678a844360728122f1fd7f4219b6
Fbada006 <ferdinandkamata@yahoo.com>
2022-05-28 21:34:26 +0300

    fix failing test in LiveFeedbackFragmentViewModelTest.kt
    Former-commit-id: fbcf92d62780a31cf37402227db1f1bbd99b51b1 [formerly a83948e12dc497ef519c92e07ad771d45a148224]
    Former-commit-id: 8952ca66ce8ecb3cd202f64b277c7205ea497cb8

--- face/src/test/java/com/simprints/face/FixtureGenerator.kt
+++ face/src/test/java/com/simprints/face/FixtureGenerator.kt
@@ -1,6 +1,9 @@
 package com.simprints.face
 
 import android.graphics.Rect
+import com.simprints.eventsystem.event.domain.models.face.FaceTemplateFormat
+import com.simprints.face.controllers.core.events.model.FaceCaptureBiometricsEvent
+import com.simprints.face.controllers.core.events.model.FaceCaptureEvent
 import com.simprints.face.data.db.person.FaceIdentity
 import com.simprints.face.data.db.person.FaceSample
 import com.simprints.face.data.moduleapi.face.responses.entities.FaceMatchResult
@@ -28,4 +31,79 @@ object FixtureGenerator {
 
     fun getFace(rect: Rect = Rect(0, 0, 60, 60), quality: Float = 1f): Face =
         Face(100, 100, rect, 0f, 0f, quality, Random.nextBytes(20), FaceDetection.TemplateFormat.MOCK)
+
+    val faceCaptureEvent1 = FaceCaptureEvent(
+        startTime = 2,
+        endTime = 3,
+        attemptNb = 0,
+        qualityThreshold = -1.0f,
+        result = FaceCaptureEvent.Result.VALID,
+        isFallback = false,
+        eventFace = FaceCaptureEvent.EventFace(
+            yaw = 0.0f,
+            roll = 0.0f,
+            quality = 1.0f,
+            format = FaceTemplateFormat.MOCK
+        )
+    )
+    val faceCaptureBiometricsEvent1 = FaceCaptureBiometricsEvent(
+        startTime = 2,
+        endTime = 0,
+        result = FaceCaptureBiometricsEvent.Result.VALID,
+        qualityThreshold = -1.0f,
+        eventFace = FaceCaptureBiometricsEvent.EventFace(
+            template = "rR/uPLRKPI0yWzd9eQLM1/ST6DQ=",
+            format = FaceTemplateFormat.MOCK
+        )
+    )
+
+    val faceCaptureEvent2 = FaceCaptureEvent(
+        startTime = 4,
+        endTime = 5,
+        attemptNb = 0,
+        qualityThreshold = -1.0f,
+        result = FaceCaptureEvent.Result.VALID,
+        isFallback = false,
+        eventFace = FaceCaptureEvent.EventFace(
+            yaw = 0.0f,
+            roll = 0.0f,
+            quality = 1.0f,
+            format = FaceTemplateFormat.MOCK
+        )
+    )
+    val faceCaptureBiometricsEvent2 = FaceCaptureBiometricsEvent(
+        startTime = 4,
+        endTime = 0,
+        result = FaceCaptureBiometricsEvent.Result.VALID,
+        qualityThreshold = -1.0f,
+        eventFace = FaceCaptureBiometricsEvent.EventFace(
+            template = "rR/uPLRKPI0yWzd9eQLM1/ST6DQ=",
+            format = FaceTemplateFormat.MOCK
+        )
+    )
+
+    val faceCaptureEvent3 = FaceCaptureEvent(
+        startTime = 0,
+        endTime = 1,
+        attemptNb = 0,
+        qualityThreshold = -1.0f,
+        result = FaceCaptureEvent.Result.VALID,
+        isFallback = true,
+        eventFace = FaceCaptureEvent.EventFace(
+            yaw = 0.0f,
+            roll = 0.0f,
+            quality = 1.0f,
+            format = FaceTemplateFormat.MOCK
+        )
+    )
+    val faceCaptureBiometricsEvent3 = FaceCaptureBiometricsEvent(
+        startTime = 0,
+        endTime = 0,
+        result = FaceCaptureBiometricsEvent.Result.VALID,
+        qualityThreshold = -1.0f,
+        eventFace = FaceCaptureBiometricsEvent.EventFace(
+            template = "rR/uPLRKPI0yWzd9eQLM1/ST6DQ=",
+            format = FaceTemplateFormat.MOCK
+        )
+    )
 }
