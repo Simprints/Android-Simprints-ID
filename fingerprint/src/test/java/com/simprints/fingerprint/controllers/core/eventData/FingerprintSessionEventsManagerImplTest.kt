d7a5391ed495c02560c58618188f7b0cfa2fd630
melad <melad@simprints.com>
2022-06-07 08:37:57 +0200

    fix test
    Former-commit-id: 264c0a6130fc19514fbe9a17a7b0629a9cb2e301 [formerly 7641af2c36a8d4c7c6afdf92defa06373dee551e]
    Former-commit-id: 960854c2f40a53c5d712e5a29d103bb4947b6e3e

--- fingerprint/src/test/java/com/simprints/fingerprint/controllers/core/eventData/FingerprintSessionEventsManagerImplTest.kt
+++ fingerprint/src/test/java/com/simprints/fingerprint/controllers/core/eventData/FingerprintSessionEventsManagerImplTest.kt
@@ -25,6 +25,7 @@ class FingerprintSessionEventsManagerImplTest {
     fun addEventInBackground() = runBlocking {
         //Given
         val eventRepository: EventRepository = mockk()
+        coEvery { eventRepository.addOrUpdateEvent(any()) } just Runs
         val eventSlot = CapturingSlot<CoreOneToOneMatchEvent>()
         val fingerprintSessionEventsManager = FingerprintSessionEventsManagerImpl(eventRepository)
         val event = OneToOneMatchEvent(
