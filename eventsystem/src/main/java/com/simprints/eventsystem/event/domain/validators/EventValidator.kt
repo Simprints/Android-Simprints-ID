30728a714f2686de231473cbb585ec418595f39d
Fbada006 <ferdinandkamata@yahoo.com>
2022-06-08 12:48:37 +0300

    optimise imports
    Former-commit-id: 413d31adfaf7e4a759488642c4f735754447d46e [formerly ce59a51efaf4842c410df83ce1689e4971808132]
    Former-commit-id: 891667ab61281c2a16bbc88285bef383c8b4e968

--- eventsystem/src/main/java/com/simprints/eventsystem/event/domain/validators/EventValidator.kt
+++ eventsystem/src/main/java/com/simprints/eventsystem/event/domain/validators/EventValidator.kt
@@ -1,7 +1,6 @@
 package com.simprints.eventsystem.event.domain.validators
 
 import com.simprints.eventsystem.event.domain.models.Event
-import com.simprints.eventsystem.event.domain.models.session.SessionCaptureEvent
 
 interface EventValidator {
 
