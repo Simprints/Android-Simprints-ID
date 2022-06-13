953855b9f950cc965ff1628dc4cc0e22d780dfd6
melad <melad@simprints.com>
2022-06-13 12:56:16 +0200

    fix FingerprintSessionEventsManagerImplTest concurrency issue
    Former-commit-id: c0e92d1085b5f1133952dd2c5544675ebf3471bc [formerly 231fb2168866b464f34fdf2e627502157d65d024]
    Former-commit-id: 6e507eeccbd1e5f2a260be80993d0359146408ed

--- fingerprint/src/test/java/com/simprints/fingerprint/controllers/core/eventData/FingerprintSessionEventsManagerImplTest.kt
+++ fingerprint/src/test/java/com/simprints/fingerprint/controllers/core/eventData/FingerprintSessionEventsManagerImplTest.kt
@@ -1,6 +1,5 @@
 package com.simprints.fingerprint.controllers.core.eventData
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.google.common.truth.Truth
 import com.simprints.eventsystem.event.EventRepository
 import com.simprints.fingerprint.controllers.core.eventData.model.FingerComparisonStrategy
@@ -8,18 +7,20 @@ import com.simprints.fingerprint.controllers.core.eventData.model.MatchEntry
 import com.simprints.fingerprint.controllers.core.eventData.model.Matcher
 import com.simprints.fingerprint.controllers.core.eventData.model.OneToOneMatchEvent
 import com.simprints.id.data.db.subject.local.SubjectQuery
+import com.simprints.testtools.common.coroutines.TestCoroutineRule
 import io.mockk.*
 import kotlinx.coroutines.runBlocking
 import org.junit.Rule
 import org.junit.Test
-import com.simprints.eventsystem.event.domain.models.OneToOneMatchEvent as CoreOneToOneMatchEvent
 import com.simprints.eventsystem.event.domain.models.FingerComparisonStrategy as CoreFingerComparisonStrategy
 import com.simprints.eventsystem.event.domain.models.Matcher as CoreMatcher
+import com.simprints.eventsystem.event.domain.models.OneToOneMatchEvent as CoreOneToOneMatchEvent
 
 class FingerprintSessionEventsManagerImplTest {
 
     @get:Rule
-    val rule = InstantTaskExecutorRule()
+    val testCoroutineRule = TestCoroutineRule()
+
 
     @Test
     fun addEventInBackground() = runBlocking {
@@ -27,7 +28,10 @@ class FingerprintSessionEventsManagerImplTest {
         val eventRepository: EventRepository = mockk()
         coEvery { eventRepository.addOrUpdateEvent(any()) } just Runs
         val eventSlot = CapturingSlot<CoreOneToOneMatchEvent>()
-        val fingerprintSessionEventsManager = FingerprintSessionEventsManagerImpl(eventRepository)
+        val fingerprintSessionEventsManager = FingerprintSessionEventsManagerImpl(
+            eventRepository,
+             testCoroutineRule.testCoroutineDispatcher
+        )
         val event = OneToOneMatchEvent(
             1L,
             1L,
