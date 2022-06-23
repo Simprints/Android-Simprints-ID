c178c72f9d4b4d8a57c90e6edb1344b94d0a76cb
Tristram Norman <tristram@simprints.com>
2022-06-23 08:37:41 +0100

    add module & fix errors
    Former-commit-id: 046aa344e5150d72cb86b158a5b401a1d4749ff9 [formerly 1582a8a94d61a55bbdcfebf7976112fed513ec99]
    Former-commit-id: 549813bd17e6268c01b0dfe226607275ab988dfb

---
+++ infralogin/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.4.2")
+    implementation("com.google.android.material:material:1.6.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+}
