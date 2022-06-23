7ec0fb7526b1327d9fd4ca789dc75c1add4e5479
Tristram Norman <tristram@simprints.com>
2022-06-23 08:43:58 +0100

    get module ready for CI
    Former-commit-id: aab213b5611347de0c9de497f8b931e3c186586e [formerly a172ffd02104d308cc44723d3a0f4f9e93430cf8]
    Former-commit-id: 0ea923755768c40f7c9c0862ff0d1de29c5e0785

--- infralogin/build.gradle.kts
+++ infralogin/build.gradle.kts
@@ -3,21 +3,20 @@ plugins {
     kotlin("android")
 }
 
+apply {
+    from("$rootDir${File.separator}buildSrc${File.separator}build_config.gradle")
+}
+
 android {
     defaultConfig {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
-
 }
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.2")
-    implementation("com.google.android.material:material:1.6.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    // Unit Tests
+    testImplementation(libs.testing.junit)
 }
