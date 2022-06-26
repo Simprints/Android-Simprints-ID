61ace1ab4d1a338aa0d0b0a870d7c453a22b29a2
melad <melad@simprints.com>
2022-06-26 15:41:56 +0200

    update some libs, and use barcode scanner instead of firebase ml
    Former-commit-id: f3d325ce20eb58cc2cc423707d00af90cc584a9c [formerly a7b844a0ae54acd8812713d89518e8183d07184b]
    Former-commit-id: fac3a945565f1b88f8002db0bff89e0d0f4e6c16

--- infralogging/build.gradle.kts
+++ infralogging/build.gradle.kts
@@ -46,8 +46,8 @@ dependencies {
     // the central buildSrc module because we do not want or expect these dependencies to be used in
     // multiple modules
     implementation("com.google.firebase:firebase-crashlytics-ktx:18.2.11")
-    implementation("com.google.firebase:firebase-analytics-ktx:20.1.2")
-    implementation("com.google.firebase:firebase-perf:20.0.5")
+    implementation("com.google.firebase:firebase-analytics-ktx:21.0.0")
+    implementation("com.google.firebase:firebase-perf:20.1.0")
 
     //4.7.1 breaks realm:
     // https://github.com/realm/realm-java/issues/6153
