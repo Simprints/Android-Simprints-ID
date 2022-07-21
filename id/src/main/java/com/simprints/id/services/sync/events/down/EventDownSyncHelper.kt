55d41a840637d7e1f712e833bf820265dbdc4405
yoann <yoann@simprints.com>
2022-07-21 08:21:55 +0200

    Fix tests
    Former-commit-id: ad1bc94016d01455b4a7182fff70e0bd901bf36b [formerly 1a650507b3040f5d34a31a7d3124b6641332cca8]
    Former-commit-id: 8cf4f28ae6d61cdcf8edbfe3d9db260e20faee2b

--- id/src/main/java/com/simprints/id/services/sync/events/down/EventDownSyncHelper.kt
+++ id/src/main/java/com/simprints/id/services/sync/events/down/EventDownSyncHelper.kt
@@ -1,15 +1,17 @@
 package com.simprints.id.services.sync.events.down
 
 import com.simprints.eventsystem.event.domain.EventCount
+import com.simprints.eventsystem.events_sync.down.domain.EventDownSyncOperation
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.ReceiveChannel
 
 interface EventDownSyncHelper {
 
-    suspend fun countForDownSync(operation: com.simprints.eventsystem.events_sync.down.domain.EventDownSyncOperation): List<EventCount>
+    suspend fun countForDownSync(operation: EventDownSyncOperation): List<EventCount>
 
-    suspend fun downSync(scope: CoroutineScope,
-                         operation: com.simprints.eventsystem.events_sync.down.domain.EventDownSyncOperation
+    suspend fun downSync(
+        scope: CoroutineScope,
+        operation: EventDownSyncOperation
     ): ReceiveChannel<EventDownSyncProgress>
 
 }
