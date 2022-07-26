668d70e83ac61f624afe42296ff23e5c415df82c
yoann <yoann@simprints.com>
2022-07-26 11:22:17 +0200

    WIP infralogin
    Former-commit-id: 6e2160ac3e9d1c51354d97ed00cc55187b80df94 [formerly 76e0628c172539b8a6b919750da308b936166dd3]
    Former-commit-id: 0dc632deefb56b7f3f57242a2997df07d39352f7

--- infralogin/build.gradle.kts
+++ infralogin/build.gradle.kts
@@ -7,16 +7,44 @@ apply {
     from("$rootDir${File.separator}buildSrc${File.separator}build_config.gradle")
 }
 
+val RELEASE_SAFETYNET_KEY: String by extra
+val STAGING_SAFETYNET_KEY: String by extra
+val DEV_SAFETYNET_KEY: String by extra
+
 android {
     defaultConfig {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
+
+    buildTypes {
+        getByName("release") {
+            buildConfigField("String", "SAFETYNET_API_KEY", "\"$RELEASE_SAFETYNET_KEY\"")
+        }
+        getByName("staging") {
+            buildConfigField("String", "SAFETYNET_API_KEY", "\"$STAGING_SAFETYNET_KEY\"")
+        }
+        getByName("debug") {
+            buildConfigField("String", "SAFETYNET_API_KEY", "\"$DEV_SAFETYNET_KEY\"")
+        }
+
+    }
 }
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(project(":infralogging"))
+    implementation(project(":infranetwork"))
+
+    implementation(libs.androidX.core)
+    implementation(libs.playServices.safetynet)
+    implementation(libs.retrofit.core)
 
     // Unit Tests
+    testImplementation(project(":testtools"))
+
+    testImplementation(libs.testing.androidX.ext.junit)
     testImplementation(libs.testing.junit)
+    testImplementation(libs.testing.truth)
+    testImplementation(libs.testing.mockk.core)
 }
---
+++ infralogin/src/main/java/com/simprints/infra/login/domain/AttestationManagerImpl.kt
@@ -0,0 +1,51 @@
+package com.simprints.infra.login.domain
+
+import android.util.Base64
+import com.google.android.gms.safetynet.SafetyNetApi
+import com.google.android.gms.safetynet.SafetyNetClient
+import com.google.android.gms.tasks.Tasks
+import com.simprints.infra.logging.Simber
+import com.simprints.infra.login.BuildConfig
+import com.simprints.infra.login.exceptions.SafetyNetException
+
+class AttestationManagerImpl(private val safetyNetClient: SafetyNetClient) : AttestationManager {
+
+    override fun requestAttestation(nonce: String): String {
+        val result = getSafetyNetAttestationResponse(safetyNetClient, nonce)
+
+        return result.let {
+            checkForErrorClaimAndThrow(it.jwsResult)
+            it.jwsResult!!
+        }
+    }
+
+    private fun getSafetyNetAttestationResponse(
+        safetyNetClient: SafetyNetClient,
+        nonce: String
+    ): SafetyNetApi.AttestationResponse {
+        return try {
+            Tasks.await(
+                safetyNetClient.attest(
+                    Base64.decode(nonce, Base64.NO_WRAP),
+                    BuildConfig.SAFETYNET_API_KEY
+                )
+            )
+        } catch (e: Throwable) {
+            throw SafetyNetException(reason = SafetyNetException.SafetyNetExceptionReason.SERVICE_UNAVAILABLE)
+        }
+    }
+
+    private fun checkForErrorClaimAndThrow(jwsResult: String?) {
+        val response = JwtTokenHelper.extractTokenPayloadAsJson(jwsResult)
+
+        Simber.tag("SafetyNet Response").i(response.toString())
+
+        if (response?.has(SAFETYNET_TOKEN_ERROR_FILED) == true || response == null) {
+            throw SafetyNetException(reason = SafetyNetException.SafetyNetExceptionReason.INVALID_CLAIMS)
+        }
+    }
+
+    companion object {
+        const val SAFETYNET_TOKEN_ERROR_FILED = "error"
+    }
+}
