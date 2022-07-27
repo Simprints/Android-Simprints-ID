21c61ebff387154043d8d5913c1b736f25f23391
melad <melad@simprints.com>
2022-07-27 07:32:28 +0200

    remove unused code in fingerprint
    Former-commit-id: 71edf000bdca17989fbac6210db7e429078c08b8 [formerly a213748e6e2f9be580e33325d76cd4f36fd1e1d7]
    Former-commit-id: 3608f4e398ec1623990ac031b504b53e2928481f

--- fingerprint/src/main/java/com/simprints/fingerprint/controllers/core/repository/FingerprintDbManagerImpl.kt
+++ fingerprint/src/main/java/com/simprints/fingerprint/controllers/core/repository/FingerprintDbManagerImpl.kt
@@ -1,7 +1,6 @@
 package com.simprints.fingerprint.controllers.core.repository
 
 import com.simprints.core.domain.fingerprint.FingerprintSample
-import com.simprints.fingerprint.data.domain.fingerprint.FingerIdentifier
 import com.simprints.fingerprint.data.domain.fingerprint.Fingerprint
 import com.simprints.fingerprint.data.domain.fingerprint.FingerprintIdentity
 import com.simprints.fingerprint.data.domain.fingerprint.fromModuleApiToDomain
@@ -9,7 +8,6 @@ import com.simprints.id.data.db.subject.local.FingerprintIdentityLocalDataSource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import java.io.Serializable
-import com.simprints.id.data.db.subject.domain.FingerIdentifier as FingerIdentifierCore
 
 class FingerprintDbManagerImpl(private val coreFingerprintIdentityLocalDataSource: FingerprintIdentityLocalDataSource) : FingerprintDbManager {
 
@@ -23,17 +21,3 @@ class FingerprintDbManagerImpl(private val coreFingerprintIdentityLocalDataSourc
 
 fun FingerprintSample.fromCoreToDomain() =
     Fingerprint(fingerIdentifier.fromModuleApiToDomain(), template)
-
-fun FingerIdentifierCore.fromCoreToDomain(): FingerIdentifier =
-    when (this) {
-        FingerIdentifierCore.RIGHT_5TH_FINGER -> FingerIdentifier.RIGHT_5TH_FINGER
-        FingerIdentifierCore.RIGHT_4TH_FINGER -> FingerIdentifier.RIGHT_4TH_FINGER
-        FingerIdentifierCore.RIGHT_3RD_FINGER -> FingerIdentifier.RIGHT_3RD_FINGER
-        FingerIdentifierCore.RIGHT_INDEX_FINGER -> FingerIdentifier.RIGHT_INDEX_FINGER
-        FingerIdentifierCore.RIGHT_THUMB -> FingerIdentifier.RIGHT_THUMB
-        FingerIdentifierCore.LEFT_THUMB -> FingerIdentifier.LEFT_THUMB
-        FingerIdentifierCore.LEFT_INDEX_FINGER -> FingerIdentifier.LEFT_INDEX_FINGER
-        FingerIdentifierCore.LEFT_3RD_FINGER -> FingerIdentifier.LEFT_3RD_FINGER
-        FingerIdentifierCore.LEFT_4TH_FINGER -> FingerIdentifier.LEFT_4TH_FINGER
-        FingerIdentifierCore.LEFT_5TH_FINGER -> FingerIdentifier.LEFT_5TH_FINGER
-    }
