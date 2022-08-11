3797c5f070fef3ca3651cc69126243b2480582a3
yoann <yoann@simprints.com>
2022-08-11 09:59:43 +0200

    WIP realm migration
    Former-commit-id: 193ac3e023e8fccd5645fa16cdb57c6fa05557a5 [formerly e25f6258253854f904fb1806f83c039cac148edb]
    Former-commit-id: b5f5f07e4a5dd1b66f4fa0600b0cbb0c136140b6

--- infraconfig/src/main/java/com/simprints/infra/config/local/migrations/realm/RealmConfig.kt
+++ infraconfig/src/main/java/com/simprints/infra/config/local/migrations/realm/RealmConfig.kt
@@ -8,19 +8,12 @@ object RealmConfig {
 
     private const val REALM_SCHEMA_VERSION = 11L
 
-    @RealmModule(
-        classes = [
-            DbProject::class
-        ]
-    )
-    class Module
 
     fun get(databaseName: String, key: ByteArray): RealmConfiguration {
         val builder = RealmConfiguration
             .Builder()
             .name("$databaseName.realm")
             .schemaVersion(REALM_SCHEMA_VERSION)
-            .modules(Module())
 
         if (BuildConfig.DB_ENCRYPTION)
             builder.encryptionKey(key)
