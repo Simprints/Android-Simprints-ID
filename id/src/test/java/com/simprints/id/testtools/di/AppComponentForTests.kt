93655869df8e20279f45d17e7b1be173d8f5b694
yoann <yoann@simprints.com>
2022-08-11 14:01:24 +0200

    Fix test
    Former-commit-id: 8e038190408a650170194d408a483cc8ababadbc [formerly b1b1afe3bb271708e9d5c3556ac503b770132dc4]
    Former-commit-id: 4d5bc1805e34a035fbe355182919c9032632af5d

--- id/src/test/java/com/simprints/id/testtools/di/AppComponentForTests.kt
+++ id/src/test/java/com/simprints/id/testtools/di/AppComponentForTests.kt
@@ -18,6 +18,7 @@ import com.simprints.id.services.sync.images.ImageUpSyncWorkerTest
 import com.simprints.infra.login.LoginManagerModule
 import com.simprints.infra.login.SafetyNetModule
 import com.simprints.infra.network.NetworkModule
+import com.simprints.infra.realm.RealmModule
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -36,7 +37,8 @@ import javax.inject.Singleton
         LoginManagerModule::class,
         NetworkModule::class,
         SafetyNetModule::class,
-        TestSecurityModule::class
+        TestSecurityModule::class,
+        RealmModule::class,
     ]
 )
 interface AppComponentForTests : AppComponent {
