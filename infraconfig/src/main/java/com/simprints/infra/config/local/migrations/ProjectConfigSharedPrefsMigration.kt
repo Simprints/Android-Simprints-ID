a3cb3d3dc90300bc26c2769d87cde30c4aa5cd0b
yoann <yoann@simprints.com>
2022-08-12 16:57:13 +0200

    WIP migration from shared pref for project config
    Former-commit-id: c7fb4a0d18d2567c1d8e338e83960559b726472a [formerly a392ed8faa4bb37f34764477d6d21583d6c27f82]
    Former-commit-id: b7ebce1b59253c8a773cfd9a9bced30ab8ab801a

--- infraconfig/src/main/java/com/simprints/infra/config/local/migrations/ProjectConfigSharedPrefsMigration.kt
+++ infraconfig/src/main/java/com/simprints/infra/config/local/migrations/ProjectConfigSharedPrefsMigration.kt
@@ -1,8 +1,12 @@
 package com.simprints.infra.config.local.migrations
 
-import android.content.SharedPreferences
+import android.content.Context
 import androidx.datastore.core.DataMigration
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.simprints.infra.config.local.migrations.models.OldProjectConfig
 import com.simprints.infra.config.local.models.ProtoProjectConfiguration
+import com.simprints.infra.config.local.models.toProto
+import com.simprints.infra.logging.Simber
 import com.simprints.infra.login.LoginManager
 import javax.inject.Inject
 
@@ -10,16 +14,26 @@ import javax.inject.Inject
  * Can be removed once all the devices have been updated to 2022.3.0
  */
 internal class ProjectConfigSharedPrefsMigration @Inject constructor(
+    ctx: Context,
     private val loginManager: LoginManager,
-    private val sharedPreferences: SharedPreferences,
 ) : DataMigration<ProtoProjectConfiguration> {
 
+    private val prefs = ctx.getSharedPreferences(PREF_FILE_NAME, PREF_MODE)
+
     override suspend fun cleanUp() {
-        TODO("Not yet implemented")
+        Simber.i("Migration of configuration to Datastore done")
     }
 
+    @Suppress("BlockingMethodInNonBlockingContext")
     override suspend fun migrate(currentData: ProtoProjectConfiguration): ProtoProjectConfiguration {
-        TODO("Not yet implemented")
+        Simber.i("Start migration of configuration to Datastore")
+        val projectSettingsJson = prefs.getString(PROJECT_SETTINGS_JSON_STRING_KEY, "")
+        if (projectSettingsJson.isNullOrEmpty()) return currentData
+
+        return jacksonObjectMapper()
+            .readValue(projectSettingsJson, OldProjectConfig::class.java)
+            .toDomain(loginManager.signedInProjectId)
+            .toProto()
     }
 
     override suspend fun shouldMigrate(currentData: ProtoProjectConfiguration): Boolean =
@@ -27,5 +41,7 @@ internal class ProjectConfigSharedPrefsMigration @Inject constructor(
 
     companion object {
         private const val PROJECT_SETTINGS_JSON_STRING_KEY = "ProjectSettingsJsonString"
+        private const val PREF_FILE_NAME = "b3f0cf9b-4f3f-4c5b-bf85-7b1f44eddd7a"
+        private const val PREF_MODE = Context.MODE_PRIVATE
     }
 }
