3d8dabc552d8b61e6fd0bc2ab50081ee4b1a6758
yoann <yoann@simprints.com>
2022-08-18 14:52:39 +0200

    Add more tests
    Former-commit-id: f78643f2faf690a715c1d84c388bfcd017065052 [formerly 91f4048def6a37a0e56011555c07dec7c2f7cf58]
    Former-commit-id: 8360da33213ea995e4a04c3a85b4af5d46fab73a

--- infraconfig/src/main/java/com/simprints/infra/config/local/serializer/ProjectConfigSerializer.kt
+++ infraconfig/src/main/java/com/simprints/infra/config/local/serializer/ProjectConfigSerializer.kt
@@ -3,73 +3,15 @@ package com.simprints.infra.config.local.serializer
 import androidx.datastore.core.CorruptionException
 import androidx.datastore.core.Serializer
 import com.google.protobuf.InvalidProtocolBufferException
-import com.simprints.infra.config.domain.models.*
+import com.simprints.infra.config.local.ConfigLocalDataSourceImpl
 import com.simprints.infra.config.local.models.ProtoProjectConfiguration
-import com.simprints.infra.config.local.models.toProto
 import java.io.InputStream
 import java.io.OutputStream
 
 @Suppress("BlockingMethodInNonBlockingContext")
 internal object ProjectConfigSerializer : Serializer<ProtoProjectConfiguration> {
     override val defaultValue: ProtoProjectConfiguration =
-        ProjectConfiguration(
-            projectId = "",
-            general = GeneralConfiguration(
-                modalities = listOf(GeneralConfiguration.Modality.FINGERPRINT),
-                languageOptions = listOf(),
-                defaultLanguage = "",
-                collectLocation = true,
-                duplicateBiometricEnrolmentCheck = false,
-            ),
-            face = null,
-            fingerprint = FingerprintConfiguration(
-                fingersToCapture = listOf(
-                    FingerprintConfiguration.Finger.LEFT_THUMB,
-                    FingerprintConfiguration.Finger.LEFT_INDEX_FINGER
-                ),
-                qualityThreshold = 60,
-                decisionPolicy = DecisionPolicy(0, 0, 700),
-                allowedVeroGenerations = listOf(FingerprintConfiguration.VeroGeneration.VERO_1),
-                comparisonStrategyForVerification = FingerprintConfiguration.FingerComparisonStrategy.SAME_FINGER,
-                displayHandIcons = true,
-                vero2 = null,
-            ),
-            consent = ConsentConfiguration(
-                programName = "this program",
-                organizationName = "This organization",
-                collectConsent = true,
-                displaySimprintsLogo = true,
-                allowParentalConsent = false,
-                generalPrompt = ConsentConfiguration.ConsentPromptConfiguration(
-                    enrolmentVariant = ConsentConfiguration.ConsentEnrolmentVariant.STANDARD,
-                    dataSharedWithPartner = false,
-                    dataUsedForRAndD = false,
-                    privacyRights = true,
-                    confirmation = true,
-                ),
-                parentalPrompt = null,
-            ),
-            identification = IdentificationConfiguration(
-                maxNbOfReturnedCandidates = 10,
-                poolType = IdentificationConfiguration.PoolType.USER,
-            ),
-            synchronization = SynchronizationConfiguration(
-                frequency = SynchronizationConfiguration.Frequency.PERIODICALLY,
-                up = UpSynchronizationConfiguration(
-                    simprints = UpSynchronizationConfiguration.SimprintsUpSynchronizationConfiguration(
-                        kind = UpSynchronizationConfiguration.UpSynchronizationKind.NONE
-                    ),
-                    coSync = UpSynchronizationConfiguration.CoSyncUpSynchronizationConfiguration(
-                        kind = UpSynchronizationConfiguration.UpSynchronizationKind.NONE
-                    )
-                ),
-                down = DownSynchronizationConfiguration(
-                    partitionType = DownSynchronizationConfiguration.PartitionType.USER,
-                    maxNbOfModules = 6,
-                    moduleOptions = listOf(),
-                ),
-            ),
-        ).toProto()
+        ConfigLocalDataSourceImpl.defaultProjectConfiguration
 
     override suspend fun readFrom(input: InputStream): ProtoProjectConfiguration {
         try {
