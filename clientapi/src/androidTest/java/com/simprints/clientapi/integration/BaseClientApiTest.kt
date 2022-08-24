9f90a6d1d6b392e2f1ab076c4752362a5585898f
Tristram Norman <tristram@simprints.com>
2022-08-24 12:06:40 +0100

    remove koin lib and files
    Former-commit-id: 09ff0dd0e6a6a0cc876427da7daf7f23b83040c7 [formerly 83934108bfeb0637214c76908e7f40e18e9a2d30]
    Former-commit-id: 449589697e9e70145bdffe6ecb821497c9204a9f

--- clientapi/src/androidTest/java/com/simprints/clientapi/integration/BaseClientApiTest.kt
+++ clientapi/src/androidTest/java/com/simprints/clientapi/integration/BaseClientApiTest.kt
@@ -8,7 +8,6 @@ import android.os.Parcelable
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
-import com.simprints.clientapi.di.KoinInjector
 import com.simprints.eventsystem.event.EventRepository
 import com.simprints.eventsystem.event.domain.models.session.SessionCaptureEvent
 import com.simprints.moduleapi.app.responses.IAppResponse
