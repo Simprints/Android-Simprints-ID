1d7c9fb186117cfb761eeb0210030917f6008706
Milen Marinov <milen.danchev.marinov@gmail.com>
2022-08-25 13:29:47 +0300

    CORE-1756 Add test for the new reconnect behaviour of ScannerOffFragment
    Former-commit-id: 7791a71545d6685fe11e3c0463256746f7097965 [formerly 13420b9b8619b2168d92842d574392ab83174b54]
    Former-commit-id: 4b176c347a76b6fc510bac6d2b1848e9aac4cf61

--- fingerprint/src/androidTest/java/com/simprints/fingerprint/activities/connect/ConnectScannerActivityAndroidTest.kt
+++ fingerprint/src/androidTest/java/com/simprints/fingerprint/activities/connect/ConnectScannerActivityAndroidTest.kt
@@ -8,10 +8,11 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import androidx.test.espresso.matcher.ViewMatchers.isRoot
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
 import com.google.common.truth.Truth.assertThat
@@ -31,11 +32,19 @@ import com.simprints.fingerprint.scanner.ScannerManagerImpl
 import com.simprints.fingerprint.scanner.wrapper.ScannerWrapper
 import com.simprints.fingerprint.testtools.FullAndroidTestConfigRule
 import com.simprints.fingerprint.tools.livedata.postEvent
+import com.simprints.fingerprint.R
+import com.simprints.fingerprint.scanner.domain.ScannerGeneration
+import com.simprints.fingerprint.scanner.domain.versions.ScannerFirmwareVersions
+import com.simprints.fingerprint.scanner.domain.versions.ScannerVersion
+import com.simprints.fingerprint.scanner.exceptions.unexpected.UnknownScannerIssueException
 import com.simprints.id.Application
+import com.simprints.testtools.android.getResourceString
 import com.simprints.testtools.android.tryOnSystemUntilTimeout
+import com.simprints.testtools.android.waitOnUi
 import com.simprints.testtools.common.mock.MockTimer
 import io.mockk.*
 import io.reactivex.Completable
+import org.hamcrest.CoreMatchers.not
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -74,10 +83,18 @@ class ConnectScannerActivityAndroidTest : KoinTest {
     }
     private val scanner: ScannerWrapper = mockk<ScannerWrapper>().apply {
         every { isLiveFeedbackAvailable() } returns false
+        every { connect() } returns Completable.complete()
+        every { versionInformation() } returns ScannerVersion(
+            hardwareVersion = "",
+            generation = ScannerGeneration.VERO_1,
+            firmware = ScannerFirmwareVersions("", "", "")
+        )
+        every { setup() } returns Completable.complete()
     }
     private val scannerManager: ScannerManager =
         spyk(ScannerManagerImpl(mockk(), mockk(), mockk(), mockk())) {
             every { checkBluetoothStatus() } returns Completable.complete()
+            every { initScanner() } returns Completable.complete()
         }
     private val nfcManager: NfcManager = mockk()
 
@@ -131,6 +148,50 @@ class ConnectScannerActivityAndroidTest : KoinTest {
         intended(hasComponent(AlertActivity::class.java.name))
 
         Intents.release()
+    }
+
+    @Test
+    fun onScannerOff_scannerOffFragmentMakesOnly5ConnectionAttemptsAtATime() {
+        // Setup connection attempts to be unsuccessful
+        every { scanner.setUiIdle() } throws UnknownScannerIssueException()
+
+        scenario = ActivityScenario.launch(connectScannerTaskRequest().toIntent())
+
+        val scannerId = "123"
+        viewModelMock.showScannerErrorDialogWithScannerId.postEvent(scannerId)
+
+        onView(withResourceName("alertTitle"))
+            .check(matches(withText(getResourceString(R.string.scanner_id_confirmation_message).format(scannerId))))
+
+        onView(withText(getResourceString(R.string.scanner_confirmation_yes)))
+            .perform(ViewActions.click())
+
+        // Verify ScannerOffFragment is displayed
+        onView(withId(R.id.scannerOffTitleTextView)).check(matches(isDisplayed()))
+        // Verify it immediately starts connection attempts
+        onView(withId(R.id.scannerOffProgressBar)).check(matches(isDisplayed()))
+        onView(withId(R.id.tryAgainButton)).check(matches(not(isDisplayed())))
+        waitOnUi(500)
+        var retryCounter = 5
+        verify(exactly = retryCounter) { viewModelMock.retryConnect() }
+
+        // Verify connection attempts stop after 5 unsuccessful ones
+        onView(withId(R.id.scannerOffProgressBar)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.tryAgainButton)).check(matches(isDisplayed()))
+        verify(exactly = retryCounter) { viewModelMock.retryConnect() }
+
+        // Clicking "Try again" starts 5 new connection attempts
+        onView(withId(R.id.tryAgainButton)).perform(ViewActions.click())
+        onView(withId(R.id.scannerOffProgressBar)).check(matches(isDisplayed()))
+        onView(withId(R.id.tryAgainButton)).check(matches(not(isDisplayed())))
+        waitOnUi(500)
+        retryCounter += 5
+        verify(exactly = retryCounter) { viewModelMock.retryConnect() }
+
+        // Then it stops again
+        onView(withId(R.id.scannerOffProgressBar)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.tryAgainButton)).check(matches(isDisplayed()))
+        verify(exactly = retryCounter) { viewModelMock.retryConnect() }
     }
 
     @Test
