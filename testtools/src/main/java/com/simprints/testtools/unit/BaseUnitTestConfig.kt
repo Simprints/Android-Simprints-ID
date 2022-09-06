da4b3af24b53fc50bbf1441c379a741bf4b145d6
melad <melad@simprints.com>
2022-09-06 11:48:32 +0200

    Revert "Remove rescheduleRxMainThread"
    This reverts commit a05d03d3b82ffa3c0d09de567e075e116cd75ca6 [formerly e9ad064d535fb29a95580d05d76fc67d5c733dd2] [formerly 5c7d45af4459ed393456f4986a085bcc4e8a294b [formerly e8d13c6a2b18080d2f0cb8951d1021f2b607abe3]].
    
    
    Former-commit-id: 0c7b6b2b1dab04ab03a79713d5d6853ebcc62344 [formerly fee0dc54c5c84936e86f713f206bd5eb3413b725]
    Former-commit-id: f95913c802869b7a2ff40433af5f76a528378813

--- testtools/src/main/java/com/simprints/testtools/unit/BaseUnitTestConfig.kt
+++ testtools/src/main/java/com/simprints/testtools/unit/BaseUnitTestConfig.kt
@@ -1,15 +1,29 @@
 package com.simprints.testtools.unit
 
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.setMain
 
 @ExperimentalCoroutinesApi
 open class BaseUnitTestConfig {
 
+    private val schedulerInstance = Schedulers.trampoline()
+
+    open fun rescheduleRxMainThread() = also {
+        // AndroidSchedulers.mainThread() requests Android APi, not available in unit tests
+        // Setting a special main Scheduler https://medium.com/@peter.tackage/overriding-rxandroid-schedulers-in-rxjava-2-5561b3d14212
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { schedulerInstance }
+        RxJavaPlugins.setIoSchedulerHandler { schedulerInstance }
+        RxJavaPlugins.setNewThreadSchedulerHandler { schedulerInstance }
+        RxJavaPlugins.setComputationSchedulerHandler { schedulerInstance }
+    }
+
     open fun coroutinesMainThread() = apply {
-        Dispatchers.setMain(StandardTestDispatcher())
+        Dispatchers.setMain(TestCoroutineDispatcher())
     }
 
 }
