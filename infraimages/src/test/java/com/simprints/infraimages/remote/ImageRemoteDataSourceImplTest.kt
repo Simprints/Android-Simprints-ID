7cc64adf1036d2691f29829e823b1f83f7b4774d
Tristram Norman <tristram@simprints.com>
2022-09-15 08:47:46 +0100

    WIP update tests
    Former-commit-id: de15efbd23b885ff319bb7074f5cafd6ca311bfa [formerly c23017e513793818f35b671d4452bff3c9633642]
    Former-commit-id: cbd43a42fa6f4eae5ae5885296090e15859196d0

--- infraimages/src/test/java/com/simprints/infraimages/remote/ImageRemoteDataSourceImplTest.kt
+++ infraimages/src/test/java/com/simprints/infraimages/remote/ImageRemoteDataSourceImplTest.kt
@@ -1,31 +1,99 @@
 package com.simprints.infraimages.remote
 
-import com.google.firebase.FirebaseApp
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.firebase.storage.FirebaseStorage
 import com.simprints.core.sharedinterfaces.ImageUrlProvider
 import com.simprints.infra.login.LoginManager
+import com.simprints.infraimages.model.SecuredImageRef
+import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.mockkStatic
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.FileInputStream
 
+@RunWith(AndroidJUnit4::class)
 class ImageRemoteDataSourceImplTest {
 
-    companion object {
-        private val fbMock = mockk<FirebaseApp>()
+    @Before
+    fun setup() {
+        // We need to mock statics and global extensions
+        mockkStatic(FirebaseStorage::class)
+        mockkStatic("kotlinx.coroutines.tasks.TasksKt")
     }
 
     @Test
-    fun `test image upload flow`() {
+    fun `test image upload flow`() = runTest() {
+
+        val imgUrlProviderMock = mockk<ImageUrlProvider>() {
+            coEvery { getImageStorageBucketUrl() } returns "gs://`simprints-dev.appspot.com"
+        }
+
+        val loginManagerMock = mockk<LoginManager>(relaxed = true) {
+            every { getLegacyAppFallback() } returns mockk(relaxed = true)
+            every { getLegacyAppFallback().options.projectId } returns "projectId"
+        }
+
+        val storageMock = mockk<FirebaseStorage>(relaxed = true) {
+            every { reference } returns mockk()
+
+            every { reference.child(any()) } returns mockk(relaxed = true) {
+                every { reference.path } returns "testPath"
+
+                coEvery { reference.putStream(any()).await() } coAnswers {
+                    mockk {
+                        every { task.isSuccessful } returns true
+                    }
+                }
+            }
+        }
+
+        every { FirebaseStorage.getInstance(any(), any()) } returns storageMock
+
+        val remoteDataSource = ImageRemoteDataSourceImpl(imgUrlProviderMock, loginManagerMock)
+        val imageStreamMock = mockk<FileInputStream>(relaxed = true)
+        
+        val securedImageRefMock = mockk<SecuredImageRef>(relaxed = true) {
+            every { relativePath.parts } returns arrayOf("Test1")
+        }
+
+        remoteDataSource.uploadImage(imageStreamMock, securedImageRefMock)
+    }
+
+    @Test
+    fun `null project returns failed upload`() = runTest {
 
         val imgUrlProviderMock = mockk<ImageUrlProvider>()
 
-        val loginManagerMock = mockk<LoginManager>(relaxed = true)
-        every { loginManagerMock.getLegacyAppFallback() } returns fbMock
-
+        val loginManagerMock = mockk<LoginManager>(relaxed = true) {
+            every { getLegacyAppFallback().options.projectId } returns null
+        }
 
         val remoteDataSource = ImageRemoteDataSourceImpl(imgUrlProviderMock, loginManagerMock)
+        val rtn = remoteDataSource.uploadImage(mockk(), mockk())
 
-        //remoteDataSource.uploadImage(mockk(), mockk())
+        assert(!rtn.isUploadSuccessful())
+    }
 
+    @Test
+    fun `null storage bucket returns failed upload`() = runTest {
+
+        val imgUrlProviderMock = mockk<ImageUrlProvider>() {
+            coEvery { getImageStorageBucketUrl() } returns null
+        }
+
+        val loginManagerMock = mockk<LoginManager>(relaxed = true) {
+            every { getLegacyAppFallback().options.projectId } returns "projectId"
+        }
+
+        val remoteDataSource = ImageRemoteDataSourceImpl(imgUrlProviderMock, loginManagerMock)
+        val rtn = remoteDataSource.uploadImage(mockk(), mockk())
+
+        assert(!rtn.isUploadSuccessful())
     }
 
 }
