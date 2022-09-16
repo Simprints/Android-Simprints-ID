9937e5614b65ca7173f453e9a039afb9d46cfccb
Tristram Norman <tristram@simprints.com>
2022-09-16 13:40:51 +0100

    fix merge conflict
    Former-commit-id: 12bc0ca284832e3a3db5484ba804a689a4fe5cad [formerly d17d0205c73e7d444e68df13054468a7183419f1]
    Former-commit-id: 8f8bb50d751b88a077e28126366b28db97a2f6bd

--- id/src/main/java/com/simprints/id/di/DataModule.kt
+++ id/src/main/java/com/simprints/id/di/DataModule.kt
@@ -26,7 +26,6 @@ import com.simprints.id.data.db.subject.local.SubjectLocalDataSourceImpl
 import com.simprints.id.data.prefs.RemoteConfigWrapper
 import com.simprints.id.enrolmentrecords.remote.EnrolmentRecordRemoteDataSource
 import com.simprints.id.enrolmentrecords.remote.EnrolmentRecordRemoteDataSourceImpl
-import com.simprints.id.network.ImageUrlProvider
 import com.simprints.infra.login.LoginManager
 import com.simprints.infra.realm.RealmWrapper
 import dagger.Module
