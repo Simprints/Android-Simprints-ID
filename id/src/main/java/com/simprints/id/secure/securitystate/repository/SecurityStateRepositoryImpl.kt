49824dcb1d850e3c859333c2b41b8786d97ac4c6
melad <melad@simprints.com>
2022-10-03 09:22:17 +0200

    remove unused imports
    Former-commit-id: 361ec24897b00762b93ed456169e0860e0aa6f06 [formerly 46477b4534fb6ed382bd72a0e822e395e05f4297]
    Former-commit-id: 7829247227806fda24b8a32407df1b43d6436992

--- id/src/main/java/com/simprints/id/secure/securitystate/repository/SecurityStateRepositoryImpl.kt
+++ id/src/main/java/com/simprints/id/secure/securitystate/repository/SecurityStateRepositoryImpl.kt
@@ -3,11 +3,7 @@ package com.simprints.id.secure.securitystate.repository
 import com.simprints.id.secure.models.SecurityState
 import com.simprints.id.secure.securitystate.local.SecurityStateLocalDataSource
 import com.simprints.id.secure.securitystate.remote.SecurityStateRemoteDataSource
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.launch
 
 @ExperimentalCoroutinesApi
 class SecurityStateRepositoryImpl(
@@ -24,10 +20,4 @@ class SecurityStateRepositoryImpl(
     override fun getSecurityStatusFromLocal(): SecurityState.Status {
         return localDataSource.securityStatus
     }
-
-    private suspend fun Channel<SecurityState.Status>.update(status: SecurityState.Status) {
-        if (!isClosedForSend)
-            send(status)
-    }
-
 }
