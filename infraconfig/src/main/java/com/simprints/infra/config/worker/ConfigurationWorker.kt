dbdc1e9e5cbbb1d8aa085798449c954f7abcb99c
yoann <yoann@simprints.com>
2022-10-05 16:51:46 +0200

    Fix test
    Former-commit-id: 1f3e4d022325fe868f058956a9bcac2fba15fc25 [formerly 8805761882b353087550f059bcfe585dd5a25204]
    Former-commit-id: a3340dbf0e45098fbe3eeef538cf3e3fe1cf8c91

--- infraconfig/src/main/java/com/simprints/infra/config/worker/ConfigurationWorker.kt
+++ infraconfig/src/main/java/com/simprints/infra/config/worker/ConfigurationWorker.kt
@@ -3,7 +3,7 @@ package com.simprints.infra.config.worker
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import com.simprints.infra.config.ConfigManager
+import com.simprints.infra.config.domain.ConfigService
 import com.simprints.infra.logging.Simber
 import com.simprints.infra.login.LoginManager
 import dagger.assisted.Assisted
@@ -19,7 +19,7 @@ class ConfigurationWorker @AssistedInject constructor(
     @Assisted context: Context,
     @Assisted params: WorkerParameters,
     private val loginManager: LoginManager,
-    private val configManager: ConfigManager,
+    private val configService: ConfigService,
 ) : CoroutineWorker(context, params) {
 
     private val dispatcher: CoroutineDispatcher = Dispatchers.IO
@@ -35,7 +35,7 @@ class ConfigurationWorker @AssistedInject constructor(
                     return@withContext Result.failure()
                 }
 
-                configManager.refreshProjectConfiguration(projectId)
+                configService.refreshConfiguration(projectId)
                 Simber.tag(tag).i("Successfully refresh the project configuration")
                 Result.success()
             } catch (e: Exception) {
