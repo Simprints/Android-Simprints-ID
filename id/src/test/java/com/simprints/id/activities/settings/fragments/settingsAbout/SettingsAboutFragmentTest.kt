24f2c083a90c274c49024200a8a92d10648c430a
Tristram Norman <tristram@simprints.com>
2022-10-07 10:53:16 +0100

    fix id tests!
    Former-commit-id: ad67863e2b28d9c4e740ee7c49c5418506ac4900 [formerly ae443303aa06cf5d381a788dbe749bb65879021f]
    Former-commit-id: 7aa961ca94014f9c23e1e491f1ba5b1bd72ea514

--- id/src/test/java/com/simprints/id/activities/settings/fragments/settingsAbout/SettingsAboutFragmentTest.kt
+++ id/src/test/java/com/simprints/id/activities/settings/fragments/settingsAbout/SettingsAboutFragmentTest.kt
@@ -27,7 +27,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
-import com.simprints.resources.R as IDR
+import com.simprints.infra.resources.R as IDR
 
 @RunWith(AndroidJUnit4::class)
 @Config(application = TestApplication::class)
