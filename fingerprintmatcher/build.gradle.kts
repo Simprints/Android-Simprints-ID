eba3a7dc2e0e6798e08d2f81dfc60323280e38fb
yoann <yoann@simprints.com>
2022-10-12 15:42:45 +0200

    Add Hilt to the fingerprint matcher module
    Former-commit-id: dc5e1c2b16a90cc41cbb72df8b3cc73ade7c925d [formerly f17cc969ad2a2ee50e440b7b8bd84c954712912b]
    Former-commit-id: 3586e52cc6e7a4089f626c00ed694ee4c65190ce

--- fingerprintmatcher/build.gradle.kts
+++ fingerprintmatcher/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("kotlin-android")
+    kotlin("kapt")
 }
 
 apply {
@@ -26,9 +27,15 @@ dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(libs.simmatcher)
     implementation(libs.androidX.annotation.annotation)
+
     // Kotlin
     implementation(libs.kotlin.coroutinesAndroid)
     compileOnly(libs.androidX.annotation.annotation)
+
+    // Hilt
+    implementation(libs.hilt)
+    kapt(libs.hilt.kapt)
+
     // Mocking and assertion frameworks
     testImplementation(libs.testing.truth)
     testImplementation(libs.testing.mockk.core)
