b665e1c4d8ffaf6e18bb26aee9064bd46f88c915
melad <melad@simprints.com>
2022-12-19 14:31:56 +0200

    Add tests to SecureLocalDbKeyProviderImplTest
    Former-commit-id: f67dd74587b8dc2aa557d7df58cc5c1bbc5bfc05 [formerly 156720c5b0b8ca0eda82ddc166c58811b8c1004b]
    Former-commit-id: 1ba5bf5ae8fb1d75c7ed31e9985896e1aaefc5e7

--- infrasecurity/src/test/java/com/simprints/infra/security/keyprovider/SecureLocalDbKeyProviderImplTest.kt
+++ infrasecurity/src/test/java/com/simprints/infra/security/keyprovider/SecureLocalDbKeyProviderImplTest.kt
@@ -5,13 +5,13 @@ import android.util.Base64.DEFAULT
 import android.util.Base64.encodeToString
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
+import com.simprints.infra.logging.Simber
+import com.simprints.infra.security.exceptions.MatchingLocalDatabaseKeyHashesException
 import com.simprints.infra.security.exceptions.MissingLocalDatabaseKeyException
+import com.simprints.infra.security.exceptions.MissingLocalDatabaseKeyHashException
 import com.simprints.infra.security.random.RandomGenerator
 import com.simprints.testtools.common.syntax.assertThrows
-import io.mockk.MockKMatcherScope
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
+import io.mockk.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,23 +21,36 @@ class SecureLocalDbKeyProviderImplTest {
 
     companion object {
         private const val DB_NAME = "name"
+        private const val KEY_NAME = "REALM_KEY_$DB_NAME"
     }
 
     private val sharedPreferencesBuilder = mockk<EncryptedSharedPreferencesBuilder>()
-    private val sharedPrefs = mockk<SharedPreferences>()
-    private val randomGenerator = mockk<RandomGenerator>()
-    private val editor = mockk<SharedPreferences.Editor>(relaxed = true)
-    private val dbKeyProvider = SecureLocalDbKeyProviderImpl(sharedPreferencesBuilder, randomGenerator)
+    private val dbKeySharedPrefs = mockk<SharedPreferences>()
+    private val hashSharedPrefs = mockk<SharedPreferences>()
+    private val randomGenerator = mockk<RandomGenerator> {
+        every { generateByteArray() } returns byteArrayOf(1, 2, 3)
+    }
+    private val dbKeyEditor = mockk<SharedPreferences.Editor>(relaxed = true)
+    private val keyHashEditor = mockk<SharedPreferences.Editor>(relaxed = true)
+    private val dbKeyProvider =
+        SecureLocalDbKeyProviderImpl(sharedPreferencesBuilder, randomGenerator)
 
     @Before
     fun setup() {
-        every { sharedPreferencesBuilder.buildEncryptedSharedPreferences(any()) } returns sharedPrefs
-        every { sharedPrefs.edit() } returns editor
+        every {
+            sharedPreferencesBuilder.buildEncryptedSharedPreferences(SecureLocalDbKeyProvider.FILENAME_FOR_REALM_KEY_SHARED_PREFS)
+        } returns dbKeySharedPrefs
+        every { dbKeySharedPrefs.edit() } returns dbKeyEditor
+        every {
+            sharedPreferencesBuilder.buildEncryptedSharedPreferences(SecureLocalDbKeyProvider.FILENAME_FOR_KEY_HASHES_SHARED_PREFS)
+        } returns hashSharedPrefs
+        every { hashSharedPrefs.edit() } returns keyHashEditor
+        mockkObject(Simber)
     }
 
     @Test
     fun `get local db key should throw an error if the key is missing`() {
-        every { sharedPrefs.getString(any(), any()) } returns null
+        every { dbKeySharedPrefs.getString(any(), any()) } returns null
 
         assertThrows<MissingLocalDatabaseKeyException> {
             dbKeyProvider.getLocalDbKeyOrThrow(DB_NAME)
@@ -47,7 +60,8 @@ class SecureLocalDbKeyProviderImplTest {
     @Test
     fun `get local db key should returns the key`() {
         val key = "aKey".toByteArray()
-        every { sharedPrefs.getString(any(), any()) } returns encodeToString(key, DEFAULT)
+        every { dbKeySharedPrefs.getString(KEY_NAME, null) } returns encodeToString(key, DEFAULT)
+        every { hashSharedPrefs.getString(KEY_NAME, null) } returns "Hash"
 
         val receivedKey = dbKeyProvider.getLocalDbKeyOrThrow(DB_NAME)
         assertThat(receivedKey).isEqualTo(LocalDbKey(DB_NAME, key))
@@ -58,20 +72,74 @@ class SecureLocalDbKeyProviderImplTest {
         val key = "aKey".toByteArray()
 
         every { randomGenerator.generateByteArray() } returns key
-        every { sharedPrefs.getString(any(), any()) } returns null
+        every { dbKeySharedPrefs.getString(any(), any()) } returns null
 
         dbKeyProvider.createLocalDatabaseKeyIfMissing(DB_NAME)
 
-        verify { editor.putString(containsString(DB_NAME), encodeToString(key, DEFAULT)) }
+        verify { dbKeyEditor.putString(containsString(DB_NAME), encodeToString(key, DEFAULT)) }
     }
 
     @Test
     fun `create local db key should not generate a new key if it exists`() {
-        every { sharedPrefs.getString(any(), any()) } returns "aKey"
+        every { dbKeySharedPrefs.getString(any(), any()) } returns "aKey"
 
         dbKeyProvider.createLocalDatabaseKeyIfMissing(DB_NAME)
 
-        verify(exactly = 0) { editor.putString(any(), any()) }
+        verify(exactly = 0) { dbKeyEditor.putString(any(), any()) }
+    }
+
+    @Test
+    fun `test recreate db key will log MissingLocalDatabaseKeyHashException if there is no hash stored`() {
+        every { dbKeySharedPrefs.getString(KEY_NAME, null) } returns "aKey"
+        every { hashSharedPrefs.getString(KEY_NAME, null) } returns null
+
+        dbKeyProvider.recreateLocalDatabaseKey(DB_NAME)
+
+        verify {
+            dbKeyEditor.putString(KEY_NAME, any())
+            keyHashEditor.putString(KEY_NAME, any())
+            Simber.i(any<MissingLocalDatabaseKeyHashException>())
+        }
+
+    }
+    @Test
+    fun `test recreate db key will log MatchingLocalDatabaseKeyHashesException if there is hash stored`() {
+        every { dbKeySharedPrefs.getString(KEY_NAME, null) } returns "aKey"
+        every { hashSharedPrefs.getString(KEY_NAME, null) } returns  "Hash"
+
+        dbKeyProvider.recreateLocalDatabaseKey(DB_NAME)
+
+        verify {
+            dbKeyEditor.putString(KEY_NAME, any())
+            keyHashEditor.putString(KEY_NAME, any())
+            Simber.i(any<MatchingLocalDatabaseKeyHashesException>())
+        }
+    }
+
+    @Test
+    fun `test recreate db key will log MismatchingLocalDatabaseKeyHashesException if the same hash stored`() {
+        every { dbKeySharedPrefs.getString(KEY_NAME, null) } returns "key"
+        every { hashSharedPrefs.getString(KEY_NAME, null) } returns  "8335fa56d487562de248f47befc72743334051ddffcc2c09275f665454990317594745ee17c08f798cd7dce0ba8155dcda14f6398c1d1545116520a133017c09"
+
+        dbKeyProvider.recreateLocalDatabaseKey(DB_NAME)
+
+        verify {
+            dbKeyEditor.putString(KEY_NAME, any())
+            keyHashEditor.putString(KEY_NAME, any())
+            Simber.i(any<MatchingLocalDatabaseKeyHashesException>())
+        }
+    }
+    @Test
+    fun `test recreate db key will log MissingLocalDatabaseKeyException if no key stored`() {
+        every { dbKeySharedPrefs.getString(KEY_NAME, null) } returns null
+
+        dbKeyProvider.recreateLocalDatabaseKey(DB_NAME)
+
+        verify {
+            dbKeyEditor.putString(KEY_NAME, any())
+            keyHashEditor.putString(KEY_NAME, any())
+            Simber.i(any<MissingLocalDatabaseKeyException>())
+        }
     }
 
     private fun MockKMatcherScope.containsString(s: String) = match<String> {
