13029c9536d39d8763877dae6b57a5d32c82bfc8
Milen Marinov <milen.danchev.marinov@gmail.com>
2023-01-13 14:30:00 +0200

    CORE-2147 Add synchronization lock to avoid race conditions in Room DB corruption handling
    Former-commit-id: a1c03dcd37597cd7112aaebb43026bf8100a0f6c [formerly c7db4f17e95d643240c9130d2efd9932c52a86a6]
    Former-commit-id: fdb33cc3d38e5f58c9f395ef6e0bcf7c0f10a382

--- eventsystem/src/main/java/com/simprints/eventsystem/event/local/EventLocalDataSourceImpl.kt
+++ eventsystem/src/main/java/com/simprints/eventsystem/event/local/EventLocalDataSourceImpl.kt
@@ -11,6 +11,8 @@ import com.simprints.infra.logging.Simber
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
 import net.sqlcipher.database.SQLiteDatabaseCorruptException
 import net.sqlcipher.database.SQLiteException
@@ -24,6 +26,7 @@ internal open class EventLocalDataSourceImpl @Inject constructor(
 ) : EventLocalDataSource {
 
     private var eventDao: EventRoomDao = eventDatabaseFactory.build().eventDao
+    private val mutex = Mutex()
 
     private suspend fun <R> useRoom(context: CoroutineContext, block: suspend () -> R): R {
         return withContext(context) {
@@ -42,17 +45,19 @@ internal open class EventLocalDataSourceImpl @Inject constructor(
     }
 
     private suspend fun <R> handleDatabaseCorruption(ex: Exception, block: suspend () -> R): R {
-        //DB corruption detected; either DB file or key is corrupt
-        //1. Delete DB file in order to create a new one at next init
-        eventDatabaseFactory.deleteDatabase()
-        //2. Recreate the DB key
-        eventDatabaseFactory.recreateDatabaseKey()
-        //3. Log exception after recreating the key so we get extra info
-        Simber.tag(DB_CORRUPTION.name).e(ex)
-        //4. Rebuild database
-        eventDao = eventDatabaseFactory.build().eventDao
-        //5. Retry operation with new file and key
-        return block()
+        mutex.withLock {
+            //DB corruption detected; either DB file or key is corrupt
+            //1. Delete DB file in order to create a new one at next init
+            eventDatabaseFactory.deleteDatabase()
+            //2. Recreate the DB key
+            eventDatabaseFactory.recreateDatabaseKey()
+            //3. Log exception after recreating the key so we get extra info
+            Simber.tag(DB_CORRUPTION.name).e(ex)
+            //4. Rebuild database
+            eventDao = eventDatabaseFactory.build().eventDao
+            //5. Retry operation with new file and key
+            return block()
+        }
     }
 
     override suspend fun loadAll(): Flow<Event> =
