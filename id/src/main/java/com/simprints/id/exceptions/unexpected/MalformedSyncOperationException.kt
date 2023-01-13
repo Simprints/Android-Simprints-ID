b686978aa88e092ea98765ff83bba507fb32f91e
yoann <yoann@simprints.com>
2023-01-13 09:51:09 +0100

    Recreate a new up sync scope when the current one is an invalid JSON
    Former-commit-id: d1dfaa41aac2a7b181337bb1190532d6b860df50 [formerly e74e17063925ae0fe576bae2d4745de8fa6f14d0]
    Former-commit-id: 6584e0ffebcb956bc63b7b71d28fe06d4565e019

--- id/src/main/java/com/simprints/id/exceptions/unexpected/MalformedSyncOperationException.kt
+++ id/src/main/java/com/simprints/id/exceptions/unexpected/MalformedSyncOperationException.kt
@@ -2,6 +2,6 @@ package com.simprints.id.exceptions.unexpected
 
 import com.simprints.core.exceptions.UnexpectedException
 
-class MalformedDownSyncOperationException(
+class MalformedSyncOperationException(
     message: String = "People downsync operation is malformed as input for downloader worker"
 ): UnexpectedException(message)
