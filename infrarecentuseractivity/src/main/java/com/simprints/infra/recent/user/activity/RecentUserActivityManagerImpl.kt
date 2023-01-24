e940256c861806f974c3b399ac09adb991bd2af0
Sergejs Luhmirins <sergejs.luhmirins@gmail.com>
2023-01-24 17:17:50 +0200

    CORE-2035 Set explicit background dispatcher in the infrarecentuseractivity module
    Former-commit-id: 09d52b148f52c664e3b7e03d6f3ce2bff6be580c [formerly 23d97c674bcd4df4567b6341221f8b08af432d35]
    Former-commit-id: 13212df95506942a55774c729410c5490a1c754f

--- infrarecentuseractivity/src/main/java/com/simprints/infra/recent/user/activity/RecentUserActivityManagerImpl.kt
+++ infrarecentuseractivity/src/main/java/com/simprints/infra/recent/user/activity/RecentUserActivityManagerImpl.kt
@@ -2,19 +2,17 @@ package com.simprints.infra.recent.user.activity
 
 import com.simprints.infra.recent.user.activity.domain.RecentUserActivity
 import com.simprints.infra.recent.user.activity.local.RecentUserActivityLocalSource
-
 import javax.inject.Inject
 
 internal class RecentUserActivityManagerImpl @Inject constructor(
-    private val localSource: RecentUserActivityLocalSource
+    private val localSource: RecentUserActivityLocalSource,
 ) : RecentUserActivityManager {
 
-    override suspend fun getRecentUserActivity(): RecentUserActivity =
-        localSource.getRecentUserActivity()
+    override suspend fun getRecentUserActivity(): RecentUserActivity = localSource.getRecentUserActivity()
 
-    override suspend fun updateRecentUserActivity(update: suspend (t: RecentUserActivity) -> RecentUserActivity): RecentUserActivity =
-        localSource.updateRecentUserActivity(update)
+    override suspend fun updateRecentUserActivity(
+        update: suspend (t: RecentUserActivity) -> RecentUserActivity
+    ): RecentUserActivity = localSource.updateRecentUserActivity(update)
 
-    override suspend fun clearRecentActivity() =
-        localSource.clearRecentActivity()
+    override suspend fun clearRecentActivity() = localSource.clearRecentActivity()
 }
