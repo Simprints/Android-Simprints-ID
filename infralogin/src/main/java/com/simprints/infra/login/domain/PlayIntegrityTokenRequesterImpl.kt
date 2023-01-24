dd6b9734e903070114a9ab3b9b7d58170142e756
melad <melad@simprints.com>
2023-01-24 15:08:23 +0200

    Set nonce without encoding to base64
    Former-commit-id: 1a4e106858fec9c95f4a6d60aa0afe340688eebf [formerly 93a19d79df8f77f65074f6f1a45a1e03264fee0b]
    Former-commit-id: 6a6005f595bd5a76612714f6a0daa9e691e7df5e

--- infralogin/src/main/java/com/simprints/infra/login/domain/PlayIntegrityTokenRequesterImpl.kt
+++ infralogin/src/main/java/com/simprints/infra/login/domain/PlayIntegrityTokenRequesterImpl.kt
@@ -1,6 +1,5 @@
 package com.simprints.infra.login.domain
 
-import android.util.Base64
 import com.google.android.gms.tasks.Tasks
 import com.google.android.play.core.integrity.IntegrityManager
 import com.google.android.play.core.integrity.IntegrityTokenRequest
@@ -26,7 +25,7 @@ internal class PlayIntegrityTokenRequesterImpl @Inject constructor(private val p
                 playIntegrityManager.requestIntegrityToken(
                     IntegrityTokenRequest
                         .builder()
-                        .setNonce(nonce.encodeToBase64())
+                        .setNonce(nonce)
                         .setCloudProjectNumber(BuildConfig.CLOUD_PROJECT_ID.toLong())
                         .build()
                 )
@@ -34,10 +33,4 @@ internal class PlayIntegrityTokenRequesterImpl @Inject constructor(private val p
         } catch (e: Throwable) {
             throw PlayIntegrityException(reason = SERVICE_UNAVAILABLE)
         }
-
 }
-
-// Encode nonce to base64 hex string
-private fun String.encodeToBase64(): String =
-    Base64.encodeToString(this.toByteArray(), Base64.NO_WRAP)
-
