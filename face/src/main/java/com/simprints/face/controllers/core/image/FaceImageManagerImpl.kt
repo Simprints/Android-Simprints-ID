46ce865eaa9c9a74e0c6ff4ca5b5a3404f83a68e
Sergejs Luhmirins <sergejs.luhmirins@gmail.com>
2023-01-27 15:51:53 +0200

    CORE-2035 Set correct background dispatcher in face module
    Former-commit-id: 6bb282095dd22533a2f85e5a1b7041ecdeca1c6c [formerly 67b5979d7fa40e4f474159fbc40f86c01a0aee50]
    Former-commit-id: 1dfc94b6f5309a5f3eeaeddf79a40c26ec8b3ecd

--- face/src/main/java/com/simprints/face/controllers/core/image/FaceImageManagerImpl.kt
+++ face/src/main/java/com/simprints/face/controllers/core/image/FaceImageManagerImpl.kt
@@ -1,32 +1,37 @@
 package com.simprints.face.controllers.core.image
 
+import com.simprints.core.DispatcherIO
 import com.simprints.eventsystem.event.EventRepository
 import com.simprints.face.data.moduleapi.face.responses.entities.Path
 import com.simprints.face.data.moduleapi.face.responses.entities.SecuredImageRef
 import com.simprints.infra.images.ImageRepository
 import com.simprints.infra.logging.Simber
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import com.simprints.infra.images.model.Path as CorePath
 
 class FaceImageManagerImpl @Inject constructor(
     private val coreImageRepository: ImageRepository,
-    private val coreEventRepository: EventRepository
+    private val coreEventRepository: EventRepository,
+    @DispatcherIO private val dispatcher: CoroutineDispatcher,
 ) : FaceImageManager {
 
-    override suspend fun save(imageBytes: ByteArray, captureEventId: String): SecuredImageRef? =
+    override suspend fun save(imageBytes: ByteArray, captureEventId: String): SecuredImageRef? = withContext(dispatcher) {
         determinePath(captureEventId)?.let { path ->
             Simber.d("Saving face image ${path.compose()}")
             val currentSession = coreEventRepository.getCurrentCaptureSessionEvent()
             val projectId = currentSession.payload.projectId
             val securedImageRef = coreImageRepository.storeImageSecurely(imageBytes, projectId, path)
 
-            return if (securedImageRef != null) {
+            if (securedImageRef != null) {
                 SecuredImageRef(securedImageRef.relativePath.toDomain())
             } else {
                 Simber.e("Saving image failed for captureId $captureEventId")
                 null
             }
         }
+    }
 
     private suspend fun determinePath(captureEventId: String): CorePath? =
         try {
