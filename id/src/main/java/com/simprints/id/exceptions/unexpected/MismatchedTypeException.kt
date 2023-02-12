c0d936b727a961d5760950f7167054d8367e36d5
melad <melad@simprints.com>
2023-02-12 17:58:42 +0200

    Handle missing or outdated google play services
    Former-commit-id: 2263aaf72d9998e373c32576fa78394976778e4c [formerly 669d08de3a1dc0c54a8138b4dec9b0b451f1f04b]
    Former-commit-id: d291b13d27c68e463fa0f2a1ac7e9ad7da6bdfc8

--- id/src/main/java/com/simprints/id/exceptions/unexpected/MismatchedTypeException.kt
+++
@@ -1,7 +0,0 @@
-package com.simprints.id.exceptions.unexpected
-
-import com.simprints.core.exceptions.UnexpectedException
-
-
-class MismatchedTypeException(message: String = "MismatchedTypeException", cause: Throwable)
-    : UnexpectedException(message, cause)
