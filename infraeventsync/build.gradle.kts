f42079f563483060f3e284136be0a3ce0120b870
Sergejs Luhmirins <sergejs.luhmirins@gmail.com>
2023-03-03 16:41:15 +0200

    CORE-2333 Basic event sync message module
    Former-commit-id: 44045cfbc06c0a7f15b750fa8c222244e4097d6c [formerly bf9b4402d2c17b405a263a2d899a1f43cf1008f7]
    Former-commit-id: 4d5e919f4d2be2269aa772fb1d6b206872ca2375

---
+++ infraeventsync/build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("dagger.hilt.android.plugin")
+    kotlin("kapt")
+}
+
+apply {
+    from("${rootDir}${File.separator}buildSrc${File.separator}build_config.gradle")
+}
+
+android {
+    namespace = "com.simprints.infra.eventsync"
+
+    defaultConfig {
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    sourceSets {
+        // Adds exported room schema location as test app assets.
+        getByName("debug") {
+            assets.srcDirs("$projectDir/schemas")
+        }
+        getByName("test") {
+            java.srcDirs("$projectDir/src/debug")
+        }
+    }
+    testOptions {
+        unitTests.isIncludeAndroidResources = true
+        execution = "ANDROIDX_TEST_ORCHESTRATOR"
+    }
+    packagingOptions {
+        resources.excludes.add("META-INF/*")
+    }
+}
+
+dependencies {
+    api(project(":core"))
+
+    implementation(libs.androidX.room.ktx)
+    kapt(libs.androidX.room.compiler)
+
+    runtimeOnly(libs.kotlin.coroutinesAndroid)
+    api(libs.sqlCipher.core)
+    implementation(libs.workManager.work)
+
+    // DI
+    implementation(libs.hilt)
+    kapt(libs.hilt.kapt)
+
+    testImplementation(libs.testing.androidX.ext.junit)
+    testImplementation(libs.testing.coroutines.test)
+    testImplementation(libs.testing.robolectric.annotation)
+    testImplementation(libs.testing.koTest.kotlin.assert)
+    testImplementation(libs.testing.androidX.room)
+    testImplementation(project(":testtools"))
+    testImplementation(libs.testing.truth)
+    testImplementation(libs.testing.mockk.core)
+    testImplementation(libs.hilt.testing)
+
+    androidTestImplementation(libs.testing.androidX.core.testing)
+    androidTestImplementation(libs.testing.androidX.ext.junit)
+    androidTestImplementation(libs.testing.mockk.android)
+
+}
