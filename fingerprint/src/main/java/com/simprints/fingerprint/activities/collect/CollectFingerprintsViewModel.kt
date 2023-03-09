ba57af35dab67ecfe46841c317d89fd034edb68f
Milen Marinov <milen.danchev.marinov@gmail.com>
2023-03-09 20:59:44 +0200

    [CORE-2334] Don't try to create FingerprintCaptureBiometricsEvent if currentCapture is not in Collected state
    Former-commit-id: c37580cb3b765801f10182d14bbc9220b19ea464 [formerly 587c37f91f7025324e3cb3b6ce8ebdec8ea45625]
    Former-commit-id: 545024824be7c75ab915bcec4511aac65d3e540d

--- fingerprint/src/main/java/com/simprints/fingerprint/activities/collect/CollectFingerprintsViewModel.kt
+++ fingerprint/src/main/java/com/simprints/fingerprint/activities/collect/CollectFingerprintsViewModel.kt
@@ -366,8 +366,9 @@ class CollectFingerprintsViewModel(
                 payloadId = payloadId
             )
             val fingerprintCaptureBiometricsEvent =
-                if (captureEvent.result == FingerprintCaptureEvent.Result.GOOD_SCAN ||
-                    tooManyBadScans(currentCapture(), plusBadScan = false)
+                if (currentCapture() is CaptureState.Collected &&
+                    (captureEvent.result == FingerprintCaptureEvent.Result.GOOD_SCAN ||
+                    tooManyBadScans(currentCapture(), plusBadScan = false))
                 )
                     FingerprintCaptureBiometricsEvent(
                         createdAt = lastCaptureStartedAt,
@@ -379,7 +380,8 @@ class CollectFingerprintsViewModel(
                             )
                         },
                         payloadId = payloadId
-                    ) else null
+                    )
+                else null
 
             captureEventIds[CaptureId(id, currentCaptureIndex)] = captureEvent.id
 
