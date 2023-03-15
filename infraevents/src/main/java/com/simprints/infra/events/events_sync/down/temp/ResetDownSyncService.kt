279d5eba921e5437a3ed1f48a3c99a95204ab7bd
Milen Marinov <milen.danchev.marinov@gmail.com>
2023-03-15 14:27:39 +0200

    [WIP] Transform ResetDownSyncService to foreground service
    Former-commit-id: c37cc8ec45bd442e74c40b571cbcfae85494c938 [formerly adb43a9f8b27114e7c34447d233527201638f6fb]
    Former-commit-id: e8a921d8f531d7e0e156ccb48aa55513e8a26d6e

--- infraevents/src/main/java/com/simprints/infra/events/events_sync/down/temp/ResetDownSyncService.kt
+++ infraevents/src/main/java/com/simprints/infra/events/events_sync/down/temp/ResetDownSyncService.kt
@@ -1,8 +1,14 @@
 package com.simprints.infra.events.events_sync.down.temp
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
 import com.simprints.core.ExternalScope
 import com.simprints.infra.events.events_sync.down.EventDownSyncScopeRepository
 import com.simprints.infra.logging.LoggingConstants.CrashReportTag.SYNC
@@ -24,9 +30,15 @@ class ResetDownSyncService : Service() {
     @ExternalScope
     lateinit var externalScope: CoroutineScope
 
+//    override fun onCreate() {
+//        super.onCreate()
+//        startForegroundIfNeeded()
+//    }
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Simber.tag(SYNC.name).i("Reset downSync")
         externalScope.launch {
+            startForegroundIfNeeded()
             downSyncScopeRepository.deleteAll()
             stopSelf()
         }
@@ -36,5 +48,26 @@ class ResetDownSyncService : Service() {
 
     override fun onBind(p0: Intent?): IBinder? {
         return null
+    }
+
+    private fun startForegroundIfNeeded() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val chan = NotificationChannel(
+                "MyChannelId",
+                "My Foreground Service",
+                NotificationManager.IMPORTANCE_LOW
+            )
+            val manager = (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+            manager.createNotificationChannel(chan)
+
+            val notificationBuilder = NotificationCompat.Builder(this, "MyChannelId")
+            val notification: Notification = notificationBuilder
+                .setContentTitle("App is running on foreground")
+                .setPriority(NotificationManager.IMPORTANCE_LOW)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .setChannelId("MyChannelId")
+                .build()
+            startForeground(1, notification)
+        }
     }
 }
