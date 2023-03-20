ebfe7d4b6b6f22bd947940b05362a44164f9cb2c
Milen Marinov <milen.danchev.marinov@gmail.com>
2023-03-20 21:42:00 +0200

    [CORE-2403] Handle exception during project details loading
    Former-commit-id: 1cf544da6cda56f73e005fc30d8bddcfb21cb31a [formerly b6df7c79a0dca93fd6a23d5ff0d1ad28282a71c6]
    Former-commit-id: 620714f845a4fe2fd5d723be9b9f7b8a8eb44d80

--- featuredashboard/src/main/java/com/simprints/feature/dashboard/main/projectdetails/DashboardProjectState.kt
+++ featuredashboard/src/main/java/com/simprints/feature/dashboard/main/projectdetails/DashboardProjectState.kt
@@ -1,3 +1,8 @@
 package com.simprints.feature.dashboard.main.projectdetails
 
-internal data class DashboardProjectState(val title: String, val lastUser: String, val lastScanner: String)
+internal data class DashboardProjectState(
+    val title: String = "",
+    val lastUser: String = "",
+    val lastScanner: String = "",
+    val isLoaded: Boolean = false
+)
