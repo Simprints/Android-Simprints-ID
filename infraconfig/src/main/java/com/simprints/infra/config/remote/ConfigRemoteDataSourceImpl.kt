4b572335781b977cb860e917c12f451b316c7de4
Sergejs Luhmirins <sergejs.luhmirins@gmail.com>
2023-03-22 15:19:34 +0200

    CORE-2407 Download privacy notice on IO dispatcher
    Former-commit-id: 442959dd2d1680c79d6968171b5865fd9ffb03bf [formerly 388570f6bf5918595a3b486ce29de207c0982f55]
    Former-commit-id: 4aaa469f8f3ee7ab59b4655285949551f17ac8f3

--- infraconfig/src/main/java/com/simprints/infra/config/remote/ConfigRemoteDataSourceImpl.kt
+++ infraconfig/src/main/java/com/simprints/infra/config/remote/ConfigRemoteDataSourceImpl.kt
@@ -1,20 +1,25 @@
 package com.simprints.infra.config.remote
 
+import com.simprints.core.DispatcherIO
 import com.simprints.infra.config.domain.models.Project
 import com.simprints.infra.config.domain.models.ProjectConfiguration
 import com.simprints.infra.login.LoginManager
 import com.simprints.infra.network.SimNetwork
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 import java.net.URL
 import javax.inject.Inject
 
 internal class ConfigRemoteDataSourceImpl(
     private val loginManager: LoginManager,
+    @DispatcherIO private val dispatcherIO: CoroutineDispatcher,
     private val urlDownloader: (String) -> String
 ) :
     ConfigRemoteDataSource {
 
     @Inject
-    constructor(loginManager: LoginManager) : this(loginManager, { url -> URL(url).readText() })
+    constructor(loginManager: LoginManager, dispatcherIO: CoroutineDispatcher) :
+        this(loginManager, dispatcherIO, { url -> URL(url).readText() })
 
     override suspend fun getConfiguration(projectId: String): ProjectConfiguration =
         getApiClient().executeCall { it.getConfiguration(projectId) }.toDomain()
@@ -22,10 +27,10 @@ internal class ConfigRemoteDataSourceImpl(
     override suspend fun getProject(projectId: String): Project =
         getApiClient().executeCall { it.getProject(projectId) }.toDomain()
 
-    override suspend fun getPrivacyNotice(projectId: String, fileId: String): String =
-        getApiClient().executeCall { it.getFileUrl(projectId, fileId) }.url.let {
-            urlDownloader(it)
-        }
+    override suspend fun getPrivacyNotice(projectId: String, fileId: String): String {
+        val url = getApiClient().executeCall { it.getFileUrl(projectId, fileId) }.url
+        return withContext(dispatcherIO) { urlDownloader(url) }
+    }
 
     private suspend fun getApiClient(): SimNetwork.SimApiClient<ConfigRemoteInterface> =
         loginManager.buildClient(ConfigRemoteInterface::class)
