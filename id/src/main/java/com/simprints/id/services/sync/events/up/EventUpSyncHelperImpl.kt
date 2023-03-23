e82468f580bcbc0c03b6d2a3c4e5a5f044e684d7
melad <melad@simprints.com>
2023-03-23 11:05:19 +0200

    CORE-2410 fix exception handling  problem in EventUpSyncHelper.upSync
    Former-commit-id: 0a889e6498b43ca202db29ec2454a4f17206bdd0 [formerly d897605e84a9484812320fc177b944274808f609]
    Former-commit-id: 31b17c64e8c81f5439172fd1cb5fbb6d801029cf

--- id/src/main/java/com/simprints/id/services/sync/events/up/EventUpSyncHelperImpl.kt
+++ id/src/main/java/com/simprints/id/services/sync/events/up/EventUpSyncHelperImpl.kt
@@ -9,7 +9,8 @@ import com.simprints.infra.config.domain.models.canSyncBiometricDataToSimprints
 import com.simprints.infra.events.EventRepository
 import com.simprints.infra.events.events_sync.up.EventUpSyncScopeRepository
 import com.simprints.infra.events.events_sync.up.domain.EventUpSyncOperation
-import com.simprints.infra.events.events_sync.up.domain.EventUpSyncOperation.UpSyncState.*
+import com.simprints.infra.events.events_sync.up.domain.EventUpSyncOperation.UpSyncState.COMPLETE
+import com.simprints.infra.events.events_sync.up.domain.EventUpSyncOperation.UpSyncState.RUNNING
 import com.simprints.infra.logging.Simber
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.FlowCollector
@@ -29,37 +30,26 @@ class EventUpSyncHelperImpl @Inject constructor(
     override fun upSync(
         scope: CoroutineScope,
         operation: EventUpSyncOperation
-    ) =
-        flow {
-            val config = configManager.getProjectConfiguration()
-            var lastOperation = operation.copy()
-            var count = 0
-            try {
-                eventRepository.uploadEvents(
-                    projectId = operation.projectId,
-                    canSyncAllDataToSimprints = config.canSyncAllDataToSimprints(),
-                    canSyncBiometricDataToSimprints = config.canSyncBiometricDataToSimprints(),
-                    canSyncAnalyticsDataToSimprints = config.canSyncAnalyticsDataToSimprints()
-                ).collect {
-                    Simber.tag(SYNC_LOG_TAG).d("[UP_SYNC_HELPER] Uploading $it events")
-                    count = it
-                    lastOperation =
-                        lastOperation.copy(lastState = RUNNING, lastSyncTime = timerHelper.now())
-                    emitProgress(lastOperation, count)
-                }
-
-                lastOperation =
-                    lastOperation.copy(lastState = COMPLETE, lastSyncTime = timerHelper.now())
-                emitProgress(lastOperation, count)
-
-
-            } catch (t: Throwable) {
-                Simber.e(t)
-                lastOperation =
-                    lastOperation.copy(lastState = FAILED, lastSyncTime = timerHelper.now())
-                emitProgress(lastOperation, count)
-            }
+    ) = flow {
+        val config = configManager.getProjectConfiguration()
+        var lastOperation = operation.copy()
+        var count = 0
+        eventRepository.uploadEvents(
+            projectId = operation.projectId,
+            canSyncAllDataToSimprints = config.canSyncAllDataToSimprints(),
+            canSyncBiometricDataToSimprints = config.canSyncBiometricDataToSimprints(),
+            canSyncAnalyticsDataToSimprints = config.canSyncAnalyticsDataToSimprints()
+        ).collect {
+            Simber.tag(SYNC_LOG_TAG).d("[UP_SYNC_HELPER] Uploading $it events")
+            count = it
+            lastOperation =
+                lastOperation.copy(lastState = RUNNING, lastSyncTime = timerHelper.now())
+            emitProgress(lastOperation, count)
         }
+        lastOperation =
+            lastOperation.copy(lastState = COMPLETE, lastSyncTime = timerHelper.now())
+        emitProgress(lastOperation, count)
+    }
 
     private suspend fun FlowCollector<EventUpSyncProgress>.emitProgress(
         lastOperation: EventUpSyncOperation,
