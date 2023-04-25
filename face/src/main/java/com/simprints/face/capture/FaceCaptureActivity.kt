3c5cddac7f27bd9c8d42ea3e7d034165c8b41327
Sergejs Luhmirins <sergejs.luhmirins@gmail.com>
2023-04-25 15:03:38 +0300

    CORE-2160 Replace custom exit form in face module
    Former-commit-id: 9b0d9ff8b7157f13b320dd87187279421528de1d [formerly 99f274d0854221b73191b4c99c5d65ee54ad4f30]
    Former-commit-id: aca14988005351c42f48535a9c80c080ee24febe

--- face/src/main/java/com/simprints/face/capture/FaceCaptureActivity.kt
+++ face/src/main/java/com/simprints/face/capture/FaceCaptureActivity.kt
@@ -5,16 +5,24 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.simprints.core.livedata.LiveDataEventObserver
 import com.simprints.core.livedata.LiveDataEventWithContentObserver
+import com.simprints.core.tools.viewbinding.viewBinding
 import com.simprints.core.tools.whenNonNull
 import com.simprints.core.tools.whenNull
 import com.simprints.face.R
 import com.simprints.face.base.FaceActivity
+import com.simprints.face.controllers.core.events.model.RefusalAnswer.Companion.fromExitFormOption
 import com.simprints.face.data.moduleapi.face.requests.FaceCaptureRequest
+import com.simprints.face.data.moduleapi.face.responses.FaceExitFormResponse
+import com.simprints.face.databinding.ActivityFaceCaptureBinding
 import com.simprints.face.exceptions.InvalidFaceRequestException
-import com.simprints.face.exitform.ExitFormViewModel
+import com.simprints.feature.exitform.ExitFormContract
+import com.simprints.feature.exitform.config.ExitFormOption
+import com.simprints.feature.exitform.exitFormConfiguration
+import com.simprints.feature.exitform.toArgs
 import com.simprints.moduleapi.face.requests.IFaceRequest
 import com.simprints.moduleapi.face.responses.IFaceResponse
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,18 +30,40 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class FaceCaptureActivity : FaceActivity() {
     private val vm: FaceCaptureViewModel by viewModels()
-    private val exitVm: ExitFormViewModel by viewModels()
+
+    private val binding by viewBinding(ActivityFaceCaptureBinding::inflate)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_face_capture)
+        setContentView(binding.root)
         bindViewModel()
 
         val faceRequest: FaceCaptureRequest =
             this.intent.extras?.getParcelable(IFaceRequest.BUNDLE_KEY)
                 ?: throw InvalidFaceRequestException("No IFaceRequest found for FaceCaptureActivity")
 
+        binding.captureHostFragment.getFragment<Fragment>().childFragmentManager
+            .setFragmentResultListener(ExitFormContract.EXIT_FORM_REQUEST, this) { _, data ->
+                val formSubmitted = ExitFormContract.isFormSubmitted(data)
+                val option = ExitFormContract.getFormOption(data)
+                val reason = ExitFormContract.getFormReason(data).orEmpty()
+
+                if (formSubmitted && option != null) {
+                    setResultAndFinish(option, reason)
+                } else {
+                    findNavController(R.id.capture_host_fragment).navigate(R.id.action_global_liveFeedback)
+                }
+            }
+
         vm.setupCapture(faceRequest)
+    }
+
+    private fun setResultAndFinish(option: ExitFormOption, reason: String) {
+        val intent = Intent().apply {
+            putExtra(IFaceResponse.BUNDLE_KEY, FaceExitFormResponse(fromExitFormOption(option), reason))
+        }
+        setResult(RESULT_OK, intent)
+        finish()
     }
 
     private fun bindViewModel() {
@@ -48,13 +78,13 @@ class FaceCaptureActivity : FaceActivity() {
         })
 
         vm.exitFormEvent.observe(this, LiveDataEventObserver {
-            findNavController(R.id.capture_host_fragment).navigate(R.id.action_global_refusalFragment)
-        })
-
-        exitVm.finishFlowWithExitFormEvent.observe(this, LiveDataEventWithContentObserver {
-            val intent = Intent().apply { putExtra(IFaceResponse.BUNDLE_KEY, it) }
-            setResult(Activity.RESULT_OK, intent)
-            finish()
+            findNavController(R.id.capture_host_fragment).navigate(
+                R.id.action_global_refusalFragment,
+                exitFormConfiguration {
+                    titleRes = R.string.why_did_you_skip_face_capture
+                    backButtonRes = R.string.exit_form_return_to_face_capture
+                }.toArgs()
+            )
         })
 
         vm.unexpectedErrorEvent.observe(this, LiveDataEventObserver {
@@ -64,9 +94,7 @@ class FaceCaptureActivity : FaceActivity() {
     }
 
     override fun onBackPressed() {
-        BackButtonContext.fromFragmentId(
-            findNavController(R.id.capture_host_fragment).currentDestination?.id
-        )
+        BackButtonContext.fromFragmentId(findNavController(R.id.capture_host_fragment).currentDestination?.id)
             .whenNonNull { vm.handleBackButton(this) }
             .whenNull { super.onBackPressed() }
     }
