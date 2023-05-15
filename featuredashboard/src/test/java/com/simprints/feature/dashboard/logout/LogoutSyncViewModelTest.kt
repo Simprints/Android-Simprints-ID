dadf65a61ee360eef54c4f0aff52abaa398b3a9d
Alexandr Zhurkov <alexandr.zhurkov@gmail.com>
2023-05-15 15:25:17 +0300

    CORE-2077 Displaying the logout confirmation dialog when user declines to synchronize the data prior to logout
    Former-commit-id: b66ef8773f9cf514c84d7ebb88c29164fce00151 [formerly c3bcec1c7279c9348d5ec1dc7248e41399e65776]
    Former-commit-id: cb73772d06deb8ee52bb4df91d9b140d7643fc9d

--- featuredashboard/src/test/java/com/simprints/feature/dashboard/logout/LogoutSyncViewModelTest.kt
+++ featuredashboard/src/test/java/com/simprints/feature/dashboard/logout/LogoutSyncViewModelTest.kt
@@ -1,10 +1,15 @@
 package com.simprints.feature.dashboard.logout
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.simprints.feature.dashboard.settings.about.AboutViewModel
+import com.google.common.truth.Truth.assertThat
 import com.simprints.feature.dashboard.settings.about.SignerManager
+import com.simprints.infra.config.ConfigManager
+import com.simprints.infra.config.domain.models.SettingsPasswordConfig
 import com.simprints.testtools.common.coroutines.TestCoroutineRule
+import com.simprints.testtools.common.livedata.getOrAwaitValue
+import io.mockk.coEvery
 import io.mockk.coVerify
+import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.CoroutineScope
 import org.junit.Rule
@@ -14,6 +19,7 @@ import org.junit.Test
 internal class LogoutSyncViewModelTest {
 
     private val signerManager = mockk<SignerManager>(relaxed = true)
+    private val configManager = mockk<ConfigManager>(relaxed = true)
 
     @get:Rule
     val rule = InstantTaskExecutorRule()
@@ -26,10 +32,28 @@ internal class LogoutSyncViewModelTest {
         val viewModel = LogoutSyncViewModel(
             signerManager = signerManager,
             externalScope = CoroutineScope(testCoroutineRule.testCoroutineDispatcher),
+            configManager = configManager
         )
 
         viewModel.logout()
 
         coVerify(exactly = 1) { signerManager.signOut() }
+    }
+
+    @Test
+    fun `password config should be fetched after initialization`() {
+        val config = SettingsPasswordConfig.Locked(password = "123")
+        coEvery { configManager.getProjectConfiguration() } returns mockk {
+            every { general } returns mockk {
+                every { settingsPassword } returns config
+            }
+        }
+        val viewModel = LogoutSyncViewModel(
+            signerManager = signerManager,
+            externalScope = CoroutineScope(testCoroutineRule.testCoroutineDispatcher),
+            configManager = configManager
+        )
+        val resultConfig = viewModel.settingsLocked.getOrAwaitValue()
+        assertThat(resultConfig).isEqualTo(config)
     }
 }
