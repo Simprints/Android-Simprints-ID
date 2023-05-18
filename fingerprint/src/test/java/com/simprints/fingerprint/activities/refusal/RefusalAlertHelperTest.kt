d45a85eb7340b786987c021238a85d8f9d76e0b7
Sergejs Luhmirins <sergejs.luhmirins@gmail.com>
2023-05-18 14:10:17 +0300

    CORE-2161 Improve code coverage
    Former-commit-id: 6f67cd50c2233f2b2512b4b4ae13f6ecdf6f505f [formerly e959cf1e5d7d1f30bf2715d95881947cc43a95e9]
    Former-commit-id: f8093c73bbdb8de8e8073f08c5af036b4e14dd36

--- fingerprint/src/test/java/com/simprints/fingerprint/activities/refusal/RefusalAlertHelperTest.kt
+++ fingerprint/src/test/java/com/simprints/fingerprint/activities/refusal/RefusalAlertHelperTest.kt
@@ -3,7 +3,9 @@ package com.simprints.fingerprint.activities.refusal
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
 import com.simprints.feature.exitform.ExitFormResult
+import com.simprints.feature.exitform.config.ExitFormConfiguration
 import com.simprints.feature.exitform.config.ExitFormOption
+import com.simprints.feature.exitform.scannerOptions
 import org.junit.Assert.fail
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -14,7 +16,17 @@ class RefusalAlertHelperTest {
     private val helper = RefusalAlertHelper
 
     @Test
-    fun `handleResult calls onBack if data is empty`() {
+    fun `default refusal args include scanner options`() {
+        val args = helper.refusalArgs()
+
+        // there must be only 1 item args bundle
+        val configKey = args.keySet().first()
+        assertThat(args.getParcelable<ExitFormConfiguration>(configKey)?.visibleOptions)
+            .containsExactlyElementsIn(scannerOptions())
+    }
+
+    @Test
+    fun `handleRefusal calls onBack if data is empty`() {
         var backCalled = false
 
         helper.handleRefusal(
@@ -26,7 +38,7 @@ class RefusalAlertHelperTest {
     }
 
     @Test
-    fun `handleResult calls onBack if form was not submitted`() {
+    fun `handleRefusal calls onBack if form was not submitted`() {
         var backCalled = false
 
         helper.handleRefusal(
@@ -38,7 +50,7 @@ class RefusalAlertHelperTest {
     }
 
     @Test
-    fun `handleResult calls onBack if form was submitted without option`() {
+    fun `handleRefusal calls onBack if form was submitted without option`() {
         var backCalled = false
 
         helper.handleRefusal(
@@ -50,11 +62,23 @@ class RefusalAlertHelperTest {
     }
 
     @Test
-    fun `handleResult calls onSubmit if form was submitted with option`() {
+    fun `handleRefusal calls onSubmit if form was submitted with option`() {
         var submitCalled = false
 
         helper.handleRefusal(
             ExitFormResult(true, ExitFormOption.Other),
+            onBack = { fail("Should not call back") },
+            onSubmit = { submitCalled = true }
+        )
+        assertThat(submitCalled).isTrue()
+    }
+
+    @Test
+    fun `handleRefusal calls onSubmit if form was submitted with option and reason`() {
+        var submitCalled = false
+
+        helper.handleRefusal(
+            ExitFormResult(true, ExitFormOption.Other, "reason"),
             onBack = { fail("Should not call back") },
             onSubmit = { submitCalled = true }
         )
