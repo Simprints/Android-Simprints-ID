4a3ffa80d44d88d0d383e041b1db839451e862dc
Alex Zhurkov <xzhorikx@gmail.com>
2023-05-22 19:50:14 +0400

    CORE-2568 Making interfaces internal and functional
    Former-commit-id: a24246f2bd6749ad3749dd42672a108d4b7df728 [formerly bce17712509ce0d278f6c6cb8d1cc41f58da22f2]
    Former-commit-id: 192f3b3545eb5ea08ee09a44ca8ba23ebe69fc32

--- infrasecurity/src/main/java/com/simprints/infra/security/keyprovider/EncryptedSharedPreferencesProvider.kt
+++ infrasecurity/src/main/java/com/simprints/infra/security/keyprovider/EncryptedSharedPreferencesProvider.kt
@@ -2,6 +2,6 @@ package com.simprints.infra.security.keyprovider
 
 import android.content.SharedPreferences
 
-interface EncryptedSharedPreferencesProvider {
+internal fun interface EncryptedSharedPreferencesProvider {
     fun provideEncryptedSharedPreferences(filename: String, masterKeyAlias: String): SharedPreferences
 }
