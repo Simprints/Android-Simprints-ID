78fc0c6c8f7fecdb135824617cafd4b60755a964
Alex Zhurkov <xzhorikx@gmail.com>
2023-05-23 12:11:35 +0400

    CORE-2568 Adding test coverage for EncryptedSharedPreferencesBuilderImpl
    Former-commit-id: 7baefa290a1fe6c76e9e76ffe555ac9cb9f3eab6 [formerly 0e8eb42604b160f18da507a85ad29c7533daadfe]
    Former-commit-id: 62b37d8020de197c32c531ff77c5a3df6cee7ae8

---
+++ infrasecurity/src/test/java/com/simprints/infra/security/keyprovider/EncryptedSharedPreferencesProviderImplTest.kt
@@ -0,0 +1,48 @@
+package com.simprints.infra.security.keyprovider
+
+import android.content.Context
+import androidx.security.crypto.EncryptedSharedPreferences
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.unmockkAll
+import io.mockk.verify
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+
+internal class EncryptedSharedPreferencesProviderImplTest {
+
+    @MockK
+    lateinit var context: Context
+    private lateinit var provider: EncryptedSharedPreferencesProvider
+    private val filename = "filename"
+    private val masterKeyAlias = "masterKeyAlias"
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this, relaxed = true)
+        provider = EncryptedSharedPreferencesProviderImpl(ctx = context)
+    }
+
+    @After
+    fun cleanup() {
+        unmockkAll()
+    }
+
+    @Test
+    fun `when shared preferences are created should call EncryptedSharedPreferences_create`() {
+        val spy = mockk<EncryptedSharedPreferences>()
+        mockkStatic(EncryptedSharedPreferences::class)
+        every { EncryptedSharedPreferences.create(any(), any(), any(), any(), any()) } returns spy
+        provider.provideEncryptedSharedPreferences(
+            filename = filename,
+            masterKeyAlias = masterKeyAlias
+        )
+        verify(exactly = 1) { EncryptedSharedPreferences.create(any(), any(), any(), any(), any()) }
+    }
+
+}
