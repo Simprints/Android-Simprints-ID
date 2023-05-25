d298894e6e188c4f6205827c9665cccda7f0e63d
Alex Zhurkov <xzhorikx@gmail.com>
2023-05-25 12:15:47 +0400

    CORE-2077 making LogoutDestination enum internal
    Former-commit-id: 9c250b7782cf0ae2d5caead22ef1134a2057ec65 [formerly 22cdaa722aaca916d15fb64212ab5dfe13f6417f]
    Former-commit-id: e3e5ab1a785c27e8e1ce0f4481f401954497d789

--- featuredashboard/src/main/java/com/simprints/feature/dashboard/settings/about/LogoutDestination.kt
+++ featuredashboard/src/main/java/com/simprints/feature/dashboard/settings/about/LogoutDestination.kt
@@ -3,6 +3,6 @@ package com.simprints.feature.dashboard.settings.about
 /**
  * Represents all possible navigation destinations for the logout
  */
-enum class LogoutDestination {
+internal enum class LogoutDestination {
     LogoutDataSyncScreen, LoginScreen
 }
