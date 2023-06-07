be8fb973cb680227738427bda6c0c5f543c06c87
Sergejs Luhmirins <sergejs.luhmirins@gmail.com>
2023-06-07 16:29:54 +0300

    CORE-2600 Cleanup old guid selection screen implementation
    Former-commit-id: f9030be835935d48ac1e379962f37b126854d42e [formerly a5e4b10f66c9021afe1a685a4a748bd92a927212]
    Former-commit-id: 57f1bfc0feca04c36366a5695192143bf4a4051f

--- id/src/main/java/com/simprints/id/orchestrator/steps/core/requests/FetchGUIDRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.simprints.id.orchestrator.steps.core.requests
-
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-class FetchGUIDRequest(val projectId: String, val verifyGuid: String): CoreRequest
