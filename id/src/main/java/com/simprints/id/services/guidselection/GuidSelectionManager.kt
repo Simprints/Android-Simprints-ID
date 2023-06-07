3e8e6df703f204456cbd97d82851768d648d2ef5
Sergejs Luhmirins <sergejs.luhmirins@gmail.com>
2023-06-07 16:29:54 +0300

    CORE-2600 Cleanup old guid selection screen implementation
    Former-commit-id: f9030be835935d48ac1e379962f37b126854d42e [formerly a5e4b10f66c9021afe1a685a4a748bd92a927212]
    Former-commit-id: 57f1bfc0feca04c36366a5695192143bf4a4051f

--- id/src/main/java/com/simprints/id/services/guidselection/GuidSelectionManager.kt
+++
@@ -1,8 +0,0 @@
-package com.simprints.id.services.guidselection
-
-import com.simprints.id.orchestrator.steps.core.requests.GuidSelectionRequest
-
-interface GuidSelectionManager {
-    fun handleConfirmIdentityRequest(request: GuidSelectionRequest)
-}
-
