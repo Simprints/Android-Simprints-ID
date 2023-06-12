42dfff467430115979c01792ee8043b6c9f16ca6
Sergejs Luhmirins <sergejs.luhmirins@gmail.com>
2023-06-12 15:52:58 +0300

    CORE-2601 Delete legacy implementation of enrol last biometric screen
    Former-commit-id: 5cdb094f36630dd40f661bc81950470bc08ef2ea [formerly 39172b6b2b9b4a47b609fae59c21b33f4d4ee549]
    Former-commit-id: d5e2cbf4133fe2edd056ab257dad6db57a09c165

--- id/src/main/java/com/simprints/id/exceptions/safe/DuplicateBiometricEnrolmentCheckFailed.kt
+++
@@ -1,7 +0,0 @@
-package com.simprints.id.exceptions.safe
-
-import com.simprints.core.exceptions.SafeException
-
-class DuplicateBiometricEnrolmentCheckFailed(
-    message: String = "There is a subject with confidence score above the high confidence level"
-) : SafeException(message)
