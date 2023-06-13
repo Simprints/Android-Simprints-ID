dfa52f9bdfe39c9723db746b4665f3f26531147e
Alexandr Zhurkov <alexandr.zhurkov@gmail.com>
2023-06-13 09:45:01 +0300

    CORE-2125: Moving ::updateOnIndex extension to a core-extensions
    Former-commit-id: 2e260c61962b4ab9d963abad7137e5de357597dd [formerly 27f5065690601376ff7920ae4c304e712856c014]
    Former-commit-id: e22176e21958ac341be6550456d14e131f9b653f

---
+++ core/src/main/java/com/simprints/core/tools/extentions/List.ext.kt
@@ -0,0 +1,10 @@
+package com.simprints.core.tools.extentions
+
+
+fun <T> List<T>.updateOnIndex(index: Int, newItem: (T) -> T): List<T> =
+    mapIndexed { i, item ->
+        when (i) {
+            index -> newItem(item)
+            else -> item
+        }
+    }
--- fingerprint/controller/src/androidTest/java/com/simprints/fingerprint/activities/collect/CollectFingerprintsActivityTest.kt
+++ fingerprint/controller/src/androidTest/java/com/simprints/fingerprint/activities/collect/CollectFingerprintsActivityTest.kt
@@ -15,6 +15,7 @@ import androidx.viewpager2.widget.ViewPager2
 import com.google.common.truth.Truth.assertThat
 import com.simprints.core.livedata.LiveDataEvent
 import com.simprints.core.livedata.LiveDataEventWithContent
+import com.simprints.core.tools.extentions.updateOnIndex
 import com.simprints.fingerprint.R
 import com.simprints.fingerprint.activities.alert.AlertError
 import com.simprints.fingerprint.activities.collect.request.CollectFingerprintsTaskRequest
@@ -407,12 +408,4 @@ class CollectFingerprintsActivityTest {
             it.putExtra(CollectFingerprintsTaskRequest.BUNDLE_KEY, this)
         }
     }
-
-    private fun <T> List<T>.updateOnIndex(index: Int, newItem: (T) -> T): List<T> =
-        mapIndexed { i, item ->
-            when (i) {
-                index -> newItem(item)
-                else -> item
-            }
-        }
 }
