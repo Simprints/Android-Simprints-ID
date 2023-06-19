30b2f9a05d21019156586d5a6fb0e6d95b8f8edd
Alexandr Zhurkov <alexandr.zhurkov@gmail.com>
2023-06-19 15:17:33 +0300

    CORE-2637 Changing the UI content of the alert for the paused project
    Former-commit-id: d591b805a5ef743056db083c4c78fc9778934fad [formerly 8a929d592d155067f845f467c3d55ce19909fb4f]
    Former-commit-id: 0f30ac246914b3d354b0dfab127607877c8683d4

--- id/src/main/java/com/simprints/id/domain/moduleapi/app/responses/AppErrorResponse.kt
+++ id/src/main/java/com/simprints/id/domain/moduleapi/app/responses/AppErrorResponse.kt
@@ -38,7 +38,6 @@ data class AppErrorResponse(val reason: Reason) : AppResponse {
                     AlertType.MISSING_OR_OUTDATED_GOOGLE_PLAY_STORE_APP,
                     AlertType.INTEGRITY_SERVICE_ERROR,
                     AlertType.UNEXPECTED_ERROR -> UNEXPECTED_ERROR
-                    AlertType.ENROLMENT_LAST_BIOMETRICS_FAILED -> ENROLMENT_LAST_BIOMETRICS_FAILED
                     AlertType.PROJECT_PAUSED -> PROJECT_PAUSED
                 }
         }
