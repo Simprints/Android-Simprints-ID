fe941a44dc4a65af17af370b386099394c6aae59
melad <melad@simprints.com>
2023-06-20 12:11:38 +0300

    CORE-2659 downgrade to jdk 11
    Former-commit-id: 9cb68d07925896f88b192668258bef8fd7cf1bff [formerly 32aef5b5017a8b895438b51ed5270b083d6656dc]
    Former-commit-id: f985345c088850bcf95d00d7c06a8a8469b158b3

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -3,10 +3,18 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
+/*
+Workaround to set jvmToolchain = 11 because R8 doesn't yet support java 17
+Should be removed before migrating to jdk 17
+https://issuetracker.google.com/issues/212279104
+ */
+kotlin {
+    jvmToolchain(11)
+}
 dependencies {
     // Gradle & Kotlin
     compileOnly(libs.plugin.gradle.android)
--- build-logic/proguard-rules.pro
+++ build-logic/proguard-rules.pro
@@ -1,3 +1,4 @@
+# Dont warn about the missing files during the obfuscation
 -dontwarn java.lang.invoke.StringConcatFactory
 -dontwarn com.simprints.infra.resources.R$string
 -dontwarn com.simprints.infra.resources.R$color
