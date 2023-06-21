ce8d276624df02e1bbbbe65d4e4c9eefff24e5a3
alex <alexandr@simprints.com>
2023-06-21 18:58:56 +0300

    CORE-2638 Fixing ProjectEndingException message
    Former-commit-id: 3b324f82c2505e3007c11195f7ecbececb1fd17e [formerly 3131bfabb56ef60e3c92e1f6e1592bd49e2a6234]
    Former-commit-id: e72ccfef92bc70fc4ef5ce1af01f3869aa1ddf82

--- id/src/main/java/com/simprints/id/exceptions/safe/secure/ProjectEndingException.kt
+++ id/src/main/java/com/simprints/id/exceptions/safe/secure/ProjectEndingException.kt
@@ -2,4 +2,4 @@ package com.simprints.id.exceptions.safe.secure
 
 import com.simprints.core.exceptions.SafeException
 
-class ProjectEndingException(message: String = "NotSignedInException") : SafeException(message)
+class ProjectEndingException(message: String = "ProjectEndingException") : SafeException(message)
