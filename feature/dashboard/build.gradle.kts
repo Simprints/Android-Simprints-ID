5072ee66020b8d064f1aa76b3154336747d39559
Alexandr Zhurkov <alexandr.zhurkov@gmail.com>
2023-06-22 13:30:03 +0300

    CORE-2638 loggin the user out if project is ending and all data is synced
    Former-commit-id: 860d94090529177eadf00a13c713c3ccef1e96a3 [formerly e33630427f90529a6cec5d0374472a1422b6c0da]
    Former-commit-id: 6d722b7b13a06d4672d6507c54f3b2281e47ad83

--- feature/dashboard/build.gradle.kts
+++ feature/dashboard/build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
     implementation(project(":infra:auth-logic"))
     implementation(project(":infra:recent-user-activity"))
     implementation(project(":feature:consent"))
+    implementation(project(":infra:project-security-store"))
 
     implementation(libs.fuzzywuzzy.core)
 
