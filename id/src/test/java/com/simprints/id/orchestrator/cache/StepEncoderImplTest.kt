0780268828f06905abeb06067b899ba9a9448fdf
melad <melad@simprints.com>
2023-07-11 23:05:32 +0300

    CORE-2735 add Tests
    Former-commit-id: e10aab15712bdb13bca9da0afb544976a27ac66e [formerly 093945fcca1a0a5b3e3f0a933e7204ce44f69006]
    Former-commit-id: df7f8f6840c26640f2f1f22bce9aa46c396073fc

--- id/src/test/java/com/simprints/id/orchestrator/cache/StepEncoderImplTest.kt
+++ id/src/test/java/com/simprints/id/orchestrator/cache/StepEncoderImplTest.kt
@@ -1,5 +1,7 @@
 package com.simprints.id.orchestrator.cache
 
+import android.os.Bundle
+import androidx.core.os.bundleOf
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.simprints.core.biometrics.FingerprintGeneratorUtils
 import com.simprints.id.domain.moduleapi.face.requests.FaceCaptureRequest
@@ -11,6 +13,7 @@ import com.simprints.id.domain.moduleapi.fingerprint.responses.FingerprintCaptur
 import com.simprints.id.domain.moduleapi.fingerprint.responses.entities.FingerprintCaptureResult
 import com.simprints.id.domain.moduleapi.fingerprint.responses.entities.FingerprintCaptureSample
 import com.simprints.id.orchestrator.steps.Step
+import com.simprints.id.orchestrator.steps.core.response.SetupResponse
 import com.simprints.id.testtools.TestApplication
 import com.simprints.infra.config.domain.models.Finger
 import com.simprints.infra.events.event.domain.models.fingerprint.FingerprintTemplateFormat
@@ -101,6 +104,39 @@ class StepEncoderImplTest {
                 getResult() as FaceCaptureResponse,
                 faceCaptureResponse as FaceCaptureResponse
             )
+        }
+    }
+
+    @Test
+    fun shouldDecodeStringToSetupStep() {
+        val setupBundle: Bundle = bundleOf()
+        val setupResponse = SetupResponse(isSetupComplete = true)
+        val step = Step(
+            requestCode = REQUEST_CODE,
+            activityName = ACTIVITY_NAME,
+            bundleKey = BUNDLE_KEY,
+            payloadType = Step.PayloadType.BUNDLE,
+            payload = setupBundle,
+            status = Step.Status.COMPLETED,
+            result = setupResponse
+        )
+        val encodedString = stepEncoder.encode(step)
+        val decodedStep = stepEncoder.decode(encodedString)
+
+        with(decodedStep) {
+            MatcherAssert.assertThat(requestCode, CoreMatchers.`is`(REQUEST_CODE))
+            MatcherAssert.assertThat(activityName, CoreMatchers.`is`(ACTIVITY_NAME))
+            MatcherAssert.assertThat(bundleKey, CoreMatchers.`is`(BUNDLE_KEY))
+            MatcherAssert.assertThat(payloadType, CoreMatchers.`is`(Step.PayloadType.BUNDLE))
+            MatcherAssert.assertThat((payload as Bundle).size(), CoreMatchers.`is`(0))
+            MatcherAssert.assertThat(getStatus(), CoreMatchers.`is`(Step.Status.COMPLETED))
+            MatcherAssert.assertThat(
+                getResult(),
+                CoreMatchers.instanceOf(SetupResponse::class.java)
+            )
+            MatcherAssert.assertThat(
+                (getResult() as SetupResponse).isSetupComplete,
+                CoreMatchers.`is`(setupResponse.isSetupComplete))
         }
     }
 
