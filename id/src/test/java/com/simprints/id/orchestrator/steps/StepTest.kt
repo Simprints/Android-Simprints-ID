b4011f4aab341665c4894501a3dc613592a0c076
melad <melad@simprints.com>
2023-07-11 23:05:32 +0300

    CORE-2735 add Tests
    Former-commit-id: e10aab15712bdb13bca9da0afb544976a27ac66e [formerly 093945fcca1a0a5b3e3f0a933e7204ce44f69006]
    Former-commit-id: df7f8f6840c26640f2f1f22bce9aa46c396073fc

--- id/src/test/java/com/simprints/id/orchestrator/steps/StepTest.kt
+++ id/src/test/java/com/simprints/id/orchestrator/steps/StepTest.kt
@@ -1,8 +1,14 @@
 package com.simprints.id.orchestrator.steps
 
 import com.google.common.truth.Truth.assertThat
+import com.simprints.id.domain.moduleapi.face.requests.FaceCaptureRequest
+import com.simprints.id.domain.moduleapi.fingerprint.requests.FingerprintCaptureRequest
 import com.simprints.id.orchestrator.steps.Step.Status.COMPLETED
 import com.simprints.id.orchestrator.steps.Step.Status.ONGOING
+import com.simprints.id.orchestrator.steps.core.requests.CoreRequest
+import com.simprints.moduleapi.face.requests.IFaceCaptureRequest
+import com.simprints.moduleapi.fingerprint.requests.IFingerprintCaptureRequest
+import com.simprints.testtools.common.syntax.assertThrows
 import io.mockk.mockk
 import org.junit.Test
 
@@ -22,4 +28,38 @@ class StepTest {
         )
         assertThat(step.getStatus()).isEqualTo(COMPLETED)
     }
+
+    @Test
+    fun `should return IFingerprintRequest when request is a FingerprintRequest`() {
+        val fingerprintRequest = mockk<FingerprintCaptureRequest>()
+        val parcelable = fingerprintRequest.fromDomainToModuleApi()
+
+        assertThat(parcelable).isInstanceOf( IFingerprintCaptureRequest::class.java)
+    }
+
+    @Test
+    fun `should return IFaceRequest when request is a FaceRequest`() {
+        val faceRequest = mockk<FaceCaptureRequest>()
+        val parcelable = faceRequest.fromDomainToModuleApi()
+
+        assertThat(parcelable).isInstanceOf(IFaceCaptureRequest::class.java)
+    }
+
+    @Test
+    fun `should return the same object when request is a CoreRequest`() {
+        val coreRequest = mockk<CoreRequest>()
+        val parcelable = coreRequest.fromDomainToModuleApi()
+
+        assertThat(parcelable).isEqualTo( coreRequest)
+    }
+
+    @Test
+    fun `should throw an exception when this is an invalid request`() {
+        val invalidRequest = mockk<Step.Request>()
+
+        assertThrows<Throwable> {
+            invalidRequest.fromDomainToModuleApi()
+        }
+    }
+
 }
