d32b6ba38b04bdf7e42f509948fb3a518d87874b
melad <melad@simprints.com>
2023-07-12 20:12:02 +0300

    CORE-2754 don't track location when it is disabled from remote config
    Former-commit-id: 71be476b209f587e3b4836737d4a66181d001510 [formerly 0535af0561eea33474fc50f0aa4dc3c2085607ec]
    Former-commit-id: 8624d9306447c1448d4a17b282c0a8cc6ee0c456

--- feature/setup/src/main/java/com/simprints/feature/setup/screen/SetupFragment.kt
+++ feature/setup/src/main/java/com/simprints/feature/setup/screen/SetupFragment.kt
@@ -5,26 +5,24 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.simprints.core.tools.extentions.hasPermission
-import com.simprints.feature.setup.LocationStore
 import com.simprints.feature.setup.R
 import com.simprints.feature.setup.SetupResult
 import com.simprints.infra.uibase.navigation.finishWithResult
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 internal class SetupFragment : Fragment(R.layout.fragment_setup) {
 
-    @Inject
-    lateinit var locationStore: LocationStore
+    private val viewModel: SetupViewModel by viewModels()
 
     private val launchLocationPermissionRequest = registerForActivityResult(
         ActivityResultContracts.RequestPermission(),
     ) { granted ->
         if (granted) {
-            locationStore.collectLocationInBackground()
+            viewModel.collectLocation()
         }
         finishWithResult(granted)
     }
@@ -32,12 +30,18 @@ internal class SetupFragment : Fragment(R.layout.fragment_setup) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        if (requireActivity().hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
-            locationStore.collectLocationInBackground()
-            finishWithResult(true)
-        } else {
-            launchLocationPermissionRequest.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        viewModel.requestLocationPermission.observe(viewLifecycleOwner) {
+            if (requireActivity().hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                viewModel.collectLocation()
+                finishWithResult(true)
+            } else {
+                launchLocationPermissionRequest.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
         }
+
+        viewModel.finish.observe(viewLifecycleOwner, ::finishWithResult)
+
+        viewModel.start()
     }
 
     private fun finishWithResult(hasPermission: Boolean) {
---
+++ feature/setup/src/main/java/com/simprints/feature/setup/screen/SetupViewModel.kt
@@ -0,0 +1,44 @@
+package com.simprints.feature.setup.screen
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.simprints.feature.setup.LocationStore
+import com.simprints.infra.config.ConfigManager
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SetupViewModel @Inject constructor(
+    private val locationStore: LocationStore, private val configManager: ConfigManager
+) : ViewModel() {
+
+    val requestLocationPermission: LiveData<Unit>
+        get() = _requestLocationPermission
+    private var _requestLocationPermission = MutableLiveData<Unit>()
+
+    val finish: LiveData<Boolean>
+        get() = _finish
+    private var _finish = MutableLiveData<Boolean>()
+
+
+    fun collectLocation() {
+        locationStore.collectLocationInBackground()
+    }
+
+    fun start() {
+        viewModelScope.launch {
+            if (shouldCollectLocation()) {
+                // request location permissions
+                _requestLocationPermission.postValue(Unit)
+            } else {
+                _finish.postValue(true)
+            }
+        }
+    }
+
+    private suspend fun shouldCollectLocation() =
+        configManager.getProjectConfiguration().general.collectLocation
+}
