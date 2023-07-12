53d58fbbcfe7800ddbc45145941374a397eb422d
Alexandr Zhurkov <alexandr.zhurkov@gmail.com>
2023-07-12 16:28:04 +0300

    [CORE-2755] Adding test coverage for refusal mappers
    Former-commit-id: 29f1e927437aad1da00d4d42a8af81d284c405ce [formerly 9aa4aaaa5547aec8f5be3c82959ef96ba2b167d1]
    Former-commit-id: b1132fcaf12f95351310b68e990f6187ad5ee8f5

--- face/src/test/java/com/simprints/face/controllers/core/events/model/RefusalAnswerTest.kt
+++ face/src/test/java/com/simprints/face/controllers/core/events/model/RefusalAnswerTest.kt
@@ -1,8 +1,9 @@
 package com.simprints.face.controllers.core.events.model
 
-import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
 import com.simprints.face.controllers.core.events.model.RefusalAnswer.Companion.fromExitFormOption
 import com.simprints.feature.exitform.config.ExitFormOption
+import com.simprints.moduleapi.face.responses.IFaceExitReason
 import org.junit.Test
 
 class RefusalAnswerTest {
@@ -19,7 +20,23 @@ class RefusalAnswerTest {
             ExitFormOption.TooYoung to RefusalAnswer.REFUSED_YOUNG,
             ExitFormOption.Other to RefusalAnswer.OTHER,
         ).forEach { (option, reason) ->
-            Truth.assertThat(fromExitFormOption(option)).isEqualTo(reason)
+            assertThat(fromExitFormOption(option)).isEqualTo(reason)
+        }
+    }
+
+    @Test
+    fun `maps domain option to exit reason correctly`() {
+        mapOf(
+            RefusalAnswer.REFUSED_RELIGION to IFaceExitReason.REFUSED_RELIGION,
+            RefusalAnswer.REFUSED_DATA_CONCERNS to IFaceExitReason.REFUSED_DATA_CONCERNS,
+            RefusalAnswer.REFUSED_PERMISSION to IFaceExitReason.REFUSED_PERMISSION,
+            RefusalAnswer.APP_NOT_WORKING to IFaceExitReason.APP_NOT_WORKING,
+            RefusalAnswer.SCANNER_NOT_WORKING to IFaceExitReason.SCANNER_NOT_WORKING,
+            RefusalAnswer.REFUSED_NOT_PRESENT to IFaceExitReason.REFUSED_NOT_PRESENT,
+            RefusalAnswer.REFUSED_YOUNG to IFaceExitReason.REFUSED_YOUNG,
+            RefusalAnswer.OTHER to IFaceExitReason.OTHER
+        ).forEach { (option, reason) ->
+            assertThat(option.fromDomainToExitReason()).isEqualTo(reason)
         }
     }
 }
