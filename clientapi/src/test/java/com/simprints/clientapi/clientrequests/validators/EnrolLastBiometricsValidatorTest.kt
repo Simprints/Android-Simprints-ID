a50b619c1b55d53de539d1012f26a09a7706c464
melad <melad@simprints.com>
2023-07-13 10:16:38 +0300

    CORE-2761 use AppRequestValidatorTest in EnrolLastBiometricsValidatorTest
    Former-commit-id: cf8a20ef7f1bc760daad6ea565a9bdfb5125a09c [formerly fcf19ee01a931e052798b7a4296b12571bf2c588]
    Former-commit-id: 2e97e440571465dccb07f1b3503fda8763f46dab

--- clientapi/src/test/java/com/simprints/clientapi/clientrequests/validators/EnrolLastBiometricsValidatorTest.kt
+++ clientapi/src/test/java/com/simprints/clientapi/clientrequests/validators/EnrolLastBiometricsValidatorTest.kt
@@ -1,52 +1,31 @@
 package com.simprints.clientapi.clientrequests.validators
 
-import com.simprints.clientapi.clientrequests.extractors.EnrolLastBiometricsExtractor
 import com.simprints.clientapi.exceptions.InvalidSessionIdException
-import com.simprints.clientapi.exceptions.InvalidUserIdException
+import com.simprints.clientapi.exceptions.InvalidStateForIntentAction
+import com.simprints.clientapi.requestFactories.EnrolLastBiometricsFactory
+import com.simprints.clientapi.requestFactories.RequestFactory
 import com.simprints.testtools.common.syntax.assertThrows
-import io.mockk.MockKAnnotations
 import io.mockk.every
-import io.mockk.impl.annotations.MockK
-import org.junit.Before
 import org.junit.Test
-import java.util.UUID
 
-class EnrolLastBiometricsValidatorTest {
+class EnrolLastBiometricsValidatorTest : AppRequestValidatorTest(EnrolLastBiometricsFactory) {
 
-    @MockK
-    lateinit var enrolLastBiometricsExtractor: EnrolLastBiometricsExtractor
-    private val sessionID = UUID.randomUUID().toString()
-
-    @Before
-    fun setUp() {
-        MockKAnnotations.init(this)
-        every { enrolLastBiometricsExtractor.getProjectId() } returns "xppPLwmR2eUmyN6LS3SN"
-        every { enrolLastBiometricsExtractor.getUserId() } returns "user_id"
-        every { enrolLastBiometricsExtractor.getModuleId() } returns "module_id"
-    }
+    private val mockExtractor = EnrolLastBiometricsFactory.getMockExtractor()
 
     @Test
     fun givenNotIdentificationAsLastFlow_enrolLastBiometricsReceived_shouldThrowAnException() {
-        val enrolLastBiometricsValidator = EnrolLastBiometricsValidator(enrolLastBiometricsExtractor, sessionID, false)
-
-        assertThrows<InvalidSessionIdException> {
-            enrolLastBiometricsValidator.validateClientRequest()
+        val validator =
+            EnrolLastBiometricsValidator(mockExtractor, RequestFactory.MOCK_SESSION_ID, false)
+        assertThrows<InvalidStateForIntentAction> {
+            validator.validateClientRequest()
         }
     }
 
     @Test
     fun aRequestWithoutSessionIdReceived_shouldThrowAnException() {
-        val enrolLastBiometricsValidator = EnrolLastBiometricsValidator(enrolLastBiometricsExtractor, sessionID, true)
+        every { mockExtractor.getSessionId() } returns ""
         assertThrows<InvalidSessionIdException> {
-            enrolLastBiometricsValidator.validateClientRequest()
-        }
-    }
-    @Test
-    fun aRequestWithoutUserIdReceived_shouldThrowAnException() {
-        every { enrolLastBiometricsExtractor.getUserId() } returns ""
-        val enrolLastBiometricsValidator = EnrolLastBiometricsValidator(enrolLastBiometricsExtractor, sessionID, true)
-        assertThrows<InvalidUserIdException> {
-            enrolLastBiometricsValidator.validateClientRequest()
+            EnrolLastBiometricsFactory.getValidator(mockExtractor).validateClientRequest()
         }
     }
 }
