7116b8ac542c4056ee4854377a9728095c9c1ac4
Alexandr Zhurkov <alexandr.zhurkov@gmail.com>
2023-07-31 12:38:17 +0300

    [CORE-2786] Adding test coverage
    Former-commit-id: dc7ab4e0247029e5dd26c9b0ac61de5824ac0a64 [formerly 9e7710751768328a134221b56ce18906b5c31fec]
    Former-commit-id: 19eb501a2b7965392f2a94e8affe1a1eaa7bd597

--- infra/event-sync/src/test/java/com/simprints/infra/eventsync/event/remote/events/callback/ApiErrorCallbackTest.kt
+++ infra/event-sync/src/test/java/com/simprints/infra/eventsync/event/remote/events/callback/ApiErrorCallbackTest.kt
@@ -1,7 +1,8 @@
 package com.simprints.infra.eventsync.event.remote.events.callback
 
 import com.google.common.truth.Truth.assertThat
-import com.simprints.infra.events.event.domain.models.callback.ErrorCallbackEvent
+import com.simprints.infra.events.event.domain.models.callback.ErrorCallbackEvent.ErrorCallbackPayload.Reason as ErrorReason
+import com.simprints.infra.eventsync.event.remote.models.callback.ApiErrorCallback
 import com.simprints.infra.eventsync.event.remote.models.callback.ApiErrorCallback.ApiReason.*
 import com.simprints.infra.eventsync.event.remote.models.callback.fromApiToDomain
 import com.simprints.infra.eventsync.event.remote.models.callback.fromDomainToApi
@@ -9,75 +10,50 @@ import org.junit.Test
 
 class ApiErrorCallbackTest {
 
-    @Test
-    fun differentProjectIdSignedIn_fromDomainToApi() {
-        val domain = ErrorCallbackEvent.ErrorCallbackPayload.Reason.DIFFERENT_PROJECT_ID_SIGNED_IN
-        val api = DIFFERENT_PROJECT_ID_SIGNED_IN
 
-        assertThat(domain.fromDomainToApi()).isEqualTo(api)
+    @Test
+    fun `ApiReason correctly mapped to domain`() {
+        mapOf(
+            ApiErrorCallback.ApiReason.DIFFERENT_PROJECT_ID_SIGNED_IN to DIFFERENT_PROJECT_ID_SIGNED_IN,
+            ApiErrorCallback.ApiReason.DIFFERENT_USER_ID_SIGNED_IN to DIFFERENT_USER_ID_SIGNED_IN,
+            ApiErrorCallback.ApiReason.GUID_NOT_FOUND_ONLINE to GUID_NOT_FOUND_ONLINE,
+            GUID_NOT_FOUND_OFFLINE to UNEXPECTED_ERROR,
+            ApiErrorCallback.ApiReason.UNEXPECTED_ERROR to UNEXPECTED_ERROR,
+            ApiErrorCallback.ApiReason.BLUETOOTH_NOT_SUPPORTED to BLUETOOTH_NOT_SUPPORTED,
+            SCANNER_LOW_BATTERY to UNEXPECTED_ERROR,
+            ApiErrorCallback.ApiReason.LOGIN_NOT_COMPLETE to LOGIN_NOT_COMPLETE,
+            ApiErrorCallback.ApiReason.ENROLMENT_LAST_BIOMETRICS_FAILED to ENROLMENT_LAST_BIOMETRICS_FAILED,
+            ApiErrorCallback.ApiReason.FACE_LICENSE_MISSING to FACE_LICENSE_MISSING,
+            ApiErrorCallback.ApiReason.FACE_LICENSE_INVALID to FACE_LICENSE_INVALID,
+            ApiErrorCallback.ApiReason.BACKEND_MAINTENANCE_ERROR to BACKEND_MAINTENANCE_ERROR,
+            ApiErrorCallback.ApiReason.PROJECT_ENDING to PROJECT_ENDING,
+            ApiErrorCallback.ApiReason.PROJECT_PAUSED to PROJECT_PAUSED,
+            ApiErrorCallback.ApiReason.BLUETOOTH_NO_PERMISSION to BLUETOOTH_NO_PERMISSION
+        ).forEach {
+            assertThat(it.key.fromApiToDomain()).isEqualTo(it.value)
+        }
     }
 
     @Test
-    fun differentUserIdSignedIn_fromDomainToApi() {
-        val domain = ErrorCallbackEvent.ErrorCallbackPayload.Reason.DIFFERENT_USER_ID_SIGNED_IN
-        val api = DIFFERENT_USER_ID_SIGNED_IN
-
-        assertThat(domain.fromDomainToApi()).isEqualTo(api)
-    }
-
-    @Test
-    fun guidNotFoundOnline_fromDomainToApi() {
-        val domain = ErrorCallbackEvent.ErrorCallbackPayload.Reason.GUID_NOT_FOUND_ONLINE
-        val api = GUID_NOT_FOUND_ONLINE
-
-        assertThat(domain.fromDomainToApi()).isEqualTo(api)
-    }
-
-    @Test
-    fun unexpectedError_fromDomainToApi() {
-        val domain = ErrorCallbackEvent.ErrorCallbackPayload.Reason.UNEXPECTED_ERROR
-        val api = UNEXPECTED_ERROR
-
-        assertThat(domain.fromDomainToApi()).isEqualTo(api)
-    }
-
-    @Test
-    fun bluetoothNotSupported_fromDomainToApi() {
-        val domain = ErrorCallbackEvent.ErrorCallbackPayload.Reason.BLUETOOTH_NOT_SUPPORTED
-        val api = BLUETOOTH_NOT_SUPPORTED
-
-        assertThat(domain.fromDomainToApi()).isEqualTo(api)
-    }
-
-    @Test
-    fun loginNotComplete_fromDomainToApi() {
-        val domain = ErrorCallbackEvent.ErrorCallbackPayload.Reason.LOGIN_NOT_COMPLETE
-        val api = LOGIN_NOT_COMPLETE
-
-        assertThat(domain.fromDomainToApi()).isEqualTo(api)
-    }
-
-    @Test
-    fun backendError_fromDomainToApi() {
-        val domain = ErrorCallbackEvent.ErrorCallbackPayload.Reason.BACKEND_MAINTENANCE_ERROR
-        val api = BACKEND_MAINTENANCE_ERROR
-
-        assertThat(domain.fromDomainToApi()).isEqualTo(api)
-    }
-
-    @Test
-    fun backendError_fromApiToDomain() {
-        val api = BACKEND_MAINTENANCE_ERROR
-        val domain = ErrorCallbackEvent.ErrorCallbackPayload.Reason.BACKEND_MAINTENANCE_ERROR
-
-        assertThat(api.fromApiToDomain()).isEqualTo(domain)
-    }
-
-    @Test
-    fun loginNotComplete_fromApiToDomain() {
-        val api = LOGIN_NOT_COMPLETE
-        val domain = ErrorCallbackEvent.ErrorCallbackPayload.Reason.LOGIN_NOT_COMPLETE
-
-        assertThat(api.fromApiToDomain()).isEqualTo(domain)
+    fun `Reason correctly mapped to ApiReason`() {
+        mapOf(
+            ErrorReason.DIFFERENT_PROJECT_ID_SIGNED_IN to ApiErrorCallback.ApiReason.DIFFERENT_PROJECT_ID_SIGNED_IN,
+            ErrorReason.DIFFERENT_USER_ID_SIGNED_IN to ApiErrorCallback.ApiReason.DIFFERENT_USER_ID_SIGNED_IN,
+            ErrorReason.GUID_NOT_FOUND_ONLINE to ApiErrorCallback.ApiReason.GUID_NOT_FOUND_ONLINE,
+            ErrorReason.UNEXPECTED_ERROR to ApiErrorCallback.ApiReason.UNEXPECTED_ERROR,
+            ErrorReason.BLUETOOTH_NOT_SUPPORTED to ApiErrorCallback.ApiReason.BLUETOOTH_NOT_SUPPORTED,
+            ErrorReason.LOGIN_NOT_COMPLETE to ApiErrorCallback.ApiReason.LOGIN_NOT_COMPLETE,
+            ErrorReason.ENROLMENT_LAST_BIOMETRICS_FAILED to ApiErrorCallback.ApiReason.ENROLMENT_LAST_BIOMETRICS_FAILED,
+            ErrorReason.FACE_LICENSE_MISSING to ApiErrorCallback.ApiReason.FACE_LICENSE_MISSING,
+            ErrorReason.FACE_LICENSE_INVALID to ApiErrorCallback.ApiReason.FACE_LICENSE_INVALID,
+            ErrorReason.FINGERPRINT_CONFIGURATION_ERROR to ApiErrorCallback.ApiReason.UNEXPECTED_ERROR,
+            ErrorReason.FACE_CONFIGURATION_ERROR to ApiErrorCallback.ApiReason.UNEXPECTED_ERROR,
+            ErrorReason.BACKEND_MAINTENANCE_ERROR to ApiErrorCallback.ApiReason.BACKEND_MAINTENANCE_ERROR,
+            ErrorReason.PROJECT_ENDING to ApiErrorCallback.ApiReason.PROJECT_ENDING,
+            ErrorReason.PROJECT_PAUSED to ApiErrorCallback.ApiReason.PROJECT_PAUSED,
+            ErrorReason.BLUETOOTH_NO_PERMISSION to ApiErrorCallback.ApiReason.BLUETOOTH_NO_PERMISSION,
+        ).forEach {
+            assertThat(it.key.fromDomainToApi()).isEqualTo(it.value)
+        }
     }
 }
