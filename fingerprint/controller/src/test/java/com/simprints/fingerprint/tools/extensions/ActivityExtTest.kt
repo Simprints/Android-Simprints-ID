3f4f92974017faadf1bd8e86d60a52d48506acec
Alexandr Zhurkov <alexandr.zhurkov@gmail.com>
2023-08-01 13:30:25 +0300

    [CORE-2786] Moving ActivityExt to 'core' module
    Former-commit-id: 551919c8d9d943f969c436b48f3359e86b73ba3a [formerly 2d4404a674bbb1d4b32d6c0dfd0f3a4a73b72ad5]
    Former-commit-id: b36a8f857ad0217d329fe193a71d1b11bfeb3d0b

--- fingerprint/controller/src/test/java/com/simprints/fingerprint/tools/extensions/ActivityExtTest.kt
+++ fingerprint/controller/src/test/java/com/simprints/fingerprint/tools/extensions/ActivityExtTest.kt
@@ -1,13 +1,9 @@
 package com.simprints.fingerprint.tools.extensions
 
-import android.app.Activity
 import android.content.Context
 import android.os.Looper
-import androidx.core.app.ActivityCompat
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
-import com.simprints.core.domain.permission.Permission
-import com.simprints.core.tools.extentions.permissionFromResult
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkStatic
@@ -53,48 +49,5 @@ class ActivityExtTest {
         ShadowLooper.idleMainLooper()
         // Then
         assertThat(x).isEqualTo(2)
-    }
-
-    @Test
-    fun `when grant result is true, then should map to Permission_Granted`() {
-        val permission = runPermissionMappingTest(
-            grantResult = true,
-            shouldShowRationale = true
-        )
-        assertThat(permission).isEqualTo(Permission.Granted)
-    }
-
-    @Test
-    fun `given grant result is false, when shouldShowRationale is true, then should map to Permission_Denied`() {
-        val permission = runPermissionMappingTest(
-            grantResult = false,
-            shouldShowRationale = true
-        )
-        assertThat(permission).isEqualTo(Permission.Denied)
-    }
-
-    @Test
-    fun `given grant result is false, when shouldShowRationale is false, then should map to Permission_DeniedNeverAskAgain`() {
-        val permission = runPermissionMappingTest(
-            grantResult = false,
-            shouldShowRationale = false
-        )
-        assertThat(permission).isEqualTo(Permission.DeniedNeverAskAgain)
-    }
-
-    private fun runPermissionMappingTest(
-        grantResult: Boolean,
-        shouldShowRationale: Boolean
-    ): Permission {
-        val activity = mockk<Activity>()
-        val permission = "permission"
-        mockk<ActivityCompat>()
-        every { ActivityCompat.shouldShowRequestPermissionRationale(activity, permission) }
-            .returns(shouldShowRationale)
-
-        return activity.permissionFromResult(
-            permission = permission,
-            grantResult = grantResult
-        )
     }
 }
