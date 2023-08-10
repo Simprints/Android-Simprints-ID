8188d7ca67e924bafaac50428eb909d6974e6441
alex <alexandr@simprints.com>
2023-08-10 13:21:37 +0300

    CORE-2503 Adding tokenization keys to the project config model; Adding Tink library dependency;
    Former-commit-id: 015d16bdbc304d0ee197b276b8177be24dc71637 [formerly 6bc370a87e520e3d1479618719dfcfc605703f03]
    Former-commit-id: 465a7138a659609870c2f942611debcb3a64d451

--- infra/config/src/main/java/com/simprints/infra/config/local/models/Project.kt
+++ infra/config/src/main/java/com/simprints/infra/config/local/models/Project.kt
@@ -1,6 +1,13 @@
 package com.simprints.infra.config.local.models
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.simprints.core.tools.extentions.filterNotNullValues
+import com.simprints.core.tools.json.JsonHelper
+import com.simprints.infra.config.domain.models.KeyMaterialType
 import com.simprints.infra.config.domain.models.Project
+import com.simprints.infra.config.domain.models.TokenKeyType
+import com.simprints.infra.config.domain.models.TokenizationKeyData
+import com.simprints.infra.logging.Simber
 
 internal fun Project.toProto(): ProtoProject =
     ProtoProject.newBuilder()
@@ -11,8 +18,94 @@ internal fun Project.toProto(): ProtoProject =
         .setImageBucket(imageBucket)
         .also {
             if (baseUrl != null) it.baseUrl = baseUrl
+            tokenizationKeys?.mapKeys { entry ->
+                entry.key.toString()
+            }?.mapValues { entry ->
+                JsonHelper.toJson(entry.value)
+            }?.let { map ->
+                it.putAllTokenizationKeys(map)
+            }
         }
         .build()
 
-internal fun ProtoProject.toDomain(): Project =
-    Project(id, name, description, creator, imageBucket, baseUrl)
+internal fun ProtoProject.toDomain(): Project {
+    val tokenizationKeys = tokenizationKeysMap.asTokenizationKeysMap()
+    return Project(
+        id = id,
+        name = name,
+        description = description,
+        creator = creator,
+        imageBucket = imageBucket,
+        baseUrl = baseUrl,
+        tokenizationKeys = tokenizationKeys
+    )
+}
+
+/**
+ * Maps remote response map of type of String <-> JsonString to the map of [TokenKeyType] <-> [TokenizationKeyData]
+ */
+internal fun Map<String, String>?.asTokenizationKeysMap(): Map<TokenKeyType, TokenizationKeyData> {
+    return (this ?: emptyMap()).mapKeys { entry ->
+        runCatching { TokenKeyType.valueOf(entry.key) }.getOrElse { TokenKeyType.Unknown }
+    }.mapValues { entry ->
+        try {
+            JsonHelper.fromJson(
+                json = entry.value,
+                type = object : TypeReference<TokenizationItem>() {}
+            ).toTokenizationKeyData()
+        } catch (e: Exception) {
+            Simber.e(e)
+            return@mapValues null
+        }
+    }.filterNotNullValues()
+}
+
+/**
+ * Data class that contains the following JSON structure which is received from the backend:
+ *
+ * {
+ *   "primaryKeyId": 987654432,
+ *   "key": [
+ *     {
+ *       "keyData": {
+ *         "typeUrl": "typeUrl",
+ *         "value": "AzT...b12C",
+ *         "keyMaterialType": "SYMMETRIC"
+ *       },
+ *       "status": "ENABLED",
+ *       "keyId": 945828840,
+ *       "outputPrefixType": "TINK"
+ *     }
+ *   ]
+ * }
+ */
+private data class TokenizationItem(
+    val primaryKeyId: Long,
+    val key: List<TokenizationKey>,
+) {
+    fun toTokenizationKeyData(): TokenizationKeyData? {
+        val tokenizationKey = key.firstOrNull() ?: return null
+        return TokenizationKeyData(
+            primaryKeyId = primaryKeyId,
+            typeUrl = tokenizationKey.keyData.typeUrl,
+            value = tokenizationKey.keyData.value,
+            keyMaterialType = runCatching { KeyMaterialType.valueOf(tokenizationKey.keyData.keyMaterialType) }.getOrElse { KeyMaterialType.Unknown },
+            isEnabled = tokenizationKey.status.equals("enabled", ignoreCase = true),
+            keyId = tokenizationKey.keyId,
+            outputPrefixType = tokenizationKey.outputPrefixType
+        )
+    }
+}
+
+private data class TokenizationKey(
+    val keyData: KeyData,
+    val status: String,
+    val keyId: Long,
+    val outputPrefixType: String,
+)
+
+private data class KeyData(
+    val typeUrl: String,
+    val value: String,
+    val keyMaterialType: String,
+)
