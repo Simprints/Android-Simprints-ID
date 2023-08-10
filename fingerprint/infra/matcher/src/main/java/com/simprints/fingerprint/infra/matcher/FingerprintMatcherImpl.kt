e8b01f7bf7e53191d0f3a02a6fb3d33947f4dbf9
melad <melad@simprints.com>
2023-08-10 09:52:22 +0300

    CORE-2810 Remove the MatchingAlgorithm enum
    Former-commit-id: cb1c741e3b4dde1cf0ce08ddc0bd5ba8ab74dffb [formerly f3570e43711a64384229b2867f85f6056ed85b17]
    Former-commit-id: ac38a33874264679d54571c6b97aa749f14c5bdb

--- fingerprint/infra/matcher/src/main/java/com/simprints/fingerprint/infra/matcher/FingerprintMatcherImpl.kt
+++ fingerprint/infra/matcher/src/main/java/com/simprints/fingerprint/infra/matcher/FingerprintMatcherImpl.kt
@@ -3,7 +3,6 @@ package com.simprints.fingerprint.infra.matcher
 import com.simprints.fingerprint.infra.matcher.algorithms.simafis.SimAfisMatcher
 import com.simprints.fingerprint.infra.matcher.domain.FingerprintIdentity
 import com.simprints.fingerprint.infra.matcher.domain.MatchResult
-import com.simprints.fingerprint.infra.matcher.domain.MatchingAlgorithm
 import javax.inject.Inject
 
 internal class FingerprintMatcherImpl @Inject constructor(
@@ -13,12 +12,9 @@ internal class FingerprintMatcherImpl @Inject constructor(
     override fun match(
         probe: FingerprintIdentity,
         candidates: List<FingerprintIdentity>,
-        matchingAlgorithm: MatchingAlgorithm,
         crossFingerComparison: Boolean
-    ): List<MatchResult> =
-        when (matchingAlgorithm) {
-            MatchingAlgorithm.SIM_AFIS -> simAfisMatch(probe, candidates, crossFingerComparison)
-        }
+    ): List<MatchResult> = simAfisMatch(probe, candidates, crossFingerComparison)
+
 
     private fun simAfisMatch(
         probe: FingerprintIdentity,
