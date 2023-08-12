405f4fd35fa497387a70af61593465331ba523f3
melad <melad@simprints.com>
2023-08-12 20:48:02 +0300

    CORE-2810 read and write the template format to the parcelable
    Former-commit-id: 1c79dab2d35ae0644940147d0b1303b6c97b695c [formerly 49f668c910f91c09bb30ed2d76c98a27ac5cdc28]
    Former-commit-id: c4d76abeb700156a3338228c61e49bf7dcb34f06

--- fingerprint/controller/src/main/java/com/simprints/fingerprint/data/domain/fingerprint/Fingerprint.kt
+++ fingerprint/controller/src/main/java/com/simprints/fingerprint/data/domain/fingerprint/Fingerprint.kt
@@ -3,8 +3,6 @@ package com.simprints.fingerprint.data.domain.fingerprint
 import android.os.Parcel
 import android.os.Parcelable
 import com.simprints.fingerprint.data.domain.images.FingerprintImageRef
-import kotlinx.parcelize.Parceler
-import kotlinx.parcelize.Parcelize
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
@@ -17,7 +15,6 @@ import java.nio.ByteOrder
  * @property imageRef  the fingerprint image that was captured to generate the template
  * @property format  the format for the template (i.e. the format used to generate the template which will determine the format used to match fingerprints)
  */
-@Parcelize
 class Fingerprint(
     val fingerId: FingerIdentifier,
     val template: ByteBuffer,
@@ -67,8 +64,19 @@ class Fingerprint(
         require(this.template.getInt(RECORD_LENGTH) == isoTemplateBytes.size) { "Invalid template: invalid length" }
         require(this.template.get(NB_FINGERPRINTS) == 1.toByte()) { "Invalid template: only single fingerprint template ares supported" }
     }
+    override fun describeContents() = 0
 
-    companion object : Parceler<Fingerprint> {
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.run {
+            writeInt(fingerId.ordinal)
+            val bytes = templateBytes
+            writeInt(bytes.size)
+            writeByteArray(bytes)
+            writeString(format)
+        }
+    }
+
+    companion object CREATOR : Parcelable.Creator<Fingerprint> {
 
         private val ISO_FORMAT_ID = Integer.parseInt("464D5200", 16)     // 'F' 'M' 'R' 00hex
         private val ISO_2005_VERSION = Integer.parseInt("20323000", 16)  // ' ' '2' '0' 00hex
@@ -78,22 +86,15 @@ class Fingerprint(
         private const val NB_FINGERPRINTS = 22       // BYTE
         private const val FIRST_QUALITY = 26         // BYTE
 
-        override fun Fingerprint.write(parcel: Parcel, flags: Int) {
-            parcel.writeInt(fingerId.ordinal)
-            val bytes = this.templateBytes
-            parcel.writeInt(bytes.size)
-            parcel.writeByteArray(bytes)
-            parcel.writeString(format)
-        }
-
-        override fun create(parcel: Parcel): Fingerprint {
-            val fingerId = FingerIdentifier.values()[parcel.readInt()]
-            val temp = ByteArray(parcel.readInt())
-            parcel.readByteArray(temp)
+        override fun createFromParcel(source: Parcel): Fingerprint {
+            val fingerId = FingerIdentifier.values()[source.readInt()]
+            val temp = ByteArray(source.readInt())
+            source.readByteArray(temp)
             val template = ByteBuffer.allocateDirect(temp.size)
             template.put(temp)
-            val format = parcel.readString()!!
+            val format = source.readString()!!
             return Fingerprint(fingerId, template, format = format)
         }
+        override fun newArray(size: Int): Array<Fingerprint?> = arrayOfNulls(size)
     }
 }
