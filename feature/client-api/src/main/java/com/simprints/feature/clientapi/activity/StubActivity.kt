e727d41807368abb5d859157b4df39075a80527f
Sergejs Luhmirins <sergejs.luhmirins@gmail.com>
2023-08-24 15:58:17 +0300

    CORE-2162 Add basic intent handling in ClientApiActivity
    Former-commit-id: 476adaab9e9e56af08c8755ab2939b87d209f956 [formerly 5e915ae6e9d27f92bc6ff1be2b35dcbe1b23d2a6]
    Former-commit-id: 01ffdf05514be8d57f326064d83c27fcf8e3dce9

--- feature/client-api/src/main/java/com/simprints/feature/clientapi/activity/StubActivity.kt
+++ feature/client-api/src/main/java/com/simprints/feature/clientapi/activity/StubActivity.kt
@@ -1,11 +1,34 @@
 package com.simprints.feature.clientapi.activity
 
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
 import com.simprints.core.tools.activity.BaseActivity
-import com.simprints.feature.clientapi.R
+import com.simprints.feature.clientapi.databinding.ClientStubActivityBinding
+import com.simprints.infra.uibase.viewbinding.viewBinding
+import com.simprints.infra.resources.R as IDR
 
 /**
  * To be used as temporary destination for unfinished flows.
  *
  * TODO: Remove once orchestrator is done - https://simprints.atlassian.net/browse/CORE-2845
  */
-class StubActivity : BaseActivity(R.layout.client_stub_activity)
+class StubActivity : BaseActivity() {
+
+    private val binding by viewBinding(ClientStubActivityBinding::inflate)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.stubTitle.text = intent.getStringExtra(EXTRA_MESSAGE)
+            ?: getString(IDR.string.app_name)
+    }
+
+    companion object {
+        fun getIntent(from: Context, message: String) = Intent(from, StubActivity::class.java)
+            .putExtra(EXTRA_MESSAGE, message)
+
+        private const val EXTRA_MESSAGE = "extra_message"
+    }
+}
--- feature/client-api/src/main/java/com/simprints/feature/clientapi/session/ClientSessionManager.kt
+++ feature/client-api/src/main/java/com/simprints/feature/clientapi/session/ClientSessionManager.kt
@@ -1,14 +1,22 @@
 package com.simprints.feature.clientapi.session
 
+import com.simprints.core.ExternalScope
+import com.simprints.core.tools.time.TimeHelper
 import com.simprints.infra.events.EventRepository
+import com.simprints.infra.events.event.domain.models.InvalidIntentEvent
+import com.simprints.infra.events.event.domain.models.SuspiciousIntentEvent
 import com.simprints.infra.events.event.domain.models.callback.IdentificationCallbackEvent
 import com.simprints.infra.events.event.domain.models.callout.EnrolmentCalloutEvent
 import com.simprints.infra.events.event.domain.models.callout.IdentificationCalloutEvent
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 internal class ClientSessionManager @Inject constructor(
     private val coreEventRepository: EventRepository,
+    private val timeHelper: TimeHelper,
+    @ExternalScope private val externalScope: CoroutineScope
 ) {
 
     suspend fun getCurrentSessionId(): String = coreEventRepository.getCurrentCaptureSessionEvent().id
@@ -24,4 +32,18 @@ internal class ClientSessionManager @Inject constructor(
         .observeEventsFromSession(sessionId)
         .toList()
         .any { it is IdentificationCallbackEvent }
+
+    fun reportUnknownExtras(unknownExtras: Map<String, Any?>) {
+        if (unknownExtras.isNotEmpty()) {
+            externalScope.launch {
+                coreEventRepository.addOrUpdateEvent(SuspiciousIntentEvent(timeHelper.now(), unknownExtras))
+            }
+        }
+    }
+
+    fun addInvalidIntentEvent(action: String, extras: Map<String, Any>) {
+        externalScope.launch {
+            coreEventRepository.addOrUpdateEvent(InvalidIntentEvent(timeHelper.now(), action, extras))
+        }
+    }
 }
