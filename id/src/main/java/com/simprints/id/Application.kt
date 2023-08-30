d44a04387c538cadbaffe8763b52116a3d59bac6
melad <melad@simprints.com>
2023-08-30 11:01:18 +0300

    rename device id to deviceHardwareId to resolve conflict with the new native APIs
    Former-commit-id: 83d5f973b6aa9cd2d5a17cc3fdbf9fec8401616c [formerly eca40c9a09447c7f2b137f2ada5c8f710ac616af]
    Former-commit-id: efcdb0e65d2ed5afdd4f60491991291a79f1ab7f

--- id/src/main/java/com/simprints/id/Application.kt
+++ id/src/main/java/com/simprints/id/Application.kt
@@ -1,10 +1,10 @@
 package com.simprints.id
 
 import android.content.Context
-import android.telephony.TelephonyManager
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import com.simprints.core.CoreApplication
+import com.simprints.core.tools.extentions.deviceHardwareId
 import com.simprints.core.tools.utils.LanguageHelper
 import com.simprints.infra.logging.LoggingConstants.CrashReportingCustomKeys.DEVICE_ID
 import com.simprints.infra.logging.Simber
@@ -40,8 +40,7 @@ open class Application : CoreApplication(), Configuration.Provider {
     open fun initApplication() {
         handleUndeliverableExceptionInRxJava()
         SimberBuilder.initialize(this)
-        ((TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId()
-        Simber.tag(DEVICE_ID, true).i(deviceId)
+        Simber.tag(DEVICE_ID, true).i(deviceHardwareId)
     }
 
     // RxJava doesn't allow not handled exceptions, when that happens the app crashes.
