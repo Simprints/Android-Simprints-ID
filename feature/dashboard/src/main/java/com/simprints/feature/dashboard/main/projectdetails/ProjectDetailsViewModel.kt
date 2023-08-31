13a80bc705ad867976312ed041b8c255ca2360b1
Alexandr Zhurkov <alexandr.zhurkov@gmail.com>
2023-08-31 17:33:10 +0300

    [CORE-2506] Decrypting attendant id (last user) on the project details screen
    Former-commit-id: 98fc5efc735ad22539f2a4cbf0806b0ca5b27ab5 [formerly 24d6fc439d52a095136c021bcc9b4624e8e40fdb]
    Former-commit-id: bd4b3bc9952cd9997aa240d8c74669362eb71c43

--- feature/dashboard/src/main/java/com/simprints/feature/dashboard/main/projectdetails/ProjectDetailsViewModel.kt
+++ feature/dashboard/src/main/java/com/simprints/feature/dashboard/main/projectdetails/ProjectDetailsViewModel.kt
@@ -6,6 +6,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.simprints.infra.config.ConfigManager
 import com.simprints.infra.authstore.AuthStore
+import com.simprints.infra.config.domain.TokenizationAction
+import com.simprints.infra.config.domain.models.TokenKeyType
+import com.simprints.infra.config.tokenization.TokenizationManager
 import com.simprints.infra.recent.user.activity.RecentUserActivityManager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -16,6 +19,7 @@ internal class ProjectDetailsViewModel @Inject constructor(
     private val configManager: ConfigManager,
     private val authStore: AuthStore,
     private val recentUserActivityManager: RecentUserActivityManager,
+    private val tokenizationManager: TokenizationManager
 ) : ViewModel() {
 
     val projectCardStateLiveData: LiveData<DashboardProjectState>
@@ -31,11 +35,17 @@ internal class ProjectDetailsViewModel @Inject constructor(
             val projectId = authStore.signedInProjectId
             val cachedProject = configManager.getProject(projectId)
             val recentUserActivity = recentUserActivityManager.getRecentUserActivity()
+            val decryptedUserId = tokenizationManager.tryTokenize(
+                value = recentUserActivity.lastUserUsed,
+                tokenKeyType = TokenKeyType.AttendantId,
+                action = TokenizationAction.Decrypt,
+                project = cachedProject
+            )
             DashboardProjectState(
-                cachedProject.name,
-                recentUserActivity.lastUserUsed,
-                recentUserActivity.lastScannerUsed,
-                true
+                title = cachedProject.name,
+                lastUser = decryptedUserId,
+                lastScanner = recentUserActivity.lastScannerUsed,
+                isLoaded = true
             )
         } catch (_: Throwable) {
             DashboardProjectState(isLoaded = false)
