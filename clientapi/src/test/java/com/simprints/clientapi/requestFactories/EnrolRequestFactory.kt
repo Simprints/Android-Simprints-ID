f46e96f8b3590e53193285ce9b85296c4d9a7d00
Alexandr Zhurkov <alexandr.zhurkov@gmail.com>
2023-08-31 10:43:36 +0300

    [CORE-2504] fixing tests
    Former-commit-id: d4b4ba6bb913be420a314fe493ef814096537227 [formerly 5c6d8adb906bf55484d39786c6d844cd1cec127b]
    Former-commit-id: 02ce7694114f7f91ce0371a80358ef66167e13f8

--- clientapi/src/test/java/com/simprints/clientapi/requestFactories/EnrolRequestFactory.kt
+++ clientapi/src/test/java/com/simprints/clientapi/requestFactories/EnrolRequestFactory.kt
@@ -1,12 +1,16 @@
 package com.simprints.clientapi.requestFactories
 
 import com.simprints.clientapi.clientrequests.builders.EnrolBuilder
+import com.simprints.clientapi.clientrequests.builders.EnrolLastBiometricsBuilder
 import com.simprints.clientapi.clientrequests.extractors.ClientRequestExtractor
 import com.simprints.clientapi.clientrequests.extractors.EnrolExtractor
+import com.simprints.clientapi.clientrequests.extractors.EnrolLastBiometricsExtractor
 import com.simprints.clientapi.clientrequests.validators.EnrolValidator
 import com.simprints.clientapi.controllers.core.eventData.model.IntegrationInfo
 import com.simprints.clientapi.domain.requests.BaseRequest
 import com.simprints.clientapi.domain.requests.EnrolRequest
+import com.simprints.core.tools.utils.Tokenization
+import com.simprints.infra.config.domain.models.Project
 import io.mockk.mockk
 
 object EnrolRequestFactory : RequestFactory() {
@@ -20,8 +24,16 @@ object EnrolRequestFactory : RequestFactory() {
             unknownExtras = emptyMap()
         )
 
-    override fun getBuilder(extractor: ClientRequestExtractor): EnrolBuilder =
-        EnrolBuilder(extractor as EnrolExtractor, getValidator(extractor))
+    override fun getBuilder(extractor: ClientRequestExtractor): EnrolBuilder {
+        val project = mockk<Project>()
+        val tokenization = mockk<Tokenization>()
+        return EnrolBuilder(
+            extractor = extractor as EnrolExtractor,
+            project = project,
+            tokenization = tokenization,
+            validator = getValidator(extractor)
+        )
+    }
 
     override fun getValidator(extractor: ClientRequestExtractor): EnrolValidator =
         EnrolValidator(extractor as EnrolExtractor)
