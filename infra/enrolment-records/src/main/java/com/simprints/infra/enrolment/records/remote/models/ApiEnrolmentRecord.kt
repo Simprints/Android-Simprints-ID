6bfa94d163f51e3a9a283efcf1a23d27dc2503db
Alexandr Zhurkov <alexandr.zhurkov@gmail.com>
2023-09-14 19:57:24 +0300

    [CORE-2860] Adding 'tokenizedFields' property to the ApiEnrolmentRecord and ApiEvent
    Former-commit-id: 10d3de1a465920481eb1e6aafac9da4d5a755301 [formerly b8b5a363128e1b694a21f0ab964cb15fe2b3636c]
    Former-commit-id: 600c9150a9907fdb8895c354b2cf14dec263b2cb

--- infra/enrolment-records/src/main/java/com/simprints/infra/enrolment/records/remote/models/ApiEnrolmentRecord.kt
+++ infra/enrolment-records/src/main/java/com/simprints/infra/enrolment/records/remote/models/ApiEnrolmentRecord.kt
@@ -3,8 +3,10 @@ package com.simprints.infra.enrolment.records.remote.models
 import androidx.annotation.Keep
 import com.simprints.core.domain.face.FaceSample
 import com.simprints.core.domain.fingerprint.FingerprintSample
+import com.simprints.core.domain.tokenization.TokenizableString
 import com.simprints.core.tools.utils.EncodingUtils
 import com.simprints.infra.enrolment.records.domain.models.Subject
+import com.simprints.infra.enrolment.records.remote.models.ApiEnrolmentRecord.Companion.MODULE_ID
 import com.simprints.infra.enrolment.records.remote.models.face.toApi
 import com.simprints.infra.enrolment.records.remote.models.fingerprint.toApi
 
@@ -13,16 +15,27 @@ internal data class ApiEnrolmentRecord(
     val subjectId: String,
     val moduleId: String,
     val attendantId: String,
+    val tokenizedFields: List<String>,
     val biometricReferences: List<ApiBiometricReference>
-)
+) {
+  companion object {
+      const val MODULE_ID = "moduleId"
+  }
+}
 
-internal fun Subject.toEnrolmentRecord(encoder: EncodingUtils): ApiEnrolmentRecord =
-    ApiEnrolmentRecord(
-        subjectId,
-        moduleId.value,
-        attendantId.value,
-        buildBiometricReferences(fingerprintSamples, faceSamples, encoder)
+internal fun Subject.toEnrolmentRecord(encoder: EncodingUtils): ApiEnrolmentRecord {
+    val tokenizedFields: List<String> = when(moduleId){
+        is TokenizableString.Raw -> emptyList()
+        is TokenizableString.Tokenized -> listOf(MODULE_ID)
+    }
+    return ApiEnrolmentRecord(
+        subjectId = subjectId,
+        moduleId = moduleId.value,
+        attendantId = attendantId.value,
+        tokenizedFields = tokenizedFields,
+        biometricReferences = buildBiometricReferences(fingerprintSamples, faceSamples, encoder)
     )
+}
 
 internal fun buildBiometricReferences(
     fingerprintSamples: List<FingerprintSample>,
