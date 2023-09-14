4765a80dd5a3fd518ac82a0e7cbe2ad3619d5480
Alexandr Zhurkov <alexandr.zhurkov@gmail.com>
2023-09-14 19:57:24 +0300

    [CORE-2860] Adding 'tokenizedFields' property to the ApiEnrolmentRecord and ApiEvent
    Former-commit-id: 10d3de1a465920481eb1e6aafac9da4d5a755301 [formerly b8b5a363128e1b694a21f0ab964cb15fe2b3636c]
    Former-commit-id: 600c9150a9907fdb8895c354b2cf14dec263b2cb

--- infra/event-sync/src/main/java/com/simprints/infra/eventsync/event/remote/models/ApiEvent.kt
+++ infra/event-sync/src/main/java/com/simprints/infra/eventsync/event/remote/models/ApiEvent.kt
@@ -1,14 +1,43 @@
 package com.simprints.infra.eventsync.event.remote.models
 
 import androidx.annotation.Keep
+import com.simprints.core.domain.tokenization.TokenizableString
+import com.simprints.infra.config.domain.models.TokenKeyType
 import com.simprints.infra.events.event.domain.models.Event
+import com.simprints.infra.eventsync.event.remote.models.ApiEvent.Companion.CALLOUT_MODULE_ID
+import com.simprints.infra.eventsync.event.remote.models.ApiEvent.Companion.CALLOUT_USER_ID
 
 @Keep
 internal data class ApiEvent(
     val id: String,
     val labels: ApiEventLabels,
-    val payload: ApiEventPayload
-)
+    val payload: ApiEventPayload,
+    val tokenizedFields: List<String>
+) {
+    companion object {
+        private const val CALLOUT_PREFIX = "callout"
+        const val CALLOUT_MODULE_ID = "$CALLOUT_PREFIX.moduleId"
+        const val CALLOUT_USER_ID = "$CALLOUT_PREFIX.userId"
+    }
+}
 
-internal fun Event.fromDomainToApi() =
-    ApiEvent(id, labels.fromDomainToApi(), payload.fromDomainToApi())
+internal fun Event.fromDomainToApi() {
+    val tokenizedFields = getTokenizedFields().mapNotNull { tokenizedFieldEntry ->
+        when (tokenizedFieldEntry.value) {
+            is TokenizableString.Raw -> null
+            is TokenizableString.Tokenized -> tokenizedFieldEntry.key.mapToTokenizedString()
+        }
+    }
+    ApiEvent(
+        id = id,
+        labels = labels.fromDomainToApi(),
+        payload = payload.fromDomainToApi(),
+        tokenizedFields = tokenizedFields
+    )
+}
+
+internal fun TokenKeyType.mapToTokenizedString(): String? = when (this) {
+    TokenKeyType.AttendantId -> CALLOUT_USER_ID
+    TokenKeyType.ModuleId -> CALLOUT_MODULE_ID
+    TokenKeyType.Unknown -> null
+}
