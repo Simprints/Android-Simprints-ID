abfb5dda12d0e27e8648610fdba01067def06169
Sergejs Luhmirins <sergejs.luhmirins@gmail.com>
2023-10-09 10:04:51 +0300

    CORE-2844 Fix typo in orchestration-data test package names
    Former-commit-id: 8d8ebdc02e3fc2ff33b86b1b5af1115e674e0a4b [formerly 0b1ab1c3283685f5f0fbc132c174a6d0f10889df]
    Former-commit-id: aa8108afe713490d2163298844e2d44ab171113f

--- infra/orchestrator-data/src/test/java/com/simprints/infra/orchestration/data/ActionRequestIdentifierTest.kt
+++ infra/orchestrator-data/src/test/java/com/simprints/infra/orchestration/data/ActionRequestIdentifierTest.kt
@@ -1,7 +1,6 @@
-package com.simprints.infra.orcehstration.data
+package com.simprints.infra.orchestration.data
 
 import com.google.common.truth.Truth.assertThat
-import com.simprints.infra.orchestration.data.ActionRequestIdentifier
 import org.junit.Test
 
 class ActionRequestIdentifierTest {
