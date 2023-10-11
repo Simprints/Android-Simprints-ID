cab3f3db04002c51fcd1a88060b736375e0bf8bf
alex <alexandr@simprints.com>
2023-10-11 16:01:54 +0300

    [CORE-2869] Adding test cases to EnrolmentHelperImplTest
    Former-commit-id: 09bced12216cc9d4c3a202d87b515544c6f4c267 [formerly f65d0283618fdd64eb1b5a5d419c9530f88bff71]
    Former-commit-id: 71b4ef6a93529dd7acaa4a2d67bce72675646e9d

--- id/src/test/java/com/simprints/id/orchestrator/EnrolmentHelperImplTest.kt
+++ id/src/test/java/com/simprints/id/orchestrator/EnrolmentHelperImplTest.kt
@@ -1,17 +1,30 @@
 package com.simprints.id.orchestrator
 
+import com.simprints.core.domain.fingerprint.FingerprintSample
+import com.simprints.core.domain.tokenization.asTokenizedRaw
 import com.simprints.core.tools.time.TimeHelper
+import com.simprints.id.domain.moduleapi.face.responses.entities.FaceCaptureResult
+import com.simprints.id.domain.moduleapi.face.responses.entities.FaceCaptureSample
+import com.simprints.id.domain.moduleapi.fingerprint.models.fromDomainToModuleApi
+import com.simprints.id.domain.moduleapi.fingerprint.responses.FingerprintCaptureResponse
+import com.simprints.id.domain.moduleapi.fingerprint.responses.entities.FingerprintCaptureResult
+import com.simprints.id.domain.moduleapi.fingerprint.responses.entities.FingerprintCaptureSample
 import com.simprints.id.testtools.TestData.defaultSubject
+import com.simprints.infra.config.store.models.Finger
 import com.simprints.infra.enrolment.records.EnrolmentRecordManager
 import com.simprints.infra.enrolment.records.domain.models.SubjectAction
 import com.simprints.infra.events.EventRepository
 import com.simprints.infra.events.event.domain.models.EnrolmentEventV2
+import com.simprints.infra.events.sampledata.FACE_TEMPLATE_FORMAT
 import com.simprints.infra.events.sampledata.SampleDefaults
 import com.simprints.infra.events.sampledata.SampleDefaults.CREATED_AT
+import com.simprints.infra.events.sampledata.SampleDefaults.STATIC_GUID
 import com.simprints.infra.events.sampledata.createPersonCreationEvent
 import com.simprints.infra.events.sampledata.createSessionCaptureEvent
 import com.simprints.infra.eventsync.sync.down.tasks.SubjectFactory
 import com.simprints.testtools.common.coroutines.TestCoroutineRule
+import com.simprints.testtools.common.mock.mockTemplate
+import com.simprints.testtools.unit.EncodingUtilsImplForTests
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.coVerify
@@ -20,12 +33,14 @@ import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
 import io.mockk.mockkStatic
 import io.mockk.unmockkAll
+import io.mockk.verify
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import java.util.Date
 import java.util.UUID
 
 class EnrolmentHelperImplTest {
@@ -60,7 +75,9 @@ class EnrolmentHelperImplTest {
         )
         every { timeHelper.now() } returns CREATED_AT
         coEvery { eventRepository.getCurrentCaptureSessionEvent() } returns createSessionCaptureEvent()
-        coEvery { eventRepository.observeEventsFromSession(any()) } returns flowOf(personCreationEvent)
+        coEvery { eventRepository.observeEventsFromSession(any()) } returns flowOf(
+            personCreationEvent
+        )
 
         mockUUID()
     }
@@ -119,6 +136,40 @@ class EnrolmentHelperImplTest {
         }
     }
 
+    @Test
+    fun `given the fingerprint capture response, when subject is build, then subject factory is invoked`() {
+        enrolmentHelper.buildSubject(
+            projectId = projectId,
+            userId = userId,
+            moduleId = moduleId,
+            fingerprintResponse = fingerprintResponse,
+            faceResponse = null,
+            timeHelper = timeHelper
+        )
+
+        val captureResult = fingerprintResponse.captureResult.first()
+        val identifier = captureResult.identifier
+        val sample = captureResult.sample!!
+        val sampleFinger = FingerprintSample(
+            fingerIdentifier = identifier.fromDomainToModuleApi(),
+            template = sample.template,
+            templateQualityScore = sample.templateQualityScore,
+            format = sample.format
+        )
+        verify {
+            subjectFactory.buildSubject(
+                subjectId = STATIC_GUID,
+                projectId = projectId,
+                attendantId = userId,
+                moduleId = moduleId,
+                createdAt = any(),
+                updatedAt = null,
+                fingerprintSamples = listOf(sampleFinger),
+                faceSamples = emptyList()
+            )
+        }
+    }
+
     @After
     fun tearDown() {
         unmockkAll()
@@ -127,7 +178,29 @@ class EnrolmentHelperImplTest {
     private fun mockUUID() {
         mockkStatic(UUID::class)
         val guid = mockk<UUID>()
-        every { guid.toString() } returns SampleDefaults.STATIC_GUID
+        every { guid.toString() } returns STATIC_GUID
         every { UUID.randomUUID() } returns guid
+    }
+
+    companion object {
+        private val fingerprintSample = FingerprintCaptureSample(
+            Finger.LEFT_THUMB,
+            templateQualityScore = 10,
+            template = EncodingUtilsImplForTests.base64ToBytes(
+                "sometemplate"
+            ),
+            format = "ISO_19794_2"
+        )
+        private val projectId = "projectId"
+        private val userId = "userId".asTokenizedRaw()
+        private val moduleId = "moduleId".asTokenizedRaw()
+        private val fingerprintResponse = FingerprintCaptureResponse(
+            captureResult = listOf(
+                FingerprintCaptureResult(
+                    Finger.LEFT_THUMB,
+                    fingerprintSample
+                )
+            )
+        )
     }
 }
