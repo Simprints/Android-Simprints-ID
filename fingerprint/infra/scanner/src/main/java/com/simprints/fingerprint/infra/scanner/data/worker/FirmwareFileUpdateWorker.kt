49dde48affa921c1992e9f2e100d52e49976de50
Sergejs Luhmirins <sergejs.luhmirins@gmail.com>
2023-10-17 10:43:11 +0300

    CORE-2951 Add test coverage
    Former-commit-id: 14dc83b152d6ea91e17abfb2a10928ef4bc41445 [formerly f6ea22163566709a6218c5d099e922ca6cb94dd0]
    Former-commit-id: c7fc0fdf1a6dab70e131e593a6f03b942109174c

--- fingerprint/infra/scanner/src/main/java/com/simprints/fingerprint/infra/scanner/data/worker/FirmwareFileUpdateWorker.kt
+++ fingerprint/infra/scanner/src/main/java/com/simprints/fingerprint/infra/scanner/data/worker/FirmwareFileUpdateWorker.kt
@@ -4,12 +4,15 @@ import android.content.Context
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.simprints.core.DispatcherBG
+import com.simprints.core.ExcludedFromGeneratedTestCoverageReports
 import com.simprints.fingerprint.infra.scanner.data.FirmwareRepository
 import com.simprints.infra.logging.Simber
 import com.simprints.infra.network.exceptions.NetworkConnectionException
 import com.simprints.infra.network.exceptions.isCausedFromBadNetworkConnection
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -21,11 +24,12 @@ import kotlinx.coroutines.withContext
 internal class FirmwareFileUpdateWorker @AssistedInject constructor(
     @Assisted context: Context,
     @Assisted params: WorkerParameters,
-    private val firmwareRepository: FirmwareRepository
+    private val firmwareRepository: FirmwareRepository,
+    @DispatcherBG private val dispatcher: CoroutineDispatcher,
 ) : CoroutineWorker(context, params) {
 
 
-    override suspend fun doWork(): Result = withContext(Dispatchers.IO) {
+    override suspend fun doWork(): Result = withContext(dispatcher) {
         try {
             Simber.d("FirmwareFileUpdateWorker started")
             firmwareRepository.updateStoredFirmwareFilesWithLatest()
