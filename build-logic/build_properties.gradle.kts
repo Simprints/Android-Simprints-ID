9f796fc05b08887118196ba02d3da2be88181c74
melad <melad@simprints.com>
2023-11-08 18:47:27 +0200

    [CORE-3008] revert build_properties.gradle.kts

--- build-logic/build_properties.gradle.kts
+++ build-logic/build_properties.gradle.kts
@@ -1,7 +1,33 @@
 extra.apply {
-    set("VERSION_CODE", 1699458)
-    set("VERSION_NAME", "0.0.0-CORE-3008-deploy-dev-to-firebase")
-    set("VERSION_SUFFIX", "alpha")
+
+    /**
+     * Debug Version code. The version code for staging and production builds are taken from the CI.
+     * Read more about our versioning here:
+     * https://simprints.atlassian.net/wiki/spaces/KB/pages/1761378305/Releasing+Simprints+ID
+     */
+    set("VERSION_CODE", 1)
+
+    /**
+     * Current release version. This base version name corresponds with the latest release being worked
+     * on, and the version of the BFSID API being used. The version name is automatically set by the
+     * CI. Read more about our versioning here:
+     * https://simprints.atlassian.net/wiki/spaces/KB/pages/1761378305/Releasing+Simprints+ID
+     *
+     * Dev version >= 2023.4.1 is required for receiving encryption Tokens from BFSID [CORE-2502]
+     */
+    set("VERSION_NAME", "2023.4.1")
+
+    /**
+     * Build type. The version code describes which build type was used for the build.
+     * Read more about our versioning here:
+     * https://simprints.atlassian.net/wiki/spaces/KB/pages/1761378305/Releasing+Simprints+ID
+     */
+    set("VERSION_SUFFIX", "dev")
+
+    /**
+     * Set debuggable, default it true so local staging and release builds can be debugged. NOTE: THE
+     * CD WILL OVERWRITE TO FALSE AS THE PLAYSTORE DOES NOT ACCEPT DEBUGGABLE TRUE APPS
+     */
     set("DEBUGGABLE", true)
     set("DB_ENCRYPTION", true)
 }
