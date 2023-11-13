cc05ac973d1b32b4ec33b94935f3dcdbd29417b9
Sergejs Luhmirins <sergejs@yetanotherapp.dev>
2023-11-13 13:05:56 +0200

    CORE-2845 Delete clientapi module

--- clientapi/src/main/java/com/simprints/clientapi/tools/ClientApiTimeHelperImpl.kt
+++
@@ -1,9 +0,0 @@
-package com.simprints.clientapi.tools
-
-import com.simprints.core.tools.time.TimeHelper
-import javax.inject.Inject
-
-class ClientApiTimeHelperImpl @Inject constructor(private val timeHelper: TimeHelper) :
-    ClientApiTimeHelper {
-    override fun now() = timeHelper.now()
-}
--- clientapi/src/test/java/com/simprints/clientapi/clientrequests/builders/IdentifyBuilderTest.kt
+++
@@ -1,5 +0,0 @@
-package com.simprints.clientapi.clientrequests.builders
-
-import com.simprints.clientapi.requestFactories.IdentifyRequestFactory
-
-class IdentifyBuilderTest : ActionRequestBuilderTest(IdentifyRequestFactory)
