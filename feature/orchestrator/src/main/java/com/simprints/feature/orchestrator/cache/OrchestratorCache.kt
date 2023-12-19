1259b33cb54c4e2f0e25f00648b59ec23a70f9ea
Milen Marinov <milen.danchev.marinov@gmail.com>
2023-12-19 17:53:57 +0200

    [CORE-3147] Remove ActionRequest from OrchestratorCache as it's not needed

--- feature/orchestrator/src/main/java/com/simprints/feature/orchestrator/cache/OrchestratorCache.kt
+++ feature/orchestrator/src/main/java/com/simprints/feature/orchestrator/cache/OrchestratorCache.kt
@@ -4,14 +4,9 @@ import android.os.Bundle
 import androidx.core.content.edit
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.module.SimpleModule
-import com.simprints.core.domain.tokenization.TokenizableString
-import com.simprints.core.domain.tokenization.serialization.TokenizationClassNameDeserializer
-import com.simprints.core.domain.tokenization.serialization.TokenizationClassNameSerializer
 import com.simprints.core.tools.json.JsonHelper
-import com.simprints.core.tools.utils.EncodingUtils
 import com.simprints.feature.orchestrator.steps.SerializableMixin
 import com.simprints.feature.orchestrator.steps.Step
-import com.simprints.infra.orchestration.data.ActionRequest
 import com.simprints.infra.security.SecurityManager
 import java.io.Serializable
 import javax.inject.Inject
@@ -20,33 +15,10 @@ import javax.inject.Singleton
 @Singleton
 internal class OrchestratorCache @Inject constructor(
     securityManager: SecurityManager,
-    private val encodingUtils: EncodingUtils,
     private val jsonHelper: JsonHelper,
-    private val parcelableConverter: ParcelableConverter,
 ) {
 
     private val prefs = securityManager.buildEncryptedSharedPreferences(ORCHESTRATION_CACHE)
-
-    var actionRequest: ActionRequest?
-        set(value) {
-            prefs.edit(commit = true) {
-                if (value == null) {
-                    remove(KEY_REQUEST)
-                } else {
-                    val json = jsonHelper.toJson(value, module = actionRequestModule)
-                    putString(KEY_REQUEST, json)
-                }
-            }
-        }
-        get() = prefs.getString(KEY_REQUEST, null)
-            ?.let {
-                jsonHelper.fromJson(
-                    json = it,
-                    module = actionRequestModule,
-                    type = object : TypeReference<ActionRequest>() {}
-                )
-            }
-
 
     var steps: List<Step>
         set(value) {
@@ -74,14 +46,8 @@ internal class OrchestratorCache @Inject constructor(
 
     companion object {
         private const val ORCHESTRATION_CACHE = "ORCHESTRATOR_CACHE"
-
-        private const val KEY_REQUEST = "actionRequest"
         private const val KEY_STEPS = "steps"
 
-        val actionRequestModule = SimpleModule().apply {
-            addSerializer(TokenizableString::class.java, TokenizationClassNameSerializer())
-            addDeserializer(TokenizableString::class.java, TokenizationClassNameDeserializer())
-        }
         private val stepsModule = SimpleModule().apply {
             addSerializer(Bundle::class.java, BundleSerializer())
             addDeserializer(Bundle::class.java, BundleDeserializer())
--- fingerprint/capture/src/main/java/com/simprints/fingerprint/capture/FingerprintCaptureResult.kt
+++ fingerprint/capture/src/main/java/com/simprints/fingerprint/capture/FingerprintCaptureResult.kt
@@ -1,10 +1,8 @@
 package com.simprints.fingerprint.capture
 
-import android.os.Parcelable
 import androidx.annotation.Keep
 import com.simprints.core.domain.fingerprint.IFingerIdentifier
 import com.simprints.infra.images.model.SecuredImageRef
-import kotlinx.parcelize.Parcelize
 import java.io.Serializable
 
 @Keep
@@ -13,21 +11,18 @@ data class FingerprintCaptureResult(
 ) : Serializable {
 
     @Keep
-    //TODO: This @Parcelize is not needed?
-    @Parcelize
     data class Item(
         val identifier: IFingerIdentifier,
         val sample: Sample?,
-    ) : Parcelable
+    ) : Serializable
 
     @Keep
-    @Parcelize
     data class Sample(
         val fingerIdentifier: IFingerIdentifier,
         val template: ByteArray,
         val templateQualityScore: Int,
         val imageRef: SecuredImageRef?,
         val format: String,
-    ) : Parcelable
+    ) : Serializable
 
 }
