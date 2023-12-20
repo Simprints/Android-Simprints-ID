5a4ff2bae679179b55916680cc79f8c7f1b20cd9
melad <melad@simprints.com>
2023-12-20 09:48:33 +0200

    [CORE-3147] remove parcelize

--- face/capture/src/main/java/com/simprints/face/capture/FaceCaptureResult.kt
+++ face/capture/src/main/java/com/simprints/face/capture/FaceCaptureResult.kt
@@ -1,9 +1,7 @@
 package com.simprints.face.capture
 
-import android.os.Parcelable
 import androidx.annotation.Keep
 import com.simprints.infra.images.model.SecuredImageRef
-import kotlinx.parcelize.Parcelize
 import java.io.Serializable
 
 @Keep
@@ -12,18 +10,16 @@ data class FaceCaptureResult(
 ) : Serializable {
 
     @Keep
-    @Parcelize
     data class Item(
         val index: Int,
         val sample: Sample?,
-    ) : Parcelable
+    ) : Serializable
 
     @Keep
-    @Parcelize
     data class Sample(
         val faceId: String,
         val template: ByteArray,
         val imageRef: SecuredImageRef?,
         val format: String,
-    ) : Parcelable
+    ) : Serializable
 }
