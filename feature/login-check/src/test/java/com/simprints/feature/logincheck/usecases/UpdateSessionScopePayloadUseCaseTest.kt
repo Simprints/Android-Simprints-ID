e9473e1093631ab92abc4da68eec90b4e5c7251e
Sergejs Luhmirins <sergejs@yetanotherapp.dev>
2024-01-24 17:16:14 +0200

    CORE-3185 Update config timestamp in session on login check

--- feature/login-check/src/test/java/com/simprints/feature/logincheck/usecases/UpdateSessionScopePayloadUseCaseTest.kt
+++ feature/login-check/src/test/java/com/simprints/feature/logincheck/usecases/UpdateSessionScopePayloadUseCaseTest.kt
@@ -1,22 +1,23 @@
 package com.simprints.feature.logincheck.usecases
 
 import com.google.common.truth.Truth.assertThat
+import com.simprints.infra.config.store.ConfigRepository
 import com.simprints.infra.enrolment.records.store.EnrolmentRecordRepository
 import com.simprints.infra.events.EventRepository
 import com.simprints.infra.events.event.domain.models.session.DatabaseInfo
 import com.simprints.infra.events.event.domain.models.session.Device
-import com.simprints.infra.events.event.domain.models.session.SessionCaptureEvent
 import com.simprints.infra.events.event.domain.models.session.SessionScope
 import com.simprints.infra.events.event.domain.models.session.SessionScopePayload
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
 
-internal class UpdateDatabaseCountsInCurrentSessionUseCaseTest {
+internal class UpdateSessionScopePayloadUseCaseTest {
 
     @MockK
     lateinit var eventRepository: EventRepository
@@ -24,19 +25,26 @@ internal class UpdateDatabaseCountsInCurrentSessionUseCaseTest {
     @MockK
     lateinit var enrolmentRecordRepository: EnrolmentRecordRepository
 
-    private lateinit var useCase: UpdateDatabaseCountsInCurrentSessionUseCase
+    @MockK
+    lateinit var configRepository: ConfigRepository
+
+    private lateinit var useCase: UpdateSessionScopePayloadUseCase
 
     @Before
     fun setUp() {
         MockKAnnotations.init(this, relaxed = true)
 
-        useCase =
-            UpdateDatabaseCountsInCurrentSessionUseCase(eventRepository, enrolmentRecordRepository)
+        useCase = UpdateSessionScopePayloadUseCase(
+            eventRepository,
+            enrolmentRecordRepository,
+            configRepository
+        )
     }
 
     @Test
-    fun `Updates current event with data from enrolments`() = runTest {
+    fun `Updates current scope with data from enrolments`() = runTest {
         coEvery { enrolmentRecordRepository.count() } returns 42
+        coEvery { configRepository.getProjectConfiguration().updatedAt } returns "configUpdatedAt"
 
         coEvery { eventRepository.getCurrentSessionScope() } returns createBlankSessionScope()
 
@@ -45,6 +53,7 @@ internal class UpdateDatabaseCountsInCurrentSessionUseCaseTest {
         coVerify {
             eventRepository.saveSessionScope(withArg {
                 assertThat(it.payload.databaseInfo.recordCount).isEqualTo(42)
+                assertThat(it.payload.projectConfigurationUpdatedAt).isEqualTo("configUpdatedAt")
             })
         }
     }
