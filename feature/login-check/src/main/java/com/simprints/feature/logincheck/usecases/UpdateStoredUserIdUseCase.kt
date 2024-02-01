a87b225e0ba536f458ec47defd83e6ec50d740ec
Sergejs Luhmirins <sergejs@yetanotherapp.dev>
2024-02-01 17:10:32 +0200

    MS-201 Store user ID on login or login check if required

---
+++ feature/login-check/src/main/java/com/simprints/feature/logincheck/usecases/UpdateStoredUserIdUseCase.kt
@@ -0,0 +1,18 @@
+package com.simprints.feature.logincheck.usecases
+
+import com.simprints.core.domain.tokenization.TokenizableString
+import com.simprints.infra.authstore.AuthStore
+import javax.inject.Inject
+
+class UpdateStoredUserIdUseCase @Inject constructor(
+    private val authStore: AuthStore,
+) {
+
+    operator fun invoke(userId: TokenizableString) {
+        // This is a hack to make sure that there is a stored user ID if user logged in before 2024.1.0.
+        // It could be removed after all we drop support for older versions.
+        if (authStore.signedInUserId == null) {
+            authStore.signedInUserId = userId
+        }
+    }
+}
