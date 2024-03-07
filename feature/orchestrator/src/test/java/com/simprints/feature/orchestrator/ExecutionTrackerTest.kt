044eeeea869c87ec03edc0eae421e164fede76f5
alexandr <alexandr@simprints.com>
2024-03-07 16:07:07 +0200

    [MS-255] ExecutionTracker now saves the 'currentLifecycleOwnerId' as a reference to the current main executing activity. All subsequent OrchestratorActivity instances will be finished, if  another one is executing.

--- feature/orchestrator/src/test/java/com/simprints/feature/orchestrator/ExecutionTrackerTest.kt
+++ feature/orchestrator/src/test/java/com/simprints/feature/orchestrator/ExecutionTrackerTest.kt
@@ -1,7 +1,11 @@
 package com.simprints.feature.orchestrator
 
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
 import com.google.common.truth.Truth.assertThat
+import com.simprints.core.tools.time.TimeHelper
+import com.simprints.core.tools.time.Timestamp
+import io.mockk.every
 import io.mockk.mockk
 import org.junit.Before
 import org.junit.Test
@@ -9,31 +13,106 @@ import org.junit.Test
 class ExecutionTrackerTest {
 
     private lateinit var executionTracker: ExecutionTracker
+    private lateinit var timeHelper: TimeHelper
+    private val executionTimeLimitSec: Int = 1
 
     @Before
     fun setUp() {
-        executionTracker = ExecutionTracker()
+        timeHelper = mockk()
+        every { timeHelper.now() } returns Timestamp(0L)
+        every { timeHelper.msBetweenNowAndTime(any()) } returns 0L
+
+        executionTracker = ExecutionTracker(
+            timeHelper = timeHelper,
+            executionTimeLimitSec = executionTimeLimitSec
+        )
     }
 
     @Test
-    fun `resets execution flag if single activity created and destroyed`() {
-        executionTracker.onStateChanged(mockk(), Lifecycle.Event.ON_CREATE)
-        executionTracker.isExecuting.set(true)
+    fun `when owner is created, then it becomes the main executor`() {
+        val ownerId = 123
+        val owner = mockk<LifecycleOwner>()
+        every { owner.hashCode() } returns ownerId
 
-        executionTracker.onStateChanged(mockk(), Lifecycle.Event.ON_DESTROY)
-        assertThat(executionTracker.isExecuting.get()).isFalse()
+        executionTracker.onStateChanged(owner, Lifecycle.Event.ON_CREATE)
+
+        assertThat(executionTracker.isMain(owner)).isTrue()
     }
 
     @Test
-    fun `resets execution flag if multiple activity created and destroyed`() {
-        executionTracker.onStateChanged(mockk(), Lifecycle.Event.ON_CREATE)
-        executionTracker.onStateChanged(mockk(), Lifecycle.Event.ON_CREATE)
-        executionTracker.isExecuting.set(true)
+    fun `when owner is destroyed, then it is no longer remains the main executor`() {
+        val ownerId = 123
+        val owner = mockk<LifecycleOwner>()
+        every { owner.hashCode() } returns ownerId
 
-        executionTracker.onStateChanged(mockk(), Lifecycle.Event.ON_DESTROY)
-        assertThat(executionTracker.isExecuting.get()).isTrue()
+        executionTracker.onStateChanged(owner, Lifecycle.Event.ON_CREATE)
+        executionTracker.onStateChanged(owner, Lifecycle.Event.ON_DESTROY)
 
-        executionTracker.onStateChanged(mockk(), Lifecycle.Event.ON_DESTROY)
-        assertThat(executionTracker.isExecuting.get()).isFalse()
+        assertThat(executionTracker.isMain(owner)).isFalse()
+    }
+
+    @Test
+    fun `given second owner, when first owner is destroyed, then the second owner becomes the main executor`() {
+        val ownerId1 = 123
+        val ownerId2 = 456
+        val firstOwner = mockk<LifecycleOwner>()
+        val secondOwner = mockk<LifecycleOwner>()
+        every { firstOwner.hashCode() } returns ownerId1
+        every { secondOwner.hashCode() } returns ownerId2
+
+        executionTracker.onStateChanged(firstOwner, Lifecycle.Event.ON_CREATE)
+        executionTracker.onStateChanged(firstOwner, Lifecycle.Event.ON_DESTROY)
+        executionTracker.onStateChanged(secondOwner, Lifecycle.Event.ON_CREATE)
+
+        assertThat(executionTracker.isMain(secondOwner)).isTrue()
+    }
+
+    @Test
+    fun `given second owner, when first owner is not yet destroyed, then the first owner remains the main executor`() {
+        val ownerId1 = 123
+        val ownerId2 = 456
+        val firstOwner = mockk<LifecycleOwner>()
+        val secondOwner = mockk<LifecycleOwner>()
+        every { firstOwner.hashCode() } returns ownerId1
+        every { secondOwner.hashCode() } returns ownerId2
+
+        executionTracker.onStateChanged(firstOwner, Lifecycle.Event.ON_CREATE)
+        executionTracker.onStateChanged(secondOwner, Lifecycle.Event.ON_CREATE)
+
+        assertThat(executionTracker.isMain(firstOwner)).isTrue()
+    }
+
+    @Test
+    fun `given second owner and not enough time passed to unlock the main executor, when first owner is not yet destroyed, then the first owner remains the main executor`() {
+        val ownerId1 = 123
+        val ownerId2 = 456
+        val firstOwner = mockk<LifecycleOwner>()
+        val secondOwner = mockk<LifecycleOwner>()
+
+        every { firstOwner.hashCode() } returns ownerId1
+        every { secondOwner.hashCode() } returns ownerId2
+        every { timeHelper.msBetweenNowAndTime(any()) } returns 1000L * (executionTimeLimitSec)
+
+        executionTracker.onStateChanged(firstOwner, Lifecycle.Event.ON_CREATE)
+        executionTracker.onStateChanged(secondOwner, Lifecycle.Event.ON_CREATE)
+
+        assertThat(executionTracker.isMain(firstOwner)).isTrue()
+    }
+
+    @Test
+    fun `given second owner and enough time passed to unlock the main executor, when first owner is not yet destroyed, then the second owner becomes the main executor`() {
+        val ownerId1 = 123
+        val ownerId2 = 456
+        val firstOwner = mockk<LifecycleOwner>()
+        val secondOwner = mockk<LifecycleOwner>()
+
+        every { firstOwner.hashCode() } returns ownerId1
+        every { secondOwner.hashCode() } returns ownerId2
+        every { timeHelper.msBetweenNowAndTime(any()) } returns 1000L * (executionTimeLimitSec + 1)
+
+        executionTracker.onStateChanged(firstOwner, Lifecycle.Event.ON_CREATE)
+        executionTracker.onStateChanged(secondOwner, Lifecycle.Event.ON_CREATE)
+
+        assertThat(executionTracker.isMain(secondOwner)).isTrue()
     }
 }
