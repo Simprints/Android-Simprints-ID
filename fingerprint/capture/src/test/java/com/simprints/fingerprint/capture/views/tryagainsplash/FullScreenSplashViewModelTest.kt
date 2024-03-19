f242e68dda1d841b156dea4c600b7a0569c872c2
Sergejs Luhmirins <sergejs@yetanotherapp.dev>
2024-03-19 10:26:59 +0200

    MS-277 Ensure that missing finger splash is closed even if it was paused

---
+++ fingerprint/capture/src/test/java/com/simprints/fingerprint/capture/views/tryagainsplash/FullScreenSplashViewModelTest.kt
@@ -0,0 +1,36 @@
+package com.simprints.fingerprint.capture.views.tryagainsplash
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import com.jraska.livedata.test
+import com.simprints.testtools.common.coroutines.TestCoroutineRule
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class FullScreenSplashViewModelTest {
+
+    @get:Rule
+    val testCoroutineRule = TestCoroutineRule()
+
+    private lateinit var viewModel: FullScreenSplashViewModel
+
+    @Before
+    fun setUp() {
+
+        viewModel = FullScreenSplashViewModel()
+    }
+
+    @Test
+    fun `dismiss value updated after delay`() = runTest {
+        val observer = viewModel.dismiss.test()
+        assertThat(observer.value()).isFalse()
+
+        advanceTimeBy(3000L)
+        assertThat(observer.value()).isTrue()
+    }
+}
