9edf2cb9b48dca58082d0ae09c04a5e0d6147977
Alex <oleksiy.vt@gmail.com>
2024-04-24 14:35:14 +0100

    MS-368 Battery optimization check platform code excluded from test coverage

--- infra/core/src/main/java/com/simprints/core/tools/utils/BatteryOptimizationUtils.kt
+++ infra/core/src/main/java/com/simprints/core/tools/utils/BatteryOptimizationUtils.kt
@@ -2,7 +2,9 @@ package com.simprints.core.tools.utils
 
 import android.content.Context
 import android.os.PowerManager
+import com.simprints.core.ExcludedFromGeneratedTestCoverageReports
 
+@ExcludedFromGeneratedTestCoverageReports("Platform glue code")
 object BatteryOptimizationUtils {
 
     fun isFollowingBatteryOptimizations(context: Context): Boolean =
