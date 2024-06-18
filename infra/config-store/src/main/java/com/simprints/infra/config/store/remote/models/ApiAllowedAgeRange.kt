f5472dc43435dc39ec64464155a2a36c01492e90
Sergejs Luhmirins <sergejs@yetanotherapp.dev>
2024-06-18 18:05:47 +0300

    MS-515 Add new fields to fingerprint configuration

--- infra/config-store/src/main/java/com/simprints/infra/config/store/remote/models/ApiAllowedAgeRange.kt
+++ infra/config-store/src/main/java/com/simprints/infra/config/store/remote/models/ApiAllowedAgeRange.kt
@@ -1,12 +1,14 @@
 package com.simprints.infra.config.store.remote.models
 
+import androidx.annotation.Keep
 import com.simprints.infra.config.store.models.AgeGroup
 
+@Keep
 data class ApiAllowedAgeRange(
     val startInclusive: Int?,
     val endExclusive: Int?,
 ) {
-    //TODO(milen): shall we stick to AgeRange name?
+
     fun toDomain() =
         // When allowedAgeRange is disabled the API returns an empty object
         // which is then parsed as {null, null}
