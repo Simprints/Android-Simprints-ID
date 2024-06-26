0c60d883c0fa64d2d890f4754c62b86e0f99b53c
alexandr <alexandr@simprints.com>
2024-06-26 16:50:44 +0300

    [MS-537] The 'expiration' field in the 'License' class is now nullable to reflect the BFSID API

--- infra/license/src/test/java/com/simprints/infra/license/LicenseUtilsTest.kt
+++ infra/license/src/test/java/com/simprints/infra/license/LicenseUtilsTest.kt
@@ -12,19 +12,22 @@ class LicenseUtilsTest {
         val nullLicense: License? = null
         assertThat(nullLicense.determineLicenseStatus()).isEqualTo(LicenseStatus.MISSING)
 
-        val expiredLicense = License("2022-01-01T00:00:00Z", "data")
+        val expiredLicense = License(expiration = "2022-01-01T00:00:00Z", data = "data")
         assertThat(expiredLicense.determineLicenseStatus()).isEqualTo(LicenseStatus.EXPIRED)
 
-        val invalidLicense = License("2223-01-01T00:00:00Z", "")
+        val invalidLicense = License(expiration = "2223-01-01T00:00:00Z", data = "")
         assertThat(invalidLicense.determineLicenseStatus()).isEqualTo(LicenseStatus.INVALID)
 
-        val validLicense = License("2123-01-01T00:00:00Z", "data")
+        val validLicense = License(expiration = "2123-01-01T00:00:00Z", data = "data")
         assertThat(validLicense.determineLicenseStatus()).isEqualTo(LicenseStatus.VALID)
 
-        val validLicenseWithEmptyExpiration = License("", "data")
+        val validLicenseWithEmptyExpiration = License(expiration = "", data = "data")
         assertThat(validLicenseWithEmptyExpiration.determineLicenseStatus()).isEqualTo(LicenseStatus.VALID)
 
-        val validLicenseWithBadExpirationDate = License("bad date", "data")
+        val validLicenseWithNullExpiration = License(expiration = null, data = "data")
+        assertThat(validLicenseWithNullExpiration.determineLicenseStatus()).isEqualTo(LicenseStatus.VALID)
+
+        val validLicenseWithBadExpirationDate = License(expiration = "bad date", data = "data")
         assertThat(validLicenseWithBadExpirationDate.determineLicenseStatus()).isEqualTo(
             LicenseStatus.VALID
         )
