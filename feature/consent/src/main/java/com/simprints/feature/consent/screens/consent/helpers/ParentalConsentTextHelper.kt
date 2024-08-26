81597a1e8854257adcbfaaa2cf1901967baab4a0
Sergejs Luhmirins <sergejs@yetanotherapp.dev>
2024-08-26 13:33:07 +0300

    MS-197 Assemble consent text using activity context instead of application

--- feature/consent/src/main/java/com/simprints/feature/consent/screens/consent/helpers/ParentalConsentTextHelper.kt
+++ feature/consent/src/main/java/com/simprints/feature/consent/screens/consent/helpers/ParentalConsentTextHelper.kt
@@ -5,39 +5,37 @@ import com.simprints.feature.consent.ConsentType
 import com.simprints.infra.config.store.models.ConsentConfiguration
 import com.simprints.infra.config.store.models.GeneralConfiguration.Modality
 import com.simprints.infra.resources.R
-import dagger.hilt.android.qualifiers.ApplicationContext
-import javax.inject.Inject
 
-internal class ParentalConsentTextHelper @Inject constructor(
-    @ApplicationContext val context: Context
+internal data class ParentalConsentTextHelper(
+    private val config: ConsentConfiguration,
+    private val modalities: List<Modality>,
+    private val consentType: ConsentType,
 ) {
     // TODO All the `getString(id).format(arg,arg)` calls should be `getString(id,arg,arg)` one strings are fixed
 
     //First argument in consent text should always be program name, second is modality specific access/use case text
-    fun assembleText(
-        config: ConsentConfiguration,
-        modalities: List<Modality>,
-        consentType: ConsentType,
-    ): String = StringBuilder().apply {
-        val modalityUseCase = getModalitySpecificUseCaseText(modalities)
-        val modalityAccess = getModalitySpecificAccessText(modalities)
+    fun assembleText(context: Context): String = StringBuilder().apply {
+        val modalityUseCase = getModalitySpecificUseCaseText(context, modalities)
+        val modalityAccess = getModalitySpecificAccessText(context, modalities)
 
-        filterAppRequestForParentalConsent(consentType, config, modalityUseCase)
-        extractDataSharingOptions(config, modalityUseCase, modalityAccess)
+        filterAppRequestForParentalConsent(context, consentType, config, modalityUseCase)
+        extractDataSharingOptions(context, config, modalityUseCase, modalityAccess)
     }.toString()
 
     private fun StringBuilder.filterAppRequestForParentalConsent(
+        context: Context,
         consentType: ConsentType,
         config: ConsentConfiguration,
         modalityUseCase: String,
     ) {
         when (consentType) {
-            ConsentType.ENROL -> appendTextForParentalEnrol(config.parentalPrompt, config.programName, modalityUseCase)
-            ConsentType.IDENTIFY, ConsentType.VERIFY -> appendTextForIdentifyOrVerify(config.programName, modalityUseCase)
+            ConsentType.ENROL -> appendTextForParentalEnrol(context, config.parentalPrompt, config.programName, modalityUseCase)
+            ConsentType.IDENTIFY, ConsentType.VERIFY -> appendTextForIdentifyOrVerify(context, config.programName, modalityUseCase)
         }
     }
 
     private fun StringBuilder.appendTextForParentalEnrol(
+        context: Context,
         config: ConsentConfiguration.ConsentPromptConfiguration?,
         programName: String,
         modalityUseCase: String,
@@ -55,11 +53,16 @@ internal class ParentalConsentTextHelper @Inject constructor(
         else -> this
     }
 
-    private fun StringBuilder.appendTextForIdentifyOrVerify(programName: String, modalityUseCase: String) = appendSentence(
+    private fun StringBuilder.appendTextForIdentifyOrVerify(
+        context: Context,
+        programName: String,
+        modalityUseCase: String,
+    ) = appendSentence(
         context.getString(R.string.consent_parental_id_verify).format(programName, modalityUseCase)
     )
 
     private fun StringBuilder.extractDataSharingOptions(
+        context: Context,
         config: ConsentConfiguration,
         modalityUseCase: String,
         modalityAccess: String
@@ -86,33 +89,46 @@ internal class ParentalConsentTextHelper @Inject constructor(
         }
     }
 
-    private fun getModalitySpecificUseCaseText(modalities: List<Modality>) = if (modalities.size == 1) {
-        getSingleModalitySpecificUseCaseText(modalities)
+    private fun getModalitySpecificUseCaseText(
+        context: Context,
+        modalities: List<Modality>,
+    ) = if (modalities.size == 1) {
+        getSingleModalitySpecificUseCaseText(context, modalities)
     } else {
-        getConcatenatedModalitiesUseCaseText()
+        getConcatenatedModalitiesUseCaseText(context)
     }
 
-    private fun getConcatenatedModalitiesUseCaseText() = "%s %s %s".format(
+    private fun getConcatenatedModalitiesUseCaseText(context: Context) = "%s %s %s".format(
         context.getString(R.string.consent_biometrics_parental_fingerprint),
         context.getString(R.string.consent_biometric_concat_modalities),
         context.getString(R.string.consent_biometrics_parental_face)
     )
 
-    private fun getSingleModalitySpecificUseCaseText(modalities: List<Modality>) = when (modalities.first()) {
+    private fun getSingleModalitySpecificUseCaseText(
+        context: Context,
+        modalities: List<Modality>,
+    ) = when (modalities.first()) {
         Modality.FACE -> context.getString(R.string.consent_biometrics_parental_face)
         Modality.FINGERPRINT -> context.getString(R.string.consent_biometrics_parental_fingerprint)
         else -> ""
     }
 
-    private fun getModalitySpecificAccessText(modalities: List<Modality>) = if (modalities.size == 1) {
-        getSingleModalityAccessText(modalities)
+    private fun getModalitySpecificAccessText(
+        context: Context,
+        modalities: List<Modality>,
+    ) = if (modalities.size == 1) {
+        getSingleModalityAccessText(context, modalities)
     } else {
-        getConcatenatedModalitiesAccessText()
+        getConcatenatedModalitiesAccessText(context)
     }
 
-    private fun getConcatenatedModalitiesAccessText() = context.getString(R.string.consent_biometrics_access_fingerprint_face)
+    private fun getConcatenatedModalitiesAccessText(context: Context) =
+        context.getString(R.string.consent_biometrics_access_fingerprint_face)
 
-    private fun getSingleModalityAccessText(modalities: List<Modality>) = when (modalities.first()) {
+    private fun getSingleModalityAccessText(
+        context: Context,
+        modalities: List<Modality>,
+    ) = when (modalities.first()) {
         Modality.FACE -> context.getString(R.string.consent_biometrics_access_face)
         Modality.FINGERPRINT -> context.getString(R.string.consent_biometrics_access_fingerprint)
         else -> ""
