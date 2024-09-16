5c6af17e95852836c38e9211b0b1d48986d46ffb
alex <alexandr@simprints.com>
2024-09-16 13:11:39 +0300

    [MS-491] Adding test cases for the IsNoFingerDetectedLimitReachedUseCaseTest

---
+++ fingerprint/capture/src/test/java/com/simprints/fingerprint/capture/usecase/IsNoFingerDetectedLimitReachedUseCaseTest.kt
@@ -0,0 +1,92 @@
+package com.simprints.fingerprint.capture.usecase
+
+import com.google.common.truth.Truth.assertThat
+import com.simprints.fingerprint.capture.state.CaptureState
+import com.simprints.infra.config.store.models.FingerprintConfiguration
+import com.simprints.infra.config.store.models.FingerprintConfiguration.FingerprintSdkConfiguration
+import com.simprints.infra.config.store.models.MaxCaptureAttempts
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Test
+
+
+internal class IsNoFingerDetectedLimitReachedUseCaseTest {
+    private val isNoFingerDetectedLimitReachedUseCase = IsNoFingerDetectedLimitReachedUseCase()
+
+    @Test
+    fun `when capture state is not ScanProcess, then returns false`() {
+        val fingerStateNotCollected = mockk<CaptureState.NotCollected>()
+        val fingerStateSkipped = mockk<CaptureState.Skipped>()
+        val configuration = mockk<FingerprintConfiguration>()
+
+        listOf(fingerStateNotCollected, fingerStateSkipped).forEach { fingerState ->
+            assertThat(
+                isNoFingerDetectedLimitReachedUseCase(
+                    fingerState = fingerState,
+                    configuration = configuration
+                )
+            ).isFalse()
+        }
+    }
+
+    @Test
+    fun `when max capture attempts is null, then inclusive MAXIMUM_LIMIT_OF_NO_FINGER_DETECTED_SCANS is used`() {
+        val fingerState = mockk<CaptureState.ScanProcess> {
+            every { numberOfNoFingerDetectedScans } returns IsNoFingerDetectedLimitReachedUseCase.MAXIMUM_LIMIT_OF_NO_FINGER_DETECTED_SCANS
+        }
+        val configuration = mockk<FingerprintConfiguration> {
+            every { secugenSimMatcher } returns null
+        }
+        assertThat(
+            isNoFingerDetectedLimitReachedUseCase(
+                fingerState = fingerState,
+                configuration = configuration
+            )
+        ).isTrue()
+    }
+
+    @Test
+    fun `when number of no finger detected scans is greater than no finger detected threshold, then true is returned`() {
+        val noFingerDetectedThreshold = 3
+        val noFingerDetectedScans = noFingerDetectedThreshold
+        val fingerState = mockk<CaptureState.ScanProcess> {
+            every { numberOfNoFingerDetectedScans } returns noFingerDetectedScans
+        }
+        val configuration = mockk<FingerprintConfiguration> {
+            every { secugenSimMatcher } returns mockk<FingerprintSdkConfiguration> {
+                every { maxCaptureAttempts } returns mockk<MaxCaptureAttempts> {
+                    every { noFingerDetected } returns noFingerDetectedThreshold
+                }
+            }
+        }
+        assertThat(
+            isNoFingerDetectedLimitReachedUseCase(
+                fingerState = fingerState,
+                configuration = configuration
+            )
+        ).isTrue()
+    }
+
+    @Test
+    fun `when threshold number of no finger detected scans is lower than 2, then MAXIMUM_LIMIT_OF_NO_FINGER_DETECTED_SCANS is used`() {
+        val noFingerDetectedThreshold =
+            IsNoFingerDetectedLimitReachedUseCase.MAXIMUM_LIMIT_OF_NO_FINGER_DETECTED_SCANS
+        val noFingerDetectedScans = noFingerDetectedThreshold - 1
+        val fingerState = mockk<CaptureState.ScanProcess> {
+            every { numberOfNoFingerDetectedScans } returns noFingerDetectedScans
+        }
+        val configuration = mockk<FingerprintConfiguration> {
+            every { secugenSimMatcher } returns mockk<FingerprintSdkConfiguration> {
+                every { maxCaptureAttempts } returns mockk<MaxCaptureAttempts> {
+                    every { noFingerDetected } returns 1
+                }
+            }
+        }
+        assertThat(
+            isNoFingerDetectedLimitReachedUseCase(
+                fingerState = fingerState,
+                configuration = configuration
+            )
+        ).isFalse()
+    }
+}
