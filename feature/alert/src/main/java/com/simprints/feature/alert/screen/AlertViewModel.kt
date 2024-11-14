2055d3bfaec11fc8374aff7c90b3b75728868959
Sergejs Luhmirins <sergejs@yetanotherapp.dev>
2024-11-14 17:25:18 +0200

    MS-801 Add data export button to alert screen

--- feature/alert/src/main/java/com/simprints/feature/alert/screen/AlertViewModel.kt
+++ feature/alert/src/main/java/com/simprints/feature/alert/screen/AlertViewModel.kt
@@ -1,25 +1,49 @@
 package com.simprints.feature.alert.screen
 
 import androidx.lifecycle.ViewModel
+import com.simprints.core.DeviceID
 import com.simprints.core.ExternalScope
 import com.simprints.core.tools.time.TimeHelper
+import com.simprints.infra.authstore.AuthStore
 import com.simprints.infra.events.SessionEventRepository
 import com.simprints.infra.events.event.domain.models.AlertScreenEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 @HiltViewModel
 internal class AlertViewModel @Inject constructor(
+    @DeviceID private val deviceId: String,
     private val timeHelper: TimeHelper,
     private val eventRepository: SessionEventRepository,
-    @ExternalScope private val ioScope: CoroutineScope,
+    private val authStore: AuthStore,
+    @ExternalScope private val externalScope: CoroutineScope,
 ) : ViewModel() {
 
+    private lateinit var cachedAlertEvent: AlertScreenEvent
+
     fun saveAlertEvent(type: AlertScreenEvent.AlertScreenPayload.AlertScreenEventType) {
-        ioScope.launch {
-            eventRepository.addOrUpdateEvent(AlertScreenEvent(timeHelper.now(), type))
+        externalScope.launch {
+            val event = AlertScreenEvent(timeHelper.now(), type)
+            eventRepository.addOrUpdateEvent(event)
+            // Preserving the alert event to be able to export its data if requested by user
+            cachedAlertEvent = event
         }
+    }
+
+    fun collectExportData(): String = runBlocking {
+        val sessionId = eventRepository.getCurrentSessionScope().id
+
+        """
+            Event ID:   ${cachedAlertEvent.id}
+            Timestamp:  ${cachedAlertEvent.payload.createdAt.ms}
+            Project ID: ${authStore.signedInProjectId}
+            User ID:    ${authStore.signedInUserId?.value}
+            Device ID:  $deviceId
+            Session ID: $sessionId
+            Alert type: ${cachedAlertEvent.payload.alertType}
+        """.trimIndent()
     }
 }
--- feature/alert/src/test/java/com/simprints/feature/alert/screen/AlertViewModelTest.kt
+++ feature/alert/src/test/java/com/simprints/feature/alert/screen/AlertViewModelTest.kt
@@ -2,17 +2,22 @@ package com.simprints.feature.alert.screen
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.google.common.truth.Truth.assertThat
+import com.simprints.core.domain.tokenization.TokenizableString
 import com.simprints.core.tools.time.TimeHelper
 import com.simprints.core.tools.time.Timestamp
+import com.simprints.infra.authstore.AuthStore
 import com.simprints.infra.events.SessionEventRepository
 import com.simprints.infra.events.event.domain.models.AlertScreenEvent
 import com.simprints.infra.events.event.domain.models.AlertScreenEvent.AlertScreenPayload.AlertScreenEventType
 import com.simprints.infra.events.event.domain.models.EventType
 import com.simprints.testtools.common.coroutines.TestCoroutineRule
 import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coJustRun
 import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
+import io.mockk.justRun
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
@@ -20,6 +25,7 @@ import org.junit.Rule
 import org.junit.Test
 
 internal class AlertViewModelTest {
+
     @get:Rule
     val rule = InstantTaskExecutorRule()
 
@@ -32,6 +38,9 @@ internal class AlertViewModelTest {
     @MockK
     lateinit var eventRepository: SessionEventRepository
 
+    @MockK
+    lateinit var authStore: AuthStore
+
     lateinit var alertViewModel: AlertViewModel
 
     @Before
@@ -39,14 +48,16 @@ internal class AlertViewModelTest {
         MockKAnnotations.init(this, relaxed = true)
 
         alertViewModel = AlertViewModel(
+            DEVICE_ID,
             timeHelper,
             eventRepository,
+            authStore,
             CoroutineScope(testCoroutineRule.testCoroutineDispatcher),
         )
     }
 
     @Test
-    fun saveAlertEvent() = runTest {
+    fun `alert event reported correctly`() = runTest {
         every { timeHelper.now() } returns Timestamp(42)
 
         alertViewModel.saveAlertEvent(AlertScreenEventType.DISCONNECTED)
@@ -60,5 +71,32 @@ internal class AlertViewModelTest {
                 assertThat(payload.alertType).isEqualTo(AlertScreenEventType.DISCONNECTED)
             })
         }
+    }
+
+    @Test
+    fun `exported data contains all the correct data`() = runTest {
+        every { timeHelper.now() } returns Timestamp(42)
+        every { authStore.signedInProjectId } returns PROJECT_ID
+        every { authStore.signedInUserId } returns TokenizableString.Raw(USER_ID)
+        coJustRun { eventRepository.addOrUpdateEvent(any()) }
+        coEvery { eventRepository.getCurrentSessionScope().id } returns SCOPE_ID
+
+        alertViewModel.saveAlertEvent(AlertScreenEventType.DISCONNECTED)
+        val exportData = alertViewModel.collectExportData()
+
+        assertThat(exportData).contains("42")
+        assertThat(exportData).contains(DEVICE_ID)
+        assertThat(exportData).contains(PROJECT_ID)
+        assertThat(exportData).contains(USER_ID)
+        assertThat(exportData).contains(SCOPE_ID)
+        assertThat(exportData).contains(AlertScreenEventType.DISCONNECTED.name)
+    }
+
+    companion object {
+
+        private const val DEVICE_ID = "device-id"
+        private const val PROJECT_ID = "project-id"
+        private const val USER_ID = "user-id"
+        private const val SCOPE_ID = "scope-id"
     }
 }
