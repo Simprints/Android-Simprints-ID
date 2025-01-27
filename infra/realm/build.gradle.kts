56015ed4d5d1d5b7fbdca626218867e471d7fee9
Melad Raouf <melad.raouf@gmail.com>
2025-01-27 09:20:27 +0200

    Remove resolutionStrategy.force from realm module
    This is no longer needed and can be safely removed.

--- infra/realm/build.gradle.kts
+++ infra/realm/build.gradle.kts
@@ -10,8 +10,3 @@ android {
 dependencies {
     implementation(project(":infra:auth-store"))
 }
-configurations.all {
-    // Realm uses old version of annotations library which conflicts with the one used by the app
-    // More details here:https://github.com/realm/realm-java/issues/6153#issuecomment-991378320
-    resolutionStrategy.force("org.jetbrains:annotations:26.0.2")
-}
