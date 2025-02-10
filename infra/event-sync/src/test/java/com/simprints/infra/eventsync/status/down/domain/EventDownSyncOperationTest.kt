ba16aaa8c2e9aee9354ebe024ccaeaa5d5157607
Sergejs Luhmirins <sergejs@yetanotherapp.dev>
2025-02-10 16:33:48 +0200

    MS-871 Increase sync state database version to force destructive migration

--- infra/event-sync/src/test/java/com/simprints/infra/eventsync/status/down/domain/EventDownSyncOperationTest.kt
+++ infra/event-sync/src/test/java/com/simprints/infra/eventsync/status/down/domain/EventDownSyncOperationTest.kt
@@ -1,7 +1,6 @@
 package com.simprints.infra.eventsync.status.down.domain
 
 import com.google.common.truth.Truth.assertThat
-import com.simprints.infra.events.sampledata.SampleDefaults.DEFAULT_MODES
 import com.simprints.infra.events.sampledata.SampleDefaults.DEFAULT_MODULE_ID
 import com.simprints.infra.events.sampledata.SampleDefaults.DEFAULT_MODULE_ID_2
 import com.simprints.infra.events.sampledata.SampleDefaults.DEFAULT_PROJECT_ID
@@ -9,7 +8,6 @@ import com.simprints.infra.events.sampledata.SampleDefaults.DEFAULT_USER_ID
 import com.simprints.infra.eventsync.SampleSyncScopes.modulesDownSyncScope
 import com.simprints.infra.eventsync.SampleSyncScopes.projectDownSyncScope
 import com.simprints.infra.eventsync.SampleSyncScopes.userDownSyncScope
-import com.simprints.infra.eventsync.status.down.domain.EventDownSyncOperation.Companion.oldTypes
 import org.junit.Test
 import java.util.UUID
 
@@ -18,9 +16,7 @@ class EventDownSyncOperationTest {
     fun eventDownSyncOperationForProjectScope_hasAnUniqueKey() {
         val op = projectDownSyncScope.operations.first()
         assertThat(op.getUniqueKey()).isEqualTo(
-            uuidFrom(
-                "${DEFAULT_PROJECT_ID}${DEFAULT_MODES.joinToString { it.name }}$oldTypes",
-            ),
+            uuidFrom(DEFAULT_PROJECT_ID),
         )
     }
 
@@ -28,9 +24,7 @@ class EventDownSyncOperationTest {
     fun eventDownSyncOperationForUserScope_hasAnUniqueKey() {
         val op = userDownSyncScope.operations.first()
         assertThat(op.getUniqueKey()).isEqualTo(
-            uuidFrom(
-                "$DEFAULT_PROJECT_ID${DEFAULT_USER_ID.value}${DEFAULT_MODES.joinToString { it.name }}$oldTypes",
-            ),
+            uuidFrom("$DEFAULT_PROJECT_ID${DEFAULT_USER_ID.value}"),
         )
     }
 
@@ -40,15 +34,11 @@ class EventDownSyncOperationTest {
         val op1 = modulesDownSyncScope.operations[1]
 
         assertThat(op.getUniqueKey()).isEqualTo(
-            uuidFrom(
-                "$DEFAULT_PROJECT_ID${DEFAULT_MODULE_ID.value}${DEFAULT_MODES.joinToString { it.name }}$oldTypes",
-            ),
+            uuidFrom("$DEFAULT_PROJECT_ID${DEFAULT_MODULE_ID.value}"),
         )
 
         assertThat(op1.getUniqueKey()).isEqualTo(
-            uuidFrom(
-                "$DEFAULT_PROJECT_ID${DEFAULT_MODULE_ID_2.value}${DEFAULT_MODES.joinToString { it.name }}$oldTypes",
-            ),
+            uuidFrom("$DEFAULT_PROJECT_ID${DEFAULT_MODULE_ID_2.value}"),
         )
     }
 
