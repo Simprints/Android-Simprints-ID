9d5039145fab3015bfedffcd2bddc1edc061892b
alex <alexandr@simprints.com>
2025-04-02 12:39:08 +0300

    [MS-964] Search & Verify: adding the QR code scanner stepˆ

--- feature/external-credential/build.gradle.kts
+++ feature/external-credential/build.gradle.kts
@@ -10,4 +10,11 @@ android {
 dependencies {
     implementation(project(":infra:external-credential-store"))
     implementation(project(":feature:exit-form"))
+
+    // QR scanner related
+    implementation(libs.androidX.cameraX.core)
+    implementation(libs.androidX.cameraX.lifecycle)
+    implementation(libs.androidX.cameraX.view)
+    implementation(libs.jackson.core)
+    implementation(libs.playServices.barcode)
 }
--- feature/external-credential/src/main/java/com/simprints/feature/externalcredential/screens/qr/ExternalCredentialQrScannerFragment.kt
+++ feature/external-credential/src/main/java/com/simprints/feature/externalcredential/screens/qr/ExternalCredentialQrScannerFragment.kt
@@ -1,29 +1,78 @@
 package com.simprints.feature.externalcredential.screens.qr
 
+import android.Manifest.permission.CAMERA
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.analytics.FirebaseAnalytics.Event.LOGIN
+import com.simprints.core.tools.extentions.hasPermission
 import com.simprints.feature.externalcredential.R
 import com.simprints.feature.externalcredential.databinding.FragmentExternalCredentialQrScannerBinding
 import com.simprints.feature.externalcredential.screens.controller.ExternalCredentialViewModel
+import com.simprints.feature.externalcredential.tools.camera.CameraHelper
+import com.simprints.feature.externalcredential.tools.camera.QrCodeAnalyzer
+import com.simprints.infra.logging.Simber
+import com.simprints.infra.uibase.navigation.finishWithResult
 import com.simprints.infra.uibase.viewbinding.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import java.util.UUID
+import javax.inject.Inject
 
 @AndroidEntryPoint
 internal class ExternalCredentialQrScannerFragment : Fragment(R.layout.fragment_external_credential_qr_scanner) {
     private val viewModel: ExternalCredentialViewModel by activityViewModels()
     private val binding by viewBinding(FragmentExternalCredentialQrScannerBinding::bind)
 
+    @Inject
+    lateinit var cameraHelper: CameraHelper
+
+    @Inject
+    lateinit var qrCodeAnalyzer: QrCodeAnalyzer
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initListeners()
-    }
 
-    private fun initListeners() {
-        binding.tempQrText.setOnClickListener {
-            viewModel.processExternalCredentialAndFinish(data = UUID.randomUUID().toString())
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                qrCodeAnalyzer.scannedCode
+                    .catch { e ->
+                        Simber.e("Camera not available for QR scanning", e, tag = LOGIN)
+                        // TODO [MS-964] Handle error properly
+                        Toast.makeText(requireActivity(), "No camera", Toast.LENGTH_LONG).show()
+                    }.collectLatest { qrCode ->
+                        if (qrCode.isNotEmpty()) {
+                            viewModel.processExternalCredentialAndFinish(data = qrCode)
+                        }
+                    }
+            }
+        }
+
+        if (requireActivity().hasPermission(CAMERA)) {
+            startCamera()
         }
     }
+
+    private fun startCamera() {
+        binding.qrScannerAim.isVisible = true
+        cameraHelper.startCamera(
+            viewLifecycleOwner,
+            binding.qrScannerPreview,
+            qrCodeAnalyzer,
+        ) {
+            // On error
+            // TODO [MS-964] Handle error properly
+            Toast.makeText(requireActivity(), "No Camera available", Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
