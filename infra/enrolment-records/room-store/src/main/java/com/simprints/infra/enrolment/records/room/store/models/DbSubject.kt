2c5a15da7191d626414f1afed5eff1bf9fd155a7
Melad Raouf <melad.raouf@gmail.com>
2025-04-05 11:23:08 +0200

    [MS-951] Refactor enrolment records data handling and update database schema for Room integration

--- infra/enrolment-records/room-store/src/main/java/com/simprints/infra/enrolment/records/room/store/models/DbSubject.kt
+++ infra/enrolment-records/room-store/src/main/java/com/simprints/infra/enrolment/records/room/store/models/DbSubject.kt
@@ -6,11 +6,13 @@ import androidx.room.PrimaryKey
 import com.simprints.infra.enrolment.records.room.store.models.DbSubject.Companion.ATTENDANT_ID_COLUMN
 import com.simprints.infra.enrolment.records.room.store.models.DbSubject.Companion.MODULE_ID_COLUMN
 import com.simprints.infra.enrolment.records.room.store.models.DbSubject.Companion.PROJECT_ID_COLUMN
+import com.simprints.infra.enrolment.records.room.store.models.DbSubject.Companion.SUBJECT_ID_COLUMN
 import java.util.UUID
 
 @Entity(
     tableName = "DbSubject",
     indices = [
+        Index(value = [SUBJECT_ID_COLUMN]),
         Index(value = [PROJECT_ID_COLUMN]),
         Index(value = [ATTENDANT_ID_COLUMN]),
         Index(value = [MODULE_ID_COLUMN]),
@@ -19,12 +21,11 @@ import java.util.UUID
 data class DbSubject(
     @PrimaryKey
     val subjectId: String = UUID.randomUUID().toString(),
-    val projectId: String? = "",
-    val attendantId: String? = "",
-    val moduleId: String? = "",
+    val projectId: String = "",
+    val attendantId: String = "",
+    val moduleId: String = "",
     val createdAt: Long? = 0,
     val updatedAt: Long? = 0,
-    val toSync: Boolean = false,
     val isAttendantIdTokenized: Boolean = false,
     val isModuleIdTokenized: Boolean = false,
 ) {
