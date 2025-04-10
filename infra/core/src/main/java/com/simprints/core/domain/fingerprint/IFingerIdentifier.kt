8351505105ad1334fde5bc26af1e1b84936233af
Melad Raouf <melad.raouf@gmail.com>
2025-04-10 12:55:23 +0200

    [MS-951] Introduce DbSubjectTemplateFormatMap table to improve query performance.

--- infra/core/src/main/java/com/simprints/core/domain/fingerprint/IFingerIdentifier.kt
+++ infra/core/src/main/java/com/simprints/core/domain/fingerprint/IFingerIdentifier.kt
@@ -5,15 +5,26 @@ import com.simprints.core.ExcludedFromGeneratedTestCoverageReports
 
 @Keep
 @ExcludedFromGeneratedTestCoverageReports("Enum")
-enum class IFingerIdentifier {
-    RIGHT_5TH_FINGER,
-    RIGHT_4TH_FINGER,
-    RIGHT_3RD_FINGER,
-    RIGHT_INDEX_FINGER,
-    RIGHT_THUMB,
-    LEFT_THUMB,
-    LEFT_INDEX_FINGER,
-    LEFT_3RD_FINGER,
-    LEFT_4TH_FINGER,
-    LEFT_5TH_FINGER,
+// **IMPORTANT**: Do NOT change the order of this enum as it is used in the database by index.
+// Changing the order of the entries in this enum will lead to data corruption or mismatches
+// when retrieving or storing data in the database. Always append new entries at the end.
+enum class IFingerIdentifier(
+    val id: Int,
+) {
+    RIGHT_5TH_FINGER(0),
+    RIGHT_4TH_FINGER(1),
+    RIGHT_3RD_FINGER(2),
+    RIGHT_INDEX_FINGER(3),
+    RIGHT_THUMB(4),
+    LEFT_THUMB(5),
+    LEFT_INDEX_FINGER(6),
+    LEFT_3RD_FINGER(7),
+    LEFT_4TH_FINGER(8),
+    LEFT_5TH_FINGER(9),
+    ;
+
+    // create from id
+    companion object {
+        fun fromId(id: Int) = entries.firstOrNull { it.id == id } ?: throw IllegalArgumentException("Invalid id: $id")
+    }
 }
