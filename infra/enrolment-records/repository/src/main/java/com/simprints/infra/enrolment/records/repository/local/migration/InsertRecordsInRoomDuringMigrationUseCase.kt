1761b4ee890074c5eeca83b0f5ff362dc0857ed2
Melad Raouf <melad.raouf@gmail.com>
2025-06-05 21:20:43 +0300

    Move logging updates during migration to InsertRecordsInRoomDuringMigrationUseCase

--- infra/enrolment-records/repository/src/main/java/com/simprints/infra/enrolment/records/repository/local/migration/InsertRecordsInRoomDuringMigrationUseCase.kt
+++ infra/enrolment-records/repository/src/main/java/com/simprints/infra/enrolment/records/repository/local/migration/InsertRecordsInRoomDuringMigrationUseCase.kt
@@ -13,14 +13,38 @@ internal class InsertRecordsInRoomDuringMigrationUseCase @Inject constructor(
     private val roomEnrolmentRecordLocalDataSource: RoomEnrolmentRecordLocalDataSource,
 ) {
     suspend operator fun invoke(
-        subjectAction: SubjectAction.Creation,
+        actions: List<SubjectAction>,
         project: Project,
     ) {
         // if the realm to room migration is in progress, we need to insert the records in the new db too
+
         if (realmToRoomMigrationFlagsStore.isMigrationInProgress()) {
-            roomEnrolmentRecordLocalDataSource.performActions(actions = listOf(subjectAction), project)
-            Simber.i(
-                "[InsertRecordsInRoomDuringMigrationUseCase] Inserted subject ${subjectAction.subject.subjectId} enrolled during migration",
+            actions.filterIsInstance<SubjectAction.Creation>().forEach {
+                insertRecordInRoom(it, project)
+            }
+            logUpdatesDuringMigration(actions)
+        }
+    }
+
+    private suspend fun insertRecordInRoom(
+        creation: SubjectAction.Creation,
+        project: Project,
+    ) {
+        roomEnrolmentRecordLocalDataSource.performActions(actions = listOf(creation), project)
+        Simber.i(
+            "[InsertRecordsInRoomDuringMigrationUseCase] Inserted subject ${creation.subject.subjectId} enrolled during migration",
+            tag = REALM_DB_MIGRATION,
+        )
+    }
+
+    private fun logUpdatesDuringMigration(actions: List<SubjectAction>) {
+        if (actions.any { it is SubjectAction.Update || it is SubjectAction.Deletion }) {
+            // if actions contains any updates or deletes and the migration is in progress, log them as an error
+            Simber.e(
+                "[EnrolmentRecordRepositoryImpl] Actions during migration: ${actions.joinToString(", ")}",
+                IllegalStateException(
+                    "Actions during migration are not allowed. Please ensure that the migration is complete before performing updates or deletions.",
+                ),
                 tag = REALM_DB_MIGRATION,
             )
         }
