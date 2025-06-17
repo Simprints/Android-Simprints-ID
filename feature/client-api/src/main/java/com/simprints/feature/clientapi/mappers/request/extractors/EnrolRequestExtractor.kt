dcd6e90d92eea6b71c76aa006958e7785d734042
Marinov <milen@simprints.com>
2025-06-17 17:23:35 +0300

    Add biometricDataSource to expectedKeys of Enrol, Identify and Verify requests

--- feature/client-api/src/main/java/com/simprints/feature/clientapi/mappers/request/extractors/EnrolRequestExtractor.kt
+++ feature/client-api/src/main/java/com/simprints/feature/clientapi/mappers/request/extractors/EnrolRequestExtractor.kt
@@ -1,7 +1,11 @@
 package com.simprints.feature.clientapi.mappers.request.extractors
 
+import com.simprints.libsimprints.Constants
+
 internal open class EnrolRequestExtractor(
     extras: Map<String, Any>,
 ) : ActionRequestExtractor(extras) {
-    override val expectedKeys = super.keys
+    override val expectedKeys = super.keys + listOf(
+        Constants.SIMPRINTS_BIOMETRIC_DATA_SOURCE,
+    )
 }
