938dee07a7c4d4b3607a559050a4e7bd84bf9368
Marinov <milen@simprints.com>
2025-07-08 20:27:25 +0300

    [MS-1044] Refactor sync classes to avoid code duplication [WIP]

--- infra/event-sync/src/main/java/com/simprints/infra/eventsync/sync/down/workers/BaseEventDownSyncDownloaderWorker.kt
+++ infra/event-sync/src/main/java/com/simprints/infra/eventsync/sync/down/workers/BaseEventDownSyncDownloaderWorker.kt
@@ -1,52 +1,39 @@
-package com.simprints.infra.eventsync.sync.commcare.workers
+package com.simprints.infra.eventsync.sync.down.workers
 
 import android.content.Context
-import androidx.hilt.work.HiltWorker
 import androidx.work.WorkInfo
 import androidx.work.WorkerParameters
 import androidx.work.workDataOf
-import com.simprints.core.DispatcherBG
 import com.simprints.core.tools.json.JsonHelper
 import com.simprints.core.workers.SimCoroutineWorker
 import com.simprints.infra.config.store.ConfigRepository
 import com.simprints.infra.events.EventRepository
 import com.simprints.infra.eventsync.status.down.EventDownSyncScopeRepository
 import com.simprints.infra.eventsync.status.down.domain.EventDownSyncOperation
-import com.simprints.infra.eventsync.sync.commcare.tasks.CommCareEventSyncTask
 import com.simprints.infra.eventsync.sync.common.EventSyncCache
-import com.simprints.infra.eventsync.sync.common.OUTPUT_FAILED_BECAUSE_PERMISSION_REQUIRED
 import com.simprints.infra.eventsync.sync.common.WorkerProgressCountReporter
-import com.simprints.infra.eventsync.sync.down.workers.EventDownSyncDownloaderWorker.Companion.OUTPUT_DOWN_MAX_SYNC
-import com.simprints.infra.eventsync.sync.down.workers.EventDownSyncDownloaderWorker.Companion.OUTPUT_DOWN_SYNC
-import com.simprints.infra.eventsync.sync.down.workers.EventDownSyncDownloaderWorker.Companion.PROGRESS_DOWN_MAX_SYNC
-import com.simprints.infra.eventsync.sync.down.workers.EventDownSyncDownloaderWorker.Companion.PROGRESS_DOWN_SYNC
+import com.simprints.infra.eventsync.sync.down.tasks.BaseEventDownSyncTask
 import com.simprints.infra.logging.Simber
-import dagger.assisted.Assisted
-import dagger.assisted.AssistedInject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 
-@HiltWorker
-internal class CommCareEventSyncDownloaderWorker @AssistedInject constructor(
-    @Assisted context: Context,
-    @Assisted params: WorkerParameters,
-    private val commCareSyncTask: CommCareEventSyncTask,
-    private val eventDownSyncScopeRepository: EventDownSyncScopeRepository,
+internal abstract class BaseEventDownSyncDownloaderWorker(
+    context: Context,
+    params: WorkerParameters,
+    protected val eventDownSyncScopeRepository: EventDownSyncScopeRepository,
     private val syncCache: EventSyncCache,
-    private val jsonHelper: JsonHelper,
-    private val eventRepository: EventRepository,
-    private val configRepository: ConfigRepository,
-    @DispatcherBG private val dispatcher: CoroutineDispatcher,
-) : SimCoroutineWorker(context, params),
-    WorkerProgressCountReporter {
+    protected val jsonHelper: JsonHelper,
+    protected val eventRepository: EventRepository,
+    protected val configRepository: ConfigRepository,
+    private val dispatcher: CoroutineDispatcher,
+) : SimCoroutineWorker(context, params), WorkerProgressCountReporter {
+
     override val tag: String = "EventDownSyncDownloader"
 
     private val downSyncOperationInput by lazy {
         val jsonInput = inputData.getString(INPUT_DOWN_SYNC_OPS)
             ?: throw IllegalArgumentException("input required")
-        jsonHelper.fromJson<EventDownSyncOperation>(
-            jsonInput,
-        )
+        jsonHelper.fromJson<EventDownSyncOperation>(jsonInput)
     }
 
     private suspend fun getEventScope() = inputData
@@ -55,6 +42,10 @@ internal class CommCareEventSyncDownloaderWorker @AssistedInject constructor(
         ?: throw IllegalArgumentException("input required")
 
     private suspend fun getDownSyncOperation() = eventDownSyncScopeRepository.refreshState(downSyncOperationInput)
+
+    abstract fun createDownSyncTask(): BaseEventDownSyncTask
+
+    abstract fun handleSyncException(t: Throwable): Result
 
     override suspend fun doWork(): Result = withContext(dispatcher) {
         crashlyticsLog("Started")
@@ -65,7 +56,7 @@ internal class CommCareEventSyncDownloaderWorker @AssistedInject constructor(
             var max: Int? = syncCache.readMax(workerId)
             val project = configRepository.getProject()
 
-            commCareSyncTask.downSync(this, getDownSyncOperation(), getEventScope(), project).collect {
+            createDownSyncTask().downSync(this, getDownSyncOperation(), getEventScope(), project).collect {
                 count = it.progress
                 max = it.maxProgress
                 syncCache.saveProgress(workerId, count)
@@ -86,13 +77,6 @@ internal class CommCareEventSyncDownloaderWorker @AssistedInject constructor(
         }
     }
 
-    private fun handleSyncException(t: Throwable) = when (t) {
-        is IllegalArgumentException -> fail(t, t.message)
-        //TODO(milen): This should be handled in UI to show a proper error message
-        is SecurityException -> fail(t, t.message, workDataOf(OUTPUT_FAILED_BECAUSE_PERMISSION_REQUIRED to true))
-        else -> retry(t)
-    }
-
     override suspend fun reportCount(
         count: Int,
         maxCount: Int?,
@@ -108,7 +92,6 @@ internal class CommCareEventSyncDownloaderWorker @AssistedInject constructor(
     companion object {
         const val INPUT_DOWN_SYNC_OPS = "INPUT_DOWN_SYNC_OPS"
         const val INPUT_EVENT_DOWN_SYNC_SCOPE_ID = "INPUT_EVENT_DOWN_SYNC_SCOPE_ID"
-        //TODO(milen): This should probably be extracted to a common file, as it is used in both down workers
         const val PROGRESS_DOWN_SYNC = "PROGRESS_DOWN_SYNC"
         const val PROGRESS_DOWN_MAX_SYNC = "PROGRESS_DOWN_MAX_SYNC"
         const val OUTPUT_DOWN_SYNC = "OUTPUT_DOWN_SYNC"
@@ -117,8 +100,8 @@ internal class CommCareEventSyncDownloaderWorker @AssistedInject constructor(
 }
 
 internal suspend fun WorkInfo.extractDownSyncProgress(eventSyncCache: EventSyncCache): Int {
-    val progress = this.progress.getInt(PROGRESS_DOWN_SYNC, -1)
-    val output = this.outputData.getInt(OUTPUT_DOWN_SYNC, -1)
+    val progress = this.progress.getInt(BaseEventDownSyncDownloaderWorker.PROGRESS_DOWN_SYNC, -1)
+    val output = this.outputData.getInt(BaseEventDownSyncDownloaderWorker.OUTPUT_DOWN_SYNC, -1)
 
     // When the worker is not running (e.g. ENQUEUED due to errors), the output and progress are cleaned.
     val cached = eventSyncCache.readProgress(id.toString())
@@ -126,8 +109,8 @@ internal suspend fun WorkInfo.extractDownSyncProgress(eventSyncCache: EventSyncC
 }
 
 internal suspend fun WorkInfo.extractDownSyncMaxCount(eventSyncCache: EventSyncCache): Int {
-    val progress = this.progress.getInt(PROGRESS_DOWN_MAX_SYNC, -1)
-    val output = this.outputData.getInt(OUTPUT_DOWN_MAX_SYNC, -1)
+    val progress = this.progress.getInt(BaseEventDownSyncDownloaderWorker.PROGRESS_DOWN_MAX_SYNC, -1)
+    val output = this.outputData.getInt(BaseEventDownSyncDownloaderWorker.OUTPUT_DOWN_MAX_SYNC, -1)
 
     // When the worker is not running (e.g. ENQUEUED due to errors), the output and progress are cleaned.
     val cached = eventSyncCache.readMax(id.toString())
