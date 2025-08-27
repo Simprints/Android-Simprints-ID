cfd741459ae37825dc5a8d98fcfb5dc8d311453d
Melad Raouf <melad.raouf@gmail.com>
2025-08-27 18:56:07 +0100

    [MS-1133] Implement Realm integration tests

---
+++ infra/enrolment-records/repository/src/androidTest/kotlin/com/simprints/infra/enrolment/records/repository/local/HiltTestRunner.kt
@@ -0,0 +1,14 @@
+package com.simprints.infra.enrolment.records.repository.local
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+class HiltTestRunner : AndroidJUnitRunner() {
+    override fun newApplication(
+        cl: ClassLoader,
+        name: String,
+        context: Context,
+    ): Application = super.newApplication(cl, HiltTestApplication::class.java.name, context)
+}
