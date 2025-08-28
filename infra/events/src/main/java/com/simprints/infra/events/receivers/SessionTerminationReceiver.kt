ba8c5c63992b2281b4dbd9599f965cabd4fe721c
Melad Raouf <melad.raouf@gmail.com>
2025-08-28 14:35:58 +0100

    revert the SessionTerminationReceiver changes

--- infra/events/src/main/java/com/simprints/infra/events/receivers/SessionTerminationReceiver.kt
+++ infra/events/src/main/java/com/simprints/infra/events/receivers/SessionTerminationReceiver.kt
@@ -5,38 +5,22 @@ import android.content.Context
 import android.content.Intent
 import com.simprints.core.ExcludedFromGeneratedTestCoverageReports
 import com.simprints.core.tools.time.TimeHelper
-import dagger.hilt.InstallIn
-import dagger.hilt.android.EarlyEntryPoint
-import dagger.hilt.android.EarlyEntryPoints
-import dagger.hilt.components.SingletonComponent
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @ExcludedFromGeneratedTestCoverageReports("Platform glue code, actual logic is in the use cases")
+@AndroidEntryPoint
 internal class SessionTerminationReceiver : BroadcastReceiver() {
-    // Normal Hilt injection works fine when running the app normally.
-    // However, during Android instrumentation tests the process crashes because
-    // the BroadcastReceiver is created before Hilt can inject dependencies.
-    // To work around this, we use an EarlyEntryPoint to fetch dependencies manually.
-    // See: https://github.com/google/dagger/issues/4903
-    @EarlyEntryPoint
-    @InstallIn(SingletonComponent::class)
-    interface SessionTerminationEntryPoint {
-        fun timeHelper(): TimeHelper
+    @Inject
+    lateinit var timeHelper: TimeHelper
 
-        fun closeSessionUseCase(): CloseSessionIfPresentUseCase
-    }
+    @Inject
+    lateinit var closeSessionUseCase: CloseSessionIfPresentUseCase
 
     override fun onReceive(
         context: Context,
         intent: Intent,
     ) {
-        // Get the early entry point to access dependencies
-        val entryPoint = EarlyEntryPoints.get(
-            context.applicationContext,
-            SessionTerminationEntryPoint::class.java,
-        )
-        val timeHelper = entryPoint.timeHelper()
-        val closeSessionUseCase = entryPoint.closeSessionUseCase()
-
         timeHelper.ensureTrustworthiness()
         if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
             closeSessionUseCase()
