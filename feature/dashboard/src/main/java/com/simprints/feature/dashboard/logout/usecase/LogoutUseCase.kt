dad5df4fd81c39f63155edcd6ba4d52458faaa22
Melad Raouf <melad.raouf@gmail.com>
2025-09-04 16:43:00 +0100

    [MS-1061] Close database connections on logout

--- feature/dashboard/src/main/java/com/simprints/feature/dashboard/logout/usecase/LogoutUseCase.kt
+++ feature/dashboard/src/main/java/com/simprints/feature/dashboard/logout/usecase/LogoutUseCase.kt
@@ -1,6 +1,7 @@
 package com.simprints.feature.dashboard.logout.usecase
 
 import com.simprints.infra.authlogic.AuthManager
+import com.simprints.infra.enrolment.records.repository.EnrolmentRecordRepository
 import com.simprints.infra.enrolment.records.repository.local.migration.RealmToRoomMigrationFlagsStore
 import com.simprints.infra.sync.SyncOrchestrator
 import kotlinx.coroutines.runBlocking
@@ -10,14 +11,15 @@ internal class LogoutUseCase @Inject constructor(
     private val syncOrchestrator: SyncOrchestrator,
     private val authManager: AuthManager,
     private val flagsStore: RealmToRoomMigrationFlagsStore,
+    private val enrolmentRecordRepository: EnrolmentRecordRepository,
 ) {
     operator fun invoke() = runBlocking {
-        // Cancel all background sync
         syncOrchestrator.cancelBackgroundWork()
         syncOrchestrator.deleteEventSyncInfo()
         // sign out the user
         authManager.signOut()
         // Reset migration flags
         flagsStore.clearMigrationFlags()
+        enrolmentRecordRepository.closeOpenDbConnection()
     }
 }
