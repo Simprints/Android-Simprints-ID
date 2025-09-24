c524a789f8639a3c11ae7e7d0fe610dab7fd155e
alex <alexandr@simprints.com>
2025-09-24 19:49:52 +0300

    [MS-1165] Moving field into a constant

--- feature/external-credential/src/main/java/com/simprints/feature/externalcredential/screens/scanqr/usecase/ExternalCredentialQrCodeValidatorUseCase.kt
+++ feature/external-credential/src/main/java/com/simprints/feature/externalcredential/screens/scanqr/usecase/ExternalCredentialQrCodeValidatorUseCase.kt
@@ -3,13 +3,15 @@ package com.simprints.feature.externalcredential.screens.scanqr.usecase
 import javax.inject.Inject
 
 internal class ExternalCredentialQrCodeValidatorUseCase @Inject constructor() {
-    private val qrCodeLength = 6
-
     /**
      * Checks whether the scanned QR code value is valid to be used in the Multi-Factor ID. Currently, it uses hardcoded values.
      * In future, the validity criteria should be passed from the project configuration.
      */
     operator fun invoke(qrCodeValue: String): Boolean {
-        return qrCodeValue.length == qrCodeLength
+        return qrCodeValue.length == QR_CODE_LENGTH
+    }
+
+    companion object {
+        private const val QR_CODE_LENGTH = 6
     }
 }
