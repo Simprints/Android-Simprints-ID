146c61d5619b924eb686262435bad590f7c0c9ec
Melad Raouf <melad.raouf@gmail.com>
2025-11-14 14:37:47 +0000

    [MS-1253] Update Dependencies

--- fingerprint/infra/scannermock/src/test/java/com/simprints/fingerprint/infra/scannermock/ByteArrayUtilsTest.kt
+++ fingerprint/infra/scannermock/src/test/java/com/simprints/fingerprint/infra/scannermock/ByteArrayUtilsTest.kt
@@ -9,7 +9,7 @@ import java.nio.ByteBuffer
 class ByteArrayUtilsTest {
     @Test
     fun convertingStringToByteArrayAndBack_works() {
-        val string = "F0 F0 F0 F0 F0 02 02 02 01 00 99 AA BB CC D3 3d ".toLowerCase()
+        val string = "F0 F0 F0 F0 F0 02 02 02 01 00 99 AA BB CC D3 3d ".lowercase()
         val bytes = hexStringToByteArray(string)
         val newString = Message.hexString(ByteBuffer.wrap(bytes), 0, bytes.size)
         assertEquals(string, newString)
