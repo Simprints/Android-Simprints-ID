8bf37d406b56ef9398660ef12741445655ff2927
Melad Raouf <melad.raouf@gmail.com>
2026-01-19 21:18:10 +0000

    Remove `@OptIn(ExperimentalSerializationApi::class)` and associated import from `EventPayload`

--- infra/events/src/main/java/com/simprints/infra/events/event/domain/models/EventPayload.kt
+++ infra/events/src/main/java/com/simprints/infra/events/event/domain/models/EventPayload.kt
@@ -1,12 +1,10 @@
 package com.simprints.infra.events.event.domain.models
 
 import com.simprints.core.tools.time.Timestamp
-import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 
 @Serializable
 sealed class EventPayload {
-    @OptIn(ExperimentalSerializationApi::class)
     abstract val type: EventType
     abstract val eventVersion: Int
     abstract val createdAt: Timestamp
