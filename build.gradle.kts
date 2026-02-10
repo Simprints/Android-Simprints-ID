a2f0389c8da62a795e90018b8d96f9ff168871ad
Melad Raouf <melad.raouf@gmail.com>
2026-02-10 21:49:24 +0000

    Update dependencies and remove unused Kotlin Android plugin
    - Update `kotlin_serialization_version` to 1.10.0
    - Update `android_gradlePlugin_version` to 9.0.0
    - Update `hilt_version` to 2.59.1
    - Update `play_publisher_version` to 4.0.0
    - Remove `kotlin-android` plugin definition from `libs.versions.toml`

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.test) apply false
-    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
 
