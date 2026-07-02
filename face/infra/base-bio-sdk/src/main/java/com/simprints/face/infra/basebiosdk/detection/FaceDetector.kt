56ddd24e3aebe36e87c5191655f2d196d20f4193
Sergejs Luhmirins <sergejs@yetanotherapp.dev>
2026-07-02 15:39:14 +0300

    MS-1491 Add spoof check to face detector API

--- face/infra/base-bio-sdk/src/main/java/com/simprints/face/infra/basebiosdk/detection/FaceDetector.kt
+++ face/infra/base-bio-sdk/src/main/java/com/simprints/face/infra/basebiosdk/detection/FaceDetector.kt
@@ -2,7 +2,7 @@ package com.simprints.face.infra.basebiosdk.detection
 
 import android.graphics.Bitmap
 
-fun interface FaceDetector {
+interface FaceDetector {
     /**
      * Analyze an ARGB_8888 bitmap and return the detected face data
      *
@@ -10,4 +10,12 @@ fun interface FaceDetector {
      * @return Face object or null if no face is detected
      */
     fun analyze(bitmap: Bitmap): Face?
+
+    /**
+     * Perform a spoof check on an ARGB_8888 bitmap
+     *
+     * @param bitmap original captured image (ARGB_8888)
+     * @return Either a spoof score (lower is better) or a reason why the check was skipped
+     */
+    fun spoofCheck(bitmap: Bitmap): SpoofCheckResult
 }
--- face/infra/roc-v3/src/main/java/com/simprints/face/infra/rocv3/detection/RocV3Detector.kt
+++ face/infra/roc-v3/src/main/java/com/simprints/face/infra/rocv3/detection/RocV3Detector.kt
@@ -10,9 +10,11 @@ import android.graphics.Rect
 import com.simprints.core.ExcludedFromGeneratedTestCoverageReports
 import com.simprints.face.infra.basebiosdk.detection.Face
 import com.simprints.face.infra.basebiosdk.detection.FaceDetector
+import com.simprints.face.infra.basebiosdk.detection.SpoofCheckResult
 import java.nio.ByteBuffer
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.math.abs
 import ai.roc.rocsdk.embedded.roc as roc3
 
 @ExcludedFromGeneratedTestCoverageReports(
@@ -172,11 +174,118 @@ class RocV3Detector @Inject constructor() : FaceDetector {
         return byteBuffer
     }
 
+    // TODO verify results
+    override fun spoofCheck(bitmap: Bitmap): SpoofCheckResult {
+        if (minOf(bitmap.width, bitmap.height) < SPOOF_MIN_SIZE) {
+            // As per documentation - smallest dimension must be at least 720px
+            return SpoofCheckResult(0f, SpoofCheckResult.SkipReason.IMAGE_TOO_SMALL)
+        }
+
+        val rocColorImage = roc_image()
+        val rocGrayImage = roc_image()
+        val byteBuffer = bitmap.toByteBuffer()
+        roc3.roc_from_rgba(
+            byteBuffer.array(),
+            bitmap.width.toLong(),
+            bitmap.height.toLong(),
+            bitmap.rowBytes.toLong(),
+            rocColorImage,
+        )
+        roc3.roc_bgr2gray(rocColorImage, rocGrayImage)
+
+        val detection = roc_detection()
+        val faceDetected = isFaceDetected(rocColorImage, detection)
+
+        var iod = 0f
+        var finalScore = 0f
+
+        val quality = roc3.new_float() // TODO remove later
+
+        if (faceDetected) {
+            val rightEye = roc_landmark()
+            val leftEye = roc_landmark()
+            val chin = roc_landmark()
+
+            val landmarks = roc3.new_roc_landmark_array(roc3.roc_num_landmarks_for_pose(detection.pose))
+            roc3.roc_embedded_landmark_face(
+                rocGrayImage,
+                detection,
+                landmarks,
+                rightEye,
+                leftEye,
+                chin,
+                null,
+                null,
+            )
+            roc3.delete_roc_landmark_array(landmarks)
+
+            iod = abs(leftEye.x - rightEye.x)
+
+            // As per documentation - Inter-ocular distance must be in 100-320px range
+            if (iod in SPOOF_MIN_IOD..SPOOF_MAX_IOD) {
+                val embeddingSpoof = roc3.new_float()
+                val livenessSpoof = roc3.new_float()
+
+                roc3.roc_embedded_represent_face(
+                    rocColorImage,
+                    detection,
+                    rightEye,
+                    leftEye,
+                    chin,
+                    null,
+                    quality,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    embeddingSpoof,
+                )
+                val embeddingScore = roc3.float_value(embeddingSpoof)
+                roc3.delete_float(embeddingSpoof)
+
+                roc3.roc_embedded_liveness(
+                    rocGrayImage,
+                    rightEye,
+                    leftEye,
+                    chin,
+                    livenessSpoof,
+                )
+                val livenessScore = roc3.float_value(livenessSpoof)
+                roc3.delete_float(livenessSpoof)
+
+                // As per documentation - weighted average of 2 call scores with 75% weight for direct liveness result
+                finalScore = ((3 * livenessScore) + embeddingScore) / 4f
+            }
+            leftEye.delete()
+            rightEye.delete()
+            chin.delete()
+        }
+        roc3.roc_free_image(rocGrayImage)
+        roc3.roc_free_image(rocColorImage)
+        detection.delete()
+        byteBuffer.clear()
+
+        return when {
+            !faceDetected -> SpoofCheckResult(finalScore, SpoofCheckResult.SkipReason.NOT_AVAILABLE)
+            iod < SPOOF_MIN_IOD -> SpoofCheckResult(finalScore, SpoofCheckResult.SkipReason.IOD_TOO_SMALL)
+            iod > SPOOF_MAX_IOD -> SpoofCheckResult(finalScore, SpoofCheckResult.SkipReason.IOD_TOO_LARGE)
+            else -> SpoofCheckResult(finalScore)
+        }
+    }
+
     companion object {
         const val RANK_ONE_TEMPLATE_FORMAT_3_1 = "RANK_ONE_3_1"
         const val MAX_FACE_DETECTION = 1
         const val FALSE_DETECTION_RATE = 0.1f
         const val RELATIVE_MIN_SIZE = 0.2f
         const val ABSOLUTE_MIN_SIZE = 36L
+
+        private const val SPOOF_MIN_SIZE = 720
+        private const val SPOOF_MIN_IOD = 100f
+        private const val SPOOF_MAX_IOD = 320f
     }
 }
