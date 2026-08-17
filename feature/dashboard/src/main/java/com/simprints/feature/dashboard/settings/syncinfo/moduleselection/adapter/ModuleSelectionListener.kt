f6cf07a9b8e8e329d4dd895a5d76ced1045505f5
Sergejs Luhmirins <sergejs@yetanotherapp.dev>
2026-08-17 11:10:13 +0300

    MS-1451 Remove old implementation

--- feature/dashboard/src/main/java/com/simprints/feature/dashboard/settings/syncinfo/moduleselection/adapter/ModuleSelectionListener.kt
+++
@@ -1,7 +0,0 @@
-package com.simprints.feature.dashboard.settings.syncinfo.moduleselection.adapter
-
-import com.simprints.infra.eventsync.module.SelectableModule
-
-internal interface ModuleSelectionListener {
-    fun onModuleSelected(module: SelectableModule)
-}
