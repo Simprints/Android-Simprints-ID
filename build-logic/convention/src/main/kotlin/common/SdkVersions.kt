854ab34d8996b1e8c6bd2821ae52ee4a07d528fc
Sergejs Luhmirins <sergejs@yetanotherapp.dev>
2026-09-01 17:16:12 +0300

    Raise target SKD version to match compile version

--- build-logic/convention/src/main/kotlin/common/SdkVersions.kt
+++ build-logic/convention/src/main/kotlin/common/SdkVersions.kt
@@ -5,7 +5,7 @@ import org.gradle.api.JavaVersion
 object SdkVersions {
     const val MIN = 24
     const val COMPILE = 37
-    const val TARGET = 36
+    const val TARGET = 37
 
     val JAVA_TARGET = JavaVersion.VERSION_21
 }
